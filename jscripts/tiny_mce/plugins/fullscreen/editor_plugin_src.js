1bea63595cd8af72969fdb5edf881e073582e16d
Spocke <spocke@moxiecode.com>
2006-10-22 19:55:28 +0000

    Fixed bug where Fullscreen plugin didn't fill containing elements if they where absolute.

--- jscripts/tiny_mce/plugins/fullscreen/editor_plugin_src.js
+++ jscripts/tiny_mce/plugins/fullscreen/editor_plugin_src.js
@@ -46,7 +46,7 @@ var TinyMCE_FullScreenPlugin = {
 	},
 
 	_toggleFullscreen : function(inst) {
-		var ds = inst.getData('fullscreen'), editorContainer, tableElm, iframe, vp, cw, cd, re, w, h;
+		var ds = inst.getData('fullscreen'), editorContainer, tableElm, iframe, vp, cw, cd, re, w, h, si;
 
 		cw = inst.getContainerWin();
 		cd = cw.document;
@@ -56,6 +56,37 @@ var TinyMCE_FullScreenPlugin = {
 		re = cd.getElementById(inst.editorId + '_resize');
 
 		if (!ds.enabled) {
+			ds.parents = [];
+
+			tinyMCE.getParentNode(tableElm.parentNode, function (n) {
+				var st = n.style;
+
+				if (n.nodeType == 1 && st) {
+					if (n.nodeName == 'BODY')
+						return true;
+
+					ds.parents.push({
+						el : n,
+						position : st.position,
+						left : st.left,
+						top : st.top,
+						right : st.right,
+						bottom : st.bottom,
+						width : st.width,
+						height : st.height,
+						margin : st.margin,
+						padding : st.padding,
+						border : st.border
+					});
+
+					st.position = 'static';
+					st.left = st.top = st.margin = st.padding = st.border = '0';
+					st.width = st.height = st.right = st.bottom = 'auto';
+				}
+
+				return false;
+			});
+
 			ds.oldOverflow = cd.body.style.overflow;
 			cd.body.style.overflow = 'hidden';
 
@@ -64,15 +95,21 @@ var TinyMCE_FullScreenPlugin = {
 
 			vp = tinyMCE.getViewPort(cw);
 
+			ds.oldWidth = iframe.style.width ? iframe.style.width : iframe.offsetWidth;
+			ds.oldHeight = iframe.style.height ? iframe.style.height : iframe.offsetHeight;
+			ds.oldTWidth = tableElm.style.width ? tableElm.style.width : tableElm.offsetWidth;
+			ds.oldTHeight = tableElm.style.height ? tableElm.style.height : tableElm.offsetHeight;
+
+			// Handle % width
+			if (ds.oldWidth && ds.oldWidth.indexOf)
+				ds.oldTWidth = ds.oldWidth.indexOf('%') != -1 ? ds.oldWidth : ds.oldTWidth;
+
 			tableElm.style.position = 'absolute';
 			tableElm.style.zIndex = 1000;
 			tableElm.style.left = tableElm.style.top = '0';
 
 			tableElm.style.width = vp.width + 'px';
 			tableElm.style.height = vp.height + 'px';
-
-			ds.oldWidth = iframe.style.offsetWidth;
-			ds.oldHeight = iframe.style.offsetHeight;
 
 			if (tinyMCE.isRealIE) {
 				iframe.style.width = vp.width + 'px';
@@ -98,14 +135,41 @@ var TinyMCE_FullScreenPlugin = {
 			tinyMCE.switchClass(inst.editorId + '_fullscreen', 'mceButtonSelected');
 			ds.enabled = true;
 		} else {
+			si = 0;
+			tinyMCE.getParentNode(tableElm.parentNode, function (n) {
+				var st = n.style, s = ds.parents[si++];
+
+				if (n.nodeName == 'BODY')
+					return true;
+
+				if (st) {
+					st.position = s.position;
+					st.left = s.left;
+					st.top = s.top;
+					st.bottom = s.bottom;
+					st.right = s.right;
+					st.width = s.width;
+					st.height = s.height;
+					st.margin = s.margin;
+					st.padding = s.padding;
+					st.border = s.border;
+				}
+			});
+
+			ds.parents = [];
+
 			cd.body.style.overflow = ds.oldOverflow ? ds.oldOverflow : '';
 
 			if (re && tinyMCE.getParam("theme_advanced_resizing", false))
 				re.style.display = 'block';
 
 			tableElm.style.position = 'static';
+			tableElm.style.zIndex = '';
 			tableElm.style.width = '';
 			tableElm.style.height = '';
+
+			tableElm.style.width = ds.oldTWidth ? ds.oldTWidth : '';
+			tableElm.style.height = ds.oldTHeight ? ds.oldTHeight : '';
 
 			iframe.style.width = ds.oldWidth ? ds.oldWidth : '';
 			iframe.style.height = ds.oldHeight ? ds.oldHeight : '';
