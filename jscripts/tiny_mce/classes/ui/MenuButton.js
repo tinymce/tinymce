7e3cb96b3b3555d653cff6af48f97d9652237cba
Spocke <spocke@moxiecode.com>
2008-01-14 19:14:50 +0000

    Fixed bug that made it impossible to visually disable a custom button that used an image instead of CSS sprites.

---
+++ jscripts/tiny_mce/classes/ui/MenuButton.js
@@ -0,0 +1,115 @@
+/**
+ * $Id: Button.js 520 2008-01-07 16:30:32Z spocke $
+ *
+ * @author Moxiecode
+ * @copyright Copyright © 2004-2008, Moxiecode Systems AB, All rights reserved.
+ */
+
+(function() {
+	var DOM = tinymce.DOM, Event = tinymce.dom.Event, each = tinymce.each;
+
+	/**#@+
+	 * @class This class is used to create a UI button. A button is basically a link
+	 * that is styled to look like a button or icon.
+	 * @member tinymce.ui.Button
+	 * @base tinymce.ui.Control
+	 */
+	tinymce.create('tinymce.ui.MenuButton:tinymce.ui.Button', {
+		/**
+		 * Constructs a new split button control instance.
+		 *
+		 * @param {String} id Control id for the split button.
+		 * @param {Object} s Optional name/value settings object.
+		 */
+		MenuButton : function(id, s) {
+			this.parent(id, s);
+			this.onRenderMenu = new tinymce.util.Dispatcher(this);
+			s.menu_container = s.menu_container || document.body;
+		},
+
+		/**#@+
+		 * @method
+		 */
+
+		/**
+		 * Shows the menu.
+		 */
+		showMenu : function() {
+			var t = this, p1, p2, e = DOM.get(t.id), m;
+
+			if (t.isDisabled())
+				return;
+
+			if (!t.isMenuRendered) {
+				t.renderMenu();
+				t.isMenuRendered = true;
+			}
+
+			p1 = DOM.getPos(t.settings.menu_container);
+			p2 = DOM.getPos(e);
+
+			m = t.menu;
+			m.settings.offset_x = p2.x;
+			m.settings.offset_y = p2.y;
+			m.settings.vp_offset_x = p2.x;
+			m.settings.vp_offset_y = p2.y;
+			m.showMenu(0, e.clientHeight);
+
+			Event.add(document, 'mousedown', t.hideMenu, t);
+			t.setState('Selected', 1);
+		},
+
+		/**
+		 * Renders the menu to the DOM.
+		 */
+		renderMenu : function() {
+			var t = this, m;
+
+			m = t.settings.control_manager.createDropMenu(t.id + '_menu', {
+				menu_line : 1,
+				'class' : this.classPrefix + 'Menu'
+			});
+
+			m.onHideMenu.add(t.hideMenu, t);
+
+			t.onRenderMenu.dispatch(t, m);
+			t.menu = m;
+		},
+
+		/**
+		 * Hides the menu. The optional event parameter is used to check where the event occured so it
+		 * doesn't close them menu if it was a event inside the menu.
+		 *
+		 * @param {Event} e Optional event object.
+		 */
+		hideMenu : function(e) {
+			var t = this;
+
+			if (!e || !DOM.getParent(e.target, function(n) {return DOM.hasClass(n, 'mceMenu');})) {
+				t.setState('Selected', 0);
+				Event.remove(document, 'mousedown', t.hideMenu, t);
+				if (t.menu)
+					t.menu.hideMenu();
+			}
+		},
+
+		/**
+		 * Post render handler. This function will be called after the UI has been
+		 * rendered so that events can be added.
+		 */
+		postRender : function() {
+			var t = this, s = t.settings;
+
+			Event.add(t.id, 'click', function() {
+				if (!t.isDisabled()) {
+					if (s.onclick)
+						s.onclick(t.value);
+
+					t.showMenu();
+				}
+			});
+		}
+
+		/**#@-*/
+	});
+})();
