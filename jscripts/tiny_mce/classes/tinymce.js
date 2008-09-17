33d7c97ce3d268f928f3ab42cfbf2fe4f8a88956
Spocke <spocke@moxiecode.com>
2008-09-17 11:56:56 +0000

    Fixed bug where font sizes and faces wouldn't be changed correctly when there was a parent with a different style.

--- jscripts/tiny_mce/classes/tinymce.js
+++ jscripts/tiny_mce/classes/tinymce.js
@@ -12,8 +12,8 @@
  */
 var tinymce = {
 	majorVersion : '3',
-	minorVersion : '2.1',
-	releaseDate : '2008-09-xx',
+	minorVersion : '2.0.1',
+	releaseDate : '2008-09-17',
 
 	/**#@+
 	 * @method
--- jscripts/tiny_mce/tiny_mce_src.js
+++ jscripts/tiny_mce/tiny_mce_src.js
@@ -3,8 +3,8 @@
 
 var tinymce = {
 	majorVersion : '3',
-	minorVersion : '2.1',
-	releaseDate : '2008-09-xx',
+	minorVersion : '2.0.1',
+	releaseDate : '2008-09-17',
 
 	_init : function() {
 		var t = this, d = document, w = window, na = navigator, ua = na.userAgent, i, nl, n, base, p, v;
