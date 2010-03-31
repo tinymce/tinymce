2f37f555818d0f9e175bb85b4c7f161d959b556d
Spocke <spocke@moxiecode.com>
2010-03-31 16:00:39 +0200

    Added various fixes and new rpc methods for the spellchecker plugin. Patch contributed by Michael Peters.

--- jscripts/tiny_mce/plugins/spellchecker/editor_plugin_src.js
+++ jscripts/tiny_mce/plugins/spellchecker/editor_plugin_src.js
@@ -40,7 +40,9 @@
 							ed.nodeChanged();
 						} else {
 							ed.setProgressState(0);
-							ed.windowManager.alert('spellchecker.no_mpell');
+
+							if (ed.getParam('spellchecker_report_no_misspellings', true))
+								ed.windowManager.alert('spellchecker.no_mpell');
 						}
 					});
 				} else
@@ -144,7 +146,7 @@
 		},
 
 		_getWords : function() {
-			var ed = this.editor, wl = [], tx = '', lo = {};
+			var ed = this.editor, wl = [], tx = '', lo = {}, rawWords = [];
 
 			// Get area text
 			this._walk(ed.getBody(), function(n) {
@@ -152,12 +154,19 @@
 					tx += n.nodeValue + ' ';
 			});
 
-			// Split words by separator
-			tx = tx.replace(new RegExp('([0-9]|[' + this._getSeparators() + '])', 'g'), ' ');
-			tx = tinymce.trim(tx.replace(/(\s+)/g, ' '));
+			// split the text up into individual words
+			if (ed.getParam('spellchecker_word_pattern')) {
+				// look for words that match the pattern
+				rawWords = tx.match('(' + ed.getParam('spellchecker_word_pattern') + ')', 'gi');
+			} else {
+				// Split words by separator
+				tx = tx.replace(new RegExp('([0-9]|[' + this._getSeparators() + '])', 'g'), ' ');
+				tx = tinymce.trim(tx.replace(/(\s+)/g, ' '));
+				rawWords = tx.split(' ');
+			}
 
 			// Build word array and remove duplicates
-			each(tx.split(' '), function(v) {
+			each(rawWords, function(v) {
 				if (!lo[v]) {
 					wl.push(v);
 					lo[v] = 1;
@@ -242,6 +251,8 @@
 				m.add({title : 'spellchecker.wait', 'class' : 'mceMenuItemTitle'}).setDisabled(1);
 
 				t._sendRPC('getSuggestions', [t.selectedLang, dom.decode(e.target.innerHTML)], function(r) {
+					var ignoreRpc;
+
 					m.removeAll();
 
 					if (r.length > 0) {
@@ -257,21 +268,60 @@
 					} else
 						m.add({title : 'spellchecker.no_sug', 'class' : 'mceMenuItemTitle'}).setDisabled(1);
 
+					ignoreRpc = t.editor.getParam("spellchecker_enable_ignore_rpc", '');
 					m.add({
 						title : 'spellchecker.ignore_word',
 						onclick : function() {
+							var word = e.target.innerHTML;
+
 							dom.remove(e.target, 1);
 							t._checkDone();
+
+							// tell the server if we need to
+							if (ignore_rpc) {
+								ed.setProgressState(1);
+								t._sendRPC('ignoreWord', [t.selectedLang, word], function(r) {
+									ed.setProgressState(0);
+								});
+							}
 						}
 					});
 
 					m.add({
 						title : 'spellchecker.ignore_words',
 						onclick : function() {
-							t._removeWords(dom.decode(e.target.innerHTML));
+							var word = e.target.innerHTML;
+
+							t._removeWords(dom.decode(word));
 							t._checkDone();
+
+							// tell the server if we need to
+							if (ignore_rpc) {
+								ed.setProgressState(1);
+								t._sendRPC('ignoreWords', [t.selectedLang, word], function(r) {
+									ed.setProgressState(0);
+								});
+							}
 						}
 					});
+
+
+					if (t.editor.getParam("spellchecker_enable_learn_rpc")) {
+						m.add({
+							title : 'spellchecker.learn_word',
+							onclick : function() {
+								var word = e.target.innerHTML;
+
+								dom.remove(e.target, 1);
+								t._checkDone();
+
+								ed.setProgressState(1);
+								t._sendRPC('learnWord', [t.selectedLang, word], function(r) {
+									ed.setProgressState(0);
+								});
+							}
+						});
+					}
 
 					m.update();
 				});
