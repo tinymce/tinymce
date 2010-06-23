504f323ea40ba006de46a4f6b6ffdb2601443731
Adrian Sutton <adrian.sutton@ephox.com>
2010-06-23 13:52:22 +0100

    MAINTENANCE: Refactored selection utils.

--- tests/js/utils.js
+++ tests/js/utils.js
@@ -5,3 +5,29 @@ function fontFace(face) {
 		return face;
 	}
 }
+
+function findContainer(selector) {
+	var container;
+	if (tinymce.is(selector, 'string')) {
+		container = editor.dom.select(selector)[0];
+	} else {
+		container = selector;
+	}
+	if (container.firstChild) {
+		container = container.firstChild;
+	}
+	return container;
+}
+
+function setSelection(startSelector, startOffset, endSelector, endOffset) {
+	if (!endSelector) {
+		endSelector = startSelector;
+		endOffset = startOffset;
+	}
+	var startContainer = findContainer(startSelector);
+	var endContainer = findContainer(endSelector);
+	var rng = editor.dom.createRng();
+	rng.setStart(startContainer, startOffset);
+	rng.setEnd(endContainer, endOffset);
+	editor.selection.setRng(rng);
+}
