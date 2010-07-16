27d8e43ef8477a01ea99b478025ff1809295d220
Yonas Yanfa <yonas.yanfa@ephox.com>
2010-07-16 16:23:47 -0400

    Add tests for autolink that use DSL natural language. The tests are broken, "action is not a function".

---
+++ tests/js/autolink/actions.js
@@ -0,0 +1,31 @@
+function fakeKeyPressAction(keyCode, shiftKey) {
+	return function(callback) {
+		setTimeout(function() {
+			window.robot.type(keyCode, shiftKey, callback);
+		}, 1);
+	};
+}
+
+function createAction(name, action) {
+	window[name.replace(/\s+/g, '')] = new dsl.Action(name, action);
+}
+
+function fakeTypeAURL(url)
+{
+    // enter the URL and then press the space bar
+    tinyMCE.execCommand('mceInsertContent', false, url); 
+    fakeKeyPressAction(32, false);
+}
+
+createAction('Typing HTTP URL', fakeTypeAURL('http://www.ephox.com'));
+createAction('Typing HTTPS URL', fakeTypeAURL('https://www.ephox.com'));
+createAction('Typing SSH URL', fakeTypeAURL('ssh://www.ephox.com'));
+createAction('Typing FTP URL', fakeTypeAURL('ftp://www.ephox.com'));
+createAction('Typing WWW URL', fakeTypeAURL('www.ephox.com'));
+createAction('Applying OL', 'InsertOrderedList');
+createAction('Applying UL', 'InsertUnorderedList');
+createAction('Indenting', 'Indent');
+createAction('Outdenting', 'Outdent');
+createAction('Typing Enter', fakeKeyPressAction('\n'));
+createAction('Typing Tab', fakeKeyPressAction('\t'));
+createAction('Typing Shift Tab', fakeKeyPressAction('\t', true));
