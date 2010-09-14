f04bf3390af4db48854fc5b666c11e902cb0f5dc
Adrian Sutton <adrian.sutton@ephox.com>
2010-09-14 09:44:35 +0100

    FIXED: When a plugin or other resource fails to load a message is logged to the console if available and the editor continues to load. Previously the editor would fail to load if any plugin wasn't found.

--- jscripts/tiny_mce/classes/dom/ScriptLoader.js
+++ jscripts/tiny_mce/classes/dom/ScriptLoader.js
@@ -40,6 +40,14 @@
 
 				callback();
 			};
+			
+			function error() {
+				// Report the error and then try to carry on anyway - it's probably just a plugin that will go missing.
+				if (typeof(console) !== "undefined" && console.log)
+					console.log("Failed to load: " + url);
+				
+				done();
+			};
 
 			id = dom.uniqueId();
 
@@ -64,7 +72,9 @@
 							dom.remove(script);
 
 							done();
-						}
+						},
+						
+						error : error
 					});
 
 					return;
@@ -80,6 +90,7 @@
 
 			// Add onload and readystate listeners
 			elm.onload = done;
+			elm.onerror = error;
 			elm.onreadystatechange = function() {
 				var state = elm.readyState;
 
