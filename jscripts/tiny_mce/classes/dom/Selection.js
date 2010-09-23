1ba7f7b60cc869fa8664ec6f2583e508ae91754d
Adrian Sutton <adrian.sutton@ephox.com>
2010-09-23 12:05:29 +0100

    FIXED: Problem where the end nodes skipped twice.

--- jscripts/tiny_mce/classes/dom/Selection.js
+++ jscripts/tiny_mce/classes/dom/Selection.js
@@ -753,7 +753,6 @@
 							start = start.parentNode;
 						}
 						if (rng.endOffset === 0) {
-							end = end.previousSibling;
 							end = skipEmptyTextNodes(end.previousSibling, false);
 						} else {
 							end = end.parentNode;
