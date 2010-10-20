035344062aad7fbd6a3be901786bea0181be75cb
Spocke <spocke@moxiecode.com>
2010-10-20 14:41:45 +0200

    Changed the radix to be 32 bits for the internal entities and added unit tests for the tinymce.html.Entities class.

--- jscripts/tiny_mce/classes/html/Entities.js
+++ jscripts/tiny_mce/classes/html/Entities.js
@@ -44,15 +44,16 @@
 	};
 
 	// Build a two way lookup table for the entities
-	function buildEntitiesLookup(items) {
+	function buildEntitiesLookup(items, radix) {
 		var i, chr, entity, lookup = {};
 
 		if (items) {
 			items = items.split(',');
+			radix = radix || 10;
 
 			// Build entities lookup table
 			for (i = 0; i < items.length; i += 2) {
-				chr = String.fromCharCode(items[i]);
+				chr = String.fromCharCode(parseInt(items[i], radix));
 
 				// Only add non base entities
 				if (!baseEntities[chr]) {
@@ -66,47 +67,34 @@
 		}
 	};
 
+	// Unpack entities lookup where the numbers are in radix 32 to reduce the size
 	namedEntities = buildEntitiesLookup(
-		'160,nbsp,161,iexcl,162,cent,163,pound,164,curren,165,yen,' +
-		'166,brvbar,167,sect,168,uml,169,copy,170,ordf,171,laquo,172,not,' +
-		'173,shy,174,reg,175,macr,176,deg,177,plusmn,178,sup2,179,sup3,' +
-		'180,acute,181,micro,182,para,183,middot,184,cedil,185,sup1,' +
-		'186,ordm,187,raquo,188,frac14,189,frac12,190,frac34,191,iquest,' +
-		'192,Agrave,193,Aacute,194,Acirc,195,Atilde,196,Auml,197,Aring,' +
-		'198,AElig,199,Ccedil,200,Egrave,201,Eacute,202,Ecirc,203,Euml,' +
-		'204,Igrave,205,Iacute,206,Icirc,207,Iuml,208,ETH,209,Ntilde,' +
-		'210,Ograve,211,Oacute,212,Ocirc,213,Otilde,214,Ouml,215,times,' +
-		'216,Oslash,217,Ugrave,218,Uacute,219,Ucirc,220,Uuml,221,Yacute,' +
-		'222,THORN,223,szlig,224,agrave,225,aacute,226,acirc,227,atilde,' +
-		'228,auml,229,aring,230,aelig,231,ccedil,232,egrave,233,eacute,' +
-		'234,ecirc,235,euml,236,igrave,237,iacute,238,icirc,239,iuml,' +
-		'240,eth,241,ntilde,242,ograve,243,oacute,244,ocirc,245,otilde,' +
-		'246,ouml,247,divide,248,oslash,249,ugrave,250,uacute,251,ucirc,' +
-		'252,uuml,253,yacute,254,thorn,255,yuml,402,fnof,913,Alpha,914,Beta,' +
-		'915,Gamma,916,Delta,917,Epsilon,918,Zeta,919,Eta,920,Theta,921,Iota,' +
-		'922,Kappa,923,Lambda,924,Mu,925,Nu,926,Xi,927,Omicron,928,Pi,929,Rho,' +
-		'931,Sigma,932,Tau,933,Upsilon,934,Phi,935,Chi,936,Psi,937,Omega,' +
-		'945,alpha,946,beta,947,gamma,948,delta,949,epsilon,950,zeta,951,eta,' +
-		'952,theta,953,iota,954,kappa,955,lambda,956,mu,957,nu,958,xi,' +
-		'959,omicron,960,pi,961,rho,962,sigmaf,963,sigma,964,tau,965,upsilon,' +
-		'966,phi,967,chi,968,psi,969,omega,977,thetasym,978,upsih,982,piv,' +
-		'8226,bull,8230,hellip,8242,prime,8243,Prime,8254,oline,8260,frasl,' +
-		'8472,weierp,8465,image,8476,real,8482,trade,8501,alefsym,8592,larr,' +
-		'8593,uarr,8594,rarr,8595,darr,8596,harr,8629,crarr,8656,lArr,8657,' +
-		'uArr,8658,rArr,8659,dArr,8660,hArr,8704,forall,8706,part,8707,exist,' +
-		'8709,empty,8711,nabla,8712,isin,8713,notin,8715,ni,8719,prod,8721,sum,' +
-		'8722,minus,8727,lowast,8730,radic,8733,prop,8734,infin,8736,ang,' +
-		'8743,and,8744,or,8745,cap,8746,cup,8747,int,8756,there4,8764,sim,' +
-		'8773,cong,8776,asymp,8800,ne,8801,equiv,8804,le,8805,ge,8834,sub,' +
-		'8835,sup,8836,nsub,8838,sube,8839,supe,8853,oplus,8855,otimes,8869,perp,' +
-		'8901,sdot,8968,lceil,8969,rceil,8970,lfloor,8971,rfloor,9001,lang,' +
-		'9002,rang,9674,loz,9824,spades,9827,clubs,9829,hearts,9830,diams,' +
-		'338,OElig,339,oelig,352,Scaron,353,scaron,376,Yuml,710,circ,732,tilde,' +
-		'8194,ensp,8195,emsp,8201,thinsp,8204,zwnj,8205,zwj,8206,lrm,8207,rlm,' +
-		'8211,ndash,8212,mdash,8216,lsquo,8217,rsquo,8218,sbquo,8220,ldquo,' +
-		'8221,rdquo,8222,bdquo,8224,dagger,8225,Dagger,8240,permil,8249,lsaquo,' +
-		'8250,rsaquo,8364,euro'
-	);
+		'50,nbsp,51,iexcl,52,cent,53,pound,54,curren,55,yen,56,brvbar,57,sect,58,uml,59,copy,' +
+		'5a,ordf,5b,laquo,5c,not,5d,shy,5e,reg,5f,macr,5g,deg,5h,plusmn,5i,sup2,5j,sup3,5k,acute,' +
+		'5l,micro,5m,para,5n,middot,5o,cedil,5p,sup1,5q,ordm,5r,raquo,5s,frac14,5t,frac12,5u,frac34,' +
+		'5v,iquest,60,Agrave,61,Aacute,62,Acirc,63,Atilde,64,Auml,65,Aring,66,AElig,67,Ccedil,' +
+		'68,Egrave,69,Eacute,6a,Ecirc,6b,Euml,6c,Igrave,6d,Iacute,6e,Icirc,6f,Iuml,6g,ETH,6h,Ntilde,' +
+		'6i,Ograve,6j,Oacute,6k,Ocirc,6l,Otilde,6m,Ouml,6n,times,6o,Oslash,6p,Ugrave,6q,Uacute,' +
+		'6r,Ucirc,6s,Uuml,6t,Yacute,6u,THORN,6v,szlig,70,agrave,71,aacute,72,acirc,73,atilde,74,auml,' +
+		'75,aring,76,aelig,77,ccedil,78,egrave,79,eacute,7a,ecirc,7b,euml,7c,igrave,7d,iacute,7e,icirc,' +
+		'7f,iuml,7g,eth,7h,ntilde,7i,ograve,7j,oacute,7k,ocirc,7l,otilde,7m,ouml,7n,divide,7o,oslash,' +
+		'7p,ugrave,7q,uacute,7r,ucirc,7s,uuml,7t,yacute,7u,thorn,7v,yuml,ci,fnof,sh,Alpha,si,Beta,' +
+		'sj,Gamma,sk,Delta,sl,Epsilon,sm,Zeta,sn,Eta,so,Theta,sp,Iota,sq,Kappa,sr,Lambda,ss,Mu,' +
+		'st,Nu,su,Xi,sv,Omicron,t0,Pi,t1,Rho,t3,Sigma,t4,Tau,t5,Upsilon,t6,Phi,t7,Chi,t8,Psi,' +
+		't9,Omega,th,alpha,ti,beta,tj,gamma,tk,delta,tl,epsilon,tm,zeta,tn,eta,to,theta,tp,iota,' +
+		'tq,kappa,tr,lambda,ts,mu,tt,nu,tu,xi,tv,omicron,u0,pi,u1,rho,u2,sigmaf,u3,sigma,u4,tau,' +
+		'u5,upsilon,u6,phi,u7,chi,u8,psi,u9,omega,uh,thetasym,ui,upsih,um,piv,812,bull,816,hellip,' +
+		'81i,prime,81j,Prime,81u,oline,824,frasl,88o,weierp,88h,image,88s,real,892,trade,89l,alefsym,' +
+		'8cg,larr,8ch,uarr,8ci,rarr,8cj,darr,8ck,harr,8dl,crarr,8eg,lArr,8eh,uArr,8ei,rArr,8ej,dArr,' +
+		'8ek,hArr,8g0,forall,8g2,part,8g3,exist,8g5,empty,8g7,nabla,8g8,isin,8g9,notin,8gb,ni,8gf,prod,' +
+		'8gh,sum,8gi,minus,8gn,lowast,8gq,radic,8gt,prop,8gu,infin,8h0,ang,8h7,and,8h8,or,8h9,cap,8ha,cup,' +
+		'8hb,int,8hk,there4,8hs,sim,8i5,cong,8i8,asymp,8j0,ne,8j1,equiv,8j4,le,8j5,ge,8k2,sub,8k3,sup,8k4,' +
+		'nsub,8k6,sube,8k7,supe,8kl,oplus,8kn,otimes,8l5,perp,8m5,sdot,8o8,lceil,8o9,rceil,8oa,lfloor,8ob,' +
+		'rfloor,8p9,lang,8pa,rang,9ea,loz,9j0,spades,9j3,clubs,9j5,hearts,9j6,diams,ai,OElig,aj,oelig,b0,' +
+		'Scaron,b1,scaron,bo,Yuml,m6,circ,ms,tilde,802,ensp,803,emsp,809,thinsp,80c,zwnj,80d,zwj,80e,lrm,' +
+		'80f,rlm,80j,ndash,80k,mdash,80o,lsquo,80p,rsquo,80q,sbquo,80s,ldquo,80t,rdquo,80u,bdquo,810,dagger,' +
+		'811,Dagger,81g,permil,81p,lsaquo,81q,rsaquo,85c,euro'
+	, 32);
 
 	tinymce.html = tinymce.html || {};
 
@@ -168,6 +156,7 @@
 		 * @method encodeNamed
 		 * @param {String} text Text to encode.
 		 * @param {Boolean} attr Optional flag to specify if the text is attribute contents.
+		 * @param {Object} entities Optional parameter with entities to use.
 		 * @return {String} Entity encoded text.
 		 */
 		encodeNamed : function(text, attr, entities) {
