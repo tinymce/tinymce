535352e66f0e58b9d630333c73be260d1328f58d
tupton <thomasupton@gmail.com>
2010-10-22 14:14:55 -0500

    Since the editor is in an iframe and has its own document, Use the editor's document so IE6/7 don't freak out when trying to append an element made by a different document.

--- jscripts/tiny_mce/classes/ForceBlocks.js
+++ jscripts/tiny_mce/classes/ForceBlocks.js
@@ -235,7 +235,7 @@
 									}
 
 									try {
-										el.appendChild(document.createTextNode('\uFEFF'));
+										el.appendChild(t.editor.getDoc().createTextNode('\uFEFF'));
 									} catch(e) {
 										// Catch "Invalid Argument" error in IE7
 									}
