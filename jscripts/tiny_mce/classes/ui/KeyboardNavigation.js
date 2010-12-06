9f6b9f3422a6d56aa8c07f3969087abb7810b139
Adrian Sutton <adrian.sutton@ephox.com>
2010-12-06 16:25:09 +0000

    MAINTENANCE: Add moxiedoc documentation.

--- jscripts/tiny_mce/classes/ui/KeyboardNavigation.js
+++ jscripts/tiny_mce/classes/ui/KeyboardNavigation.js
@@ -1,9 +1,27 @@
 (function(tinymce) {
 	var Event = tinymce.dom.Event, each = tinymce.each;
 
+	/**
+	 * This class provides basic keyboard navigation using the arrow keys to children of a component.
+	 * For example, this class handles moving between the buttons on the toolbars. 
+	 * 
+	 * @class tinymce.ui.KeyboardNavigation
+	 */
 	tinymce.create('tinymce.ui.KeyboardNavigation', {
 		// Root must be an element or ID of the root node.
 		// items must be an array of focusable objects, each with an ID property to link to the appropriate DOM element.
+		
+		/**
+		 * 
+		 * @constructor
+		 * @method KeyboardNavigation
+		 * @param {DOMUtils} dom the DOMUtils instance to use.
+		 * @param {Element/String} the root element or ID of the root element for the control.
+		 * @param {Array} items an array containing the items to move focus between. Every object in this array must have an id attribute which maps to the actual DOM element. If the actual elements are passed without an ID then one is automatically assigned.
+		 * @param {Function} cancelAction the callback for when the user presses escape or otherwise indicates cancelling.
+		 * @param {Function} actionHandler (optional) the action handler to call when the user activates an item.
+		 * @param {Boolean} verticalOrientation (optional) when true, the up/down arrows move through items instead of left/right.
+		 */
 		KeyboardNavigation: function(dom, root, items, cancelAction, actionHandler, verticalOrientation) {
 		
 			var t = this;//, items = dom.select(selector, root);
