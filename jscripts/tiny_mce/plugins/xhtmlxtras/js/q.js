178b8220eef44eac2d5d6b082841777429970935
unknown <florent@.(none)>
2011-02-28 14:40:12 +0100

    Create q.js like cite.js
    Create q.js like cite.js

---
+++ jscripts/tiny_mce/plugins/xhtmlxtras/js/q.js
@@ -0,0 +1,1 @@
+/*** quote.js***/function init() {	SXE.initElementDialog('q');	if (SXE.currentAction == "update") {		SXE.showRemoveButton();	}}function insertQuote() {	SXE.insertElement('q');	tinyMCEPopup.close();}function removeQuote() {	SXE.removeElement('q');	tinyMCEPopup.close();}tinyMCEPopup.onInit.add(init);
