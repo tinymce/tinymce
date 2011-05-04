bfd2876681fb6f3ef1792d7184e262c44bc4f794
Spocke <spocke@moxiecode.com>
2011-05-04 16:07:54 +0200

    Fixed so ?debug=true can be used to get Firebug Lite started when opening a unit test directly.

--- tests/js/tiny_mce_loader.js
+++ tests/js/tiny_mce_loader.js
@@ -2,6 +2,6 @@
 	if (document.location.search.indexOf('min=true') > 0) {
 		document.write('<script src="../jscripts/tiny_mce/tiny_mce.js"></script>');
 	} else {
-		document.write('<script src="../jscripts/tiny_mce/tiny_mce_dev.js"></script>');		
+		document.write('<script src="../jscripts/tiny_mce/tiny_mce_dev.js' + document.location.search + '"></script>');		
 	}
 })();
