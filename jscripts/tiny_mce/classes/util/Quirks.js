e1591d58d764878b72146d69c106fedb66852c31
Spocke <spocke@moxiecode.com>
2011-09-01 18:50:33 +0200

    Moved logic for checking if the editor is empty so it runs on IE as well.

--- jscripts/tiny_mce/classes/util/Quirks.js
+++ jscripts/tiny_mce/classes/util/Quirks.js
@@ -1,12 +1,29 @@
 (function(tinymce) {
+	var VK = tinymce.VK, BACKSPACE = VK.BACKSPACE, DELETE = VK.DELETE;
+
+	/**
+	 * Fixes a WebKit bug when deleting contents using backspace or delete key.
+	 * WebKit will produce a span element if you delete across two block elements.
+	 *
+	 * Example:
+	 * <h1>a</h1><p>|b</p>
+	 *
+	 * Will produce this on backspace:
+	 * <h1>a<span class="Apple-style-span" style="<all runtime styles>">b</span></p>
+	 *
+	 * This fixes the backspace to produce:
+	 * <h1>a|b</p>
+	 *
+	 * See bug: https://bugs.webkit.org/show_bug.cgi?id=45784
+	 */
 	function cleanupStylesWhenDeleting(ed) {
-		var dom = ed.dom, selection = ed.selection, VK = tinymce.VK;
+		var dom = ed.dom, selection = ed.selection;
 
 		ed.onKeyDown.add(function(ed, e) {
 			var rng, blockElm, node, clonedSpan, isDelete;
 
-			isDelete = e.keyCode == VK.DELETE;
-			if (isDelete || e.keyCode == VK.BACKSPACE) {
+			isDelete = e.keyCode == DELETE;
+			if (isDelete || e.keyCode == BACKSPACE) {
 				e.preventDefault();
 				rng = selection.getRng();
 
@@ -29,12 +46,6 @@
 				// Do the backspace/delete actiopn
 				ed.getDoc().execCommand(isDelete ? 'ForwardDelete' : 'Delete', false, null);
 
-				// Check if editor contents is emptyish like <h1><br /></h1> then remove all contents
-				if (/^<(p|h[1-6]|div)><br[^>]*><\/(p|h[1-6]|div)>$/(ed.getBody().innerHTML)) {
-					ed.setContent('', {format : 'raw'});
-					return;
-				}
-
 				// Find all odd apple-style-spans
 				blockElm = dom.getParent(rng.startContainer, dom.isBlock);
 				tinymce.each(dom.select('span.Apple-style-span,font.Apple-style-span', blockElm), function(span) {
@@ -55,14 +66,38 @@
 				});
 			}
 		});
-	}
+	};
 
+	/**
+	 * WebKit and IE doesn't empty the editor if you select all contents and hit backspace or delete. This fix will check if the body is empty
+	 * like a <h1></h1> or <p></p> and then forcefully remove all contents.
+	 */
+	function emptyEditorWhenDeleting(ed) {
+		ed.onKeyUp.add(function(ed, e) {
+			var keyCode = e.keyCode;
+
+			if (keyCode == DELETE || keyCode == BACKSPACE) {
+				if (ed.dom.isEmpty(ed.getBody())) {
+					ed.setContent('', {format : 'raw'});
+					ed.nodeChanged();
+					return;
+				}
+			}
+		});
+	};
+	
 	tinymce.create('tinymce.util.Quirks', {
 		Quirks: function(ed) {
+			// Load WebKit specific fixed
 			if (tinymce.isWebKit) {
 				cleanupStylesWhenDeleting(ed);
+				emptyEditorWhenDeleting(ed);
 			}
-						
+
+			// Load IE specific fixes
+			if (tinymce.isIE) {
+				emptyEditorWhenDeleting(ed);
+			}
 		}
 	});
 })(tinymce);
