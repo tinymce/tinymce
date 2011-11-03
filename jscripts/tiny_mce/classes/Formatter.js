f03127388db8fd4542ed6b5a31932d828a25438d
Spocke <spocke@moxiecode.com>
2011-11-03 14:28:34 +0100

    Fixed issue where formatting wasn't removed if pressing enter after removing it from the caret.

--- jscripts/tiny_mce/classes/Formatter.js
+++ jscripts/tiny_mce/classes/Formatter.js
@@ -2002,7 +2002,7 @@
 			ed.onKeyDown.addToTop(function(ed, e) {
 				var keyCode = e.keyCode;
 
-				if (keyCode == 8 || keyCode == 37 || keyCode == 39 || keyCode == 13) {
+				if (keyCode == 8 || keyCode == 37 || keyCode == 39) {
 					removeCaretContainer(getParentCaretContainer(selection.getStart()));
 				}
 			});
