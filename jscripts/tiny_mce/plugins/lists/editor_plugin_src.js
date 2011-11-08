6eadbe0037670ce67c9dede6b15277d4a70e79aa
Rob Dawson <robert@rojotek.com>
2011-11-08 16:31:41 +1000

    Fix deleting empty list item containing a list in Webkit. (CONFEXT-53)

--- jscripts/tiny_mce/plugins/lists/editor_plugin_src.js
+++ jscripts/tiny_mce/plugins/lists/editor_plugin_src.js
@@ -460,6 +460,24 @@
 				}
 			}
 
+			function fixDeletingEmptyLiInWebkit(ed, e) {
+				var li = getLi();
+				if (e.keyCode === tinymce.VK.BACKSPACE && ed.dom.is(li, 'li') && li.parentNode.firstChild!==li) {
+					if (ed.dom.select('ul,ol', li).length === 1) {
+						var prevLi = li.previousSibling;
+						ed.dom.remove(ed.dom.select('br', li));
+						ed.dom.remove(li, true);
+						var textNodes = tinymce.grep(prevLi.childNodes, function(n){ return n.nodeType === 3 });
+						if (textNodes.length === 1) {
+							var textNode = textNodes[0]
+							ed.selection.setCursorLocation(textNode, textNode.length);
+						}
+						ed.undoManager.add();
+						return Event.cancel(e);
+					}
+				}
+			}
+
 			ed.onKeyDown.add(function(_, e) { state = getListKeyState(e); });
 			ed.onKeyDown.add(cancelDefaultEvents);
 			ed.onKeyDown.add(imageJoiningListItem);
@@ -473,6 +491,9 @@
 			}
 			if (tinymce.isGecko || tinymce.isWebKit) {
 				ed.onKeyDown.add(fixDeletingFirstCharOfList);
+			}
+			if (tinymce.isWebKit) {
+				ed.onKeyDown.add(fixDeletingEmptyLiInWebkit);
 			}
 		},
 
