b95d3c6d16bd74c60a8b935380d9ea8edfc1b7cc
Spocke <spocke@moxiecode.com>
2011-11-15 09:34:56 +0100

    Updated noneditable plugin to include initial caret control logic.

--- jscripts/tiny_mce/plugins/noneditable/editor_plugin_src.js
+++ jscripts/tiny_mce/plugins/noneditable/editor_plugin_src.js
@@ -11,7 +11,13 @@
 (function() {
 	var externalName = 'contenteditable', internalName = 'data-mce-' + externalName;
 
-	function findParentContentEditable(node) {
+	// Checks if the specified node is uneditable
+	function isUneditable(node) {
+		return node && node.nodeType === 1 && (node.getAttribute(internalName) || node.contentEditable) === "false";
+	};
+
+	// Finds the first uneditable parent node
+	function findUneditableParent(node) {
 		var parent = node;
 
 		while (parent) {
@@ -24,11 +30,13 @@
 	};
 
 	function fakeContentEditable(ed) {
+		var dom = ed.dom;
+
 		function expandToNonEditable(collapse) {
 			var selection = ed.selection, origRng, startState, endState;
 
 			function setEndPoint(start) {
-				var elm = findParentContentEditable(start ? selection.getStart() : selection.getEnd());
+				var elm = findUneditableParent(start ? selection.getStart() : selection.getEnd());
 
 				if (elm) {
 					rng = origRng.duplicate();
@@ -80,7 +88,7 @@
 			keyCode = e.keyCode;
 			shiftKey = e.shiftKey;
 
-			// Left arrow
+			// Left/right arrow
 			if (keyCode === 37 || keyCode === 39) {
 				if (expandToNonEditable(keyCode === 37)) {
 					e.preventDefault();
@@ -89,24 +97,87 @@
 		});
 
 		ed.onNodeChange.addToTop(function() {
-			expandToNonEditable();
+			//expandToNonEditable();
 		});
+
+		ed.onClick.add(function(ed, e) {
+			var elm = findUneditableParent(e.target);
+
+			if (elm) {
+				insertCaretContainer(elm, true);
+			}
+		});
+
+		ed.onKeyDown.add(function(ed, e) {
+			var node = ed.selection.getStart(), caretContainer, keyCode = e.keyCode, sibling;
+
+			do {
+				if (node.id === '_mce_caretcontainer') {
+					caretContainer = node;
+					break;
+				}
+
+				node = node.parentNode;
+			} while (node);
+
+			if (caretContainer) {
+				sibling = caretContainer.previousSibling;
+				if (keyCode === 8 && isUneditable(sibling)) {
+					e.preventDefault();
+					dom.remove(sibling);
+				}
+
+				sibling = caretContainer.nextSibling;
+				if (keyCode === 46 && isUneditable(sibling)) {
+					e.preventDefault();
+					dom.remove(sibling);
+				}
+
+				removeCaretContainer();
+			}
+		});
+		
+		function insertCaretContainer(node, before) {
+			var caretContainer, rng;
+
+			removeCaretContainer();
+			caretContainer = dom.create('span', {'id': '_mce_caretcontainer', 'data-mce-bogus' : true}, '\uFEFF\uFEFF');
+
+			if (before) {
+				node.parentNode.insertBefore(caretContainer, node);
+			} else {
+				dom.insertAfter(caretContainer, node);
+			}
+
+			// Move caret in to the middle of caret container
+			rng = dom.createRng();
+			rng.setStart(caretContainer.firstChild, 1);
+			rng.setEnd(caretContainer.firstChild, 1);
+			ed.selection.setRng(rng);
+			ed.nodeChanged();
+		};
+
+		function removeCaretContainer() {
+			var elm;
+
+			while (elm = dom.get('_mce_caretcontainer')) {
+				dom.remove(elm, elm.innerHTML !== '\uFEFF\uFEFF');
+			}
+		};
 	};
 
 	tinymce.create('tinymce.plugins.NonEditablePlugin', {
 		init : function(ed, url) {
-			var t = this, editClass, nonEditClass, contentEditableAttrName = tinymce.isIE ? internalName : externalName;
+			var t = this, editClass, nonEditClass, contentEditableAttrName = !tinymce.isIE ? internalName : externalName;
 
 			t.editor = ed;
 			editClass = " " + tinymce.trim(ed.getParam("noneditable_editable_class", "mceEditable")) + " ";
 			nonEditClass = " " + tinymce.trim(ed.getParam("noneditable_noneditable_class", "mceNonEditable")) + " ";
 
 			ed.onPreInit.add(function() {
-				if (tinymce.isIE) {
-					fakeContentEditable(ed);
-				}
+				fakeContentEditable(ed);
 
-				// Convert video elements to image placeholder
+				// Apply contentEditable true/false on elements with the noneditable/editable classes
 				ed.parser.addAttributeFilter('class', function(nodes) {
 					var i = nodes.length, className, node;
 
