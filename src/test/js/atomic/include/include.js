d438d7bda05fc5b846f72a2877ef0b328dda1330
troll <troll@ephox.com>
2011-12-21 10:56:58 +1000

    Fix failing build.

--- src/test/js/atomic/include/include.js
+++ src/test/js/atomic/include/include.js
@@ -5,6 +5,9 @@ require("../../../../main/js/compile/bootstrap.js");
 require("../../../../main/js/compile/porkbun.js");
 
 define = ephox.bolt.module.api.define;
+// FIX: this is crap, we need a better way to do this
 require("../../../../../lib/test/ephox.scullion.Struct.js");
+require("../../../../../lib/test/ephox.scullion.Mutable.js");
+require("../../../../../lib/test/ephox.scullion.Immutable.js");
 
 demand = ephox.bolt.module.api.demand;
