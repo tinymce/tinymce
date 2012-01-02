d13e8c8648f867372cc743a631ba819eee645e5e
Spocke <spocke@moxiecode.com>
2012-01-02 16:41:10 +0100

    Fixed remaining issues with visualblocks and added images for each element type.

--- jscripts/tiny_mce/plugins/visualblocks/editor_plugin_src.js
+++ jscripts/tiny_mce/plugins/visualblocks/editor_plugin_src.js
@@ -11,20 +11,40 @@
 (function() {
 	tinymce.create('tinymce.plugins.VisualBlocks', {
 		init : function(ed, url) {
-			ed.addCommand('mceVisualBlocks', function() {
-				var dom = ed.dom, body = ed.getBody();
+			var cssId;
 
-				// Toggle mceVisualBlocks class on/off
-				if (!dom.hasClass(body, 'mceVisualBlocks')) {
-					dom.addClass(body, 'mceVisualBlocks');
+			// We don't support older browsers like IE6/7 and they don't provide prototypes for DOM objects
+			if (!window.NodeList) {
+				return;
+			}
+
+			ed.addCommand('mceVisualBlocks', function() {
+				var dom = ed.dom, linkElm;
+
+				if (!cssId) {
+					cssId = dom.uniqueId();
+					linkElm = dom.create('link', {
+						id: cssId,
+						rel : 'stylesheet',
+						href : url + '/css/visualblocks.css'
+					});
+
+					ed.getDoc().getElementsByTagName('head')[0].appendChild(linkElm);
 				} else {
-					dom.removeClass(body, 'mceVisualBlocks');
+					linkElm = dom.get(cssId);
+					linkElm.disabled = !linkElm.disabled;
 				}
+
+				ed.controlManager.setActive('visualblocks', !linkElm.disabled);
 			});
 
 			ed.addButton('visualblocks', {title : 'visualblocks.desc', cmd : 'mceVisualBlocks'});
 
-			ed.contentCSS.push(url + '/css/visualblocks.css');
+			ed.onInit.add(function() {
+				if (ed.settings.visualblocks_default_state) {
+					ed.execCommand('mceVisualBlocks');
+				}
+			});
 		},
 
 		getInfo : function() {
