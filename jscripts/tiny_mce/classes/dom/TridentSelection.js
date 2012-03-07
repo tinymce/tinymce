634fd58eb29ab2e4e5ab4aa9a6fb4c59ee7ac24f
Spocke <spocke@moxiecode.com>
2012-03-07 14:55:46 +0100

    More fixes for the selection of empty blocks.

--- jscripts/tiny_mce/classes/dom/TridentSelection.js
+++ jscripts/tiny_mce/classes/dom/TridentSelection.js
@@ -417,12 +417,13 @@
 
 			// If single element selection then try making a control selection out of it
 			if (startContainer == endContainer && startContainer.nodeType == 1) {
+				// Trick to place the caret inside an empty block element like <p></p>
 				if (!startContainer.hasChildNodes()) {
 					startContainer.innerHTML = '<span>\uFEFF</span>';
 					ieRng.moveToElementText(startContainer.firstChild);
+					ieRng.collapse(false); // Needs to be collapse false for IE8 mode in IE9
 					ieRng.select();
-					startContainer.innerHTML = '';
-					ieRng.select();
+					dom.remove(startContainer.firstChild);
 					return;
 				}
 
