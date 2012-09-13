77f5c5e61055b9e82756efaff17b6f7384c6852f
mps <dev@vapid.(none)>
2012-09-13 12:47:52 +1000

    tests

--- src/test/js/browser/extract/ExtractTest.js
+++ src/test/js/browser/extract/ExtractTest.js
@@ -22,6 +22,43 @@ test(
   }
 );
 
+test(
+  'From',
+
+  [
+    'ephox.compass.Arr',
+    'ephox.phoenix.extract.Extract',
+    'ephox.phoenix.test.Page',
+    'ephox.sugar.api.Element',
+    'ephox.sugar.api.Text'
+  ],
+
+  function (Arr, Extract, Page, Element, Text) {
+    var check = function (expected, input) {
+      var rawActual = Extract.from(input);
+      var actual = Arr.map(rawActual, function (x) {
+        return x.fold(function () {
+          return '\\w';
+        }, function () {
+          return '-';
+        }, function (t) {
+          return Text.get(t);
+        });
+      }).join('');
+      assert.eq(expected, actual);
+    };
+
+    // IMPORTANT: Otherwise CSS display does not work.
+    Page.connect();
+
+    check('', Element.fromText(''));
+    check('\\wFirst paragraph\\w', Page.p1);
+    check('\\w\\wFirst paragraph\\w\\wSecond here is something\\w\\wMore data\\w\\w', Page.div1);
+    check('\\w\\w\\wFirst paragraph\\w\\wSecond here is something\\w\\wMore data\\w\\w\\wNext \\wSection now\\w\\w\\w', Page.container);
+  }
+);
+
+
 /*
 <container><div1><p1>{t1:First paragraph}<p2>{t2:Second }<s1>{t3:here}<s2>{t4: is }<s3>{t5:something}
   <p3>{t6:More data}
