c751213fc4b419cfb8f2758ea1cff36d00073ef7
ephox_dev <dev@klaatu.(none)>
2012-11-07 15:16:51 +1000

    identify test

---
+++ src/test/js/browser/word/IdentifyTest.js
@@ -0,0 +1,60 @@
+test(
+  'IdentifyTest',
+
+  {
+    'ephox.sugar.api.Element': '../mock/ephox/sugar/api/Element',
+    'ephox.sugar.api.SelectorFilter': '../mock/ephox/sugar/api/SelectorFilter',
+    'ephox.wrap.JQuery': '../mock/ephox/wrap/JQuery'
+  },
+
+  [
+    'ephox.compass.Arr',
+    'ephox.perhaps.Option',
+    'ephox.robin.data.WordScope',
+    'ephox.robin.word.Identify'
+  ],
+
+  function (Arr, Option, WordScope, Identify) {
+    var none = Option.none();
+    var some = Option.some;
+    
+    var check = function (expected, input) {
+      var actual = Identify.words(input);
+      assert.eq(expected.length, actual.length);
+      Arr.map(expected, function (x, i) {
+        assert.eq(expected[i].word(), actual[i].word());
+        assert.eq(true, expected[i].left().equals(actual[i].left()));
+        assert.eq(true, expected[i].right().equals(actual[i].right()));
+      });
+    };
+
+    check([], '');
+    check([], ' ');
+    check([WordScope('one', none, none)], 'one');
+    check([WordScope('this', some('('), some(')'))], '(this)');
+    check([WordScope("don't", some(' '), some(' '))], ' don\'t ');
+    check([
+      WordScope('it', some('"'), some(' ')),
+      WordScope('is', some(' '), some(' ')),
+      WordScope('a', some(' '), some(' ')),
+      WordScope('good', some(' '), some(' ')),
+      WordScope('day', some(' '), some(' ')),
+      WordScope('to', some(' '), some(' ')),
+      WordScope('live', some(' '), some('"'))
+      ], '"it is a good day to live"');
+    check([
+      WordScope("'twas", some(' '), some(' ')),
+      WordScope('the', some(' '), some(' ')),
+      WordScope('night', some(' '), some(' ')),
+      WordScope('before', some(' '), none)
+      ], ' \'twas the night before');
+
+    check([
+      WordScope("this", some("'"), some(' ')),
+      WordScope('the', some(' '), some(' ')),
+      WordScope('night', some(' '), some(' ')),
+      WordScope('before', some(' '), none)
+      ], ' \'this the night before');
+
+  }
+);
