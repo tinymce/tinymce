2ddddf0790740f5aedca6a278ef0ca7529983471
Spocke <spocke@moxiecode.com>
2012-11-20 13:06:37 +0100

    Removed autosave lang file since it no longer used.

--- jscripts/tiny_mce/plugins/autosave/langs/en.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addI18n('en.autosave',{
-restore_content: "Restore auto-saved content",
-warning_message: "If you restore the saved content, you will lose all the content that is currently in the editor.\n\nAre you sure you want to restore the saved content?"
-});
