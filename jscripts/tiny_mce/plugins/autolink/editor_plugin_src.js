240a7cc9e923e9548296da95df8aa23355702c3e
Spocke <spocke@moxiecode.com>
2012-11-20 13:29:03 +0100

    Fixed bug where the autolink plugin would produce DOM exceptions when pressing shift+enter inside a block element.

--- jscripts/tiny_mce/plugins/autolink/editor_plugin_src.js
+++ jscripts/tiny_mce/plugins/autolink/editor_plugin_src.js
@@ -107,8 +107,8 @@
 			do
 			{
 				// Move the selection one character backwards.
-				r.setStart(endContainer, end - 2);
-				r.setEnd(endContainer, end - 1);
+				r.setStart(endContainer, end >= 2 ? end - 2 : 0);
+				r.setEnd(endContainer, end >= 1 ? end - 1 : 0);
 				end -= 1;
 
 				// Loop until one of the following is found: a blank space, &nbsp;, delimeter, (end-2) >= 0
