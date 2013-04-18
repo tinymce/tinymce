cf660eb63880bf057279177c8b7e8a998c0f33d3
ephox_dev <dev@klaatu.(none)>
2013-04-18 18:57:16 +1000

    cleanup

--- src/main/js/ephox/robin/words/Cluster.js
+++ src/main/js/ephox/robin/words/Cluster.js
@@ -22,32 +22,19 @@ define(
       var left = gathered.left();
       var right = gathered.right();
 
-      console.log('left: ', left.length);
-      console.log('right: ', right.length);
-
       var inner = Extract.all(element);
       var middle = Arr.map(inner, function (x) {
         return Spot.text(x, Text.getOption(x).getOr(''));
       });
 
-      console.log('middle: ', middle.length);
-
       var elements = left.concat(middle).concat(right);
-      console.log('all: ', Arr.map(elements, function (x) { return x.element().dom(); }));
       var groups = Split.split(elements, function (x) {
         var elem = x.element();
         return Classification.isBoundary(elem) || Classification.isEmpty(elem);
       });
 
-      console.log('groups: ', Arr.map(groups, function (x) {
-        return Arr.map(x, function (y) {
-          return y.element().dom().nodeValue;
-        });
-      }));
-
       var words = Arr.bind(groups, function (x) {
         var text = Arr.bind(x, function (y) {
-          console.log('y.text: ', y.text());
           return y.text();
         }).join('');
         return Identify.words(text);
