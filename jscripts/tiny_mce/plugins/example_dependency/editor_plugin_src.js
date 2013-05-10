59fa8f350b53ff3e4015aa5926263f053acb8801
Veres Lajos <vlajos@gmail.com>
2013-05-10 11:53:37 +0100

    typofixes

--- jscripts/tiny_mce/plugins/example_dependency/editor_plugin_src.js
+++ jscripts/tiny_mce/plugins/example_dependency/editor_plugin_src.js
@@ -43,7 +43,7 @@
 	/**
 	 * Register the plugin, specifying the list of the plugins that this plugin depends on.  They are specified in a list, with the list loaded in order.
 	 * plugins in this list will be initialised when this plugin is initialized. (before the init method is called).
-	 * plugins in a depends list should typically be specified using the short name).  If neccesary this can be done
+	 * plugins in a depends list should typically be specified using the short name).  If necessary this can be done
 	 * with an object which has the url to the plugin and the shortname.
 	 */
 	tinymce.PluginManager.add('example_dependency', tinymce.plugins.ExampleDependencyPlugin, ['example']);
