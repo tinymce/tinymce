fdf455f0a8ecc4ab5b4405531e481ee95580d05b
ephox_dev <dev@klaatu.(none)>
2013-05-22 14:23:33 +1000

    removed unused Find.from

--- src/main/js/ephox/phoenix/util/str/Find.js
+++ src/main/js/ephox/phoenix/util/str/Find.js
@@ -25,22 +25,8 @@ define(
       return r;
     };
 
-    var from = function (input, pattern, index) {
-      var term = pattern.term();
-      term.lastIndex = index;
-      var match = term.exec(input);
-      if (match && match.index >= index) {
-        var start = match.index + pattern.preOffset(match);
-        var length = match[0].length - pattern.preOffset(match) - pattern.postOffset(match);
-        return Option.some([start, start + length]);
-      } else {
-        return Option.none();
-      }
-    };
-
     return {
-      all: all,
-      from: from
+      all: all
     };
   }
 );
--- src/test/js/atomic/util/str/FindTest.js
+++ src/test/js/atomic/util/str/FindTest.js
@@ -23,19 +23,6 @@ test(
       });
     };
 
-    var checkFrom = function (expected, input, pattern, offset) {
-      var actual = Find.from(input, pattern, offset);
-      expected.fold(function () {
-        assert.eq(true, actual.isNone(), 'Expected none, given: ' + actual.getOr('none'));
-      }, function (v) {
-        actual.fold(function () {
-          assert.fail('Expected ' + v + ', given: none');
-        }, function (vv) {
-          assert.eq(v, vv);
-        });
-      });
-    };
-
     checkAll([], 'eskimo', Pattern.token('hi'));
     checkAll([[1, 7]], ' cattle', Pattern.token('cattle'));
     checkAll([], 'acattle', Pattern.word('cattle'));
@@ -52,16 +39,5 @@ test(
     checkAll([[1, 4], [5, 8]], ' sre sre', Pattern.word('sre'));
     checkAll([[1, 4], [5, 8], [9, 12]], ' sre sre sre', Pattern.word('sre'));
     checkAll([[1, 4], [5, 8], [9, 12]], ' sre sre sre ', Pattern.word('sre'));
-
-    checkFrom(Option.some([0, 2]), 'hi', Pattern.word('hi'), 0);
-    checkFrom(Option.some([1, 3]), ' hi', Pattern.word('hi'), 0);
-    checkFrom(Option.none(), ' hello', Pattern.word('he'), 0);
-    checkFrom(Option.none(), ' hello', Pattern.word('hello'), 1);
-    checkFrom(Option.some([1, 6]), ' hello', Pattern.word('hello'), 0);
-
-
-    checkFrom(Option.some([11, 21]), 'doloremque laudantium, totam rem aperiam, eaque', Pattern.word('laudantium'), 10);
-    checkFrom(Option.some([48, 58]), 'doloremque laudantium, totam rem aperiam, eaque doloremque', Pattern.word('doloremque'), 15);
-
   }
 );
