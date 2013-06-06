95760202002d595c2b81cd93bee9e450045a8625
ephox_dev <dev@klaatu.(none)>
2013-06-06 14:37:11 +1000

    group using ghetto

--- src/main/js/ephox/phoenix/group/Group.js
+++ src/main/js/ephox/phoenix/group/Group.js
@@ -2,21 +2,15 @@ define(
   'ephox.phoenix.group.Group',
 
   [
-    'ephox.compass.Arr',
-    'ephox.phoenix.api.DomExtract',
-    'ephox.phoenix.util.arr.Split'
+    'ephox.boss.api.DomUniverse',
+    'ephox.phoenix.ghetto.family.GhettoGroup'
   ],
 
-  function (Arr, DomExtract, Split) {
+  function (DomUniverse, GhettoGroup) {
+    var universe = DomUniverse();
 
     var group = function (elements) {
-      // return [];
-      var extractions = Arr.bind(elements, DomExtract.from);
-      var segments = Split.split(extractions, function (x) {
-        return x.isBoundary();
-      });
-
-      return Arr.filter(segments, function (x) { return x.length > 0; });
+      return GhettoGroup.group(universe, elements);
     };
 
     return {
