2068866bead35f76d93b538744b994afb84bf10c
ephox_dev <dev@klaatu.(none)>
2013-06-06 11:21:34 +1000

    ghetto extract done

--- src/main/js/ephox/phoenix/extract/Extract.js
+++ src/main/js/ephox/phoenix/extract/Extract.js
@@ -3,17 +3,10 @@ define(
 
   [
     'ephox.boss.api.DomUniverse',
-    'ephox.compass.Arr',
-    'ephox.phoenix.data.Spot',
-    'ephox.phoenix.ghetto.extract.GhettoExtract',
-    'ephox.phoenix.util.doc.List',
-    'ephox.phoenix.util.node.Classification',
-    'ephox.sugar.api.Node',
-    'ephox.sugar.api.PredicateFind',
-    'ephox.sugar.api.Traverse'
+    'ephox.phoenix.ghetto.extract.GhettoExtract'
   ],
 
-  function (DomUniverse, Arr, Spot, GhettoExtract, List, Classification, Node, PredicateFind, Traverse) {
+  function (DomUniverse, GhettoExtract) {
 
     var from = function (element) {
       return GhettoExtract.typed(DomUniverse(), element);
@@ -23,24 +16,12 @@ define(
       return GhettoExtract.items(DomUniverse(), element);
     };
 
-    var extractToElem = function (child, offset, element) {
-      var extractions = from(element);
-      var prior = List.dropUntil(extractions, child);
-      var count = List.count(prior);
-      return Spot.point(element, count + offset);
-    };
-
     var extract = function (child, offset) {
       return GhettoExtract.extract(DomUniverse(), child, offset);
     };
 
     var extractTo = function (child, offset, pred) {
-      // return Spot.point(child, offset);
-      return PredicateFind.ancestor(child, pred).fold(function () {
-        return Spot.point(child, offset);
-      }, function (v) {
-        return extractToElem(child, offset, v);
-      });
+      return GhettoExtract.extractTo(DomUniverse(), child, offset, pred);
     };
 
     return {
