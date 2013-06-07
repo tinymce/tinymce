512b6659f3672f6e57b30f0f3d5d53d1757ce815
ephox_dev <dev@klaatu.(none)>
2013-06-07 12:26:40 +1000

    string split

---
+++ src/test/js/atomic/api/StringSplitTest.js
@@ -0,0 +1,21 @@
+test(
+  'split.Strings',
+
+  [
+    'ephox.polaris.api.Strings'
+  ],
+
+  function (Strings) {
+    var check = function (expected, input, points) {
+      var actual = Strings.splits(input, points);
+      assert.eq(expected, actual);
+    };
+
+    check([ '' ], '', [ ]);
+    check([ 'a' ], 'a', [ ]);
+    check([ 'a' ], 'a', [ 0 ]);
+    check([ 'a' ], 'a', [ 1 ]);
+    check([ 'a', 'b', 'c' ], 'abc', [ 0, 1, 2, 3 ]);
+    check([ 'ab', 'cdef', 'ghi' ], 'abcdefghi', [ 2, 6 ]);
+  }
+);
