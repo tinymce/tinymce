8baee2f86946aba1ca7c90c84cf37e344b196ac8
ephox_dev <dev@klaatu.(none)>
2013-06-07 11:07:39 +1000

    slicing

--- src/test/js/atomic/array/SliceTest.js
+++ src/test/js/atomic/array/SliceTest.js
@@ -2,9 +2,24 @@ test(
   'SliceTest',
 
   [
+    'ephox.polaris.array.Slice'
   ],
 
-  function () {
+  function (Slice) {
+    var check = function (expected, input, pred) {
+      var actual = Slice.sliceBy(input, pred);
+      assert.eq(expected, actual);
+    };
 
+    var is = function (value) {
+      return function (x) {
+        return x === value;
+      };
+    };
+
+    check([ ], [ ], is(0));
+    check([ ], [ 1 ], is(1));
+    check([ 1 ], [ 1, 2 ], is(2));
+    check([ 1, 2, 3 ], [1, 2, 3, 4 ], is(4));
   }
 );
