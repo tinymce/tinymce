273983fac330b474ea6ef3af97d81d0f4a2b5c09
ephox_dev <dev@klaatu.(none)>
2013-06-07 15:30:32 +1000

    cleanup

--- src/main/js/ephox/phoenix/wrap/Identify.js
+++ src/main/js/ephox/phoenix/wrap/Identify.js
@@ -3,42 +3,14 @@ define(
 
   [
     'ephox.boss.api.DomUniverse',
-    'ephox.phoenix.api.DomSplit',
-    'ephox.phoenix.ghetto.wrap.GhettoIdentify',
-    'ephox.phoenix.util.node.DomRange',
-    'ephox.scullion.Struct',
-    'ephox.sugar.api.Compare'
+    'ephox.phoenix.ghetto.wrap.GhettoIdentify'
   ],
 
-  function (DomUniverse, DomSplit, GhettoIdentify, DomRange, Struct, Compare) {
-
-    // FIX: Rename me.
-    var zz = Struct.immutable('element', 'offset');
-
-    var diff = function (base, baseOffset, end, endOffset) {
-      var start = DomSplit.split(base, baseOffset).after().fold(function () {
-        return zz(base, 1);
-      }, function (v) {
-        return zz(v, 0);
-      });
-
-      var finish = DomSplit.split(end, endOffset).before().fold(function () {
-        return zz(end, 0);
-      }, function (v) {
-        return zz(v, 1);
-      });
-      return DomRange.range(start, finish);
-    };
-
-    var same = function (base, baseOffset, end, endOffset) {
-      var middle = DomSplit.splitByPair(base, baseOffset, endOffset);
-      return [middle];
-    };
-
+  function (DomUniverse, GhettoIdentify) {
+    var universe = DomUniverse();
+    
     var nodes = function (base, baseOffset, end, endOffset, c) {
-      return GhettoIdentify.nodes(DomUniverse(), base, baseOffset, end, endOffset, c);
-      var f = Compare.eq(base, end) ? same : diff;
-      return f(base, baseOffset, end, endOffset);
+      return GhettoIdentify.nodes(universe, base, baseOffset, end, endOffset, c);
     };
 
     return {
