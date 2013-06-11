ae0eb05e856005e250dd8e58195171a26a8b6743
ephox_dev <dev@klaatu.(none)>
2013-06-11 23:42:28 +1000

    test cases

--- src/test/js/atomic/leftblock/LeftBlockTest.js
+++ src/test/js/atomic/leftblock/LeftBlockTest.js
@@ -5,11 +5,13 @@ test(
     'ephox.boss.api.Gene',
     'ephox.boss.api.TestUniverse',
     'ephox.boss.api.TextGene',
+    'ephox.compass.Arr',
     'ephox.robin.api.general.LeftBlock'
   ],
 
-  function (Gene, TestUniverse, TextGene, LeftBlock) {
+  function (Gene, TestUniverse, TextGene, Arr, LeftBlock) {
     var universe = TestUniverse(Gene('root', 'root', [
+      TextGene('t0', 'text0'),
       Gene('p1', 'p', [
         Gene('span1', 'span', [
           TextGene('s1-text', 'bolded-text')
@@ -21,7 +23,14 @@ test(
       ])
     ]));
 
-    LeftBlock.all(universe, universe.find(universe.get(), 't3').getOrDie());
+    var check = function (expected, id, method) {
+      var actual = method(universe, universe.find(universe.get(), id).getOrDie());
+      assert.eq(expected, Arr.map(actual, function (x) { return x.id; }));
+    };
+
+    check([ 's1-text', 's2-text', 't3' ], 't3', LeftBlock.all);
+    check([ 'span1', 'span2', 't3' ], 't3', LeftBlock.top);
+    check([ 't0' ], 't0', LeftBlock.all);
 
   }
 );
