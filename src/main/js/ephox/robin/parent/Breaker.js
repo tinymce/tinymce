9bf183b67861c3966bf482420b91bc7590d658a9
ephox_dev <dev@klaatu.(none)>
2013-06-11 18:46:34 +1000

    breaker test

--- src/main/js/ephox/robin/parent/Breaker.js
+++ src/main/js/ephox/robin/parent/Breaker.js
@@ -4,36 +4,30 @@ define(
   [
     'ephox.compass.Arr',
     'ephox.peanut.Fun',
-    'ephox.perhaps.Option',
-    'ephox.sugar.api.Compare',
-    'ephox.sugar.api.Element',
-    'ephox.sugar.api.Insert',
-    'ephox.sugar.api.InsertAll',
-    'ephox.sugar.api.Node',
-    'ephox.sugar.api.Traverse'
+    'ephox.perhaps.Option'
   ],
 
-  function (Arr, Fun, Option, Compare, Element, Insert, InsertAll, Node, Traverse) {
-    var bisect = function (parent, child) {
-      var children = Traverse.children(parent);
-      var index = Arr.findIndex(children, Fun.curry(Compare.eq, child));
+  function (Arr, Fun, Option) {
+    var bisect = function (universe, parent, child) {
+      var children = universe.property().children(parent);
+      var index = Arr.findIndex(children, Fun.curry(universe.eq, child));
       return index > -1 ? Option.some({
         before: Fun.constant(children.slice(0, index)),
         after: Fun.constant(children.slice(index + 1))
       }) : Option.none();
     };
 
-    var unsafeBreakAt = function (parent, parts) {
-      var second = Element.fromDom(parent.dom().cloneNode(false));
-      InsertAll.append(second, parts.after());
-      Insert.after(parent, second);
+    var unsafeBreakAt = function (universe, parent, parts) {
+      var second = universe.create().clone(parent);
+      universe.insert().appendAll(second, parts.after());
+      universe.insert().after(parent, second);
       return second;
     };
 
-    var breakAt = function (parent, child) {
-      var parts = bisect(parent, child);
+    var breakAt = function (universe, parent, child) {
+      var parts = bisect(universe, parent, child);
       return parts.map(function (ps) {
-        return unsafeBreakAt(parent, ps);
+        return unsafeBreakAt(universe, parent, ps);
       });
     };
 
---
+++ src/test/js/atomic/parent/BreakerTest.js
@@ -0,0 +1,58 @@
+test(
+  'BreakerTest',
+
+  [
+    'ephox.boss.api.Gene',
+    'ephox.boss.api.TestUniverse',
+    'ephox.boss.api.TextGene',
+    'ephox.boss.mutant.Logger',
+    'ephox.compass.Arr',
+    'ephox.robin.parent.Breaker'
+  ],
+
+  function (Gene, TestUniverse, TextGene, Logger, Arr, Breaker) {
+
+    var breakAt = function (universe, parent, child) {
+      var parts = bisect(universe, parent, child);
+      return parts.map(function (ps) {
+        return unsafeBreakAt(universe, parent, ps);
+      });
+    };
+
+
+    var doc = TestUniverse(Gene('root', 'root', [
+      Gene('d1', 'div', [
+        TextGene('d1_t1', 'List: '),
+        Gene('ol1', 'ol', [
+          Gene('li1', 'li', [
+            TextGene('li1_text', 'Apples')
+          ]),
+          Gene('li2', 'li', [
+            TextGene('li2_text', 'Beans')
+          ]),
+          Gene('li3', 'li', [
+            TextGene('li3_text', 'Carrots')
+          ]),
+          Gene('li4', 'li', [
+            TextGene('li4_text', 'Diced Tomatoes')
+          ])
+        ])
+      ])
+    ]));
+
+    Breaker.breakAt(doc, doc.find(doc.get(), 'ol1').getOrDie(), doc.find(doc.get(), 'li3').getOrDie());
+    assert.eq(
+      'root(' +
+        'd1(' +
+          'd1_t1,' +
+          'ol1(' +
+            'li1(li1_text),li2(li2_text),li3(li3_text)' +
+          '),' +
+          'clone**<ol1>(' +
+            'li4(li4_text)' +
+          ')' +
+        ')' +
+      ')', Logger.basic(doc.get()));
+
+  }
+);
