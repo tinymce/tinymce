696d7cc707d2db7ceda4594899e57fe51593aa4a
ephox_dev <dev@klaatu.(none)>
2013-06-12 14:35:57 +1000

    cleanup of sourcing

--- config/bolt/prod.js
+++ config/bolt/prod.js
@@ -1,9 +1,6 @@
 configure({
   sources: [
     source('amd', 'ephox.robin', '../../src/main/js', mapper.hierarchical),
-    // source('amd', 'ephox.boss', '../../../boss/src/main/js', mapper.hierarchical),
-    // source('amd', 'ephox.phoenix', '../../../phoenix/src/main/js', mapper.hierarchical),
-    // source('amd', 'ephox.polaris', '../../../polaris/src/main/js', mapper.hierarchical),
     source('amd', 'ephox', '../../lib/run/depend', mapper.flat)
   ]
 });
