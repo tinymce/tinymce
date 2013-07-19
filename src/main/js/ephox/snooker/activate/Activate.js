c426964425d0a0d189b3f6a23475ab643f82c56a
mps <dev@vapid.(none)>
2013-07-19 17:03:40 +1000

    playing with embedding cells

--- src/main/js/ephox/snooker/activate/Activate.js
+++ src/main/js/ephox/snooker/activate/Activate.js
@@ -4,6 +4,7 @@ define(
   [
     'ephox.compass.Arr',
     'ephox.lid.Jam',
+    'ephox.sugar.api.Attr',
     'ephox.sugar.api.Css',
     'ephox.sugar.api.Element',
     'ephox.sugar.api.InsertAll',
@@ -11,7 +12,7 @@ define(
     'ephox.sugar.api.SelectorFilter'
   ],
 
-  function (Arr, Jam, Css, Element, InsertAll, Remove, SelectorFilter) {
+  function (Arr, Jam, Attr, Css, Element, InsertAll, Remove, SelectorFilter) {
     var glossy = function (table) {
       var rows = SelectorFilter.descendants(table, 'tr');
       Arr.each(rows, function (row, i) {
@@ -20,8 +21,10 @@ define(
           var r = Element.fromTag('td');
           Css.setAll(r, {
             width: '2px',
-            background: i === 0 ? 'yellow' : 'none'
+            background: i === 0 ? 'yellow' : 'none',
+            cursor: 'w-resize'
           });
+          Attr.set(r, 'contenteditable', 'false');
           console.log('r: ', r.dom());
           return r;
         });
