e0c6c7c0c18ff9996244914f6fd60b2f07a55dd2
Ben Davey <ben@mobilenationhq.com>
2013-07-29 14:20:33 +1000

    added unit tests

--- src/main/js/ephox/snooker/picker/CellPosition.js
+++ src/main/js/ephox/snooker/picker/CellPosition.js
@@ -6,11 +6,25 @@ define(
 
   function () {
     
-    var getPosition = function (position, size, numCols, numRows, mousePosition) {
+    var getPosition = function (tablePosition, tableSize, numCols, numRows, mousePosition) {
+      
+      var relativeMousePosition = {x:0, y:0};
+      relativeMousePosition.x = mousePosition.x - tablePosition.x;
+      relativeMousePosition.y = mousePosition.y - tablePosition.y;
+
+      var cellWidth = tableSize.width/numCols;
+      var cellHeight = tableSize.height/numRows;
+
+      var col = Math.floor(relativeMousePosition.x/cellWidth);
+      var row = Math.floor(relativeMousePosition.y/cellHeight);
+
+
+
       return {
-        row: 0,
-        col: 0
+        row: row,
+        col: col
       }  
+
     };
 
 
--- src/test/js/atomic/picker/CellPositionTest.js
+++ src/test/js/atomic/picker/CellPositionTest.js
@@ -14,6 +14,9 @@ test(
    }
 
    check({row:0, col:0}, {x:0,y:0}, {width:500, height:500}, 10,10, {x:0,y:0});
-   check({row:3, col:3}, {x:0,y:0}, {width:500, height:500}, 10,10, {x:110,y:110});
+   check({row:2, col:2}, {x:0,y:0}, {width:500, height:500}, 10,10, {x:110,y:110});
+   check({row:0, col:2}, {x:0,y:0}, {width:500, height:500}, 10,10, {x:110,y:10});
+   check({row:2, col:1}, {x:0,y:0}, {width:300, height:1000}, 5,10, {x:110,y:210});
+   check({row:0, col:1}, {x:50,y:180}, {width:300, height:1000}, 5,10, {x:110,y:210});
   }
 );
