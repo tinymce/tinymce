f4b256d9f450a4e9c5e19088e15189c13f33995c
Ben Davey <ben@mobilenationhq.com>
2013-07-29 15:19:55 +1000

    added resize events and mouse click events to picker

--- src/demo/js/ephox/snooker/demo/PickerDemo.js
+++ src/demo/js/ephox/snooker/demo/PickerDemo.js
@@ -4,11 +4,13 @@ define(
   [
     'ephox.snooker.picker.Picker',
     'ephox.sugar.api.Element',
+    'ephox.sugar.api.Height',
     'ephox.sugar.api.Insert',
+    'ephox.sugar.api.Width',
     'global!Math'
   ],
 
-  function (Picker, Element, Insert, Math) {
+  function (Picker, Element, Height, Insert, Width, Math) {
     return function () {
       console.log('hello world');
 
@@ -19,12 +21,29 @@ define(
 
       var val = 3;
 
-      setInterval(function () {
-        val++;
-        picker.setSize(val, val);
+      picker.setSize(10, 10);
+      picker.setSelection(2, 2);
 
-        picker.setSelection( Math.ceil(Math.random() * val) ,Math.ceil(Math.random() * val));
-      }, 1000);
+      picker.events.resize.bind( function() { 
+        var width = Width.get(picker.element());
+        var height = Height.get(picker.element());
+        console.log('picker was resized : (', width, ',', height, ')');
+      });
+
+      picker.events.select.bind( function(event) {
+        console.log('need to create table with ', event.cols(), 'columns and ', event.rows(), 'rows' );
+      });
+
+
+      picker.on();
+      picker.go();
+
+      // setInterval(function () {
+      //   val++;
+      //   picker.setSize(val, val);
+
+      //   picker.setSelection( Math.ceil(Math.random() * val) ,Math.ceil(Math.random() * val));
+      // }, 1000);
     };
   }
 );
--- src/main/js/ephox/snooker/picker/Picker.js
+++ src/main/js/ephox/snooker/picker/Picker.js
@@ -3,26 +3,59 @@ define(
 
   [
     'ephox.compass.Arr',
+    'ephox.dragster.api.Dragger',
+    'ephox.perhaps.Option',
+    'ephox.porkbun.Event',
+    'ephox.porkbun.Events',
+    'ephox.snooker.adjust.Mutation',
+    'ephox.snooker.picker.CellPosition',
     'ephox.snooker.style.Styles',
+    'ephox.sugar.api.Attr',
     'ephox.sugar.api.Class',
+    'ephox.sugar.api.DomEvent',
     'ephox.sugar.api.Element',
+    'ephox.sugar.api.Height',
     'ephox.sugar.api.Insert',
     'ephox.sugar.api.InsertAll',
+    'ephox.sugar.api.Location',
+    'ephox.sugar.api.Node',
     'ephox.sugar.api.Remove',
-    'ephox.sugar.api.SelectorFilter'
+    'ephox.sugar.api.SelectorFilter',
+    'ephox.sugar.api.SelectorFind',
+    'ephox.sugar.api.Width'
   ],
 
-  function(Arr, Styles, Class, Element, Insert, InsertAll, Remove, SelectorFilter) {
-    return function() {
+  function(Arr, Dragger, Option, Event, Events, Mutation, CellPosition, Styles, Attr, Class, DomEvent, Element, Height, Insert, InsertAll, Location, Node, Remove, SelectorFilter, SelectorFind, Width) {
+    return function () {
+
+      var MAX_ROWS = 10;
+      var MIN_ROWS = 1;
+      var MAX_COLS = 10;
+      var MIN_COLS = 1;
+
+      var events = Events.create({
+        resize: Event([]),
+        select: Event(['cols', 'rows'])
+      });
+
       var table = Element.fromTag('table');
       Class.add(table, Styles.resolve('table-picker'));
       var tbody = Element.fromTag('tbody');
       Insert.append(table, tbody);
 
+      var mutation = Mutation();
+      var dragger = Dragger.transform(mutation, {});
+
+      var size = { width: 0, height: 0};
+
+      mutation.events.drag.bind(function (event) {
+        console.log('event.xDelta', event.xDelta());
+      });
+
       var repeat = function(repititions, f) {
         var r = [];
         for (var i = 0; i < repititions; i++) {
-          r.push(f());
+          r.push(f(i));
         }
         return r;
       }
@@ -32,26 +65,45 @@ define(
       };
 
       var destroy = function() {
-
+        clicker.unbind();
       };
 
       var setSize = function (numRows, numCols) {
         //repeat()
+        size = { width: numCols, height: numRows };
         Remove.empty(tbody);
         //create a set of trs, then for each tr, insert numCols tds
-        var rows = repeat(numRows, function() {
+        var rows = repeat(numRows, function (r) {
           return Element.fromTag('tr');
         });
 
-        Arr.each(rows, function(row) {
-          var cells = repeat(numCols, function() {
-            return Element.fromTag('td');
+        Arr.each(rows, function (row, r) {
+          var cells = repeat(numCols, function (c) {
+            var td = Element.fromTag('td');
+            Attr.set(td, 'data-picker-row', r);
+            Attr.set(td, 'data-picker-col', c);
+            return td;
           });
 
           InsertAll.append(row, cells);
           Insert.append(tbody, row);
         });
       };
+
+      var clicker = DomEvent.bind(table, 'mousedown', function (event) {
+        var target = event.target();
+        var cell = Node.name(target) === 'td' ? Option.some(target) : SelectorFind.ancestor(target, 'td');
+        cell.each(function (c) {
+          var col = parseInt(Attr.get(c, 'data-picker-col'), 10);
+          var row = parseInt(Attr.get(c, 'data-picker-row'), 10);
+          console.log('col: ', col, 'row: ', row);
+          events.trigger.select(col + 1, row + 1 );
+        });
+      });
+
+      var getSize = function () {
+
+      }
 
       var setSelection = function(numRows, numCols) {
 
@@ -69,14 +121,58 @@ define(
             Class.add(cell, Styles.resolve('picker-selected'));
           });
         });
+      };
+
+      var active = false;
+
+      var on = function () {
+        active = true;
+      };
+
+      var off = function () {
+        active = false;
+      };
+
+      var handler = function (event) {
+        if (active) {
+          var width = Width.get(table);
+          var height = Height.get(table);
+          var position = Location.absolute(table);
+          var cell = CellPosition.getPosition({x: position.left(), y: position.top()}, {width: width, height: height}, size.width, size.height, { x: event.x(), y: event.y() });
+          var newSize = { col: cell.col + 1, row: cell.row + 1};
+          //validate the size
+          var selection = ensureSize(newSize);
+          var tableSize = ensureSize({ col: selection.col + 1, row: selection.row + 1});
+
+          if (tableSize.row !== size.height || tableSize.col !== size.width) {
+            setSize(tableSize.row, tableSize.col);
+            size = { width: tableSize.col, height: tableSize.row };
+            events.trigger.resize();
+          }
+          
+          setSelection(selection.row, selection.col); 
+        }
       }
 
+      var ensureSize = function (cell) {
+        var col = Math.max(MIN_COLS, Math.min(MAX_COLS, cell.col));
+        var row =  Math.max(MIN_ROWS, Math.min(MAX_ROWS, cell.row));
+        return {col: col, row: row};
+      }
+
+      var go = function () {
+        var hack = DomEvent.bind(Element.fromDom(document), 'mousemove', handler);
+      };
 
       return {
         element: element,
         destroy: destroy,
         setSize: setSize,
-        setSelection: setSelection
+        setSelection: setSelection,
+        go: go,
+        on: on,
+        off: off,
+        events: events.registry
       };
     }
   }
