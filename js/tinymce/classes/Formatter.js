b11d3066170dbf3c48fd08a8e9916471036525a3
Spocke <spocke@moxiecode.com>
2013-08-02 15:08:53 +0200

    Fixed so sub/sup elements gets removed when the Clear formatting action is executed.

--- js/tinymce/classes/Formatter.js
+++ js/tinymce/classes/Formatter.js
@@ -140,7 +140,14 @@ define("tinymce/Formatter", [
 				},
 
 				removeformat: [
-					{selector: 'b,strong,em,i,font,u,strike', remove: 'all', split: true, expand: false, block_expand: true, deep: true},
+					{
+						selector: 'b,strong,em,i,font,u,strike,sub,sup',
+						remove: 'all',
+						split: true,
+						expand: false,
+						block_expand: true,
+						deep: true
+					},
 					{selector: 'span', attributes: ['style', 'class'], remove: 'empty', split: true, expand: false, deep: true},
 					{selector: '*', attributes: ['style', 'class'], split: false, expand: false, deep: true}
 				]
