0dfddf2dd28488d4df9289c5ac415dd7e4c7864f
Spocke <spocke@moxiecode.com>
2013-08-23 17:06:59 +0200

    Fixed bug where save button in save plugin wouldn't get properly enabled when contents was changed.

--- js/tinymce/plugins/save/plugin.js
+++ js/tinymce/plugins/save/plugin.js
@@ -16,7 +16,7 @@ tinymce.PluginManager.add('save', function(editor) {
 
 		formObj = tinymce.DOM.getParent(editor.id, 'form');
 
-		if (editor.getParam("save_enablewhendirty") && !editor.isDirty()) {
+		if (editor.getParam("save_enablewhendirty", true) && !editor.isDirty()) {
 			return;
 		}
 
@@ -67,7 +67,7 @@ tinymce.PluginManager.add('save', function(editor) {
 		var self = this;
 
 		editor.on('nodeChange', function() {
-			self.disabled(editor.getParam("save_enablewhendirty") && !editor.isDirty());
+			self.disabled(editor.getParam("save_enablewhendirty", true) && !editor.isDirty());
 		});
 	}
 
