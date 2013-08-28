73e8c53014a805e21a8dd1a9883c0d65a500d065
Andrew Herron <andrew.herron@ephox.com>
2013-08-29 09:15:25 +1000

    moved isBlock from Soldier to Robin, created Parent.sharedBlock()

--- src/main/js/ephox/robin/api/general/Parent.js
+++ src/main/js/ephox/robin/api/general/Parent.js
@@ -2,12 +2,25 @@ define(
   'ephox.robin.api.general.Parent',
 
   [
+    'ephox.peanut.Fun',
+    'ephox.perhaps.Option',
+    'ephox.robin.api.general.Identify',
     'ephox.robin.parent.Breaker',
     'ephox.robin.parent.Shared',
     'ephox.robin.parent.Subset'
   ],
 
-  function (Breaker, Shared, Subset) {
+  function (Fun, Option, Identify, Breaker, Shared, Subset) {
+    var sharedBlock = function (universe, elements) {
+      var look = function(universe, element) {
+        var domIsBlock = Fun.curry(Identify.isBlock, universe);
+        return domIsBlock(element) ?
+            Option.some(element) :
+            universe.up().predicate(element, domIsBlock);
+      };
+      return Shared.oneAll(universe, look, elements);
+    };
+
     var sharedOne = function (universe, look, elements) {
       return Shared.oneAll(universe, look, elements);
     };
@@ -22,6 +35,7 @@ define(
 
     return {
       sharedOne: sharedOne,
+      sharedBlock: sharedBlock,
       subset: subset,
       breakAt: breakAt
     };
