41eff283fde5fc2cb1cfc80246f73bb63c29de13
Nick Simmons <nick.simmons@jadedpixel.com>
2013-08-29 14:25:34 -0400

    Added noparsing plugin

---
+++ js/tinymce/plugins/noparsing/plugin.js
@@ -0,0 +1,25 @@
+/**
+ * plugin.js
+ *
+ * Copyright, Shopify Inc.
+ * Released under LGPL License.
+ *
+ * License: http://www.tinymce.com/license
+ */
+
+/**
+ * No Parsing
+ *
+ * This plugin prevents HTML content from being parsed and reformatted by TinyMCE.
+ */
+tinymce.PluginManager.add('noparsing', function(editor) {
+
+  editor.on("BeforeSetContent", function(args) {
+    args.format = "unvalidated_html";
+  });
+
+  editor.on("BeforeGetContent", function(args) {
+    args.format = "unvalidated_html";
+  });
+
+});
