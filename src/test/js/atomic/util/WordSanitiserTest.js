fe81a755d573f756eece61488bddeb8d7869bf94
Andrew Herron <andrew.herron@ephox.com>
2013-09-03 11:23:48 +1000

    unused functions, whitespace

--- src/test/js/atomic/util/WordSanitiserTest.js
+++ src/test/js/atomic/util/WordSanitiserTest.js
@@ -10,18 +10,9 @@ test(
   function (Option, WordScope, WordSanitiser) {
 
     var some = Option.some;
-    var none = Option.none();
-
-    var ns = function (word, v) {
-      return WordScope(word, none, some(v));
-    };
 
     var ss = function (word, v1, v2) {
       return WordScope(word, some(v1), some(v2));
-    };
-
-    var nn = function (word) {
-      return WordScope(word, none, none);
     };
 
     var check = function (expected, input) {
@@ -31,13 +22,13 @@ test(
       assert.eq(true, expected.right().equals(actual.right()));
     };
 
-    check(ss('one',         '<',  '>' ),  ss('one',         '<', '>'));
+    check(ss('one',         '<',  '>'),   ss('one',         '<', '>'));
     check(ss('one',         '<',  '\''),  ss('one\'',       '<', '>'));
-    check(ss('one',         '\'', '>' ),  ss('\'one',       '<', '>'));
-    check(ss('\'twas',      '<',  '>' ),  ss('\'twas',      '<', '>'));
+    check(ss('one',         '\'', '>'),   ss('\'one',       '<', '>'));
+    check(ss('\'twas',      '<',  '>'),   ss('\'twas',      '<', '>'));
     check(ss('\'twas',      '\'', '\''),  ss('\'\'twas\'',  '<', '>'));
-    check(ss('\'\'one\'\'', '<',  '>' ),  ss('\'\'one\'\'', '<', '>'));
-    check(ss('\'twas',      '\'', '>' ),  ss('\'\'twas',    '<', '>'));
+    check(ss('\'\'one\'\'', '<',  '>'),   ss('\'\'one\'\'', '<', '>'));
+    check(ss('\'twas',      '\'', '>'),   ss('\'\'twas',    '<', '>'));
 
   }
 );
