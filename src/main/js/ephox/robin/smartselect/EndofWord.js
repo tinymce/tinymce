c6e21dfa04afda1c2cd1bcd5ee629eef0772df7a
mps <dev@vapid.(none)>
2013-10-02 13:48:51 +1000

    TBIO-143: a bit of cleanup in end of word

--- src/main/js/ephox/robin/smartselect/EndofWord.js
+++ src/main/js/ephox/robin/smartselect/EndofWord.js
@@ -11,46 +11,49 @@ define(
   ],
 
   function (Arr, Fun, Option, Spot, WordRange, WordUtil) {
-    var mogel = function (universe, item, offset, left, right) {      
+    var mogel = function (universe, item, offset, left, right) {
+      var getText = function (target) {
+        return universe.property().isText(target) ? universe.property().getText(target) : '';
+      };
+
       // ASSUMPTION: item is a text node. Probably an invalid assumption. Clean this up.
       var text = universe.property().getText(item);
       var parts = WordUtil.around(text, offset);
 
       /* Check the text node length of the items left. If text length is zero, then at end of word */
       var leftText = Arr.map(left, function (l) {
-        var elem = l.element();
-        return universe.property().isText(elem) ? universe.property().getText(elem).substring(l.offset()) : '';
+        return getText(l.element()).substring(l.offset());
       }).join('');
 
       var rightText = Arr.map(right, function (r) {
-        var elem = r.element();
-        return universe.property().isText(elem) ? universe.property().getText(elem).substring(0, r.offset()) : '';
+        return getText(r.element()).substring(0, r.offset());
       }).join('');
 
       var start = Option.from(left[0]).getOr(Spot.point(item, 0));
       var finish = Option.from(right[right.length - 1]).getOr(Spot.point(item, text.length));
       var current = WordRange(item, offset, item, offset);
 
+      var isLeftEdge = function () {
+        return leftText.length === 0 && offset === 0;
+      };
+
+      var isRightEdge = function () {
+        return rightText.length === 0 && offset === text.length;
+      };
+
       var neither = function () {
-        console.log('neither');
-        if ((leftText.length === 0 && offset === 0) || (rightText.length === 0 && offset === text.length)) return current;
-        else return WordRange(start.element(), start.offset(), finish.element(), finish.offset());
+        return isLeftEdge() || isRightEdge() ? current : WordRange(start.element(), start.offset(), finish.element(), finish.offset());
       };
 
       var justBefore = function (bindex) {
-        console.log('just.before', bindex);
-        if (rightText.length === 0 && offset === text.length) return current;
-        else return WordRange(item, bindex, finish.element(), finish.offset());
+        return isRightEdge() ? current : WordRange(item, bindex, finish.element(), finish.offset());
       };
 
       var justAfter = function (aindex) {
-        console.log('just.after', aindex);
-        if (leftText.length === 0 && offset === 0) return current;
-        else return WordRange(start.element(), start.offset(), item, aindex);
+        return isLeftEdge() ? current : WordRange(start.element(), start.offset(), item, aindex);
       };
 
       var both = function (bindex, aindex) {
-        console.log('both', bindex, aindex);
         return WordRange(item, bindex, item, aindex);
       };
 
