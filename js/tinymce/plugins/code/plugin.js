3f61b24add87586e91ab6f700a6c981305fb404d
Spocke <spocke@moxiecode.com>
2013-10-24 11:54:50 +0200

    Fixed so source editor moves focus to editor after setting new contents.

--- js/tinymce/plugins/code/plugin.js
+++ js/tinymce/plugins/code/plugin.js
@@ -26,9 +26,9 @@ tinymce.PluginManager.add('code', function(editor) {
 			},
 			onSubmit: function(e) {
 				editor.undoManager.transact(function() {
-					editor.setContent(e.data.code);
+					editor.setContent(e.data.code, {focus: true});
 				});
-	
+
 				editor.nodeChanged();
 			}
 		});
