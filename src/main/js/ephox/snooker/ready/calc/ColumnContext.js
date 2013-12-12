97c5089491c2816c57b7e54ba6e278bf5bfc2fc1
mps <morgan.smith@ephox.com>
2013-12-12 17:36:19 +1000

    TBIO-418: creating ready

---
+++ src/main/js/ephox/snooker/ready/calc/ColumnContext.js
@@ -0,0 +1,52 @@
+define(
+  'ephox.snooker.ready.calc.ColumnContext',
+
+  [
+  ],
+
+  function () {
+    var none = function () {
+      return folder(function (n, o, l, m, r) {
+        return n();
+      });
+    };
+
+    var only = function () {
+      return folder(function (n, o, l, m, r) {
+        return o();
+      });
+    };
+
+    var left = function (index, next) {
+      return folder(function (n, o, l, m, r) {
+        return l(index, next);
+      });
+    };
+
+    var middle = function (prev, index, next) {
+      return folder(function (n, o, l, m, r) {
+        return m(prev, index, next);
+      });
+    };
+
+    var right = function (prev, index) {
+      return folder(function (n, o, l, m, r) {
+        return r(prev, index);
+      });
+    };
+
+    var folder = function (fold) {
+      return {
+        fold: fold
+      };
+    };
+
+    return {
+      none: none,
+      only: only,
+      left: left,
+      middle: middle,
+      right: right
+    };
+  }
+);
