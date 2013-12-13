7608a47d6c75b0a4e28d589abe8da84ec7035d14
mps <morgan.smith@ephox.com>
2013-12-13 11:58:12 +1000

    TBIO-409: resizing picker component

--- src/main/js/ephox/snooker/ready/picker/Redimension.js
+++ src/main/js/ephox/snooker/ready/picker/Redimension.js
@@ -2,22 +2,17 @@ define(
   'ephox.snooker.ready.picker.Redimension',
 
   [
-    'ephox.porkbun.Event',
-    'ephox.porkbun.Events',
     'ephox.snooker.ready.data.Structs',
     'ephox.snooker.ready.picker.CellPosition',
+    'ephox.snooker.ready.picker.Sizing',
     'ephox.sugar.api.Height',
     'ephox.sugar.api.Location',
     'ephox.sugar.api.Width',
     'global!Math'
   ],
 
-  function (Event, Events, Structs, CellPosition, Height, Location, Width, Math) {
+  function (Structs, CellPosition, Sizing, Height, Location, Width, Math) {
     return function (settings) {
-      var events = Events.create({
-        resize: Event(['grid'])
-      });
-
       var active = false;
 
       var on = function () {
@@ -36,36 +31,19 @@ define(
 
       var getPosition = function (table) {
         var position = Location.absolute(table.element());
-        return Structs.xy(position.left(), position.top());
-      };
-
-      var translate = function (cell, row, column) {
-        return Structs.cell(cell.row() + row, cell.column() + column);
-      };
-
-      var validate = function (cell, minX, maxX, minY, maxY) {
-        var col = Math.max(minX, Math.min(maxX, cell.column()));
-        var row =  Math.max(minY, Math.min(maxY, cell.row()));
-        return Structs.cell(row, col);
+        return Structs.coords(position.left(), position.top());
       };
 
       var handle = function (table, grid, x, y) {
         if (active) {
           var dimensions = getDimensions(table);
           var position = getPosition(table);
-          var mouse = Structs.xy(x, y);
-          var cell = CellPosition.findCell(position, dimensions, grid, mouse);
-          var newSize = translate(cell, 1, 1);
-
-          var selection = validate(newSize, 0, settings.maxCols, 0, settings.maxRows);
-          var fullGrid = validate(translate(selection, 1, 1), settings.minCols, settings.maxCols, settings.minRows, settings.maxRows);
-
-          if (fullGrid.row() !== grid.rows() || fullGrid.column() !== grid.columns()) {
-            table.setSize(fullGrid.row(), fullGrid.column());
-            events.trigger.resize(fullGrid);
-          }
-          
-          table.setSelection(selection.row(), selection.column());
+          var mouse = Structs.coords(x, y);
+          var address = CellPosition.findCell(position, dimensions, grid, mouse);
+          var changes = Sizing.resize(address, settings);
+          var full = changes.full();
+          if (full.row() !== grid.rows() || full.column() !== grid.columns()) table.setSize(full.row(), full.column());
+          table.setSelection(changes.selection().row(), changes.selection().column());
         }
       };
 
