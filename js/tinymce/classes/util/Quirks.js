3fe05c6ab1da649118ea75f871db04908b68a0c7
Spocke <spocke@moxiecode.com>
2014-01-14 17:46:04 +0100

    Fixed bug where WebKit would fire focus/blur events incorrectly if the editor was empty due to a WebKit focus bug.

--- js/tinymce/classes/util/Quirks.js
+++ js/tinymce/classes/util/Quirks.js
@@ -338,7 +338,8 @@ define("tinymce/util/Quirks", [
 				// Case 2 IME doesn't initialize if you click the documentElement it also doesn't properly fire the focusin event
 				dom.bind(editor.getDoc(), 'mousedown', function(e) {
 					if (e.target == editor.getDoc().documentElement) {
-						editor.getWin().focus();
+						e.preventDefault();
+						editor.getBody().focus();
 						selection.setRng(selection.getRng());
 					}
 				});
