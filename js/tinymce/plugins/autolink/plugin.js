af23e302bac1124b363f0225a4dd8d8cee93ae8e
Spocke <spocke@moxiecode.com>
2014-01-31 15:45:59 +0100

    Fixed a bug where the autolink plugin would steal focus when initialized on IE 9+.

--- js/tinymce/plugins/autolink/plugin.js
+++ js/tinymce/plugins/autolink/plugin.js
@@ -11,6 +11,8 @@
 /*global tinymce:true */
 
 tinymce.PluginManager.add('autolink', function(editor) {
+	var AutoUrlDetectState;
+
 	editor.on("keydown", function(e) {
 		if (e.keyCode == 13) {
 			return handleEnter(editor);
@@ -19,11 +21,15 @@ tinymce.PluginManager.add('autolink', function(editor) {
 
 	// Internet Explorer has built-in automatic linking for most cases
 	if (tinymce.Env.ie) {
-		editor.on("init", function() {
-			try {
-				editor.execCommand('AutoUrlDetect', false, true);
-			} catch (ex) {
-				// Ignore
+		editor.on("focus", function() {
+			if (!AutoUrlDetectState) {
+				AutoUrlDetectState = true;
+
+				try {
+					editor.execCommand('AutoUrlDetect', false, true);
+				} catch (ex) {
+					// Ignore
+				}
 			}
 		});
 
