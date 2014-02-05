6dddb52e32029bfde9cadb67aca859f1e70ccb43
Naim Hammadi <hammadi_naim@yahoo.fr>
2014-02-05 01:18:02 -0800

    Added visual_anchor_class configuration option

--- js/tinymce/classes/Editor.js
+++ js/tinymce/classes/Editor.js
@@ -1979,8 +1979,8 @@ define("tinymce/Editor", [
 
 					case 'A':
 						if (!dom.getAttrib(elm, 'href', false)) {
-							value = dom.getAttrib(elm, 'name') || elm.id;
-							cls = 'mce-item-anchor';
+						    value = dom.getAttrib(elm, 'name') || elm.id;
+						    cls = settings.visual_anchor_class || 'mce-item-anchor';
 
 							if (value) {
 								if (self.hasVisual) {
