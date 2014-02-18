e724d187a481656a23cd54562752d3bf6a7dad3d
Diane <dianek@surveymonkey.com>
2014-02-18 14:06:03 -0800

    Remove extra br tag for IE9/10
    http://www.tinymce.com/develop/bugtracker_view.php?id=6610
    Only add a bogus br tag on empty editors for IE < 9 instead of IE < 11,
    as IE9 and IE10 render the elements properly and don't need the extra
    tag.

--- js/tinymce/classes/Editor.js
+++ js/tinymce/classes/Editor.js
@@ -1709,9 +1709,9 @@ define("tinymce/Editor", [
 				// Check if forcedRootBlock is configured and that the block is a valid child of the body
 				if (forcedRootBlockName && self.schema.isValidChild(body.nodeName.toLowerCase(), forcedRootBlockName.toLowerCase())) {
 					// Padd with bogus BR elements on modern browsers and IE 7 and 8 since they don't render empty P tags properly
-					content = ie && ie < 11 ? '' : '<br data-mce-bogus="1">';
+					content = ie && ie < 9 ? '' : '<br data-mce-bogus="1">';
 					content = self.dom.createHTML(forcedRootBlockName, self.settings.forced_root_block_attrs, content);
-				} else if (!ie || ie < 11) {
+				} else if (!ie || ie < 9) {
 					// We need to add a BR when forced_root_block is disabled on non IE browsers to place the caret
 					content = '<br data-mce-bogus="1">';
 				}
