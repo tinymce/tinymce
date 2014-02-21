ec67a0c4c090a5ccf48063e17321954b8d933a2c
mps <morgan.smith@ephox.com>
2014-02-21 15:53:10 +1000

    TBIO-141: typo

--- src/main/js/ephox/phoenix/split/Positions.js
+++ src/main/js/ephox/phoenix/split/Positions.js
@@ -7,7 +7,7 @@ define(
 
   function (SplitPosition) {
     /* 
-     * Category a split of a text node as: none, start, middle, or end
+     * Categorise a split of a text node as: none, start, middle, or end
      */
     var determine = function (target) {
       return target.before().fold(function () {
