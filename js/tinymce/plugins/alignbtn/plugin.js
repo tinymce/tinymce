37185874e1614483273dbf6c2573a2991490eb06
Matthias Burtscher <matthias.burtscher@fusonic.net>
2014-04-11 09:21:53 +0200

    Add alignbtn plugin

---
+++ js/tinymce/plugins/alignbtn/plugin.js
@@ -0,0 +1,50 @@
+tinymce.PluginManager.add('alignbtn', function(editor) {
+    var plugin = this;
+
+    editor.addButton('align', {
+        type: 'menubutton',
+        title: 'Alignment',
+        icon: 'alignleft',
+        menu: {
+            type: 'menu',
+            items: [
+                {
+                    text: 'Left',
+                    icon: 'alignleft',
+                    cmd: 'JustifyLeft'
+                },
+                {
+                    text: 'Center',
+                    icon: 'aligncenter',
+                    cmd: 'JustifyCenter'
+                },
+                {
+                    text: 'Right',
+                    icon: 'alignright',
+                    cmd: 'JustifyRight'
+                },
+                {
+                    text: 'Justify',
+                    icon: 'alignjustify',
+                    cmd: 'JustifyFull'
+                }
+            ]
+        },
+        onPostRender: function() {
+            var ctrl = this;
+
+            editor.on('init', function() {
+                tinymce.each(ctrl.settings.menu.items, function(option) {
+                    var align = option.text;
+
+                    editor.formatter.formatChanged('align' + align.toLowerCase(), function(state) {
+                        ctrl.icon('align' + (state ? align.toLowerCase() : 'left'));
+                    });
+                });
+            });
+        },
+        onselect: function(e) {
+            editor.execCommand(e.control.settings.cmd);
+        }
+    });
+});
