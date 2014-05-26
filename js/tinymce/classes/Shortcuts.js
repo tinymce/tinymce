66234693a4f13ab9fad33df80067e54dbb38f09c
Spocke <spocke@moxiecode.com>
2014-05-26 16:30:00 +0200

    Fixed so numeric keycodes can be used in the shortcut format enabling support for any key to be specified.

--- js/tinymce/classes/Shortcuts.js
+++ js/tinymce/classes/Shortcuts.js
@@ -92,8 +92,13 @@ define("tinymce/Shortcuts", [
 							break;
 
 						default:
-							shortcut.charCode = value.charCodeAt(0);
-							shortcut.keyCode = keyCodeLookup[value] || value.toUpperCase().charCodeAt(0);
+							// Allow numeric keycodes like ctrl+219 for ctrl+[
+							if (/^[0-9]{2,}$/.test(value)) {
+								shortcut.keyCode = parseInt(value, 10);
+							} else {
+								shortcut.charCode = value.charCodeAt(0);
+								shortcut.keyCode = keyCodeLookup[value] || value.toUpperCase().charCodeAt(0);
+							}
 					}
 				});
 
