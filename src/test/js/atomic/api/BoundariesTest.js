36880482b8ad9749657d074097ff58d7e55d8a81
Maurizio Napoleoni <maurizio.napoleoni@ephox.com>
2014-08-18 10:00:32 +1000

    TBIO-1173: Added a new boundaries module

---
+++ src/test/js/atomic/api/BoundariesTest.js
@@ -0,0 +1,22 @@
+test(
+  'BoundariesTest',
+
+  [
+  	'ephox.polaris.api.Boundaries'
+  ],
+
+  function (Boundaries) {
+    var comparator = function (a, b) {
+    	return a === b;
+    };
+
+    var check = function (items, l, r, pred, expected) {
+    	assert.eq(Boundaries.boundAt(items, l, r, pred), expected);
+    };
+
+    check(['a', 'b', 'c', 'd', 'e'], 'b', 'd', comparator, ['b', 'c', 'd']);
+    check(['a', 'b', 'c', 'd', 'e'], 'a', 'e', comparator, ['a', 'b', 'c', 'd', 'e']);
+    check(['a'], 'a', 'a', comparator, ['a']);
+    check([], '1', '3', comparator, []);
+  }
+);
