cb7a0c30ebe86c8dcf04168efe660a8b65a5cc3e
Mike Chau <mike.chau@ephox.com>
2014-08-21 15:38:42 +1000

    TBIO-1244: case 1 black list approach, the textnodes' parents cannot be these tags

--- src/main/js/ephox/phoenix/family/Range.js
+++ src/main/js/ephox/phoenix/family/Range.js
@@ -4,14 +4,13 @@ define(
   [
     'ephox.compass.Arr',
     'ephox.peanut.Fun',
-    'ephox.perhaps.Option',
     'ephox.phoenix.api.general.Extract',
     'ephox.phoenix.family.Parents',
     'ephox.sugar.api.Node',
     'ephox.sugar.api.Traverse'
   ],
 
-  function (Arr, Fun, Option, Extract, Parents, Node, Traverse) {
+  function (Arr, Fun, Extract, Parents, Node, Traverse) {
     var index = function (universe, items, item) {
       return Arr.findIndex(items, Fun.curry(universe.eq, item));
     };
@@ -25,60 +24,17 @@ define(
      *
      * Deltas are a broken concept. They control whether the item passed is included in the result.
      */
-
-
-
-
     var range = function (universe, item1, delta1, item2, delta2) {
+      var validParents = function (element) {
+        // textnodes can't be children of
+        var blacklist = [ 'table', 'tbody', 'tr', 'ul', 'ol' ];
 
-    var piggy = function (element, tag) {
-      return Traverse.prevSibling(element).bind(function (left) {
-        return Traverse.nextSibling(element).map(function (right) {
-          var me = Node.name(element);
-          var l = Node.name(element);
-          var r = Node.name(element);
-          console.log(l, me, r, tag);
-
-          return Array(l, me , r );
-        });
-      });
-    };
-
-    var magic = function (el) {
-e = el
-      console.log(el.dom())
-
-p = piggy(el, 'td')
-console.log(p.isSome());
-
-
-      if(el.dom().nodeValue !== null) {
-        console.log(el.dom().nodeValue.charCodeAt(0))
-
-        var enter = el.dom().nodeValue.charCodeAt(0);
-console.log(enter)
-        // if(enter === 13) return false
-      }
-
-// if(el.dom().nodeType === 3) {debugger}
-
-      // Option.from(Node.isText(el)).map(function () {
-      //   return Option.some(Node.value(el).charCodeAt(0)).forAll(function () {
-
-      //   });
-
-      // }).getOr(false);
-
-      // return Node.isText(el) && el.dom().nodeType !== 3 ? true : false;
-
-      // return universe.property().isText(el);
-
-      if(Node.isText(el) && Node.value(el).charCodeAt(0) !== 13) {
-        return true;
-      } else {
-        return false;
-      }
-    };
+        return universe.property().isText(element) && Traverse.parent(element).map(function (parent) {
+          return Arr.forall(blacklist, function (tag) {
+            return tag !== Node.name(parent);
+          });
+        }).getOr(false);
+      };
 
       if (universe.eq(item1, item2)) return [item1];
 
@@ -89,7 +45,7 @@ console.log(enter)
         var start = index(universe, items, item1);
         var finish = index(universe, items, item2);
         var result = start > -1 && finish > -1 ? order(items, start, delta1, finish, delta2) : [];
-        return Arr.filter(result, magic);
+        return Arr.filter(result, validParents);
       });
     };
 
