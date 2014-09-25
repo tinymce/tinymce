20af2aa1eb56da4864233600078f05c7f5aa836f
Spocke <spocke@moxiecode.com>
2014-09-25 11:13:08 +0200

    Updated changelog and added u as a valid word element.

--- js/tinymce/plugins/paste/classes/WordFilter.js
+++ js/tinymce/plugins/paste/classes/WordFilter.js
@@ -366,7 +366,7 @@ define("tinymce/pasteplugin/WordFilter", [
 				var validElements = settings.paste_word_valid_elements;
 				if (!validElements) {
 					validElements = (
-						'-strong/b,-em/i,-span,-p,-ol,-ul,-li,-h1,-h2,-h3,-h4,-h5,-h6,' +
+						'-strong/b,-em/i,-u,-span,-p,-ol,-ul,-li,-h1,-h2,-h3,-h4,-h5,-h6,' +
 						'-p/div,-a[href|name],sub,sup,strike,br,del,table[width],tr,' +
 						'td[colspan|rowspan|width],th[colspan|rowspan|width],thead,tfoot,tbody'
 					);
