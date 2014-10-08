b5484d7961759650ede88475e612024567f0ec24
suit <suit@rebell.at>
2014-10-08 10:36:05 +0200

    Create plugin.js

---
+++ js/tinymce/plugins/shy/plugin.js
@@ -0,0 +1,28 @@
+/**
+ * plugin.js for TinyMCE soft hyphen (shy)
+ *
+ */
+
+tinymce.PluginManager.requireLangPack('shy', 'de');
+
+tinymce.PluginManager.add('shy', function(editor) {
+	editor.addCommand('mceShy', function() {
+		editor.insertContent(
+			(editor.plugins.visualchars && editor.plugins.visualchars.state) ?
+			'<span class="mce-shy">&shy;</span>' : '&shy;'
+		);
+		editor.dom.setAttrib(editor.dom.select('span.mce-shy'), 'data-mce-bogus', '1');
+	});
+
+	editor.addButton('shy', {
+		title: 'Soft hyphen',
+		image: tinyMCE.baseURL + '/plugins/shy/icon.png',
+		cmd: 'mceShy'
+	});
+
+	editor.addMenuItem('shy', {
+		text: 'Soft hyphen',
+		cmd: 'mceShy',
+		context: 'insert'
+	});
+});
