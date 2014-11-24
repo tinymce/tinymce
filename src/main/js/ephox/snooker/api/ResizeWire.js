2f25a5315f278f62a83d50e1c8fafcdc07000594
mps <morgan.smith@ephox.com>
2014-11-24 18:05:18 +1000

    TBIO-1824: cleanup

--- src/main/js/ephox/snooker/api/ResizeWire.js
+++ src/main/js/ephox/snooker/api/ResizeWire.js
@@ -3,15 +3,16 @@ define(
 
   [
     'ephox.peanut.Fun',
-    'ephox.scullion.Struct',
+    'ephox.perhaps.Option',
     'ephox.sugar.alien.Position',
     'ephox.sugar.api.Element',
     'ephox.sugar.api.Location'
   ],
 
-  function (Fun, Struct, Position, Element, Location) {
+  function (Fun, Option, Position, Element, Location) {
     var only = function (element) {
-      var parent = element.dom().documentElement ? Element.fromDom(element.dom().documentElement) : element;
+      // If we are a document, use the document element for appending.
+      var parent = Option.from(element.dom().documentElement).map(Element.fromDom).getOr(element);
       return {
         parent: Fun.constant(parent),
         view: Fun.constant(element),
