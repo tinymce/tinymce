e0083a6e83cb10926c848974f0d4b2ae315c79f0
mps <morgan.smith@ephox.com>
2014-12-24 13:19:08 +1000

    TBIO-179-b: cleanup

--- src/main/js/ephox/phoenix/api/general/Gather.js
+++ src/main/js/ephox/phoenix/api/general/Gather.js
@@ -2,29 +2,32 @@ define(
   'ephox.phoenix.api.general.Gather',
 
   [
-    'ephox.phoenix.gather.LeafGather',
-    'ephox.sugar.api.Traverse'
+    'ephox.phoenix.gather.LeafGather'
   ],
 
   /**
    * Documentation is in the actual implementations.
    */
-  function (LeafGather, Traverse) {
+  function (LeafGather) {
     var gather = function (universe, item, prune, transform) {
       return [];
       // return Gather.gather(universe, item, prune, transform);
     };
 
-    var isLeaf = function (element) {
-      return Traverse.chlidren(element).length === 0;
+    var isLeaf = function (universe, element) {
+      return universe.property().children(element).length === 0;
     };
 
     var before = function (universe, item, isRoot) {
-      return LeafGather.before(universe, item, isLeaf);
+      return LeafGather.before(universe, item, function (elem) {
+        return isLeaf(universe, elem);
+      });
     };
 
     var after = function (universe, item, isRoot) {
-      return LeafGather.after(universe, item, isLeaf);
+      return LeafGather.after(universe, item, function (elem) {
+        return isLeaf(universe, elem);
+      });
     };
 
     return {
