4a75b2a8148ea119b82a6301f0c31e2c4e01bb98
hashmode <hashmode@users.noreply.github.com>
2014-12-27 18:45:09 +0400

    Update plugin.js
    Adding a possibility to set title attribute of the img tag, might become handy for SEO purposes.

--- js/tinymce/plugins/image/plugin.js
+++ js/tinymce/plugins/image/plugin.js
@@ -146,6 +146,10 @@ tinymce.PluginManager.add('image', function(editor) {
 			if (!data.alt) {
 				data.alt = '';
 			}
+			
+			if (!data.title) {
+				data.title = '';
+			}
 
 			if (data.width === '') {
 				data.width = null;
@@ -163,6 +167,7 @@ tinymce.PluginManager.add('image', function(editor) {
 			data = {
 				src: data.src,
 				alt: data.alt,
+				title: data.title,
 				width: data.width,
 				height: data.height,
 				style: data.style,
@@ -242,6 +247,7 @@ tinymce.PluginManager.add('image', function(editor) {
 			data = {
 				src: dom.getAttrib(imgElm, 'src'),
 				alt: dom.getAttrib(imgElm, 'alt'),
+				title: dom.getAttrib(imgElm, 'title'),
 				"class": dom.getAttrib(imgElm, 'class'),
 				width: width,
 				height: height
@@ -310,6 +316,7 @@ tinymce.PluginManager.add('image', function(editor) {
 
 		if (editor.settings.image_description !== false) {
 			generalFormItems.push({name: 'alt', type: 'textbox', label: 'Image description'});
+			generalFormItems.push({name: 'title', type: 'textbox', label: 'Image Title'});
 		}
 
 		if (imageDimensions) {
