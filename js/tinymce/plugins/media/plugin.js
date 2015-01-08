6d197dd445581ad7fe7bb6bc0f0a236f5df9a660
Spocke <spocke@moxiecode.com>
2015-01-08 16:27:29 +0100

    Added aria labels for Width/Height in media dialog.

--- js/tinymce/plugins/media/plugin.js
+++ js/tinymce/plugins/media/plugin.js
@@ -121,9 +121,9 @@ tinymce.PluginManager.add('media', function(editor, url) {
 				align: 'center',
 				spacing: 5,
 				items: [
-					{name: 'width', type: 'textbox', maxLength: 5, size: 3, onchange: recalcSize},
+					{name: 'width', type: 'textbox', maxLength: 5, size: 3, onchange: recalcSize, ariaLabel: 'Width'},
 					{type: 'label', text: 'x'},
-					{name: 'height', type: 'textbox', maxLength: 5, size: 3, onchange: recalcSize},
+					{name: 'height', type: 'textbox', maxLength: 5, size: 3, onchange: recalcSize, ariaLabel: 'Height'},
 					{name: 'constrain', type: 'checkbox', checked: true, text: 'Constrain proportions'}
 				]
 			});
