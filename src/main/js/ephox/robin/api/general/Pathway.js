4630dee843b3224dd9fbe669b5019b3afac2cf50
mps <morgan.smith@ephox.com>
2015-01-21 10:42:22 +1000

    TBIO-197-b: pathway is not used

--- src/main/js/ephox/robin/api/general/Pathway.js
+++ src/main/js/ephox/robin/api/general/Pathway.js
@@ -2,25 +2,11 @@ define(
   'ephox.robin.api.general.Pathway',
 
   [
-    'ephox.phoenix.api.general.Gather',
-    'ephox.robin.pathway.Prune',
     'ephox.robin.pathway.Simplify',
     'ephox.robin.pathway.Transform'
   ],
 
-  function (Gather, Prune, Simplify, Transform) {
-
-    /**
-     * Gathers nodes between the start and finish elements.
-     *
-     * Where a node's children are all included, only the node is returned.
-     */
-    var between = function (universe, start, finish) {
-      var prune = Prune.range(universe, start, finish);
-      var actual = Gather.gather(universe, start, prune, transform(universe));
-      return actual.left().concat([start]).concat(actual.right());
-    };
-
+  function (Simplify, Transform) {
     /**
      * @see Simplify.simplify()
      */
@@ -33,7 +19,6 @@ define(
     };
 
     return {
-      between: between,
       simplify: simplify,
       transform: transform
     };
