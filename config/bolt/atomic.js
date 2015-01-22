e577afe871c648204adbb92b234630216c67c474
mps <morgan.smith@ephox.com>
2015-01-23 09:34:12 +1000

    TBIO-179-b: cleaning up test profiles

--- config/bolt/atomic.js
+++ config/bolt/atomic.js
@@ -2,8 +2,5 @@ configure({
   configs: [
     './test.js',
     './prod.js'
-  ],
-  sources: [
-    source('amd', 'ephox.phoenix', '../../../phoenix/src/main/js', mapper.hierarchical)
   ]
 });
--- config/bolt/test.js
+++ config/bolt/test.js
@@ -1,7 +1,6 @@
 configure({
   sources: [
     source('amd', 'ephox.robin.test', '../../src/test/js/module', mapper.hierarchical),
-    source('amd', 'ephox.phoenix', '../../../phoenix/src/main/js', mapper.hierarchical),
     source('amd', 'ephox.wrap.Json', '../../lib/test', mapper.flat)
   ]
 });
