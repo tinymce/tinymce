68afebae064621a2cbcbea9dd7ecde3bfecd1570
mps <morgan.smith@ephox.com>
2015-01-23 09:36:37 +1000

    TBIO-179-b: comments

--- src/main/js/ephox/robin/api/general/LeftBlock.js
+++ src/main/js/ephox/robin/api/general/LeftBlock.js
@@ -12,10 +12,13 @@ define(
     var walkers = Gather.walkers();
 
     var goLeft = function (universe, item, mode, strategy) {
+      // Walk the DOM to the left using the appropriate rules.
       var next = Gather.walk(universe, item, mode, walkers.left(), strategy.rules);
       return next.map(function (n) {
+        // If we hit a block, then we stop.
         var isBlock = universe.property().isEmptyTag(n.item()) || universe.property().isBoundary(n.item());
         if (isBlock) return [];
+        // Only include the current item if the strategy permits it. Top won't add parents.
         var inclusions = strategy.inclusion(universe, n, item) ? [ n.item() ] : [];
         return inclusions.concat(goLeft(universe, n.item(), n.mode(), strategy));
       }).getOr([]);
