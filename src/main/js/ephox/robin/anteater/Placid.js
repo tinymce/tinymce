5102cfac02330bd950f0fbe1db076f38d5ced203
mps <morgan.smith@ephox.com>
2015-01-28 15:09:26 +1000

    TBIO-1766: using entry points everywhere

--- src/main/js/ephox/robin/anteater/Placid.js
+++ src/main/js/ephox/robin/anteater/Placid.js
@@ -2,48 +2,14 @@ define(
   'ephox.robin.anteater.Placid',
 
   [
-    'ephox.peanut.Fun',
     'ephox.perhaps.Option',
     'ephox.phoenix.api.general.Gather',
     'ephox.phoenix.api.general.Split',
     'ephox.robin.anteater.Anteater',
-    'ephox.robin.anteater.EntryPoints',
-    'ephox.scullion.ADT'
+    'ephox.robin.anteater.EntryPoints'
   ],
 
-  function (Fun, Option, Gather, Split, Anteater, EntryPoints, ADT) {
-    var adt = ADT.generate([
-      { left: [ 'element' ] },
-      { between: [ 'before', 'after' ] },
-      { right: [ 'element' ] },
-    ]);
-
-    var onText = function (universe, element, offset) {
-      var raw = Split.split(universe, element, offset);
-      var positions = Split.position(universe, raw);
-      return positions.fold(function () {
-        return adt.left(element);
-      }, function (e) {
-        return adt.right(element);
-      }, function (b, a) {
-        return adt.between(b, a);
-      }, function (e) {
-        return adt.left(e);
-      });
-    };
-
-    var onElement = function (universe, element, offset) {
-      var children = universe.property().children(element);
-      if (offset === 0) return adt.right(element);
-      else if (offset === children.length) return adt.left(element);
-      else if (offset > 0 && offset < children.length) return adt.between(children[offset - 1], children[offset]);
-      else return adt.left(element);
-    };
-
-    var analyse = function (universe, element, offset) {
-      var handler = universe.property().isText(element) ? onText : onElement;
-      return handler(universe, element, offset);
-    };
+  function (Option, Gather, Split, Anteater, EntryPoints) {
 
     var lake = function (universe, isRoot, element, soffset, foffset) {
       var middle = Split.splitByPair(universe, element, soffset, foffset);
@@ -55,19 +21,7 @@ define(
     var placid = function (universe, isRoot, start, soffset, finish, foffset) {
       if (universe.property().isText(start) && universe.eq(start, finish)) return lake(universe, isRoot, start, soffset, foffset);
       var leftSide = EntryPoints.toLeft(universe, isRoot, start, soffset);
-
-      var rightSide = analyse(universe, finish, foffset).fold(function (l) {
-        return l;
-      }, function (b, a) {
-        return b;
-      }, function (r) {
-        console.log('*************************', 'original.right: ', r.dom(), finish.dom(), foffset);
-        // really not sure what to do here.
-        return Gather.before(universe, r, isRoot).getOr(r);
-      });
-
-      console.log('leftSide: ', leftSide.dom(), 'rightSide: ', rightSide.dom());
-
+      var rightSide = EntryPoints.toRight(universe, isRoot, finish, foffset);
       return Anteater.fossil(universe, isRoot, leftSide, rightSide);
       // If we are starting on a text node, we need to split the text node first, and break from the split text node.
       // If we are starting on an element at the left edge, start from the first element if left or none if right.
