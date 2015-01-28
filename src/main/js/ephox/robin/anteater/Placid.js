a2c80336782243a9ef3150c2d611015392f44611
mps <morgan.smith@ephox.com>
2015-01-28 15:23:05 +1000

    TBIO-1766: rewriting Placid

--- src/main/js/ephox/robin/anteater/Placid.js
+++ src/main/js/ephox/robin/anteater/Placid.js
@@ -3,28 +3,32 @@ define(
 
   [
     'ephox.perhaps.Option',
-    'ephox.phoenix.api.general.Gather',
     'ephox.phoenix.api.general.Split',
     'ephox.robin.anteater.Anteater',
     'ephox.robin.anteater.EntryPoints'
   ],
 
-  function (Option, Gather, Split, Anteater, EntryPoints) {
-
-    var lake = function (universe, isRoot, element, soffset, foffset) {
+  function (Option, Split, Anteater, EntryPoints) {
+    var same = function (universe, element, soffset, foffset) {
       var middle = Split.splitByPair(universe, element, soffset, foffset);
       return Option.some([ middle ]);
     };
 
-
-    // TODO: Handle backwards selections !
-    var placid = function (universe, isRoot, start, soffset, finish, foffset) {
-      if (universe.property().isText(start) && universe.eq(start, finish)) return lake(universe, isRoot, start, soffset, foffset);
+    var diff = function (universe, isRoot, start, soffset, finish, foffset) {
       var leftSide = EntryPoints.toLeft(universe, isRoot, start, soffset);
       var rightSide = EntryPoints.toRight(universe, isRoot, finish, foffset);
       return Anteater.fossil(universe, isRoot, leftSide, rightSide);
-      // If we are starting on a text node, we need to split the text node first, and break from the split text node.
-      // If we are starting on an element at the left edge, start from the first element if left or none if right.
+    };
+
+    
+    var safeBias = function (universe, isRoot, start, soffset, finish, foffset) {
+
+    };
+
+    // TODO: Handle backwards selections !
+    var placid = function (universe, isRoot, start, soffset, finish, foffset) {
+      var sameText = universe.property().isText(start) && universe.eq(start, finish);
+      return sameText ? same(universe, start, soffset, foffset) : diff(universe, isRoot, start, soffset, finish, foffset);
     };
 
     return {
