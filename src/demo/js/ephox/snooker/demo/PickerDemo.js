954046e36eef9c409eb48a11ed31f193f0776297
mps <morgan.smith@ephox.com>
2015-02-10 14:30:14 +1000

    TBIO-889: keyboard shortcuts

--- src/demo/js/ephox/snooker/demo/PickerDemo.js
+++ src/demo/js/ephox/snooker/demo/PickerDemo.js
@@ -4,13 +4,17 @@ define(
   [
     'ephox.snooker.api.PickerDirection',
     'ephox.snooker.picker.PickerUi',
+    'ephox.sugar.api.Attr',
+    'ephox.sugar.api.DomEvent',
     'ephox.sugar.api.Element',
+    'ephox.sugar.api.Focus',
     'ephox.sugar.api.Insert',
     'ephox.sugar.api.Remove',
-    'global!Math'
+    'global!Math',
+    'global!document'
   ],
 
-  function (PickerDirection, PickerUi, Element, Insert, Remove, Math) {
+  function (PickerDirection, PickerUi, Attr, DomEvent, Element, Focus, Insert, Remove, Math, document) {
     return function () {
 
       var picker = PickerUi(PickerDirection.ltr, {
@@ -28,6 +32,40 @@ define(
       Insert.append(ephoxUi, wrap);
       Insert.append(wrap, picker.element());
 
+/*
+BACKSPACE : getConstant([8]),
+      TAB : getConstant([9]),
+      ENTER : getConstant([13]),
+      SHIFT : getConstant([16]),
+      CTRL : getConstant([17]),
+      ALT : getConstant([18]),
+      CAPSLOCK : getConstant([20]),
+      ESCAPE : getConstant([27]),
+      SPACE: getConstant([32]),
+      PAGEUP: getConstant([33]),
+      PAGEDOWN: getConstant([34]),
+      END: getConstant([35]),
+      HOME: getConstant([36]),
+      LEFT: getConstant([37]),
+      UP: getConstant([38]),
+      RIGHT: getConstant([39]),
+      DOWN: getConstant([40]),
+      INSERT: getConstant([45]),
+      DEL: getConstant([46]),
+      META: getConstant([91, 93, 224]),
+      F10: getConstant([121])
+      */
+
+      DomEvent.bind(ephoxUi, 'keydown', function (event) {
+        var key = event.raw().which;
+        if (key === 37) picker.sendLeft();
+        else if (key === 39) picker.sendRight();
+        else if (key === 40) picker.sendDown();
+        else if (key === 38) picker.sendUp();
+        else if (key === 32 || key === 13) picker.sendExecute();
+        event.kill();
+      });
+
 
       var val = 3;
 
@@ -41,7 +79,11 @@ define(
       });
 
 
+
       picker.on();
+
+      Attr.set(ephoxUi, 'tabIndex', '-1');
+      Focus.focus(ephoxUi);
     };
   }
 );
--- src/main/js/ephox/snooker/picker/PickerUi.js
+++ src/main/js/ephox/snooker/picker/PickerUi.js
@@ -19,10 +19,11 @@ define(
     'ephox.sugar.api.Element',
     'ephox.sugar.api.Insert',
     'ephox.sugar.api.InsertAll',
-    'ephox.sugar.api.Remove'
+    'ephox.sugar.api.Remove',
+    'global!parseInt'
   ],
 
-  function (Arr, Fun, Event, Events, Structs, PickerLookup, PickerStyles, Redimension, Styles, Util, Attr, Class, Classes, DomEvent, Element, Insert, InsertAll, Remove) {
+  function (Arr, Fun, Event, Events, Structs, PickerLookup, PickerStyles, Redimension, Styles, Util, Attr, Class, Classes, DomEvent, Element, Insert, InsertAll, Remove, parseInt) {
     return function (direction, settings) {
       var events = Events.create({
         select: Event(['rows', 'cols', 'rowHeaders', 'columnHeaders'])
@@ -98,15 +99,26 @@ define(
         Attr.set(table, 'data-picker-row', numRows - 1);
       };
 
+      var getSelection = function () {
+        var cols = parseInt(Attr.get(table, 'data-picker-col'), 10) + 1;
+        var rows = parseInt(Attr.get(table, 'data-picker-row'), 10) + 1;
+        return Structs.grid(rows, cols);
+      };
+
+      var sizeApi = {
+        element: Fun.constant(table),
+        setSelection: setSelection,
+        setSize: setSize
+      };
+
       var redimension = Redimension(direction, settings);
       var mover = DomEvent.bind(table, 'mousemove', function (event) {
-        var bridge = {
-          element: Fun.constant(table),
-          setSelection: setSelection,
-          setSize: setSize
-        };
-        redimension.handle(bridge, Structs.grid(size.height, size.width), event.raw().pageX, event.raw().pageY);
+        redimension.mousemove(sizeApi, Structs.grid(size.height, size.width), event.raw().pageX, event.raw().pageY);
       });
+
+      var resize = function (xDelta, yDelta) {
+        redimension.manual(sizeApi, getSelection(), xDelta, yDelta);
+      };
 
       var clicker = DomEvent.bind(table, 'click', function (event) {
         var result = PickerLookup.grid(table, 'data-picker-row', 'data-picker-col');
@@ -130,7 +142,12 @@ define(
         off: redimension.off,
         reset: reset,
         refresh: refresh,
-        events: events.registry
+        events: events.registry,
+
+        sendLeft: Fun.curry(resize, -1, 0),
+        sendRight: Fun.curry(resize, +1, 0),
+        sendUp: Fun.curry(resize, 0, -1),
+        sendDown: Fun.curry(resize, 0, +1)
       };
     };
   }
