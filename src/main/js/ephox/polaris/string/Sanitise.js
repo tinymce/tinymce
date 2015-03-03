8f61681d69c2b8b91ff95f54300f4b6fb96c579c
Andrew Herron <thespyder@programmer.net>
2015-03-03 12:22:03 +1000

    TBIO-2190: Simplified start of string test to avoid mutation. Added atomic tests.

--- src/main/js/ephox/polaris/string/Sanitise.js
+++ src/main/js/ephox/polaris/string/Sanitise.js
@@ -10,10 +10,8 @@ define(
      * Sanitises a string for use in a CSS class name
      */
     var css = function (str) {
-      var r = '';
-
       // special case; the first character must a letter. More strict than CSS, but easier to implement.
-      if (/[a-zA-Z]/.exec(str[0]) === null) r = 'e';
+      var r = /^[a-zA-Z]/.test(str) ? '' : 'e';
 
       // any non-word character becomes a hyphen
       var sanitised = str.replace(/[^\w]/gi, '-');
