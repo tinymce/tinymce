b5b5aa719f9e28fa73af44179cf2c8648728a646
Andrew Herron <thespyder@programmer.net>
2015-03-13 16:30:43 +1000

    Update Demo Translations for echo change

--- src/demo/js/ephox/snooker/demo/DemoTranslations.js
+++ src/demo/js/ephox/snooker/demo/DemoTranslations.js
@@ -2,29 +2,13 @@ define(
   'ephox.snooker.demo.DemoTranslations',
 
   [
-    'global!Array'
+
   ],
 
-  function (Array) {
-    var keys = {
-      'table.picker.rows': '{0} high',
-      'table.picker.cols': '{0} wide'
-    };
-
-    return function (key) {
-      if (keys[key] === undefined) throw 'key ' + key + ' not found';
-      var r = keys[key];
-
-      if (arguments.length > 1) {
-        var parameters = Array.prototype.slice.call(arguments, 1);
-        return r.replace(/\{(\d+)\}/g, function (match, contents) {
-          var index = parseInt(contents, 10);
-          if (parameters[index] === undefined) throw 'No value for token: ' + match + ' in translation: ' + r;
-          return parameters[index];
-        });
-      } else {
-        return r;
-      }
+  function () {
+    return {
+      row: function (row) { return row + ' high'; },
+      col: function (col) { return col + ' wide'; }
     };
   }
 );
