ce33c2158a4a166764435e422d3bf00c2cecd7b8
mps <morgan.smith@ephox.com>
2015-03-25 10:45:05 +1000

    TBIO-1368: adding smart quotes to word detection

--- src/main/js/ephox/polaris/pattern/Chars.js
+++ src/main/js/ephox/polaris/pattern/Chars.js
@@ -6,7 +6,13 @@ define(
   ],
 
   function (Fun) {
-    var chars = '\\w\'\\-\\u00C0-\\u00FF\\uFEFF';
+    // \w is a word character
+    // \' is an apostrophe
+    // '-' is a hyphen
+    // \u00C0 - \u00FF are various language characters
+    // \uFEFF is the unicode zero width cursor
+    // \u2018 and \u2019 are the smart quote characters
+    var chars = '\\w' + '\'' + '\\-' + '\\u00C0-\\u00FF' + '\\uFEFF' + '\\u2018\\u2019';
     var wordbreak = '[^' + chars + ']';
     var wordchar = '[' + chars + ']';
 
