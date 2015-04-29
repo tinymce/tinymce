8c38cae7c4d25ac1dfb2542be897a2d638774137
mps <morgan.smith@ephox.com>
2015-04-29 12:09:13 +1000

    TBIO-2087: very very basic keyboard selection for tables

--- src/demo/js/ephox/darwin/demo/DarwinTableDemo.js
+++ src/demo/js/ephox/darwin/demo/DarwinTableDemo.js
@@ -112,8 +112,18 @@ define(
               });
             }
           }, function (selected) {
+
+            var update = function (newSels) {
+              CellSelection.clear(ephoxUi);
+              CellSelection.select(newSels);
+            };
+
             // ignoring bias for the time being.
             if (event.raw().shiftKey && event.raw().which >= 37 && event.raw().which <= 40) {
+              if (event.raw().which === 39) CellSelection.shiftRight(selected).each(update);
+              else if (event.raw().which === 37) CellSelection.shiftLeft(selected).each(update);
+              else if (event.raw().which === 38) CellSelection.shiftUp(selected).each(update);
+              else if (event.raw().which === 40) CellSelection.shiftDown(selected).each(update);
               console.log('this is where you would handle expanding the table selection');
               event.kill();
             } else if (event.raw().shiftKey === false) {
--- src/main/js/ephox/darwin/mouse/CellSelection.js
+++ src/main/js/ephox/darwin/mouse/CellSelection.js
@@ -11,10 +11,11 @@ define(
     'ephox.sugar.api.Compare',
     'ephox.sugar.api.SelectorFilter',
     'ephox.sugar.api.SelectorFind',
+    'ephox.sugar.api.Traverse',
     'global!Math'
   ],
 
-  function (Arr, Styles, Fun, Option, DomParent, Class, Compare, SelectorFilter, SelectorFind, Math) {
+  function (Arr, Styles, Fun, Option, DomParent, Class, Compare, SelectorFilter, SelectorFind, Traverse, Math) {
     var selected = Styles.resolve('selected');
 
     var clear = function (container) {
@@ -39,8 +40,8 @@ define(
       return DomParent.sharedOne(lookupTable, [ start, finish ]).bind(function (tbl) {
         // For all the rows, identify the information.
         var rows = SelectorFilter.descendants(tbl, 'tr');
-        return findInTable(tbl, start).bind(function (startData) {
-          return findInTable(tbl, finish).map(function (finishData) {
+        return findInTable(start).bind(function (startData) {
+          return findInTable(finish).map(function (finishData) {
             var minRowIndex = Math.min(startData.rowIndex(), finishData.rowIndex());
             var maxRowIndex = Math.max(startData.rowIndex(), finishData.rowIndex());
             var subrows = rows.slice(minRowIndex, maxRowIndex + 1);
@@ -55,7 +56,7 @@ define(
       });
     };
 
-    var findInTable = function (container, cell) {
+    var findInTable = function (cell) {
       return findColumn(cell).bind(function (colIndex) {
         return findRow(cell).map(function (rowIndex) {
           return {
@@ -86,16 +87,87 @@ define(
       });
     };
 
+    var gotoCell = function (table, rowIndex, colIndex) {
+      var rows = SelectorFilter.descendants(table, 'tr');
+      return Option.from(rows[rowIndex]).bind(function (row) {
+        return Traverse.child(row, colIndex);
+      });
+    };
+
     var retrieve = function (container) {
       var sels = SelectorFilter.descendants(container, '.' + selected);
       return sels.length > 0 ? Option.some(sels) : Option.none();
     };
 
+    var shiftRight = function (boxes) {
+      // Assume that the last cell is the one to shift right.
+      return Option.from(boxes[boxes.length - 1]).bind(Traverse.nextSibling).bind(function (finish) {
+        return identify(boxes[0], finish);
+      });
+    };
+
+    var shiftLeft = function (boxes) {
+      // Assume that the last cell is the one to shift right.
+      return Option.from(boxes[boxes.length - 1]).bind(Traverse.prevSibling).bind(function (finish) {
+        return Option.some(finish);
+        return Compare.eq(finish, boxes[0]) ? Traverse.prevSibling(finish) : Option.some(finish);
+      }).bind(function (finish) {
+        return identify(boxes[0], finish);
+      });
+    };
+
+    var shiftUp = function (boxes) {
+      // Assume that the last cell is the one to shift right.
+      return Option.from(boxes[boxes.length - 1]).bind(findInTable).bind(function (position) {
+        console.log('shiftUp.position', position);
+        return SelectorFind.ancestor(boxes[0], 'table').bind(function (table) {
+          return gotoCell(table, position.rowIndex() - 1, position.colIndex()).bind(function (target) {
+            console.log('target', target.dom());
+            return Option.some(target);
+            return Compare.eq(target, boxes[0]) ? gotoCell(table, position.rowIndex() - 2, position.colIndex()) : Option.some(target);
+          });
+        });
+      }).bind(function (finish) {
+        console.log('shiftUp.finish: ', finish.dom());
+        return identify(boxes[0], finish);
+      });
+    };
+
+    var shiftDown = function (boxes) {
+      // Assume that the last cell is the one to shift right.
+      return Option.from(boxes[boxes.length - 1]).bind(findInTable).bind(function (position) {
+        console.log('shiftDown.position', position);
+        return SelectorFind.ancestor(boxes[0], 'table').bind(function (table) {
+          return gotoCell(table, position.rowIndex() + 1, position.colIndex()).bind(function (target) {
+            console.log('target', target.dom());
+            return Option.some(target);
+            return Compare.eq(target, boxes[0]) ? gotoCell(table, position.rowIndex() + 2, position.colIndex()) : Option.some(target);
+          });
+        });
+      }).bind(function (finish) {
+        console.log('shiftDown.finish: ', finish.dom());
+        return identify(boxes[0], finish);
+      });
+    };
+
+    // var shiftDown = function (boxes) {
+    //   // Assume that the last cell is the one to shift right.
+    //   return Option.from(boxes[boxes.length - 1]).bind(Traverse.prevSibling).bind(function (finish) {
+    //     return Compare.eq(finish, boxes[0]) ? Traverse.prevSibling(finish) : Option.some(finish);
+    //   }).bind(function (finish) {
+    //     return identify(boxes[0], finish);
+    //   });
+    // };
+
     return {
       clear: clear,
       select: select,
       identify: identify,
-      retrieve: retrieve
+      retrieve: retrieve,
+      shiftRight: shiftRight,
+      shiftLeft: shiftLeft,
+      shiftUp: shiftUp,
+      shiftDown: shiftDown
     };
   }
 );
