385672f0980ae53daab845c94720bc06fed59836
Maurizio Napoleoni <maurizio.napoleoni@ephox.com>
2015-05-26 14:59:31 +1000

    TBIO-405: Starting off

--- src/main/js/ephox/snooker/model/Divide.js
+++ src/main/js/ephox/snooker/model/Divide.js
@@ -18,5 +18,20 @@ define(
         [ 1, nu, 3 ]
       ]
      */
+
+    var generate = function (structure, element, comparator, substitution) {
+      for (var i=0; i<structure.length; i++) {
+        for (var j=0; j<structure[0].length; j++) {
+          var current = structure[i][j];
+          var isToReplace = comparator(current, element);
+          if (isToReplace)
+            structure[i][j] = substitution;
+        }
+      }
+    };
+
+    return {
+      generate: generate
+    };
   }
 );
