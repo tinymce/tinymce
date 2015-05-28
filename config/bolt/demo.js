b9f60f620f62a0d15a6ad0d83c22dafc3c8d1f5d
mps <morgan.smith@ephox.com>
2015-05-28 12:40:26 +1000

    TBIO-2621: cleanup

--- config/bolt/demo.js
+++ config/bolt/demo.js
@@ -1,12 +1,10 @@
 configure({
   configs: [
-    '../../lib/config/exhibition.js',
     './prod.js'
   ],
   sources: [
     source('amd', 'ephox.darwin.demo', '../../src/demo/js', mapper.hierarchical, { fresh: true }),
-    source('amd', 'ephox.darwin', '../../src/main/js', mapper.hierarchical, { fresh: true }),
-    source('amd', 'ephox.fussy', '../../../fussy/src/main/js', mapper.hierarchical, { fresh: true }),
+    source('amd', 'ephox.darwin', '../../src/main/js', mapper.hierarchical, { fresh: true })
   ]
 });
 
