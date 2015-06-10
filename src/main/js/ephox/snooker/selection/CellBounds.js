3f5863516b5b0de466e3c06fa123cab6f3b505de
mps <morgan.smith@ephox.com>
2015-06-10 10:35:09 +1000

    TBIO-405: spanning test

--- src/main/js/ephox/snooker/selection/CellBounds.js
+++ src/main/js/ephox/snooker/selection/CellBounds.js
@@ -8,7 +8,7 @@ define(
   ],
 
   function (Fun, Option, Warehouse) {
-    // Note, something is considered within the selection if it overlaps any part of the bounds. It does not have to completely within it.
+    // Note, something is considered within the selection if it overlaps any part of the bounds. It does not have to be completely within it.
     var inSelection = function (bounds, detail) {
       return (
         (detail.column() >= bounds.startCol() && detail.column()  <= bounds.finishCol()) ||
@@ -22,9 +22,9 @@ define(
     // Note, something is *within* if it is completely contained within the bounds.
     var isWithin = function (bounds, detail) {
       return (
-        (detail.column() >= bounds.startCol() && 
-        (detail.column() + detail.colspan() - 1) <= bounds.finishCol()) &&
-        (detail.row() >= bounds.startRow()) &&
+        detail.column() >= bounds.startCol() && 
+        (detail.column() + detail.colspan() - 1) <= bounds.finishCol() &&
+        detail.row() >= bounds.startRow() &&
         (detail.row() + detail.rowspan() - 1) <= bounds.finishRow()
       );
     };
@@ -35,7 +35,7 @@ define(
 
       
       for (var i = bounds.startRow(); i<=bounds.finishRow(); i++) {
-        for (var j = bounds.startCol(); j<=bounds.finishCol(); j++ ) {
+        for (var j = bounds.startCol(); j<=bounds.finishCol(); j++) {
           isRect = isRect && Warehouse.getAt(warehouse, i, j).exists(detailIsWithin);
         }
       }
--- src/test/js/atomic/rectangular/SpanningCellsTest.js
+++ src/test/js/atomic/rectangular/SpanningCellsTest.js
@@ -5,10 +5,10 @@ test(
     'ephox.scullion.Struct',
     'ephox.snooker.api.Structs',
     'ephox.snooker.model.Warehouse',
-    'ephox.snooker.selection.SpanningCells'
+    'ephox.snooker.selection.CellBounds'
   ],
 
-  function (Struct, Structs, Warehouse, SpanningCells) {
+  function (Struct, Structs, Warehouse, CellBounds) {
 
 
     var s = Structs.detail;  // 'element', 'rowspan', 'colspan'
@@ -23,47 +23,6 @@ test(
     ];
     var inputA = Warehouse.generate(testTableA);
 
-    // var bounds = Struct.immutableBag([ 'startRow', 'startCol', 'finishRow', 'finishCol'], []);
-    // var cell = Struct.immutableBag([ 'row', 'col'], [] );
-
-    var baseBound = Structs.bounds({
-      startRow: 1,
-      startCol: 1,
-      finishRow: 3,
-      finishCol: 3
-    });
-    var cellA = Structs.cell({
-      row: 2,
-      col: 2
-    });
-
-    var resultA = SpanningCells.isSpanning(inputA.access(), cellA, baseBound);
-    assert.eq(false, resultA);
-
-    var cellB = Structs.cell({
-      row: 3,
-      col: 3
-    });
-    var resultB = SpanningCells.isSpanning(inputA.access(), cellB, baseBound);
-    assert.eq(true, resultB);
-
-    var cellC = Structs.cell({
-      row: 0,
-      col: 0
-    });
-    var resultC = SpanningCells.isSpanning(inputA.access(), cellC, baseBound);
-    assert.eq(false, resultC);
-
-
-    var cellD = Structs.cell({
-      row: 3,
-      col: 1
-    });
-    var resultD = SpanningCells.isSpanning(inputA.access(), cellD, baseBound);
-    assert.eq(true, resultD);
-
-
-
     // 'element', 'rowspan', 'colspan'
     var testTableB = [
       f('r1', [ s('a',3,1), s('b',1,1), s('c',1,1), s('d',2,1) ]),
@@ -73,45 +32,29 @@ test(
     ];
     var inputB = Warehouse.generate(testTableB);
 
-    var baseBoundB = Structs.bounds({
-      startRow: 0,
-      startCol: 0,
-      finishRow: 2,
-      finishCol: 2
-    });
-    var cellE = Structs.cell({
-      row: 3,
-      col: 0
-    });
-    var resultE = SpanningCells.isSpanning(inputB.access(), cellE, baseBoundB);
-    assert.eq(false, resultE);
-
-    var cellF = Structs.cell({
-      row: 0,
-      col: 1
-    });
-    var resultF = SpanningCells.isSpanning(inputB.access(), cellF, baseBoundB);
-    assert.eq(true, resultF);
-
-    var cellG = Structs.cell({
-      row: 1,
-      col: 1
-    });
-    var resultG = SpanningCells.isSpanning(inputB.access(), cellG, baseBoundB);
-    assert.eq(true, resultG);
-
-    var cellH = Structs.cell({
+    var baseBound = Structs.bounds({ startRow: 1, startCol: 1, finishRow: 3, finishCol: 3 });
+    var cellA = Structs.cell({
       row: 2,
       col: 2
     });
-    var resultH = SpanningCells.isSpanning(inputB.access(), cellH, baseBoundB);
-    assert.eq(true, resultH);
 
-    var cellI = Structs.cell({
-      row: 1,
-      col: 3
-    });
-    var resultI = SpanningCells.isSpanning(inputB.access(), cellI, baseBoundB);
-    assert.eq(false, resultI);
+    var check = function (expected, warehouse, bounds, row, column) {
+      var cell = Warehouse.getAt(warehouse, row, column).getOrDie();
+      var actual = CellBounds.isWithin(bounds, cell);
+      assert.eq(expected, actual);
+    };
+
+    check(false, inputA, baseBound, 2, 2);
+    check(true, inputA, baseBound, 3, 3);
+    check(false, inputA, baseBound, 0, 0);
+    check(true, inputA, baseBound, 3, 1);
+
+    var baseBoundB = Structs.bounds({ startRow: 0, startCol: 0, finishRow: 2, finishCol: 2 });
+
+    check(false, inputB, baseBoundB, 3, 0);
+    check(true, inputB, baseBoundB, 0, 1);
+    check(true, inputB, baseBoundB, 1, 1);
+    check(true, inputB, baseBoundB, 2, 2);
+    check(false, inputB, baseBoundB, 1, 3);
   }
 );
