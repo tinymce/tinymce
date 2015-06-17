40c83483b20acb4bf4323a149cf490f42e9603c3
Mike Chau <mike.chau@ephox.com>
2015-06-17 10:52:47 +1000

    TBIO-2546: polyfill tables when inserting a larger table so that there are no jagged tables cells, we call this method tailer

--- src/main/js/ephox/snooker/model/Fitment.js
+++ src/main/js/ephox/snooker/model/Fitment.js
@@ -2,14 +2,16 @@ define(
   'ephox.snooker.model.Fitment',
 
   [
-    'ephox.peanut.Fun'
+    'ephox.compass.Arr',
+    'ephox.peanut.Fun',
+    'global!Math'
   ],
 
-  function (Fun) {
-    var measure = function (selection, gridA, gridB) {
-      if(selection.row() >= gridA.length || selection.column() >= gridA[0].length) throw 'invalid selection out of bounds, do we care about this?';
-      var rowRemainder = gridA.slice(selection.row());
-      var colRemainder = rowRemainder[0].slice(selection.column());
+  function (Arr, Fun, Math) {
+    var measure = function (startAddress, gridA, gridB) {
+      if(startAddress.row() >= gridA.length || startAddress.column() >= gridA[0].length) throw 'invalid startAddress out of bounds, do we care about this?';
+      var rowRemainder = gridA.slice(startAddress.row());
+      var colRemainder = rowRemainder[0].slice(startAddress.column());
 
       var rowRequired = gridB[0].length;
       var colRequired = gridB.length;
@@ -19,8 +21,41 @@ define(
       };
     };
 
+    var rowFill = function (grid, amount, generator) {
+      var nuRow = Arr.map(grid[0], function (cell) {
+        return '?';
+      });
+      return grid.concat([nuRow]);
+    };
+
+    var colFill = function (grid, amount, generator) {
+      return Arr.map(grid, function (rows) {
+        return rows.concat(['?']);
+      });
+    };
+
+    var abs = function (negInt) {
+      var posInt = Math.abs(negInt);
+      return posInt;
+    };
+
+    var patch = function (current, ideal, generator) {
+      var patchCols = ideal.colDelta() < 0 ? colFill : Fun.identity;
+      var patchRows = ideal.rowDelta() < 0 ? rowFill : Fun.identity;
+
+      var modifiedCols = patchCols(current, abs(ideal.colDelta()));
+      return patchRows(modifiedCols, abs(ideal.rowDelta()));
+    };
+
+    var tailor = function (startAddress, tableA, tableB, generator) {
+      var ideal = measure(startAddress, tableA, tableB);
+      var fitted = patch(tableA, ideal);
+      return fitted;
+    };
+
     return {
-      measure: measure
+      measure: measure,
+      tailor: tailor
     };
   }
 );
--- src/test/js/atomic/model/FitmentTest.js
+++ src/test/js/atomic/model/FitmentTest.js
@@ -8,9 +8,9 @@ test(
 
   function (Structs, Fitment) {
 
-    var sel = Structs.address;
-    var check = function (expected, selection, gridA, gridB) {
-      var tux = Fitment.measure(selection, gridA, gridB);
+    var start = Structs.address;
+    var measureTest = function (expected, startAddress, gridA, gridB) {
+      var tux = Fitment.measure(startAddress, gridA, gridB);
       assert.eq(expected.rowDelta, tux.rowDelta(), 'rowDelta expected: ' + expected.rowDelta + ' actual: '+ tux.rowDelta());
       assert.eq(expected.colDelta, tux.colDelta(), 'colDelta expected: ' + expected.colDelta + ' actual: '+ tux.colDelta());
     };
@@ -27,10 +27,46 @@ test(
       [3, 4]
     ];
 
-    check({ rowDelta: 1, colDelta: 1 }, sel(0, 0), gridA, gridB);    // col and row are + meaning gridB fits into gridA, given the starting selection point 'a'
-    check({ rowDelta: 0, colDelta: 0 }, sel(1, 1), gridA, gridB);    // col and row are > -1 meaning gridB fits into gridA, given the starting selection point 'e'
-    check({ rowDelta: -1, colDelta: -1 }, sel(2, 2), gridA, gridB);  // row is 1 too short col is 1 too short, given the starting selection point 'i'
-    check({ rowDelta: 1, colDelta: -1 }, sel(0, 2), gridA, gridB);   // col is 1 too short, given the starting selection point 'c' (need to add another column)
-    // check({ rowDelta: null, colDelta: null }, sel(3, 3), gridA, gridB); // out of bounds selection, do we care?
+    measureTest({ rowDelta: 1, colDelta: 1 }, start(0, 0), gridA, gridB);    // col and row are + meaning gridB fits into gridA, given the starting selection point 'a'
+    measureTest({ rowDelta: 0, colDelta: 0 }, start(1, 1), gridA, gridB);    // col and row are > -1 meaning gridB fits into gridA, given the starting selection point 'e'
+    measureTest({ rowDelta: -1, colDelta: -1 }, start(2, 2), gridA, gridB);  // row is 1 too short col is 1 too short, given the starting selection point 'i'
+    measureTest({ rowDelta: 1, colDelta: -1 }, start(0, 2), gridA, gridB);   // col is 1 too short, given the starting selection point 'c' (need to add another column)
+    // // check({ rowDelta: null, colDelta: null }, start(3, 3), gridA, gridB); // out of bounds startAddress, do we care?
+
+
+    var tailorTest = function (expected, startAddress, gridA, gridB) {
+      var tux = Fitment.tailor(startAddress, gridA, gridB);
+      assert.eq(expected, tux);
+    };
+
+
+    tailorTest(      [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f'],
+        ['g', 'h', 'i']
+      ], start(0, 0), gridA, gridB);
+
+    tailorTest(
+      [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f'],
+        ['g', 'h', 'i']
+      ], start(1, 1), gridA, gridB);
+
+    tailorTest(
+      [
+        ['a', 'b', 'c', '?'],
+        ['d', 'e', 'f', '?'],
+        ['g', 'h', 'i', '?'],
+        ['?', '?', '?', '?']
+      ], start(2, 2), gridA, gridB);
+
+    tailorTest([
+        ['a', 'b', 'c', '?'],
+        ['d', 'e', 'f', '?'],
+        ['g', 'h', 'i', '?']
+      ], start(0, 2), gridA, gridB);
+
+
   }
 );
