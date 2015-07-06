f5be8394626dd2f2944e9c439d0a798f24fec06c
Maurizio Napoleoni <maurizio.napoleoni@ephox.com>
2015-07-06 15:41:44 +1000

    TBIO-2529: Bar positions

--- src/main/js/ephox/snooker/resize/BarPositions.js
+++ src/main/js/ephox/snooker/resize/BarPositions.js
@@ -14,40 +14,6 @@ define(
     var rowInfo = Struct.immutable('row', 'y');
     var colInfo = Struct.immutable('col', 'x');
 
-    var ltrPositions = function (cols, _table) {
-      if (cols.length === 0 ) return [];
-      var lines = Arr.map(cols.slice(1), function (cellOption, col) {
-        return cellOption.map(function (cell) {
-          var pos = Location.absolute(cell);
-          return colInfo(col, pos.left());
-        });
-      });
-
-      var lastLine = cols[cols.length - 1].map(function (lastCol) {
-        var lastX = Location.absolute(lastCol).left() + Width.getOuter(lastCol);
-        return colInfo(cols.length - 1, lastX);
-      });
-
-      return lines.concat([ lastLine ]);
-    };
-
-    var rtlPositions = function (cols, _table) {
-      if (cols.length === 0 ) return [];
-      var lines = Arr.map(cols.slice(1), function (cellOption, col) {
-        return cellOption.map(function (cell) {
-          var pos = Location.absolute(cell  );
-          return colInfo(col, pos.left() + Width.getOuter(cell));
-        });
-      });
-
-      var lastLine = cols[cols.length - 1].map(function (lastCol) {
-        var lastX = Location.absolute(lastCol).left();
-        return colInfo(cols.length - 1, lastX);
-      });
-
-      return lines.concat([ lastLine ]);
-    };
-
     var rtlEdge = function (cell) {
       var pos = Location.absolute(cell);
       return pos.left() + Width.getOuter(cell);
@@ -57,47 +23,61 @@ define(
       return Location.absolute(cell).left();
     };
 
+    var getLeftEdge = function (index, cell) {
+      return colInfo(index, ltrEdge(cell));
+    };
+
+    var getRightEdge = function (index, cell) {
+      return colInfo(index, rtlEdge(cell));
+    };
+
+    var getTop = function (cell) {
+      return Location.absolute(cell).top();
+    };
+
+    var getTopEdge = function (index, cell) {
+      return rowInfo(index, getTop(cell));
+    };
+
+    var getBottomEdge = function (index, cell) {
+      return rowInfo(index, getTop(cell) + Height.getOuter(cell));
+    };
+
+    var findPositions = function (getInnerEdge, getOuterEdge, array) {
+      if (array.length === 0 ) return [];
+      var lines = Arr.map(array.slice(1), function (cellOption, index) {
+        return cellOption.map(function (cell) {
+          return getInnerEdge(index, cell);
+        });
+      });
+
+      var lastLine = array[array.length - 1].map(function (cell) {
+        return getOuterEdge(array.length - 1, cell);
+      });
+
+      return lines.concat([ lastLine ]);
+    };
+
     var negate = function (step, _table) {
       return -step;
     };
 
-    var heightPositions = function (rows, _table) {
-      if (rows.length === 0 ) return [];
-      var lines = Arr.map(rows.slice(1), function (cellOption, row) {
-        return cellOption.map(function (cell) {
-          var pos = Location.absolute(cell);
-          return rowInfo(row, pos.top());
-        });
-      });
-
-      var lastLine = rows[rows.length - 1].map(function (lastRow) {
-        var lastY = Location.absolute(lastRow).top() + Height.getOuter(lastRow);
-        return rowInfo(rows.length - 1, lastY);
-      });
-
-      return lines.concat( [ lastLine ] );
-    };
-
-    var heightEdge = function (cell) {
-      return Location.absolute(cell).top();
-    };
-
     var height = {
       delta: Fun.identity,
-      positions: heightPositions,
-      edge: heightEdge
+      positions: Fun.curry(findPositions, getTopEdge, getBottomEdge),
+      edge: getTop
     };
 
     var ltr = {
       delta: Fun.identity,
       edge: ltrEdge,
-      positions: ltrPositions
+      positions: Fun.curry(findPositions, getLeftEdge, getRightEdge)
     };
 
     var rtl = {
       delta: negate,
       edge: rtlEdge,
-      positions: rtlPositions
+      positions: Fun.curry(findPositions, getRightEdge, getLeftEdge)
     };
 
     return {
