dce38191d0d0c9c304665ba37a3f5afe95c373d9
Andrew Herron <thespyder@programmer.net>
2015-08-21 17:11:09 +0100

    TBIO-3090: Don't refocus if the active element hasn't changed

--- src/main/js/ephox/echo/api/AriaFocus.js
+++ src/main/js/ephox/echo/api/AriaFocus.js
@@ -2,27 +2,27 @@ define(
   'ephox.echo.api.AriaFocus',
 
   [
-    'ephox.perhaps.Option',
     'ephox.sugar.api.Compare',
     'ephox.sugar.api.Focus',
-    'ephox.sugar.api.PredicateFind',
     'ephox.sugar.api.Traverse'
   ],
 
-  function (Option, Compare, Focus, PredicateFind, Traverse) {
+  function (Compare, Focus, Traverse) {
     var preserve = function (f, container) {
       var ownerDoc = Traverse.owner(container);
 
       // If there is a focussed element, the F function may cause focus to be lost (such as by hiding elements). Restore it afterwards.
-      var refocuser = Focus.active(ownerDoc).bind(function (focused) {
-        var hasFocus = function (elem) {
-          return Compare.eq(focused, elem);
-        };
-        return hasFocus(container) ? Option.some(container) : PredicateFind.descendant(container, hasFocus);
-      });
+      var refocus = Focus.active(ownerDoc);
 
       var result = f(container);
-      refocuser.each(Focus.focus);
+      refocus.each(function (oldFocus) {
+        Focus.active(ownerDoc).filter(function (newFocus) {
+          return Compare.eq(newFocus, oldFocus);
+        }).orThunk(function () {
+          // Only refocus if the focus has changed, otherwise we break IE
+          Focus.focus(oldFocus);
+        });
+      });
       return result;
     };
 
