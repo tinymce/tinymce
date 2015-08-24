5d47be969831822805a5baab014845103ce4ad89
Andrew Herron <thespyder@programmer.net>
2015-08-24 14:18:24 +1000

    Restore refocus detection logic

--- src/main/js/ephox/echo/api/AriaFocus.js
+++ src/main/js/ephox/echo/api/AriaFocus.js
@@ -2,19 +2,27 @@ define(
   'ephox.echo.api.AriaFocus',
 
   [
+    'ephox.perhaps.Option',
     'ephox.sugar.api.Compare',
     'ephox.sugar.api.Focus',
+    'ephox.sugar.api.PredicateFind',
     'ephox.sugar.api.Traverse'
   ],
 
-  function (Compare, Focus, Traverse) {
+  function (Option, Compare, Focus, PredicateFind, Traverse) {
     var preserve = function (f, container) {
       var ownerDoc = Traverse.owner(container);
 
-      // If there is a focussed element, the F function may cause focus to be lost (such as by hiding elements). Restore it afterwards.
-      var refocus = Focus.active(ownerDoc);
+      var refocus = Focus.active(ownerDoc).bind(function (focused) {
+        var hasFocus = function (elem) {
+          return Compare.eq(focused, elem);
+        };
+        return hasFocus(container) ? Option.some(container) : PredicateFind.descendant(container, hasFocus);
+      });
 
       var result = f(container);
+
+      // If there is a focussed element, the F function may cause focus to be lost (such as by hiding elements). Restore it afterwards.
       refocus.each(function (oldFocus) {
         Focus.active(ownerDoc).filter(function (newFocus) {
           return Compare.eq(newFocus, oldFocus);
