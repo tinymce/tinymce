bc24fbd96b783c2295f12e0edabc45e46252b2ab
Spocke <spocke@moxiecode.com>
2015-08-24 17:00:08 +0200

    Fixed bug where the uploadImages call didn't work on IE 10.

--- js/tinymce/classes/file/Uploader.js
+++ js/tinymce/classes/file/Uploader.js
@@ -69,8 +69,8 @@ define("tinymce/file/Uploader", [
 			var xhr, formData;
 
 			xhr = new XMLHttpRequest();
-			xhr.withCredentials = settings.credentials;
 			xhr.open('POST', settings.url);
+			xhr.withCredentials = settings.credentials;
 
 			xhr.onload = function() {
 				var json;
