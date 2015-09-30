6f363ec1e65070ad72b40f2ad74d1db3573b4790
Spocke <spocke@moxiecode.com>
2015-09-30 17:11:59 +0200

    Fixed bug where images within contentEditable=false would be selected in WebKit on mouse click.

--- js/tinymce/classes/util/Quirks.js
+++ js/tinymce/classes/util/Quirks.js
@@ -937,7 +937,7 @@ define("tinymce/util/Quirks", [
 				// Workaround for bug, http://bugs.webkit.org/show_bug.cgi?id=12250
 				// WebKit can't even do simple things like selecting an image
 				// Needs to be the setBaseAndExtend or it will fail to select floated images
-				if (/^(IMG|HR)$/.test(target.nodeName)) {
+				if (/^(IMG|HR)$/.test(target.nodeName) && dom.getContentEditableParent(target) !== "false") {
 					e.preventDefault();
 					selection.getSel().setBaseAndExtent(target, 0, target, 1);
 					editor.nodeChanged();
