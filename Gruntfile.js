6bbe3dd458a4e4f0c683cf6ef5fd49300e3f8d3c
Spocke <spocke@moxiecode.com>
2015-10-02 11:33:00 +0200

    Added codesample plugin and updated less to latest version.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,9 @@ module.exports = function(grunt) {
 				"!js/tinymce/plugins/paste/plugin.js",
 				"!js/tinymce/plugins/table/plugin.js",
 				"!js/tinymce/plugins/spellchecker/plugin.js",
-				"!js/tinymce/plugins/imagetools/plugin.js"
+				"!js/tinymce/plugins/imagetools/plugin.js",
+				"!js/tinymce/plugins/codesample/plugin.js",
+				"!js/tinymce/plugins/codesample/classes/Prism.js"
 			],
 
 			themes: ["js/tinymce/themes/*/theme.js"]
@@ -41,7 +43,9 @@ module.exports = function(grunt) {
 				"!js/tinymce/plugins/paste/plugin.js",
 				"!js/tinymce/plugins/table/plugin.js",
 				"!js/tinymce/plugins/spellchecker/plugin.js",
-				"!js/tinymce/plugins/imagetools/plugin.js"
+				"!js/tinymce/plugins/imagetools/plugin.js",
+				"!js/tinymce/plugins/codesample/plugin.js",
+				"!js/tinymce/plugins/codesample/classes/Prism.js"
 			],
 
 			themes: ["js/tinymce/themes/*/theme.js"]
@@ -175,6 +179,21 @@ module.exports = function(grunt) {
 
 					from: "Plugin.js"
 				}
+			},
+
+			"codesample-plugin": {
+				options: {
+					baseDir: "js/tinymce/plugins/codesample/classes",
+					rootNS: "tinymce.codesampleplugin",
+					outputSource: "js/tinymce/plugins/codesample/plugin.js",
+					outputMinified: "js/tinymce/plugins/codesample/plugin.min.js",
+					outputDev: "js/tinymce/plugins/codesample/plugin.dev.js",
+					verbose: false,
+					expose: "public",
+					compress: true,
+
+					from: "Plugin.js"
+				}
 			}
 		},
 
@@ -222,7 +241,8 @@ module.exports = function(grunt) {
 			modern: {
 				options: {
 					cleancss: true,
-					strictImports: true
+					strictImports: true,
+					compress: true
 				},
 
 				expand: true,
@@ -245,7 +265,8 @@ module.exports = function(grunt) {
 			content: {
 				options: {
 					cleancss: true,
-					strictImports: true
+					strictImports: true,
+					compress: true
 				},
 
 				rename: function(dest, src) {
@@ -683,7 +704,7 @@ module.exports = function(grunt) {
 			plugins: {
 				files: ["js/tinymce/plugins/**/*.js"],
 				tasks: [
-					"amdlc:paste-plugin", "amdlc:imagetools-plugin",
+					"amdlc:paste-plugin", "amdlc:imagetools-plugin", "amdlc:codesample-plugin",
 					"amdlc:table-plugin", "amdlc:spellchecker-plugin", "uglify:plugins",
 					"eslint:plugins", "jscs:plugins"
 				],
