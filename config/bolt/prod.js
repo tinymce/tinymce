4854525ef1d6ddc5a7bb19ae72beed781d4db864
Andrew Herron <thespyder@programmer.net>
2015-11-19 14:15:35 +1000

    Moved fallback source to demo

--- config/bolt/prod.js
+++ config/bolt/prod.js
@@ -1,6 +1,5 @@
 configure({
   sources: [
-    source('amd', 'ephox/imagetools', '../../src/main/js', mapper.hierarchical),
-    source('amd', 'ephox', '../../lib/run/depend', mapper.flat)
+    source('amd', 'ephox/imagetools', '../../src/main/js', mapper.hierarchical)
   ]
 });
