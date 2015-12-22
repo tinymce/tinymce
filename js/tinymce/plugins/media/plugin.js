937d008be82b50b96f063fca7c9eba274606f229
1099511627776 <1099511627776@mail.ru>
2015-12-22 14:45:41 +0200

    * Fixed lint errors

--- js/tinymce/plugins/media/plugin.js
+++ js/tinymce/plugins/media/plugin.js
@@ -249,8 +249,8 @@ tinymce.PluginManager.add('media', function(editor, url) {
 		data.source2mime = guessMime(data.source2);
 		data.poster = editor.convertURL(data.poster, "poster");
 		data.flashPlayerUrl = editor.convertURL(url + '/moxieplayer.swf', "movie");
-		if(!!editor.settings.media_urlPatterns){
-			if(Tools.isArray(editor.settings.media_urlPatterns)){
+		if (editor.settings.media_urlPatterns) {
+			if (tinymce.isArray(editor.settings.media_urlPatterns)) {
 				urlPatterns = urlPatterns.concat(editor.settings.media_urlPatterns);
 			}
 		}
