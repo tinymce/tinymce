02dfab98310c0b0a2c93660ba661e3b4962355f1
James Tarkenton <jtark@users.noreply.github.com>
2016-05-13 17:31:45 -0400

    Fix for multibyte numeric entity encoding
    Fixes an issue where multibyte sequences would not be properly encoded as numeric entities if the entity_encoding setting was set to 'named+numeric'.

--- js/tinymce/classes/html/Entities.js
+++ js/tinymce/classes/html/Entities.js
@@ -198,7 +198,9 @@ define("tinymce/html/Entities", [
 
 			function encodeNamedAndNumeric(text, attr) {
 				return text.replace(attr ? attrsCharsRegExp : textCharsRegExp, function(chr) {
-					return baseEntities[chr] || entities[chr] || '&#' + chr.charCodeAt(0) + ';' || chr;
+					return baseEntities[chr] || entities[chr] || (chr.length > 1
+				        ? '&#' + (((chr.charCodeAt(0) - 0xD800) * 0x400) + (chr.charCodeAt(1) - 0xDC00) + 0x10000) + ';'
+				        : '&#' + chr.charCodeAt(0) + ';');
 				});
 			}
 
