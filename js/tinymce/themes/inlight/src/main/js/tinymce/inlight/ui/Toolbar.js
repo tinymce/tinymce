08879fcbdce4651f0d93a49a3cbea9bd42264e45
Spocke <spocke@moxiecode.com>
2016-06-10 11:48:40 +0200

    split bindSelectorChanged

--- js/tinymce/themes/inlight/src/main/js/tinymce/inlight/ui/Toolbar.js
+++ js/tinymce/themes/inlight/src/main/js/tinymce/inlight/ui/Toolbar.js
@@ -27,28 +27,46 @@ define('tinymce/inlight/ui/Toolbar', [
 		};
 	};
 
+	var getSelectorStateResult = function (itemName, item) {
+		var result = function (selector, handler) {
+			return {
+				selector: selector,
+				handler: handler
+			};
+		};
+
+		var activeHandler = function(state) {
+			item.active(state);
+		};
+
+		var disabledHandler = function (state) {
+			item.disabled(state);
+		};
+
+		if (itemName == 'bullist') {
+			return result('ul > li', setActiveItem(item, 'UL'));
+		}
+
+		if (itemName == 'numlist') {
+			return result('ol > li', setActiveItem(item, 'OL'));
+		}
+
+		if (item.settings.stateSelector) {
+			return result(item.settings.stateSelector, activeHandler);
+		}
+
+		if (item.settings.disabledStateSelector) {
+			return result(item.settings.disabledStateSelector, disabledHandler);
+		}
+
+		return null;
+	};
+
 	var bindSelectorChanged = function (editor, itemName, item) {
 		return function () {
-			var selection = editor.selection;
-
-			if (itemName == 'bullist') {
-				selection.selectorChanged('ul > li', setActiveItem(item, 'UL'));
-			}
-
-			if (itemName == 'numlist') {
-				selection.selectorChanged('ol > li', setActiveItem(item, 'OL'));
-			}
-
-			if (item.settings.stateSelector) {
-				selection.selectorChanged(item.settings.stateSelector, function(state) {
-					item.active(state);
-				}, true);
-			}
-
-			if (item.settings.disabledStateSelector) {
-				selection.selectorChanged(item.settings.disabledStateSelector, function(state) {
-					item.disabled(state);
-				});
+			var result = getSelectorStateResult(itemName, item);
+			if (result !== null) {
+				editor.selection.selectorChanged(result.selector, result.handler);
 			}
 		};
 	};
