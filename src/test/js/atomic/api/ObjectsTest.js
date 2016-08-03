a693fe632fdf816fc4ffccc21c34d300b8b52539
morgan <morgan.smith@ephox.com>
2016-08-03 16:48:30 +1000

    VAN-10: spec test for narrow

---
+++ src/test/js/atomic/api/ObjectsTest.js
@@ -0,0 +1,67 @@
+test(
+  'ObjectsTest',
+
+  [
+    'ephox.boulder.api.Objects',
+    'ephox.compass.Arr',
+    'ephox.compass.Obj',
+    'ephox.wrap.Jsc'
+  ],
+
+  function (Objects, Arr, Obj, Jsc) {
+    // TODO: Add more tests.
+    // Jsc.check(
+    //       Jsc.forall.apply(Jsc, args),
+    //       options
+    //     ).then(function (result) {
+    //       return result !== true ? die(formatErr(name, result)) : next();
+    //     }, function (err) {
+    //       die(err);
+    //     });
+
+
+    // var narrowGen = Jsc.tuple(].generator.flatMap(function ()))
+
+    var smallSet = Jsc.nestring;
+
+    var check = function (arb, checker) {
+      Jsc.check(
+        Jsc.forall(arb, function (input) {
+          checker(input);
+          return true;
+        })
+      );
+    };
+
+    var narrowGen = Jsc.bless({
+      generator: Jsc.dict(smallSet).generator.flatMap(function (obj) {
+        var keys = Obj.keys(obj);
+        console.log('keys', keys);
+        return keys.length === 0 ? Jsc.constant( { obj: obj, fields: [ ] }).generator : Jsc.array(Jsc.elements(keys)).generator.map(function (fields) {
+          console.log('here', fields);
+          return {
+            obj: obj,
+            fields: fields
+          };
+        });
+      })
+    });
+
+    check(narrowGen, function (input) {
+      var narrowed = Objects.narrow(input.obj, input.fields);
+      console.log('obj.narrow', narrowed);
+
+      Obj.each(narrowed, function (_, k) {        
+        if (!Arr.contains(input.fields, k)) throw 'Narrowed object contained property: ' + k + ' which was not in fields: [' + input.fields.join(', ') + ']';
+      });
+      return true;
+    });
+
+    (function () {
+      var actual = Objects.narrow({ a: 'a', b: 'b', c: 'c' }, [ 'a', 'c', 'e' ]);
+      assert.eq({ a: 'a', c: 'c' }, actual);
+    })();
+
+
+  }
+);
