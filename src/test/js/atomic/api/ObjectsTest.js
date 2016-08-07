e25fa2da2def78273aba317f9c49bfc1e36324cd
morgan <morgan.smith@ephox.com>
2016-08-08 08:42:15 +1000

    VAN-10: property-based test for consolidate

--- src/test/js/atomic/api/ObjectsTest.js
+++ src/test/js/atomic/api/ObjectsTest.js
@@ -5,10 +5,12 @@ test(
     'ephox.boulder.api.Objects',
     'ephox.compass.Arr',
     'ephox.compass.Obj',
+    'ephox.numerosity.api.JSON',
+    'ephox.perhaps.Result',
     'ephox.wrap.Jsc'
   ],
 
-  function (Objects, Arr, Obj, Jsc) {
+  function (Objects, Arr, Obj, Json, Result, Jsc) {
     var smallSet = Jsc.nestring;
 
     var check = function (arb, checker) {
@@ -59,8 +61,139 @@ test(
       assert.eq('Alpha', Objects.readOptFrom(subject, 'alpha').getOrDie('readOptFrom(alpha) => some(Alpha)'), 'readOptFrom(alpha) => some(Alpha)');
       assert.eq(true, Objects.readOptFrom(subject, 'beta').isNone(), 'readOptFrom(beta) => none');
     };
+
+    var testConsolidate = function () {
+      var checkErr = function (label, expected, objs, base) {
+        Objects.consolidate(objs, base).fold(function (err) {
+          assert.eq(expected, err);
+        }, function (val) {
+          assert.fail(label + '\nExpected error(' + Json.stringify(expected) + '), but was: ' + Json.stringify(val));
+        });
+      };
+
+      var checkVal = function (label, expected, objs, base) {
+        Objects.consolidate(objs, base).fold(function (err) {
+          assert.fail(label + '\nExpected value(' + Json.stringify(expected) + '), but was error(' + Json.stringify(err) + ')');
+        }, function (val) {
+          assert.eq(expected, val);
+        });
+      };
+
+      checkVal(
+        'empty objects',
+        { },
+        [ ], {}
+      );
+
+      checkErr(
+        '[ failure ] ]',
+        [ 'failure.1' ],
+        [
+          Result.error([ 'failure.1' ])
+        ], {}
+      );
+
+      checkErr(
+        '[ failure {1,2,3} ]',
+        [ 'failure.1', 'failure.2', 'failure.3' ],
+        [
+          Result.error([ 'failure.1' ]),
+          Result.error([ 'failure.2' ]),
+          Result.error([ 'failure.3' ])
+        ], {}
+      );
+
+      checkVal(
+        '[ value.a ]',
+        { a: 'A' },
+        [
+          Result.value({ a: 'A' })
+        ], {}
+      );
+
+      checkVal(
+        '[ value.{a,b,c} ]',
+        { a: 'A' , b: 'B', c: 'C'},
+        [
+          Result.value({ a: 'A' }),
+          Result.value({ b: 'B' }),
+          Result.value({ c: 'C' })
+        ], {}
+      );
+
+      checkErr(
+        '[ value.a, failure.1, value.b ]',
+        [ 'failure.1' ],
+        [
+          Result.value({ a: 'A' }),
+          Result.error([ 'failure.1' ]),
+          Result.value({ b: 'B' })
+        ], {}
+      );
+
+      checkErr(
+        '[ value.a, failure.1, value.b, failure.2, failure.3 ]',
+        [ 'failure.1', 'failure.2', 'failure.3' ],
+        [
+          Result.value({ a: 'A' }),
+          Result.error([ 'failure.1' ]),
+          Result.value({ b: 'B' }),
+          Result.error([ 'failure.2' ]),
+          Result.error([ 'failure.3' ])
+        ], {}
+      );
+
+      checkErr(
+        '[ value.{a,b,c} ]',
+        [ 'failure.last' ],
+        [
+          Result.value({ a: 'A' }),
+          Result.value({ b: 'B' }),
+          Result.value({ c: 'C' }),
+          Result.error([ 'failure.last' ])
+        ], {}
+      );
+
+
+      // some property-based tests
+      var resultList = Jsc.bless({
+        generator: Jsc.dict(smallSet).generator.flatMap(function (obj) {
+          return Jsc.bool.generator.flatMap(function (flag) {
+            return flag ? Jsc.constant(Result.value(obj)).generator : (function () {
+              return Jsc.array(Jsc.nestring).generator.map(Result.error);
+            })();
+          });
+        })
+      });
+
+      var inputList = Jsc.bless({
+        generator: Jsc.array(resultList).generator.flatMap(function (results) {
+          return Jsc.dict(smallSet).generator.map(function (base) {
+            return {
+              results: results,
+              base: base
+            };
+          });
+        })
+      });
+
+      // HERE.
+      check(inputList, function (input) {
+        var actual = Objects.consolidate(input.results, input.base);
+
+        var hasError = Arr.exists(input.results, function (res) {
+          return res.isError();
+        });
+
+        if (hasError) assert.eq(true, actual.isError(), 'Error contained in list, so should be error overall');
+        else assert.eq(true, actual.isValue(), 'No errors in list, so should be value overall');
+      });
+    };
+
+
     
     testNarrow();
     testReaders();
+    testConsolidate();
   }
 );
