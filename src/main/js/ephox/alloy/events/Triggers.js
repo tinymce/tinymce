e4bc50ecc87704ff635eb9b44c4667856277207c
morgan <morgan.smith@ephox.com>
2016-08-08 20:40:42 +1000

    VAN-1: basic triggering

---
+++ src/main/js/ephox/alloy/events/Triggers.js
@@ -0,0 +1,54 @@
+define(
+  'ephox.alloy.events.Triggers',
+
+  [
+    'ephox.peanut.Fun',
+    'ephox.scullion.ADT',
+    'ephox.scullion.Cell',
+    'ephox.sugar.api.Traverse'
+  ],
+
+  function (Fun, Adt, Cell, Traverse) {
+    var adt = Adt.generate([
+      { stopped: [ ] },
+      { resume: [ 'element' ] },
+      { complete: [ ] }
+    ]);
+
+    var triggerHandler = function (lookup, eventType, rawEvent, target) {
+      var handler = lookup(eventType, target);
+
+      var stopper = Cell(false);
+
+      var stop = function () {
+        stopper.set(true);
+      };
+
+      var labEvent = {
+        stop: stop,
+        event: Fun.constant(rawEvent)
+      };
+
+      return handler.fold(function () {
+        // No handler, so complete.
+        return adt.complete();
+      }, function (handlerInfo) {
+        handlerInfo.handler(labEvent);
+
+        // Now, check if the event was stopped.
+        if (stopper.get() === true) return adt.stopped();
+        else return Traverse.parent(handlerInfo.element).fold(function () {
+          // No parent, so complete.
+          return adt.complete();
+        }, function (parent) {
+          // Resume at parent
+          return adt.resume(parent);
+        });
+      });
+    };
+
+    return {
+      triggerHandler: triggerHandler
+    };
+  }
+);
