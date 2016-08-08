7144d8089c5e666ceb3ac48be0e6b6f08b58f53d
morgan <morgan.smith@ephox.com>
2016-08-09 07:59:29 +1000

    VAN-1: comment explaining focus

--- src/main/js/ephox/alloy/api/SystemEvents.js
+++ src/main/js/ephox/alloy/api/SystemEvents.js
@@ -7,6 +7,9 @@ define(
 
   function (Fun) {
     return {
+      // This is used to pass focus to a component. A component might interpret
+      // this event and pass the DOM focus to one of its children, depending on its
+      // focus model.
       focus: Fun.constant('alloy.focus')
     };
   }
