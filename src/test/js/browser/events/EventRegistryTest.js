4b518ba541392f25f3d8d47011ff8605f21e9467
morgan <morgan.smith@ephox.com>
2016-08-09 09:28:42 +1000

    VAN-1: basic filterByType

--- src/test/js/browser/events/EventRegistryTest.js
+++ src/test/js/browser/events/EventRegistryTest.js
@@ -6,14 +6,16 @@ asynctest(
     'ephox.agar.api.RawAssertions',
     'ephox.agar.api.Step',
     'ephox.alloy.events.EventRegistry',
+    'ephox.compass.Arr',
     'ephox.numerosity.api.JSON',
+    'ephox.peanut.Fun',
     'ephox.sugar.api.Element',
     'ephox.sugar.api.Html',
     'ephox.sugar.api.Insert',
     'global!document'
   ],
  
-  function (Pipeline, RawAssertions, Step, EventRegistry, Json, Element, Html, Insert, document) {
+  function (Pipeline, RawAssertions, Step, EventRegistry, Arr, Json, Fun, Element, Html, Insert, document) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
@@ -38,18 +40,30 @@ asynctest(
     var events = EventRegistry();
 
     events.registerId([ 'extraArgs' ], 'comp-1', {
-      'event.alpha': 'handler.alpha'
+      'event.alpha': function (extra) {
+        return 'f(' + extra + ')';
+      }
     });
  
     var sAssertFilterByType = function (expected, type) {
       return Step.sync(function () {
         var filtered = events.filterByType(type);
-        RawAssertions.assertEq('filter(' + type + ') = ' + Json.stringify(expected), expected, filtered);
+        var raw = Arr.map(filtered, function (f) {
+          console.log('f', f);
+          return { handler: f.handler(), id: f.id() };
+        });
+
+        RawAssertions.assertEq('filter(' + type + ') = ' + Json.stringify(expected), expected, raw);
       });
     };
 
     Pipeline.async({}, [
-      sAssertFilterByType([ ], 'event.none')
+      sAssertFilterByType([ ], 'event.none'),
+      sAssertFilterByType([
+        { handler: 'f(extraArgs)', id: 'comp-1' }
+      ], 'event.alpha' ),
+
+      
 
     ], function () { success(); }, failure);
 
