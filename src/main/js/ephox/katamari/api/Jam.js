b6ac18443308e4901ccf9b00e447e991e3b61d0f
morgan <morgan.smith@ephox.com>
2016-08-22 12:32:29 +1000

    VAN-21: removing tabulate and untabulate

--- src/main/js/ephox/katamari/api/Jam.js
+++ src/main/js/ephox/katamari/api/Jam.js
@@ -29,37 +29,9 @@ define(
       return intersperseThunk(array, thunk);
     };
 
-    var tabulate = function(array) {
-      var ret = {};
-      for (var i = 0; i < array.length; i++) {
-        var curObject = array[i];
-        for (var key in curObject) if (Object.prototype.hasOwnProperty.call(curObject, key)) {
-          var value = curObject[key];
-          if (!Object.prototype.hasOwnProperty.call(ret, key)) ret[key] = [];
-          ret[key][i] = value;
-        }
-      }
-      return ret;
-    };
-
-    var untabulate = function(object) {
-      var ret = [];
-      for (var key in object) if (Object.prototype.hasOwnProperty.call(object, key)) {
-        var curArray = object[key];
-        for (var i = 0; i < curArray.length; i++) {
-            var value = curArray[i];
-            ret[i] = ret[i] || {};
-            ret[i][key] = value;
-        }
-      }
-      return ret;
-    };
-
     return {
       intersperse: intersperse,
-      intersperseThunk: intersperseThunk,
-      untabulate: untabulate,
-      tabulate: tabulate
+      intersperseThunk: intersperseThunk
     };
   }
 );
