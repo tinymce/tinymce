f6522fafa7d389f115d1d6e51cef86dc65810fd9
morgan <morgan.smith@ephox.com>
2016-08-23 11:52:30 +1000

    VAN-25: added basic test for every method

--- src/test/js/atomic/api/data/ResultTest.js
+++ src/test/js/atomic/api/data/ResultTest.js
@@ -2,10 +2,11 @@ test(
   'ValueTest',
 
   [
+    'ephox.katamari.api.Fun',
     'ephox.katamari.api.Result'
   ],
 
-  function (Result) {
+  function (Fun, Result) {
 
     var s = Result.value(5);
     assert.eq(true, s.isValue());
@@ -40,12 +41,14 @@ test(
   'ErrorTest',
 
   [
-    'ephox.katamari.api.Result'
+    'ephox.katamari.api.Result',
+    'ephox.katamari.api.Fun'
   ],
 
-  function (Result) {
+  function (Result, Fun) {
 
     var s = Result.error('error');
+    assert.eq(false, s.is('error'));
     assert.eq(false, s.isValue());
     assert.eq(true, s.isError());
     assert.eq(6, s.getOr(6));
@@ -60,8 +63,8 @@ test(
       }).getOrDie();
     });
 
-    assert.eq('error', s.fold(function (e) {
-      return e;
+    assert.eq('error!', s.fold(function (e) {
+      return e + '!';
     }, function (v) {
       return v + 6;
     }));
@@ -78,6 +81,9 @@ test(
       }).getOrDie();
     });
 
+    assert.eq(false, s.exists(Fun.always));
+    assert.eq(true, s.forall(Fun.never));
+    
     assert.eq(true, Result.error(4).toOption().isNone());
   }
 );
