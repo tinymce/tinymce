7d539cb6285742731df8a7a094db70907a1500a5
morgan <morgan.smith@ephox.com>
2016-08-25 08:59:25 +1000

    VAN-26: Lazy values

--- src/test/js/atomic/api/async/LazyValueTest.js
+++ src/test/js/atomic/api/async/LazyValueTest.js
@@ -3,39 +3,44 @@ asynctest(
  
   [
     'ephox.katamari.api.LazyValue',
+    'ephox.katamari.test.AsyncProps',
     'global!Promise'
   ],
  
-  function (LazyValue, Promise) {
+  function (LazyValue, AsyncProps, Promise) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
  
     var testGet = function () {
       return new Promise(function (resolve, reject) {
-        var lazyval = LazyValue.nu(function () {
-
-         
-        })
+        resolve(true);
       });
     };
 
     var testMap = function () {
       return new Promise(function (resolve, reject) {
-
+        resolve(true);
       });
     };
 
     var testIsReady = function () {
       return new Promise(function (resolve, reject) {
-
+        resolve(true);
       });
     };
 
     var testPure = function () {
       return new Promise(function (resolve, reject) {
-
+        resolve(true);
       });
     };
 
+    var testSpecs = function () {
+      return AsyncProps.checkProps([ ]);
+    };
+
+    return testGet().then(testMap).then(testIsReady).then(testPure).then(testSpecs).then(function () {
+      success();
+    }, failure);
   }
 );
