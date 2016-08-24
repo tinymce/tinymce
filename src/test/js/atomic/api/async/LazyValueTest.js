737d646dc792d5e74148b87cc14c11b710aa6822
morgan <morgan.smith@ephox.com>
2016-08-25 09:08:30 +1000

    VAN-26: checking cached value

--- src/test/js/atomic/api/async/LazyValueTest.js
+++ src/test/js/atomic/api/async/LazyValueTest.js
@@ -4,16 +4,33 @@ asynctest(
   [
     'ephox.katamari.api.LazyValue',
     'ephox.katamari.test.AsyncProps',
+    'ephox.wrap.Jsc',
     'global!Promise'
   ],
  
-  function (LazyValue, AsyncProps, Promise) {
+  function (LazyValue, AsyncProps, Jsc, Promise) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
+
+    var lazyCounter = function () {
+      var counter = 0;
+      return LazyValue.nu(function (callback) {
+        counter++;
+        callback(counter);
+      });
+    };
  
     var testGet = function () {
       return new Promise(function (resolve, reject) {
-        resolve(true);
+        var lazy = lazyCounter();
+
+        lazy.get(function (val) {
+          if (! Jsc.eq(val, 1)) reject('The counter should be 1 after 1 call');
+          else lazy.get(function (val2) {
+            if (Jsc.eq(val2, 1)) resolve(true);
+            else reject('The counter should still be 1 because it is cached. Was: ' + val2);
+          });
+        });
       });
     };
 
