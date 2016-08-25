1ca1fff403bf4b0c67f1e4112479a63df9549c2a
morgan <morgan.smith@ephox.com>
2016-08-25 13:40:50 +1000

    VAN-25: sanity test

--- src/test/js/atomic/api/data/ResultsTest.js
+++ src/test/js/atomic/api/data/ResultsTest.js
@@ -2,10 +2,29 @@ test(
   'ResultsTest',
 
   [
-
+    'ephox.katamari.api.Result',
+    'ephox.katamari.api.Results'
   ],
 
-  function () {
-    assert.eq(1, 2);
+  function (Result, Results) {
+    var testPartition = function () {
+      var actual = Results.partition([
+        Result.value('a'),
+        Result.value('b'),
+        Result.error('e1'),
+        Result.error('e2'),
+        Result.value('c'),
+        Result.value('d')
+      ]);
+
+      assert.eq('a', actual.values[0]);
+      assert.eq('b', actual.values[1]);
+      assert.eq('c', actual.values[2]);
+      assert.eq('d', actual.values[3]);
+      assert.eq('e1', actual.errors[0]);
+      assert.eq('e2', actual.errors[1]);
+    };
+
+    testPartition();
   }
 );
