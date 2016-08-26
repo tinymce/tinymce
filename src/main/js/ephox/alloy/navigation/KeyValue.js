58e186980bade0462a7b503b17e8506490ae684a
morgan <morgan.smith@ephox.com>
2016-08-26 10:48:40 +1000

    VAN-42: KeyValue and KeyRules

---
+++ src/main/js/ephox/alloy/navigation/KeyValue.js
@@ -0,0 +1,40 @@
+define(
+  'ephox.alloy.navigation.KeyValue',
+
+  [
+    'ephox.compass.Arr'
+  ],
+
+  function (Arr) {
+    var inSet = function (keys) {
+      return function (event) {
+        return Arr.contains(keys, event.raw().which);
+      };
+    };
+
+    var and = function (preds) {
+      return function (event) {
+        return Arr.forall(preds, function (pred) {
+          return pred(event);
+        });
+      };
+    };
+
+    var is = function (key) {
+      return function (event) {
+        return event.raw().which === key;
+      };
+    };
+
+    var isShift = function (event) {
+      return event.raw().shiftKey === true;
+    };
+
+    return {
+      inSet: inSet,
+      and: and,
+      is: is,
+      isShift: isShift
+    };
+  }
+);
