1cb224be3524d5ef27932590082e311e8f8600c7
morgan <morgan.smith@ephox.com>
2016-08-29 12:32:48 +1000

    VAN-42: prop test for flat grid navigation

--- src/test/js/browser/behaviour/FlatgridKeyingTest.js
+++ src/test/js/browser/behaviour/FlatgridKeyingTest.js
@@ -22,7 +22,8 @@ asynctest(
       's01', 's02', 's03', 's04', 's05', 's06',
       's07', 's08', 's09', 's10', 's11', 's12',
       's13', 's14', 's15', 's16', 's17', 's18',
-      's19', 's20', 's21'
+      's19'
+     //  , 's20', 's21', 's22', 's23'
     ];
 
     GuiSetup.setup(function (store, doc, body) {
@@ -87,10 +88,13 @@ asynctest(
       console.log('targets', targets);
 
       return [
+     
         FocusTools.sSetFocus('Initial focus', gui.element(), '.s11'),
         Step.sync(function () {
           component.apis().setGridSize(4, 6);
         }),
+         function () { },
+      ];return[
         NavigationUtils.sequence(
           doc,
           Keys.down(),
