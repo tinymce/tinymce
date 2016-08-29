cc837d5327e463170eb4df446366978d526eb072
morgan <morgan.smith@ephox.com>
2016-08-29 18:16:01 +1000

    VAN-40: sanity tests

--- src/test/js/atomic/api/async/FutureResultTest.js
+++ src/test/js/atomic/api/async/FutureResultTest.js
@@ -2,11 +2,13 @@ asynctest(
   'FutureResultsTest',
 
   [
+    'ephox.katamari.api.Future',
     'ephox.katamari.api.FutureResult',
+    'ephox.katamari.api.Result',
     'global!Promise'
   ],
 
-  function (FutureResult, Promise) {
+  function (Future, FutureResult, Result, Promise) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
@@ -36,15 +38,50 @@ asynctest(
       });
     };
 
-    // return {
-    //   pure: pure,
-    //   error: error,
-    //   fromResult: fromResult,
-    //   fromFuture: fromFuture,
-    //   nu: nu
-    // };
+    var testFromResult = function () {
+      return new Promise(function (resolve, reject) {
+        FutureResult.fromResult(Result.error('future.from.result.error')).get(function (res) {
+          res.fold(function (err) {
+            assert.eq('future.from.result.error', err);
+            resolve(true);
+          }, function (val) {
+            reject('testFromResult:error: Unexpected success: ' + val);
+          });
+        });
+      });
+    };
 
-    testPure().then(testError).then(function () {
+    var testFromFuture = function () {
+      return new Promise(function (resolve, reject) {
+        FutureResult.fromFuture(Future.pure('future.from.future')).get(function (res) {
+          res.fold(function (err) {
+            reject('testFromFuture: Unexpected error: ' + err);
+          }, function (val) {
+            assert.eq('future.from.future', val);
+            resolve(true);
+          });
+        });
+      });
+    };
+
+    var testNu = function () {
+      return new Promise(function (resolve, reject) {
+        FutureResult.nu(function (callback) {
+          callback(
+            Result.value('future.nu')
+          );
+        }).get(function (res) {
+          res.fold(function (err) {
+            reject('testNu: Unexpected error: ' + err);
+          }, function (val) {
+            assert.eq('future.nu', val);
+            resolve(true);
+          });
+        });
+      });
+    };
+
+    testPure().then(testError).then(testFromResult).then(testFromFuture).then(testNu).then(function () {
       success();
     }, failure);
   }
