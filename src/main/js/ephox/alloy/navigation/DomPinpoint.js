1a4235aa8e7867ab4dac23ca265195ad3eae7f1b
morgan <morgan.smith@ephox.com>
2016-08-29 13:55:24 +1000

    VAN-42: removing commented code

--- src/main/js/ephox/alloy/navigation/DomPinpoint.js
+++ src/main/js/ephox/alloy/navigation/DomPinpoint.js
@@ -12,35 +12,6 @@ define(
   ],
 
   function (ArrPinpoint, Arr, Fun, Option, Compare, SelectorFilter, Visibility) {
-    // var indexInfo = Struct.immutable('index', 'candidates');
-
-    // Assumptions:
-    //   - the focused element is in the group.
-    //   - the focus options in the group are descendants which match the selectors.item() selector.
-    // var ofFocus = function (container, grouper, selector) {
-    //   return Focus.search(container).bind(function (focused) {
-    //     // Identify the group of the focused item.
-    //     return SelectorFind.closest(focused, grouper).bind(function (group) {
-    //       var anyItems = SelectorFilter.descendants(group, selector);
-    //       var items = Arr.filter(anyItems, Visibility.isVisible);
-    //       var index = Arr.findIndex(items, Fun.curry(Compare.eq, focused));
-    //       return index > -1 ? Option.some(itemInfo({ index: index, focused: focused, group: group, items: items })) : Option.none();
-    //     });
-    //   });
-    // };
-
-    // var ofSector = function (root, sectorer, group) {
-    //   var sectors = SelectorFilter.descendants(root, sectorer);
-    //   return SelectorFind.closest(group, sectorer).bind(function (sector) {
-    //     var index = Arr.findIndex(sectors, Fun.curry(Compare.eq, sector));
-    //     return index > -1 ? Option.some(sectorInfo({
-    //       index:  index,
-    //       sectors: sectors,
-    //       sector: sector
-    //     })): Option.none();
-    //   });
-    // };
-
     var locateVisible = function (container, current, selector) {
       var filter = Visibility.isVisible;
       return locateIn(container, current, selector, filter);
