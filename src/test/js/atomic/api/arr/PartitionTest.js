6017261382484e2d5c7310aac15190067c2efc73
morgan <morgan.smith@ephox.com>
2016-08-30 16:08:30 +1000

    VAN-29: Partition tests

--- src/test/js/atomic/api/arr/PartitionTest.js
+++ src/test/js/atomic/api/arr/PartitionTest.js
@@ -2,10 +2,12 @@ test(
   'Partition Test',
 
   [
-    'ephox.katamari.api.Arr'
+    'ephox.katamari.api.Arr',
+    'ephox.katamari.api.Fun',
+    'ephox.wrap.Jsc'
   ],
 
-  function (Arr) {
+  function (Arr, Fun, Jsc) {
 
     (function() {
 
@@ -27,6 +29,39 @@ test(
         ['no 1', 'no 2', 'yes 1', 'yes 2', 'yes 3', 'no 3', 'no 4', 'yes 4', 'no 5', 'yes 5'],
         {
           pass: ['yes 1', 'yes 2', 'yes 3', 'yes 4', 'yes 5'], fail: ['no 1', 'no 2', 'no 3', 'no 4', 'no 5']
+        }
+      );
+
+      Jsc.property(
+        'Check that if the filter always returns false, then everything is in "fail"',
+        Jsc.array(Jsc.json),
+        function (arr) {
+          var output = Arr.partition(arr, Fun.constant(false));
+          return Jsc.eq(0, output.pass.length) && Jsc.eq(arr, output.fail);
+        }
+      );
+
+      Jsc.property(
+        'Check that if the filter always returns true, then everything is in "pass"',
+        Jsc.array(Jsc.json),
+        function (arr) {
+          var output = Arr.partition(arr, Fun.constant(true));
+          return Jsc.eq(0, output.fail.length) && Jsc.eq(arr, output.pass);
+        }
+      );
+   
+      Jsc.property(
+        'Check that everything in fail fails predicate and everything in pass passes predicate',
+        Jsc.array(Jsc.json),
+        Jsc.fun(Jsc.bool),
+        function (arr, predicate) {
+          var output = Arr.partition(arr, predicate);
+
+          return Arr.forall(output.fail, function (x) {
+            return predicate(x) === false;
+          }) && Arr.forall(output.pass, function (x) {
+            return predicate(x) === true;
+          });
         }
       );
     })();
