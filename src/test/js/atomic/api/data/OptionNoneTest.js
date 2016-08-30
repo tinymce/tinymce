7c2f6831d46498dffa68581d3ae1eecb0b93a3cb
morgan <morgan.smith@ephox.com>
2016-08-30 18:13:11 +1000

    VAN-29: moved option tests

--- src/test/js/atomic/api/data/OptionNoneTest.js
+++ src/test/js/atomic/api/data/OptionNoneTest.js
@@ -3,10 +3,11 @@ test(
 
   [
     'ephox.katamari.api.Fun',
-    'ephox.katamari.api.Option'
+    'ephox.katamari.api.Option',
+    'global!Array'
   ],
 
-  function (Fun, Option) {
+  function (Fun, Option, Array) {
     var testSanity = function () {
       var s = Option.none();
       assert.eq(false, s.isSome());
@@ -31,6 +32,35 @@ test(
       assert.eq(true, s.flatten().isNone());
       assert.eq(true, s.filter(Fun.constant(true)).flatten().isNone());
       assert.eq(true, s.filter(Fun.constant(false)).flatten().isNone());
+
+      assert.eq(false, Option.from(null).isSome());
+      assert.eq(false, Option.from(undefined).isSome());
+
+      assert.eq(true, Option.none().equals(Option.none()));
+      assert.eq(false, Option.none().equals(Option.some(3)));
+
+      assert.eq([], Option.none().toArray());
+
+      assert.eq(true, Option.none().ap(Option.some(Fun.die('boom'))).equals(Option.none()));
+
+      assert.eq(true, Option.none().or(Option.some(7)).equals(Option.some(7)));
+      assert.eq(true, Option.none().or(Option.none()).equals(Option.none()));
+
+      var assertOptionEq = function(expected, actual) {
+        var same = expected.isNone() ? actual.isNone() : (actual.isSome() && expected.getOrDie() === actual.getOrDie());
+        if (!same) {
+          // assumes toString() works
+          assert.fail('Expected: ' + expected.toString() + ' Actual: ' + actual.toString());
+        }
+      };
+
+      assertOptionEq(Option.none(), Option.some(5).filter(function(x) { return x === 8; }));
+      assertOptionEq(Option.none(), Option.some(5).filter(Fun.constant(false)));
+      assertOptionEq(Option.none(), Option.none().filter(Fun.die('boom')));
+
+      assert.eq('zz', Option.none().fold(function() { return 'zz'; }, Fun.die('boom')));
+      assert.eq([], Option.none().fold(function () { return Array.prototype.slice.call(arguments); }, Fun.die('boom')));
+      assert.eq('b', Option.none().fold(Fun.constant('b'), Fun.die('boom')));
     };
 
     testSanity();
--- src/test/js/atomic/api/data/OptionSomeTest.js
+++ src/test/js/atomic/api/data/OptionSomeTest.js
@@ -3,10 +3,12 @@ test(
 
   [
     'ephox.katamari.api.Fun',
-    'ephox.katamari.api.Option'
+    'ephox.katamari.api.Option',
+    'global!Array',
+    'global!Math'
   ],
 
-  function (Fun, Option) {
+  function (Fun, Option, Array, Math) {
     var testSanity = function () {
       var boom = function(f) { throw 'Should not be called'; };
 
@@ -36,6 +38,56 @@ test(
       assert.eq(5, s.filter(Fun.constant(true)).getOrDie());
       assert.eq(true, s.filter(Fun.constant(false)).flatten().isNone());
 
+      assert.eq(true, Option.from(5).isSome());
+      assert.eq(5, Option.from(5).getOrDie('Died!'));
+
+
+      assert.eq(false, Option.some(4).equals(Option.none()));
+      assert.eq(false, Option.some(2).equals(Option.some(4)));
+      assert.eq(true, Option.some(5).equals(Option.some(5)));
+      assert.eq(false, Option.some(5.1).equals(Option.some(5.3)));
+
+      var comparator = function(a, b) { return Math.round(a) === Math.round(b); };
+
+      assert.eq(true, Option.some(5.1).equals_(Option.some(5.3), comparator));
+      assert.eq(false, Option.some(5.1).equals_(Option.some(5.9), comparator));
+      assert.eq(true, Option.equals_(Option.some(5.1), Option.some(5.3), comparator));
+
+      assert.eq([1], Option.some(1).toArray());
+      assert.eq([{ cat: 'dog' }], Option.some({ cat: 'dog' }).toArray());
+      assert.eq([[ 1 ]], Option.some([1]).toArray());
+
+      var plus2 = function(a) { return a + 2; };
+      assert.eq(true, Option.some(5).ap(Option.some(plus2)).equals(Option.some(7)));
+      assert.eq(true, Option.some(5).ap(Option.none()).equals(Option.none()));
+
+      var person = function (name) {
+        return function (age) {
+          return function (address) {
+            return { name: name, age: age, address: address };
+          };
+        };
+      };
+
+      assert.eq({name:'bob', age:25, address:'the moon'}, Option.some('the moon').ap(Option.some(25).ap(Option.some('bob').map(person))).getOrDie());
+
+      assert.eq(true, Option.some(6).or(Option.some(7)).equals(Option.some(6)));
+      assert.eq(true, Option.some(3).or(Option.none()).equals(Option.some(3)));
+
+      var assertOptionEq = function(expected, actual) {
+        var same = expected.isNone() ? actual.isNone() : (actual.isSome() && expected.getOrDie() === actual.getOrDie());
+        if (!same) {
+          // assumes toString() works
+          assert.fail('Expected: ' + expected.toString() + ' Actual: ' + actual.toString());
+        }
+      };
+
+      assertOptionEq(Option.some(6), Option.some(6).filter(function(x) { return x === 6; }));
+      assertOptionEq(Option.some(6), Option.some(6).filter(Fun.constant(true)));
+
+      assert.eq('a', Option.some('a').fold(Fun.die('boom'), Fun.identity));
+      assert.eq(['z'], Option.some('z').fold(Fun.die('boom'), function () { return Array.prototype.slice.call(arguments); }));
+      assert.eq('az', Option.some('a').fold(Fun.die('boom'), function(x) { return x + 'z'; }));
     };
 
     testSanity();
