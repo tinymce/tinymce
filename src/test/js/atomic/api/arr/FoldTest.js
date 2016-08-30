9da4640b99d5fa633d84e6b832ba3f434a5be974
morgan <morgan.smith@ephox.com>
2016-08-30 13:41:13 +1000

    VAN-29: arr folding

--- src/test/js/atomic/api/arr/FoldTest.js
+++ src/test/js/atomic/api/arr/FoldTest.js
@@ -2,10 +2,11 @@ test(
   'FoldTest',
 
   [
-    'ephox.katamari.api.Arr'
+    'ephox.katamari.api.Arr',
+    'ephox.wrap.Jsc'
   ],
 
-  function (Arr) {
+  function (Arr, Jsc) {
     var checkl = function (expected, input, f, acc) {
       assert.eq(expected, Arr.foldl(input, f, acc));
     };
@@ -25,5 +26,51 @@ test(
     checkr(13, [1, 2, 3], function (acc, x) { return acc + x; }, 7);
     // foldr with cons and [] should be identity
     checkr([1, 2, 3], [1, 2, 3], function (acc, x) { return [x].concat(acc); }, []);
+
+    Jsc.property(
+      'foldl concat [ ] xs === reverse(xs)',
+      Jsc.array(Jsc.json),
+      function (arr) {
+        var output = Arr.foldl(arr, function (b, a) {
+          return [ a ].concat(b);
+        }, [ ]);
+        return Jsc.eq(Arr.reverse(arr), output);
+      }
+    );
+
+    Jsc.property(
+      'foldr concat [ ] xs === xs',
+      Jsc.array(Jsc.json),
+      function (arr) {
+        var output = Arr.foldr(arr, function (b, a) {
+          return [ a ].concat(b);
+        }, [ ]);
+        return Jsc.eq(arr, output);
+      }
+    );
+
+    Jsc.property(
+      'foldr concat ys xs === xs ++ ys',
+      Jsc.array(Jsc.json),
+      Jsc.array(Jsc.json),
+      function (xs, ys) {
+        var output = Arr.foldr(xs, function (b, a) {
+          return [ a ].concat(b);
+        }, ys);
+        return Jsc.eq(xs.concat(ys), output);
+      }
+    );
+
+    Jsc.property(
+      'foldl concat ys xs === reverse(xs) ++ ys',
+      Jsc.array(Jsc.json),
+      Jsc.array(Jsc.json),
+      function (xs, ys) {
+        var output = Arr.foldl(xs, function (b, a) {
+          return [ a ].concat(b);
+        }, ys);
+        return Jsc.eq(Arr.reverse(xs).concat(ys), output);
+      }
+    );
   }
 );
