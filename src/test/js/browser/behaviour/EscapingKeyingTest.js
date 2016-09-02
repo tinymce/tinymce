4c4af504b82d67fc3ed1056283754901bb81f647
morgan <morgan.smith@ephox.com>
2016-09-02 12:57:59 +1000

    VAN-44: escaping test

--- src/test/js/browser/behaviour/EscapingKeyingTest.js
+++ src/test/js/browser/behaviour/EscapingKeyingTest.js
@@ -2,15 +2,48 @@ asynctest(
   'EscapingKeyingTest',
  
   [
- 
+    'ephox.agar.api.Keyboard',
+    'ephox.agar.api.Keys',
+    'ephox.agar.api.Step',
+    'ephox.alloy.api.GuiFactory',
+    'ephox.alloy.test.GuiSetup'
   ],
  
-  function () {
+  function (Keyboard, Keys, Step, GuiFactory, GuiSetup) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
-    success();
- 
+    GuiSetup.setup(function (store, doc, body) {
+      return GuiFactory.build({
+        uiType: 'custom',
+        dom: {
+          tag: 'div',
+          classes: [ 'escape-keying-test'],
+          styles: {
+            
+          }
+        },
+        uid: 'custom-uid',
+        keying: {
+          mode: 'escaping',
+          onEscape: store.adder('detected.escape')
+        },
+        components: [
+          
+        ],
+        focusing: true
+      });
+
+    }, function (doc, body, gui, component, store) {
+      return [
+        Step.sync(function () {
+          component.apis().focus();
+        }),
+        store.sAssertEq('Initially empty', [ ]),
+        Keyboard.sKeydown(doc, Keys.escape(), { }),
+        store.sAssertEq('Post escape', [ 'detected.escape' ])
+      ];
+    }, function () { success(); }, failure);
 
   }
 );
