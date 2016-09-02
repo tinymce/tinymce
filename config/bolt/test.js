467c21320db20fa73b39f317af55fb3fcbd8918a
morgan <morgan.smith@ephox.com>
2016-09-02 13:25:57 +1000

    VAN-42: had it twice

--- config/bolt/test.js
+++ config/bolt/test.js
@@ -3,7 +3,6 @@ configure({
     source('amd', 'ephox.alloy.test', '../../src/test/js/module', mapper.hierarchical),
     source('amd', 'ephox.agar', '../../lib/test', mapper.flat),
     source('amd', 'ephox.wrap.JQuery', '../../lib/test', mapper.flat),
-    source('amd', 'ephox.wrap.Jsc', '../../lib/test', mapper.flat),
-    source('amd', 'ephox.wrap.JQuery', '../../lib/test', mapper.flat)
+    source('amd', 'ephox.wrap.Jsc', '../../lib/test', mapper.flat)
   ]
 });
