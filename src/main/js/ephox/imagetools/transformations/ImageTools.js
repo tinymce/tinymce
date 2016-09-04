062151048d1304119fb7b17f655481d9d527c8fd
Davit Barbakadze <davit.barbakadze@ephox.com>
2016-09-04 13:26:37 +0400

    Filters/ImageTools: Accept and produce ImageResult instead of Blob.

--- src/main/js/ephox/imagetools/transformations/ImageTools.js
+++ src/main/js/ephox/imagetools/transformations/ImageTools.js
@@ -12,93 +12,68 @@
  * Modifies image blobs.
  */
 define("ephox/imagetools/transformations/ImageTools", [
-  "ephox/imagetools/util/Conversions",
   "ephox/imagetools/util/Canvas",
-  "ephox/imagetools/util/ImageSize",
+  "ephox/imagetools/util/ImageResult",
   "ephox/imagetools/transformations/ImageResizerCanvas"
-], function(Conversions, Canvas, ImageSize, ImageResizerCanvas) {
-  var revokeImageUrl = Conversions.revokeImageUrl;
+], function(Canvas, ImageResult, ImageResizerCanvas) {
 
-  function rotate(blob, angle) {
-    return Conversions.blobToImage(blob).then(function(image) {
-      var canvas = Canvas.create(ImageSize.getWidth(image), ImageSize.getHeight(image)),
-        context = Canvas.get2dContext(canvas),
-        translateX = 0, translateY = 0;
+  function rotate(ir, angle) {
+    var image = ir.toCanvas();
+    var canvas = Canvas.create(image.width, image.height);
+    var context = Canvas.get2dContext(canvas);
+    var translateX = 0, translateY = 0;
 
-      angle = angle < 0 ? 360 + angle : angle;
+    angle = angle < 0 ? 360 + angle : angle;
 
-      if (angle == 90 || angle == 270) {
-        Canvas.resize(canvas, canvas.height, canvas.width);
-      }
+    if (angle == 90 || angle == 270) {
+      Canvas.resize(canvas, canvas.height, canvas.width);
+    }
 
-      if (angle == 90 || angle == 180) {
-        translateX = canvas.width;
-      }
+    if (angle == 90 || angle == 180) {
+      translateX = canvas.width;
+    }
 
-      if (angle == 270 || angle == 180) {
-        translateY = canvas.height;
-      }
+    if (angle == 270 || angle == 180) {
+      translateY = canvas.height;
+    }
 
-      context.translate(translateX, translateY);
-      context.rotate(angle * Math.PI / 180);
-      context.drawImage(image, 0, 0);
-      revokeImageUrl(image);
+    context.translate(translateX, translateY);
+    context.rotate(angle * Math.PI / 180);
+    context.drawImage(image, 0, 0);
 
-      return Conversions.canvasToImageResult(canvas);
-    });
+    return ImageResult.fromCanvas(canvas);
   }
 
-  function flip(blob, axis) {
-    return Conversions.blobToImage(blob).then(function(image) {
-      var canvas = Canvas.create(ImageSize.getWidth(image), ImageSize.getHeight(image)),
-        context = Canvas.get2dContext(canvas);
+  function flip(ir, axis) {
+    var image = ir.toCanvas();
+    var canvas = Canvas.create(image.width, image.height);
+    var context = Canvas.get2dContext(canvas);
 
-      if (axis == 'v') {
-        context.scale(1, -1);
-        context.drawImage(image, 0, -canvas.height);
-      } else {
-        context.scale(-1, 1);
-        context.drawImage(image, -canvas.width, 0);
-      }
+    if (axis == 'v') {
+      context.scale(1, -1);
+      context.drawImage(image, 0, -canvas.height);
+    } else {
+      context.scale(-1, 1);
+      context.drawImage(image, -canvas.width, 0);
+    }
 
-      revokeImageUrl(image);
-
-      return Conversions.canvasToImageResult(canvas);
-    });
+    return ImageResult.fromCanvas(canvas);
   }
 
-  function crop(blob, x, y, w, h) {
-    return Conversions.blobToImage(blob).then(function(image) {
-      var canvas = Canvas.create(w, h),
-        context = Canvas.get2dContext(canvas);
+  function crop(ir, x, y, w, h) {
+    var image = ir.toCanvas();
+    var canvas = Canvas.create(w, h);
+    var context = Canvas.get2dContext(canvas);
 
-      context.drawImage(image, -x, -y);
-      revokeImageUrl(image);
+    context.drawImage(image, -x, -y);
 
-      return Conversions.canvasToImageResult(canvas);
-    });
+    return ImageResult.fromCanvas(canvas);
   }
 
-  var revokeImage = function (image) {
-    return function (result) {
-      revokeImageUrl(image);
-      return result;
-    };
-  };
 
-  function resize(blob, w, h) {
-    return Conversions.blobToImage(blob).then(function(image) {
-      var cleanup = revokeImage(image);
-      var result;
-
-      result = ImageResizerCanvas.scale(image, w, h)
-        .then(function(canvas) {
-          return Conversions.canvasToImageResult(canvas);
-        })
-        .then(cleanup, cleanup);
-
-      return result;
-    });
+  function resize(ir, w, h) {
+    return ImageResizerCanvas.scale(ir.toCanvas(), w, h)
+        .then(ImageResult.fromCanvas);
   }
 
   return {
