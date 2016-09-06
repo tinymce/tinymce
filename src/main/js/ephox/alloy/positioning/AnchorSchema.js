039273c36a8e6c030eed2aa1dd22e89894fc555f
morgan <morgan.smith@ephox.com>
2016-09-06 10:43:41 +1000

    VAN-48: submenu anchoring

--- src/main/js/ephox/alloy/positioning/AnchorSchema.js
+++ src/main/js/ephox/alloy/positioning/AnchorSchema.js
@@ -3,17 +3,18 @@ define(
 
   [
     'ephox.alloy.positioning.HotspotAnchor',
+    'ephox.alloy.positioning.SubmenuAnchor',
     'ephox.boulder.api.ValueSchema',
     'ephox.peanut.Fun'
   ],
 
-  function (HotspotAnchor, ValueSchema, Fun) {
+  function (HotspotAnchor, SubmenuAnchor, ValueSchema, Fun) {
     return ValueSchema.choose(
       'anchor', {
         // range: rangeAnchorSchema,
         // image: imageAnchorSchema,
-        hotspot: HotspotAnchor
-        // submenu: submenuAnchorSchema
+        hotspot: HotspotAnchor,
+        submenu: SubmenuAnchor
       }
     );
   }
---
+++ src/main/js/ephox/alloy/positioning/SubmenuAnchor.js
@@ -0,0 +1,33 @@
+define(
+  'ephox.alloy.positioning.SubmenuAnchor',
+
+  [
+    'ephox.boulder.api.FieldSchema',
+    'ephox.peanut.Fun',
+    'ephox.repartee.api.Bubble',
+    'ephox.repartee.api.LinkedLayout',
+    'ephox.repartee.api.MaxHeight',
+    'ephox.repartee.api.Origins'
+  ],
+
+  function (FieldSchema, Fun, Bubble, LinkedLayout, MaxHeight, Origins) {
+    var placement = function (component, posInfo, submenuInfo, origin) {
+      var anchorBox = Origins.toBox(origin, submenuInfo.item());
+
+      return {
+        anchorBox: Fun.constant(anchorBox),
+        bubble: Fun.constant(Bubble(0, 0)),
+        maxHeightFunction: Fun.constant(MaxHeight.available()),
+        layouts: Fun.constant(LinkedLayout)
+      };
+    };
+
+    return [
+      // FieldSchema.defaulted(/) // ui direction
+      FieldSchema.strict('item'),
+      FieldSchema.state('placement', function () {
+        return placement;
+      })
+    ];
+  }
+);
