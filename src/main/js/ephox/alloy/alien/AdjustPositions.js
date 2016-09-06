3904cc1474283d45921c091eeb3802eaa9154658
morgan <morgan.smith@ephox.com>
2016-09-07 09:01:33 +1000

    VAN-48: removing unnecessary code

--- src/main/js/ephox/alloy/alien/AdjustPositions.js
+++ src/main/js/ephox/alloy/alien/AdjustPositions.js
@@ -8,32 +8,14 @@ define(
 
   function (Rectangles, Fun) {
     var adjust = function (origin, optOffset, scroll, box) {
-      var addScroll = function (rect) {
-        return Rectangles.translate(rect, scroll.left(), scroll.top());
-      };
-
-      var subtractScroll = function (rect) {
-        return Rectangles.translate(rect, -scroll.left(), -scroll.top());
-      };
-
-      var addExtra = function (f, g, rect) {
-        return optOffset.fold(function () {
-          return rect;
-        }, function (offset) {
-          return Rectangles.translate(rect, offset.left(), offset.top());
-        });
-      };
-
       // Shared concept with repartee, but we don't have an element.
       // (calls OuterPosition.find in repartee)
 
       // Not using Origins.cata here because it has DOM imports
-      return origin.fold(function (/*none*/) {
-        return addExtra(Fun.identity, Fun.identity, box);
-      }, function (/* relative */) {
-        return addExtra(Fun.identity, Fun.identity, box);
-      }, function (/* fixed */) {
-        return addExtra(subtractScroll, subtractScroll, box);
+      return optOffset.fold(function () {
+        return box;
+      }, function (offset) {
+        return Rectangles.translate(box, offset.left(), offset.top());
       });
     };
 
