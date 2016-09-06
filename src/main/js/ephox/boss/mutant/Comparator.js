052307da984b5ca5f50f9171045f92cb88451039
Mike Pheasant <mike.pheasant@ephox.com>
2016-09-06 18:14:36 +1000

    TBIO-2299: adding simple [attr] selector support

--- src/main/js/ephox/boss/mutant/Comparator.js
+++ src/main/js/ephox/boss/mutant/Comparator.js
@@ -2,18 +2,30 @@ define(
   'ephox.boss.mutant.Comparator',
 
   [
-    'ephox.compass.Arr'
+    'ephox.boss.mutant.Attribution',
+    'ephox.compass.Arr',
+    'ephox.perhaps.Option'
   ],
 
-  function (Arr) {
+  function (Attribution, Arr, Option) {
+
+    var ATTR_REGEX = /^\[(.*)\]$/;
+
     var eq = function (a, b) {
       return a.id === undefined && b.id === undefined ? a.name === b.name : a.id === b.id;
     };
 
     /* Obviously, we can't support full selector syntax ... so let's just split by comma and use as array to compare with name */
+    // Selector support, either:
+    // 'name,name,...' : comma-list of names
+    // '[attr]'        : attribute name
     var is = function (item, selector) {
-      var matches = selector.split(',');
-      return Arr.contains(matches, item.name);
+      return Option.from(selector.match(ATTR_REGEX)).fold(function () { // not [attr], assume list of names
+        var matches = selector.split(',');
+        return Arr.contains(matches, item.name);
+      }, function (attrMatch) { // matched [attr]
+        return Attribution.get(item, attrMatch[1]);
+      });
     };
 
     return {
