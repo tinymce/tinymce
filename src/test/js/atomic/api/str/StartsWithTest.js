297537624d42c32875a3b965d19c4fc1654d146f
morgan <morgan.smith@ephox.com>
2016-09-13 15:21:24 +1000

    VAN-33: ends with and starts with

--- src/test/js/atomic/api/str/StartsWithTest.js
+++ src/test/js/atomic/api/str/StartsWithTest.js
@@ -1,23 +1,34 @@
 test('startsWith',
 
   [
-    'ephox.katamari.api.Strings'
+    'ephox.katamari.api.Strings',
+    'ephox.wrap.Jsc'
   ],
 
-  function(Strings) {
+  function(Strings, Jsc) {
 
     function check(expected, str, prefix) {
         var actual = Strings.startsWith(str, prefix);
         assert.eq(expected, actual);
     }
 
-    check(true, "", "");
-    check(true, "a", "");
-    check(true, "a", "a");
-    check(true, "ab", "a");
-    check(true, "abc", "ab");
+    check(true, '', '');
+    check(true, 'a', '');
+    check(true, 'a', 'a');
+    check(true, 'ab', 'a');
+    check(true, 'abc', 'ab');
 
-    check(false, "", "a");
-    check(false, "caatatetatat", "cat");
+    check(false, '', 'a');
+    check(false, 'caatatetatat', 'cat');
+
+    Jsc.property(
+      'A string added to a string (at the start) must have startsWith as true',
+      Jsc.string,
+      Jsc.nestring,
+      function (str, contents) {
+        var r = contents + str;
+        return Jsc.eq(true, Strings.startsWith(r, contents));
+      }
+    );
   }
 );
