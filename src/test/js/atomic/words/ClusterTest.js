dbbe152e601bc9b648b81dd9e02f3b6e0bffacd7
morgan <morgan.smith@ephox.com>
2016-09-16 06:48:57 +1000

    TBIO-3819: cluster test working again

--- src/test/js/atomic/words/ClusterTest.js
+++ src/test/js/atomic/words/ClusterTest.js
@@ -6,10 +6,11 @@ test(
     'ephox.boss.api.TestUniverse',
     'ephox.boss.api.TextGene',
     'ephox.compass.Arr',
+    'ephox.perhaps.Option',
     'ephox.robin.words.Cluster'
   ],
 
-  function (Gene, TestUniverse, TextGene, Arr, Cluster) {
+  function (Gene, TestUniverse, TextGene, Arr, Option, Cluster) {
     var doc = TestUniverse(Gene('root', 'root', [
       Gene('p1', 'p', [
         TextGene('a', 'There i'),
@@ -33,13 +34,36 @@ test(
     var check = function (expected, id) {
       var item = doc.find(doc.get(), id).getOrDie();
       var actual = Cluster.generate(doc, item);
-      assert.eq(expected.words, Arr.map(actual.words(), function (x) { return x.word(); }));
-      assert.eq(expected.items, Arr.map(actual.zone().elements(), function (x) { return x.id; }));
+      var aZones = actual.zones();
+      assert.eq(1, aZones.length);
+      var aZone = aZones[0];
+      assert.eq(expected.words, Arr.map(aZone.words(), function (x) { return x.word(); }));
+      assert.eq(expected.items, Arr.map(aZone.elements(), function (x) { return x.id; }));
+      assert.eq(true, Option.equals(expected.lang, aZone.lang()));
     };
 
-    check({ words: [ 'determined', 'and'], items: [ 'd', 'e', 'f' ] }, 'e');
-    check({ words: [ 'again' ], items: [ 'i' ] }, 'i');
-    check({ words: [ 'and', 'it\'s', 'driving', 'me', 'insane' ], items: [ 'f', 'g', 'h' ] }, 'g');
-    check({ words: [ 'What', 'now' ], items: [ 'j' ] }, 'j');
+    check({
+      words: [ 'determined', 'and'],
+      items: [ 'd', 'e', 'f' ],
+      lang: Option.none()
+    }, 'e');
+
+    check({
+      words: [ 'again' ],
+      items: [ 'i' ],
+      lang: Option.none()
+    }, 'i');
+
+    check({
+      words: [ 'and', 'it\'s', 'driving', 'me', 'insane' ],
+      items: [ 'f', 'g', 'h' ],
+      lang: Option.none()
+    }, 'g');
+
+    check({
+      words: [ 'What', 'now' ],
+      items: [ 'j' ],
+      lang: Option.none()
+    }, 'j');
   }
 );
