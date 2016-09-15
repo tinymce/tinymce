4de3dd2e9e07e7e38d27b0cea0248268edae77a2
morgan <morgan.smith@ephox.com>
2016-09-16 08:56:25 +1000

    TBIO-3819: restructuring clusters

--- src/main/js/ephox/robin/api/dom/DomTextZones.js
+++ src/main/js/ephox/robin/api/dom/DomTextZones.js
@@ -2,10 +2,26 @@ define(
   'ephox.robin.api.dom.DomTextZones',
 
   [
-
+    'ephox.boss.api.DomUniverse',
+    'ephox.robin.api.general.TextZones'
   ],
 
-  function () {
-    return null;
+  function (DomUniverse, TextZones) {
+    var universe = DomUniverse();
+
+    var single = function (element) {
+      return TextZones.single(universe, element);
+    };
+
+    var range = function (start, soffset, finish, foffset) {
+      return TextZones.range(start, soffset, finish, foffset);
+    };
+
+    // Something for full document in stages?
+
+    return {
+      single: single,
+      range: range
+    };
   }
 );
--- src/main/js/ephox/robin/api/general/TextZones.js
+++ src/main/js/ephox/robin/api/general/TextZones.js
@@ -2,10 +2,23 @@ define(
   'ephox.robin.api.general.TextZones',
 
   [
-
+    'ephox.robin.words.Cluster'
   ],
 
-  function () {
-    return null;
+  function (Cluster) {
+    var single = function (universe, element) {
+      var clusters = universe.property().isBoundary(element) ? Cluster.block : Cluster.inline;
+      return clusters(universe, element);
+    };
+
+    var range = function (universe, start, soffset, finish, foffset) {
+
+      // return universe.property().isBoundary(element) ? Cluster.block(universe, element) : Cluster.generate(universe, element, optimise);
+    };
+
+    return {
+      single: single,
+      range: range
+    };
   }
 );
