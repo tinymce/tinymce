7f191dcb32fc4825112b7d337dfb28b675c07337
morgan <morgan.smith@ephox.com>
2016-09-28 13:57:40 +1000

    VAN-49: throttler types

--- src/test/js/atomic/api/fun/ThrottlerTest.js
+++ src/test/js/atomic/api/fun/ThrottlerTest.js
@@ -2,29 +2,89 @@ asynctest(
   'ThrottlerTest',
 
   [
+    'ephox.katamari.api.Future',
     'ephox.katamari.api.Throttler',
     'global!setTimeout'
   ],
 
-  function (Throttler, setTimeout, success, failure) {
-    var data = [];
-    var throttler = Throttler(function (value) {
-      data.push(value);
-    }, 250);
+  function (Future, Throttler, setTimeout) {
+    var success = arguments[arguments.length - 2];
+    var failure = arguments[arguments.length - 1];
 
-    throttler.throttle('cat');
-    throttler.throttle('dog');
-    throttler.throttle('elephant');
-    throttler.throttle('frog');
+    var testAdaptable = function () {
+      return Future.nu(function (callback) {
+        var data = [];
+        var throttler = Throttler.adaptable(function (value) {
+          data.push(value);
+        }, 250);
 
-    setTimeout(function () {
-      assert.eq([ 'frog' ], data);
-      throttler.throttle('frog-goose');
-      throttler.throttle('goose');
-      setTimeout(function () {
-        assert.eq([ 'frog', 'goose' ], data);
-        success();
-      }, 500);
-    }, 500);
+        throttler.throttle('cat');
+        throttler.throttle('dog');
+        throttler.throttle('elephant');
+        throttler.throttle('frog');
+
+        setTimeout(function () {
+          assert.eq([ 'frog' ], data);
+          throttler.throttle('frog-goose');
+          throttler.throttle('goose');
+          setTimeout(function () {
+            assert.eq([ 'frog', 'goose' ], data);
+            callback();
+          }, 500);
+        }, 500);
+      });
+    };
+
+    var testFirst = function () {
+      return Future.nu(function (callback) {
+        var data = [];
+        var throttler = Throttler.first(function (value) {
+          data.push(value);
+        }, 250);
+
+        throttler.throttle('cat');
+        throttler.throttle('dog');
+        throttler.throttle('elephant');
+        throttler.throttle('frog');
+
+        setTimeout(function () {
+          assert.eq([ 'cat' ], data);
+          throttler.throttle('frog-goose');
+          throttler.throttle('goose');
+          setTimeout(function () {
+            assert.eq([ 'cat', 'frog-goose' ], data);
+            callback();
+          }, 500);
+        }, 500);
+      });
+    };
+
+    var testLast = function () {
+      return Future.nu(function (callback) {
+        var data = [];
+        var throttler = Throttler.last(function (value) {
+          data.push(value);
+        }, 250);
+
+        throttler.throttle('cat');
+        throttler.throttle('dog');
+        throttler.throttle('elephant');
+        throttler.throttle('frog');
+
+        setTimeout(function () {
+          assert.eq([ 'frog' ], data);
+          throttler.throttle('frog-goose');
+          throttler.throttle('goose');
+          setTimeout(function () {
+            assert.eq([ 'frog', 'goose' ], data);
+            callback();
+          }, 500);
+        }, 500);
+      });
+    };
+
+    testAdaptable().bind(testFirst).bind(testLast).get(function () {
+      success();
+    });
   }
 );
