dbede49b0e25e0a56a6c0316d30e620372497be2
morgan <morgan.smith@ephox.com>
2016-10-10 12:37:41 +1000

    TBIO-4391: Supported Latin Extended-A

--- src/main/js/ephox/polaris/pattern/Chars.js
+++ src/main/js/ephox/polaris/pattern/Chars.js
@@ -12,7 +12,7 @@ define(
     // '-' is a hyphen
     // \u00C0 - \u00FF are various language characters
     // \u2018 and \u2019 are the smart quote characters
-    var chars = '\\w' + '\'' + '\\-' + '\\u00C0-\\u00FF' + Unicode.zeroWidth() + '\\u2018\\u2019';
+    var chars = '\\w' + '\'' + '\\-' + '\\u0100-\\u017F\\u00C0-\\u00FF' + Unicode.zeroWidth() + '\\u2018\\u2019';
     var wordbreak = '[^' + chars + ']';
     var wordchar = '[' + chars + ']';
 
--- src/test/js/atomic/pattern/CharsTest.js
+++ src/test/js/atomic/pattern/CharsTest.js
@@ -8,58 +8,96 @@ test(
   ],
 
   function (Arr, Chars, RegExp) {
-// ["fr","en_au","pt","it","nl","en_uk","pt_pt","de","nb","en_br","sv","da","en","es","en_gb","fi","en_us"]}
-
-    var supported = ['fr','en_au','pt','it','nl','en_uk','pt_pt','de','nb','en_br','sv','da','en','es','en_gb','fi','en_us'];
-
+    var supported = [ 'fr','en_au','pt','it','nl','en_uk','pt_pt','de','nb','en_br','sv','da','en','es','en_gb','fi','en_us' ];
 
     var extras = {
       fr: {
-        chars: 'àÀâÂèÈéÉêÊëËîÎïÏôÔùÙûÛüÜÿŸçÇœŒ',
-        html: 'https://www.cs.tut.fi/~jkorpela/html/french.html',
         label: 'French language',
-        code: 'fr'
+        html: 'http://character-code.com/french-html-codes.php || https://www.cs.tut.fi/~jkorpela/html/french.html',
+        chars: 'àÀâÂèÈéÉêÊëËîÎïÏôÔùÙûÛüÜÿŸçÇœŒ' +
+               'ÀàÂâÆæÇçÈèÉéÊêËëÎîÏïÔôŒœÙùÛûÜü'
       },
-      en_au: { },
+      en_au: {
+        label: 'English (Aus)',
+        html: '<known>',
+        chars: ''
+      },
       pt: {
+        label: 'Brazilian Portuguese',
+        html: 'http://www.geocities.ws/click2speak/unicode/chars_pt.html',
+        chars: 'ÀàÁáÂâÃãÉéÊêÍíÒòÓóÔôÕõÚúÜüÇç'
 
       },
       it: {
-
+        label: 'Italian',
+        html: 'https://mcaboni.wordpress.com/2012/01/09/107/',
+        chars: 'àÈèéìòù'
       },
       nl: {
-
+        label: 'Dutch',
+        html: 'http://symbolcodes.tlt.psu.edu/bylanguage/dutch.html',
+        chars: 'ÁÉÍÓÚÝ áéíóúý ÄËÏÖÜŸ äëïöüÿ ÀÈÌÒÙ àèìòù ÂÊÎÔÛ âêîôû'
       },
       en_uk: {
-
+        label: 'Emglish (UK)',
+        html: '<known>',
+        chars: ''
       },
       pt_pt: {
-
+        label: 'European Portuguese',
+        html: 'http://www.geocities.ws/click2speak/unicode/chars_pt.html',
+        chars: 'ÀàÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇç'
       },
       de: {
-
+        label: 'German',
+        html: 'http://character-code.com/german-html-codes.php',
+        chars: 'ÄäÉéÖöÜüß'
       },
       nb: {
-
+        label: 'Norwegian',
+        html: 'http://symbolcodes.tlt.psu.edu/bylanguage/nordic.html',
+        chars: 'ÅåÆæØø'
       },
-      en_br: { },
+      en_br: {
+        label: 'English (British)',
+        html: '<known>',
+        chars: ''
+      },
       sv: {
-
+        label: 'Swedish',
+        html: 'http://symbolcodes.tlt.psu.edu/bylanguage/nordic.html',
+        chars: 'ÅåÄäÖö'
       },
       da: {
-
+        label: 'Danish',
+        html: 'http://symbolcodes.tlt.psu.edu/bylanguage/nordic.html',
+        chars: 'ÅåÆæØø'
       },
-      en: { },
+      en: {
+        label: 'English',
+        html: '<known>',
+        chars: ''
+      },
       es: {
-
+        label: 'Spanish',
+        html: 'http://character-code.com/spanish-html-codes.php',
+        chars: 'ÁáÉéÍíÑñÓóÚúÜü'
       },
       en_gb: {
-
+        label: 'English (GB)',
+        html: '<known>',
+        chars: ''
       },
       fi: {
-
+        label: 'Finnish',
+        html: 'http://symbolcodes.tlt.psu.edu/bylanguage/nordic.html',
+        chars: 'ÅåÄäÖö'
       },
-      en_us: { }
+      en_us: {
+        label: 'English (US)',
+        html: '<known>',
+        chars: ''
+      }
     };
 
     /*
