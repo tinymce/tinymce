30d30c9d4d20da5480d358bc2540a44cda369484
morgan <morgan.smith@ephox.com>
2016-10-18 15:56:38 +1000

    VAN-44-wheel: tweaking for inline toolbars in polish

--- src/main/js/ephox/alloy/positioning/SelectionAnchor.js
+++ src/main/js/ephox/alloy/positioning/SelectionAnchor.js
@@ -8,6 +8,7 @@ define(
     'ephox.alloy.positioning.Anchoring',
     'ephox.alloy.positioning.ContainerOffsets',
     'ephox.boulder.api.FieldSchema',
+    'ephox.bud.Unicode',
     'ephox.fussy.api.SelectionRange',
     'ephox.fussy.api.WindowSelection',
     'ephox.oath.proximity.Awareness',
@@ -19,11 +20,15 @@ define(
     'ephox.scullion.Struct',
     'ephox.sugar.alien.Position',
     'ephox.sugar.api.Direction',
+    'ephox.sugar.api.Element',
+    'ephox.sugar.api.Insert',
     'ephox.sugar.api.Node',
-    'ephox.sugar.api.Traverse'
+    'ephox.sugar.api.Remove',
+    'ephox.sugar.api.Traverse',
+    'global!Math'
   ],
 
-  function (Boxes, CssPosition, Descend, Anchoring, ContainerOffsets, FieldSchema, SelectionRange, WindowSelection, Awareness, Fun, Option, Bubble, Layout, Origins, Struct, Position, Direction, Node, Traverse) {
+  function (Boxes, CssPosition, Descend, Anchoring, ContainerOffsets, FieldSchema, Unicode, SelectionRange, WindowSelection, Awareness, Fun, Option, Bubble, Layout, Origins, Struct, Position, Direction, Element, Insert, Node, Remove, Traverse, Math) {
     var point = Struct.immutable('element', 'offset');
 
     // A range from (a, 1) to (body, end) was giving the wrong bounds.
@@ -41,11 +46,7 @@ define(
       return getSelection().map(function (sel) {
         var modStart = descendOnce(sel.start(), sel.soffset());
         var modFinish = descendOnce(sel.finish(), sel.foffset());
-        var originalStart = point(sel.start(), sel.soffset());
-        var originalFinish = point(sel.finish(), sel.foffset());
-        var useStart = Node.name(modStart.element()) === 'br' ? originalStart : modStart;
-        var useFinish = Node.name(modFinish.element()) === 'br' ? originalFinish : modFinish;
-        return SelectionRange.general(useStart.element(), useStart.offset(), useFinish.element(), useFinish.offset());
+        return SelectionRange.general(modStart.element(), modStart.offset(), modFinish.element(), modFinish.offset());
       });
     };
 
@@ -56,25 +57,27 @@ define(
       console.log('selection.placement');
 
       var selectionBox = getAnchorSelection(win, anchorInfo).bind(function (sel) {
+        console.log('sel', sel.start().dom());
         // This represents the *visual* rectangle of the selection.
         var optRect = WindowSelection.rectangleAt(win, sel.start(), sel.soffset(), sel.finish(), sel.foffset()).orThunk(function () {
-          // Certain things like <p><br/></p> with (p, 0) as collapsed selection do not return a client rectangle
-          return WindowSelection.rectangleAt(win, sel.start(), sel.soffset(), anchorInfo.root(), Awareness.getEnd(anchorInfo.root())).map(function (rectToEnd) {
-            return {
-              left: rectToEnd.left,
-              top: rectToEnd.top,
-              width: 0,
-              height: 0
-            };
-          });
+          var x = Element.fromText(Unicode.zeroWidth());
+          Insert.before(sel.start(), x);
+          // Certain things like <p><br/></p> with (p, 0) or <br>) as collapsed selection do not return a client rectangle
+          return WindowSelection.rectangleAt(win, x, 0, x, 1).map(function (rect) {
+            Remove.remove(x);
+            return rect;
+          });          
         });
         return optRect.map(function (rawRect) {
           // NOTE: We are going to have to do some interesting things to make inline toolbars not appear over the toolbar.
           var point = CssPosition.screen(
-            Position(rawRect.left, rawRect.top)
+            Position(
+              Math.max(0, rawRect.left),
+              Math.max(0, rawRect.top)
+            )
           );
 
-          console.log('rawRect', rawRect.height, rawRect.top);
+          console.log('rawRect', rawRect.height, rawRect.top, rawRect.width, rawRect.height);
 
           return Boxes.pointed(point, rawRect.width, rawRect.height);
         });
@@ -105,12 +108,20 @@ define(
           return Node.isElement(sel.start()) ? Option.some(sel.start()) : Traverse.parent(sel.start());
         });
 
-        var getLayouts = Direction.onDirection([ Layout.northwest ], Layout.allRtl());
-        var layouts = targetElement.map(getLayouts).getOrThunk(Layout.all);
+        var layoutsLtr = function () {
+          return [ Layout.northeast, Layout.northwest, Layout.southeast, Layout.southwest, Layout.northmiddle, Layout.southmiddle ];
+        };
+
+        var layoutsRtl = function () {
+          return [ Layout.northwest, Layout.northeast, Layout.southwest, Layout.southeast, Layout.northmiddle, Layout.southmiddle ];
+        };
+
+        var getLayouts = Direction.onDirection(layoutsLtr(), layoutsRtl());
+        var layouts = targetElement.map(getLayouts).getOrThunk(layoutsLtr);
 
         return Anchoring({
           anchorBox: Fun.constant(anchorBox),
-          bubble: Fun.constant(Bubble(0, 0)),
+          bubble: Fun.constant(anchorInfo.bubble().getOr(Bubble(0, 0))),
           overrides: Fun.constant({ }),
           layouts: Fun.constant(layouts)
         });
@@ -120,6 +131,7 @@ define(
     return [
       FieldSchema.option('getSelection'),
       FieldSchema.strict('root'),
+      FieldSchema.option('bubble'),
       FieldSchema.state('placement', function () {
         return placement;
       })
