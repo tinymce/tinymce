77e9f89f3793b66137e86b503d4313e66c564c8f
morgan <morgan.smith@ephox.com>
2016-11-04 08:26:32 +1000

    VAN-44-wheel-hackery-apis: Receiving interface

---
+++ src/main/js/ephox/alloy/api/behaviour/Receiving.js
@@ -0,0 +1,15 @@
+define(
+  'ephox.alloy.api.behaviour.Receiving',
+
+  [
+    'ephox.alloy.api.behaviour.BehaviourExport'
+  ],
+
+  function (BehaviourExport) {
+    return BehaviourExport.build(
+      'receiving',
+      [ ],
+      { }
+    );
+  }
+);
