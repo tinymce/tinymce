3de93b64e8a64bc920e5cc8bc92a15d75655ec94
morgan <morgan.smith@ephox.com>
2016-11-04 07:33:03 +1000

    VAN-44-wheel-hackery-apis: generic behaviour export

---
+++ src/main/js/ephox/alloy/api/behaviour/BehaviourExport.js
@@ -0,0 +1,49 @@
+define(
+  'ephox.alloy.api.behaviour.BehaviourExport',
+
+  [
+    'ephox.boulder.api.Objects',
+    'ephox.compass.Arr',
+    'global!Array'
+  ],
+
+  function (Objects, Arr, Array) {
+    // Add some off behaviour also.
+    var build = function (behaviourName, apiNames, alternatives) {
+      // If readability becomes a problem, stop dynamically generating these.
+
+      var access = function (component) {
+        return component.apis();
+      };
+
+      return Objects.wrapAll(
+        Arr.bind(apiNames, function (apiName) {
+          return [
+            {
+              key: apiName,
+              // e.g. Highlighting.highlight(component, item)
+              value: function (component/*, args */) {
+                var args = Array.prototype.slice.call(arguments, 1);
+                return access(component)[apiName].apply(undefined, args);
+              }
+            },
+            {
+              key: apiName + '_',
+              // e.g. Highlighting.highlight(item)(component)
+              value: function (/* */) {
+                var args = Array.prototype.slice.call(arguments, 0);
+                return function (component) {
+                  return access(component)[apiName].apply(undefined, args);
+                };
+              }
+            }
+          ];
+        })
+      );
+    };
+
+    return {
+      build: build
+    };
+  }
+);
--- src/main/js/ephox/alloy/api/behaviour/Highlighting.js
+++ src/main/js/ephox/alloy/api/behaviour/Highlighting.js
@@ -2,54 +2,28 @@ define(
   'ephox.alloy.api.behaviour.Highlighting',
 
   [
-    'ephox.boulder.api.Objects',
-    'ephox.compass.Arr',
+    'ephox.alloy.api.behaviour.BehaviourExport',
     'global!Array'
   ],
 
-  function (Objects, Arr, Array) {
+  function (BehaviourExport, Array) {
     // If readability becomes a problem, stop dynamically generating these.
-
-    var access = function (component) {
-      return component.apis();
-    };
-
-    // TODO: Get from behaviour
-    var apiNames = [
-      'highlight',
-      'dehighlight',
-      'dehighlightAll',
-      'highlightFirst',
-      'highlightLast',
-      'isHighlighted',
-      'getHighlighted',
-      'getFirst',
-      'getLast',
-      'getPrevious',
-      'getNext'
-    ];
-
-    return Objects.wrapAll(
-      Arr.bind(apiNames, function (apiName) {
-        return [
-          {
-            key: apiName,
-            value: function (component/*, args */) {
-              var args = Array.prototype.slice.call(arguments, 1);
-              return access(component)[apiName].apply(undefined, args);
-            }
-          },
-          {
-            key: apiName + '_',
-            value: function (/* */) {
-              var args = Array.prototype.slice.call(arguments, 0);
-              return function (component) {
-                return access(component)[apiName].apply(undefined, args);
-              };
-            }
-          }
-        ];
-      })
+    return BehaviourExport.build(
+      'highlighting',
+      [
+        'highlight',
+        'dehighlight',
+        'dehighlightAll',
+        'highlightFirst',
+        'highlightLast',
+        'isHighlighted',
+        'getHighlighted',
+        'getFirst',
+        'getLast',
+        'getPrevious',
+        'getNext'
+      ],
+      { }
     );
   }
 );
