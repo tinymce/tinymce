0f9f2f3704b95bf5d0366c254c055ca29acfa10c
morgan <morgan.smith@ephox.com>
2016-11-17 12:20:25 +1000

    VAN-54: Using 100 tests and cleanup

--- config/bolt/test.js
+++ config/bolt/test.js
@@ -1,19 +1,3 @@
-var grouping = function (splitter, groupIndex, base, idMapper) {
-  return function (id) {
-    var groups = id.split('.');
-
-    return [
-      groups[groupIndex],
-      base,
-      idMapper(id)
-    ].join('/');
-  }
-};
-
-var repo = function (base, idMapper) {
-  return grouping('.', 1, base, idMapper);
-};
-
 configure({
   sources: [
     source('amd', 'ephox.katamari.test', '../../src/test/js/module', mapper.hierarchical),
