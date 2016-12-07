a8a2923f5f9f912da61c96aad41c6812f485b606
morgan <morgan.smith@ephox.com>
2016-12-07 14:12:05 +1000

    VAN-44-wheel-hackery-apis-2: introducing part types

--- src/main/js/ephox/alloy/api/ui/SplitDropdown.js
+++ src/main/js/ephox/alloy/api/ui/SplitDropdown.js
@@ -4,20 +4,17 @@ define(
   [
     'ephox.alloy.api.SystemEvents',
     'ephox.alloy.construct.EventHandler',
-    'ephox.alloy.parts.InternalSink',
+    'ephox.alloy.parts.PartType',
     'ephox.alloy.registry.Tagger',
     'ephox.alloy.spec.SpecSchema',
     'ephox.alloy.spec.SplitDropdownSpec',
-    'ephox.alloy.spec.UiSubstitutes',
     'ephox.boulder.api.FieldSchema',
-    'ephox.compass.Obj',
     'ephox.highway.Merger',
     'ephox.peanut.Fun',
-    'ephox.perhaps.Option',
     'global!Error'
   ],
 
-  function (SystemEvents, EventHandler, InternalSink, Tagger, SpecSchema, SplitDropdownSpec, UiSubstitutes, FieldSchema, Obj, Merger, Fun, Option, Error) {
+  function (SystemEvents, EventHandler, PartType, Tagger, SpecSchema, SplitDropdownSpec, FieldSchema, Merger, Fun, Error) {
     var schema = [
       FieldSchema.strict('toggleClass'),
       FieldSchema.strict('fetch'),
@@ -30,70 +27,165 @@ define(
       FieldSchema.defaulted('matchWidth', false)
     ];
 
+    var arrowPart = PartType.internal(
+      'arrow',
+      '<alloy.split-dropdown.arrow>',
+      function (detail) {
+        return {
+          // FIX: new style.
+          uiType: 'button',
+          behaviours: {
+            // FIX undefined
+            tabstopping: undefined,
+            focusing: undefined
+          }
+        };
+      },
+      function (detail) {
+        return {
+          action: function (arrow) {
+            var hotspot = arrow.getSystem().getByUid(detail.uid()).getOrDie();
+            hotspot.getSystem().triggerEvent(SystemEvents.execute(), hotspot.element(), { });
+          },
+          behaviours: {
+            toggling: {
+              toggleOnExecute: false
+            }
+          }
+        };
+      }
+    );
+
+    var buttonPart = PartType.internal(
+      'button',
+      '<alloy.split-dropdown.button>',
+      function (detail) {
+        return {
+          behaviours: {
+          // FIX: Undefined false
+            focusing: undefined
+          }
+        };
+      },
+      function (detail) {
+        return {
+          uiType: 'button',
+          action: detail.onExecute()
+        };
+      }
+    );
+
+    var sinkPart = PartType.optional(
+      'sink',
+      '<alloy.sink>',
+      Fun.constant({ }),
+      Fun.constant({
+        dom: {
+          tag: 'div'
+        },
+        uiType: 'custom',
+        behaviours: {
+          positioning: {
+            // FIX: configurable
+            useFixed: true
+          }
+        },
+        events: {
+          // Probably a behaviour: cut mania
+          'keydown': EventHandler.nu({
+            run: function (component, simulatedEvent) {
+              // Sinks should not let keydown or click propagate
+              simulatedEvent.cut();
+            }
+          }),
+          'mousedown': EventHandler.nu({
+            run: function (component, simulatedEvent) {
+              // Sinks should not let keydown or click propagate or mousedown
+              simulatedEvent.cut();
+            }
+          }),
+          'click': EventHandler.nu({
+            run: function (component, simulatedEvent) {
+              // Sinks should not let keydown or click propagate
+              simulatedEvent.cut();
+            }
+          })
+        }
+      })
+    );
+
+    var partTypes = [
+      arrowPart,
+      buttonPart,
+      PartType.external('menu'),
+      sinkPart
+    ];
+
     var build = function (f) {
 
-      var parts = {
-        arrow: Fun.constant({
-          placeholder: Fun.constant({ uiType: 'placeholder', owner: 'split-dropdown', name: '<alloy.split-dropdown.arrow>' }),
-          build: function (spec) {
-            return UiSubstitutes.single(true,  function () {
-              return Merger.deepMerge(
-                {
-                  // FIX: new style.
-                  uiType: 'button',
-                  behaviours: {
-                    // FIX undefined
-                    tabstopping: undefined,
-                    focusing: undefined
-                  }
-                },
-                spec,
-                {
-                  uid: detail.partUids()['arrow'],
-                  action: function (arrow) {
-                    var hotspot = arrow.getSystem().getByUid(detail.uid()).getOrDie();
-                    hotspot.getSystem().triggerEvent(SystemEvents.execute(), hotspot.element(), { });
-                  },
-                  behaviours: {
-                    toggling: {
-                      toggleOnExecute: false
-                    }
-                  }
-                }
-              );
-            });
-          }
-        }),
-        button: Fun.constant({
-          placeholder: Fun.constant({ uiType: 'placeholder', owner: 'split-dropdown', name: '<alloy.split-dropdown.button>' }),
-          build: function (spec) {
-            return UiSubstitutes.single(true,  function () {
-              return Merger.deepMerge(
-                {
-                  behaviours: {
-                    // FIX: Undefined false
-                    focusing: undefined
-                  }
-                },
-                spec,
-                {
-                  uid: detail.partUids()['button'],
-                  uiType: 'button',
-                  action: detail.onExecute()
-                }
-              );
-            });
-          }
-        }),
-        menu: Fun.constant({
-          placeholder: Fun.die('The part menu should not appear in components'),
-          build: function (spec) {
-            return spec;
-          }
-        }),
+      var parts = PartType.generate('split-dropdown', partTypes);
+      // {
+      //   arrow: Fun.constant({
+      //     placeholder: Fun.constant({ uiType: 'placeholder', owner: 'split-dropdown', name: '<alloy.split-dropdown.arrow>' }),
+      //     build: function (spec) {
+      //       return UiSubstitutes.single(true,  function () {
+      //         return Merger.deepMerge(
+      //           {
+      //             // FIX: new style.
+      //             uiType: 'button',
+      //             behaviours: {
+      //               // FIX undefined
+      //               tabstopping: undefined,
+      //               focusing: undefined
+      //             }
+      //           },
+      //           spec,
+      //           {
+      //             uid: detail.partUids()['arrow'],
+      //             action: function (arrow) {
+      //               var hotspot = arrow.getSystem().getByUid(detail.uid()).getOrDie();
+      //               hotspot.getSystem().triggerEvent(SystemEvents.execute(), hotspot.element(), { });
+      //             },
+      //             behaviours: {
+      //               toggling: {
+      //                 toggleOnExecute: false
+      //               }
+      //             }
+      //           }
+      //         );
+      //       });
+      //     }
+      //   }),
+      //   button: Fun.constant({
+      //     placeholder: Fun.constant({ uiType: 'placeholder', owner: 'split-dropdown', name: '<alloy.split-dropdown.button>' }),
+      //     build: function (spec) {
+      //       return UiSubstitutes.single(true,  function () {
+      //         return Merger.deepMerge(
+      //           {
+      //             behaviours: {
+      //               // FIX: Undefined false
+      //               focusing: undefined
+      //             }
+      //           },
+      //           spec,
+      //           {
+      //             uid: detail.partUids()['button'],
+      //             uiType: 'button',
+      //             action: detail.onExecute()
+      //           }
+      //         );
+      //       });
+      //     }
+      //   }),
+      //   menu: Fun.constant({
+      //     placeholder: Fun.die('The part menu should not appear in components'),
+      //     build: function (spec) {
+      //       return spec;
+      //     }
+      //   }),
 
-        sink: Fun.constant(InternalSink)
-      };
+      //   sink: Fun.constant(InternalSink)
+      // };
 
 
       var spec = f(parts);
@@ -101,14 +193,19 @@ define(
         uid: Tagger.generate('uid')
       }, spec);
 
+      var schemas = PartType.schemas(partTypes);
 
-      var detail = SpecSchema.asStructOrDie('split-dropdown.build', schema, userSpec, [ 'button', 'arrow', 'menu' ], [ 'sink' ]);
+      var detail = SpecSchema.asStructOrDie('split-dropdown.build', schema, userSpec, schemas.required(), schemas.optional());
 
-      var components = UiSubstitutes.substitutePlaces(Option.some('split-dropdown'), detail, detail.components(), {
-        '<alloy.split-dropdown.button>': detail.parts().button(),
-        '<alloy.split-dropdown.arrow>': detail.parts().arrow(),
-        '<alloy.sink>': detail.parts().sink()
-      });
+      var components = PartType.components('split-dropdown', detail, partTypes);
+
+      // var components = UiSubstitutes.substitutePlaces(Option.some('split-dropdown'), detail, detail.components(), {
+      //   '<alloy.split-dropdown.button>': detail.parts().button(),
+      //   '<alloy.split-dropdown.arrow>': detail.parts().arrow(),
+      //   '<alloy.sink>': detail.parts().sink()
+      // });
+
+      debugger;
 
       return Merger.deepMerge(
         spec,
---
+++ src/main/js/ephox/alloy/parts/PartType.js
@@ -0,0 +1,129 @@
+define(
+  'ephox.alloy.parts.PartType',
+
+  [
+    'ephox.alloy.spec.UiSubstitutes',
+    'ephox.compass.Arr',
+    'ephox.compass.Obj',
+    'ephox.highway.Merger',
+    'ephox.peanut.Fun',
+    'ephox.perhaps.Option',
+    'ephox.scullion.ADT'
+  ],
+
+  function (UiSubstitutes, Arr, Obj, Merger, Fun, Option, Adt) {
+    var adt = Adt.generate([
+      { internal: [ 'name', 'pname', 'defaults', 'overrides' ] },
+      { external: [ 'name' ] },
+      { optional: [ 'name', 'pname', 'defaults', 'overrides' ] }
+    ]);
+
+    // TODO: Make more functional if performance isn't an issue.
+
+    var schemas = function (parts) {
+      var required = [ ];
+      var optional = [ ];
+
+      Arr.each(parts, function (part) {
+        part.fold(
+          function (name, pname, defaults, overrides) {
+            required.push(name);
+          },
+          function (name) {
+            required.push(name);
+          },
+          function (name, pname, defaults, overrides) {
+            optional.push(name);
+          }
+        );
+      });
+
+      return {
+        required: Fun.constant(required),
+        optional: Fun.constant(optional)
+      };
+    };
+
+    var generate = function (owner, parts) {
+      var r = { };
+
+      Arr.each(parts, function (part) {
+        part.fold(
+          function (name, pname, defaults, overrides) {
+            r[name] = {
+              placeholder: Fun.constant({uiType: 'placeholder', owner: owner, name: pname }),
+              build: function (spec) {
+                return UiSubstitutes.single(true, function (detail) {
+                  return Merger.deepMerge(
+                    defaults(detail),
+                    spec,
+                    {
+                      uid: detail.partUids()[name]
+                    },
+                    overrides(detail)
+                  );
+                });
+              }
+            };
+          },
+          function (name) {
+            r[name] = {
+              placeholder: Fun.die('The part: ' + name + ' should not appear in components for: ' + owner),
+              build: Fun.identity
+            };
+            // Do nothing ... has no placeholder.
+          },
+          function (name, pname, defaults, overrides) {
+            r[name] = {
+              placeholder: Fun.constant({uiType: 'placeholder', owner: owner, name: pname }),
+              build: function (spec) {
+                return UiSubstitutes.single(false, function (detail) {
+                  return Merger.deepMerge(
+                    defaults(detail),
+                    spec,
+                    {
+                      uid: detail.partUids()[name]
+                    },
+                    overrides(detail)
+                  );
+                });
+              }
+            };
+          }
+        );
+      });
+
+      return Obj.map(r, Fun.constant);
+
+    };
+
+    var components = function (owner, detail, parts) {
+      var ps = { };
+      Arr.each(parts, function (part) {
+        part.fold(
+          function (name, pname, defaults, overrides) {
+            ps[pname] = detail.parts()[name]();
+          },
+          function (name) {
+            // do nothing ... should not be in components
+          },
+          function (name, pname, defaults, overrides) {
+            ps[pname] = detail.parts()[name]();
+          }
+        );
+      });
+
+      return UiSubstitutes.substitutePlaces(Option.some(owner), detail, detail.components(), ps);
+    };
+
+    return {
+      internal: adt.internal,
+      external: adt.external,
+      optional: adt.optional,
+
+      schemas: schemas,
+      generate: generate,
+      components: components
+    };
+  }
+);
