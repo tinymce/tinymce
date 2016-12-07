5446e93590b2d2aff3c9132822b6e828af0be84b
morgan <morgan.smith@ephox.com>
2016-12-07 14:18:51 +1000

    VAN-44-wheel-hackery-apis-2: streamlined part builder

--- src/main/js/ephox/alloy/api/ui/Dropdown.js
+++ src/main/js/ephox/alloy/api/ui/Dropdown.js
@@ -2,22 +2,21 @@ define(
   'ephox.alloy.api.ui.Dropdown',
 
   [
+    'ephox.alloy.api.ui.CompositeBuilder',
     'ephox.alloy.parts.InternalSink',
-    'ephox.alloy.registry.Tagger',
+    'ephox.alloy.parts.PartType',
     'ephox.alloy.spec.DropdownSpec',
-    'ephox.alloy.spec.SpecSchema',
-    'ephox.alloy.spec.UiSubstitutes',
     'ephox.boulder.api.FieldSchema',
-    'ephox.highway.Merger',
     'ephox.peanut.Fun',
     'ephox.perhaps.Option'
   ],
 
-  function (InternalSink, Tagger, DropdownSpec, SpecSchema, UiSubstitutes, FieldSchema, Merger, Fun, Option) {
+  function (CompositeBuilder, InternalSink, PartType, DropdownSpec, FieldSchema, Fun, Option) {
     var schema = [
       FieldSchema.strict('fetch'),
       FieldSchema.defaulted('onOpen', Fun.noop),
       FieldSchema.defaulted('onExecute', Option.none),
+      // FIX: Remove defaulted toggleClass
       FieldSchema.defaulted('toggleClass', 'alloy-selected-button'),
       FieldSchema.strict('dom'),
       FieldSchema.defaulted('displayer', Fun.identity),
@@ -25,30 +24,13 @@ define(
       FieldSchema.defaulted('matchWidth', false)
     ];
 
+    var partTypes = [
+      PartType.external('menu'),
+      InternalSink
+    ];
+
     var build = function (f) {
-      var parts = {
-        menu: Fun.constant({
-          placeholder: Fun.die('The part menu should not appear in components'),
-          build: function (spec) {
-            return spec;
-          }
-        }),
-
-        sink: Fun.constant(InternalSink)
-      };
-
-      var spec = f(parts);
-      var userSpec = Merger.deepMerge({
-        uid: Tagger.generate('uid')
-      }, spec);
-
-      var detail = SpecSchema.asStructOrDie('dropdown.build', schema, userSpec, [ 'menu' ], [ 'sink' ]);
-
-      var components = UiSubstitutes.substitutePlaces(Option.some('split-dropdown'), detail, detail.components(), {
-        '<alloy.sink>': detail.parts().sink()
-      });
-
-      return DropdownSpec.make(detail, components);
+      return CompositeBuilder.build('dropdown', schema, partTypes, DropdownSpec.make, f);
     };
 
     return {
