65fd9eef0072f910d1799c7447b27efa33a36830
morgan <morgan.smith@ephox.com>
2016-12-09 07:50:27 +1000

    VAN-44-wheel-hackery-apis-2: flowing keyboard for tabbar

--- src/main/js/ephox/alloy/ui/composite/TabbarSpec.js
+++ src/main/js/ephox/alloy/ui/composite/TabbarSpec.js
@@ -2,10 +2,10 @@ define(
   'ephox.alloy.ui.composite.TabbarSpec',
 
   [
-
+    'ephox.alloy.api.behaviour.Highlighting'
   ],
 
-  function () {
+  function (Highlighting) {
     var make = function (detail, components, spec, externals) {
       return {
         uiType: 'custom',
@@ -19,11 +19,60 @@ define(
 
         behaviours: {
           highlighting: {
-            highlightClass: 'demo-selected-tab',
-            itemClass: 'demo-tab'
+            highlightClass: detail.markers().selectedClass(),
+            itemClass: detail.markers().tabClass()
+          },
+
+          keying: {
+            mode: 'flow',
+            getInitial: function (tabbar) {
+              // Restore focus to the previously highlighted tab.
+              return Highlighting.getHighlighted(tabbar).map(function (tab) {
+                return tab.element();
+              });
+            },
+            selector: '.' + detail.markers().tabClass(),
+            executeOnMove: true
           }
         }
       };
+
+
+
+      // return {
+      //   uid: detail.uid(),
+      //   uiType: 'custom',
+      //   dom: detail.dom(),
+      //   highlighting: {
+      //     highlightClass: detail.markers().selectedClass(),
+      //     itemClass: detail.markers().tabClass()
+      //   },
+      //   components: components,
+      //   keying: {
+      //     mode: 'flow',
+      //     getInitial: function (tabbar) {
+      //       // Restore focus to the previously highlighted tab.
+      //       return Highlighting.getHighlighted(tabbar).map(function (tab) {
+      //         return tab.element();
+      //       });
+      //     },
+      //     selector: '.' + detail.markers().tabClass(),
+      //     executeOnMove: true
+      //   },
+        
+      //   apis: {
+      //     selectFirst: function (tabbar) {
+      //       Highlighting.getFirst(tabbar).each(function (button) {
+      //         tabbar.getSystem().triggerEvent(SystemEvents.execute(), button.element(), { });
+      //       });
+      //     }
+      //   },
+
+
+      //   tabstopping: true
+      // };
+
+
     };
 
     return {
