8e71aba31d50f44cf1c6ff26172e1c6151acf936
morgan <morgan.smith@ephox.com>
2016-12-08 17:13:28 +1000

    VAN-44-wheel-hackery-apis-2: asserting structure

--- src/test/js/browser/ui/tabs/TabSectionTest.js
+++ src/test/js/browser/ui/tabs/TabSectionTest.js
@@ -2,13 +2,16 @@ asynctest(
   'TabSection Test',
  
   [
+    'ephox.agar.api.ApproxStructure',
+    'ephox.agar.api.Assertions',
     'ephox.agar.api.Step',
+    'ephox.agar.assertions.ApproxStructures',
     'ephox.alloy.api.GuiFactory',
     'ephox.alloy.api.ui.TabSection',
     'ephox.alloy.test.GuiSetup'
   ],
  
-  function (Step, GuiFactory, TabSection, GuiSetup) {
+  function (ApproxStructure, Assertions, Step, ApproxStructures, GuiFactory, TabSection, GuiSetup) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
@@ -41,6 +44,14 @@ asynctest(
 
     }, function (doc, body, gui, component, store) {
       return [
+        Assertions.sAssertStructure('Checking initial tab section', ApproxStructure.build(function (s, str, arr) {
+          return s.element('div', {
+            children: [
+              s.element('div', { }),
+              s.element('div', { })
+            ]
+          });
+        }), component.element()),
         Step.fail('done')
       ];
     }, function () { success(); }, failure);
