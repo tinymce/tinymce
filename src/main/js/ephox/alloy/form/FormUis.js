f852313157c75903b59703418c5c681f45c631b3
morgan <morgan.smith@ephox.com>
2016-12-09 13:20:14 +1000

    VAN-44-wheel-hackery-apis-2: cleaning out old form files

--- src/main/js/ephox/alloy/form/FormUis.js
+++ src/main/js/ephox/alloy/form/FormUis.js
@@ -4,23 +4,17 @@ define(
   [
     'ephox.alloy.form.CoupledTextInputSpec',
     'ephox.alloy.form.CustomFieldSpec',
-    'ephox.alloy.form.CustomRadioGroupSpec',
-    'ephox.alloy.form.RadioGroupSpec',
     'ephox.alloy.form.SelectInputSpec',
     'ephox.alloy.form.TextInputSpec',
     'ephox.boulder.api.ValueSchema',
     'ephox.peanut.Fun'
   ],
 
-  function (CoupledTextInputSpec, CustomFieldSpec, CustomRadioGroupSpec, RadioGroupSpec, SelectInputSpec, TextInputSpec, ValueSchema, Fun) {
+  function (CoupledTextInputSpec, CustomFieldSpec, SelectInputSpec, TextInputSpec, ValueSchema, Fun) {
     var schema = ValueSchema.choose(
       'type',
       {
         'coupled-text-input': CoupledTextInputSpec,
-        'text-input': TextInputSpec.schema(),
-        'radio-group': RadioGroupSpec,
-        'custom-radio-group': CustomRadioGroupSpec,
-        'select-input': SelectInputSpec,
         'custom-field': CustomFieldSpec
       }
     );
