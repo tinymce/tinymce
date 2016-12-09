11310a0902ec6672a43fe2940b174ae237ff9ff8
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2016-12-09 17:21:56 +0100

    GH-3371: review fix, change regexp

--- js/tinymce/classes/Formatter.js
+++ js/tinymce/classes/Formatter.js
@@ -2256,7 +2256,7 @@ define("tinymce/Formatter", [
 				}
 
 				// Expand to word if caret is in the middle of a text node and the char before/after is a alpha numeric character
-				var wordcharRegex = /^[A-Za-z0-9\u00C0-\u017F]+$/;
+				var wordcharRegex = /[^\s\u00a0\u00ad\u200b\ufeff]/;
 				if (text && offset > 0 && offset < text.length &&
 					wordcharRegex.test(text.charAt(offset)) && wordcharRegex.test(text.charAt(offset - 1))) {
 					// Get bookmark of caret position
