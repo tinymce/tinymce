bd22f35f9508459341807ea66edd2e8c08d173ab
morgan <morgan.smith@ephox.com>
2016-12-13 09:19:31 +1000

    VAN-44-wheel-hackery-apis-2: basic modal dialog scaffold

--- src/main/js/ephox/alloy/api/ui/ModalDialog.js
+++ src/main/js/ephox/alloy/api/ui/ModalDialog.js
@@ -2,30 +2,57 @@ define(
   'ephox.alloy.api.ui.ModalDialog',
 
   [
-    'ephox.alloy.api.ui.CompositeBuilder'
+    'ephox.alloy.api.ui.CompositeBuilder',
+    'ephox.alloy.parts.PartType',
+    'ephox.peanut.Fun',
+    'ephox.sugar.api.SelectorFind'
   ],
 
-  function (CompositeBuilder) {
+  function (CompositeBuilder, PartType, Fun, SelectorFind) {
     var schema = [
       
     ];
 
-     var partTypes = [
-     
+    var basic = { build: Fun.identity };
+
+    var partTypes = [
+      PartType.optional(basic, 'handle', '<alloy.dialog.draghandle>', Fun.constant({}), 
+        function (detail) {
+          return {
+            behaviours: {
+              dragging: {
+                mode: 'mouse',
+                getTarget: function (handle) {
+                  return SelectorFind.ancestor(handle, '[role="dialog"]').getOr(handle);
+                }
+              }
+            }
+          };
+        }
+      ),
+      PartType.internal(basic, 'title', '<alloy.dialog.title>', Fun.constant({}), Fun.constant({})),
+      PartType.internal(basic, 'close', '<alloy.dialog.close>', Fun.constant({}), Fun.constant({})),
+      PartType.internal(basic, 'body', '<alloy.dialog.body>', Fun.constant({}), Fun.constant({})),
+      PartType.internal(basic, 'footer', '<alloy.dialog.footer>', Fun.constant({}), Fun.constant({}))
     ];
 
     var build = function (spec) {
       return CompositeBuilder.build('modal-dialog', schema, partTypes, make, spec);
     };
 
+    var parts = PartType.generate('modal-dialog', partTypes);
+
     var make = function (detail, components, spec, externals) {
       return {
-        uiType: 'container'
+        uiType: 'container',
+        dom: detail.dom(),
+        components: components
       };
     };
 
     return {
-      build: build
+      build: build,
+      parts: Fun.constant(parts)
     };
   }
 );
--- src/test/js/browser/ui/dialog/ModalDialogTest.js
+++ src/test/js/browser/ui/dialog/ModalDialogTest.js
@@ -2,23 +2,61 @@ asynctest(
   'ModalDialogTest',
  
   [
+    'ephox.agar.api.Step',
     'ephox.alloy.api.GuiFactory',
     'ephox.alloy.api.ui.ModalDialog',
     'ephox.alloy.test.GuiSetup'
   ],
  
-  function (GuiFactory, ModalDialog, GuiSetup) {
+  function (Step, GuiFactory, ModalDialog, GuiSetup) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
     GuiSetup.setup(function (store, doc, body) {
       return GuiFactory.build(
-        ModalDialog.build({ })
+        ModalDialog.build({
+          dom: {
+            tag: 'div'
+          },
+          components: [
+            ModalDialog.parts().title(),
+            ModalDialog.parts().close(),
+            ModalDialog.parts().body(),
+            ModalDialog.parts().footer()
+          ],
+
+          parts: {
+            title: {
+              dom: {
+                tag: 'div'
+              },
+              components: [ ]
+            },
+            close: {
+              dom: {
+                tag: 'div'
+              },
+              components: [ ]
+            },
+            body: {
+              dom: {
+                tag: 'div'
+              },
+              components: [ ]
+            },
+            footer: {
+              dom: {
+                tag: 'div'
+              },
+              components: [ ]
+            }
+          }
+        })
       );
 
     }, function (doc, body, gui, component, store) {
       return [
-        
+        Step.fail('fake')
       ];
     }, function () { success(); }, failure);
 
