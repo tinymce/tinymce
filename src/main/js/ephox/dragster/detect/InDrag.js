98143cb27817bfed8d92e232d9a75f0bb15926b6
Mike Pheasant <mike.pheasant@ephox.com>
2016-12-14 09:35:23 +1000

    TBIO-4678: add comment on dragster move event mechanism

--- src/main/js/ephox/dragster/detect/InDrag.js
+++ src/main/js/ephox/dragster/detect/InDrag.js
@@ -16,6 +16,8 @@ define(
         previous = Option.none();
       };
 
+      // Return position delta between previous position and nu position, 
+      // or None if this is the first. Set the previous position to nu.
       var update = function (mode, nu) {
         var result = previous.map(function (old) {
           return mode.compare(old, nu);
@@ -28,6 +30,8 @@ define(
       var onEvent = function (event, mode) {
         var dataOption = mode.extract(event);
 
+        // Dragster move events require a position delta. The moveevent is only triggered
+        // on the second and subsequent dragster move events. The first is dropped.
         dataOption.each(function (data) {
           var offset = update(mode, data);
           offset.each(function (d) {
