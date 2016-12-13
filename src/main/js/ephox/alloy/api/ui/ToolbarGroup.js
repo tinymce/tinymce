27baae71b78801c9ee0f2e1376503eb4740d1f1b
morgan <morgan.smith@ephox.com>
2016-12-14 08:56:44 +1000

    VAN-44-wheel-hackery-apis-2: toolbar roles

--- src/main/js/ephox/alloy/api/ui/ToolbarGroup.js
+++ src/main/js/ephox/alloy/api/ui/ToolbarGroup.js
@@ -2,73 +2,45 @@ define(
   'ephox.alloy.api.ui.ToolbarGroup',
 
   [
+    'ephox.alloy.api.ui.CompositeBuilder',
     'ephox.alloy.data.Fields',
     'ephox.alloy.parts.PartType',
-    'ephox.alloy.registry.Tagger',
-    'ephox.alloy.spec.SpecSchema',
-    'ephox.alloy.spec.UiSubstitutes',
     'ephox.boulder.api.FieldSchema',
     'ephox.highway.Merger',
     'ephox.peanut.Fun',
     'global!Error'
   ],
 
-  function (Fields, PartType, Tagger, SpecSchema, UiSubstitutes, FieldSchema, Merger, Fun, Error) {
+  function (CompositeBuilder, Fields, PartType, FieldSchema, Merger, Fun, Error) {
     var schema = [
       FieldSchema.strict('items'),
-      FieldSchema.defaulted('shell', false),
       Fields.members([ 'item' ])
     ];
 
-    var makeItems = function (detail) {
-      return {
-        behaviours: {
-          replacing: { }
-        }
-      };
-    };
-
     var partTypes = [
-      PartType.group({ build: Fun.identity }, 'items', 'item', '<alloy.toolbar-group.items>', Fun.constant({ }), makeItems)
+      PartType.group({ build: Fun.identity }, 'items', 'item', '<alloy.toolbar-group.items>', Fun.constant({ }), Fun.constant({ }))
     ];
 
-    var make = function (detail, spec) {
-      var structure = detail.shell() ? (function () {
-        // First level is the container.
-        // Therefore merege all "items" part information into the base
-        // Generate the components
-        var ps = PartType.placeholders('toolbar-group', detail, partTypes);
-        console.log('ps', ps);
-        return {
-          components: UiSubstitutes.singleReplace(detail, ps['<alloy.toolbar-group.items>']),
-          base: Merger.deepMerge(
-            detail.parts().items(),
-            makeItems(detail)
-          )
-        };
-      })() : (function () {
-        var comps = PartType.components('toolbar-group', detail, partTypes);
-        return {
-          components: comps,
-          base: { }
-        };
-      })();
-      // TODO: make shell work.
-      // return detail.shell() ? Merger.deepMerge(
-
-      // );
-      return Merger.deepMerge(structure.base, {
-        uiType: 'custom',
-        uid: detail.uid(),
-        dom: detail.dom(),
-        components: structure.components
-      });
+    var make = function (detail, components, spec, _externals) {
+      return Merger.deepMerge(
+        {
+          dom: {
+            attributes: {
+              role: 'toolbar'
+            }
+          }
+        },
+        {
+          uiType: 'custom',
+          uid: detail.uid(),
+          dom: detail.dom(),
+          components: components
+        }
+      );
     };
 
     var build = function (spec) {
-      var rawUiSpec = Merger.deepMerge({ uid: Tagger.generate('') }, spec);
-      var uiSpec = SpecSchema.asStructOrDie('Menu', schema, rawUiSpec, [ 'items' ]);
-      return make(uiSpec, rawUiSpec);
+      return CompositeBuilder.build('toolbar-group', schema, partTypes, make, spec);
     };
 
     // TODO: Remove likely dupe
--- src/test/js/browser/ui/toolbar/ToolbarGroupTest.js
+++ src/test/js/browser/ui/toolbar/ToolbarGroupTest.js
@@ -7,114 +7,63 @@ asynctest(
     'ephox.agar.api.Step',
     'ephox.alloy.api.GuiFactory',
     'ephox.alloy.api.ui.ToolbarGroup',
-    'ephox.alloy.test.GuiSetup',
-    'ephox.peanut.Fun'
+    'ephox.alloy.test.GuiSetup'
   ],
  
-  function (ApproxStructure, Assertions, Step, GuiFactory, ToolbarGroup, GuiSetup, Fun) {
+  function (ApproxStructure, Assertions, Step, GuiFactory, ToolbarGroup, GuiSetup) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
     GuiSetup.setup(function (store, doc, body) {
       return GuiFactory.build(
-        {
-          uiType: 'container',
+        ToolbarGroup.build({
+          dom: {
+            tag: 'div'
+          },
           components: [
-            ToolbarGroup.build({
-              uid: 'nested-toolbar',
-              dom: {
-                tag: 'div'
-              },
-              components: [
-                ToolbarGroup.parts().items()
-              ],
+            ToolbarGroup.parts().items()
+          ],
 
-              items: [ { data: { value: 'a', text: 'A' } }, { data: { value: 'b', text: 'B' }} ],
+          items: [ { data: { value: 'a', text: 'A' } }, { data: { value: 'b', text: 'B' }} ],
 
-              members: {
-                item: {
-                  munge: function (itemSpec) {
-                    return {
-                      uiType: 'custom',
-                      dom: {
-                        tag: 'button',
-                        innerHtml: itemSpec.data.text
-                      }
-                    };
-                  }
-                }
-              },
-
-              parts: {
-                items: {
+          members: {
+            item: {
+              munge: function (itemSpec) {
+                return {
+                  uiType: 'custom',
                   dom: {
-                    tag: 'div',
-                    classes: [ 'group-items' ]
+                    tag: 'button',
+                    innerHtml: itemSpec.data.text
                   }
-                }
+                };
               }
-            }),
+            }
+          },
 
-            ToolbarGroup.build({
-              uid: 'shell-toolbar',
-              dom: { tag: 'div' },
-              shell: true,
-              components: [ ],
-              items: [ { data: { value: 'a', text: 'A' } }, { data: { value: 'b', text: 'B' }} ],
-              members: {
-                item: {
-                  munge: function (itemSpec) {
-                    return {
-                      uiType: 'custom',
-                      dom: {
-                        tag: 'button',
-                        innerHtml: itemSpec.data.text
-                      }
-                    };
-                  }
-                }
-              },
-
-              parts: { items: { dom: { tag: 'div', classes: [ 'group-items' ] } } }
-            })
-          ]
-        }
+          parts: {
+            items: {
+              dom: {
+                tag: 'div',
+                classes: [ 'group-items' ]
+              }
+            }
+          }
+        })
       );
 
     }, function (doc, body, gui, component, store) {
-      var shelled = component.getSystem().getByUid('shell-toolbar').getOrDie();
-      var nested = component.getSystem().getByUid('nested-toolbar').getOrDie();
-
       return [
         Assertions.sAssertStructure(
           'Checking initial toolbar groups',
           ApproxStructure.build(function (s, str, arr) {
             return s.element('div', {
+              classes: [ arr.not('group-items') ],
+              attrs: {
+                role: str.is('toolbar')
+              },
               children: [
-                s.element('div', {
-                  attrs: {
-                    'data-alloy-id': str.is('nested-toolbar')
-                  },
-                  classes: [ arr.not('group-items') ],
-                  children: [
-                    s.element('div', {
-                      children: [
-                        s.element('button', { html: str.is('A') }),
-                        s.element('button', { html: str.is('B') })
-                      ]
-                    })
-                  ]
-                }),
-                s.element('div', {
-                  attrs: {
-                    'data-alloy-id': str.is('shell-toolbar')
-                  },
-                  classes: [ arr.has('group-items') ],
-                  children: [
-                    s.element('button', { html: str.is('A') }),
-                    s.element('button', { html: str.is('B') })
-                  ]
-                })
+                s.element('button', { html: str.is('A') }),
+                s.element('button', { html: str.is('B') })
               ]
             });
           }),
