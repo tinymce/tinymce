b8e295389af0bf6f2d0906f1ff9b81b8b743e701
morgan <morgan.smith@ephox.com>
2016-12-14 11:05:26 +1000

    VAN-44-wheel-hackery-apis-2: asserting initial structure of toolbar

--- src/test/js/browser/ui/toolbar/BasicToolbarTest.js
+++ src/test/js/browser/ui/toolbar/BasicToolbarTest.js
@@ -2,13 +2,15 @@ asynctest(
   'BasicToolbarTest',
  
   [
+    'ephox.agar.api.ApproxStructure',
+    'ephox.agar.api.Assertions',
     'ephox.agar.api.Step',
     'ephox.alloy.api.GuiFactory',
     'ephox.alloy.api.ui.Toolbar',
     'ephox.alloy.test.GuiSetup'
   ],
  
-  function (Step, GuiFactory, Toolbar, GuiSetup) {
+  function (ApproxStructure, Assertions, Step, GuiFactory, Toolbar, GuiSetup) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
@@ -23,7 +25,13 @@ asynctest(
 
     }, function (doc, body, gui, component, store) {
       return [
-        Step.fail('toolbar.test')
+        Assertions.sAssertStructure(
+          'Checking initial structure of toolbar',
+          ApproxStructure.build(function (s, str, arr) {
+            return s.element('div', { });
+          }),
+          component.element()
+        )
       ];
     }, function () { success(); }, failure);
 
