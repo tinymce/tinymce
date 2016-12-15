34362574f9ce8588c480b0a194281278522f1ecf
morgan <morgan.smith@ephox.com>
2016-12-16 07:57:03 +1000

    VAN-44-wheel-hackery-apis-2: added buttons and dropdown to inline menu

--- src/test/js/browser/ui/inline/InlineViewTest.js
+++ src/test/js/browser/ui/inline/InlineViewTest.js
@@ -2,17 +2,23 @@ asynctest(
   'InlineViewTest',
  
   [
+    'ephox.agar.api.Logger',
     'ephox.agar.api.Step',
     'ephox.agar.api.UiFinder',
     'ephox.agar.api.Waiter',
     'ephox.alloy.api.GuiFactory',
+    'ephox.alloy.api.ui.Button',
+    'ephox.alloy.api.ui.Dropdown',
     'ephox.alloy.api.ui.InlineView',
+    'ephox.alloy.api.ui.menus.MenuData',
     'ephox.alloy.test.GuiSetup',
     'ephox.alloy.test.Sinks',
+    'ephox.alloy.test.dropdown.TestDropdownMenu',
+    'ephox.knoch.future.Future',
     'ephox.perhaps.Result'
   ],
  
-  function (Step, UiFinder, Waiter, GuiFactory, InlineView, GuiSetup, Sinks, Result) {
+  function (Logger, Step, UiFinder, Waiter, GuiFactory, Button, Dropdown, InlineView, MenuData, GuiSetup, Sinks, TestDropdownMenu, Future, Result) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
@@ -63,7 +69,51 @@ asynctest(
           UiFinder.sNotExists(gui.element(), '.test-inline'),
           100,
           1000
-        )
+        ),
+
+        Logger.t(
+          'Show inline view again, this time with buttons',
+          Step.sync(function () {
+            InlineView.showAt(inline, {
+              anchor: 'selection',
+              root: gui.element()
+            }, {
+              uiType: 'custom',
+              dom: {
+                tag: 'div'
+              },
+              components: [
+                Button.build({ uid: 'bold-button', dom: { tag: 'button', innerHtml: 'B' }, action: store.adder('bold') }),
+                Button.build({ uid: 'italic-button', dom: { tag: 'button', innerHtml: 'I' }, action: store.adder('italic') }),
+                Button.build({ uid: 'underline-button', dom: { tag: 'button', innerHtml: 'U' }, action: store.adder('underline') }),
+                Dropdown.build({
+                  dom: {
+                    tag: 'button',
+                    innerHtml: '+'
+                  },
+                  components: [ ],
+                  lazySink: function () { return Result.value(component); },
+                  parts: {
+                    menu: TestDropdownMenu(store)
+                  },
+                  fetch: function () {
+                    var future = Future.pure([
+                      { type: 'item', data: { value: 'option-1', text: 'Option-1' } },
+                      { type: 'item', data: { value: 'option-2', text: 'Option-2' } }
+                    ]);
+
+                    return future.map(function (f) {
+                      return MenuData.simple('test', 'Test', f);
+                    });
+                  }
+                })
+              ]
+            });
+          })
+        ),
+
+        Step.fail('in progress')
+
       ];
     }, function () { success(); }, failure);
 
