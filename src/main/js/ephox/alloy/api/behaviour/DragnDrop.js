7794055ea679f3da7f4c5a35a8112e92471549be
morgan <morgan.smith@ephox.com>
2016-12-22 12:29:05 +1000

    VAN-44-wheel-hackery-apis-2: button dragging

--- src/main/js/ephox/alloy/api/behaviour/DragnDrop.js
+++ src/main/js/ephox/alloy/api/behaviour/DragnDrop.js
@@ -12,6 +12,12 @@ define(
       DragnDropBranches,
       'dragndrop',
       {
+        exhibit: function (base, dragInfo) {
+          var instance = dragInfo.instance();
+          return instance.exhibit(base, dragInfo);
+        },
+
+
         events: function (dragInfo) {
           var instance = dragInfo.instance();
           return instance.handlers(dragInfo);
--- src/main/js/ephox/alloy/behaviour/dragndrop/DragStarting.js
+++ src/main/js/ephox/alloy/behaviour/dragndrop/DragStarting.js
@@ -2,18 +2,33 @@ define(
   'ephox.alloy.behaviour.dragndrop.DragStarting',
 
   [
+    'ephox.alloy.dom.DomModification',
     'ephox.boulder.api.FieldSchema'
   ],
 
-  function (FieldSchema) {
+  function (DomModification, FieldSchema) {
     return [
       FieldSchema.strict('type'),
       FieldSchema.state('instance', function () {
-        var handlers = function () {
-          return { };
+
+        var exhibit = function () {
+          return DomModification.nu({
+            attributes: {
+              draggable: 'true'
+            }
+          });
+        };
+
+        var handlers = function (dragInfo) {
+          return {
+            'dragstart': function () {
+              debugger
+            }
+          };
         };
 
         return {
+          exhibit: exhibit,
           handlers: handlers
         };
       })
