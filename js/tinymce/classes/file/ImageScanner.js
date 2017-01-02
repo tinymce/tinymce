5a7c5aba1bb7bfe0958fa5431954170ec7eca3d3
Davit Barbakadze <davit.barbakadze@ephox.com>
2017-01-02 22:11:20 +0400

    TINY-807: little cleanup

--- js/tinymce/classes/file/ImageScanner.js
+++ js/tinymce/classes/file/ImageScanner.js
@@ -23,6 +23,10 @@ define("tinymce/file/ImageScanner", [
 ], function(Promise, Arr, Fun, Conversions, Env) {
 	var count = 0;
 
+	var uniqueId = function(prefix) {
+		return (prefix || 'blobid') + (count++);
+	};
+
 	return function(uploadStatus, blobCache) {
 		var cachedPromises = {};
 
@@ -44,9 +48,7 @@ define("tinymce/file/ImageScanner", [
 						Conversions.uriToBlob(img.src).then(function (blob) {
 							Conversions.blobToDataUri(blob).then(function (dataUri) {
 								base64 = Conversions.parseDataUri(dataUri).data;
-								var blobInfoId = 'blobid' + (count++),
-									blobInfo = blobCache.create(blobInfoId, blob, base64);
-
+								blobInfo = blobCache.create(uniqueId(), blob, base64);
 								blobCache.add(blobInfo);
 
 								resolve({
@@ -72,9 +74,7 @@ define("tinymce/file/ImageScanner", [
 					});
 				} else {
 					Conversions.uriToBlob(img.src).then(function(blob) {
-						var blobInfoId = 'blobid' + (count++),
-							blobInfo = blobCache.create(blobInfoId, blob, base64);
-
+						blobInfo = blobCache.create(uniqueId(), blob, base64);
 						blobCache.add(blobInfo);
 
 						resolve({
