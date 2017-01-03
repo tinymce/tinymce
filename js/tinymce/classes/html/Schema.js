707de8070df2fe6752c2cbc69c47382c78730b26
Spocke <spocke@moxiecode.com>
2017-01-03 10:06:21 +0100

    TINY-835: added code and pre as non empties

--- js/tinymce/classes/html/Schema.js
+++ js/tinymce/classes/html/Schema.js
@@ -388,7 +388,8 @@ define("tinymce/html/Schema", [
 		'meta param embed source wbr track');
 		boolAttrMap = createLookupTable('boolean_attributes', 'checked compact declare defer disabled ismap multiple nohref noresize ' +
 		'noshade nowrap readonly selected autoplay loop controls');
-		nonEmptyElementsMap = createLookupTable('non_empty_elements', 'td th iframe video audio object script', shortEndedElementsMap);
+		nonEmptyElementsMap = createLookupTable('non_empty_elements', 'td th iframe video audio object ' +
+		'script pre code', shortEndedElementsMap);
 		moveCaretBeforeOnEnterElementsMap = createLookupTable('move_caret_before_on_enter_elements', 'table', nonEmptyElementsMap);
 		textBlockElementsMap = createLookupTable('text_block_elements', 'h1 h2 h3 h4 h5 h6 p div address pre form ' +
 		'blockquote center dir fieldset header footer article section hgroup aside nav figure');
