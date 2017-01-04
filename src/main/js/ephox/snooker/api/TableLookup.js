2ca5689efbd36c89424324c484f28de43fc4be28
Maurizio Napoleoni <napy84@gmail.com>
2017-01-04 11:53:47 +1000

    TBIO-4684: The table might be nested and we need to get the cell down if the table element is found

--- src/main/js/ephox/snooker/api/TableLookup.js
+++ src/main/js/ephox/snooker/api/TableLookup.js
@@ -11,20 +11,38 @@ define(
     'ephox.sugar.api.Node',
     'ephox.sugar.api.SelectorFilter',
     'ephox.sugar.api.SelectorFind',
+    'ephox.sugar.api.Selectors',
     'ephox.sugar.api.Traverse',
     'global!parseInt'
   ],
 
-  function (Arr, Fun, Option, Structs, LayerSelector, Attr, Node, SelectorFilter, SelectorFind, Traverse, parseInt) {
+  function (Arr, Fun, Option, Structs, LayerSelector, Attr, Node, SelectorFilter, SelectorFind, Selectors, Traverse, parseInt) {
+    // This one has to find the closest
     var lookup = function (tags, element) {
       return Arr.contains(tags, Node.name(element)) ? Option.some(element) : SelectorFind.ancestor(element, tags.join(','));
+    };
+
+    var lookupOrDown = function (tags, element, invertSearch) {
+      if(Arr.contains(tags, Node.name(element))) {
+        // The tag is already of the specified type
+        return Option.some(element);
+      } else {
+
+        var isRoot = function (element) {
+          return Selectors.is(element, 'table');
+        };
+
+        return SelectorFind.ancestor(element, tags.join(','), isRoot).orThunk(function () {
+          return SelectorFind.descendant(element, tags.join(','));
+        });
+      }
     };
 
     /*
      * Identify the optional cell that element represents.
      */
     var cell = function (element) {
-      return lookup([ 'td', 'th' ], element);
+      return lookupOrDown([ 'td', 'th' ], element);
     };
 
     var cells = function (ancestor) {
