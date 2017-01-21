e823f290690af30885e387ff72998857d7e2e547
morgan <morgan.smith@ephox.com>
2017-01-21 06:48:31 +0100

    VAN-44: compare size

--- bin/compare-size.sh
+++ bin/compare-size.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
-echo "Building normally" &&
-bolt build -i -n ephox.alloy.docs.Everything -e src/docs/js/ephox/alloy/docs/Everything.js -c config/bolt/doc.js  &&
+# Usage bin/compare-size.sh ${CONFIG} ${NAMESPACE} ${LOCATION}
 
-mv scratch/main/js/inline/ephox.alloy.docs.Everything.js scratch/main/js/inline/ephox.alloy.docs.Everything.max.js &&
+CONFIG=$1
+NAMESPACE=$2
+LOCATION=$3
+
+echo "Building normally" &&
+bolt build -i -n $NAMESPACE -e $LOCATION -c $CONFIG  &&
+
+mv scratch/main/js/inline/$NAMESPACE.js scratch/main/js/inline/$NAMESPACE.max.js &&
 
 echo "Building with shortened module names"
 
-bolt build -i -n ephox.alloy.docs.Everything -e src/docs/js/ephox/alloy/docs/Everything.js -c config/bolt/doc.js -a &&
+bolt build -i -n $NAMESPACE -e $LOCATION -c $CONFIG -a &&
 
 echo "Minifying"
 
-shrink scratch/main/js/inline/ephox.alloy.docs.Everything.js &&
+shrink scratch/main/js/inline/$NAMESPACE.js &&
 
 echo "Gzipping"
 
-gzip -k scratch/main/js/inline/ephox.alloy.docs.Everything.min.js &&
+# -f overwrite file, -k keep the min.js file as well
+gzip -f -k scratch/main/js/inline/$NAMESPACE.min.js &&
 
 echo "Showing sizes ..." && 
 
