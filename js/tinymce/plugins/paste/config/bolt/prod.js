59eadd6f60e40bbffefbf973bf97dc96c0796b47
Spocke <spocke@moxiecode.com>
2017-01-31 10:28:23 +0100

    TM-1: boltified paste plugin

---
+++ js/tinymce/plugins/paste/config/bolt/prod.js
@@ -0,0 +1,5 @@
+configure({
+  sources: [
+    source('amd', 'tinymce.paste', '../../src/main/js', mapper.hierarchical)
+  ]
+});
---
+++ js/tinymce/plugins/paste/src/demo/js/tinymce/paste/demo/Demo.js
@@ -0,0 +1,33 @@
+/**
+ * Demo.js
+ *
+ * Released under LGPL License.
+ * Copyright (c) 1999-2017 Ephox Corp. All rights reserved
+ *
+ * License: http://www.tinymce.com/license
+ * Contributing: http://www.tinymce.com/contributing
+ */
+
+/*eslint no-console:0 */
+
+define(
+	'tinymce.paste.demo.Demo',
+
+	[
+		'tinymce.paste.Plugin',
+		'global!tinymce'
+	],
+
+	function (Plugin, tinymce) {
+		return function () {
+
+			tinymce.init({
+				selector: "textarea.tinymce",
+				theme: "modern",
+				plugins: "paste code preview",
+				toolbar: "pastetext code preview",
+				height: 600
+			});
+		};
+	}
+);
