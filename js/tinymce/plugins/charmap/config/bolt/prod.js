b75f0855bdfb32e7cc902b8ba32a2d15b9bed9e5
morgan <morgan.smith@ephox.com>
2017-01-31 14:23:42 +1000

    TM-1: charmap

---
+++ js/tinymce/plugins/charmap/config/bolt/prod.js
@@ -0,0 +1,5 @@
+configure({
+  sources: [
+    source('amd', 'tinymce.charmap', '../../src/main/js', mapper.hierarchical)
+  ]
+});
---
+++ js/tinymce/plugins/charmap/src/demo/js/tinymce/charmap/demo/Demo.js
@@ -0,0 +1,33 @@
+/**
+ * Demo.js
+ *
+ * Released under LGPL License.
+ * Copyright (c) 1999-2016 Ephox Corp. All rights reserved
+ *
+ * License: http://www.tinymce.com/license
+ * Contributing: http://www.tinymce.com/contributing
+ */
+
+/*eslint no-console:0 */
+
+define(
+	'tinymce.charmap.demo.Demo',
+
+	[
+		'tinymce.charmap.Plugin',
+		'global!tinymce'
+	],
+
+	function (Plugin, tinymce) {
+		return function () {
+
+			tinymce.init({
+				selector: "textarea.tinymce",
+				theme: "modern",
+				plugins: "charmap code preview",
+				toolbar: "charmap code preview",
+				height: 600
+			});
+		};
+	}
+);
