b240780e5196d5ed1b6fc964f9435966768fc80f
morgan <morgan.smith@ephox.com>
2017-01-31 15:33:35 +1000

    TM-1: contextmenu

---
+++ js/tinymce/plugins/contextmenu/config/bolt/prod.js
@@ -0,0 +1,5 @@
+configure({
+  sources: [
+    source('amd', 'tinymce.contextmenu', '../../src/main/js', mapper.hierarchical)
+  ]
+});
---
+++ js/tinymce/plugins/contextmenu/src/demo/js/tinymce/contextmenu/demo/Demo.js
@@ -0,0 +1,35 @@
+/**
+ * Demo.js
+ *
+ * Released under LGPL License.
+ * Copyright (c) 1999-2016 Ephox Corp. All rights reserved
+ *
+ * License: http://www.tinymce.com/license
+ * Contributing: http://www.tinymce.com/contributing
+ */
+
+/*eslint no-console:0 */
+
+define(
+	'tinymce.contextmenu.demo.Demo',
+
+	[
+		'tinymce.contextmenu.Plugin',
+		'global!tinymce'
+	],
+
+	function (Plugin, tinymce) {
+		return function () {
+
+			tinymce.init({
+				selector: "textarea.tinymce",
+				theme: "modern",
+				plugins: "contextmenu code preview",
+				toolbar: "contextmenu code preview",
+				height: 600,
+
+				contextmenu: 'code preview'
+			});
+		};
+	}
+);
