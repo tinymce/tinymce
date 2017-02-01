b3605c1f92aefd128203d284dddb49353d5d3187
Spocke <spocke@moxiecode.com>
2017-02-01 12:28:30 +0100

    fixed broken media test

--- js/tinymce/plugins/media/src/test/js/browser/ReopenResizeTest.js
+++ js/tinymce/plugins/media/src/test/js/browser/ReopenResizeTest.js
@@ -11,10 +11,11 @@ asynctest('browser.core.PluginTest', [
 	'ephox.agar.api.Step',
 	'ephox.agar.api.Waiter',
 	'ephox.agar.api.ApproxStructure',
+	'ephox.agar.api.RawAssertions',
 	'tinymce.media.test.Utils'
 ], function (
 	tinymce, Plugin, TinyLoader, TinyDom, TinyApis,
-	TinyUi, Pipeline, UiFinder, FocusTools, Step, Waiter, ApproxStructure, Utils
+	TinyUi, Pipeline, UiFinder, FocusTools, Step, Waiter, ApproxStructure, RawAssertions, Utils
 ) {
 	var success = arguments[arguments.length - 2];
 	var failure = arguments[arguments.length - 1];
@@ -37,6 +38,12 @@ asynctest('browser.core.PluginTest', [
 		});
 	};
 
+	var sWaitForResizeHandles = function (editor) {
+		return Waiter.sTryUntil('Wait for new width value', Step.sync(function () {
+			RawAssertions.assertEq('Resize handle should exist', editor.dom.select('#mceResizeHandlenw').length, 1);
+		}), 1, 3000);
+	};
+
 	TinyLoader.setup(function (editor, onSuccess, onFailure) {
 		var ui = TinyUi(editor);
 		var apis = TinyApis(editor);
@@ -44,12 +51,13 @@ asynctest('browser.core.PluginTest', [
 		Pipeline.async({}, [
 			Utils.sOpenDialog(ui),
 			Utils.sPasteSourceValue(ui, 'a'),
-			Step.wait(10),
+			Utils.sAssertWidthValue(ui, '300'),
 			ui.sClickOnUi('Click on close button', 'button:contains("Ok")'),
+			sWaitForResizeHandles(editor),
 			Utils.sOpenDialog(ui),
 			Utils.sChangeWidthValue(ui, '500'),
-			Step.wait(10),
 			ui.sClickOnUi('Click on close button', 'button:contains("Ok")'),
+			sWaitForResizeHandles(editor),
 			apis.sAssertContentStructure(videoWithWidth('500'))
 		], onSuccess, onFailure);
 	}, {
