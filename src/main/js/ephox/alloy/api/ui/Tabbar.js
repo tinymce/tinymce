7793d9cfa01984d7bc616ee28e80e532ab261a77
morgan <morgan.smith@ephox.com>
2017-02-07 11:28:59 +1000

    VAN-44-docs: tabbar schema

--- src/main/js/ephox/alloy/api/ui/Tabbar.js
+++ src/main/js/ephox/alloy/api/ui/Tabbar.js
@@ -2,85 +2,22 @@ define(
   'ephox.alloy.api.ui.Tabbar',
 
   [
-    'ephox.alloy.api.events.SystemEvents',
     'ephox.alloy.api.behaviour.Behaviour',
     'ephox.alloy.api.behaviour.Highlighting',
+    'ephox.alloy.api.events.SystemEvents',
     'ephox.alloy.api.ui.TabButton',
     'ephox.alloy.api.ui.UiBuilder',
     'ephox.alloy.data.Fields',
     'ephox.alloy.dom.DomModification',
     'ephox.alloy.parts.PartType',
+    'ephox.alloy.ui.schema.TabbarSchema',
     'ephox.boulder.api.FieldSchema',
     'ephox.peanut.Fun'
   ],
 
-  function (SystemEvents, Behaviour, Highlighting, TabButton, UiBuilder, Fields, DomModification, PartType, FieldSchema, Fun) {
-    var schema = [
-      FieldSchema.strict('tabs'),
-
-      FieldSchema.strict('dom'),
-
-      Fields.members([ 'tab' ]),
-
-      FieldSchema.defaulted('clickToDismiss', false),
-
-      Fields.markers([ 'tabClass', 'selectedClass' ])
-    ];
-
-
-    var tabsPart = PartType.group(
-      TabButton,
-      'tabs',
-      'tab',
-      '<alloy.tabs>',
-      Fun.constant({ }),
-      function (barDetail, tabSpec) {
-        var dismissTab = function (tabbar, button) {
-          Highlighting.dehighlight(tabbar, button);
-          SystemEvents.trigger(tabbar, SystemEvents.dismissTab(), {
-            tabbar: tabbar,
-            button: button
-          });
-        };
-
-        var changeTab = function (tabbar, button) {
-          Highlighting.highlight(tabbar, button);
-          SystemEvents.trigger(tabbar, SystemEvents.changeTab(), {
-            tabbar: tabbar,
-            button: button
-          });
-        };
-
-        return {
-          behaviours: {
-            representing: {
-              initialValue: tabSpec.value
-            },
-            'tabbar.tabbuttons': { }
-          },
-          action: function (button) {
-            var tabbar = button.getSystem().getByUid(barDetail.uid()).getOrDie();
-            var activeButton = Highlighting.isHighlighted(tabbar, button);
-
-            var response = (function () {
-              if (activeButton && barDetail.clickToDismiss()) return dismissTab;
-              else if (! activeButton) return changeTab;
-              else return Fun.noop;
-            })();
-
-            response(tabbar, button);
-          },
-
-          domModification: {
-            classes: [ barDetail.markers().tabClass() ]
-          }
-        };
-      }
-    );
-  
-    var partTypes = [
-      tabsPart
-    ];
+  function (Behaviour, Highlighting, SystemEvents, TabButton, UiBuilder, Fields, DomModification, PartType, TabbarSchema, FieldSchema, Fun) {
+    var schema = TabbarSchema.schema();
+    var partTypes = TabbarSchema.partTypes();
 
     var make = function (detail, components, spec, externals) {
       return {
@@ -115,11 +52,11 @@ define(
 
 
     var build = function (spec) {
-      return UiBuilder.composite('tab-bar', schema, partTypes, make, spec);
+      return UiBuilder.composite(TabbarSchema.name(), schema, partTypes, make, spec);
     };
 
     // TODO: Remove likely dupe
-    var parts = PartType.generate('tab-bar', partTypes);
+    var parts = PartType.generate(TabbarSchema.name(), partTypes);
 
     return {
       build: build,
