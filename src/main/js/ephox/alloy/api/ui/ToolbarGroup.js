bfa4a4cfe471a8bae1a885c106624c50c3964b7b
morgan <morgan.smith@ephox.com>
2017-02-07 11:40:09 +1000

    VAN-44-docs: toolbar group schema

--- src/main/js/ephox/alloy/api/ui/ToolbarGroup.js
+++ src/main/js/ephox/alloy/api/ui/ToolbarGroup.js
@@ -7,30 +7,17 @@ define(
     'ephox.alloy.data.Fields',
     'ephox.alloy.dom.DomModification',
     'ephox.alloy.parts.PartType',
+    'ephox.alloy.ui.schema.ToolbarGroupSchema',
+    'ephox.alloy.ui.schema.ToolbarSchema',
     'ephox.boulder.api.FieldSchema',
     'ephox.highway.Merger',
     'ephox.peanut.Fun',
     'global!Error'
   ],
 
-  function (Behaviour, UiBuilder, Fields, DomModification, PartType, FieldSchema, Merger, Fun, Error) {
-    var schema = [
-      FieldSchema.strict('items'),
-      Fields.members([ 'item' ]),
-      Fields.markers([ 'itemClass' ]),
-      FieldSchema.defaulted('hasTabstop', true)
-    ];
-
-    var partTypes = [
-      PartType.group({ build: Fun.identity }, 'items', 'item', '<alloy.toolbar-group.items>', Fun.constant({ }), function (detail) {
-        return {
-          'debug.label': 'ToolbarGroup.item',
-          domModification: {
-            classes: [ detail.markers().itemClass() ]
-          }
-        };
-      })
-    ];
+  function (Behaviour, UiBuilder, Fields, DomModification, PartType, ToolbarGroupSchema, ToolbarSchema, FieldSchema, Merger, Fun, Error) {
+    var schema = ToolbarGroupSchema.schema();
+    var partTypes = ToolbarGroupSchema.parts();
 
     var make = function (detail, components, spec, _externals) {
       return Merger.deepMerge(
@@ -59,11 +46,11 @@ define(
     };
 
     var build = function (spec) {
-      return UiBuilder.composite('toolbar-group', schema, partTypes, make, spec);
+      return UiBuilder.composite(ToolbarGroupSchema.name(), schema, partTypes, make, spec);
     };
 
     // TODO: Remove likely dupe
-    var parts = PartType.generate('toolbar-group', partTypes);
+    var parts = PartType.generate(ToolbarGroupSchema.name(), partTypes);
 
     return {
       build: build,
