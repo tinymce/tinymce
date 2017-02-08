c42c6917c81c071f5ad44875ba425e91f76a8e1b
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2017-02-08 17:31:21 +0100

    TINY-823: move to new boltified setup, fix regexp

--- src/plugins/visualchars/config/bolt/demo.js
+++ src/plugins/visualchars/config/bolt/demo.js
@@ -5,7 +5,8 @@ configure({
   sources: [
     source('amd', 'tinymce.plugins.visualchars.demo', '../../src/demo/js', function (id) {
       return mapper.hierarchical(id).replace(/^tinymce\/plugins\/visualchars\//, '');
-    })
+    }),
+    source('amd', 'ephox', '../../../../../node_modules/@ephox', mapper.repo('js', mapper.flat))
   ]
 });
 
--- src/plugins/visualchars/src/main/js/Plugin.js
+++ src/plugins/visualchars/src/main/js/Plugin.js
@@ -19,98 +19,151 @@ define(
 
   [
     'global!tinymce.PluginManager',
-    'global!tinymce.walk'
+    'global!tinymce.dom.TreeWalker',
+    'global!tinymce.util.Delay',
+    'ephox.katamari.api.Arr'
   ],
 
-  function (PluginManager, walk) {
+  function (PluginManager, TreeWalker, Delay, Arr) {
+    var charMap = {
+      '\u00a0': 'nbsp',
+      '\u00ad': 'shy'
+    };
+
+    var charMapToRegExp = function (charMap) {
+      var key, regExp = '';
+
+      for (key in charMap) {
+        regExp += key;
+      }
+
+      return new RegExp('[' + regExp + ']');
+    };
+
+    var wrapCharWithSpan = function (charMap, value) {
+      return '<span data-mce-bogus="1" class="mce-' + charMap[value] + '">' + value + '</span>';
+    };
+
+    var isTextNodeMatchingRegExp = function (node, regExp) {
+      return node && node.nodeType === 3 && node.nodeValue !== undefined && regExp.test(node.nodeValue);
+    };
+
+    var getNodeList = function (regExp, rootElm) {
+      var walker = new TreeWalker(rootElm);
+      var nodeList = [];
+
+      do {
+        if (isTextNodeMatchingRegExp(walker.current(), regExp)) {
+          nodeList.push(walker.current());
+        }
+      } while (walker.next() !== undefined);
+
+      return nodeList;
+    };
+
     PluginManager.add('visualchars', function (editor) {
       var self = this, state;
 
-      function toggleVisualChars(addBookmark) {
-        var node, nodeList, i, body = editor.getBody(), nodeValue, selection = editor.selection, div, bookmark;
-        var charMap, visualCharsRegExp;
+      var showVisualChars = function (charMap, rootElm) {
+        var node, div;
+        var regExp = charMapToRegExp(charMap);
+        var nodeList = getNodeList(regExp, rootElm);
 
-        charMap = {
-          '\u00a0': 'nbsp',
-          '\u00ad': 'shy'
-        };
+        Arr.each(nodeList, function (n) {
+          var nodeValue = n.nodeValue;
+          var replaced = Arr.map(nodeValue.split(''), function (c) {
+            return regExp.test(c) ? wrapCharWithSpan(charMap, c) : c;
+          }).join('');
 
-        function wrapCharWithSpan(value) {
-          return '<span data-mce-bogus="1" class="mce-' + charMap[value] + '">' + value + '</span>';
-        }
-
-        function compileCharMapToRegExp() {
-          var key, regExp = '';
-
-          for (key in charMap) {
-            regExp += key;
+          div = editor.dom.create('div', null, replaced);
+          while ((node = div.lastChild)) {
+            editor.dom.insertAfter(node, n);
           }
 
-          return new RegExp('[' + regExp + ']', 'g');
+          editor.dom.remove(n);
+        });
+      };
+
+      var hideVisualChars = function (charMap, body) {
+        var nodeList = editor.dom.select(compileCharMapToCssSelector(charMap), body);
+        var i;
+
+        for (i = nodeList.length - 1; i >= 0; i--) {
+          editor.dom.remove(nodeList[i], 1);
         }
+      };
 
-        function compileCharMapToCssSelector() {
-          var key, selector = '';
+      var compileCharMapToCssSelector = function (charMap) {
+        var key, selector = '';
 
-          for (key in charMap) {
-            if (selector) {
-              selector += ',';
-            }
-
-            selector += 'span.mce-' + charMap[key];
+        for (key in charMap) {
+          if (selector) {
+            selector += ',';
           }
-
-          return selector;
+          selector += 'span.mce-' + charMap[key];
         }
+
+        return selector;
+      };
+
+      var findParentElm = function (elm, rootElm) {
+        while (elm.parentNode) {
+          if (elm.parentNode === rootElm) {
+            return elm;
+          }
+          elm = elm.parentNode;
+        }
+      };
+
+      var toggleVisualChars = function () {
+        var body = editor.getBody();
+        var bookmark = editor.selection.getBookmark();
+        var parentNode = findParentElm(editor.selection.getNode(), body);
+
+        // if user does select all the parentNode will be undefined
+        parentNode = parentNode !== undefined ? parentNode : body;
+
+        hideVisualChars(charMap, parentNode);
+        showVisualChars(charMap, parentNode);
+
+        editor.selection.moveToBookmark(bookmark);
+      };
+
+      var toggleVisualCharsAndState = function (addBookmark) {
+        var body = editor.getBody(), selection = editor.selection, bookmark;
 
         state = !state;
         self.state = state;
         editor.fire('VisualChars', { state: state });
-        visualCharsRegExp = compileCharMapToRegExp();
 
-        if (addBookmark) {
+        if (addBookmark !== false) {
           bookmark = selection.getBookmark();
         }
 
-        if (state) {
-          nodeList = [];
-          walk(body, function (n) {
-            if (n.nodeType == 3 && n.nodeValue && visualCharsRegExp.test(n.nodeValue)) {
-              nodeList.push(n);
-            }
-          }, 'childNodes');
-
-          for (i = 0; i < nodeList.length; i++) {
-            nodeValue = nodeList[i].nodeValue;
-            nodeValue = nodeValue.replace(visualCharsRegExp, wrapCharWithSpan);
-
-            div = editor.dom.create('div', null, nodeValue);
-            while ((node = div.lastChild)) {
-              editor.dom.insertAfter(node, nodeList[i]);
-            }
-
-            editor.dom.remove(nodeList[i]);
-          }
-        } else {
-          nodeList = editor.dom.select(compileCharMapToCssSelector(), body);
-
-          for (i = nodeList.length - 1; i >= 0; i--) {
-            editor.dom.remove(nodeList[i], 1);
-          }
-        }
+        state === true ? showVisualChars(charMap, body) : hideVisualChars(charMap, body);
 
         selection.moveToBookmark(bookmark);
-      }
+      };
 
-      function toggleActiveState() {
+      var toggleActiveState = function () {
         var self = this;
 
         editor.on('VisualChars', function (e) {
           self.active(e.state);
         });
+      };
+
+      var debouncedToggle = Delay.debounce(toggleVisualChars, 300);
+
+      if (editor.settings.forced_root_block !== false) {
+        editor.on('keydown', function (e) {
+          if (self.state === true) {
+            e.keyCode === 13 ? toggleVisualChars() : debouncedToggle();
+          }
+        });
       }
 
-      editor.addCommand('mceVisualChars', toggleVisualChars);
+      editor.addCommand('mceVisualChars', toggleVisualCharsAndState);
 
       editor.addButton('visualchars', {
         title: 'Show invisible characters',
@@ -128,6 +181,10 @@ define(
       });
     });
 
-    return function () { };
+    return {
+      charMap: charMap,
+      charMapToRegExp: charMapToRegExp,
+      getNodeList: getNodeList
+    };
   }
 );
