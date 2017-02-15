521d0df3a1c2e6c6edf4e7d0ab86d03a2cb7d8d9
morgan <morgan.smith@ephox.com>
2017-02-15 13:50:09 +1000

    VAN-44: cleanup of configs

--- config/bolt/browser.js
+++ config/bolt/browser.js
@@ -1,10 +1,8 @@
 configure({
   configs: [
-    './test.js',
-    './prod.js'
+    './test.js'
   ],
   sources: [
-    source('amd', 'ephox.boulder', 'http://lenovo-morgan/me/work/van/boulder/src/main/js', mapper.hierarchical, { absolute: true }),
-    source('amd', 'ephox.lumber', 'http://lenovo-morgan/me/work/van/boulder/lib/test', mapper.flat, { absolute: true })
+    
   ]
 });
