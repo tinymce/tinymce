497dc18aa88b18203e1847a57cae0d8a482f4a73
Spocke <spocke@moxiecode.com>
2017-02-15 17:29:09 +0100

    TM-1: fixed core so it just loads bolt config from root

--- src/core/config/bolt/demo.js
+++ src/core/config/bolt/demo.js
@@ -1,23 +1,5 @@
 configure({
   configs: [
-  ],
-  sources: [
-    source('amd', 'ephox/imagetools', '../../../../node_modules/@ephox/imagetools/src/main/js', mapper.hierarchical),
-    source('amd', 'ephox', '../../../../node_modules/@ephox', mapper.repo('js', mapper.flat)),
-    source('amd', 'tinymce.core.demo', '../../src/demo/js', function (id) {
-      return mapper.hierarchical(id).replace(/^tinymce\/core\//, '');
-    }),
-    source('amd', 'tinymce.core', '../../src/main/js', function (id) {
-      return mapper.hierarchical(id).replace(/^tinymce\/core\//, '');
-    }),
-    source('amd', 'tinymce.plugins', '../../../plugins', function (id) {
-      var parts = id.split('.');
-      return parts.slice(2, 3).concat(['src/main/js']).concat(parts.slice(3)).join('/');
-    }),
-    source('amd', 'tinymce.themes', '../../../themes', function (id) {
-      var parts = id.split('.');
-      return parts.slice(2, 3).concat(['src/main/js']).concat(parts.slice(3)).join('/');
-    })
+    '../../../../config/bolt/demo.js'
   ]
 });
-
