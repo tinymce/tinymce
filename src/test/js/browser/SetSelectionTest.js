93423a5a509a5231e97e3ee106b6e6e639eee490
morgan <morgan.smith@ephox.com>
2017-02-21 17:39:21 +1000

    TM-3: asserting selection

--- src/test/js/browser/SetSelectionTest.js
+++ src/test/js/browser/SetSelectionTest.js
@@ -9,13 +9,14 @@ asynctest(
     'ephox.sugar.api.node.Element',
     'ephox.sugar.api.node.Node',
     'ephox.sugar.api.node.Text',
+    'ephox.sugar.api.properties.Html',
     'ephox.sugar.api.selection.WindowSelection',
     'global!setTimeout',
     'global!window'
   ],
 
   function (
-    Compare, Hierarchy, InsertAll, Body, Element, Node, Text, WindowSelection, setTimeout,
+    Compare, Hierarchy, InsertAll, Body, Element, Node, Text, Html, WindowSelection, setTimeout,
     window
   ) {
     var success = arguments[arguments.length - 2];
@@ -35,18 +36,33 @@ asynctest(
       assert.fail('There should not be a selection yet');
     });
 
-    WindowSelection.set(window, p1text, 1, p2text, 1);
+    var setSelection = function (start, soffset, finish, foffset) {
+      WindowSelection.set(window, start, soffset, finish, foffset);
+    };
 
-    // Check that the selection is the text nodes
-    WindowSelection.get(window).fold(function () {
-      assert.fail('After setting selection (p1, p2), could not find a selection');
-    }, function (sel) {
-      assert.eq(true, Compare.eq(sel.start(), p1text), 'Start container should be p1text');
-      assert.eq(true, Compare.eq(sel.finish(), p2text), 'Finish container should be p2text');
-    });
+    var assertNoSelection = function (label) {
+      WindowSelection.get(window).each(function (sel) {
+        assert.fail('There should not be a selection yet: ' + label);
+      });
+    };
+
+    var assertSelection = function (label, expStart, expSoffset, expFinish, expFoffset) {
+      WindowSelection.get(window).fold(function () {
+        assert.fail('After setting selection ' + label + ', could not find a selection');
+      }, function (sel) {
+        assert.eq(true, Compare.eq(sel.start(), p1text), 'Start container should be: ' + Html.getOuter(expStart) + '\n' + label)
+        assert.eq(expSoffset, sel.soffset());
+        assert.eq(true, Compare.eq(sel.finish(), p2text), 'Finish container should be ' + Html.getOuter(expFinish) + '\n' + label);
+        assert.eq(expFoffset, sel.foffset());
+      });
+    };
+
+    setSelection(p1text, 1, p2text, 1);
+    assertSelection('(p1text, 1) -> (p2text, 2)', p1text, 1, p2text, 1);
 
     setTimeout(function () {
-      WindowSelection.set(window, p2text, 2, p1text, 2);
+      setSelection(p2text, 2, p1text, 3);
+      assertSelection('(p2text, 2) -> (p1text, 3)', p2text, 2, p1text, 3);
     }, 2000);
 
 
