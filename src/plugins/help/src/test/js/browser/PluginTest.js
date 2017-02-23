ceade9bafd664f63f5ee6fcc5ad1dfd9c506de39
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2017-02-23 17:19:37 +0100

    TINY-876: fix test

--- src/plugins/help/src/test/js/browser/PluginTest.js
+++ src/plugins/help/src/test/js/browser/PluginTest.js
@@ -1,14 +1,17 @@
 asynctest('browser.plugin.PluginTest', [
-  'tinymce.help.Plugin',
+  'tinymce.plugins.help.Plugin',
   'ephox.mcagar.api.TinyLoader',
   'ephox.agar.api.Pipeline',
   'ephox.mcagar.api.TinyApis',
-  'ephox.mcagar.api.TinyUi'
+  'ephox.mcagar.api.TinyUi',
+  'tinymce.themes.modern.Theme'
 ], function (
-	Plugin, TinyLoader, Pipeline, TinyApis, TinyUi
+	Plugin, TinyLoader, Pipeline, TinyApis, TinyUi, Theme
 ) {
   var success = arguments[arguments.length - 2];
   var failure = arguments[arguments.length - 1];
+
+  Theme();
 
   TinyLoader.setup(function (editor, onSuccess, onFailure) {
 		var tinyApis = TinyApis(editor); // eslint-disable-line
@@ -16,10 +19,11 @@ asynctest('browser.plugin.PluginTest', [
 
     Pipeline.async({}, [
       tinyUi.sClickOnToolbar('click on button', 'button'),
-      tinyUi.sWaitForPopup('Wait for dialog popup', 'div.mce-window-head:contains("Help")')
+      tinyUi.sWaitForPopup('Wait for dialog popup', 'div.mce-title > img')
     ], onSuccess, onFailure);
   }, {
     plugins: 'help',
-    toolbar: 'help'
+    toolbar: 'help',
+    skin_url: '/project/src/skins/lightgray/dist/lightgray'
   }, success, failure);
 });
