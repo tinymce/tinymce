5bce3cc726964fbe69ff1f226b25348e5898ac1a
morgan <morgan.smith@ephox.com>
2017-02-24 12:10:55 +1000

    TM-3: typo

--- src/main/js/ephox/sugar/api/selection/Edge.js
+++ src/main/js/ephox/sugar/api/selection/Edge.js
@@ -20,8 +20,8 @@ define(
       return isAtEdge(parent, selection.startContainer(), selection.startOffset(), CursorPosition.first, Awareness.isStart);
     };
 
-    // This is doing exactly the same thing as the above isLeft method, checking to see if an element/offset selection endpoint is at the left edge of it's parent
-    // after ascending up to that parent except we explicitly provide the element and it's offset instead of just using the selection object.
+    // This is doing exactly the same thing as the above isLeft method, checking to see if an element/offset selection endpoint is at the left edge of its parent
+    // after ascending up to that parent except we explicitly provide the element and its offset instead of just using the selection object.
     var isAtLeftEdge = function (parent, element, offset) {
       return isAtEdge(parent, element, offset, CursorPosition.first, Awareness.isStart);
     };
