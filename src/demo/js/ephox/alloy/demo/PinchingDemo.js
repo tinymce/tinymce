f73216ef7d89cb4aed792bdcada870f58e92746b
morgan <morgan.smith@ephox.com>
2017-02-28 14:19:03 +1000

    TM-11: initial demo pages

---
+++ src/demo/js/ephox/alloy/demo/PinchingDemo.js
@@ -0,0 +1,59 @@
+define(
+  'ephox.alloy.demo.PinchingDemo',
+
+  [
+    'ephox.alloy.api.component.GuiFactory',
+    'ephox.alloy.api.system.Gui',
+    'ephox.sugar.api.dom.Insert',
+    'ephox.sugar.api.properties.Css',
+    'ephox.sugar.api.search.SelectorFind',
+    'ephox.sugar.api.view.Height',
+    'ephox.sugar.api.view.Width'
+  ],
+
+  function (GuiFactory, Gui, Insert, Css, SelectorFind, Height, Width) {
+    return function () {
+      var ephoxUi = SelectorFind.first('#ephox-ui').getOrDie();
+
+      // Naive resize handler
+      var resize = function (element, changeX, changeY) {
+        var width = Css.getRaw(element, 'width').map(function (w) {
+          return parseInt(w, 10);
+        }).getOrThunk(function () {
+          return Width.get(element);
+        });
+
+        var height = Css.getRaw(element, 'height').map(function (h) {
+          return parseInt(h, 10);
+        }).getOrThunk(function () {
+          return Height.get(element);
+        });
+
+        Css.set(element, 'width', (width + changeX) + 'px');
+        Css.set(element, 'height', (height + changeY) + 'px');
+      };
+
+      var box = GuiFactory.build({
+        dom: {
+          tag: 'div',
+          classes: [ 'demo-pinch-box' ]
+        },
+        behaviours: {
+          pinching: {
+            onPinch: function (span, changeX, changeY) {
+              resize(span, changeX, changeY);
+            },
+            onPunch: function (span, changeX, changeY) {
+              resize(span, changeX, changeY);
+            }
+          }
+        }
+      });
+
+      var gui = Gui.create();
+      gui.add(box);
+
+      Insert.append(ephoxUi, gui.element());
+    };
+  }
+);
