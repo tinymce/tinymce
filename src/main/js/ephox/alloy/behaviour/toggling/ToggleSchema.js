9e33a032b7d9c199cc678c8aa6617ae8c61877eb
morgan <morgan.smith@ephox.com>
2017-03-02 09:44:28 +1000

    TM-14: supporting more modes and defaulting to aria:none

--- src/main/js/ephox/alloy/behaviour/toggling/ToggleSchema.js
+++ src/main/js/ephox/alloy/behaviour/toggling/ToggleSchema.js
@@ -2,29 +2,37 @@ define(
   'ephox.alloy.behaviour.toggling.ToggleSchema',
 
   [
+    'ephox.alloy.behaviour.toggling.ToggleModes',
     'ephox.boulder.api.FieldSchema',
     'ephox.boulder.api.ValueSchema',
-    'ephox.sugar.api.properties.Attr'
+    'ephox.katamari.api.Fun'
   ],
 
-  function (FieldSchema, ValueSchema, Attr) {
+  function (ToggleModes, FieldSchema, ValueSchema, Fun) {
     return [
       FieldSchema.defaulted('selected', false),
       FieldSchema.strict('toggleClass'),
       FieldSchema.defaulted('toggleOnExecute', true),
 
       FieldSchema.defaultedOf('aria', {
-        mode: 'pressed'
+        mode: 'none'
        }, ValueSchema.choose(
         'mode', {
           'pressed': [
             FieldSchema.defaulted('syncWithExpanded', false),
-            FieldSchema.state('update', function () {
-              return function (component, ariaInfo, status) {
-                Attr.set(component.element(), 'aria-pressed', status);
-                if (ariaInfo.syncWithExpanded()) Attr.set(component.element(), 'aria-expanded', status);
-              };
-            })
+            FieldSchema.state('update', Fun.constant(ToggleModes.updatePressed))
+          ],
+          'checked': [
+            FieldSchema.state('update', Fun.constant(ToggleModes.updateChecked))
+          ],
+          'expanded': [
+            FieldSchema.state('update', Fun.constant(ToggleModes.updateExpanded))
+          ],
+          'selected': [
+            FieldSchema.state('update', Fun.constant(ToggleModes.updateSelected))
+          ],
+          'none': [
+            FieldSchema.state('update', Fun.constant(Fun.noop))
           ]
         }
       ))
