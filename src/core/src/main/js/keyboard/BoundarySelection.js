69b0ae5f2502a070661a722f272f2f7b2845f5cb
Spocke <spocke@moxiecode.com>
2017-03-22 12:13:14 +0100

    TINY-953: fixed minor issue with dblclick selection on IE

--- src/core/src/main/js/keyboard/BoundarySelection.js
+++ src/core/src/main/js/keyboard/BoundarySelection.js
@@ -57,7 +57,7 @@ define(
     };
 
     var safeRemoveCaretContainer = function (editor, caret) {
-      if (editor.selection.isCollapsed() && editor.composing !== true) {
+      if (editor.selection.isCollapsed() && editor.composing !== true && caret.get()) {
         var pos = CaretPosition.fromRangeStart(editor.selection.getRng());
         if (CaretPosition.isTextPosition(pos) && InlineUtils.isAtZwsp(pos) === false) {
           setCaretPosition(editor, CaretContainerRemove.removeAndReposition(caret.get(), pos));
