fa0b5256db44e43bf94c55e26dfa79d6ceb9d31d
morgan <morgan.smith@ephox.com>
2017-03-24 09:31:09 +1000

    TM-12: snap start

--- src/main/js/ephox/alloy/ui/slider/SliderSchema.js
+++ src/main/js/ephox/alloy/ui/slider/SliderSchema.js
@@ -17,7 +17,7 @@ define(
       FieldSchema.defaulted('stepSize', 1),
       FieldSchema.defaulted('onChange', Fun.noop),
       FieldSchema.defaulted('snapToGrid', false),
-      FieldSchema.defaulted('snapOffset', 0),
+      FieldSchema.option('snapStart'),
       FieldSchema.strict('initialValue'),
       
       FieldSchema.state('value', function (spec) { return Cell(spec.initialValue); }),
