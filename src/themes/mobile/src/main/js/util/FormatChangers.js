0f5a9133aabf37c05a06f27549f2919ef2ed5857
morgan <morgan.smith@ephox.com>
2017-04-03 14:15:51 +1000

    TM-23: moved font size stuff out

--- src/themes/mobile/src/main/js/util/FormatChangers.js
+++ src/themes/mobile/src/main/js/util/FormatChangers.js
@@ -4,11 +4,10 @@ define(
   [
     'ephox.katamari.api.Arr',
     'ephox.katamari.api.Fun',
-    'tinymce.core.fmt.FontInfo',
     'tinymce.themes.mobile.channels.TinyChannels'
   ],
 
-  function (Arr, Fun, FontInfo, TinyChannels) {
+  function (Arr, Fun, TinyChannels) {
     var fontSizes = [ 'x-small', 'small', 'medium', 'large', 'x-large' ];
 
     var fireChange = function (ios, command, state) {
@@ -16,16 +15,6 @@ define(
         command: command,
         state: state
       });
-    };
-
-    var getFontSize = function (editor) {
-      var node = editor.selection.getStart();
-      var px = FontInfo.getFontSize(editor.getBody(), node);
-      var pt = FontInfo.toPt(px);
-
-      return Arr.find(fontSizes, function (size) {
-        return px === size || pt === size;
-      }).getOr('medium');
     };
 
     var init = function (ios, editor) {
@@ -38,7 +27,6 @@ define(
 
     return {
       init: init,
-      getFontSize: getFontSize,
       fontSizes: Fun.constant(fontSizes)
     };
   }
