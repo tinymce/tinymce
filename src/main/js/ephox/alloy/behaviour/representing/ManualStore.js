0e17d76d15aedee6fac40f2f027b94fbcb2eba3a
morgan <morgan.smith@ephox.com>
2017-04-05 08:58:41 +1000

    VAN-77: separating out stores

---
+++ src/main/js/ephox/alloy/behaviour/representing/ManualStore.js
@@ -0,0 +1,37 @@
+define(
+  'ephox.alloy.behaviour.representing.ManualStore',
+
+  [
+    'ephox.boulder.api.FieldSchema',
+    'ephox.katamari.api.Fun'
+  ],
+
+  function (FieldSchema, Fun) {
+    return [
+      FieldSchema.strict('getValue'),
+      FieldSchema.defaulted('setValue', Fun.noop),
+      FieldSchema.option('initialValue'),
+      FieldSchema.state('manager', function () {
+        var getValue = function (component, repInfo) {
+          return repInfo.store().getValue()(component);
+        };
+
+        var setValue = function (component, repInfo, data) {
+          repInfo.store().setValue()(component, data);
+        };
+
+        var onLoad = function (component, repInfo) {
+          repInfo.store().initialValue().each(function (data) {
+            repInfo.store().setValue()(component, data);
+          });
+        };
+
+        return {
+          setValue: setValue,
+          getValue: getValue,
+          onLoad: onLoad
+        };
+      })
+    ];
+  }
+);
--- src/main/js/ephox/alloy/behaviour/representing/RepresentSchema.js
+++ src/main/js/ephox/alloy/behaviour/representing/RepresentSchema.js
@@ -2,108 +2,19 @@ define(
   'ephox.alloy.behaviour.representing.RepresentSchema',
 
   [
+    'ephox.alloy.behaviour.representing.DatasetStore',
+    'ephox.alloy.behaviour.representing.ManualStore',
+    'ephox.alloy.behaviour.representing.MemoryStore',
     'ephox.boulder.api.FieldSchema',
-    'ephox.boulder.api.Objects',
-    'ephox.boulder.api.ValueSchema',
-    'ephox.katamari.api.Cell',
-    'ephox.katamari.api.Fun'
+    'ephox.boulder.api.ValueSchema'
   ],
 
-  function (FieldSchema, Objects, ValueSchema, Cell, Fun) {
+  function (DatasetStore, ManualStore, MemoryStore, FieldSchema, ValueSchema) {
     return [
       FieldSchema.defaultedOf('store', { mode: 'memory' }, ValueSchema.choose('mode', {
-        memory: [
-          // TODO: Find a better way of storing this.
-          FieldSchema.state('state', function () { return Cell(null); }),
-          FieldSchema.option('initialValue'),
-          
-          FieldSchema.state('manager', function () {
-
-            var setValue = function (component, repInfo, data) {
-              repInfo.store().state().set(data);
-            };
-
-            var getValue = function (component, repInfo) {
-              return repInfo.store().state().get();
-            };
-
-            var onLoad = function (component, repInfo) {
-              var current = repInfo.store().state().get();
-              repInfo.store().initialValue().each(function (initVal) {
-                if (current === null) repInfo.store().state().set(initVal);
-              });
-            };
-
-            return {
-              setValue: setValue,
-              getValue: getValue,
-              onLoad: onLoad
-            };
-          })
-
-        ],
-        manual: [
-          FieldSchema.strict('getValue'),
-          FieldSchema.defaulted('setValue', Fun.noop),
-          FieldSchema.option('initialValue'),
-          FieldSchema.state('manager', function () {
-            var getValue = function (component, repInfo) {
-              return repInfo.store().getValue()(component);
-            };
-
-            var setValue = function (component, repInfo, data) {
-              repInfo.store().setValue()(component, data);
-            };
-
-            var onLoad = function (component, repInfo) {
-              repInfo.store().initialValue().each(function (data) {
-                repInfo.store().setValue()(component, data);
-              });
-            };
-
-            return {
-              setValue: setValue,
-              getValue: getValue,
-              onLoad: onLoad
-            };
-          })
-        ],
-
-        dataset: [
-          FieldSchema.state('dataset', Fun.constant(Cell({ }))),
-          FieldSchema.option('initialValue'),
-          FieldSchema.strict('getFallbackEntry'),
-          FieldSchema.strict('getDataKey'),
-          FieldSchema.strict('setData'),
-          FieldSchema.state('manager', function () {
-            var setValue = function (component, repInfo, data) {
-              repInfo.store().dataset().set({ });
-              repInfo.store().setData()(component, data);
-            };
-
-            var getValue = function (component, repInfo) {
-              var key = repInfo.store().getDataKey()(component);
-              var dataset = repInfo.store().dataset().get();
-              return Objects.readOptFrom(dataset, key).fold(function () {
-                return repInfo.store().getFallbackEntry()(key);
-              }, function (data) {
-                return data;
-              });
-            };
-
-            var onLoad = function (component, repInfo) {
-              repInfo.store().initialValue().each(function (data) {
-                setValue(component, repInfo, data);
-              });
-            };
-
-            return {
-              setValue: setValue,
-              getValue: getValue,
-              onLoad: onLoad
-            };
-          })
-        ]
+        memory: MemoryStore,
+        manual: ManualStore,
+        dataset: DatasetStore
       }))
     ];
   }
