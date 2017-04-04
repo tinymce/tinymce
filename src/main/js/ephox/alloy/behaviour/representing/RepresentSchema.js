5bf404dfba4620a2f97e402011e79d5b489324bd
morgan <morgan.smith@ephox.com>
2017-04-04 15:10:35 +1000

    VAN-77: fixing schema

--- src/main/js/ephox/alloy/behaviour/representing/RepresentSchema.js
+++ src/main/js/ephox/alloy/behaviour/representing/RepresentSchema.js
@@ -45,7 +45,7 @@ define(
         manual: [
           FieldSchema.strict('getValue'),
           FieldSchema.defaulted('setValue', Fun.noop),
-          FieldSchema.option('initialValue', { }),
+          FieldSchema.option('initialValue'),
           FieldSchema.state('manager', function () {
             var getValue = function (component, repInfo) {
               return repInfo.store().getValue()(component);
