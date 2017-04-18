698d2523538eaa34cf913939c501280ca2012df2
morgan <morgan.smith@ephox.com>
2017-04-18 15:18:57 +1000

    TM-43: adding to link bridge test

--- src/themes/mobile/src/test/js/phantom/bridge/LinkBridgeTest.js
+++ src/themes/mobile/src/test/js/phantom/bridge/LinkBridgeTest.js
@@ -5,14 +5,16 @@ test(
     'ephox.agar.api.Logger',
     'ephox.agar.api.RawAssertions',
     'ephox.alloy.test.TestStore',
+    'ephox.boulder.api.FieldSchema',
     'ephox.boulder.api.Objects',
+    'ephox.boulder.api.ValueSchema',
     'ephox.katamari.api.Cell',
     'ephox.katamari.api.Fun',
     'ephox.sugar.api.node.Element',
     'tinymce.themes.mobile.bridge.LinkBridge'
   ],
 
-  function (Logger, RawAssertions, TestStore, Objects, Cell, Fun, Element, LinkBridge) {
+  function (Logger, RawAssertions, TestStore, FieldSchema, Objects, ValueSchema, Cell, Fun, Element, LinkBridge) {
     var store = TestStore();
 
     var editorState = {
@@ -38,18 +40,45 @@ test(
       focus: Fun.noop
     };
 
-    Logger.sync(
-      'Checking getting info for just text node',
-      function () {
-        editorState.start.set(Element.fromText('hi').dom());
-        editorState.content.set('');
+    var checkGetNoLink = function (rawScenario) {
+      var schema = ValueSchema.objOfOnly([
+        FieldSchema.strict('label'),
+        FieldSchema.defaulted('nodeText', ''),
+        FieldSchema.defaulted('selection', ''),
+        FieldSchema.strict('expected')
+      ]);
+
+      var scenario = ValueSchema.asRawOrDie(rawScenario.label, schema, rawScenario);
+
+      Logger.sync('getInfo ... ' + scenario.label, function () {
+        editorState.start.set(Element.fromText(scenario.nodeText).dom());
+        editorState.content.set(scenario.selection);
         var info = LinkBridge.getInfo(editor);
-        RawAssertions.assertEq('Checking getInfo', {
-          text: ''
+        RawAssertions.assertEq('Checking getInfo (no link)', {
+          text: scenario.expected
         }, Objects.narrow(info, [ 'url', 'text', 'target', 'title' ]));
         RawAssertions.assertEq('Checking link is not set', true, info.link.isNone());
-      }
-    );
+      });
+    };
+
+    checkGetNoLink({
+      label: 'Basic text node with no text',
+      expected: ''
+    });
+
+    checkGetNoLink({
+      label: 'Basic text node with some text and no selection',
+      nodeText: 'some',
+      expected: ''
+    });
+
+    checkGetNoLink({
+      label: 'Basic text node with some text and "sel" selection',
+      nodeText: 'some',
+      selection: 'sel',
+      expected: 'sel'
+    });
+    
 
   }
 );
