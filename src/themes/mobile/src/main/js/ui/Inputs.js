d6d4bc2bc33129176b86de2f0cc5e32e86954be3
morgan <morgan.smith@ephox.com>
2017-04-21 13:59:35 +1000

    TM-39: inputs with x

--- src/themes/mobile/src/main/js/ui/Inputs.js
+++ src/themes/mobile/src/main/js/ui/Inputs.js
@@ -2,20 +2,98 @@ define(
   'tinymce.themes.mobile.ui.Inputs',
 
   [
+    'ephox.alloy.alien.EventRoot',
+    'ephox.alloy.api.behaviour.AdhocBehaviour',
+    'ephox.alloy.api.behaviour.Behaviour',
+    'ephox.alloy.api.behaviour.Composing',
+    'ephox.alloy.api.behaviour.Representing',
+    'ephox.alloy.api.behaviour.Toggling',
+    'ephox.alloy.api.component.Memento',
+    'ephox.alloy.api.events.SystemEvents',
+    'ephox.alloy.api.ui.Button',
+    'ephox.alloy.api.ui.Container',
     'ephox.alloy.api.ui.DataField',
     'ephox.alloy.api.ui.Input',
-    'ephox.katamari.api.Option'
+    'ephox.alloy.construct.EventHandler',
+    'ephox.boulder.api.Objects',
+    'ephox.katamari.api.Fun',
+    'ephox.katamari.api.Merger',
+    'ephox.katamari.api.Option',
+    'tinymce.themes.mobile.style.Styles'
   ],
 
-  function (DataField, Input, Option) {
+  function (
+    EventRoot, AdhocBehaviour, Behaviour, Composing, Representing, Toggling, Memento, SystemEvents, Button, Container, DataField, Input, EventHandler, Objects,
+    Fun, Merger, Option, Styles
+  ) {
     var field = function (name, placeholder) {
+      // TODO: simplify the adhoc thing (inputEvents ?)
+      var inputSpec = Memento.record(Input.sketch({
+        placeholder: placeholder,
+        onSetValue: function (input, data) {
+          input.getSystem().triggerEvent('input', input.element(), {
+            target: Fun.constant(input.element())
+          });
+        },
+        inputBehaviours: {
+          composing: { find: Option.some }
+        }
+      }));
+
+      var buttonSpec = Memento.record(
+        Button.sketch({
+          dom: {
+            tag: 'button',
+            classes: [ Styles.resolve('input-container-x') ],
+            innerHtml: 'x'
+          },
+          action: function (button) {
+            var input = inputSpec.get(button);
+            Representing.setValue(input, '');
+          }
+        })
+      );
+
       return {
         name: name,
-        spec: Input.sketch({
-          placeholder: placeholder,
-          inputBehaviours: {
-            composing: { find: Option.some }
-          }
+        spec: Container.sketch({
+          dom: {
+            classes: [ Styles.resolve('input-container') ]
+          },
+          components: [
+            inputSpec.asSpec(),
+            buttonSpec.asSpec()
+          ],
+          containerBehaviours: Behaviour.derive([
+            Toggling.config({
+              toggleClass: Styles.resolve('input-container-empty')
+            }),
+            Composing.config({
+              find: function (comp) {
+                return Option.some(inputSpec.get(comp));
+              }
+            }),
+            {
+              key: 'input-clearing',
+              value: { enabled: true }
+            }
+          ]),
+          customBehaviours: [
+            AdhocBehaviour.events('input-clearing', 
+              Merger.deepMerge(
+                {
+                  input: EventHandler.nu({
+                    run: function (iContainer) {
+                      var input = inputSpec.get(iContainer);
+                      var val = Representing.getValue(input);
+                      if (val.length > 0) Toggling.off(iContainer);
+                      else Toggling.on(iContainer);
+                    }
+                  })
+                }
+              )
+            )
+          ]
         })
       };
     };
