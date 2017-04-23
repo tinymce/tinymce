a9922871c607a06a3d56cbfe6ea8247bee4a7d4d
morgan <morgan.smith@ephox.com>
2017-04-24 08:03:15 +1000

    VAN-81: run all the tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
       
       watching: {
         config: 'config/bolt/browser.js',
-        testfiles: 'src/test/js/browser/' + grunt.option('my-dir') + '/**/**Test.js',
+        testfiles: 'src/test/js/browser/**/**Test.js',
         browser: driver,
         options: {
           stopOnFailure: true
