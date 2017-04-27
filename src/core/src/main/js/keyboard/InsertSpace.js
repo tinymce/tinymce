f78971bea12ef1f2ddcd288abfeeb53177d0d61c
Spocke <spocke@moxiecode.com>
2017-04-27 14:38:43 +0200

    TINY-995: added logic for space inside inline boundary elements

---
+++ src/core/src/main/js/keyboard/InsertSpace.js
@@ -0,0 +1,64 @@
+/**
+ * InsertSpace.js
+ *
+ * Released under LGPL License.
+ * Copyright (c) 1999-2017 Ephox Corp. All rights reserved
+ *
+ * License: http://www.tinymce.com/license
+ * Contributing: http://www.tinymce.com/contributing
+ */
+
+define(
+  'tinymce.core.keyboard.InsertSpace',
+  [
+    'ephox.katamari.api.Fun',
+    'tinymce.core.caret.CaretPosition',
+    'tinymce.core.dom.NodeType',
+    'tinymce.core.keyboard.BoundaryLocation'
+  ],
+  function (Fun, CaretPosition, NodeType, BoundaryLocation) {
+    var isValidInsertPoint = function (location, caretPosition) {
+      return isAtStart(location) && NodeType.isText(caretPosition.container());
+    };
+
+    var insertNbspAtPosition = function (editor, caretPosition) {
+      var container = caretPosition.container();
+      var offset = caretPosition.offset();
+
+      container.insertData(offset, '\u00a0');
+      editor.selection.setCursorLocation(container, offset + 1);
+    };
+
+    var insertAtLocation = function (editor, caretPosition, location) {
+      if (isValidInsertPoint(location, caretPosition)) {
+        insertNbspAtPosition(editor, caretPosition);
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    var insertAtCaret = function (editor) {
+      var caretPosition = CaretPosition.fromRangeStart(editor.selection.getRng());
+      var boundaryLocation = BoundaryLocation.readLocation(editor.getBody(), caretPosition);
+      return boundaryLocation.map(Fun.curry(insertAtLocation, editor, caretPosition)).getOr(false);
+    };
+
+    var isAtStart = function (location) {
+      return location.fold(
+        Fun.constant(false), // Before
+        Fun.constant(true),  // Start
+        Fun.constant(false), // End
+        Fun.constant(false)  // After
+      );
+    };
+
+    var insertAtSelection = function (editor) {
+      return editor.selection.isCollapsed() ? insertAtCaret(editor) : false;
+    };
+
+    return {
+      insertAtSelection: insertAtSelection
+    };
+  }
+);
