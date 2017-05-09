ca25ed1aa5167e0bf9338b97fdd6953459603498
morgan <morgan.smith@ephox.com>
2017-05-10 09:51:12 +1000

    TM-34: need to use elementFromPoint for IOS ... caret range gives different values

--- src/main/js/ephox/alloy/alien/ElementFromPoint.js
+++ src/main/js/ephox/alloy/alien/ElementFromPoint.js
@@ -3,25 +3,35 @@ define(
 
   [
     'ephox.katamari.api.Option',
+    'ephox.sugar.api.node.Element',
     'ephox.sugar.api.node.Node',
     'ephox.sugar.api.search.Traverse',
-    'ephox.sugar.api.selection.WindowSelection',
     'global!document'
   ],
 
-  function (Option, Node, Traverse, WindowSelection, document) {
+  function (Option, Element, Node, Traverse, document) {
+    // Note, elementFromPoint gives a different answer than caretRangeFromPoint
+    var elementFromPoint = function (doc, x, y) {
+      return Option.from(
+        doc.dom().elementFromPoint(x, y)
+      ).map(Element.fromDom);
+    };
+
     var insideComponent = function (component, x, y) {
-      var win = Traverse.owner(component.element()).dom().defaultView;
-      return WindowSelection.getAtPoint(win, x, y).bind(function (rng) {
-        var start = rng.start();
-        return component.element().dom().contains(start.dom()) ? Option.some(rng.start()) : Option.none();
-      }).filter(function (node) {
+      var isInside = function (node) {
+        return component.element().dom().contains(node.dom());
+      };
+
+      var hasValidRect = function (node) {
         var elem = Node.isText(node) ? Traverse.parent(node) : Option.some(node);
         return elem.exists(function (e) {
           var rect = e.dom().getBoundingClientRect();
           return x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
         });
-      });
+      };
+
+      var doc = Traverse.owner(component.element());
+      return elementFromPoint(doc, x, y).filter(isInside).filter(hasValidRect);
     };
 
     return {
