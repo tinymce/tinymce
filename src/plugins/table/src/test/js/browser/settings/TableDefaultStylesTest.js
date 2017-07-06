91e7fd03089c4b05ea80c11a06f0d0613b0b6459
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2017-07-06 16:23:52 +0200

    TINY-1111: table_default_styles test

--- src/plugins/table/src/test/js/browser/settings/TableDefaultStylesTest.js
+++ src/plugins/table/src/test/js/browser/settings/TableDefaultStylesTest.js
@@ -1,17 +1,19 @@
 asynctest(
-  'browser.tinymce.plugins.table.TableDefaultAttributesTest',
+  'browser.tinymce.plugins.table.TableDefaultStylesTest',
   [
     'ephox.agar.api.GeneralSteps',
     'ephox.agar.api.Logger',
     'ephox.agar.api.Pipeline',
+    'ephox.agar.api.RawAssertions',
     'ephox.agar.api.Step',
     'ephox.mcagar.api.TinyApis',
     'ephox.mcagar.api.TinyLoader',
     'ephox.mcagar.api.TinyUi',
+    'tinymce.core.Env',
     'tinymce.plugins.table.Plugin',
     'tinymce.themes.modern.Theme'
   ],
-  function (GeneralSteps, Logger, Pipeline, Step, TinyApis, TinyLoader, TinyUi, TablePlugin, ModernTheme) {
+  function (GeneralSteps, Logger, Pipeline, RawAssertions, Step, TinyApis, TinyLoader, TinyUi, Env, TablePlugin, ModernTheme) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
@@ -23,22 +25,28 @@ asynctest(
       var tinyUi = TinyUi(editor);
 
       Pipeline.async({}, [
-        Logger.t('no attributes without setting', GeneralSteps.sequence([
+        Logger.t('no styles without setting', GeneralSteps.sequence([
           tinyApis.sFocus,
           tinyUi.sClickOnMenu('click table menu', 'span:contains("Table")'),
           tinyUi.sClickOnUi('click table menu', 'div[role="menu"] span:contains("Table")'),
           tinyUi.sClickOnUi('click table grid', 'a#mcegrid11'),
-          tinyApis.sAssertContentPresence({ 'table': 1, 'table[title="x"]': 0 }),
+          Step.sync(function () {
+            var table = editor.getBody().querySelector('table');
+            RawAssertions.assertEq('should be empty', '', table.style.border);
+          }),
           tinyApis.sSetContent('')
         ])),
 
         Logger.t('test default title attribute', GeneralSteps.sequence([
           tinyApis.sFocus,
-          tinyApis.sSetSetting('table_default_attributes', { title: 'x' }),
+          tinyApis.sSetSetting('table_default_styles', { border: '3px solid blue' }),
           tinyUi.sClickOnMenu('click table menu', 'span:contains("Table")'),
           tinyUi.sClickOnUi('click table menu', 'div[role="menu"] span:contains("Table")'),
           tinyUi.sClickOnUi('click table grid', 'a#mcegrid11'),
-          tinyApis.sAssertContentPresence({ 'table': 1, 'table[title="x"]': 1 }),
+          Step.sync(function () {
+            var table = editor.getBody().querySelector('table');
+            RawAssertions.assertEq('should be undefined', '3px solid blue', table.style.border);
+          }),
           tinyApis.sSetContent('')
         ]))
       ], onSuccess, onFailure);
