6c46c96fcfe937d14c3c8e9d7529edbe0ee03ffc
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2017-07-06 15:52:15 +0200

    TINY-1111: table_default_attributes setting test

--- src/plugins/table/src/test/js/browser/settings/TableTabNavigationDisabledTest.js
+++ src/plugins/table/src/test/js/browser/settings/TableTabNavigationDisabledTest.js
@@ -1,49 +1,45 @@
 asynctest(
-  'browser.tinymce.plugins.table.TableGridFalse',
+  'browser.tinymce.plugins.table.TableTablNavigationDisabledTest',
   [
     'ephox.agar.api.Assertions',
     'ephox.agar.api.Chain',
     'ephox.agar.api.GeneralSteps',
+    'ephox.agar.api.Keys',
     'ephox.agar.api.Logger',
     'ephox.agar.api.Pipeline',
+    'ephox.mcagar.api.TinyActions',
     'ephox.mcagar.api.TinyApis',
     'ephox.mcagar.api.TinyLoader',
     'ephox.mcagar.api.TinyUi',
     'tinymce.plugins.table.Plugin',
+    'tinymce.plugins.table.test.TableTestUtils',
     'tinymce.themes.modern.Theme'
   ],
-  function (Assertions, Chain, GeneralSteps, Logger, Pipeline, TinyApis, TinyLoader, TinyUi, TablePlugin, ModernTheme) {
+  function (Assertions, Chain, GeneralSteps, Keys, Logger, Pipeline, TinyActions, TinyApis, TinyLoader, TinyUi, TablePlugin, TableTestUtils, ModernTheme) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
     ModernTheme();
     TablePlugin();
 
+    var tableHtml = '<table><tbody><tr><td>a</td></tr><tr><td>a</td></tr></tbody></table>';
+
     TinyLoader.setup(function (editor, onSuccess, onFailure) {
       var tinyApis = TinyApis(editor);
-      var tinyUi = TinyUi(editor);
+      var tinyActions = TinyActions(editor);
 
       Pipeline.async({}, [
         Logger.t('test table grid disabled', GeneralSteps.sequence([
           tinyApis.sFocus,
-          tinyUi.sClickOnMenu('click table menu', 'span:contains("Table")'),
-          tinyUi.sClickOnUi('click table menu', 'div[role="menu"] span:contains("Table")'),
-          Chain.asStep({}, [
-            tinyUi.cWaitForPopup('wait for popup', 'div[aria-label="Table properties"][role="dialog"]'),
-            Chain.op(function (x) {
-              Assertions.assertPresence(
-                'assert presence of col and row input',
-                {
-                  'label:contains("Cols")': 1,
-                  'label:contains("Rows")': 1
-                }, x);
-            })
-          ])
+          tinyApis.sSetContent(tableHtml),
+          TableTestUtils.sOpenToolbarOn(editor, 'td', [0]),
+          tinyActions.sContentKeystroke(Keys.tab(), {}),
+          tinyApis.sAssertSelection([0, 0, 0, 0], 0, [0, 0, 0, 0], 1)
         ]))
       ], onSuccess, onFailure);
     }, {
       plugins: 'table',
-      table_grid: false,
+      table_tab_navigation: false,
       skin_url: '/project/src/skins/lightgray/dist/lightgray'
     }, success, failure);
   }
