610648ccb7bf2a0dc54a400c4844f7742225d514
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2017-07-18 14:51:59 +0200

    TINY-1128: reset testfile

--- src/core/src/test/js/browser/EditorSettingsTest.js
+++ src/core/src/test/js/browser/EditorSettingsTest.js
@@ -2,14 +2,13 @@ asynctest(
   'browser.tinymce.core.EditorSettingsTest',
   [
     'ephox.agar.api.Assertions',
-    'ephox.agar.api.Logger',
     'ephox.agar.api.Pipeline',
     'ephox.agar.api.Step',
     'ephox.mcagar.api.TinyLoader',
     'tinymce.core.EditorSettings',
     'tinymce.themes.modern.Theme'
   ],
-  function (Assertions, Logger, Pipeline, Step, TinyLoader, EditorSettings, Theme) {
+  function (Assertions, Pipeline, Step, TinyLoader, EditorSettings, Theme) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
@@ -17,29 +16,27 @@ asynctest(
 
     TinyLoader.setup(function (editor, onSuccess, onFailure) {
       Pipeline.async({}, [
-        Logger.t('Test basic settings',
-          Step.sync(function () {
-            var settings = EditorSettings.getEditorSettings(
-              editor,
-              'id',
-              'documentBaseUrl',
-              {
-                plugins: ['a']
-              },
-              {
-                validate: false,
-                userSetting: 'x'
-              }
-            );
+        Step.sync(function () {
+          var settings = EditorSettings.getEditorSettings(
+            editor,
+            'id',
+            'documentBaseUrl',
+            {
+              plugins: ['a']
+            },
+            {
+              validate: false,
+              userSetting: 'x'
+            }
+          );
 
-            Assertions.assertEq('Should have the specified id', 'id', settings.id);
-            Assertions.assertEq('Should have the specified documentBaseUrl', 'documentBaseUrl', settings.document_base_url);
-            Assertions.assertEq('Should have the specified userSetting', 'x', settings.userSetting);
-            Assertions.assertEq('Should have the forced validate setting', true, settings.validate);
-            Assertions.assertEq('Should have the default theme', 'modern', settings.theme);
-            Assertions.assertEq('Should have the default overrides plugins', ['a'], settings.plugins);
-          })
-        )
+          Assertions.assertEq('Should have the specified id', 'id', settings.id);
+          Assertions.assertEq('Should have the specified documentBaseUrl', 'documentBaseUrl', settings.document_base_url);
+          Assertions.assertEq('Should have the specified userSetting', 'x', settings.userSetting);
+          Assertions.assertEq('Should have the forced validate setting', true, settings.validate);
+          Assertions.assertEq('Should have the default theme', 'modern', settings.theme);
+          Assertions.assertEq('Should have the default overrides plugins', ['a'], settings.plugins);
+        })
       ], onSuccess, onFailure);
     }, {
       skin_url: '/project/src/skins/lightgray/dist/lightgray'
