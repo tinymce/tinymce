832bff0b9d021cb11451e750b702df1ec43fb95a
morgan <morgan.smith@ephox.com>
2017-08-04 09:09:04 +1000

    TM-35-settings: refactoring code for converting menu layers

--- src/themes/mobile/src/main/js/util/StyleConversions.js
+++ src/themes/mobile/src/main/js/util/StyleConversions.js
@@ -8,46 +8,45 @@ define(
   ],
 
   function (Objects, Arr, Merger) {
+
+    var getFromExpandingItem = function (item) {
+      var newItem = Objects.exclude(item, [ 'items' ]);
+
+      var rest = getAlpha(item.items);
+
+      var newMenus = Merger.deepMerge(
+        rest.menus,
+        Objects.wrap(item.title, rest.items)
+      );
+      var newExpansions = Merger.deepMerge(
+        rest.expansions,
+        Objects.wrap(item.title, item.title)
+      );
+
+      return {
+        item: newItem,
+        menus: newMenus,
+        expansions: newExpansions
+      };
+    };
+
+    var getFromItem = function (item) {
+      return Objects.hasKey(item, 'items') ? getFromExpandingItem(item) : {
+        item: item,
+        menus: { },
+        expansions: { }
+      };
+    };
+
     // Takes items, and consolidates them into its return value
     var getAlpha = function (items) {
       return Arr.foldr(items, function (acc, item) {
-        var r = (function () {
-          if (item.items !== undefined) {
-            // We have a submenu
-            var menuName = item.title;
-            var rest = getAlpha(item.items);
-            var newMenus = Merger.deepMerge(
-              acc.menus,
-              rest.menus,
-              Objects.wrap(menuName, rest.items)
-            );
-            var newExpansions = Merger.deepMerge(
-              acc.expansions,
-              rest.expansions,
-              Objects.wrap(menuName, menuName)
-            );
-            var newItems = [ Objects.exclude(item, [ 'items' ]) ].concat(acc.items);
-            return {
-              menus: newMenus,
-              expansions: newExpansions,
-              items: newItems
-            };
-          } else {
-            // FIX: Remove hoisting
-            var newItems = [ item ].concat(acc.items);
-            var newExpansions = acc.expansions;
-            var newMenus = acc.menus;
-            return {
-              menus: newMenus,
-              expansions: newExpansions,
-              items: newItems
-            };
-          }
-        })();
-
-
-        console.log('item', item.title, 'r', r, 'initially', acc);
-        return r;
+        var newData = getFromItem(item);
+        return {
+          menus: Merger.deepMerge(acc.menus, newData.menus),
+          items: [ newData.item ].concat(acc.items),
+          expansions: Merger.deepMerge(acc.expansions, newData.expansions)
+        };
       }, {
         menus: { },
         expansions: { },
