14c0f085a09af1641c52189964c7099905d569b5
Joshua Haines <josh.j.haines@gmail.com>
2017-08-07 13:36:03 +1000

    TBIO-4980: update blocks test to have thead and tfoot

--- src/test/js/atomic/lookup/BlocksTest.js
+++ src/test/js/atomic/lookup/BlocksTest.js
@@ -11,9 +11,10 @@ test(
     var s = Structs.detail;
     var f = Structs.rowdata;
     var warehouse = Warehouse.generate([
-      f('r1', [ s('a', 1, 1), s('b', 1, 2) ], 'tbody'),
+      f('r1', [ s('a', 1, 1), s('b', 1, 2) ], 'thead'),
       f('r2', [ s('c', 2, 1), s('d', 1, 1), s('e', 1, 1) ], 'tbody'),
-      f('r3', [ s('f', 1, 2) ], 'tbody')
+      f('r2', [ s('f', 1, 1), s('g', 1, 1) ], 'tbody'),
+      f('r3', [ s('h', 1, 1), s('i', 1, 2) ], 'tfoot')
     ]);
   
     assert.eq(['a', 'd', 'e'], Blocks.columns(warehouse).map(function (c) { return c.getOrDie(); }));
