3eb47621953bc4635262d637e7245129dd3b480b
morgan <morgan.smith@ephox.com>
2017-08-22 13:41:32 +1000

    TM-66: asserting states of buttons

--- src/themes/mobile/src/test/js/browser/ui/ListTest.js
+++ src/themes/mobile/src/test/js/browser/ui/ListTest.js
@@ -2,12 +2,19 @@ asynctest(
   'Browser Test: ui.ListTest',
 
   [
+    'ephox.agar.api.Assertions',
+    'ephox.agar.api.Logger',
+    'ephox.agar.api.Pipeline',
+    'ephox.agar.api.Step',
     'ephox.alloy.api.behaviour.Behaviour',
     'ephox.alloy.api.behaviour.Replacing',
+    'ephox.alloy.api.behaviour.Toggling',
     'ephox.alloy.api.component.GuiFactory',
+    'ephox.alloy.api.component.Memento',
     'ephox.alloy.api.system.Attachment',
     'ephox.alloy.api.system.Gui',
     'ephox.katamari.api.Fun',
+    'ephox.mcagar.api.TinyApis',
     'ephox.mcagar.api.TinyLoader',
     'ephox.sugar.api.node.Body',
     'tinymce.core.ThemeManager',
@@ -15,7 +22,10 @@ asynctest(
     'tinymce.themes.mobile.Theme'
   ],
 
-  function (Behaviour, Replacing, GuiFactory, Attachment, Gui, Fun, TinyLoader, Body, ThemeManager, Features, Theme) {
+  function (
+    Assertions, Logger, Pipeline, Step, Behaviour, Replacing, Toggling, GuiFactory, Memento, Attachment, Gui, Fun, TinyApis, TinyLoader, Body, ThemeManager,
+    Features, Theme
+  ) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
@@ -63,15 +73,48 @@ asynctest(
 
     TinyLoader.setup(function (editor, onSuccess, onFailure) {
       var features = Features.setup(realm, editor);
+
+      var apis = TinyApis(editor);
+
       console.log('features', features);
 
-      Replacing.set(toolbar, [
-        features.bullist.spec(),
+      var memBullist = Memento.record(
+        features.bullist.spec()
+      );
+
+      var memNumlist = Memento.record(
         features.numlist.spec()
+      );
+
+      Replacing.set(toolbar, [
+        memBullist.asSpec(),
+        memNumlist.asSpec()
       ]);
 
+      var sSetP1 = apis.sSetSelection([ 0, 0, 0 ], 'Thi'.length, [ 0, 0, 0 ], 'Thi'.length);
+      var sSetP2 = apis.sSetSelection([ 1, 0 ], 'Norma'.length, [ 1, 0 ], 'Norma'.length);
+      var sSetP3 = apis.sSetSelection([ 2, 0, 0 ], 'Bu'.length, [ 2, 0, 0 ], 'Bu'.length);
 
-      onFailure('no');
+      var sAssertListIs = function (label, mem, state) {
+        return Logger.t(label, Step.sync(function () {
+          var button = mem.get(socket);
+          Assertions.assertEq('Selected state of button', state, Toggling.isOn(button));
+        }));
+      };
+
+      Pipeline.async({}, [
+        apis.sFocus,
+        apis.sSetContent(
+          '<ol><li>This is an ordered list</li></ol><p>Normal paragraph</p><ul><li>Bullet list</li></ul>'
+        ),
+        sSetP1,
+        // sAssertListIs('numlist: initial selection in p1', memNumlist, true),
+        Step.sync(function () {
+          var numlist = memNumlist.get(socket);
+          Assertions.assertEq('The numlist list should be selected', true, Toggling.isOn(numlist));
+        }),
+        Step.fail('Just checking toolbar')
+      ], onSuccess, onFailure);
     }, {
       theme: 'headlesstest'
       // toolbar: 'bullist numlist undo',
