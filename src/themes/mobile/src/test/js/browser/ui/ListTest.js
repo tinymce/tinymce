21dbe0d66361014b1b311577657b97c224a02d3e
morgan <morgan.smith@ephox.com>
2017-08-23 09:10:45 +1000

    TM-93: separated out test theme

--- src/themes/mobile/src/test/js/browser/ui/ListTest.js
+++ src/themes/mobile/src/test/js/browser/ui/ListTest.js
@@ -4,14 +4,9 @@ asynctest(
   [
     'ephox.agar.api.GeneralSteps',
     'ephox.agar.api.Pipeline',
-    'ephox.alloy.api.behaviour.Behaviour',
     'ephox.alloy.api.behaviour.Replacing',
-    'ephox.alloy.api.component.GuiFactory',
     'ephox.alloy.api.component.Memento',
-    'ephox.alloy.api.system.Attachment',
-    'ephox.alloy.api.system.Gui',
     'ephox.alloy.test.GuiSetup',
-    'ephox.katamari.api.Fun',
     'ephox.mcagar.api.TinyApis',
     'ephox.mcagar.api.TinyLoader',
     'ephox.sugar.api.node.Body',
@@ -22,145 +17,116 @@ asynctest(
     'tinymce.themes.mobile.util.FormatChangers'
   ],
 
-  function (
-    GeneralSteps, Pipeline, Behaviour, Replacing, GuiFactory, Memento, Attachment, Gui, GuiSetup, Fun, TinyApis, TinyLoader, Body, Traverse, Features, TestTheme,
-    TestUi, FormatChangers
-  ) {
+  function (GeneralSteps, Pipeline, Replacing, Memento, GuiSetup, TinyApis, TinyLoader, Body, Traverse, Features, TestTheme, TestUi, FormatChangers) {
     var success = arguments[arguments.length - 2];
     var failure = arguments[arguments.length - 1];
 
     /* This test is going to create a toolbar with both list items on it */
-    var alloy = Gui.create();
-
     var body = Body.body();
-    Attachment.attachSystem(body, alloy);
 
-    var toolbar = GuiFactory.build({
-      dom: {
-        tag: 'div',
-        classes: [ 'test-toolbar' ]
-      },
-      behaviours: Behaviour.derive([
-        Replacing.config({ })
-      ])
-    });
+    TestTheme.setup(body).use(
+      function (realm, toolbar, socket) {
 
-    var socket = GuiFactory.build({
-      dom: {
-        tag: 'div',
-        classes: [ 'test-socket' ]
+        TinyLoader.setup(function (editor, onSuccess, onFailure) {
+          var features = Features.setup(realm, editor);
+
+          FormatChangers.init(realm, editor);
+
+          var apis = TinyApis(editor);
+
+          var memBullist = Memento.record(
+            features.bullist.spec()
+          );
+
+          var memNumlist = Memento.record(
+            features.numlist.spec()
+          );
+
+          Replacing.set(toolbar, [
+            memBullist.asSpec(),
+            memNumlist.asSpec()
+          ]);
+
+          var sSetP1 = apis.sSetSelection([ 0, 0, 0 ], 'Thi'.length, [ 0, 0, 0 ], 'Thi'.length);
+          var sSetP2 = apis.sSetSelection([ 1, 0 ], 'Norma'.length, [ 1, 0 ], 'Norma'.length);
+          var sSetP3 = apis.sSetSelection([ 2, 0, 0 ], 'Bu'.length, [ 2, 0, 0 ], 'Bu'.length);
+
+          var sCheckComponent = function (label, state) {
+            return function (memento) {
+              return TestUi.sWaitForToggledState(label, state, realm, memento);
+            };
+          };
+
+          var sCheckLists = function (situation, stateOfNumlist, stateOfBullist) {
+            return GeneralSteps.sequence([
+              sCheckComponent('checking numlist: ' + situation, stateOfNumlist)(memNumlist),
+              sCheckComponent('checking bullist: ' + situation, stateOfBullist)(memBullist)
+            ]);
+          };
+
+          var sCheckInNumlist = function (situation) {
+            return sCheckLists(situation, true, false);
+          };
+
+          var sCheckInBullist = function (situation) {
+            return sCheckLists(situation, false, true);
+          };
+
+          var sCheckInNoList = function (situation) {
+            return sCheckLists(situation, false, false);
+          };
+
+          var sCheckP1 = function (situation) {
+            return GeneralSteps.sequence([
+              sSetP1,
+              sCheckInNumlist(situation)
+            ]);
+          };
+
+          var sCheckP2 = function (situation) {
+            return GeneralSteps.sequence([
+              sSetP2,
+              sCheckInNoList(situation)
+            ]);
+          };
+
+          var sCheckP3 = function (situation) {
+            return GeneralSteps.sequence([
+              sSetP3,
+              sCheckInBullist(situation)
+            ]);
+          };
+
+          Pipeline.async({}, [
+            GuiSetup.mAddStyles(Traverse.owner(body), [
+              '.tinymce-mobile-toolbar-button { padding: 2px; border: 1px solid black; background: white; }',
+              '.tinymce-mobile-toolbar-button.tinymce-mobile-toolbar-button-selected { background: #cadbee; }',
+              '.tinymce-mobile-icon-unordered-list:before { content: "ul"; }',
+              '.tinymce-mobile-icon-ordered-list:before { content: "ol"; }'
+            ]),
+            apis.sFocus,
+            apis.sSetContent(
+              '<ol><li>This is an ordered list</li></ol><p>Normal paragraph</p><ul><li>Bullet list</li></ul>'
+            ),
+            sCheckP1('initial selection in ol'),
+            sCheckP2('ol >>> p'),
+            sCheckP3('p >>> ul'),
+            sCheckP1('ul >>> ol'),
+
+            TestUi.sClickComponent(realm, memBullist),
+            sCheckInBullist('ol converted to ul'),
+            TestUi.sClickComponent(realm, memNumlist),
+            sCheckInNumlist('ul converted back to ol'),
+            TestUi.sClickComponent(realm, memNumlist),
+            sCheckInNoList('ol converted to p'),
+            GuiSetup.mRemoveStyles
+          ], onSuccess, onFailure);
+        }, {
+          theme: TestTheme.name()
+        }, function () {
+          success();
+        }, failure);
       }
-    });
-
-    alloy.add(toolbar);
-    alloy.add(socket);
-
-    TestTheme.setup(alloy, socket);
-
-    var realm = {
-      system: Fun.constant(alloy),
-      socket: Fun.constant(socket)
-    };
-
-    TinyLoader.setup(function (editor, onSuccess, onFailure) {
-      var features = Features.setup(realm, editor);
-
-      FormatChangers.init(realm, editor);
-
-      var apis = TinyApis(editor);
-
-      var memBullist = Memento.record(
-        features.bullist.spec()
-      );
-
-      var memNumlist = Memento.record(
-        features.numlist.spec()
-      );
-
-      Replacing.set(toolbar, [
-        memBullist.asSpec(),
-        memNumlist.asSpec()
-      ]);
-
-      var sSetP1 = apis.sSetSelection([ 0, 0, 0 ], 'Thi'.length, [ 0, 0, 0 ], 'Thi'.length);
-      var sSetP2 = apis.sSetSelection([ 1, 0 ], 'Norma'.length, [ 1, 0 ], 'Norma'.length);
-      var sSetP3 = apis.sSetSelection([ 2, 0, 0 ], 'Bu'.length, [ 2, 0, 0 ], 'Bu'.length);
-
-      var sCheckComponent = function (label, state) {
-        return function (memento) {
-          return TestUi.sWaitForToggledState(label, state, realm, memento);
-        };
-      };
-
-      var sCheckLists = function (situation, stateOfNumlist, stateOfBullist) {
-        return GeneralSteps.sequence([
-          sCheckComponent('checking numlist: ' + situation, stateOfNumlist)(memNumlist),
-          sCheckComponent('checking bullist: ' + situation, stateOfBullist)(memBullist)
-        ]);
-      };
-
-      var sCheckInNumlist = function (situation) {
-        return sCheckLists(situation, true, false);
-      };
-
-      var sCheckInBullist = function (situation) {
-        return sCheckLists(situation, false, true);
-      };
-
-      var sCheckInNoList = function (situation) {
-        return sCheckLists(situation, false, false);
-      };
-
-      var sCheckP1 = function (situation) {
-        return GeneralSteps.sequence([
-          sSetP1,
-          sCheckInNumlist(situation)
-        ]);
-      };
-
-      var sCheckP2 = function (situation) {
-        return GeneralSteps.sequence([
-          sSetP2,
-          sCheckInNoList(situation)
-        ]);
-      };
-
-      var sCheckP3 = function (situation) {
-        return GeneralSteps.sequence([
-          sSetP3,
-          sCheckInBullist(situation)
-        ]);
-      };
-
-      Pipeline.async({}, [
-        GuiSetup.mAddStyles(Traverse.owner(body), [
-          '.tinymce-mobile-toolbar-button { padding: 2px; border: 1px solid black; background: white; }',
-          '.tinymce-mobile-toolbar-button.tinymce-mobile-toolbar-button-selected { background: #cadbee; }',
-          '.tinymce-mobile-icon-unordered-list:before { content: "ul"; }',
-          '.tinymce-mobile-icon-ordered-list:before { content: "ol"; }'
-        ]),
-        apis.sFocus,
-        apis.sSetContent(
-          '<ol><li>This is an ordered list</li></ol><p>Normal paragraph</p><ul><li>Bullet list</li></ul>'
-        ),
-        sCheckP1('initial selection in ol'),
-        sCheckP2('ol >>> p'),
-        sCheckP3('p >>> ul'),
-        sCheckP1('ul >>> ol'),
-
-        TestUi.sClickComponent(realm, memBullist),
-        sCheckInBullist('ol converted to ul'),
-        TestUi.sClickComponent(realm, memNumlist),
-        sCheckInNumlist('ul converted back to ol'),
-        TestUi.sClickComponent(realm, memNumlist),
-        sCheckInNoList('ol converted to p'),
-        GuiSetup.mRemoveStyles
-      ], onSuccess, onFailure);
-    }, {
-      theme: TestTheme.name()
-    }, function () {
-      success();
-    }, failure);
-
+    );
   }
 );
--- src/themes/mobile/src/test/js/module/test/theme/TestTheme.js
+++ src/themes/mobile/src/test/js/module/test/theme/TestTheme.js
@@ -2,14 +2,52 @@ define(
   'tinymce.themes.mobile.test.theme.TestTheme',
 
   [
+    'ephox.alloy.api.behaviour.Behaviour',
+    'ephox.alloy.api.behaviour.Replacing',
+    'ephox.alloy.api.component.GuiFactory',
+    'ephox.alloy.api.system.Attachment',
+    'ephox.alloy.api.system.Gui',
     'ephox.katamari.api.Fun',
     'tinymce.core.ThemeManager'
   ],
 
-  function (Fun, ThemeManager) {
+  function (Behaviour, Replacing, GuiFactory, Attachment, Gui, Fun, ThemeManager) {
     var name = 'test';
 
-    var setup = function (alloy, socket) {
+
+    var setup = function (container) {
+
+      /* This test is going to create a toolbar with both list items on it */
+      var alloy = Gui.create();
+
+      Attachment.attachSystem(container, alloy);
+
+      var toolbar = GuiFactory.build({
+        dom: {
+          tag: 'div',
+          classes: [ 'test-toolbar' ]
+        },
+        behaviours: Behaviour.derive([
+          Replacing.config({ })
+        ])
+      });
+
+      var socket = GuiFactory.build({
+        dom: {
+          tag: 'div',
+          classes: [ 'test-socket' ]
+        }
+      });
+
+      alloy.add(toolbar);
+      alloy.add(socket);
+
+      var realm = {
+        system: Fun.constant(alloy),
+        socket: Fun.constant(socket)
+      };
+
+
       ThemeManager.add(name, function (editor) {
         return {
           renderUI: function (args) {
@@ -21,6 +59,12 @@ define(
           }
         };
       });
+
+      return {
+        use: function (f) {
+          f(realm, toolbar, socket);
+        }
+      };
     };
 
     return {
