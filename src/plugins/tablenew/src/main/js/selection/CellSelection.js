6cbda77b443b58d206ad47a36b755bbfcbedc33f
Joshua Haines <josh.j.haines@gmail.com>
2017-08-24 09:31:32 +1000

    TBIO-4987: window range setting

--- src/plugins/tablenew/src/main/js/selection/CellSelection.js
+++ src/plugins/tablenew/src/main/js/selection/CellSelection.js
@@ -5,6 +5,8 @@ define(
     'ephox.darwin.api.InputHandlers',
     'ephox.darwin.api.SelectionKeys',
     'ephox.darwin.api.TableSelection',
+    'ephox.fussy.api.SelectionRange',
+    'ephox.fussy.api.WindowSelection',
     'ephox.katamari.api.Fun',
     'ephox.katamari.api.Option',
     'ephox.snooker.api.TableLookup',
@@ -15,7 +17,7 @@ define(
     'tinymce.plugins.tablenew.queries.Direction'
   ],
 
-  function (InputHandlers, SelectionKeys, TableSelection, Fun, Option, TableLookup, Compare, DomEvent, MouseEvent, Element, Direction) {
+  function (InputHandlers, SelectionKeys, TableSelection, SelectionRange, WindowSelection, Fun, Option, TableLookup, Compare, DomEvent, MouseEvent, Element, Direction) {
     return function (editor, lazyResize) {
       editor.on('init', function (e) {
         var win = editor.getWin();
@@ -48,9 +50,8 @@ define(
             event.kill();
           }
           response.selection().each(function (ns) {
-            // var range = SelectionRange.write(ns.start(), ns.finish());
-            // WindowSelection.set(win, range);
-            //editor.selection.setRng(.setRelativeSelection(ns.start(), ns.finish());
+            var range = SelectionRange.write(ns.start(), ns.finish());
+            WindowSelection.set(win, range);
           });
         };
 
@@ -75,10 +76,8 @@ define(
           var startContainer = Element.fromDom(editor.selection.getStart());
           var start = Element.fromDom(rng.startContainer);
           var end = Element.fromDom(rng.endContainer);
-          console.log(editor.selection.getStart(), editor.selection.getRng());
           var direction = Direction.directionAt(startContainer).isRtl() ? SelectionKeys.rtl : SelectionKeys.ltr;
           keyHandlers.keydown(event, start, rng.startOffset, end, rng.endOffset, direction).each(function (response) {
-            console.log('keydownresponse');
             handleResponse(event, response);
           });
           lazyResize().each(function (resize) {
