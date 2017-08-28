eed6881c3b9401d26134a4e016b92c25dabe3299
morgan <morgan.smith@ephox.com>
2017-08-28 11:12:11 +1000

    TM-86: adding simpler tasks

--- src/themes/mobile/Gruntfile.js
+++ src/themes/mobile/Gruntfile.js
@@ -241,6 +241,8 @@ module.exports = function (grunt) {
   grunt.task.loadTasks('../../../node_modules/grunt-contrib-watch/tasks');
   grunt.task.loadTasks('../../../node_modules/grunt-replace/tasks');
 
+  grunt.registerTask('copy-dist', ['copy-dist-mobile-theme', 'copy:dist-mobile-css']);
+
   grunt.registerTask('dist', ['bolt-init', 'bolt-build', 'replace:mobile-version', 'copy:dist-mobile-theme', 'copy:dist-mobile-css', /*'eslint',*/ 'uglify:theme']);
   grunt.registerTask('atomic-tests', ['bolt-build', 'bolt-test:atomic']);
   grunt.registerTask('phantom-tests', ['bedrock-auto:phantomjs']);
