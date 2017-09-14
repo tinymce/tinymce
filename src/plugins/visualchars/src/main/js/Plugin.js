9205d36cee0d53130a6d0f04df5ef9e51349341a
Spocke <spocke@moxiecode.com>
2017-09-14 17:59:27 +0200

    TINY-1227: separated more plugins

--- src/plugins/visualchars/src/main/js/Plugin.js
+++ src/plugins/visualchars/src/main/js/Plugin.js
@@ -7,71 +7,23 @@
  * License: http://www.tinymce.com/license
  * Contributing: http://www.tinymce.com/contributing
  */
+
 define(
   'tinymce.plugins.visualchars.Plugin',
   [
+    'ephox.katamari.api.Cell',
     'tinymce.core.PluginManager',
-    'tinymce.core.util.Delay',
-    'ephox.katamari.api.Arr',
-    'ephox.sugar.api.node.Element',
-    'tinymce.plugins.visualchars.core.VisualChars'
+    'tinymce.plugins.visualchars.api.Commands',
+    'tinymce.plugins.visualchars.core.Keyboard',
+    'tinymce.plugins.visualchars.ui.Buttons'
   ],
-  function (PluginManager, Delay, Arr, Element, VisualChars) {
+  function (Cell, PluginManager, Commands, Keyboard, Buttons) {
     PluginManager.add('visualchars', function (editor) {
-      var self = this, state;
+      var toggleState = Cell(false);
 
-      var toggleActiveState = function () {
-        var self = this;
-
-        editor.on('VisualChars', function (e) {
-          self.active(e.state);
-        });
-      };
-
-      var debouncedToggle = Delay.debounce(function () {
-        VisualChars.toggle(editor);
-      }, 300);
-
-      if (editor.settings.forced_root_block !== false) {
-        editor.on('keydown', function (e) {
-          if (self.state === true) {
-            e.keyCode === 13 ? VisualChars.toggle(editor) : debouncedToggle();
-          }
-        });
-      }
-
-      editor.addCommand('mceVisualChars', function () {
-        var body = editor.getBody(), selection = editor.selection, bookmark;
-
-        state = !state;
-        self.state = state;
-        editor.fire('VisualChars', { state: state });
-
-        bookmark = selection.getBookmark();
-
-        if (state === true) {
-          VisualChars.show(editor, body);
-        } else {
-          VisualChars.hide(editor, body);
-        }
-
-        selection.moveToBookmark(bookmark);
-      });
-
-      editor.addButton('visualchars', {
-        title: 'Show invisible characters',
-        cmd: 'mceVisualChars',
-        onPostRender: toggleActiveState
-      });
-
-      editor.addMenuItem('visualchars', {
-        text: 'Show invisible characters',
-        cmd: 'mceVisualChars',
-        onPostRender: toggleActiveState,
-        selectable: true,
-        context: 'view',
-        prependToContext: true
-      });
+      Commands.register(editor, toggleState);
+      Buttons.register(editor);
+      Keyboard.setup(editor, toggleState);
     });
 
     return function () {};
