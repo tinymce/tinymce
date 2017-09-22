32b9e881099555675c5ca440cb9977f645c3adff
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2017-09-22 14:08:19 +0200

    TINY-1256: added selection bookmark module with some tests

---
+++ src/core/src/main/js/selection/SelectionBookmark.js
@@ -0,0 +1,83 @@
+define(
+  'tinymce.core.selection.SelectionBookmark',
+
+  [
+    'ephox.katamari.api.Option',
+    'ephox.sugar.api.dom.Compare',
+    'ephox.sugar.api.node.Node',
+    'ephox.sugar.api.node.Text',
+    'ephox.sugar.api.search.Traverse',
+    'ephox.sugar.api.selection.Selection',
+    'ephox.sugar.api.selection.WindowSelection'
+  ],
+
+  function (Option, Compare, Node, Text, Traverse, Selection, WindowSelection) {
+    var clamp = function (offset, element) {
+      var max = Node.isText(element) ? Text.get(element).length : Traverse.children(element).length + 1;
+
+      if (offset > max) {
+        return max;
+      } else if (offset < 0) {
+        return 0;
+      }
+
+      return offset;
+    };
+
+    var normalizeRng = function (rng) {
+      return Selection.range(
+        rng.start(),
+        clamp(rng.soffset(), rng.start()),
+        rng.finish(),
+        clamp(rng.foffset(), rng.finish())
+      );
+    };
+
+    var isOrContains = function (root, elm) {
+      return Compare.contains(root, elm) || Compare.eq(root, elm);
+    };
+
+    var isRngInRoot = function (root) {
+      return function (rng) {
+        return isOrContains(root, rng.start()) && isOrContains(root, rng.finish());
+      };
+    };
+
+    var getBookmark = function (root) {
+      var win = Traverse.defaultView(root);
+
+      return WindowSelection.getExact(win.dom())
+        .filter(isRngInRoot(root));
+    };
+
+    var setBookmark = function (root, bookmark) {
+      var win = Traverse.defaultView(root);
+
+      validate(root, bookmark)
+        .each(function (rng) {
+          WindowSelection.setExact(win.dom(), rng.start(), rng.soffset(), rng.finish(), rng.foffset());
+        });
+    };
+
+    var validate = function (root, bookmark) {
+      return Option.from(bookmark)
+          .filter(isRngInRoot(root))
+          .map(normalizeRng);
+    };
+
+    var bookmarkToNativeRng = function (bookmark) {
+      var rng = document.createRange();
+      rng.setStart(bookmark.start().dom(), bookmark.soffset());
+      rng.setEnd(bookmark.finish().dom(), bookmark.foffset());
+
+      return Option.some(rng);
+    };
+
+    return {
+      getBookmark: getBookmark,
+      setBookmark: setBookmark,
+      validate: validate,
+      bookmarkToNativeRng: bookmarkToNativeRng
+    };
+  }
+);
