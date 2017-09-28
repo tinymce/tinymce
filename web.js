55bacc3e861768761513f1e51d27325f60507d26
Nick Small <nick@shopify.com>
2017-09-28 18:27:43 -0400

    Add web package plugins

---
+++ web.js
@@ -0,0 +1,7 @@
+var tinymce = require('./index.js');
+module.exports = tinymce;
+
+require('./plugins/autoresize/plugin.js');
+require('./plugins/table/plugin.js');
+require('./plugins/paste/plugin.js');
+require('./plugins/noparsing/plugin.js');
