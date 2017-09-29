071eaf929e4cecef1d7a12f0eb2ad52b4483f23e
Joshua Haines <josh.j.haines@gmail.com>
2017-09-29 10:17:48 +1000

    TBIO-4984: ready phoenix for open sourcing

--- src/demo/js/ephox/phoenix/demo/SearchDemo.js
+++ src/demo/js/ephox/phoenix/demo/SearchDemo.js
@@ -5,22 +5,41 @@ define(
     'ephox.katamari.api.Arr',
     'ephox.phoenix.api.dom.DomSearch',
     'ephox.phoenix.api.dom.DomWrapping',
+    'ephox.sugar.api.dom.Insert',
+    'ephox.sugar.api.dom.InsertAll',
+    'ephox.sugar.api.events.DomEvent',
+    'ephox.sugar.api.node.Element',
     'ephox.sugar.api.properties.Attr',
     'ephox.sugar.api.properties.Class',
     'ephox.sugar.api.properties.Css',
-    'ephox.sugar.api.events.DomEvent',
-    'ephox.sugar.api.node.Element',
-    'ephox.sugar.api.dom.Insert',
-    'ephox.sugar.api.dom.InsertAll',
-    'ephox.sugar.api.properties.Value',
-    'text!html/content.html'
+    'ephox.sugar.api.properties.Value'
   ],
 
-  function (Arr, DomSearch, DomWrapping, Attr, Class, Css, DomEvent, Element, Insert, InsertAll, Value, ContentHtml) {
+  function (Arr, DomSearch, DomWrapping, Insert, InsertAll, DomEvent, Element, Attr, Class, Css, Value) {
     return function () {
       var container = Element.fromTag('div');
 
-      var content = Element.fromHtml(ContentHtml);
+      var contentHtml = '<div>' +
+      '<p>This is the first paragraph<\/p>' +
+      '<p>This is the second paragraph<\/p>' +
+      '<p>The third paragraph is <span>significantly <span>more tricky<\/span> than the other stuff \'twas twas<\/span>.' +
+      '<table>' +
+      '<thead>' +
+      '<tr>' +
+      '<th>Column 1<\/th>' +
+      '<th>Column 2<\/th>' +
+      '<\/tr>' +
+      '<\/thead>' +
+      '<tbody>' +
+      '<tr>' +
+      '<td>Value 1<\/td>' +
+      '<td>Value 2<\/td>' +
+      '<\/tr>' +
+      '<\/tbody>' +
+      '<\/table>' +
+      '<\/div>';
+
+      var content = Element.fromHtml(contentHtml);
       var input = Element.fromTag('input');
       var button = Element.fromTag('button');
       Insert.append(button, Element.fromText('Highlight token'));
@@ -58,7 +77,7 @@ define(
           DomWrapping.wrapper(x.elements(), wrapper);
         });
       };
-      
+
       InsertAll.append(container, [input, button, buttonWord, content]);
 
       var ephoxUi = Element.fromDom(document.getElementById('ephox-ui'));
