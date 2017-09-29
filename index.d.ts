1210ee9606756d2e07ada756c5d10bd4a496fb91
Nick Small <nick@shopify.com>
2017-09-29 15:05:08 -0400

    Retype tinymce.init to return a Promise

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,10 @@
+import * as tinymce from 'tinymce';
 export * from 'tinymce';
 
-export interface Settings {
+export interface Settings extends tinymce.Settings {
   [key: string]: any,
 }
+
+export function init(settings: Settings): Promise<tinymce.Editor[]>;
 
 export const shopifyConfig: Settings;
