8a05b4e7abb9571d1a4553432eecd9532c467391
Tzvi Melamed <hartley@melamed.biz>
2017-10-03 11:38:38 -0400

    Fix typings for init

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
 import * as tinymce from 'tinymce';
-export * from 'tinymce';
 
 export interface Settings extends tinymce.Settings {
   [key: string]: any,
 }
 
-export function init(settings: Settings): Promise<tinymce.Editor[]>;
+declare module 'tinymce' {
+  function init(settings: Settings): Promise<tinymce.Editor[]>;
+}
 
 export const shopifyConfig: Settings;
+export * from 'tinymce';
