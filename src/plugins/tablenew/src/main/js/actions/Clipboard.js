2cb41406e33312738761f6d58f85cbe7e35c07bc
Joshua Haines <josh.j.haines@gmail.com>
2017-10-04 10:38:51 +1000

    TBIO-4987-b: fix bug when pasting into regular non-table areas of the editor

--- src/plugins/tablenew/src/main/js/actions/Clipboard.js
+++ src/plugins/tablenew/src/main/js/actions/Clipboard.js
@@ -4,6 +4,7 @@ define(
   [
     'ephox.katamari.api.Arr',
     'ephox.katamari.api.Fun',
+    'ephox.katamari.api.Option',
     'ephox.snooker.api.CopySelected',
     'ephox.snooker.api.TableFill',
     'ephox.snooker.api.TableLookup',
@@ -17,7 +18,7 @@ define(
     'tinymce.plugins.tablenew.selection.SelectionTypes'
   ],
 
-  function (Arr, Fun, CopySelected, TableFill, TableLookup, Replication, Element, Elements, Node, Html, TableTargets, Ephemera, SelectionTypes) {
+  function (Arr, Fun, Option, CopySelected, TableFill, TableLookup, Replication, Element, Elements, Node, Html, TableTargets, Ephemera, SelectionTypes) {
     var extractSelected = function (cells) {
       // Assume for now that we only have one table (also handles the case where we multi select outside a table)
       return TableLookup.table(cells[0]).map(Replication.deep).map(function (replica) {
@@ -41,26 +42,29 @@ define(
 
       editor.on('BeforeSetContent', function (e) {
         if (e.selection === true && e.paste === true) {
-          var cell = Element.fromDom(editor.dom.getParent(editor.selection.getStart(), 'th,td'));
-          var table = TableLookup.table(cell);
-          table.bind(function (table) {
+          var cellOpt = Option.from(editor.dom.getParent(editor.selection.getStart(), 'th,td'));
+          cellOpt.each(function (domCell) {
+            var cell = Element.fromDom(domCell);
+            var table = TableLookup.table(cell);
+            table.bind(function (table) {
 
-            var elements = Arr.filter(Elements.fromHtml(e.content), function (content) {
-              return Node.name(content) !== 'meta';
-            });
-
-            if (elements.length === 1 && Node.name(elements[0]) === 'table') {
-              e.preventDefault();
-
-              var doc = Element.fromDom(editor.getDoc());
-              var generators = TableFill.paste(doc);
-              var targets = TableTargets.paste(cell, elements[0], generators);
-              actions.pasteCells(table, targets).each(function (rng) {
-                editor.selection.setRng(rng);
-                editor.focus();
-                cellSelection.clear(table);
+              var elements = Arr.filter(Elements.fromHtml(e.content), function (content) {
+                return Node.name(content) !== 'meta';
               });
-            }
+
+              if (elements.length === 1 && Node.name(elements[0]) === 'table') {
+                e.preventDefault();
+
+                var doc = Element.fromDom(editor.getDoc());
+                var generators = TableFill.paste(doc);
+                var targets = TableTargets.paste(cell, elements[0], generators);
+                actions.pasteCells(table, targets).each(function (rng) {
+                  editor.selection.setRng(rng);
+                  editor.focus();
+                  cellSelection.clear(table);
+                });
+              }
+            });
           });
         }
       });
