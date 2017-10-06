abff7ccd46b0feec7793b7d17402956ac5702e7d
Spocke <spocke@moxiecode.com>
2017-10-06 15:05:17 +0200

    TINY-1309: fixed bug where theme: false would render a extra div element for inline editors

--- src/core/src/main/js/init/Init.js
+++ src/core/src/main/js/init/Init.js
@@ -114,7 +114,6 @@ define(
         }
       }
 
-      editor.editorContainer = info.editorContainer;
       info.height = h;
 
       return info;
@@ -134,22 +133,28 @@ define(
       }
 
       info.height = info.iframeHeight ? info.iframeHeight : elm.offsetHeight;
-      editor.editorContainer = info.editorContainer;
 
       return info;
     };
 
-    var renderThemeFalse = function (editor) {
+    var createThemeFalseResult = function (element) {
+      return {
+        editorContainer: element,
+        iframeContainer: element
+      };
+    };
+
+    var renderThemeFalseIframe = function (targetElement) {
       var iframeContainer = DOM.create('div');
 
-      DOM.insertAfter(iframeContainer, editor.getElement());
+      DOM.insertAfter(iframeContainer, targetElement);
 
-      editor.editorContainer = iframeContainer;
+      return createThemeFalseResult(iframeContainer);
+    };
 
-      return {
-        editorContainer: iframeContainer,
-        iframeContainer: iframeContainer
-      };
+    var renderThemeFalse = function (editor) {
+      var targetElement = editor.getElement();
+      return editor.inline ? createThemeFalseResult(targetElement) : renderThemeFalseIframe(targetElement);
     };
 
     var renderThemeUi = function (editor) {
@@ -271,6 +276,7 @@ define(
       initTheme(editor);
       initPlugins(editor);
       boxInfo = renderThemeUi(editor);
+      editor.editorContainer = boxInfo.editorContainer;
 
       // Load specified content CSS last
       if (settings.content_css) {
