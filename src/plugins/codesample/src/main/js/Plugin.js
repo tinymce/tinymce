b6260d0eec0dd918ecd5947b3856c0d77c919dda
David Hunt <bitbucket.davidhunt@holodyn.com>
2017-11-08 11:02:39 -0500

    Added dblClick behavior to the codesample plugin

--- src/plugins/codesample/src/main/js/Plugin.js
+++ src/plugins/codesample/src/main/js/Plugin.js
@@ -16,9 +16,11 @@ define(
     'tinymce.plugins.codesample.api.Commands',
     'tinymce.plugins.codesample.core.FilterContent',
     'tinymce.plugins.codesample.core.LoadCss',
-    'tinymce.plugins.codesample.ui.Buttons'
+    'tinymce.plugins.codesample.ui.Buttons',
+    'tinymce.plugins.codesample.ui.Dialog',
+    'tinymce.plugins.codesample.util.Utils'
   ],
-  function (Cell, PluginManager, Commands, FilterContent, LoadCss, Buttons) {
+  function (Cell, PluginManager, Commands, FilterContent, LoadCss, Buttons, Dialog, Utils) {
     var addedInlineCss = Cell(false);
 
     PluginManager.add('codesample', function (editor, pluginUrl) {
@@ -30,6 +32,12 @@ define(
 
       editor.on('init', function () {
         LoadCss.loadCss(editor, pluginUrl, addedInlineCss, addedCss);
+      });
+
+      editor.on('dblclick', function (ev) {
+        if (Utils.isCodeSample(ev.target)) {
+          Dialog.open(editor);
+        }
       });
     });
 
