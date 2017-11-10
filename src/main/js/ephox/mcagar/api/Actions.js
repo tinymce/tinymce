61b1769600563eeeba4c294246f8331b259a0a7a
Davit Barbakadze <davit.barbakadze@ephox.com>
2017-11-10 09:34:31 +0400

    TINY-1376, Actions: rewrite using NamedChain

--- src/main/js/ephox/mcagar/api/Actions.js
+++ src/main/js/ephox/mcagar/api/Actions.js
@@ -3,13 +3,14 @@ define(
 
   [
     'ephox.agar.api.Chain',
+    'ephox.agar.api.NamedChain',
     'ephox.agar.api.Keyboard',
     'ephox.agar.api.FocusTools',
     'ephox.katamari.api.Fun',
     'ephox.sugar.api.node.Element'
   ],
 
-  function (Chain, Keyboard, FocusTools, Fun, Element) {
+  function (Chain, NamedChain, Keyboard, FocusTools, Fun, Element) {
     var cIDoc = Chain.mapper(function (editor) {
       return Element.fromDom(editor.getDoc());
     });
@@ -19,14 +20,13 @@ define(
     });
 
     var cTriggerKeyEvent = function (cTarget, evtType, code, modifiers) {
-      return Chain.fromParent(Chain.identity, [
-        Chain.fromChains([
-          cTarget,
-          FocusTools.cGetFocused,
-          Chain.op(function (dispatcher) {
-            Keyboard[evtType](code, modifiers !== undefined ? modifiers : {}, dispatcher);
-          })
-        ])
+      return NamedChain.asChain([
+        NamedChain.direct(NamedChain.inputName(), cTarget, 'doc'),
+        NamedChain.direct('doc', FocusTools.cGetFocused, 'activeElement'),
+        NamedChain.direct('activeElement', Chain.op(function (dispatcher) {
+          Keyboard[evtType](code, modifiers !== undefined ? modifiers : {}, dispatcher);
+        }), '_'),
+        NamedChain.output(NamedChain.inputName())
       ]);
     };
 
