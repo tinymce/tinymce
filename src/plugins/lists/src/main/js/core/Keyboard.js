c6fd8c5e7ddf1cf71acdf75a18daecac894c7495
Dima Sokol <sokoldv@ukr.net>
2017-11-13 13:41:38 +0200

    Update Keyboard.js
    Change "cmd" key check to "ctrl" key since only "ctrl" key is used for tab switches

--- src/plugins/lists/src/main/js/core/Keyboard.js
+++ src/plugins/lists/src/main/js/core/Keyboard.js
@@ -20,8 +20,8 @@ define(
   function (VK, Indent, Outdent, Settings, Delete) {
     var setupTabKey = function (editor) {
       editor.on('keydown', function (e) {
-        // Check for tab but not ctrl/cmd+tab since it switches browser tabs
-        if (e.keyCode !== VK.TAB || VK.metaKeyPressed(e)) {
+        // Check for tab but not ctrl+tab since it switches browser tabs on any OS
+        if (e.keyCode !== VK.TAB || (e.ctrlKey && !e.altKey)) {
           return;
         }
 
