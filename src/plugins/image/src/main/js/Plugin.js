7659d2771c8e7aad7a3708ecdedeec2b77eb6690
Davit Barbakadze <davit.barbakadze@ephox.com>
2017-11-14 10:54:42 +0400

    TINY-1370: implement figure.image resizing concept

--- src/plugins/image/src/main/js/Plugin.js
+++ src/plugins/image/src/main/js/Plugin.js
@@ -21,6 +21,28 @@ define(
       FilterContent.setup(editor);
       Buttons.register(editor);
       Commands.register(editor);
+
+      var imgSize, figureSize;
+
+      editor.on('ObjectResizeStart', function (e) {
+        if (editor.dom.is(e.target, 'figure.image')) {
+          figureSize = editor.dom.getSize(e.target);
+          imgSize = editor.dom.getSize(e.target.firstChild);
+        }
+      });
+
+      editor.on('ObjectResized', function (e) {
+        if (editor.dom.is(e.target, 'figure.image')) {
+          var newFigureSize = editor.dom.getSize(e.target);
+
+          if (newFigureSize != figureSize) {
+            editor.dom.setStyles(e.target.firstChild, {
+              width: imgSize.w + (newFigureSize.w - figureSize.w) + 'px',
+              height: imgSize.h + (newFigureSize.h - figureSize.h) + 'px'
+            });
+          }
+        }
+      });
     });
 
     return function () { };
