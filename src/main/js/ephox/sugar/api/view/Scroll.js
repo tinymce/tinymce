02823d872c9a2870f3d299590f0f40222f61fa70
Mike Pheasant <mike.pheasant@ephox.com>
2017-11-17 11:21:27 +1000

    TBIO-5098-2: ditch old comment

--- src/main/js/ephox/sugar/api/view/Scroll.js
+++ src/main/js/ephox/sugar/api/view/Scroll.js
@@ -97,21 +97,6 @@ define(
       };
     };
 
-    // CLIPPY: It looks like you're trying to write a Scroll library :)
-    //
-    // If this is actually required, just browse the JQuery codebase. That's all I did for the other modules.
-    // Looks like it's using window.pageXOffset, window.pageYOffset, and win.scrollTo().
-    //
-    // var top = function (element) {
-    //   $(element.dom()).scrollTop(0);
-    // };
-    //
-    // var bottom = function (element) {};
-    // var left = function (element) {};
-    // var right = function (element) {};
-    // var fromTop = function (element, v) {};
-    // var fromLeft = function (element, v) {};
-
     // TBIO-4472 Safari 10 - Scrolling typeahead with keyboard scrolls page
     var intoView = function (element, alignToTop) {
       if (isSafari && Type.isFunction(element.dom().scrollIntoViewIfNeeded)) {
