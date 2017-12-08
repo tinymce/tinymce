6f64262547ff2464414b009b67c99d15272c6e74
Mike Pheasant <mike.pheasant@ephox.com>
2017-12-08 16:19:25 +1000

    jenkins-browser-testing: unused

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,31 +11,15 @@ module.exports = function(grunt) {
             'all': {
                 config: 'config/bolt/browser.js',
                 // Exclude webdriver tests
-                testfiles: [
-                    'src/test/js/browser/**/*Test.js',
-                    'src/test/js/phantom/**/*Test.js'],
+                testfiles: [ 'src/test/js/browser/**/*Test.js' ],
                 options: { stopOnFailure: true }
             }
         },
 
         'bedrock-auto': {
-            'phantomjs': {
-                config: 'config/bolt/browser.js',
-                // browser tests may still run in phantomjs but they haven't been tried
-                testfiles: [
-                    //'src/test/js/browser/**/*Test.js',
-                    'src/test/js/phantom/**/*Test.js',
-                    'src/test/js/webdriver/**/*Test.js'],
-                browser: 'phantomjs',
-                options: { stopOnFailure: true }
-            },
-
             'chrome': {
                 config: 'config/bolt/browser.js',
-                testfiles: [
-                    'src/test/js/browser/**/*Test.js',
-                    'src/test/js/phantom/**/*Test.js',
-                    'src/test/js/webdriver/**/*Test.js'],
+                testfiles: [ 'src/test/js/browser/**/*Test.js' ],
                 browser: 'chrome',
                 options: { stopOnFailure: true }
             }
