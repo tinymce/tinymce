cdc90182f656fc1e79fb93d89bc00a4de2885a0d
Spocke <spocke@moxiecode.com>
2017-12-11 15:23:31 +0100

    TINY-1461: added main api entry point

---
+++ src/main/ts/ephox/agar/api/Main.ts
@@ -0,0 +1,59 @@
+import ApproxStructure from './ApproxStructure';
+import Arbitraries from './Arbitraries';
+import Assertions from './Assertions';
+import Chain from './Chain';
+import Cursors from './Cursors';
+import FocusTools from './FocusTools';
+import GeneralSteps from './GeneralSteps';
+import Generators from './Generators';
+import GroupStore from './GroupStore';
+import Guard from './Guard';
+import Keyboard from './Keyboard';
+import Keys from './Keys';
+import LegacyAssert from './LegacyAssert';
+import Logger from './Logger';
+import Monitor from './Monitor';
+import Mouse from './Mouse';
+import NamedChain from './NamedChain';
+import Pipeline from './Pipeline';
+import PropertySteps from './PropertySteps';
+import RawAssertions from './RawAssertions';
+import RealClipboard from './RealClipboard';
+import RealKeys from './RealKeys';
+import RealMouse from './RealMouse';
+import Step from './Step';
+import UiControls from './UiControls';
+import UiFinder from './UiFinder';
+import UnitTest from './UnitTest';
+import Waiter from './Waiter';
+
+export {
+  ApproxStructure,
+  Arbitraries,
+  Assertions,
+  Chain,
+  Cursors,
+  FocusTools,
+  GeneralSteps,
+  Generators,
+  GroupStore,
+  Guard,
+  Keyboard,
+  Keys,
+  LegacyAssert,
+  Logger,
+  Monitor,
+  Mouse,
+  NamedChain,
+  Pipeline,
+  PropertySteps,
+  RawAssertions,
+  RealClipboard,
+  RealKeys,
+  RealMouse,
+  Step,
+  UiControls,
+  UiFinder,
+  UnitTest,
+  Waiter
+};
