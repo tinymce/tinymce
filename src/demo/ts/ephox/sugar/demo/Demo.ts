4d2f5de5c3878cd879300d412ce7b91c0649df9f
Spocke <spocke@moxiecode.com>
2017-12-11 14:58:39 +0100

    TINY-1461: fixed demo

--- src/demo/ts/ephox/sugar/demo/Demo.ts
+++ src/demo/ts/ephox/sugar/demo/Demo.ts
@@ -5,40 +5,35 @@ import Element from 'ephox/sugar/api/node/Element';
 import Css from 'ephox/sugar/api/properties/Css';
 import Html from 'ephox/sugar/api/properties/Html';
 
+var container = Element.fromTag('div');
 
+var instructions = Element.fromTag('p');
+Html.set(instructions, 'Clicking on the button will remove "width" from the blue rectangle. Clicking it again will do nothing.');
+Insert.append(container, instructions);
 
-export default <any> function () {
-  var container = Element.fromTag('div');
+var button = Element.fromTag('button');
+Html.set(button, 'Click on me');
+var input = Element.fromTag('input');
 
-  var instructions = Element.fromTag('p');
-  Html.set(instructions, 'Clicking on the button will remove "width" from the blue rectangle. Clicking it again will do nothing.');
-  Insert.append(container, instructions);
+InsertAll.append(container, [button, input]);
 
-  var button = Element.fromTag('button');
-  Html.set(button, 'Click on me');
-  var input = Element.fromTag('input');
+var doc = Element.fromDom(document);
+DomEvent.bind(doc, 'click', function (event) {
+  console.log('target: ', event.target().dom());
+  console.log('x: ', event.x());
+  console.log('y: ', event.y());
 
-  InsertAll.append(container, [button, input]);
+  Css.remove(div, 'width');
+});
 
-  var doc = Element.fromDom(document);
-  DomEvent.bind(doc, 'click', function (event) {
-    console.log('target: ', event.target().dom());
-    console.log('x: ', event.x());
-    console.log('y: ', event.y());
+var div = Element.fromTag('div');
+Css.setAll(div, {
+  width: '100px',
+  height: '300px',
+  background: 'blue'
+});
 
-    Css.remove(div, 'width');
-  });
+Insert.append(container, div);
 
-  var div = Element.fromTag('div');
-  Css.setAll(div, {
-    width: '100px',
-    height: '300px',
-    background: 'blue'
-  });
-
-  Insert.append(container, div);
-
-  var ephoxUi = Element.fromDom(document.getElementById('ephox-ui'));
-  Insert.append(ephoxUi, container);
-
-};
+var ephoxUi = Element.fromDom(document.getElementById('ephox-ui'));
+Insert.append(ephoxUi, container);
