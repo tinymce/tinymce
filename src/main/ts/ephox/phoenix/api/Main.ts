02d35a0228bd1899fa887f93e02e86423cc0a9cb
Spocke <spocke@moxiecode.com>
2017-12-11 20:23:00 +0100

    TINY-1461: added main api entry point

---
+++ src/main/ts/ephox/phoenix/api/Main.ts
@@ -0,0 +1,49 @@
+import Focus from './data/Focus';
+import GatherResult from './data/GatherResult';
+import InjectPosition from './data/InjectPosition';
+import NamedPattern from './data/NamedPattern';
+import SplitPosition from './data/SplitPosition';
+import Spot from './data/Spot';
+import TextSplit from './data/TextSplit';
+import DomDescent from './dom/DomDescent';
+import DomExtract from './dom/DomExtract';
+import DomFamily from './dom/DomFamily';
+import DomGather from './dom/DomGather';
+import DomInjection from './dom/DomInjection';
+import DomSearch from './dom/DomSearch';
+import DomSplit from './dom/DomSplit';
+import DomWrapping from './dom/DomWrapping';
+import Descent from './general/Descent';
+import Extract from './general/Extract';
+import Family from './general/Family';
+import Gather from './general/Gather';
+import Injection from './general/Injection';
+import Search from './general/Search';
+import Split from './general/Split';
+import Wrapping from './general/Wrapping';
+
+export {
+  Focus,
+  GatherResult,
+  InjectPosition,
+  NamedPattern,
+  SplitPosition,
+  Spot,
+  TextSplit,
+  DomDescent,
+  DomExtract,
+  DomFamily,
+  DomGather,
+  DomInjection,
+  DomSearch,
+  DomSplit,
+  DomWrapping,
+  Descent,
+  Extract,
+  Family,
+  Gather,
+  Injection,
+  Search,
+  Split,
+  Wrapping
+};
