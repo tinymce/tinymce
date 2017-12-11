b9c49a1fd566fa9503fd9face1cec47396d7aa9a
Spocke <spocke@moxiecode.com>
2017-12-11 16:53:09 +0100

    TINY-1461: added main api entry point

---
+++ src/main/ts/ephox/polaris/api/Main.ts
@@ -0,0 +1,17 @@
+import Arrays from './Arrays';
+import Pattern from './Pattern';
+import PositionArray from './PositionArray';
+import Regexes from './Regexes';
+import Search from './Search';
+import Splitting from './Splitting';
+import Strings from './Strings';
+
+export {
+  Arrays,
+  Pattern,
+  PositionArray,
+  Regexes,
+  Search,
+  Splitting,
+  Strings
+};
