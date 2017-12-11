cd53c464dfdaf82a17653ee1fab2c92841dfe948
Spocke <spocke@moxiecode.com>
2017-12-11 15:20:55 +0100

    TINY-1461: fixed demo

--- src/demo/ts/ephox/jax/demo/Demo.ts
+++ src/demo/ts/ephox/jax/demo/Demo.ts
@@ -4,53 +4,48 @@ import Credentials from 'ephox/jax/api/Credentials';
 import ResponseType from 'ephox/jax/api/ResponseType';
 import { FormData } from '@ephox/sand';
 
-
-
-export default <any> function () {
-
-  var handler = function (label) {
-    return function (result) {
-      result.fold(
-        function (err) {
-          console.error(label, err);
-        },
-        function (val) {
-          console.log(label, val);
-        }
-      );
-    };
+var handler = function (label) {
+  return function (result) {
+    result.fold(
+      function (err) {
+        console.error(label, err);
+      },
+      function (val) {
+        console.log(label, val);
+      }
+    );
   };
-
-
-  Ajax.get(
-    '../../../package.json',
-    ResponseType.json(),
-    Credentials.none(),
-    { }
-  ).get(handler('valid json file'));
-
-  Ajax.get(
-    '../../../src/demo/xml/data.xml',
-    ResponseType.xml(),
-    Credentials.none(),
-    { }
-  ).get(handler('valid xml file'));
-
-  Ajax.get(
-    '../../../package.json',
-    ResponseType.text(),
-    Credentials.none(),
-    { }
-  ).get(handler('valid text file'));
-
-  var formdata = FormData();
-  formdata.append('alpha', 'Alpha');
-  formdata.append('beta', 'Beta');
-  Ajax.post(
-    'http://fake-url/fake-url',
-    ContentType.form(formdata),
-    ResponseType.json(),
-    Credentials.none(),
-    { }
-  ).get(handler('form encoded attempt without a real server'));
 };
+
+
+Ajax.get(
+  '../../../package.json',
+  ResponseType.json(),
+  Credentials.none(),
+  { }
+).get(handler('valid json file'));
+
+Ajax.get(
+  '../../../src/demo/xml/data.xml',
+  ResponseType.xml(),
+  Credentials.none(),
+  { }
+).get(handler('valid xml file'));
+
+Ajax.get(
+  '../../../package.json',
+  ResponseType.text(),
+  Credentials.none(),
+  { }
+).get(handler('valid text file'));
+
+var formdata = FormData();
+formdata.append('alpha', 'Alpha');
+formdata.append('beta', 'Beta');
+Ajax.post(
+  'http://fake-url/fake-url',
+  ContentType.form(formdata),
+  ResponseType.json(),
+  Credentials.none(),
+  { }
+).get(handler('form encoded attempt without a real server'));
