2274861bd2d5f28ec5e5e478447979460f66aced
Spocke <spocke@moxiecode.com>
2017-12-11 15:09:44 +0100

    TINY-1461: added main api entry point

---
+++ src/main/ts/ephox/jax/api/Main.ts
@@ -0,0 +1,13 @@
+import Ajax from './Ajax';
+import ContentType from './ContentType';
+import Credentials from './Credentials';
+import Methods from './Methods';
+import ResponseType from './ResponseType';
+
+export {
+  Ajax,
+  ContentType,
+  Credentials,
+  Methods,
+  ResponseType
+};
