25199667a0f3c5af423482ba80d2b220972ea57f
Spocke <spocke@moxiecode.com>
2017-12-11 14:36:45 +0100

    TINY-1461: added main api entry point

---
+++ src/main/ts/ephox/sugar/api/Main.ts
@@ -0,0 +1,139 @@
+import Compare from './dom/Compare';
+import DocumentPosition from './dom/DocumentPosition';
+import Dom from './dom/Dom';
+import DomFuture from './dom/DomFuture';
+import Focus from './dom/Focus';
+import Hierarchy from './dom/Hierarchy';
+import Insert from './dom/Insert';
+import InsertAll from './dom/InsertAll';
+import Link from './dom/Link';
+import Remove from './dom/Remove';
+import Replication from './dom/Replication';
+import DomEvent from './events/DomEvent';
+import MouseEvent from './events/MouseEvent';
+import Ready from './events/Ready';
+import Resize from './events/Resize';
+import ScrollChange from './events/ScrollChange';
+import Viewable from './events/Viewable';
+import Body from './node/Body';
+import Comment from './node/Comment';
+import Comments from './node/Comments';
+import Element from './node/Element';
+import Elements from './node/Elements';
+import Fragment from './node/Fragment';
+import Node from './node/Node';
+import NodeTypes from './node/NodeTypes';
+import Text from './node/Text';
+import Alignment from './properties/Alignment';
+import Attr from './properties/Attr';
+import AttributeProperty from './properties/AttributeProperty';
+import AttrList from './properties/AttrList';
+import Checked from './properties/Checked';
+import Class from './properties/Class';
+import Classes from './properties/Classes';
+import Css from './properties/Css';
+import CssProperty from './properties/CssProperty';
+import Direction from './properties/Direction';
+import Float from './properties/Float';
+import Html from './properties/Html';
+import OnNode from './properties/OnNode';
+import TextContent from './properties/TextContent';
+import Toggler from './properties/Toggler';
+import Value from './properties/Value';
+import ElementAddress from './search/ElementAddress';
+import Has from './search/Has';
+import PredicateExists from './search/PredicateExists';
+import PredicateFilter from './search/PredicateFilter';
+import PredicateFind from './search/PredicateFind';
+import SelectorExists from './search/SelectorExists';
+import SelectorFilter from './search/SelectorFilter';
+import SelectorFind from './search/SelectorFind';
+import Selectors from './search/Selectors';
+import TransformFind from './search/TransformFind';
+import Traverse from './search/Traverse';
+import Awareness from './selection/Awareness';
+import CursorPosition from './selection/CursorPosition';
+import Edge from './selection/Edge';
+import Selection from './selection/Selection';
+import Situ from './selection/Situ';
+import WindowSelection from './selection/WindowSelection';
+import OptionTag from './tag/OptionTag';
+import SelectTag from './tag/SelectTag';
+import Height from './view/Height';
+import Location from './view/Location';
+import Platform from './view/Platform';
+import Position from './view/Position';
+import Scroll from './view/Scroll';
+import Visibility from './view/Visibility';
+import Width from './view/Width';
+
+export {
+  Compare,
+  DocumentPosition,
+  Dom,
+  DomFuture,
+  Focus,
+  Hierarchy,
+  Insert,
+  InsertAll,
+  Link,
+  Remove,
+  Replication,
+  DomEvent,
+  MouseEvent,
+  Ready,
+  Resize,
+  ScrollChange,
+  Viewable,
+  Body,
+  Comment,
+  Comments,
+  Element,
+  Elements,
+  Fragment,
+  Node,
+  NodeTypes,
+  Text,
+  Alignment,
+  Attr,
+  AttributeProperty,
+  AttrList,
+  Checked,
+  Class,
+  Classes,
+  Css,
+  CssProperty,
+  Direction,
+  Float,
+  Html,
+  OnNode,
+  TextContent,
+  Toggler,
+  Value,
+  ElementAddress,
+  Has,
+  PredicateExists,
+  PredicateFilter,
+  PredicateFind,
+  SelectorExists,
+  SelectorFilter,
+  SelectorFind,
+  Selectors,
+  TransformFind,
+  Traverse,
+  Awareness,
+  CursorPosition,
+  Edge,
+  Selection,
+  Situ,
+  WindowSelection,
+  OptionTag,
+  SelectTag,
+  Height,
+  Location,
+  Platform,
+  Position,
+  Scroll,
+  Visibility,
+  Width
+};
