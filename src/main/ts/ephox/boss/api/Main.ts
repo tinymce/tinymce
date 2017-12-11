945ba07728623a6491b8159c9a31da3dde2592f4
Spocke <spocke@moxiecode.com>
2017-12-11 17:04:46 +0100

    TINY-1461: added main api entry point

---
+++ src/main/ts/ephox/boss/api/Main.ts
@@ -0,0 +1,15 @@
+import BasicPage from './BasicPage';
+import CommentGene from './CommentGene';
+import DomUniverse from './DomUniverse';
+import Gene from './Gene';
+import TestUniverse from './TestUniverse';
+import TextGene from './TextGene';
+
+export {
+  BasicPage,
+  CommentGene,
+  DomUniverse,
+  Gene,
+  TestUniverse,
+  TextGene
+};
