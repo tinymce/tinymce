1b5a4a1df26b7f16052b0572482487369da512e7
Spocke <spocke@moxiecode.com>
2017-12-11 20:23:00 +0100

    TINY-1461: added tsconfig.json/webpack.config.js and deleted bolt config

--- config/bolt/atomic.js
+++
@@ -1,6 +0,0 @@
-configure({
-  configs: [
-    './test.js',
-    './prod.js'
-  ]
-});
--- config/bolt/test.js
+++
@@ -1,5 +0,0 @@
-configure({
-  sources: [
-    source('amd', 'ephox.phoenix.test', '../../src/test/js/module', mapper.hierarchical)
-  ]
-});
