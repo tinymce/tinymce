cbe9b610bce498aa25f0cee7a3d0582e30bbd4cb
James Johnson <james.johnson@ephox.com>
2017-12-12 11:49:47 +1000

    TBIO-5141: added main api entry point

---
+++ src/main/ts/ephox/boulder/api/Main.ts
@@ -0,0 +1,13 @@
+import DslType from './DslType';
+import FieldPresence from './FieldPresence';
+import FieldSchema from './FieldSchema';
+import Objects from './Objects';
+import ValueSchema from './ValueSchema';
+
+export {
+  DslType,
+  FieldPresence,
+  FieldSchema,
+  Objects,
+  ValueSchema
+};
