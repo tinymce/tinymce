bd7e4af90ae0b7311210c30964996cb2a01338c0
Spocke <spocke@moxiecode.com>
2017-12-12 10:52:25 +0100

    TINY-1461: added main api entry point

---
+++ src/main/ts/ephox/alloy/api/Main.ts
@@ -0,0 +1,153 @@
+import AddEventsBehaviour from './behaviour/AddEventsBehaviour';
+import Behaviour from './behaviour/Behaviour';
+import Composing from './behaviour/Composing';
+import Coupling from './behaviour/Coupling';
+import Disabling from './behaviour/Disabling';
+import Docking from './behaviour/Docking';
+import Dragging from './behaviour/Dragging';
+import Focusing from './behaviour/Focusing';
+import Highlighting from './behaviour/Highlighting';
+import Invalidating from './behaviour/Invalidating';
+import Keying from './behaviour/Keying';
+import Pinching from './behaviour/Pinching';
+import Positioning from './behaviour/Positioning';
+import Receiving from './behaviour/Receiving';
+import Replacing from './behaviour/Replacing';
+import Representing from './behaviour/Representing';
+import Sandboxing from './behaviour/Sandboxing';
+import Sliding from './behaviour/Sliding';
+import Streaming from './behaviour/Streaming';
+import Swapping from './behaviour/Swapping';
+import Tabstopping from './behaviour/Tabstopping';
+import Toggling from './behaviour/Toggling';
+import Transitioning from './behaviour/Transitioning';
+import Unselecting from './behaviour/Unselecting';
+import CompBehaviours from './component/CompBehaviours';
+import Component from './component/Component';
+import ComponentApi from './component/ComponentApi';
+import ComponentUtil from './component/ComponentUtil';
+import DomFactory from './component/DomFactory';
+import GuiFactory from './component/GuiFactory';
+import GuiTemplate from './component/GuiTemplate';
+import Memento from './component/Memento';
+import SketchBehaviours from './component/SketchBehaviours';
+import DragCoord from './data/DragCoord';
+import AlloyEvents from './events/AlloyEvents';
+import AlloyTriggers from './events/AlloyTriggers';
+import NativeEvents from './events/NativeEvents';
+import SystemEvents from './events/SystemEvents';
+import FocusManagers from './focus/FocusManagers';
+import Channels from './messages/Channels';
+import Attachment from './system/Attachment';
+import ForeignGui from './system/ForeignGui';
+import Gui from './system/Gui';
+import NoContextApi from './system/NoContextApi';
+import SystemApi from './system/SystemApi';
+import Button from './ui/Button';
+import Container from './ui/Container';
+import DataField from './ui/DataField';
+import Dropdown from './ui/Dropdown';
+import ExpandableForm from './ui/ExpandableForm';
+import Form from './ui/Form';
+import FormChooser from './ui/FormChooser';
+import FormCoupledInputs from './ui/FormCoupledInputs';
+import FormField from './ui/FormField';
+import GuiTypes from './ui/GuiTypes';
+import HtmlSelect from './ui/HtmlSelect';
+import InlineView from './ui/InlineView';
+import Input from './ui/Input';
+import ItemWidget from './ui/ItemWidget';
+import Menu from './ui/Menu';
+import ModalDialog from './ui/ModalDialog';
+import Sketcher from './ui/Sketcher';
+import Slider from './ui/Slider';
+import SplitDropdown from './ui/SplitDropdown';
+import SplitToolbar from './ui/SplitToolbar';
+import Tabbar from './ui/Tabbar';
+import TabButton from './ui/TabButton';
+import TabSection from './ui/TabSection';
+import Tabview from './ui/Tabview';
+import TieredMenu from './ui/TieredMenu';
+import Toolbar from './ui/Toolbar';
+import ToolbarGroup from './ui/ToolbarGroup';
+import TouchMenu from './ui/TouchMenu';
+import Typeahead from './ui/Typeahead';
+import UiSketcher from './ui/UiSketcher';
+
+export {
+  AddEventsBehaviour,
+  Behaviour,
+  Composing,
+  Coupling,
+  Disabling,
+  Docking,
+  Dragging,
+  Focusing,
+  Highlighting,
+  Invalidating,
+  Keying,
+  Pinching,
+  Positioning,
+  Receiving,
+  Replacing,
+  Representing,
+  Sandboxing,
+  Sliding,
+  Streaming,
+  Swapping,
+  Tabstopping,
+  Toggling,
+  Transitioning,
+  Unselecting,
+  CompBehaviours,
+  Component,
+  ComponentApi,
+  ComponentUtil,
+  DomFactory,
+  GuiFactory,
+  GuiTemplate,
+  Memento,
+  SketchBehaviours,
+  DragCoord,
+  AlloyEvents,
+  AlloyTriggers,
+  NativeEvents,
+  SystemEvents,
+  FocusManagers,
+  Channels,
+  Attachment,
+  ForeignGui,
+  Gui,
+  NoContextApi,
+  SystemApi,
+  Button,
+  Container,
+  DataField,
+  Dropdown,
+  ExpandableForm,
+  Form,
+  FormChooser,
+  FormCoupledInputs,
+  FormField,
+  GuiTypes,
+  HtmlSelect,
+  InlineView,
+  Input,
+  ItemWidget,
+  Menu,
+  ModalDialog,
+  Sketcher,
+  Slider,
+  SplitDropdown,
+  SplitToolbar,
+  Tabbar,
+  TabButton,
+  TabSection,
+  Tabview,
+  TieredMenu,
+  Toolbar,
+  ToolbarGroup,
+  TouchMenu,
+  Typeahead,
+  UiSketcher
+};
