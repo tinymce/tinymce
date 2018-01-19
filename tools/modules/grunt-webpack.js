6351add1a7f6c145fd0530428267b02aef88369e
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2018-01-19 11:44:15 +0100

    WDS: updated readme to add grunt start explanation

--- tools/modules/grunt-webpack.js
+++ tools/modules/grunt-webpack.js
@@ -94,11 +94,33 @@ let all = (plugins, themes) => {
   ];
 }
 
+let generateDemoIndex = (grunt, app, plugins, themes) => {
+  let demoList = grunt.file.expand(['src/**/demo/html/*.html'])
+  let links = `<ul>${demoList.map(link => `<li><a href="${link}">${link}</a></li>`).join('')}</ul>`
+  let html = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demos</title>
+  </head>
+  <body>
+    ${links}
+  </body>
+  </html>
+  `
+
+  app.get('/', (req, res) => res.send(html))
+}
+
 module.exports = {
   createPlugin,
   createTheme,
   create,
   all,
   allPluginDemos,
-  allThemeDemos
+  allThemeDemos,
+  generateDemoIndex
 };
