a721c26d433ecf51bdb15dcd04762f769c8a1767
Mike Chau <mike.chau@ephox.com>
2018-03-12 10:52:07 +1000

    TINY-1547: Channels

--- src/main/ts/ephox/alloy/api/messages/Channels.ts
+++ src/main/ts/ephox/alloy/api/messages/Channels.ts
@@ -1,6 +1,7 @@
 import { Fun } from '@ephox/katamari';
+import { StringConstant } from 'ephox/alloy/alien/TypeDefinitions';
 
-const dismissPopups = Fun.constant('dismiss.popups');
+const dismissPopups = Fun.constant('dismiss.popups') as StringConstant;
 
 export {
   dismissPopups
