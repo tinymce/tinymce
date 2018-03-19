72053833181994f101f3d1bc3fbd5324f55b3141
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2018-03-19 10:21:04 +0100

    TINY-1569: review fixes

--- src/core/test/ts/browser/EditorGetContentTextFormatTest.ts
+++ src/core/test/ts/browser/EditorGetContentTextFormatTest.ts
@@ -4,10 +4,7 @@ import Theme from 'tinymce/themes/modern/Theme';
 import { UnitTest } from '@ephox/bedrock';
 import Zwsp from 'tinymce/core/text/Zwsp';
 
-UnitTest.asynctest('browser.tinymce.core.EditorGetContentTextFormatTest', function () {
-  const success = arguments[arguments.length - 2];
-  const failure = arguments[arguments.length - 1];
-
+UnitTest.asynctest('browser.tinymce.core.EditorGetContentTextFormatTest', (success, failure) => {
   Theme();
 
   TinyLoader.setup(function (editor, onSuccess, onFailure) {
@@ -16,7 +13,6 @@ UnitTest.asynctest('browser.tinymce.core.EditorGetContentTextFormatTest', functi
     Pipeline.async({}, [
       Logger.t('get text format content should trim zwsp', GeneralSteps.sequence([
         tinyApis.sSetContent('<p>' + Zwsp.ZWSP + 'a</p>'),
-        tinyApis.sExecCommand('bold'),
         Step.sync(function () {
           const html = editor.getContent({ format: 'text' });
           Assertions.assertEq('Should be expected html', 'a', html);
