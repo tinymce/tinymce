46cc2e000da5f9cea027e28f64b1b2870a861cbd
Josh Haines <josh.j.haines@gmail.com>
2018-04-18 14:42:17 +1000

    TBIO-4937: wip2

--- src/main/ts/ephox/alloy/ui/composite/ColourPickerSpec.ts
+++ src/main/ts/ephox/alloy/ui/composite/ColourPickerSpec.ts
@@ -1,9 +1,108 @@
+import * as Memento from 'ephox/alloy/api/component/Memento';
+import { Form } from 'ephox/alloy/api/ui/Form';
+import { FormField } from 'ephox/alloy/api/ui/FormField';
+import { Input } from 'ephox/alloy/api/ui/Input';
+
+import { Slider } from 'ephox/alloy/api/ui/Slider';
+
 const make = function (detail, components, spec, externals) {
+
+  var renderRawInput = function (spec) {
+    var pLabel = FormField.parts().label({
+      dom: { tag: 'label', innerHtml: spec.label }
+    });
+  
+    var pField = FormField.parts().field({
+      factory: Input,
+      // inputBehaviours: Behaviour.derive([
+      //   Tabstopping.config({ })
+      // ])
+    });
+  
+    return {
+      dom: {
+        tag: 'div',
+        classes: [ 'example-field-container' ]
+      },
+      components: [
+        pLabel,
+        pField
+      ]
+    };
+  };
+
+  const stops = '#ff0000,#ff0080,#ff00ff,#8000ff,#0000ff,#0080ff,#00ffff,#00ff80,#00ff00,#80ff00,#ffff00,#ff8000,#ff0000';
+  const gradientCssText = (
+    'background: -ms-linear-gradient(right,' + stops + ');' +
+    'background: linear-gradient(to right,' + stops + ');'
+  );
+
+  var spectrum = Slider.parts().spectrum({
+    dom: {
+      tag: 'div',
+      attributes: {
+        title: 'spectrum',
+        style: gradientCssText
+      },
+      classes: [ 'example-slider-spectrum' ]
+    }
+  });
+
+  var thumb = Slider.parts().thumb({
+    dom: {
+      tag: 'div',
+      innerHtml: 'Thumb',
+      attributes: {
+        title: 'thumb'
+      },
+      classes: [ 'example-slider-thumb' ]
+    }
+  });
+
+
+  var hue = Slider.sketch({
+    dom: {
+      tag: 'div',
+      attributes: {
+        title: 'slider'
+      },
+      classes: [ 'example-slider' ]
+    },
+    components: [
+      spectrum,
+      thumb
+    ],
+    min: 0,
+    max: 100,
+    getInitialValue: function () { return 10; }
+  });
+
+
+
+  var memForm = Memento.record(
+    Form.sketch((parts) => {
+      return {
+        dom: {
+          tag: 'div',
+          classes: [ 'colour-picker-body' ]
+        },
+        components: [
+          parts.field('green', FormField.sketch(renderRawInput({ label: 'green' }))),
+          hue
+        ]
+      };
+    })
+  );
+
+  // Making this a simple spec and then we'll introduce where they put the body
 
   return {
     uid: detail.uid(),
     dom: detail.dom(),
-    components
+    // components
+    components: [
+      memForm.asSpec()
+    ]
   };
 };
 
--- src/main/ts/ephox/alloy/ui/schema/ColourPickerSchema.ts
+++ src/main/ts/ephox/alloy/ui/schema/ColourPickerSchema.ts
@@ -4,29 +4,32 @@ import { FormField } from '../../api/ui/FormField';
 import { Slider } from '../../api/ui/Slider';
 import * as PartType from '../../parts/PartType';
 
-const schema = Fun.constant([]);
+const schema = Fun.constant([
+  
+]);
 
 const parts = Fun.constant([
-  PartType.required({
-    factory: Slider,
-    name: 'hue'
-  }),
-  PartType.required({
-    factory: FormField,
-    name: 'red'
-  }),
-  PartType.required({
-    factory: FormField,
-    name: 'green'
-  }),
-  PartType.required({
-    factory: FormField,
-    name: 'blue'
-  }),
-  PartType.required({
-    factory: FormField,
-    name: 'hex'
-  })
+  // PartType.required({
+  //   factory: Slider,
+  //   name: 'hue'
+  // }),
+  // PartType.required({
+  //   factory: FormField,
+  //   name: 'red'
+  // }),
+  // PartType.required({
+  //   factory: FormField,
+  //   name: 'green'
+  // })
+  //,
+  // PartType.required({
+  //   factory: FormField,
+  //   name: 'blue'
+  // }),
+  // PartType.required({
+  //   factory: FormField,
+  //   name: 'hex'
+  // })
 ]);
 
 const name = Fun.constant('ColourPicker');
