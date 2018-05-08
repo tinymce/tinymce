db9e351ccf8e76f8980ee04674b5f52545428c60
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2018-05-08 15:40:10 +1000

    GH-4226: cleanup test

--- src/core/test/ts/browser/DragDropOverridesTest.ts
+++ src/core/test/ts/browser/DragDropOverridesTest.ts
@@ -1,33 +1,34 @@
 import { Assertions, GeneralSteps, Logger, Pipeline, Step } from '@ephox/agar';
 import { TinyApis, TinyLoader } from '@ephox/mcagar';
+import { Cell } from '@ephox/katamari';
 import { Hierarchy, Element } from '@ephox/sugar';
 import Theme from 'tinymce/themes/modern/Theme';
 import { UnitTest } from '@ephox/bedrock';
 
-UnitTest.asynctest('browser.tinymce.core.DragDropOverridesTest', function () {
-  const success = arguments[arguments.length - 2];
-  const failure = arguments[arguments.length - 1];
-
+UnitTest.asynctest('browser.tinymce.core.DragDropOverridesTest', (success, failure) => {
   Theme();
 
   TinyLoader.setup(function (editor, onSuccess, onFailure) {
     const tinyApis = TinyApis(editor);
+    const fired = Cell(false);
+
+    editor.on('dragend', function () {
+      fired.set(true);
+    });
 
     Pipeline.async({}, [
-      Logger.t('Drop draggable element outside of editor', GeneralSteps.sequence([
+      Logger.t('drop draggable element outside of editor', GeneralSteps.sequence([
         tinyApis.sSetContent('<p contenteditable="false">a</p>'),
-        Step.sync(function () {
-          let fired = false;
-          editor.on('dragend', function () {
-            fired = true;
-          });
+        Step.sync(() => {
           const target = Hierarchy.follow(Element.fromDom(editor.getBody()), [0]).getOrDie().dom();
           const rect = target.getBoundingClientRect();
           const button = 0, screenX = (rect.left + rect.width / 2), screenY = (rect.top + rect.height / 2);
+
           editor.fire('mousedown', { button, screenX, screenY, target });
           editor.fire('mousemove', { button, screenX: screenX + 20, screenY: screenY + 20, target });
           editor.dom.fire(document.body, 'mouseup');
-          Assertions.assertEq('Should fire dragend event', true, fired);
+
+          Assertions.assertEq('Should fire dragend event', true, fired.get());
         })
       ])),
     ], onSuccess, onFailure);
