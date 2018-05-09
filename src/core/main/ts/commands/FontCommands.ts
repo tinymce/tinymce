df2f348a212bf2ee3c7eac2e38880e36d063ca34
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2018-05-09 10:30:19 +1000

    TINY-1667: fix fontsize command bug

--- src/core/main/ts/commands/FontCommands.ts
+++ src/core/main/ts/commands/FontCommands.ts
@@ -34,17 +34,21 @@ const getCaretElement = (editor: Editor): Option<Node> => {
 };
 
 const fromFontSizeNumber = (editor: Editor, value: string): string => {
-  const fontSizeNumber = parseInt(value, 10);
+  if (/^[0-9\.]+$/.test(value)) {
+    const fontSizeNumber = parseInt(value, 10);
 
-  // Convert font size 1-7 to styles
-  if (fontSizeNumber >= 1 && fontSizeNumber <= 7) {
-    const fontSizes = Settings.getFontStyleValues(editor);
-    const fontClasses = Settings.getFontSizeClasses(editor);
+    // Convert font size 1-7 to styles
+    if (fontSizeNumber >= 1 && fontSizeNumber <= 7) {
+      const fontSizes = Settings.getFontStyleValues(editor);
+      const fontClasses = Settings.getFontSizeClasses(editor);
 
-    if (fontClasses) {
-      return fontClasses[fontSizeNumber - 1] || value;
+      if (fontClasses) {
+        return fontClasses[fontSizeNumber - 1] || value;
+      } else {
+        return fontSizes[fontSizeNumber - 1] || value;
+      }
     } else {
-      return fontSizes[fontSizeNumber - 1] || value;
+      return value;
     }
   } else {
     return value;
