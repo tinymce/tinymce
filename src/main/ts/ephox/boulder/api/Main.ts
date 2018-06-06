41c1ddcc9af339d3fa1b9df6946d67666ffe487b
morgan <morgan.smith@ephox.com>
2018-06-07 02:21:36 +1000

    TINY-1720: exporting FieldProcessorAdt and Processor

--- src/main/ts/ephox/boulder/api/Main.ts
+++ src/main/ts/ephox/boulder/api/Main.ts
@@ -3,11 +3,14 @@ import * as FieldPresence from './FieldPresence';
 import * as FieldSchema from './FieldSchema';
 import * as Objects from './Objects';
 import * as ValueSchema from './ValueSchema';
+import { Processor, FieldProcessorAdt } from './DslType';
 
 export {
   DslType,
   FieldPresence,
   FieldSchema,
   Objects,
-  ValueSchema
+  ValueSchema,
+  Processor,
+  FieldProcessorAdt
 };
