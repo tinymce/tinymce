0201716118b95e0af60342d6bac70d5805571ca0
morgan <morgan.smith@ephox.com>
2018-06-07 00:57:55 +1000

    TINY-1720: positioning types

--- src/main/ts/ephox/alloy/behaviour/positioning/ActivePosition.ts
+++ src/main/ts/ephox/alloy/behaviour/positioning/ActivePosition.ts
@@ -1,6 +1,7 @@
 import * as DomModification from '../../dom/DomModification';
+import { PositioningConfig } from 'ephox/alloy/behaviour/positioning/PositioningTypes';
 
-const exhibit = function (base, posConfig/*, posState */) {
+const exhibit = function (base: { }, posConfig: PositioningConfig/*, posState */): { } {
   return DomModification.nu({
     classes: [ ],
     styles: posConfig.useFixed() ? { } : { position: 'relative' }
