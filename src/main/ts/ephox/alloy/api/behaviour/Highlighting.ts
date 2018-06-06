b70a891690dfab4ddc52bbb8e0bfb91b139a566f
morgan <morgan.smith@ephox.com>
2018-06-07 00:06:03 +1000

    TINY-1720: Highlighting types

--- src/main/ts/ephox/alloy/api/behaviour/Highlighting.ts
+++ src/main/ts/ephox/alloy/api/behaviour/Highlighting.ts
@@ -4,30 +4,7 @@ import HighlightSchema from '../../behaviour/highlighting/HighlightSchema';
 import { SugarElement } from '../../alien/TypeDefinitions';
 import { AlloyComponent } from '../../api/component/ComponentApi';
 import { Option } from '@ephox/katamari';
-
-export interface HighlightingBehaviour extends Behaviour.AlloyBehaviour {
-  config: (config: HighlightingConfig) => Behaviour.NamedConfiguredBehaviour;
-  dehighlightAll: (component: AlloyComponent) => void;
-  dehighlight: (component: AlloyComponent, target: AlloyComponent) => void;
-  highlight: (component: AlloyComponent, target: AlloyComponent) => void;
-  highlightFirst: (component: AlloyComponent) => void;
-  highlightLast: (component: AlloyComponent) => void;
-  highlightAt: (component: AlloyComponent, index: number) => void;
-  highlightBy: (component: AlloyComponent, predicate: (any) => any) => void;
-  isHighlighted: (component: AlloyComponent, queryTarget: any ) => void;
-  getHighlighted: (component: AlloyComponent) => Option<AlloyComponent>;
-  getFirst: (component: AlloyComponent) => Option<AlloyComponent>;
-  getLast: (component: AlloyComponent) => Option<AlloyComponent>;
-  getPrevious: (component: AlloyComponent) => Option<AlloyComponent>;
-  getNext: (component: AlloyComponent) => Option<AlloyComponent>;
-}
-
-export interface HighlightingConfig {
-  itemClass: string;
-  highlightClass: string;
-  onHighlight?: (chooser, choice) => void;
-  onDehighlight?: (chooser, choice) => void;
-}
+import { HighlightingBehaviour } from 'ephox/alloy/behaviour/highlighting/HighlightingTypes';
 
 const Highlighting = Behaviour.create({
   fields: HighlightSchema,
---
+++ src/main/ts/ephox/alloy/behaviour/highlighting/HighlightingTypes.ts
@@ -0,0 +1,34 @@
+import * as Behaviour from '../../api/behaviour/Behaviour';
+import { AlloyComponent } from 'ephox/alloy/api/component/ComponentApi';
+import { Option } from '@ephox/katamari';
+
+export interface HighlightingBehaviour extends Behaviour.AlloyBehaviour {
+  config: (config: HighlightingConfigSpec) => Behaviour.NamedConfiguredBehaviour;
+  dehighlightAll: (component: AlloyComponent) => void;
+  dehighlight: (component: AlloyComponent, target: AlloyComponent) => void;
+  highlight: (component: AlloyComponent, target: AlloyComponent) => void;
+  highlightFirst: (component: AlloyComponent) => void;
+  highlightLast: (component: AlloyComponent) => void;
+  highlightAt: (component: AlloyComponent, index: number) => void;
+  highlightBy: (component: AlloyComponent, predicate: (any) => any) => void;
+  isHighlighted: (component: AlloyComponent, queryTarget: any ) => void;
+  getHighlighted: (component: AlloyComponent) => Option<AlloyComponent>;
+  getFirst: (component: AlloyComponent) => Option<AlloyComponent>;
+  getLast: (component: AlloyComponent) => Option<AlloyComponent>;
+  getPrevious: (component: AlloyComponent) => Option<AlloyComponent>;
+  getNext: (component: AlloyComponent) => Option<AlloyComponent>;
+}
+
+export interface HighlightingConfigSpec {
+  itemClass: string;
+  highlightClass: string;
+  onHighlight?: (comp: AlloyComponent, target: AlloyComponent) => void;
+  onDehighlight?: (comp: AlloyComponent, target: AlloyComponent) => void;
+}
+
+export interface HighlightingConfig {
+  itemClass: () => string;
+  highlightClass: () => string;
+  onHighlight: () => (comp: AlloyComponent, target: AlloyComponent) => void;
+  onDehighlight: () => (comp: AlloyComponent, target: AlloyComponent) => void;
+}
