c21a12635771ad1ea5adcb45822d4dafaf461bd6
morgan <morgan.smith@ephox.com>
2018-06-07 00:23:48 +1000

    TINY-1720: invalidating types

--- src/main/ts/ephox/alloy/behaviour/invalidating/ActiveInvalidate.ts
+++ src/main/ts/ephox/alloy/behaviour/invalidating/ActiveInvalidate.ts
@@ -1,8 +1,10 @@
 import * as AlloyEvents from '../../api/events/AlloyEvents';
 import * as InvalidateApis from './InvalidateApis';
 import { Fun } from '@ephox/katamari';
+import { Stateless } from 'ephox/alloy/behaviour/common/NoState';
+import { InvalidatingConfig } from 'ephox/alloy/behaviour/invalidating/InvalidateTypes';
 
-const events = function (invalidConfig, invalidState) {
+const events = function (invalidConfig: InvalidatingConfig, invalidState: Stateless): AlloyEvents.EventHandlerConfigRecord {
   return invalidConfig.validator().map(function (validatorInfo) {
     return AlloyEvents.derive([
       AlloyEvents.run(validatorInfo.onEvent(), function (component) {
---
+++ src/main/ts/ephox/alloy/behaviour/invalidating/InvalidateTypes.ts
@@ -0,0 +1,50 @@
+
+import * as Behaviour from '../../api/behaviour/Behaviour';
+import { AlloyComponent } from 'ephox/alloy/api/component/ComponentApi';
+import { Option, Future, Result } from '@ephox/katamari';
+import { SugarElement } from 'ephox/alloy/alien/TypeDefinitions';
+
+export interface InvalidatingBehaviour extends Behaviour.AlloyBehaviour {
+  config: (config: InvalidatingConfigSpec) => Behaviour.NamedConfiguredBehaviour;
+  markValid: (Component: AlloyComponent) => void;
+  markInvalid: (Component: AlloyComponent) => void;
+  query: <T>(Component: AlloyComponent) => Future<T>;
+  run: <T>(Component: AlloyComponent) => Future<T>;
+  validation: (validate: <T, E>(v: string) => Result<T, E>) => (component: AlloyComponent) => any;
+}
+
+export interface InvalidatingConfigSpec {
+  invalidClass: string;
+  getRoot?: any;
+  notify?: {
+    aria?: string;
+    getContainer?: any;
+    validHtml: string;
+    onValid?: any;
+    onInvalid?: any;
+    onValidate?: (comp: AlloyComponent) => void;
+  }
+  validator?: {
+    validate: (input: AlloyComponent) => Future<Result<any, string>>;
+    onEvent?: string;
+    validateOnLoad: boolean;
+  }
+}
+
+export interface InvalidatingConfig {
+  invalidClass: () => string;
+  notify?: () => Option<{
+    getContainer?: () => (input: AlloyComponent) => Option<SugarElement>;
+    onValid?: () => (comp: AlloyComponent) => void;
+    validHtml: () => string;
+    onInvalid?: () => (comp: AlloyComponent, err: string) => void;
+    onValidate: () => (comp: AlloyComponent) => void;
+  }>;
+  onEvent?: () => string;
+  getRoot?: () => (input: AlloyComponent) => any;
+  validator: () => Option<{
+    validate: () => (input: AlloyComponent) => Future<Result<any, string>>;
+    onEvent?: () => string;
+    validateOnLoad?: () => boolean
+  }>;
+}
