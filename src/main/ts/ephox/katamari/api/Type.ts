9659565f3fb98f842974d2fc39538ae5cb0d94df
James Johnson <james.johnson@ephox.com>
2018-06-11 09:42:34 +1000

    TINY-1722: Add type guards to katamari Type.isX methods

--- src/main/ts/ephox/katamari/api/Type.ts
+++ src/main/ts/ephox/katamari/api/Type.ts
@@ -1,4 +1,4 @@
-var typeOf = function(x) {
+var typeOf = function(x: any) {
   if (x === null) return 'null';
   var t = typeof x;
   if (t === 'object' && Array.prototype.isPrototypeOf(x)) return 'array';
@@ -6,19 +6,19 @@ var typeOf = function(x) {
   return t;
 };
 
-var isType = function (type) {
-  return function (value) {
+var isType = function (type: string) {
+  return function (value: any) {
     return typeOf(value) === type;
   };
 };
 
-export default <any> {
-  isString: isType('string'),
-  isObject: isType('object'),
-  isArray: isType('array'),
-  isNull: isType('null'),
-  isBoolean: isType('boolean'),
-  isUndefined: isType('undefined'),
-  isFunction: isType('function'),
-  isNumber: isType('number')
+export default {
+  isString: <(value: any) => value is string>isType('string'),
+  isObject: <(value: any) => value is object>isType('object'),
+  isArray: <(value: any) => value is Array<any>>isType('array'),
+  isNull: <(value: any) => value is null>isType('null'),
+  isBoolean: <(value: any) => value is boolean>isType('boolean'),
+  isUndefined: <(value: any) => value is undefined>isType('undefined'),
+  isFunction: <(value: any) => value is Function>isType('function'),
+  isNumber: <(value: any) => value is number>isType('number')
 };
