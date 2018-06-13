8442083362320b56577db564e9dbad3b99971377
Joshua Haines <josh.j.haines@gmail.com>
2018-06-13 17:16:56 +1000

    TBIO-4937-b: update palette schema to include min/max values

--- src/main/ts/ephox/alloy/ui/palette/PaletteSchema.ts
+++ src/main/ts/ephox/alloy/ui/palette/PaletteSchema.ts
@@ -1,20 +1,25 @@
-import { FieldSchema, DslType } from '@ephox/boulder';
+import { FieldSchema, DslType, FieldProcessorAdt } from '@ephox/boulder';
 import { Cell, Fun } from '@ephox/katamari';
 import { PlatformDetection } from '@ephox/sand';
 
 import { Keying } from '../../api/behaviour/Keying';
 import { Representing } from '../../api/behaviour/Representing';
 import * as SketchBehaviours from '../../api/component/SketchBehaviours';
+import { RgbaColour } from '@ephox/acid';
 
 const isTouch = PlatformDetection.detect().deviceType.isTouch();
 
-const PaletteSchema = [
+const PaletteSchema : FieldProcessorAdt[] = [
+  FieldSchema.strict('minX'),
+  FieldSchema.strict('maxX'),
+  FieldSchema.strict('minY'),
+  FieldSchema.strict('maxY'),
   FieldSchema.defaulted('onChange', Fun.noop),
   FieldSchema.defaulted('onInit', Fun.noop),
   FieldSchema.defaulted('onDragStart', Fun.noop),
   FieldSchema.defaulted('onDragEnd', Fun.noop),
   FieldSchema.state('value', function (spec) { return Cell({x: 0, y: 0}); }),
-  FieldSchema.state('colour', function (spec) { return Cell({r: 255, g: 0, b: 0, a: 1}); }),
+  FieldSchema.state('colour', function (spec) { return Cell(RgbaColour.rgbaColour(0, 0, 0, 1)); }),
   SketchBehaviours.field('paletteBehaviours', [ Keying, Representing ])
 ].concat(! isTouch ? [
   // Only add if not on a touch device
--- src/main/ts/ephox/alloy/ui/palette/PaletteUi.ts
+++ src/main/ts/ephox/alloy/ui/palette/PaletteUi.ts
@@ -76,8 +76,7 @@ const sketch = function (detail, components, spec, externals) {
     var width1 = palette.width;
     var height1 = palette.height;
 
-    // var colour = detail.colour().get();
-    var rgba = `rgba(${colour.red},${colour.green},${colour.blue},${colour.alpha})`;
+    var rgba = `rgba(${colour.red()},${colour.green()},${colour.blue()},${colour.alpha()})`;
     ctx1.fillStyle = rgba;
     ctx1.fillRect(0, 0, width1, height1);
   
