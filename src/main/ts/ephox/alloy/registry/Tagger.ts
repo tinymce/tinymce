f55cbe58595096aefad09b6a4ad939968aedb178
morgan <morgan.smith@ephox.com>
2018-06-15 20:54:25 +1000

    sketcher-typing: Tagger

--- src/main/ts/ephox/alloy/registry/Tagger.ts
+++ src/main/ts/ephox/alloy/registry/Tagger.ts
@@ -7,40 +7,40 @@ import { SugarElement } from '../alien/TypeDefinitions';
 const prefix = AlloyTags.prefix();
 const idAttr = AlloyTags.idAttr();
 
-const write = (label, elem) => {
-  const id = Id.generate(prefix + label);
+const write = (label: string, elem: SugarElement): string => {
+  const id: string = Id.generate(prefix + label);
   Attr.set(elem, idAttr, id);
   return id;
 };
 
-const writeOnly = (elem, uid) => {
+const writeOnly = (elem: SugarElement, uid: string) => {
   Attr.set(elem, idAttr, uid);
 };
 
-const read = (elem: SugarElement): Option<any> => {
+const read = (elem: SugarElement): Option<string> => {
   const id = Node.isElement(elem) ? Attr.get(elem, idAttr) : null;
   return Option.from(id);
 };
 
-const find = (container, id) => {
-  return SelectorFind.descendant(container, id);
-};
+// const find = (container: SugarElement, id: string): Option<SugarElement> => {
+//   return SelectorFind.descendant(container, id);
+// };
 
-const generate = (prefix) => {
+const generate = (prefix: string): string => {
   return Id.generate(prefix);
 };
 
-const revoke = (elem) => {
+const revoke = (elem: SugarElement): void => {
   Attr.remove(elem, idAttr);
 };
 
-const attribute = Fun.constant(idAttr);
+const attribute: () => string = Fun.constant(idAttr);
 export {
   revoke,
   write,
   writeOnly,
   read,
-  find,
+  // find,
   generate,
   attribute
 };
