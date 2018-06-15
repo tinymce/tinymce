e651001dc593d78a380273f7a37b04255ae944b1
morgan <morgan.smith@ephox.com>
2018-06-15 20:55:33 +1000

    sketcher-typing: alloy logger

--- src/main/ts/ephox/alloy/log/AlloyLogger.ts
+++ src/main/ts/ephox/alloy/log/AlloyLogger.ts
@@ -1,6 +1,8 @@
+import { SugarElement } from 'ephox/alloy/alien/TypeDefinitions';
+
 import * as Truncate from '../alien/Truncate';
 
-const element = (elem) => {
+const element = (elem: SugarElement): string => {
   return Truncate.getHtml(elem);
 };
 
