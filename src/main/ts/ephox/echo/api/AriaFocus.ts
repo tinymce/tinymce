2750a7b661686e13245fed396cf93317fed8ee58
Andrew Herron <thespyder@programmer.net>
2018-06-18 16:38:10 +1000

    TINY-1600: Correct error picked up by sugar types

--- src/main/ts/ephox/echo/api/AriaFocus.ts
+++ src/main/ts/ephox/echo/api/AriaFocus.ts
@@ -18,12 +18,13 @@ var preserve = function (f, container) {
 
   // If there is a focussed element, the F function may cause focus to be lost (such as by hiding elements). Restore it afterwards.
   refocus.each(function (oldFocus) {
-    Focus.active(ownerDoc).filter(function (newFocus) {
+    const shouldFocus = Focus.active(ownerDoc).filter(function (newFocus) {
       return Compare.eq(newFocus, oldFocus);
-    }).orThunk(function () {
+    }).isNone();
+    if (shouldFocus) {
       // Only refocus if the focus has changed, otherwise we break IE
       Focus.focus(oldFocus);
-    });
+    }
   });
   return result;
 };
