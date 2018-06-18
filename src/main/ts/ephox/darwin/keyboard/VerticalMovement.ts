69d23fd365947d73613d849f82c9fa5543d17252
Andrew Herron <thespyder@programmer.net>
2018-06-18 15:38:47 +1000

    TINY-1600: Correct error picked up by sugar types

--- src/main/ts/ephox/darwin/keyboard/VerticalMovement.ts
+++ src/main/ts/ephox/darwin/keyboard/VerticalMovement.ts
@@ -27,8 +27,13 @@ var inSameTable = function (elem, table) {
 // Note: initial is the finishing element, because that's where the cursor starts from
 // Anchor is the starting element, and is only used to work out if we are in the same table
 var simulate = function (bridge, isRoot, direction, initial, anchor) {
+  interface Simulated {
+    start: () => number,
+    finish: () => number,
+    range: () => any
+  };
   return SelectorFind.closest(initial, 'td,th', isRoot).bind(function (start) {
-    return SelectorFind.closest(start, 'table', isRoot).bind(function (table) {
+    return SelectorFind.closest(start, 'table', isRoot).bind<Simulated>(function (table) {
       if (!inSameTable(anchor, table)) return Option.none();
       return TableKeys.handle(bridge, isRoot, direction).bind(function (range) {
         return SelectorFind.closest(range.finish(), 'td,th', isRoot).map(function (finish) {
