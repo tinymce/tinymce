d248a1f1584a2c30638defc30802e9529087d69e
Andrew Herron <thespyder@programmer.net>
2018-06-19 17:11:23 +1000

    TINY-1600: Add a couple of extra types to Compare so return types are inferred correctly

--- src/main/ts/ephox/sugar/api/dom/Compare.ts
+++ src/main/ts/ephox/sugar/api/dom/Compare.ts
@@ -4,13 +4,14 @@ import { Node } from '@ephox/sand';
 import { PlatformDetection } from '@ephox/sand';
 import Selectors from '../search/Selectors';
 import Element from '../node/Element';
+import { Node as DomNode } from '@ephox/dom-globals';
 
 var eq = function (e1: Element, e2: Element) {
   return e1.dom() === e2.dom();
 };
 
 var isEqualNode = function (e1: Element, e2: Element) {
-  return e1.dom().isEqualNode(e2.dom());
+  return (e1.dom() as DomNode).isEqualNode(e2.dom() as DomNode);
 };
 
 var member = function (element: Element, elements: Element[]) {
@@ -19,7 +20,7 @@ var member = function (element: Element, elements: Element[]) {
 
 // DOM contains() method returns true if e1===e2, we define our contains() to return false (a node does not contain itself).
 var regularContains = function (e1: Element, e2: Element) {
-  var d1 = e1.dom(), d2 = e2.dom();
+  var d1: DomNode = e1.dom(), d2: DomNode = e2.dom();
   return d1 === d2 ? false : d1.contains(d2);
 };
 
@@ -29,7 +30,7 @@ var ieContains = function (e1: Element, e2: Element) {
   // https://connect.microsoft.com/IE/feedback/details/780874/node-contains-is-incorrect
   // Note that compareDocumentPosition returns CONTAINED_BY if 'e2 *is_contained_by* e1':
   // Also, compareDocumentPosition defines a node containing itself as false.
-  return Node.documentPositionContainedBy(e1.dom(), e2.dom());
+  return Node.documentPositionContainedBy(e1.dom(), e2.dom()) as boolean;
 };
 
 var browser = PlatformDetection.detect().browser;
