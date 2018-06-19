6a178e4ba492f2cc8cef21f0ffe626ebb2de13ef
morgan <morgan.smith@ephox.com>
2018-06-19 16:37:05 +1000

    TBS-2374: removing active annotation class

--- src/core/main/ts/annotate/Markings.ts
+++ src/core/main/ts/annotate/Markings.ts
@@ -1,14 +1,12 @@
 import { Fun } from '@ephox/katamari';
 
 const annotation = Fun.constant('mce-annotation');
-const activeAnnotation = Fun.constant('mce-active-annotation');
 
 const dataAnnotation = Fun.constant('data-mce-annotation');
 const dataAnnotationId = Fun.constant('data-mce-annotation-uid');
 
 export {
   annotation,
-  activeAnnotation,
   dataAnnotation,
   dataAnnotationId
 };
