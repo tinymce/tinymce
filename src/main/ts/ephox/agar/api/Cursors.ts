b18fefdf075b8bd61e860ed6dc93c3423ace6509
James Johnson <james.johnson@ephox.com>
2018-06-21 11:58:05 +1000

    TINY-1755: Fixed type errors exposed by katamari

--- src/main/ts/ephox/agar/api/Cursors.ts
+++ src/main/ts/ephox/agar/api/Cursors.ts
@@ -1,10 +1,29 @@
 import Chain from './Chain';
 import { Result } from '@ephox/katamari';
 import { Struct } from '@ephox/katamari';
-import { Hierarchy } from '@ephox/sugar';
+import { Hierarchy, Element } from '@ephox/sugar';
 
-var range = Struct.immutableBag([ 'start', 'soffset', 'finish', 'foffset' ], [ ]);
-var path = Struct.immutableBag([ 'startPath', 'soffset', 'finishPath', 'foffset' ], [ ]);
+export interface CursorRange {
+  start: () => Element;
+  soffset: () => number;
+  finish: () => Element;
+  foffset: () => number;
+};
+
+export interface CursorPath {
+  startPath: () => number[];
+  soffset: () => any;
+  finishPath: () => number[];
+  foffset: () => any;
+};
+
+type RangeConstructor = (obj: { start: Element; soffset: number; finish: Element; foffset: number; }) => CursorRange;
+
+var range: RangeConstructor = Struct.immutableBag([ 'start', 'soffset', 'finish', 'foffset' ], [ ]);
+
+type PathConstructor = (obj: { startPath: number[]; soffset: number; finishPath: number[]; foffset: number; }) => CursorPath;
+
+var path: PathConstructor = Struct.immutableBag([ 'startPath', 'soffset', 'finishPath', 'foffset' ], [ ]);
 
 var pathFromCollapsed = function (spec) {
   return path({
@@ -29,7 +48,7 @@ var pathFrom = function (spec) {
   return spec.start === undefined && spec.element !== undefined ? pathFromCollapsed(spec) : pathFromRange(spec);
 };
 
-var follow = function (container, calcPath) {
+var follow = function (container: Element, calcPath: number[]): Result<Element, string> {
   return Hierarchy.follow(container, calcPath).fold(function () {
     return Result.error('Could not follow path: ' + calcPath.join(','));
   }, function (p) {
@@ -37,7 +56,7 @@ var follow = function (container, calcPath) {
   });
 };
 
-var followPath = function (container, calcPath) {
+var followPath = function (container: Element, calcPath: CursorPath) {
   return follow(container, calcPath.startPath()).bind(function (start) {
     return follow(container, calcPath.finishPath()).map(function (finish) {
       return range({
@@ -50,15 +69,15 @@ var followPath = function (container, calcPath) {
   });
 };
 
-var cFollowPath = function (calcPath) {
-  return Chain.binder(function (container) {
+var cFollowPath = function (calcPath: CursorPath) {
+  return Chain.binder(function (container: Element) {
     return followPath(container, calcPath);
   });
 };
 
-var cFollowCursor = function (elementPath, offset) {
-  return Chain.binder(function (container) {
-    return follow(container, elementPath).bind(function (element) {
+var cFollowCursor = function (elementPath: number[], offset: number) {
+  return Chain.binder(function (container: Element) {
+    return follow(container, elementPath).map(function (element) {
       return range({
         start: element,
         soffset: offset,
@@ -69,8 +88,8 @@ var cFollowCursor = function (elementPath, offset) {
   });
 };
 
-var cFollow = function (elementPath) {
-  return Chain.binder(function (container) {
+var cFollow = function (elementPath: number[]) {
+  return Chain.binder(function (container: Element) {
     return follow(container, elementPath);
   });
 };
@@ -78,11 +97,11 @@ var cFollow = function (elementPath) {
 var cToRange = Chain.mapper(range);
 var cToPath = Chain.mapper(path);
 
-var calculate = function (container, calcPath) {
+var calculate = function (container: Element, calcPath: CursorPath) {
   return followPath(container, calcPath).getOrDie();
 };
 
-var calculateOne = function (container, calcPath) {
+var calculateOne = function (container: Element, calcPath: number[]) {
   return follow(container, calcPath).getOrDie();
 };
 
