b803196fe6ae2a480a500de63a20b53bb46cd0c7
James Johnson <james.johnson@ephox.com>
2018-06-21 13:09:38 +1000

    TINY-1753: Replace var with const/let

--- src/main/ts/ephox/katamari/api/Obj.ts
+++ src/main/ts/ephox/katamari/api/Obj.ts
@@ -4,15 +4,15 @@ import { Option } from './Option';
 // http://jsperf.com/object-keys-iteration/107
 //
 // Use the native keys if it is available (IE9+), otherwise fall back to manually filtering
-var keys = (function () {
-  var fastKeys = Object.keys;
+const keys = (function () {
+  const fastKeys = Object.keys;
 
   // This technically means that 'each' and 'find' on IE8 iterate through the object twice.
   // This code doesn't run on IE8 much, so it's an acceptable tradeoff.
   // If it becomes a problem we can always duplicate the feature detection inside each and find as well.
-  var slowKeys = function (o: {}) {
-    var r: string[] = [];
-    for (var i in o) {
+  const slowKeys = function (o: {}) {
+    const r: string[] = [];
+    for (const i in o) {
       if (o.hasOwnProperty(i)) {
         r.push(i);
       }
@@ -24,17 +24,17 @@ var keys = (function () {
 })();
 
 
-var each = function <T> (obj: T, f: (value: any, key: string, obj: T) => void) {
-  var props = keys(obj);
-  for (var k = 0, len = props.length; k < len; k++) {
-    var i = props[k];
-    var x = obj[i];
+const each = function <T> (obj: T, f: (value: any, key: string, obj: T) => void) {
+  const props = keys(obj);
+  for (let k = 0, len = props.length; k < len; k++) {
+    const i = props[k];
+    const x = obj[i];
     f(x, i, obj);
   }
 };
 
 /** objectMap :: (JsObj(k, v), (v, k, JsObj(k, v) -> x)) -> JsObj(k, x) */
-var objectMap = function <R, T> (obj: T, f: (value: any, key: string, obj: T) => any) {
+const objectMap = function <R, T> (obj: T, f: (value: any, key: string, obj: T) => any) {
   return tupleMap<R, T>(obj, function (x, i, obj) {
     return {
       k: i,
@@ -44,21 +44,21 @@ var objectMap = function <R, T> (obj: T, f: (value: any, key: string, obj: T) =>
 };
 
 /** tupleMap :: (JsObj(k, v), (v, k, JsObj(k, v) -> { k: x, v: y })) -> JsObj(x, y) */
-var tupleMap = function <R, T> (obj: T, f: (value: any, key: string, obj: T) => {k: string, v: any}) : R {
-  var r: Record<string, any> = {};
+const tupleMap = function <R, T> (obj: T, f: (value: any, key: string, obj: T) => {k: string, v: any}) : R {
+  const r: Record<string, any> = {};
   each(obj, function (x, i) {
-    var tuple = f(x, i, obj);
+    const tuple = f(x, i, obj);
     r[tuple.k] = tuple.v;
   });
   return <R>r;
 };
 
 /** bifilter :: (JsObj(k, v), (v, k -> Bool)) -> { t: JsObj(k, v), f: JsObj(k, v) } */
-var bifilter = function <V> (obj: Record<string,V>, pred: (value: V, key: string) => boolean) {
-  var t: Record<string,V> = {};
-  var f: Record<string,V> = {};
+const bifilter = function <V> (obj: Record<string,V>, pred: (value: V, key: string) => boolean) {
+  const t: Record<string,V> = {};
+  const f: Record<string,V> = {};
   each(obj, function(x, i) {
-    var branch = pred(x, i) ? t : f;
+    const branch = pred(x, i) ? t : f;
     branch[i] = x;
   });
   return {
@@ -68,8 +68,8 @@ var bifilter = function <V> (obj: Record<string,V>, pred: (value: V, key: string
 };
 
 /** mapToArray :: (JsObj(k, v), (v, k -> a)) -> [a] */
-var mapToArray = function <T2> (obj: {}, f: (value: any, key: string) => T2) {
-  var r: T2[] = [];
+const mapToArray = function <T2> (obj: {}, f: (value: any, key: string) => T2) {
+  const r: T2[] = [];
   each(obj, function(value, name) {
     r.push(f(value, name));
   });
@@ -77,11 +77,11 @@ var mapToArray = function <T2> (obj: {}, f: (value: any, key: string) => T2) {
 };
 
 /** find :: (JsObj(k, v), (v, k, JsObj(k, v) -> Bool)) -> Option v */
-var find = function <V, T extends Record<string,V>> (obj: T, pred: (value: V, key: string, obj: T) => boolean): Option<V> {
-  var props = keys(obj);
-  for (var k = 0, len = props.length; k < len; k++) {
-    var i = props[k];
-    var x = obj[i];
+const find = function <V, T extends Record<string,V>> (obj: T, pred: (value: V, key: string, obj: T) => boolean): Option<V> {
+  const props = keys(obj);
+  for (let k = 0, len = props.length; k < len; k++) {
+    const i = props[k];
+    const x = obj[i];
     if (pred(x, i, obj)) {
       return Option.some(x);
     }
@@ -90,13 +90,13 @@ var find = function <V, T extends Record<string,V>> (obj: T, pred: (value: V, ke
 };
 
 /** values :: JsObj(k, v) -> [v] */
-var values = function <V> (obj: Record<string,V> | V[] | {}) {
+const values = function <V> (obj: Record<string,V> | V[] | {}) {
   return mapToArray(obj, function (v: V) {
     return v;
   });
 };
 
-var size = function (obj: {}) {
+const size = function (obj: {}) {
   return values(obj).length;
 };
 
