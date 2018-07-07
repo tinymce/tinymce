7d6d1d703d6d7073cecac46f16b99865a302d693
hejinjun <hejinjun529@163.com>
2018-07-07 12:50:14 +0800

    # 代码不规范修改

--- src/plugins/mathtools/main/ts/core/Utils.ts
+++ src/plugins/mathtools/main/ts/core/Utils.ts
@@ -8,7 +8,7 @@
  * Contributing: http://www.tinymce.com/contributing
  */
 const loadMathJax = function () {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     const config = document.createElement('script');
     config.type = 'text/x-mathjax-config';
     config.text = `
@@ -52,55 +52,54 @@ const loadMathJax = function () {
 
     const script = document.createElement('script');
     document.body.appendChild(script);
-    script.onload = function() {
-      resolve()
+    script.onload = function () {
+      resolve();
     };
-    script.onerror = function() {
-      reject()
+    script.onerror = function () {
+      reject();
     };
     script.src = 'https://static.speiyou.com/MathJax-2.6.1/MathJax.js';
-  })
-}
+  });
+};
 
 const wraperLatex = function (latex) {
-  return '$' + latex + '$'
-}
+  return '$' + latex + '$';
+};
 
-// const wrapperSVG = function (svg, latex) {
-//   return `<span class="mathjax_content" title="${latex}">
-//             ${svg}
-//           </span>`
-// }
+const wrapperSVG = function (svg, latex) {
+  return `<span class="mathjax_content" title="${latex}">${svg}</span>`;
+};
 
 const renderLatex = function (latex) {
-  const MathJax = (<any>window).MathJax;
+  const MathJax = (<any> window).MathJax;
   const element = document.createElement('span');
-  element.textContent = wraperLatex(latex)
+  element.textContent = wraperLatex(latex);
   element.style.opacity = '0';
   document.body.appendChild(element);
   return new Promise(function (resolve, reject) {
-    MathJax.Hub.Queue(["Typeset", MathJax.Hub, element], function(){
-      const outputEle = element.querySelector('.MathJax_SVG svg').cloneNode(true);
-      // const svgOutput = wrapperSVG(outputEle.innerHTML, latex);
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub, element], function () {
+      const outputEle = element.querySelector('.MathJax_SVG');
+      const svgOutput = wrapperSVG(outputEle.innerHTML, latex);
       element.parentElement.removeChild(element);
-      resolve(outputEle)
+      resolve(svgOutput);
     });
-  })
-}
+  });
+};
 
 const getSVGLatex = function (latex) {
   return new Promise(function (resolve, reject) {
-    if ((<any>window).MathJax) {
-      resolve(renderLatex(latex))
+    if ((<any> window).MathJax) {
+      resolve(renderLatex(latex));
     } else {
-     loadMathJax().then(function() {
-        renderLatex(latex).then(function(data){
-          resolve(data)
-        })
-      })
+     loadMathJax().then(function () {
+        renderLatex(latex).then(function (data) {
+          resolve(data);
+        });
+      });
     }
-  })
-}
+  });
+};
+
 export default {
   getSVGLatex
 };
