dd3e0b48a7d2c89fd9ca67f1a9b194fe6843ca3a
Joshua Haines <josh.j.haines@gmail.com>
2018-07-20 16:16:05 +1000

    fix yesterdays commit

--- src/main/ts/ephox/alloy/api/ui/FormField.ts
+++ src/main/ts/ephox/alloy/api/ui/FormField.ts
@@ -55,10 +55,7 @@ const factory: CompositeSketchFactory<FormFieldDetail, FormFieldSpec> = (detail,
         ps['aria-descriptor']().each((descriptor) => {
           const descriptorId = Id.generate(detail.prefix());
           Attr.set(descriptor.element(), 'id', descriptorId);
-          Attr.set(field.element(), 'aria-describedby', descriptorId);
-          if (ps.label().toOption().isNone()) {
-            Attr.set(field.element(), 'aria-labelledby', descriptorId);
-          }
+          Attr.set(field.element(), 'aria-labelledby', descriptorId);
         })
       });
     })
