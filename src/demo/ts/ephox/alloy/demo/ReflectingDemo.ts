969e796c67a00a476e6f4cc79a16c5763ac115b6
morgan <morgan.smith@ephox.com>
2018-08-07 18:13:37 +1000

    AP-114: improving the dynamic nature of the demo page

--- src/demo/ts/ephox/alloy/demo/ReflectingDemo.ts
+++ src/demo/ts/ephox/alloy/demo/ReflectingDemo.ts
@@ -1,4 +1,4 @@
-import { Class, Element } from '@ephox/sugar';
+import { Class, Element, Value } from '@ephox/sugar';
 import * as Behaviour from 'ephox/alloy/api/behaviour/Behaviour';
 import { Replacing } from 'ephox/alloy/api/behaviour/Replacing';
 import * as GuiFactory from 'ephox/alloy/api/component/GuiFactory';
@@ -10,6 +10,9 @@ import * as HtmlDisplay from 'ephox/alloy/demo/HtmlDisplay';
 import { document, setTimeout } from '@ephox/dom-globals';
 import { Reflecting } from '../../../../../main/ts/ephox/alloy/api/behaviour/Reflecting';
 import { Arr } from '@ephox/katamari';
+import { Input } from 'ephox/alloy/api/ui/Input';
+import { Streaming } from 'ephox/alloy/api/behaviour/Streaming';
+import * as DomFactory from 'ephox/alloy/api/component/DomFactory';
 
 export default (): void => {
   const gui = Gui.create();
@@ -19,66 +22,48 @@ export default (): void => {
 
   const list = HtmlDisplay.section(
     gui,
-    'This list will change after three seconds (when the square is added to the page)',
+    'Change the entries by entering data into the input field. Use a comma delimiter for multiple entries',
     Container.sketch({
-      dom: {
-        tag: 'ol'
-      },
-      components: [ ],
+      components: [
+        Input.sketch({
+          inputBehaviours: Behaviour.derive([
+            Streaming.config({
+              stream: {
+                mode: 'throttle',
+                delay: 2000
+              },
+              onStream: (comp) => {
+                comp.getSystem().broadcastOn([ 'reflecting-channel' ], {
+                  items: Value.get(comp.element()).split(',')
+                })
+              }
+            })
+          ])
+        }),
 
-      containerBehaviours: Behaviour.derive([
-        Reflecting.config({
-          channel: 'reflecting-channel',
-          renderComponents: (data: { items: string [] }) => {
-            return Arr.map(data.items, (d) => {
-              return {
-                dom: { tag: 'li', innerHtml: d }
-              };
-            });
-          }
+        {
+          dom: DomFactory.fromHtml('<h2>Entries</h2>')
+        },
+        Container.sketch({
+          dom: {
+            tag: 'ol'
+          },
+          components: [ ],
+
+          containerBehaviours: Behaviour.derive([
+            Reflecting.config({
+              channel: 'reflecting-channel',
+              renderComponents: (data: { items: string [] }) => {
+                return Arr.map(data.items, (d) => {
+                  return {
+                    dom: { tag: 'li', innerHtml: d }
+                  };
+                });
+              }
+            })
+          ])
         })
-      ])
+      ]
     })
   );
-
-  const square = GuiFactory.build({
-    dom: {
-      tag: 'div',
-      styles: {
-        position: 'absolute',
-        width: '20px',
-        height: '20px',
-        background: 'black'
-      }
-    },
-
-    events: AlloyEvents.derive([
-      AlloyEvents.runOnAttached((sq, simulatedEvent) => {
-        simulatedEvent.stop();
-        Replacing.append(list, {
-          dom: {
-            tag: 'li',
-            innerHtml: 'The square has been attached to the DOM: ' + new Date().getSeconds()
-          }
-        });
-      }),
-
-      AlloyEvents.runOnInit((sq, simulatedEvent) => {
-        simulatedEvent.stop();
-        Replacing.append(list, {
-          dom: {
-            tag: 'li',
-            innerHtml: 'The square has been added to the system: ' + new Date().getSeconds()
-          }
-        });
-      })
-    ])
-  });
-
-  setTimeout(() => {
-    list.getSystem().broadcastOn([ 'reflecting-channel' ], {
-      items: [ 'alpha', 'beta', 'gamma' ]
-    })
-  }, 2000);
-
 };
