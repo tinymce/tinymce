2f0681fbb61da4c9931f3c7c5048e91c436d3e73
morgan <morgan.smith@ephox.com>
2018-08-21 01:56:21 +1000

    AP-156: experimenting with tooltips

--- src/main/ts/ephox/alloy/behaviour/tooltipping/ActiveTooltipping.ts
+++ src/main/ts/ephox/alloy/behaviour/tooltipping/ActiveTooltipping.ts
@@ -30,6 +30,7 @@ const events = (tooltipConfig: TooltippingConfig, state: TooltippingState): Allo
       const sink = tooltipConfig.lazySink()(comp).getOrDie();
       const popup = comp.getSystem().build({
         dom: tooltipConfig.tooltipDom(),
+        components: tooltipConfig.components(),
         events: AlloyEvents.derive([
           AlloyEvents.run(NativeEvents.mouseover(), (_) => {
             AlloyTriggers.emit(comp, ShowTooltipEvent);
@@ -85,6 +86,10 @@ const events = (tooltipConfig: TooltippingConfig, state: TooltippingState): Allo
    }),
     AlloyEvents.run(NativeEvents.mouseout(), (comp) => {
       AlloyTriggers.emit(comp, HideTooltipEvent);
+    }),
+
+    AlloyEvents.runOnDetached((comp) => {
+      hide();
     })
   ]);
 
