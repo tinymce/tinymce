96b66e9748f2c65ef5f12f418efdb505a0a9ea3b
Millie Macdonald <millie.macdonald@ephox.com>
2018-08-27 17:02:36 +1000

    AP-176 Finished Log.step() case of tests

--- src/test/ts/atomic/api/LogTest.ts
+++ src/test/ts/atomic/api/LogTest.ts
@@ -1,10 +1,18 @@
 import { UnitTest } from "@ephox/bedrock";
-import { Pipeline } from "../../../../main/ts/ephox/agar/api/Main";
+import { Pipeline, Step, Assertions } from "../../../../main/ts/ephox/agar/api/Main";
+import StepAssertions from "../../module/ephox/agar/test/StepAssertions";
+import Log from "../../../../main/ts/ephox/agar/api/Log";
 
 UnitTest.asynctest('LogTest', (success, failure) => {
+  const logStepTest = StepAssertions.testStepsFail(
+    'TestCase-01-Step failure',
+    [
+      Log.step('01', 'Step failure', Assertions.sAssertEq('Assert failure', true, false))
+    ]
+  );
 
   Pipeline.async({}, [
-
+    logStepTest
   ], success, failure);
 
 });
