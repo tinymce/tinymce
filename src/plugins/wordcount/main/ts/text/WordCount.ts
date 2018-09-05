ec2d7cac0913765499f428fe0af344694f587d90
Simon Fjeldså <simon.fjeldsa@ephox.com>
2018-09-05 15:36:14 +0200

    TINY-1682: add custom innerText for ie

--- src/plugins/wordcount/main/ts/text/WordCount.ts
+++ src/plugins/wordcount/main/ts/text/WordCount.ts
@@ -8,13 +8,43 @@
  * Contributing: http://www.tinymce.com/contributing
  */
 
+import { CharacterData, Node } from '@ephox/dom-globals';
+import TreeWalker from 'tinymce/core/api/dom/TreeWalker';
+import { Editor } from 'tinymce/core/api/Editor';
+import Env from 'tinymce/core/api/Env';
+import Schema, { SchemaMap } from 'tinymce/core/api/html/Schema';
 import WordGetter from './WordGetter';
 
-const getTextContent = function (editor) {
-  return editor.removed ? '' : editor.getBody().innerText;
+const getText = (node: Node, schema: Schema): string => {
+  const blockElements: SchemaMap = schema.getBlockElements();
+  const shortEndedElements: SchemaMap = schema.getShortEndedElements();
+  const whiteSpaceElements = schema.getWhiteSpaceElements();
+  const isSeparator = (node: Node) => (
+    blockElements[node.nodeName] || shortEndedElements[node.nodeName] || whiteSpaceElements[node.nodeName]
+  );
+
+  let txt = '';
+  const treeWalker = new TreeWalker(node, node);
+  while ((node = treeWalker.next())) {
+    if (node.nodeType === 3) {
+      txt += (node as CharacterData).data;
+    } else if (isSeparator(node)) {
+      txt += ' ';
+    }
+  }
+
+  return txt;
 };
 
-const getCount = function (editor) {
+const innerText = (node: Node, schema: Schema) => {
+  return Env.ie ? getText(node, schema) : (node as any).innerText;
+};
+
+const getTextContent = (editor: Editor) => {
+  return editor.removed ? '' : innerText(editor.getBody(), editor.schema);
+};
+
+const getCount = (editor: Editor) => {
   return WordGetter.getWords(getTextContent(editor)).length;
 };
 
