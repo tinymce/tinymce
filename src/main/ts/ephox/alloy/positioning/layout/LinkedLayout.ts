e28df79f9c0c4544834feeede48f11d1f5b549d4
morgan <morgan.smith@ephox.com>
2018-09-19 12:18:15 +1000

    bubbles: adding more anchor types

--- src/main/ts/ephox/alloy/positioning/layout/LinkedLayout.ts
+++ src/main/ts/ephox/alloy/positioning/layout/LinkedLayout.ts
@@ -9,39 +9,39 @@ import { AnchorBox, AnchorElement, AnchorLayout } from '../../positioning/layout
  */
 
 // display element to the right, left edge against the right of the menu
-const east = (anchor: AnchorBox): number => {
+const eastX = (anchor: AnchorBox): number => {
   return anchor.x() + anchor.width();
 };
 
 // display element to the left, right edge against the left of the menu
-const west = (anchor: AnchorBox, element: AnchorElement): number => {
+const westX = (anchor: AnchorBox, element: AnchorElement): number => {
   return anchor.x() - element.width();
 };
 
 // display element pointing up, bottom edge against the bottom of the menu (usually to one side)
-const north = (anchor: AnchorBox, element: AnchorElement): number => {
+const northY = (anchor: AnchorBox, element: AnchorElement): number => {
   return anchor.y() - element.height() + anchor.height();
 };
 
 // display element pointing down, top edge against the top of the menu (usually to one side)
-const south = (anchor: AnchorBox): number => {
+const southY = (anchor: AnchorBox): number => {
   return anchor.y();
 };
 
 const southeast: AnchorLayout = (anchor, element, bubbles) => {
-  return NuSpotInfo(east(anchor), south(anchor), bubbles.southeast(), Direction.southeast(), 'link-layout-se');
+  return NuSpotInfo(eastX(anchor), southY(anchor), bubbles.southeast(), Direction.southeast(), 'link-layout-se');
 };
 
 const southwest: AnchorLayout = (anchor, element, bubbles) => {
-  return NuSpotInfo(west(anchor, element), south(anchor), bubbles.southwest(), Direction.southwest(), 'link-layout-sw');
+  return NuSpotInfo(westX(anchor, element), southY(anchor), bubbles.southwest(), Direction.southwest(), 'link-layout-sw');
 };
 
 const northeast: AnchorLayout = (anchor, element, bubbles) => {
-  return NuSpotInfo(east(anchor), north(anchor, element), bubbles.northeast(), Direction.northeast(), 'link-layout-ne');
+  return NuSpotInfo(eastX(anchor), northY(anchor, element), bubbles.northeast(), Direction.northeast(), 'link-layout-ne');
 };
 
 const northwest: AnchorLayout = (anchor, element, bubbles) => {
-  return NuSpotInfo(west(anchor, element), north(anchor, element), bubbles.northwest(), Direction.northwest(), 'link-layout-nw');
+  return NuSpotInfo(westX(anchor, element), northY(anchor, element), bubbles.northwest(), Direction.northwest(), 'link-layout-nw');
 };
 
 const all = (): AnchorLayout[] => {
