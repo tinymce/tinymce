63faf4db28c1bd4a99f5d276cfa67079a727cfce
Joshua Haines <josh.j.haines@gmail.com>
2018-09-21 09:26:32 +1000

    AP-251: prevent keyboard navigation to disabled toolbar buttons

--- src/main/ts/ephox/alloy/navigation/DomNavigation.ts
+++ src/main/ts/ephox/alloy/navigation/DomNavigation.ts
@@ -2,15 +2,30 @@ import { Fun, Option } from '@ephox/katamari';
 
 import * as Cycles from '../alien/Cycles';
 import * as DomPinpoint from './DomPinpoint';
-import { Element } from '@ephox/sugar';
+import { Element, Attr, Node } from '@ephox/sugar';
 
 const horizontal = (container, selector, current, delta): Option<Element> => {
+
+  const isDisabledButton = (candidate) => {
+    return Node.name(candidate) === 'button' && Attr.get(candidate, 'disabled') === 'disabled';
+  };
+
+  const tryCycle = (initial, index, candidates) => {
+    const newIndex = Cycles.cycleBy(index, delta, 0, candidates.length - 1);
+    if (newIndex === initial) {
+      return Option.none();
+    } else {
+      return isDisabledButton(candidates[newIndex]) ?
+        tryCycle(initial, newIndex, candidates) :
+        Option.from(candidates[newIndex]);
+    }
+  };
+
   // I wonder if this will be a problem when the focused element is invisible (shouldn't happen)
   return DomPinpoint.locateVisible(container, current, selector).bind((identified) => {
     const index = identified.index();
     const candidates = identified.candidates();
-    const newIndex = Cycles.cycleBy(index, delta, 0, candidates.length - 1);
-    return Option.from(candidates[newIndex]);
+    return tryCycle(index, index, candidates);
   });
 };
 
