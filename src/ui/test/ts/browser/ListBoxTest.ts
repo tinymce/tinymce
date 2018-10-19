0e0f89646a487bd3d5d803a8dad8d6752c12a4b3
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2018-10-19 13:41:13 +0200

    fix linting problems

--- src/ui/test/ts/browser/ListBoxTest.ts
+++ src/ui/test/ts/browser/ListBoxTest.ts
@@ -7,7 +7,7 @@ import Api from 'tinymce/ui/Api';
 import Factory from 'tinymce/core/api/ui/Factory';
 import Tools from 'tinymce/core/api/util/Tools';
 import { UnitTest } from '@ephox/bedrock';
-import * as Assertions from "@ephox/agar/lib/main/ts/ephox/agar/api/Assertions";
+import * as Assertions from '@ephox/agar/lib/main/ts/ephox/agar/api/Assertions';
 
 UnitTest.asynctest('browser.tinymce.ui.ListBoxtest', function () {
   const success = arguments[arguments.length - 2];
@@ -48,13 +48,13 @@ UnitTest.asynctest('browser.tinymce.ui.ListBoxtest', function () {
       ]
     });
 
-    Assertions.assertEq("Should equal the first value", "/", listBox.value());
+    Assertions.assertEq('Should equal the first value', '/', listBox.value());
 
     // Simulate selecting an item by setting a value
-    listBox.value("/c1/foo");
+    listBox.value('/c1/foo');
 
-    Assertions.assertEq("Should equal the selected items value", "/c1/foo", listBox.value());
-    Assertions.assertEq("Should equal the selected items text", "C1 Page 1", listBox.text());
+    Assertions.assertEq('Should equal the selected items value', '/c1/foo', listBox.value());
+    Assertions.assertEq('Should equal the selected items text', 'C1 Page 1', listBox.text());
   });
 
   UiUtils.loadSkinAndOverride(viewBlock, function () {
