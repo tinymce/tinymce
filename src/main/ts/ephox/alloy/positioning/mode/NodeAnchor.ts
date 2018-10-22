3dcd9e9620ad6f6ef734a73aa59b18e4557ae120
Joshua Haines <josh.j.haines@gmail.com>
2018-10-22 15:10:19 +1000

    AP-318: changelog, package.json and code reuse

--- src/main/ts/ephox/alloy/positioning/mode/NodeAnchor.ts
+++ src/main/ts/ephox/alloy/positioning/mode/NodeAnchor.ts
@@ -1,17 +1,16 @@
 import { FieldSchema } from '@ephox/boulder';
-import { Fun, Option } from '@ephox/katamari';
+import { Option } from '@ephox/katamari';
 import { Position } from '@ephox/sugar';
 
 import * as Boxes from '../../alien/Boxes';
 import * as CssPosition from '../../alien/CssPosition';
 import * as Fields from '../../data/Fields';
-import * as Bubble from '../layout/Bubble';
-import * as Layout from '../layout/Layout';
 import * as Origins from '../layout/Origins';
-import { NodeAnchor, nu as NuAnchor, Anchoring } from './Anchoring';
+import { NodeAnchor, Anchoring } from './Anchoring';
 import * as ContainerOffsets from './ContainerOffsets';
 import { AlloyComponent } from '../../api/component/ComponentApi';
 import * as AnchorLayouts from './AnchorLayouts';
+import ContentAnchorCommon from './ContentAnchorCommon';
 
 const placement = (component: AlloyComponent, anchorInfo: NodeAnchor, origin: Origins.OriginAdt): Option<Anchoring> => {
   const rootPoint = ContainerOffsets.getRootPoint(component, origin, anchorInfo);
@@ -20,52 +19,9 @@ const placement = (component: AlloyComponent, anchorInfo: NodeAnchor, origin: Or
     const rect = target.dom().getBoundingClientRect();
     const point = CssPosition.screen(Position(rect.left, rect.top));
     const nodeBox = Option.some(Boxes.pointed(point, rect.width, rect.height));
-    return nodeBox.map((box) => {
-    const points = [ rootPoint, box.point() ];
-    const topLeft = Origins.cata(origin,
-      () => {
-        return CssPosition.sumAsAbsolute(points);
-      },
-      () => {
-        return CssPosition.sumAsAbsolute(points);
-      },
-      () => {
-        return CssPosition.sumAsFixed(points);
-      }
-    );
-
-    const anchorBox = Boxes.rect(
-      topLeft.left(),
-      topLeft.top(),
-      box.width(),
-      box.height()
-    );
-
-    const layoutsLtr = (): Layout.AnchorLayout[] => {
-      return anchorInfo.showAbove() ?
-        [ Layout.northeast, Layout.northwest, Layout.southeast, Layout.southwest, Layout.north, Layout.south ] :
-        [ Layout.southeast, Layout.southwest, Layout.northeast, Layout.northwest, Layout.south, Layout.south ];
-    };
-
-    const layoutsRtl = (): Layout.AnchorLayout[] => {
-      return anchorInfo.showAbove() ?
-        [ Layout.northwest, Layout.northeast, Layout.southwest, Layout.southeast, Layout.north, Layout.south ] :
-        [ Layout.southwest, Layout.southeast, Layout.northwest, Layout.northeast, Layout.south, Layout.north ];
-    };
-
     const elem = anchorInfo.node().getOr(component.element());
-
-    const layouts = AnchorLayouts.get(elem, anchorInfo, layoutsLtr(), layoutsRtl());
-
-    return NuAnchor({
-      anchorBox: Fun.constant(anchorBox),
-      bubble: Fun.constant(anchorInfo.bubble().getOr(Bubble.fallback())),
-      overrides: anchorInfo.overrides,
-      layouts: Fun.constant(layouts),
-      placer: Option.none
-    });
+    return ContentAnchorCommon.calcNewAnchor(nodeBox, rootPoint, anchorInfo, origin, elem);
   });
-});
 };
 
 export default [
@@ -74,7 +30,7 @@ export default [
   FieldSchema.option('bubble'),
   AnchorLayouts.schema(),
   // chiefly MaxHeight.expandable()
-  FieldSchema.defaulted('overrides', { }),
+  FieldSchema.defaulted('overrides', {}),
   FieldSchema.defaulted('showAbove', false),
   Fields.output('placement', placement)
 ];
--- src/main/ts/ephox/alloy/positioning/mode/SelectionAnchor.ts
+++ src/main/ts/ephox/alloy/positioning/mode/SelectionAnchor.ts
@@ -1,5 +1,5 @@
 import { FieldSchema } from '@ephox/boulder';
-import { Fun, Option, Struct, Unicode, Arr } from '@ephox/katamari';
+import { Option, Struct, Unicode } from '@ephox/katamari';
 import { Element, Insert, Node, Position, Remove, Selection, Traverse, WindowSelection, Compare } from '@ephox/sugar';
 
 import { Window } from '@ephox/dom-globals';
@@ -8,15 +8,13 @@ import * as Boxes from '../../alien/Boxes';
 import * as CssPosition from '../../alien/CssPosition';
 import * as Descend from '../../alien/Descend';
 import * as Fields from '../../data/Fields';
-import * as Bubble from '../layout/Bubble';
-import * as Layout from '../layout/Layout';
 import * as Origins from '../layout/Origins';
-import { SelectionAnchor, nu as NuAnchor, Anchoring } from './Anchoring';
+import { SelectionAnchor, Anchoring } from './Anchoring';
 import * as ContainerOffsets from './ContainerOffsets';
 import { AlloyComponent } from '../../api/component/ComponentApi';
-import { PositioningConfig } from '../../behaviour/positioning/PositioningTypes';
 import { SugarRange } from '../../alien/TypeDefinitions';
 import * as AnchorLayouts from './AnchorLayouts';
+import ContentAnchorCommon from './ContentAnchorCommon';
 
 const point: (element: Element, offset: number) => {element: () => Element; offset: () => number; } = Struct.immutable('element', 'offset');
 
@@ -68,54 +66,12 @@ const placement = (component: AlloyComponent, anchorInfo: SelectionAnchor, origi
     });
   });
 
-  return selectionBox.map((box) => {
-    const points = [ rootPoint, box.point() ];
-    const topLeft = Origins.cata(origin,
-      () => {
-        return CssPosition.sumAsAbsolute(points);
-      },
-      () => {
-        return CssPosition.sumAsAbsolute(points);
-      },
-      () => {
-        return CssPosition.sumAsFixed(points);
-      }
-    );
-
-    const anchorBox = Boxes.rect(
-      topLeft.left(),
-      topLeft.top(),
-      box.width(),
-      box.height()
-    );
-
-    const targetElement: Option<Element> = getAnchorSelection(win, anchorInfo).bind((sel) => {
-      return Node.isElement(sel.start()) ? Option.some(sel.start()) : Traverse.parent(sel.start());
-    });
-
-    const layoutsLtr = (): Layout.AnchorLayout[] => {
-      return anchorInfo.showAbove() ?
-        [ Layout.northeast, Layout.northwest, Layout.southeast, Layout.southwest, Layout.north, Layout.south ] :
-        [ Layout.southeast, Layout.southwest, Layout.northeast, Layout.northwest, Layout.south, Layout.south ];
-    };
-
-    const layoutsRtl = (): Layout.AnchorLayout[] => {
-      return anchorInfo.showAbove() ?
-        [ Layout.northwest, Layout.northeast, Layout.southwest, Layout.southeast, Layout.north, Layout.south ] :
-        [ Layout.southwest, Layout.southeast, Layout.northwest, Layout.northeast, Layout.south, Layout.north ];
-    };
-
-    const elem = targetElement.getOr(component.element());
-    const layouts = AnchorLayouts.get(elem, anchorInfo, layoutsLtr(), layoutsRtl());
-
-    return NuAnchor({
-      anchorBox: Fun.constant(anchorBox),
-      bubble: Fun.constant(anchorInfo.bubble().getOr(Bubble.fallback())),
-      overrides: anchorInfo.overrides,
-      layouts: Fun.constant(layouts),
-      placer: Option.none
-    });
+  const targetElement: Option<Element> = getAnchorSelection(win, anchorInfo).bind((sel) => {
+    return Node.isElement(sel.start()) ? Option.some(sel.start()) : Traverse.parent(sel.start());
   });
+  const elem = targetElement.getOr(component.element());
+
+  return ContentAnchorCommon.calcNewAnchor(selectionBox, rootPoint, anchorInfo, origin, elem);
 };
 
 export default [
