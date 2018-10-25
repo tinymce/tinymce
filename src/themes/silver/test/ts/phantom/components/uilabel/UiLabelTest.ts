5e1512646c4d7597e46dc8f1673e6206b721d383
Lawanya Baskarran <lawanya.baskarran@ephox.com>
2018-10-25 16:35:45 +1000

    AP-385 Image and Table Dialog: Added caption label to the caption checkbox

--- src/themes/silver/test/ts/phantom/components/uilabel/UiLabelTest.ts
+++ src/themes/silver/test/ts/phantom/components/uilabel/UiLabelTest.ts
@@ -1,35 +1,36 @@
 import { ApproxStructure, Assertions } from '@ephox/agar';
 import { GuiFactory } from '@ephox/alloy';
 import { UnitTest } from '@ephox/bedrock';
-import { Option } from '@ephox/katamari';
 import { setupDemo } from 'src/themes/silver/demo/ts/components/DemoHelpers';
-import { renderCheckbox } from 'tinymce/themes/silver/ui/general/Checkbox';
 import { renderUiLabel } from 'tinymce/themes/silver/ui/general/UiLabel';
 
-import { renderInput } from '../../../../../main/ts/ui/dialog/TextField';
 import { GuiSetup } from '../../../module/AlloyTestUtils';
 
 UnitTest.asynctest('Ui Label component Test', (success, failure) => {
   const helpers = setupDemo();
-  const providers = helpers.extras.backstage.shared.providers;
+  const sharedBackstage = helpers.extras.backstage.shared;
 
   GuiSetup.setup(
     (store, doc, body) => {
       return GuiFactory.build(
         renderUiLabel({
+          type: 'label',
           label: 'Group of Options',
           items: [
-            renderCheckbox({
-              label: 'check box item 1',
-              name: 'one'
-            }, providers),
-            renderInput({
-              label: Option.some('exampleInput'),
-              name: 'exampleinputfieldname',
-              validation: Option.none()
-            }, providers)
+            {
+              dom: {
+                tag: 'label',
+                classes: ['tox-checkbox']
+              }
+            } as any,
+            {
+              dom: {
+                tag: 'div',
+                classes: ['tox-form__group']
+              }
+            } as any
           ]
-        })
+        }, sharedBackstage)
       );
     },
     (doc, body, gui, component, store) => {
