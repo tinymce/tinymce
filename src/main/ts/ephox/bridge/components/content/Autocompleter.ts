d9c4caedaef45a82b23607ad55545e6124ee9edf
Lee Newson <lee.newson@tiny.cloud>
2018-10-29 11:36:51 +1000

    AP-366: Swapped minChars to use 0 by default instead of 2, so as not to break other plugins that do need to trigger with no content

--- src/main/ts/ephox/bridge/components/content/Autocompleter.ts
+++ src/main/ts/ephox/bridge/components/content/Autocompleter.ts
@@ -61,7 +61,7 @@ const autocompleterItemSchema = ValueSchema.objOf([
 const autocompleterSchema = ValueSchema.objOf([
   FieldSchema.strictString('type'),
   FieldSchema.strictString('ch'),
-  FieldSchema.defaultedNumber('minChars', 2),
+  FieldSchema.defaultedNumber('minChars', 0),
   FieldSchema.defaulted('columns', 1),
   FieldSchema.defaultedNumber('maxResults', 10),
   FieldSchema.defaultedFunction('matches', () => true),
