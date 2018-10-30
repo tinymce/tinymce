165bec5aee8d7476a2277961ea55fe3e11b89ddc
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2018-10-30 14:01:13 +0100

    skip corstest on ie11

--- src/core/test/ts/browser/dom/ContentCssCorsTest.ts
+++ src/core/test/ts/browser/dom/ContentCssCorsTest.ts
@@ -3,6 +3,7 @@ import { UnitTest } from '@ephox/bedrock';
 import { Editor } from 'tinymce/core/api/Editor';
 import { Editor as McEditor } from '@ephox/mcagar';
 import { Pipeline, RawAssertions, Logger, Chain } from '@ephox/agar';
+import Env from 'tinymce/core/api/Env';
 
 UnitTest.asynctest('browser.tinymce.core.dom.ContentCssCorsTest', (success, failure) => {
   Theme();
@@ -12,7 +13,7 @@ UnitTest.asynctest('browser.tinymce.core.dom.ContentCssCorsTest', (success, fail
     RawAssertions.assertEq('should have link with crossorigin="anonymous"', expected, corsLinks.length > 0);
   });
 
-  Pipeline.async({}, [
+  Pipeline.async({}, Env.ie < 12 ? [] : [
     Logger.t('assert crossorigin link presence with setting set', Chain.asStep({}, [
       McEditor.cFromSettings({ content_css_cors: true, skin_url: '/project/js/tinymce/skins/lightgray' }),
       cAssertCorsLinkPresence(true),
