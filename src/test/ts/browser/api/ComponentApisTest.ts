9f8431033643eabd23e1008eb8aa561b85915431
morgan <morgan.smith@ephox.com>
2018-11-01 18:07:55 +1000

    TC-3-runApis: Basic testing of component APIs

---
+++ src/test/ts/browser/api/ComponentApisTest.ts
@@ -0,0 +1,46 @@
+import { Step, GeneralSteps, Logger, Assertions } from '@ephox/agar';
+import { UnitTest } from '@ephox/bedrock';
+import * as GuiFactory from 'ephox/alloy/api/component/GuiFactory';
+import * as GuiSetup from 'ephox/alloy/test/GuiSetup';
+
+UnitTest.asynctest('ComponentApisTest', (success, failure) => {
+  GuiSetup.setup((store, doc, body) => {
+    return GuiFactory.build({
+      dom: {
+        tag: 'div',
+        classes: [ 'component-with-api' ]
+      },
+      apis: {
+        doFirstThing: store.adder('doFirstThing'),
+        doSecondThing: store.adder('doSecondThing'),
+        doThirdThing: (comp) => 'thirdThing'
+      }
+    });
+  }, (doc, body, gui, component, store) => {
+    return [
+      Logger.t('Testing "doFirstThing"', GeneralSteps.sequence([
+        store.sAssertEq('No APIs have been called yet', [ ]),
+        Step.sync(() => {
+          component.runApi<any, any>((apis) => apis.doFirstThing());
+        }),
+        store.sAssertEq('First thing API should have been called', [ 'doFirstThing' ]),
+        store.sClear
+      ])),
+
+      Logger.t('Testing "doSecondThing"', GeneralSteps.sequence([
+        Step.sync(() => {
+          component.runApi<any, any>((apis) => apis.doSecondThing());
+        }),
+        store.sAssertEq('Second thing API should have been called', [ 'doSecondThing' ]),
+        store.sClear
+      ])),
+
+      Logger.t('Testing "doThirdThing"', GeneralSteps.sequence([
+        Step.sync(() => {
+          const value = component.runApi<any, any>((apis) => apis.doThirdThing());
+          Assertions.assertEq('Checking api return value', 'thirdThing', value);
+        })
+      ]))
+    ];
+  }, success, failure);
+});
