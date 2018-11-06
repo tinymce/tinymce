f5120ccbc0df662b061024ceffc5dc179f92330f
Lee Newson <lee.newson@tiny.cloud>
2018-11-06 13:56:58 +1000

    TINY-2281: Allow placeholder text to be specified on input/textarea elements

--- src/main/ts/ephox/bridge/components/dialog/Input.ts
+++ src/main/ts/ephox/bridge/components/dialog/Input.ts
@@ -1,16 +1,20 @@
-import { ValueSchema } from '@ephox/boulder';
-import { Result } from '@ephox/katamari';
+import { FieldSchema, ValueSchema } from '@ephox/boulder';
+import { Option, Result } from '@ephox/katamari';
 import { FormComponentApi, FormComponent, formComponentFields } from './FormComponent';
 
 export interface InputApi extends FormComponentApi {
   type: 'input';
+  placeholder?: string;
 }
 
 export interface Input extends FormComponent {
   type: 'input';
+  placeholder?: Option<string>;
 }
 
-export const inputFields = formComponentFields;
+export const inputFields = formComponentFields.concat([
+  FieldSchema.optionString('placeholder')
+]);
 
 export const inputSchema = ValueSchema.objOf(inputFields);
 
