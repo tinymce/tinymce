4923012650ce3d7ab297254160c670165dbd66de
Lee Newson <lee.newson@tiny.cloud>
2018-11-06 15:35:59 +1000

    AP-435: Use HTMLAnchorElement instead of Element for the type in getHref()

--- src/plugins/link/main/ts/core/Utils.ts
+++ src/plugins/link/main/ts/core/Utils.ts
@@ -8,11 +8,11 @@
  * Contributing: http://www.tinymce.com/contributing
  */
 
-import { Element } from '@ephox/dom-globals';
+import { HTMLAnchorElement } from '@ephox/dom-globals';
 import Tools from 'tinymce/core/api/util/Tools';
 import Settings from '../api/Settings';
 
-const getHref = (elm: Element) => {
+const getHref = (elm: HTMLAnchorElement): string => {
   // Returns the real href value not the resolved a.href value
   const href = elm.getAttribute('data-mce-href');
   return href ? href : elm.getAttribute('href');
