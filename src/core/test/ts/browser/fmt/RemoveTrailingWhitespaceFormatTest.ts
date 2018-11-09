cc79ab541d3e1d3cf5856af72a1758c77ac2e3a4
James Johnson <james.johnson@ephox.com>
2018-11-09 10:53:29 +1000

    AP-283: Fixed button selectors

--- src/core/test/ts/browser/fmt/RemoveTrailingWhitespaceFormatTest.ts
+++ src/core/test/ts/browser/fmt/RemoveTrailingWhitespaceFormatTest.ts
@@ -17,30 +17,33 @@ UnitTest.asynctest(
       const tinyApis = TinyApis(editor);
       const tinyUi = TinyUi(editor);
 
+      const boldSelector = 'button[aria-label="Bold"]';
+      const unlinkSelector = 'button:has(span>svg>g[id="icon-unlink"])';
+
       Pipeline.async({}, [
         tinyApis.sFocus,
         Logger.t('remove bold with leading whitespace', GeneralSteps.sequence([
           tinyApis.sSetRawContent('<p><strong>a b</strong></p>'),
           tinyApis.sSetSelection([0, 0, 0], 1, [0, 0, 0], 3),
-          tinyUi.sClickOnToolbar('toggle off bold', 'button[aria-label="Bold"]'),
+          tinyUi.sClickOnToolbar('toggle off bold', boldSelector),
           tinyApis.sAssertContent('<p><strong>a</strong> b</p>')
         ])),
         Logger.t('remove bold with trailing whitespace', GeneralSteps.sequence([
           tinyApis.sSetRawContent('<p><strong>a b</strong></p>'),
           tinyApis.sSetSelection([0, 0, 0], 0, [0, 0, 0], 2),
-          tinyUi.sClickOnToolbar('toggle off bold', 'button[aria-label="Bold"]'),
+          tinyUi.sClickOnToolbar('toggle off bold', boldSelector),
           tinyApis.sAssertContent('<p>a <strong>b</strong></p>')
         ])),
         Logger.t('unlink with leading whitespace', GeneralSteps.sequence([
           tinyApis.sSetRawContent('<p><a href="#">a b</a></p>'),
           tinyApis.sSetSelection([0, 0, 0], 1, [0, 0, 0], 3),
-          tinyUi.sClickOnToolbar('click unlink', 'button[aria-label="Remove link"]'),
+          tinyUi.sClickOnToolbar('click unlink', unlinkSelector),
           tinyApis.sAssertContent('<p><a href="#">a</a> b</p>')
         ])),
         Logger.t('unlink with trailing whitespace', GeneralSteps.sequence([
           tinyApis.sSetRawContent('<p><a href="#">a b</a></p>'),
           tinyApis.sSetSelection([0, 0, 0], 0, [0, 0, 0], 2),
-          tinyUi.sClickOnToolbar('click unlink', 'button[aria-label="Remove link"]'),
+          tinyUi.sClickOnToolbar('click unlink', unlinkSelector),
           tinyApis.sAssertContent('<p>a <a href="#">b</a></p>')
         ]))
       ], onSuccess, onFailure);
