7be7470cb16d05d70685367ecc0525b5883c8db3
Lawanya Baskarran <lawanya.baskarran@tiny.cloud>
2018-11-12 08:21:42 +1000

    TINY-2211 Advcode dialog: Added a wrapper class around codemirror

--- src/themes/silver/main/ts/ui/dialog/CustomEditor.ts
+++ src/themes/silver/main/ts/ui/dialog/CustomEditor.ts
@@ -26,7 +26,8 @@ export const renderCustomEditor = (spec: CustomEditorFoo): SimpleSpec => {
 
   return {
     dom: {
-      tag: 'div'
+      tag: 'div',
+      classes: [ 'tox-well', 'tox-well--no-padding' ]
     },
     behaviours: Behaviour.derive([
       AddEventsBehaviour.config('editor-foo-events', [
