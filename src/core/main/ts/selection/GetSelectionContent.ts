7316c9eca561e68508e3d7088ac2e061de2fc973
Lee Newson <lee.newson@tiny.cloud>
2018-11-15 11:31:23 +1000

    TINY-2325: Fixed an issue in IE 11 where getting the content would fail if the editor didn't have focus

--- src/core/main/ts/selection/GetSelectionContent.ts
+++ src/core/main/ts/selection/GetSelectionContent.ts
@@ -8,18 +8,58 @@
  * Contributing: http://www.tinymce.com/contributing
  */
 
+import { Option } from '@ephox/katamari';
 import { Element } from '@ephox/sugar';
 import EventProcessRanges from './EventProcessRanges';
 import FragmentReader from './FragmentReader';
 import MultiRange from './MultiRange';
+import SelectionBookmark from './SelectionBookmark';
 import Zwsp from '../text/Zwsp';
+import EditorFocus from '../focus/EditorFocus';
+import { Editor } from '../api/Editor';
 
-const getContent = function (editor, args) {
+const getTextContent = (editor: Editor): string => {
+  const sel = editor.selection.getSel();
+  if (editor.selection.isCollapsed()) {
+    return '';
+  } else {
+    // If the editor doesn't have focus, then on some browsers it loses the selection (eg IE). So in that case
+    // attempt to use the saved selection bookmark if it exists
+    const bookmarkOpt = !EditorFocus.hasFocus(editor) ? SelectionBookmark.getRng(editor) : Option.none();
+    return Zwsp.trim(bookmarkOpt.fold(
+      () => sel.toString ? sel.toString() : '',
+      (rng) => rng.toString()
+    ));
+  }
+};
+
+const getHtmlContent = (editor: Editor, args: any): string => {
   const rng = editor.selection.getRng(), tmpElm = editor.dom.create('body');
   const sel = editor.selection.getSel();
   let fragment;
   const ranges = EventProcessRanges.processRanges(editor, MultiRange.getRanges(sel));
 
+  if (rng.cloneContents) {
+    fragment = args.contextual ? FragmentReader.read(Element.fromDom(editor.getBody()), ranges).dom() : rng.cloneContents();
+    if (fragment) {
+      tmpElm.appendChild(fragment);
+    }
+  // We maybe dealing with an IE TextRange object here, so check the item/htmlText properties
+  } else if ((<any> rng).item !== undefined || (<any> rng).htmlText !== undefined) {
+    // IE will produce invalid markup if elements are present that
+    // it doesn't understand like custom elements or HTML5 elements.
+    // Adding a BR in front of the contents and then removing it seems to fix it though.
+    const textRng = (<any> rng);
+    tmpElm.innerHTML = '<br>' + (textRng.item ? textRng.item(0).outerHTML : textRng.htmlText);
+    tmpElm.removeChild(tmpElm.firstChild);
+  } else {
+    tmpElm.innerHTML = rng.toString();
+  }
+
+  return editor.selection.serializer.serialize(tmpElm, args);
+};
+
+const getContent = (editor: Editor, args: any): string => {
   args = args || {};
   args.get = true;
   args.format = args.format || 'html';
@@ -32,35 +72,19 @@ const getContent = function (editor, args) {
   }
 
   if (args.format === 'text') {
-    return editor.selection.isCollapsed() ? '' : Zwsp.trim(rng.text || (sel.toString ? sel.toString() : ''));
-  }
-
-  if (rng.cloneContents) {
-    fragment = args.contextual ? FragmentReader.read(Element.fromDom(editor.getBody()), ranges).dom() : rng.cloneContents();
-    if (fragment) {
-      tmpElm.appendChild(fragment);
-    }
-  } else if (rng.item !== undefined || rng.htmlText !== undefined) {
-    // IE will produce invalid markup if elements are present that
-    // it doesn't understand like custom elements or HTML5 elements.
-    // Adding a BR in front of the contents and then remoiving it seems to fix it though.
-    tmpElm.innerHTML = '<br>' + (rng.item ? rng.item(0).outerHTML : rng.htmlText);
-    tmpElm.removeChild(tmpElm.firstChild);
+    return getTextContent(editor);
   } else {
-    tmpElm.innerHTML = rng.toString();
+    args.getInner = true;
+    const content = getHtmlContent(editor, args);
+
+    if (args.format === 'tree') {
+      return content;
+    } else {
+      args.content = editor.selection.isCollapsed() ? '' : content;
+      editor.fire('GetContent', args);
+      return args.content;
+    }
   }
-
-  args.getInner = true;
-
-  const content = editor.selection.serializer.serialize(tmpElm, args);
-  if (args.format === 'tree') {
-    return content;
-  }
-
-  args.content = editor.selection.isCollapsed() ? '' : content;
-  editor.fire('GetContent', args);
-
-  return args.content;
 };
 
 export default {
