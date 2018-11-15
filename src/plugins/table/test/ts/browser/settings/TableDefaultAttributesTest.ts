4dd5c84e0f6da510761ab9d9d4f1186de0bf984c
James Johnson <james.johnson@ephox.com>
2018-11-15 10:18:03 +1000

    AP-283: Fixed selectors for table menus

--- src/plugins/table/test/ts/browser/settings/TableDefaultAttributesTest.ts
+++ src/plugins/table/test/ts/browser/settings/TableDefaultAttributesTest.ts
@@ -1,4 +1,4 @@
-import { GeneralSteps, Logger, Pipeline, ApproxStructure } from '@ephox/agar';
+import { GeneralSteps, Logger, Pipeline, ApproxStructure, Waiter } from '@ephox/agar';
 import { UnitTest } from '@ephox/bedrock';
 import { TinyApis, TinyLoader, TinyUi } from '@ephox/mcagar';
 import TablePlugin from 'tinymce/plugins/table/Plugin';
@@ -14,12 +14,57 @@ UnitTest.asynctest('browser.tinymce.plugins.table.TableDefaultAttributesTest', (
     const tinyApis = TinyApis(editor);
     const tinyUi = TinyUi(editor);
 
+    const tableBody2By2 = (s, str, arr) => s.element('tbody', {
+      children: [
+        s.element('tr', {
+          children: [
+            s.element('td', {
+              styles: {
+                width: str.is('50%')
+              },
+              children: [
+                s.element('br', {})
+              ]
+            }),
+            s.element('td', {
+              styles: {
+                width: str.is('50%')
+              },
+              children: [
+                s.element('br', {})
+              ]
+            })
+          ]
+        }),
+        s.element('tr', {
+          children: [
+            s.element('td', {
+              styles: {
+                width: str.is('50%')
+              },
+              children: [
+                s.element('br', {})
+              ]
+            }),
+            s.element('td', {
+              styles: {
+                width: str.is('50%')
+              },
+              children: [
+                s.element('br', {})
+              ]
+            })
+          ]
+        })
+      ]
+    });
+
     Pipeline.async({}, [
       Logger.t('no attributes without setting', GeneralSteps.sequence([
         tinyApis.sFocus,
         tinyUi.sClickOnMenu('click table menu', 'span:contains("Table")'),
-        tinyUi.sClickOnUi('click table menu', 'div[role="menu"] span:contains("Table")'),
-        tinyUi.sClickOnUi('click table grid', 'a#mcegrid11'),
+        Waiter.sTryUntil('click table menu', tinyUi.sClickOnUi('click table menu', 'div.tox-menu div.tox-collection__item span.tox-collection__item-label:contains("Table")'), 10, 1000),
+        Waiter.sTryUntil('click table grid', tinyUi.sClickOnUi('click table grid', 'div.tox-insert-table-picker div[role="button"]:nth(11)'), 10, 1000), // button for 2x2 table
         TableTestUtils.sAssertTableStructure(editor, ApproxStructure.build((s, str, arr) => {
           return s.element('table', {
             styles: {
@@ -30,22 +75,7 @@ UnitTest.asynctest('browser.tinymce.plugins.table.TableDefaultAttributesTest', (
               border: str.is('1')
             },
             children: [
-              s.element('tbody', {
-                children: [
-                  s.element('tr', {
-                    children: [
-                      s.element('td', {
-                        styles: {
-                          width: str.is('100%')
-                        },
-                        children: [
-                          s.element('br', {})
-                        ]
-                      })
-                    ]
-                  })
-                ]
-              })
+              tableBody2By2(s, str, arr)
             ]
           });
         })),
@@ -56,8 +86,8 @@ UnitTest.asynctest('browser.tinymce.plugins.table.TableDefaultAttributesTest', (
         tinyApis.sFocus,
         tinyApis.sSetSetting('table_default_attributes', { title: 'x' }),
         tinyUi.sClickOnMenu('click table menu', 'span:contains("Table")'),
-        tinyUi.sClickOnUi('click table menu', 'div[role="menu"] span:contains("Table")'),
-        tinyUi.sClickOnUi('click table grid', 'a#mcegrid11'),
+        Waiter.sTryUntil('click table menu', tinyUi.sClickOnUi('click table menu', 'div.tox-menu div.tox-collection__item span.tox-collection__item-label:contains("Table")'), 10, 1000),
+        Waiter.sTryUntil('click table grid', tinyUi.sClickOnUi('click table grid', 'div.tox-insert-table-picker div[role="button"]:nth(11)'), 10, 1000), // button for 2x2 table
         TableTestUtils.sAssertTableStructure(editor, ApproxStructure.build((s, str, arr) => {
           return s.element('table', {
             styles: {
@@ -69,22 +99,7 @@ UnitTest.asynctest('browser.tinymce.plugins.table.TableDefaultAttributesTest', (
               title: str.is('x')
             },
             children: [
-              s.element('tbody', {
-                children: [
-                  s.element('tr', {
-                    children: [
-                      s.element('td', {
-                        styles: {
-                          width: str.is('100%')
-                        },
-                        children: [
-                          s.element('br', {})
-                        ]
-                      })
-                    ]
-                  })
-                ]
-              })
+              tableBody2By2(s, str, arr)
             ]
           });
         })),
