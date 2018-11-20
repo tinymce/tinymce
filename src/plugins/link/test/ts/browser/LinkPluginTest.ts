99eefd75f52ca6b7ed0a5d4a31a512b2a02a188c
Lee Newson <lee.newson@tiny.cloud>
2018-11-20 17:15:45 +1000

    TINY-2683: Updated the LinkPluginTest to work with 5.0

--- src/plugins/link/test/ts/browser/LinkPluginTest.ts
+++ src/plugins/link/test/ts/browser/LinkPluginTest.ts
@@ -1,211 +1,281 @@
-import { Pipeline, Step } from '@ephox/agar';
+import { ApproxStructure, Assertions, Chain, GeneralSteps, Log, Pipeline, Step, Waiter } from '@ephox/agar';
 import { UnitTest } from '@ephox/bedrock';
-import { Arr } from '@ephox/katamari';
-import { LegacyUnit, TinyLoader } from '@ephox/mcagar';
+import { Arr, Merger } from '@ephox/katamari';
+import { TinyLoader, TinyApis, TinyUi } from '@ephox/mcagar';
 
+import { Editor } from 'tinymce/core/api/Editor';
 import LinkPlugin from 'tinymce/plugins/link/Plugin';
 import Theme from 'tinymce/themes/silver/Theme';
+import { TestLinkUi } from 'src/plugins/link/test/ts/module/TestLinkUi';
 
-UnitTest.asynctest('browser.tinymce.plugins.link.LinkPluginTest', function () {
-  const success = arguments[arguments.length - 2];
-  const failure = arguments[arguments.length - 1];
-  const suite = LegacyUnit.createSuite();
-
+UnitTest.asynctest('browser.tinymce.plugins.link.LinkPluginTest', (success, failure) => {
   Theme();
   LinkPlugin();
 
   const nonRelativeRegex = /^\w+:/i;
 
-  const _cleanHtml = function (html) {
-    html = html.toLowerCase().replace(/[\r\n]+/gi, '');
-    html = html.replace(/ (sizcache[0-9]+|sizcache|nodeindex|sizset[0-9]+|sizset|data\-mce\-expando|data\-mce\-selected)="[^"]*"/gi, '');
-    html = html.replace(/<span[^>]+data-mce-bogus[^>]+>[\u200B\uFEFF]+<\/span>|<div[^>]+data-mce-bogus[^>]+><\/div>/gi, '');
-    html = html.replace(/ style="([^"]+)"/gi, function (val1, val2) {
-      val2 = val2.replace(/;$/, '');
-      return ' style="' + val2.replace(/\:([^ ])/g, ': $1') + ';"';
-    });
-
-    return html;
+  const emptyData = {
+    url: {
+      value: ''
+    },
+    text: '',
+    title: '',
+    target: ''
   };
 
-  const cleanHtml = function (html) {
-    return _cleanHtml(html).replace(/<p>(&nbsp;|<br[^>]+>)<\/p>$/, '');
+  const getFrontmostDialog = (editor: Editor) => {
+    const windows = editor.windowManager.getWindows();
+    return windows[ windows.length - 1 ];
   };
 
-  const getFrontmostWindow = function (editor) {
-    return editor.windowManager.windows[editor.windowManager.windows.length - 1];
+  const sFillAndSubmitDialog = (editor: Editor, data) => {
+    return GeneralSteps.sequence([
+      Step.sync(() => {
+        getFrontmostDialog(editor).setData(data);
+      }),
+      TestLinkUi.sClickSave
+    ]);
   };
 
-  const fillAndSubmitWindowForm = function (editor, data) {
-    const win = getFrontmostWindow(editor);
-
-    win.fromJSON(data);
-    win.find('form')[0].submit();
-    win.close();
-  };
-
-  const sTeardown = function (editor) {
-    return Step.sync(function () {
+  const sTeardown = (editor: Editor) => {
+    return Step.sync(() => {
       delete editor.settings.file_browser_callback;
       delete editor.settings.link_list;
       delete editor.settings.link_class_list;
       delete editor.settings.link_target_list;
       delete editor.settings.rel_list;
 
-      const win = getFrontmostWindow(editor);
-
+      const win = getFrontmostDialog(editor);
       if (win) {
         win.close();
       }
     });
   };
 
-  const appendTeardown = function (editor, steps) {
-    return Arr.bind(steps, function (step) {
+  const appendTeardown = (editor, steps) => {
+    return Arr.bind(steps, (step) => {
       return [step, sTeardown(editor)];
     });
   };
 
-  suite.test('Default link dialog on empty editor', function (editor) {
-    editor.setContent('');
-    editor.execCommand('mceLink', true);
+  TinyLoader.setup((editor, onSuccess, onFailure) => {
+    const tinyApis = TinyApis(editor);
+    const tinyUi = TinyUi(editor);
 
-    LegacyUnit.deepEqual(getFrontmostWindow(editor).toJSON(), {
-      href: '',
-      target: '',
-      text: '',
-      title: ''
-    });
+    const sWaitForDialog = () => {
+      return tinyUi.sWaitForPopup('Waiting for link dialog to show', '[role="dialog"]');
+    };
 
-    fillAndSubmitWindowForm(editor, {
-      href: 'href',
-      target: '_blank',
-      text: 'text',
-      title: 'title'
-    });
+    const sAssertContentStructure = (structure) => {
+      return Waiter.sTryUntil(
+        'Waiting for content to have expected structure',
+        tinyApis.sAssertContentStructure(ApproxStructure.build((s, str) => {
+          return s.element('body', {
+            children: structure(s, str)
+          });
+        })), 100, 1000);
+    };
 
-    LegacyUnit.equal(
-      cleanHtml(editor.getContent()),
-      '<p><a title="title" href="href" target="_blank" rel="noopener">text</a></p>'
-    );
-  });
+    const sAssertDialogData = (data) => {
+      return Chain.asStep({}, [
+        Chain.mapper(() => {
+          const data = getFrontmostDialog(editor).getData();
+          delete data.url.meta;
+          return data;
+        }),
+        Assertions.cAssertEq('Link dialog data should match', data)
+      ]);
+    };
 
-  suite.test('Default link dialog on text selection', function (editor) {
-    editor.setContent('<p>abc</p>');
-    LegacyUnit.setSelection(editor, 'p', 1, 'p', 2);
-    editor.execCommand('mceLink', true);
-
-    LegacyUnit.deepEqual(getFrontmostWindow(editor).toJSON(), {
-      href: '',
-      target: '',
-      text: 'b',
-      title: ''
-    });
-
-    fillAndSubmitWindowForm(editor, {
-      href: 'href',
-      target: '_blank',
-      title: 'title'
-    });
-
-    LegacyUnit.equal(
-      cleanHtml(editor.getContent()),
-      '<p>a<a title="title" href="href" target="_blank" rel="noopener">b</a>c</p>'
-    );
-  });
-
-  suite.test('Default link dialog on non pure text selection', function (editor) {
-    editor.setContent('<p>a</p><p>bc</p>');
-    LegacyUnit.setSelection(editor, 'p:nth-child(1)', 0, 'p:nth-child(2)', 2);
-    editor.execCommand('mceLink', true);
-
-    LegacyUnit.deepEqual(getFrontmostWindow(editor).toJSON(), {
-      href: '',
-      target: '',
-      title: ''
-    });
-
-    fillAndSubmitWindowForm(editor, {
-      href: 'href',
-      target: '_blank',
-      title: 'title'
-    });
-
-    LegacyUnit.equal(
-      cleanHtml(editor.getContent()),
-      '<p><a title="title" href="href" target="_blank" rel="noopener">a</a></p>' +
-      '<p><a title="title" href="href" target="_blank" rel="noopener">bc</a></p>'
-    );
-  });
-
-  suite.test('All lists link dialog on empty editor', function (editor) {
-    editor.settings.link_list = [
-      { title: 'link1', value: 'link1' },
-      { title: 'link2', value: 'link2' }
+    const steps = [
+      Log.stepsAsStep('TBA', 'Default link dialog on empty editor', [
+        tinyApis.sSetContent(''),
+        tinyApis.sExecCommand('mceLink'),
+        sWaitForDialog(),
+        sAssertDialogData(emptyData),
+        sFillAndSubmitDialog(editor, {
+          url: { value: 'href' },
+          target: '_blank',
+          text: 'text',
+          title: 'title'
+        }),
+        sAssertContentStructure((s, str) => {
+          return [
+            s.element('p', {
+              children: [
+                s.element('a', {
+                  attrs: {
+                    title: str.is('title'),
+                    href: str.is('href'),
+                    target: str.is('_blank'),
+                    rel: str.is('noopener')
+                  },
+                  children: [
+                    s.text(str.is('text'))
+                  ]
+                })
+              ]
+            })
+          ];
+        })
+      ]),
+      Log.stepsAsStep('TBA', 'Default link dialog on text selection', [
+        tinyApis.sSetContent('<p>abc</p>'),
+        tinyApis.sSetSelection([0, 0], 1, [0, 0], 2),
+        tinyApis.sExecCommand('mceLink'),
+        sWaitForDialog(),
+        sAssertDialogData(Merger.merge(emptyData, { text: 'b' })),
+        sFillAndSubmitDialog(editor, {
+          url: { value: 'href' },
+          target: '_blank',
+          title: 'title'
+        }),
+        sAssertContentStructure((s, str) => {
+          return [
+            s.element('p', {
+              children: [
+                s.text(str.is('a')),
+                s.element('a', {
+                  attrs: {
+                    title: str.is('title'),
+                    href: str.is('href'),
+                    target: str.is('_blank'),
+                    rel: str.is('noopener')
+                  },
+                  children: [
+                    s.text(str.is('b'))
+                  ]
+                }),
+                s.text(str.is('c'))
+              ]
+            })
+          ];
+        })
+      ]),
+      Log.stepsAsStep('TBA', 'Default link dialog on non pure text selection', [
+        tinyApis.sSetContent('<p>a</p><p>bc</p>'),
+        tinyApis.sSetSelection([0, 0], 0, [1, 0], 2),
+        tinyApis.sExecCommand('mceLink'),
+        sWaitForDialog(),
+        sAssertDialogData({
+          url: {
+            value: ''
+          },
+          title: '',
+          target: ''
+        }),
+        sFillAndSubmitDialog(editor, {
+          url: { value: 'href' },
+          target: '_blank',
+          title: 'title'
+        }),
+        sAssertContentStructure((s, str) => {
+          return [
+            s.element('p', {
+              children: [
+                s.element('a', {
+                  attrs: {
+                    title: str.is('title'),
+                    href: str.is('href'),
+                    target: str.is('_blank'),
+                    rel: str.is('noopener')
+                  },
+                  children: [
+                    s.text(str.is('a'))
+                  ]
+                })
+              ]
+            }),
+            s.element('p', {
+              children: [
+                s.element('a', {
+                  attrs: {
+                    title: str.is('title'),
+                    href: str.is('href'),
+                    target: str.is('_blank'),
+                    rel: str.is('noopener')
+                  },
+                  children: [
+                    s.text(str.is('bc'))
+                  ]
+                })
+              ]
+            })
+          ];
+        })
+      ]),
+      Log.stepsAsStep('TBA', 'All lists link dialog on empty editor', [
+        tinyApis.sSetSetting('link_list', [
+          { title: 'link1', value: 'link1' },
+          { title: 'link2', value: 'link2' }
+        ]),
+        tinyApis.sSetSetting('link_class_list', [
+          { title: 'class1', value: 'class1' },
+          { title: 'class2', value: 'class2' }
+        ]),
+        tinyApis.sSetSetting('target_list', [
+          { title: 'target1', value: 'target1' },
+          { title: 'target2', value: 'target2' }
+        ]),
+        tinyApis.sSetSetting('rel_list', [
+          { title: 'rel1', value: 'rel1' },
+          { title: 'rel2', value: 'rel2' }
+        ]),
+        tinyApis.sSetContent(''),
+        tinyApis.sExecCommand('mceLink'),
+        sWaitForDialog(),
+        sAssertDialogData(Merger.merge(emptyData, {
+          classz: 'class1',
+          target: 'target1',
+          rel: 'rel1',
+          link: 'link1'
+        })),
+        sFillAndSubmitDialog(editor, {
+          url: { value: 'href' },
+          title: 'title',
+          text: 'text'
+        }),
+        sAssertContentStructure((s, str) => {
+          return [
+            s.element('p', {
+              children: [
+                s.element('a', {
+                  attrs: {
+                    class: str.is('class1'),
+                    title: str.is('title'),
+                    href: str.is('href'),
+                    target: str.is('target1'),
+                    rel: str.is('rel1')
+                  },
+                  children: [
+                    s.text(str.is('text'))
+                  ]
+                }),
+              ]
+            })
+          ];
+        })
+      ]),
+      // Since there's no capability to use the confirm dialog with unit tests, simply test the regex we're using,
+      Log.stepsAsStep('TBA', 'Test new regex for non relative link setting ftp', [
+        Assertions.sAssertEq('', true, nonRelativeRegex.test('ftp://testftp.com'))
+      ]),
+      Log.stepsAsStep('TBA', 'Test new regex for non relative link setting http', [
+        Assertions.sAssertEq('', true, nonRelativeRegex.test('http://testhttp.com'))
+      ]),
+      Log.stepsAsStep('TBA', 'Test new regex for non relative link setting relative', [
+        Assertions.sAssertEq('', false, nonRelativeRegex.test('testhttp.com'))
+      ]),
+      Log.stepsAsStep('TBA', 'Test new regex for non relative link setting relative base', [
+        Assertions.sAssertEq('', false, nonRelativeRegex.test('/testjpg.jpg'))
+      ])
     ];
 
-    editor.settings.link_class_list = [
-      { title: 'class1', value: 'class1' },
-      { title: 'class2', value: 'class2' }
-    ];
-
-    editor.settings.target_list = [
-      { title: 'target1', value: 'target1' },
-      { title: 'target2', value: 'target2' }
-    ];
-
-    editor.settings.rel_list = [
-      { title: 'rel1', value: 'rel1' },
-      { title: 'rel2', value: 'rel2' }
-    ];
-
-    editor.setContent('');
-    editor.execCommand('mceLink', true);
-
-    LegacyUnit.deepEqual(getFrontmostWindow(editor).toJSON(), {
-      class: 'class1',
-      href: '',
-      rel: 'rel1',
-      target: 'target1',
-      text: '',
-      title: ''
-    });
-
-    fillAndSubmitWindowForm(editor, {
-      href: 'href',
-      text: 'text',
-      title: 'title'
-    });
-
-    LegacyUnit.equal(
-      cleanHtml(editor.getContent()),
-      '<p><a class="class1" title="title" href="href" target="target1" rel="rel1">text</a></p>'
-    );
-  });
-
-  // Since there's no capability to use the confirm dialog with unit tests, simply test the regex we're using
-  suite.test('Test new regex for non relative link setting ftp', function () {
-    LegacyUnit.equal(nonRelativeRegex.test('ftp://testftp.com'), true);
-  });
-
-  suite.test('Test new regex for non relative link setting http', function () {
-    LegacyUnit.equal(nonRelativeRegex.test('http://testhttp.com'), true);
-  });
-
-  suite.test('Test new regex for non relative link setting relative', function () {
-    LegacyUnit.equal(nonRelativeRegex.test('testhttp.com'), false);
-  });
-
-  suite.test('Test new regex for non relative link setting relative base', function () {
-    LegacyUnit.equal(nonRelativeRegex.test('/testjpg.jpg'), false);
-  });
-
-  TinyLoader.setup(function (editor, onSuccess, onFailure) {
-    Pipeline.async({}, appendTeardown(editor, suite.toSteps(editor)), onSuccess, onFailure);
+    Pipeline.async({}, [tinyApis.sFocus].concat(appendTeardown(editor, steps)), onSuccess, onFailure);
   }, {
+    theme: 'silver',
     plugins: 'link',
-    add_unload_trigger: false,
-    indent: false,
+    toolbar: 'link',
     skin_url: '/project/js/tinymce/skins/oxide'
   }, success, failure);
 });
