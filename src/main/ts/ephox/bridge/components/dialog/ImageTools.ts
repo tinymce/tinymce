a8260d85579f43e294b813bd96b6a5825831136e
Andrew Herron <thespyder@programmer.net>
2018-12-03 15:42:32 +1000

    TINY-2603: ImageTools was missing a property on the interface

--- src/main/ts/ephox/bridge/components/dialog/ImageTools.ts
+++ src/main/ts/ephox/bridge/components/dialog/ImageTools.ts
@@ -1,13 +1,21 @@
 import { ValueSchema, FieldSchema, FieldProcessorAdt } from '@ephox/boulder';
 import { Result } from '@ephox/katamari';
 import { FormComponent, FormComponentApi, formComponentFields } from './FormComponent';
+import { Blob } from '@ephox/dom-globals';
+
+export interface ImageToolsState {
+  blob: Blob;
+  url: string;
+};
 
 export interface ImageToolsApi extends FormComponentApi {
   type: 'imagetools';
+  currentState: ImageToolsState;
 }
 
 export interface ImageTools extends FormComponent {
   type: 'imagetools';
+  currentState: ImageToolsState;
 }
 
 export const imageToolsFields: FieldProcessorAdt[] = formComponentFields.concat([
