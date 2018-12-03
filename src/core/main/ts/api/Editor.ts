ed28802aa3de0f1a4d421096b3e52aecc318497f
Andrew Herron <thespyder@programmer.net>
2018-12-03 14:45:19 +1000

    TINY-2603: editor.ui is new in v5, I think these properties were an idea we dismissed

--- src/core/main/ts/api/Editor.ts
+++ src/core/main/ts/api/Editor.ts
@@ -7,7 +7,6 @@
 
 import { Registry } from '@ephox/bridge';
 import { Document, HTMLElement, Window } from '@ephox/dom-globals';
-import { Fun } from '@ephox/katamari';
 import { Annotator } from 'tinymce/core/api/Annotator';
 import { Selection } from 'tinymce/core/api/dom/Selection';
 import Schema from 'tinymce/core/api/html/Schema';
@@ -186,17 +185,6 @@ export interface Editor {
 
 export interface Ui {
   registry: Registry.Registry;
-  // WIP - TODO
-  dialog: {
-    open: () => void,    // windowManager.open
-    confirm: () => void, // windowManager.confirm
-    alert: () => void    // windowManager.alert
-  };
-  notification: {
-    open: () => void,   // notificationManager.open
-    close: () => void,  // notificationManager.open
-    getNotifications: () => void // notificationManager.getNotifications
-  };
 }
 
 export interface AddButtonSettings {
@@ -379,17 +367,7 @@ export const Editor = function (id, settings, editorManager) {
    * @type Object
    */
   self.ui = {
-    registry,
-    dialog: {
-      open: Fun.noop,    // windowManager.open
-      confirm: Fun.noop, // windowManager.confirm
-      alert: Fun.noop    // windowManager.alert
-    },
-    notification: {
-      open: Fun.noop,   // notificationManager.open
-      close: Fun.noop,  // notificationManager.open
-      getNotifications: Fun.noop // notificationManager.getNotifications
-    }
+    registry
   };
 
   // Call setup
