feb4ef344e08a70c81525fb8628842eb9657499f
Spocke <spocke@moxiecode.com>
2018-12-04 09:37:35 +0100

    added Options.lift to replace liftN

--- src/main/ts/ephox/katamari/api/Options.ts
+++ src/main/ts/ephox/katamari/api/Options.ts
@@ -1,7 +1,7 @@
 import { Option } from './Option';
 
 /** cat :: [Option a] -> [a] */
-export const cat = function <A> (arr: Option<A>[]) {
+const cat = function <A> (arr: Option<A>[]) {
   const r: A[] = [];
   const push = function (x: A) {
     r.push(x);
@@ -13,7 +13,7 @@ export const cat = function <A> (arr: Option<A>[]) {
 };
 
 /** findMap :: ([a], (a, Int -> Option b)) -> Option b */
-export const findMap = function <A, B> (arr: A[], f: (a: A, index: number) => Option<B>) {
+const findMap = function <A, B> (arr: A[], f: (a: A, index: number) => Option<B>) {
   for (let i = 0; i < arr.length; i++) {
     const r = f(arr[i], i);
     if (r.isSome()) {
@@ -27,7 +27,7 @@ export const findMap = function <A, B> (arr: A[], f: (a: A, index: number) => Op
  * if all elements in arr are 'some', their inner values are passed as arguments to f
  * f must have arity arr.length
 */
-export const liftN = function <B> (arr: Option<any>[], f: (...args: any[]) => B) {
+const liftN = function <B> (arr: Option<any>[], f: (...args: any[]) => B) {
   const r: any[] = [];
   for (let i = 0; i < arr.length; i++) {
     const x = arr[i];
@@ -38,4 +38,20 @@ export const liftN = function <B> (arr: Option<any>[], f: (...args: any[]) => B)
     }
   }
   return Option.some(<B>f.apply(null, r));
+};
+
+function lift<A, B, C>(a: Option<A>, b: Option<B>, f: (a: A, b: B) => C): Option<C>;
+function lift<A, B, C, D>(a: Option<A>, b: Option<B>, c: Option<C>, f: (a: A, b: B, c: C) => D): Option<D>;
+function lift<A, B, C, D, E>(a: Option<A>, b: Option<B>, c: Option<C>, d: Option<D>, f: (a: A, b: B, c: C, d: D) => E): Option<E>;
+function lift<A, B, C, D, E, F>(a: Option<A>, b: Option<B>, c: Option<C>, d: Option<D>, e: Option<E>, f: (a: A, b: B, c: C, d: D, e: E) => F): Option<F>;
+function lift(...args) {
+  const f = args.pop();
+  return liftN(args, f);
+};
+
+export {
+  cat,
+  findMap,
+  liftN,
+  lift
 };
