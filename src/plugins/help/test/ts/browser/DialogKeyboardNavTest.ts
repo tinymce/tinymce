9f4a021af97b31443e70f4e63a493802dfc59111
Joshua Haines <josh.j.haines@gmail.com>
2018-12-18 09:55:55 +1000

    5.x: remove incorrect import

--- src/plugins/help/test/ts/browser/DialogKeyboardNavTest.ts
+++ src/plugins/help/test/ts/browser/DialogKeyboardNavTest.ts
@@ -5,8 +5,6 @@ import { Element } from '@ephox/sugar';
 import { document } from '@ephox/dom-globals';
 import Theme from '../../../../../themes/silver/main/ts/Theme';
 import HelpPlugin from 'tinymce/plugins/help/Plugin';
-// TODO immediately
-import { addLogEntry } from '@ephox/agar/lib/main/ts/ephox/agar/api/TestLogs';
 
 UnitTest.asynctest('browser.tinymce.plugins.help.DialogKeyboardNavTest', (success, failure) => {
 
@@ -33,10 +31,6 @@ UnitTest.asynctest('browser.tinymce.plugins.help.DialogKeyboardNavTest', (succes
     };
 
     Pipeline.async({}, [
-      Step.raw((value, next, die, logs) => {
-        const newLogs = addLogEntry(logs, 'Current focus: ' + (document.activeElement ? document.activeElement.nodeName : 'none'));
-        next(value, newLogs);
-      }),
       Log.stepsAsStep('TBA', 'Help: Open dialog', [
         tinyApis.sFocus,
         tinyApis.sExecCommand('mceHelp')
