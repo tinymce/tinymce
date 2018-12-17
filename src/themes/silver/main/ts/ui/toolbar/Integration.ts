8e6bc85d1f5f3414492a850463dcc16074004b6f
Simon Fjeldså <simon.fjeldsa@ephox.com>
2018-12-17 10:59:26 +0100

    PEPE-20: adding permanent pen to default toolbar

--- src/themes/silver/main/ts/ui/toolbar/Integration.ts
+++ src/themes/silver/main/ts/ui/toolbar/Integration.ts
@@ -30,7 +30,7 @@ export const handleError = (error) => {
   console.error(ValueSchema.formatError(error));
 };
 
-const defaultToolbar = 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent';
+const defaultToolbar = 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | permanentpen';
 
 const renderFromBridge = <BI, BO>(bridgeBuilder: (i: BI) => Result<BO, ValueSchema.SchemaError<any>>, render: (o: BO, extras) => AlloySpec) => {
   return (spec, extras) => {
