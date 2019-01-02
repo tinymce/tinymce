ade447bc8e226e655954eaf1dd474aaa80ea842b
James Johnson <james.johnson@ephox.com>
2019-01-02 16:04:43 +1000

    TINY-2991: Added replacement patterns to demo

--- src/plugins/textpattern/demo/ts/demo/Demo.ts
+++ src/plugins/textpattern/demo/ts/demo/Demo.ts
@@ -5,7 +5,22 @@ tinymce.init({
   plugins: 'textpattern code',
   skin_url: '../../../../../js/tinymce/skins/ui/oxide',
   toolbar: 'code',
-  height: 600
+  height: 600,
+  textpattern_patterns: [
+  { start: '*', end: '*', format: 'italic' },
+  { start: '**', end: '**', format: 'bold' },
+  { start: '#', format: 'h1' },
+  { start: '##', format: 'h2' },
+  { start: '###', format: 'h3' },
+  { start: '####', format: 'h4' },
+  { start: '#####', format: 'h5' },
+  { start: '######', format: 'h6' },
+  { start: '1. ', cmd: 'InsertOrderedList' },
+  { start: '* ', cmd: 'InsertUnorderedList' },
+  { start: '- ', cmd: 'InsertUnorderedList' },
+  { start: 'brb', replacement: 'be right back'},
+  { start: 'irl', replacement: 'in real life'}
+  ]
 });
 
 export {};
