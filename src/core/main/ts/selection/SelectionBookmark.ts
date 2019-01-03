bf81548a6e5eac1d2bde4701ba46bd6d47a8de70
Michal Turecki <mturecki@speedwellsoftware.com>
2019-01-03 12:54:48 +0000

    TINY-2194 Exception in Firefox if a file input element has focus when tinymce is initialized

--- src/core/main/ts/selection/SelectionBookmark.ts
+++ src/core/main/ts/selection/SelectionBookmark.ts
@@ -9,6 +9,7 @@ import { Fun, Option } from '@ephox/katamari';
 import { PlatformDetection } from '@ephox/sand';
 import { Compare, Element, Node, Text, Traverse, Selection } from '@ephox/sugar';
 import { document } from '@ephox/dom-globals';
+import Tools from '../api/util/Tools';
 
 const browser = PlatformDetection.detect().browser;
 
@@ -34,7 +35,7 @@ const normalizeRng = function (rng) {
 };
 
 const isOrContains = function (root, elm) {
-  return Compare.contains(root, elm) || Compare.eq(root, elm);
+  return !(elm && Tools.isRestricted(elm.dom())) && (Compare.contains(root, elm) || Compare.eq(root, elm));
 };
 
 const isRngInRoot = function (root) {
