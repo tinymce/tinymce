b2eafc9f106edeabf903fa95b29122ac59f7f306
Lee Newson <lee.newson@tiny.cloud>
2019-01-03 12:30:48 +1000

    TINY-2807: Added comment about removing the old browser quirk fallback logic

--- src/plugins/autoresize/main/ts/core/Resize.ts
+++ src/plugins/autoresize/main/ts/core/Resize.ts
@@ -82,6 +82,8 @@ const resize = (editor: Editor, oldSize: Cell<number>) => {
   contentHeight = body.offsetHeight + marginTop + marginBottom;
 
   // Make sure we have a valid height
+  // Note: Previously we had to do some fallbacks here for IE/Webkit, as the height calculation above didn't work.
+  //       However using the latest supported browsers (IE 11 & Safari 11), the fallbacks were no longer needed and were removed.
   if (contentHeight < 0) {
     contentHeight = 0;
   }
