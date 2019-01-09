c603d21c02e9dc4ee082862ebd92abb7a3b69221
Lee Newson <lee.newson@tiny.cloud>
2019-01-09 12:04:58 +1000

    TINY-2786: Re-enabled the browser.tinymce.core.dom.ControlSelectionTest test

--- src/core/test/ts/browser/dom/ControlSelectionTest.ts
+++ src/core/test/ts/browser/dom/ControlSelectionTest.ts
@@ -5,10 +5,6 @@ import Theme from 'tinymce/themes/silver/Theme';
 import { UnitTest } from '@ephox/bedrock';
 
 UnitTest.asynctest('browser.tinymce.core.dom.ControlSelectionTest', function (success, failure) {
-  // TODO FIXME DISABLED-TEST TINY-2786
-  // Disable reason: Context menu is not popuplated, works in normal tinymce
-  success();
-  return;
 
   Theme();
 
@@ -19,7 +15,7 @@ UnitTest.asynctest('browser.tinymce.core.dom.ControlSelectionTest', function (su
       const clientX = (rect.left + rect.width / 2), clientY = (rect.top + rect.height / 2);
       editor.fire('mousedown', { target: element, clientX, clientY, button: 2 });
       editor.fire('mouseup', { target: element, clientX, clientY, button: 2 });
-      editor.fire('contextmenu', { target: element, clientX, clientY });
+      editor.fire('contextmenu', { target: element, clientX, clientY, button: 2 });
     });
   };
 
@@ -28,15 +24,10 @@ UnitTest.asynctest('browser.tinymce.core.dom.ControlSelectionTest', function (su
 
     Pipeline.async({}, [
       Logger.t('Select image by context menu clicking on it', GeneralSteps.sequence([
-        Step.wait(1000),
         Step.label('Focus editor', tinyApis.sFocus),
-        Step.wait(1000),
         Step.label('Set editor content to a paragraph with a image within', tinyApis.sSetContent('<p><img src="http://www.google.com/google.jpg" width="100" height="100"></p>')),
-        Step.wait(3000),
         Step.label('Context menu click on the image', sContextMenuClickInMiddleOf(editor, [0, 0])),
-        Step.wait(1000),
-        Step.label('Check that the image is selected', tinyApis.sAssertSelection([0], 0, [0], 1)),
-        Step.wait(1000)
+        Step.label('Check that the image is selected', tinyApis.sAssertSelection([0], 0, [0], 1))
       ]))
     ], onSuccess, onFailure);
   }, {
