94baec277a189cf744f7151df0cadb569855b5d4
James Johnson <james.johnson@ephox.com>
2019-01-09 11:31:01 +1000

    TINY-2991: Fixed code indentation

--- src/plugins/textpattern/demo/ts/demo/Demo.ts
+++ src/plugins/textpattern/demo/ts/demo/Demo.ts
@@ -7,21 +7,21 @@ tinymce.init({
   toolbar: 'code',
   height: 600,
   textpattern_patterns: [
-  { start: '*', end: '*', format: 'italic' },
-  { start: '**', end: '**', format: 'bold' },
-  { start: '#', format: 'h1' },
-  { start: '##', format: 'h2' },
-  { start: '###', format: 'h3' },
-  { start: '####', format: 'h4' },
-  { start: '#####', format: 'h5' },
-  { start: '######', format: 'h6' },
-  { start: '1. ', cmd: 'InsertOrderedList' },
-  { start: '* ', cmd: 'InsertUnorderedList' },
-  { start: '- ', cmd: 'InsertUnorderedList' },
-  { start: 'brb', replacement: 'be right back'},
-  { start: 'irl', replacement: 'in real life'},
-  { start: 'heading', replacement: '<h1>My heading</h1>' }
+    { start: '*', end: '*', format: 'italic' },
+    { start: '**', end: '**', format: 'bold' },
+    { start: '#', format: 'h1' },
+    { start: '##', format: 'h2' },
+    { start: '###', format: 'h3' },
+    { start: '####', format: 'h4' },
+    { start: '#####', format: 'h5' },
+    { start: '######', format: 'h6' },
+    { start: '1. ', cmd: 'InsertOrderedList' },
+    { start: '* ', cmd: 'InsertUnorderedList' },
+    { start: '- ', cmd: 'InsertUnorderedList' },
+    { start: 'brb', replacement: 'be right back' },
+    { start: 'irl', replacement: 'in real life' },
+    { start: 'heading', replacement: '<h1>My heading</h1>' }
   ]
 });
 
-export {};
+export { };
