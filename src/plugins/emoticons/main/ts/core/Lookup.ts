712df7398685160fd748c2774c228615384d25ca
Lee Newson <lee.newson@tiny.cloud>
2019-01-10 14:15:41 +1000

    TINY-3084: Reworked where lowercase conversions were done to be better for performance

--- src/plugins/emoticons/main/ts/core/Lookup.ts
+++ src/plugins/emoticons/main/ts/core/Lookup.ts
@@ -9,16 +9,17 @@ import { Arr, Fun, Strings, Option } from '@ephox/katamari';
 
 import { EmojiEntry } from './EmojiDatabase';
 
-const emojiMatches = (emoji: EmojiEntry, pattern: string): boolean => {
-  return Strings.contains(emoji.title.toLowerCase(), pattern.toLowerCase()) || Arr.exists(emoji.keywords, (k) => Strings.contains(k.toLowerCase(), pattern.toLowerCase()));
+const emojiMatches = (emoji: EmojiEntry, lowerCasePattern: string): boolean => {
+  return Strings.contains(emoji.title.toLowerCase(), lowerCasePattern) || Arr.exists(emoji.keywords, (k) => Strings.contains(k.toLowerCase(), lowerCasePattern));
 };
 
 const emojisFrom = (list: EmojiEntry[], pattern: string, maxResults: Option<number>): Array<{value: string, icon: string, text: string }> => {
   const matches = [];
+  const lowerCasePattern = pattern.toLowerCase();
   const reachedLimit = maxResults.fold(() => Fun.never, (max) => (size) => size >= max);
   for (let i = 0; i < list.length; i++) {
     // TODO: more intelligent search by showing title matches at the top, keyword matches after that (use two arrays and concat at the end)
-    if (pattern.length === 0 || emojiMatches(list[i], pattern)) {
+    if (pattern.length === 0 || emojiMatches(list[i], lowerCasePattern)) {
       matches.push({
         value: list[i].char,
         text: list[i].title,
