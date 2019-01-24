96db93da5bd11b149604e77ad88cc7575b75b0fe
Lee Newson <lee.newson@tiny.cloud>
2019-01-24 16:54:47 +1000

    TINY-2196: Minor cleanup as orThunk wasn't needed

--- src/main/ts/ephox/alloy/positioning/mode/HotspotAnchor.ts
+++ src/main/ts/ephox/alloy/positioning/mode/HotspotAnchor.ts
@@ -17,7 +17,7 @@ const placement = (component, anchorInfo: HotspotAnchor, origin) => {
   return Option.some(
     NuAnchor({
       anchorBox: anchorBox,
-      bubble: anchorInfo.bubble.getOrThunk(() => Bubble.fallback()),
+      bubble: anchorInfo.bubble.getOr(Bubble.fallback()),
       overrides: { },
       layouts: layouts,
       placer: Option.none()
