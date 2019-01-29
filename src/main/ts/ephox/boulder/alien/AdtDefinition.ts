cf4ca0c2bd0e47cbc561019e5991ab0d0b5ae612
Andrew Herron <thespyder@programmer.net>
2019-01-29 22:08:52 +1000

    TINY-3248: Removed interface that is now in Katamari

--- src/main/ts/ephox/boulder/alien/AdtDefinition.ts
+++
@@ -1,8 +0,0 @@
-
-// TODO move this generic into Katamari Adt
-// All boulder adts extend this generic interface
-export interface AdtInterface {
-  fold: <T>(...fn: Array<(...x: any[]) => T>) => T;
-  match: <T>(branches: { [k: string]: (...x: any[]) => T }) => T;
-  log: (label: string) => string;
-}
