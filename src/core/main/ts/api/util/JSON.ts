0afeddb9f8f87aa93c6d9ecf0749508968838008
Lee Newson <lee.newson@tiny.cloud>
2019-02-04 19:11:58 +1000

    TINY-3281: Fixed an issue where JSON serialization could generate invalid JSON content

--- src/core/main/ts/api/util/JSON.ts
+++ src/core/main/ts/api/util/JSON.ts
@@ -5,6 +5,8 @@
  * For commercial licenses see https://www.tiny.cloud/
  */
 
+import { Arr, Obj, Type } from '@ephox/katamari';
+
 /**
  * JSON parser and serializer class.
  *
@@ -18,61 +20,55 @@
  * var str = tinymce.util.JSON.serialize(obj);
  */
 
-const serialize = function (o, quote?) {
-  let i, v, t, name;
-
-  quote = quote || '"';
-
-  if (o === null) {
+const serialize = function (o: any, quote: string = '"') {
+  // TODO: Can we just use JSON.stringify here instead?
+  if (Type.isNull(o)) {
     return 'null';
-  }
-
-  t = typeof o;
-
-  if (t === 'string') {
-    v = '\bb\tt\nn\ff\rr\""\'\'\\\\';
+  } else if (Type.isNumber(o) || Type.isBoolean(o)) {
+    return o.toString();
+  } else if (Type.isString(o)) {
+    const specialChars = '\bb\tt\nn\ff\rr\""\'\'\\\\';
 
     /*eslint no-control-regex:0 */
-    return quote + o.replace(/([\u0080-\uFFFF\x00-\x1f\"\'\\])/g, function (a, b) {
+    return quote + o.replace(/([\u0080-\uFFFF\x00-\x1f\"\'\\])/g, (match, group1) => {
       // Make sure single quotes never get encoded inside double quotes for JSON compatibility
-      if (quote === '"' && a === '\'') {
-        return a;
+      if (quote === '"' && match === '\'') {
+        return match;
       }
 
-      i = v.indexOf(b);
-
-      if (i + 1) {
-        return '\\' + v.charAt(i + 1);
+      const i = specialChars.indexOf(group1);
+      if (i !== -1) {
+        return '\\' + specialChars.charAt(i + 1);
       }
 
-      a = b.charCodeAt().toString(16);
-
-      return '\\u' + '0000'.substring(a.length) + a;
+      const hexCode = group1.charCodeAt(0).toString(16);
+      return '\\u' + '0000'.substring(hexCode.length) + hexCode;
     }) + quote;
-  }
+  } else if (Type.isArray(o)) {
+    const content = Arr.foldl(o, (acc, value) => {
+      const v = (Type.isFunction(value) || Type.isUndefined(value)) ? 'null' : serialize(value, quote);
+      return acc + (acc.length > 0 ? ',' : '') + v;
+    }, '');
 
-  if (t === 'object') {
-    if (o.hasOwnProperty && Object.prototype.toString.call(o) === '[object Array]') {
-      for (i = 0, v = '['; i < o.length; i++) {
-        v += (i > 0 ? ',' : '') + serialize(o[i], quote);
-      }
+    return '[' + content + ']';
+  } else if (Type.isObject(o)) {
+    if (Date.prototype.isPrototypeOf(o)) {
+      return quote + o.toISOString() + quote;
+    } else {
+      const content = Arr.foldl(Obj.keys(o), (acc, key) => {
+        const value = o[key];
+        if (Type.isFunction(value) || Type.isUndefined(value)) {
+          return acc;
+        } else {
+          return acc + (acc.length > 0 ? ',' : '') + serialize(key, quote) + ':' + serialize(value, quote);
+        }
+      }, '');
 
-      return v + ']';
+      return '{' + content + '}';
     }
-
-    v = '{';
-
-    for (name in o) {
-      if (o.hasOwnProperty(name)) {
-        v += typeof o[name] !== 'function' ? (v.length > 1 ? ',' + quote : quote) + name +
-          quote + ':' + serialize(o[name], quote) : '';
-      }
-    }
-
-    return v + '}';
+  } else {
+    return '';
   }
-
-  return '' + o;
 };
 
 export default {
@@ -90,10 +86,10 @@ export default {
    * Unserializes/parses the specified JSON string into a object.
    *
    * @method parse
-   * @param {string} s JSON String to parse into a JavaScript object.
+   * @param {string} text JSON String to parse into a JavaScript object.
    * @return {Object} Object from input JSON string or undefined if it failed.
    */
-  parse (text) {
+  parse (text: string) {
     try {
       return JSON.parse(text);
     } catch (ex) {
--- src/core/test/ts/browser/util/JsonTest.ts
+++ src/core/test/ts/browser/util/JsonTest.ts
@@ -11,39 +11,53 @@ UnitTest.asynctest('browser.tinymce.core.util.JsonTest', function () {
   suite.test('serialize', function () {
     LegacyUnit.equal(
       Json.serialize({
-        arr1: [1, 2, 3, [1, 2, 3]],
-        bool1: true,
-        float1: 3.14,
-        int1: 123,
-        null1: null,
-        obj1: { key1: 'val1', key2: 'val2' }, str1: '\"\'abc\u00C5123\\'
+        'arr1': [1, 2, 3, [1, 2, 3]],
+        'bool1': true,
+        'float1': 3.14,
+        'int1': 123,
+        'null1': null,
+        'obj1': { key1: 'val1', key2: 'val2' },
+        '\"obj2': { key1: undefined },
+        'str1': '\"\'abc\u00C5123\\',
+        'date1': new Date(0)
       }
       ),
       '{"arr1":[1,2,3,[1,2,3]],"bool1":true,"float1":3.14,"int1":123,"null1":null,' +
-      '"obj1":{"key1":"val1","key2":"val2"},"str1":"\\"\'abc\\u00c5123\\\\"}'
+      '"obj1":{"key1":"val1","key2":"val2"},"\\"obj2":{},"str1":"\\"\'abc\\u00c5123\\\\",' +
+      '"date1":"1970-01-01T00:00:00.000Z"}'
     );
 
     LegacyUnit.equal(
       Json.serialize({
-        arr1: [1, 2, 3, [1, 2, 3]],
-        bool1: true,
-        float1: 3.14,
-        int1: 123,
-        null1: null,
-        obj1: { key1: 'val1', key2: 'val2' }, str1: '\"\'abc\u00C5123'
+        'arr1': [1, 2, 3, [1, 2, 3]],
+        'bool1': true,
+        'float1': 3.14,
+        'int1': 123,
+        'null1': null,
+        'obj1': { key1: 'val1', key2: 'val2' },
+        '\'obj2': { key1: undefined },
+        'str1': '\"\'abc\u00C5123',
+        'date1': new Date(0)
       }, '\''
       ),
       '{\'arr1\':[1,2,3,[1,2,3]],\'bool1\':true,\'float1\':3.14,\'int1\':123,\'null1\':null,' +
-      '\'obj1\':{\'key1\':\'val1\',\'key2\':\'val2\'},\'str1\':\'\\"\\\'abc\\u00c5123\'}'
+      '\'obj1\':{\'key1\':\'val1\',\'key2\':\'val2\'},\'\\\'obj2\':{},\'str1\':\'\\"\\\'abc\\u00c5123\',' +
+      '\'date1\':\'1970-01-01T00:00:00.000Z\'}'
     );
   });
 
   suite.test('parse', function () {
-    LegacyUnit.equal(
-      Json.parse('{"arr1":[1,2,3,[1,2,3]],"bool1":true,"float1":3.14,"int1":123,"null1":null,' +
-      '"obj1":{"key1":"val1","key2":"val2"},"str1":"abc\\u00c5123"}').str1,
-      'abc\u00c5123'
-    );
+    const parsedValue = Json.parse('{"arr1":[1,2,3,[1,2,3]],"bool1":true,"float1":3.14,"int1":123,"null1":null,' +
+      '"obj1":{"key1":"val1","key2":"val2"},"\\"obj2":{},"str1":"abc\\u00c5123","date1":"1970-01-01T00:00:00.000Z"}');
+    LegacyUnit.equal(parsedValue.arr1, [ 1, 2, 3, [1, 2, 3] ]);
+    LegacyUnit.equal(parsedValue.bool1, true);
+    LegacyUnit.equal(parsedValue.float1, 3.14);
+    LegacyUnit.equal(parsedValue.int1, 123);
+    LegacyUnit.equal(parsedValue.null1, null);
+    LegacyUnit.equal(parsedValue.obj1, { key1: 'val1', key2: 'val2' });
+    LegacyUnit.equal(parsedValue['\"obj2'], { });
+    LegacyUnit.equal(parsedValue.str1, 'abc\u00c5123');
+    LegacyUnit.equal(parsedValue.date1, '1970-01-01T00:00:00.000Z');
   });
 
   Pipeline.async({}, suite.toSteps({}), function () {
