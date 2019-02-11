64ea6d5f1f6fa8f9a67835849a575b61ffab1bee
Lee Newson <lee.newson@tiny.cloud>
2019-02-11 10:19:44 +1000

    TINY-3199: Fixed an issue where clicking remove link, wasn't removing the link in some cases

--- src/core/test/ts/browser/fmt/RemoveTrailingWhitespaceFormatTest.ts
+++ src/core/test/ts/browser/fmt/RemoveTrailingWhitespaceFormatTest.ts
@@ -1,24 +1,17 @@
 import { GeneralSteps, Logger, Pipeline } from '@ephox/agar';
 import { TinyApis, TinyLoader, TinyUi } from '@ephox/mcagar';
-import LinkPlugin from 'tinymce/plugins/link/Plugin';
 import Theme from 'tinymce/themes/silver/Theme';
 import { UnitTest } from '@ephox/bedrock';
 
-UnitTest.asynctest(
-  'browser.tinymce.core.fmt.RemoveTrailingWhitespaceFormatTest',
-  function () {
-    const success = arguments[arguments.length - 2];
-    const failure = arguments[arguments.length - 1];
+UnitTest.asynctest('browser.tinymce.core.fmt.RemoveTrailingWhitespaceFormatTest', (success, failure) => {
 
     Theme();
-    LinkPlugin();
 
     TinyLoader.setup(function (editor, onSuccess, onFailure) {
       const tinyApis = TinyApis(editor);
       const tinyUi = TinyUi(editor);
 
       const boldSelector = 'button[aria-label="Bold"]';
-      const unlinkSelector = 'button[title="Remove link"]';
 
       Pipeline.async({}, [
         tinyApis.sFocus,
@@ -37,19 +30,18 @@ UnitTest.asynctest(
         Logger.t('unlink with leading whitespace', GeneralSteps.sequence([
           tinyApis.sSetRawContent('<p><a href="#">a b</a></p>'),
           tinyApis.sSetSelection([0, 0, 0], 1, [0, 0, 0], 3),
-          tinyUi.sClickOnToolbar('click unlink', unlinkSelector),
+          tinyApis.sExecCommand('unlink'),
           tinyApis.sAssertContent('<p><a href="#">a</a> b</p>')
         ])),
         Logger.t('unlink with trailing whitespace', GeneralSteps.sequence([
           tinyApis.sSetRawContent('<p><a href="#">a b</a></p>'),
           tinyApis.sSetSelection([0, 0, 0], 0, [0, 0, 0], 2),
-          tinyUi.sClickOnToolbar('click unlink', unlinkSelector),
+          tinyApis.sExecCommand('unlink'),
           tinyApis.sAssertContent('<p>a <a href="#">b</a></p>')
         ]))
       ], onSuccess, onFailure);
     }, {
-      plugins: 'link',
-      toolbar: 'bold unlink',
+      toolbar: 'bold',
       base_url: '/project/js/tinymce'
     }, success, failure);
   }
