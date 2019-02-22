a3f02318c4899245b8d128e37b081edfd7f2acf9
Millie <millie.macdonald@ephox.com>
2019-02-22 14:35:11 +1000

    TINY-2712 Add fixed_toolbar_container and attach chrome to specified element

--- src/themes/silver/main/ts/modes/Inline.ts
+++ src/themes/silver/main/ts/modes/Inline.ts
@@ -6,8 +6,8 @@
  */
 
 import { Attachment, Docking, Focusing, SplitToolbar } from '@ephox/alloy';
-import { Option } from '@ephox/katamari';
-import { Body, Css, Element, Height, Location } from '@ephox/sugar';
+import { Option, Type } from '@ephox/katamari';
+import { Body, Css, Element, Height, Location, Node, SelectorFind } from '@ephox/sugar';
 import DOMUtils from 'tinymce/core/api/dom/DOMUtils';
 import { Editor } from 'tinymce/core/api/Editor';
 
@@ -18,6 +18,10 @@ import { inline as loadInlineSkin } from './../ui/skin/Loader';
 import { RenderUiComponents, RenderUiConfig, RenderArgs, ModeRenderInfo } from '../Render';
 import { UiFactoryBackstage } from '../backstage/Backstage';
 import { isSplitToolbar } from '../api/Settings';
+
+const fixedContainerSelector = (editor) => editor.getParam('fixed_toolbar_container');
+const fixedContainerElement = (editor) => SelectorFind.descendant(Body.body(), fixedContainerSelector(editor));
+const useFixedContainer = (editor) => fixedContainerElement(editor).isSome();
 
 const render = (editor: Editor, uiComponents: RenderUiComponents, rawUiConfig: RenderUiConfig, backstage: UiFactoryBackstage, args: RenderArgs): ModeRenderInfo => {
   let floatContainer;
@@ -38,30 +42,31 @@ const render = (editor: Editor, uiComponents: RenderUiComponents, rawUiConfig: R
   };
 
   const setPosition = () => {
-    const toolbar = OuterContainer.getToolbar(uiComponents.outerContainer);
-    if (split) {
-      toolbar.each(SplitToolbar.refresh);
-    }
+    if (!useFixedContainer(editor)) {
+      const toolbar = OuterContainer.getToolbar(uiComponents.outerContainer);
+      if (split) {
+        toolbar.each(SplitToolbar.refresh);
+      }
 
-    const isDocked = Css.getRaw(floatContainer.element(), 'position').is('fixed');
-    if (!isDocked) {
-      // We need to update the toolbar location if the window has resized while the toolbar is position absolute
-      // Not sure if we should always set this, or if it's worth checking against the current position
-      const offset = split ? toolbar.fold(() => 0, (tbar) => {
-        // If we have an overflow toolbar, we need to offset the positioning by the height of the overflow toolbar
-        return Height.get(tbar.components()[1].element());
-      }) : 0;
-      Css.setAll(floatContainer.element(), calcPosition(offset));
+      const isDocked = Css.getRaw(floatContainer.element(), 'position').is('fixed');
+      if (!isDocked) {
+        // We need to update the toolbar location if the window has resized while the toolbar is position absolute
+        // Not sure if we should always set this, or if it's worth checking against the current position
+        const offset = split ? toolbar.fold(() => 0, (tbar) => {
+          // If we have an overflow toolbar, we need to offset the positioning by the height of the overflow toolbar
+          return Height.get(tbar.components()[1].element());
+        }) : 0;
+        Css.setAll(floatContainer.element(), calcPosition(offset));
+      }
+      // Let docking handle fixed <-> absolute transitions, etc.
+      Docking.refresh(floatContainer);
     }
-    // Let docking handle fixed <-> absolute transitions, etc.
-    Docking.refresh(floatContainer);
   };
 
   const show = () => {
     Css.set(uiComponents.outerContainer.element(), 'display', 'flex');
     DOM.addClass(editor.getBody(), 'mce-edit-focus');
     setPosition();
-    Docking.refresh(floatContainer);
   };
 
   const hide = () => {
@@ -79,8 +84,13 @@ const render = (editor: Editor, uiComponents: RenderUiComponents, rawUiConfig: R
 
     floatContainer = uiComponents.outerContainer;
 
-    Attachment.attachSystem(Body.body(), uiComponents.mothership);
-    Attachment.attachSystem(Body.body(), uiComponents.uiMothership);
+    let uiContainer = Body.body();
+    if (useFixedContainer) {
+      uiContainer = fixedContainerElement(editor).getOr(Body.body());
+    }
+
+    Attachment.attachSystem(uiContainer, uiComponents.mothership);
+    Attachment.attachSystem(uiContainer, uiComponents.uiMothership);
 
     OuterContainer.setToolbar(
       uiComponents.outerContainer,
@@ -92,12 +102,15 @@ const render = (editor: Editor, uiComponents: RenderUiComponents, rawUiConfig: R
       identifyMenus(editor, rawUiConfig)
     );
 
-    // initialise the toolbar - initial positioning, refresh docking, then show
-    Css.set(floatContainer.element(), 'position', 'absolute');
-    setPosition();
-    show();
+    if (!useFixedContainer) {
+      // initialise the toolbar - initial positioning, refresh docking, then show
+      Css.set(floatContainer.element(), 'position', 'absolute');
+      setPosition();
+      show();
 
-    editor.on('nodeChange ResizeWindow', setPosition);
+      editor.on('nodeChange ResizeWindow', setPosition);
+    }
+
     editor.on('activate', show);
     editor.on('deactivate', hide);
 
@@ -119,7 +132,7 @@ const render = (editor: Editor, uiComponents: RenderUiComponents, rawUiConfig: R
 };
 
 const getBehaviours = (editor: Editor) => {
-  return [
+  return useFixedContainer(editor) ? [] : [
     Docking.config({
       leftAttr: 'data-dock-left',
       topAttr: 'data-dock-top',
