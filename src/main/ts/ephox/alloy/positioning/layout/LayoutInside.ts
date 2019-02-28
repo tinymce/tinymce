269cdc8dc6ef7353c5f156ae1f6876fd7e20694e
Millie <millie.macdonald@ephox.com>
2019-02-28 15:00:44 +1000

    TINY-2712 Cleaned up and commented LayoutInside

--- src/main/ts/ephox/alloy/positioning/layout/LayoutInside.ts
+++ src/main/ts/ephox/alloy/positioning/layout/LayoutInside.ts
@@ -9,72 +9,72 @@ import { AnchorBox, AnchorElement, AnchorLayout } from './LayoutTypes';
   See the LayoutInsideDemo for examples.
  */
 
-// display element to the right, left edge against the anchor
-const eastX = (anchor: AnchorBox): number => {
-  return anchor.x();
-};
-
-// element centre aligned horizontally with the anchor
-const middleX = (anchor: AnchorBox, element: AnchorElement): number => {
-  return anchor.x() + (anchor.width() / 2) - (element.width() / 2);
-};
-
-// display element to the left, right edge against the right of the anchor
-const westX = (anchor: AnchorBox, element: AnchorElement): number => {
-  return anchor.x() + anchor.width() - element.width();
-};
-
-// display element above, bottom edge against the top of the anchor
-const northY = (anchor: AnchorBox): number => {
-  return anchor.y();
-};
-
-// display element below, top edge against the bottom of the anchor
-const southY = (anchor: AnchorBox, element: AnchorElement): number => {
-  return anchor.y() + anchor.height() - element.height();
-};
-
-// display element below, top edge against the bottom of the anchor
-const centreY = (anchor: AnchorBox, element: AnchorElement): number => {
-  return anchor.y() + (anchor.height() / 2) - (element.height() / 2);
-};
-
-const eastEdgeX = (anchor: AnchorBox, element: AnchorElement): number => {
-  return anchor.x() + anchor.width() - element.width();
-};
-
+// returns left edge of anchor - used to display element to the left, left edge against the anchor
 const westEdgeX = (anchor: AnchorBox): number => {
   return anchor.x();
 };
 
+// returns middle of anchor minus half the element width - used to horizontally centre element to the anchor
+const middleX = (anchor: AnchorBox, element: AnchorElement): number => {
+  return anchor.x() + (anchor.width() / 2) - (element.width() / 2);
+};
+
+// returns right edge of anchor minus element width - used to display element to the right, right edge against the anchor
+const eastEdgeX = (anchor: AnchorBox, element: AnchorElement): number => {
+  return anchor.x() + anchor.width() - element.width();
+};
+
+// returns top edge - used to display element to the top, top edge against the anchor
+const northY = (anchor: AnchorBox): number => {
+  return anchor.y();
+};
+
+// returns bottom edge minus element height - used to display element at the bottom, bottom edge against the anchor
+const southY = (anchor: AnchorBox, element: AnchorElement): number => {
+  return anchor.y() + anchor.height() - element.height();
+};
+
+// returns centre of anchor minus half the element height - used to vertically centre element to the anchor
+const centreY = (anchor: AnchorBox, element: AnchorElement): number => {
+  return anchor.y() + (anchor.height() / 2) - (element.height() / 2);
+};
+
+// positions element in bottom right of the anchor
 const southeast: AnchorLayout = (anchor: AnchorBox, element: AnchorElement, bubbles: Bubble) => {
-  return NuSpotInfo(eastX(anchor), southY(anchor, element), bubbles.southeast(), Direction.southeast(), 'layout-se');
+  return NuSpotInfo(westEdgeX(anchor), southY(anchor, element), bubbles.southeast(), Direction.southeast(), 'layout-se');
 };
 
+// positions element in the bottom left of the anchor
 const southwest: AnchorLayout = (anchor: AnchorBox, element: AnchorElement, bubbles: Bubble) => {
-  return NuSpotInfo(westX(anchor, element), southY(anchor, element), bubbles.southwest(), Direction.southwest(), 'layout-sw');
+  return NuSpotInfo(eastEdgeX(anchor, element), southY(anchor, element), bubbles.southwest(), Direction.southwest(), 'layout-sw');
 };
 
+// positions element in the top right of the anchor
 const northeast: AnchorLayout = (anchor: AnchorBox, element: AnchorElement, bubbles: Bubble) => {
-  return NuSpotInfo(eastX(anchor), northY(anchor), bubbles.northeast(), Direction.northeast(), 'layout-ne');
+  return NuSpotInfo(westEdgeX(anchor), northY(anchor), bubbles.northeast(), Direction.northeast(), 'layout-ne');
 };
 
+// positions element in the top left of the anchor
 const northwest: AnchorLayout = (anchor: AnchorBox, element: AnchorElement, bubbles: Bubble) => {
-  return NuSpotInfo(westX(anchor, element), northY(anchor), bubbles.northwest(), Direction.northwest(), 'layout-nw');
+  return NuSpotInfo(eastEdgeX(anchor, element), northY(anchor), bubbles.northwest(), Direction.northwest(), 'layout-nw');
 };
 
+// positions element at the top of the anchor, horizontally centered
 const north: AnchorLayout = (anchor: AnchorBox, element: AnchorElement, bubbles: Bubble) => {
   return NuSpotInfo(middleX(anchor, element), northY(anchor), bubbles.north(), Direction.north(), 'layout-n');
 };
 
+// positions element at the bottom of the anchor, horizontally centered
 const south: AnchorLayout = (anchor: AnchorBox, element: AnchorElement, bubbles: Bubble) => {
   return NuSpotInfo(middleX(anchor, element), southY(anchor, element), bubbles.south(), Direction.south(), 'layout-s');
 };
 
+// positions element with right edge against the anchor, vertically centered
 const east: AnchorLayout = (anchor: AnchorBox, element: AnchorElement, bubbles: Bubble) => {
   return NuSpotInfo(eastEdgeX(anchor, element), centreY(anchor, element), bubbles.east(), Direction.east(), 'layout-e');
 };
 
+// positions element with left each against the anchor, vertically centered
 const west: AnchorLayout = (anchor: AnchorBox, element: AnchorElement, bubbles: Bubble) => {
   return NuSpotInfo(westEdgeX(anchor), centreY(anchor, element), bubbles.west(), Direction.west(), 'layout-w');
 };
