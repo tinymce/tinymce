f9e2abd57fd719cd0753ddebd985a1d4c11e6484
Joshua Haines <josh.j.haines@gmail.com>
2019-03-08 11:48:06 +1000

    TINY-3138: remove unused code

--- src/plugins/directionality/main/ts/ui/Buttons.ts
+++ src/plugins/directionality/main/ts/ui/Buttons.ts
@@ -5,20 +5,9 @@
  * For commercial licenses see https://www.tiny.cloud/
  */
 
-import Tools from 'tinymce/core/api/util/Tools';
 import { Editor } from 'tinymce/core/api/Editor';
 import Direction from '../core/Direction';
 import { Element } from '@ephox/sugar';
-
-const generateSelector = function (dir) {
-  const selector = [];
-
-  Tools.each('h1 h2 h3 h4 h5 h6 div p'.split(' '), function (name) {
-    selector.push(name + '[dir=' + dir + ']');
-  });
-
-  return selector.join(',');
-};
 
 const getNodeChangeHandler = (editor, dir) => {
   return (api) => {
