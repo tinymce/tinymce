607c93ddddd4202c4c024f0847efa1648fa47bdf
Spocke <spocke@moxiecode.com>
2019-03-12 17:07:53 +0100

    TINY-2995: fixed typos in test

--- src/core/test/ts/browser/keyboard/HomeEndKeysTest.ts
+++ src/core/test/ts/browser/keyboard/HomeEndKeysTest.ts
@@ -14,13 +14,13 @@ UnitTest.asynctest('browser.tinymce.core.keyboard.HomeEndKeysTest', (success, fa
       Pipeline.async({}, [
         tinyApis.sFocus,
         Logger.t('Home key', GeneralSteps.sequence([
-          Logger.t('Home key should caret before cef within the same block', GeneralSteps.sequence([
+          Logger.t('Home key should move caret before cef within the same block', GeneralSteps.sequence([
             tinyApis.sSetContent('<p>123</p><p><span contenteditable="false">CEF</span>456</p>'),
             tinyApis.sSetCursor([1, 1], 3),
             tinyActions.sContentKeystroke(VK.HOME, { }),
             tinyApis.sAssertSelection([1, 0], 0, [1, 0], 0)
           ])),
-          Logger.t('Home key should not caret before cef within the same block if there is a BR in between', GeneralSteps.sequence([
+          Logger.t('Home key should not move caret before cef within the same block if there is a BR in between', GeneralSteps.sequence([
             tinyApis.sSetContent('<p>123</p><p><span contenteditable="false">CEF</span><br>456</p>'),
             tinyApis.sSetCursor([1, 2], 3),
             tinyActions.sContentKeystroke(VK.HOME, { }),
@@ -28,13 +28,13 @@ UnitTest.asynctest('browser.tinymce.core.keyboard.HomeEndKeysTest', (success, fa
           ]))
         ])),
         Logger.t('End key', GeneralSteps.sequence([
-          Logger.t('End key should caret after cef within the same block', GeneralSteps.sequence([
+          Logger.t('End key should move caret after cef within the same block', GeneralSteps.sequence([
             tinyApis.sSetContent('<p>123<span contenteditable="false">CEF</span></p><p>456</p>'),
             tinyApis.sSetCursor([0, 0], 0),
             tinyActions.sContentKeystroke(VK.END, { }),
             tinyApis.sAssertSelection([0, 2], 1, [0, 2], 1)
           ])),
-          Logger.t('End key should not caret after cef within the same block if there is a BR in between', GeneralSteps.sequence([
+          Logger.t('End key should not move caret after cef within the same block if there is a BR in between', GeneralSteps.sequence([
             tinyApis.sSetContent('<p>123<br><span contenteditable="false">CEF</span></p><p>456</p>'),
             tinyApis.sSetCursor([0, 0], 0),
             tinyActions.sContentKeystroke(VK.END, { }),
