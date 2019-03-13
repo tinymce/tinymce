1fc073da1e4c5c9f5b23925d13f24f27325e175b
Andrew Herron <thespyder@programmer.net>
2019-03-13 15:28:30 +1000

    TINY-3074: Update to new jax API

--- src/main/ts/ephox/agar/server/SeleniumAction.ts
+++ src/main/ts/ephox/agar/server/SeleniumAction.ts
@@ -1,33 +1,29 @@
-import { Ajax, ContentType, Credentials, ResponseType } from '@ephox/jax';
-
 import { Step } from '../api/Step';
 import { Chain } from '../api/Chain';
+import { Http, DataType } from '@ephox/jax';
 
-const sPerform = function <T>(path: string, info: any) {
-  return Step.async<T>(function (next, die) {
-    Ajax.post(
-      path,
-      ContentType.json(info),
-      ResponseType.json(),
-      Credentials.none(),
-      {}
-    ).get(function (res) {
-      res.fold(die, next);
-    });
+
+const postInfo = (path: string, info: any, die: (err: any) => void, next: (v: {}) => void) => {
+  Http.post({
+    url: path,
+    body: {
+      type: DataType.JSON,
+      data: info
+    },
+    responseType: DataType.JSON
+  }).get((res) => {
+    res.fold(die, next);
   });
 };
 
+const sPerform = <T>(path: string, info: any) =>
+  Step.async<T>(function (next, die) {
+    postInfo(path, info, die, next);
+  });
+
 const cPerform = (path: string) =>
   Chain.async((info, next, die) => {
-    Ajax.post(
-      path,
-      ContentType.json(info),
-      ResponseType.json(),
-      Credentials.none(),
-      {}
-    ).get((res) => {
-      res.fold(die, next);
-    })
+    postInfo(path, info, die, next);
   });
 
 export {
