ef1ace13cf9a4977d4b3226fdddec7dc993ddacb
Andrew Herron <thespyder@programmer.net>
2019-06-24 12:15:01 +1000

    When is a CSSStyleDeclaration not a prototype of CSSStyleDeclaration? I don't know, but Iwe have switched to checking the getPropertyValue function instead.

--- modules/sugar/src/main/ts/ephox/sugar/impl/Style.ts
+++ modules/sugar/src/main/ts/ephox/sugar/impl/Style.ts
@@ -1,8 +1,9 @@
 import { HTMLElement, CSSStyleDeclaration } from '@ephox/dom-globals';
+import { Type } from '@ephox/katamari';
 
 // some elements, such as mathml, don't have style attributes
 // others, such as angular elements, have style attributes that aren't a CSSStyleDeclaration
-const isSupported = (dom: HTMLElement) => CSSStyleDeclaration.prototype.isPrototypeOf(dom.style);
+const isSupported = (dom: HTMLElement) => dom.style !== undefined && Type.isFunction(dom.style.getPropertyValue);
 
 export {
   isSupported
