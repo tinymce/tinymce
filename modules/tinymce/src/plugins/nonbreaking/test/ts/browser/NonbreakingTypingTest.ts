b4b2699606620f7a012a47e0566f19b8acb51dec
Luke Trouton <luke.trouton@tiny.cloud>
2019-07-09 16:26:41 +1000

    TINY-3647: Work on NonbreakingTypingTest.ts

--- modules/tinymce/src/plugins/nonbreaking/test/ts/browser/NonbreakingTypingTest.ts
+++ modules/tinymce/src/plugins/nonbreaking/test/ts/browser/NonbreakingTypingTest.ts
@@ -1,7 +1,6 @@
-import { ApproxStructure, Pipeline, Log, RealKeys } from '@ephox/agar';
+import { ApproxStructure, Log, Pipeline, RealKeys } from '@ephox/agar';
 import { UnitTest } from '@ephox/bedrock';
 import { TinyApis, TinyLoader, TinyUi } from '@ephox/mcagar';
-
 import NonbreakingPlugin from 'tinymce/plugins/nonbreaking/Plugin';
 import theme from 'tinymce/themes/silver/Theme';
 
@@ -15,26 +14,117 @@ UnitTest.asynctest('browser.tinymce.plugins.nonbreaking.NonbreakingSanityTest', 
     const tinyUi = TinyUi(editor);
     const tinyApis = TinyApis(editor);
 
-    Pipeline.async({}, Log.steps('TBA', 'NonBreaking: Click on the nbsp button then type some text, and assert content is correct', [
-      tinyUi.sClickOnToolbar('click on nbsp button', 'button[aria-label="Nonbreaking space"]'),
-      RealKeys.sSendKeysOn(
-        'iframe => body => p',
-        [
-          RealKeys.text('test')
-        ]
-      ),
-      tinyApis.sAssertContentStructure(ApproxStructure.build(function (s, str) {
-        return s.element('body', {
-          children: [
-            s.element('p', {
-              children: [
-                s.text(str.is('\u00a0test'))
-              ]
-            })
+    Pipeline.async({}, [
+      Log.stepsAsStep('TBA', 'NonBreaking: Click on the nbsp button then type some text, and assert content is correct', [
+        tinyUi.sClickOnToolbar('click on nbsp button', 'button[aria-label="Nonbreaking space"]'),
+        RealKeys.sSendKeysOn(
+          'iframe => body => p',
+          [
+            RealKeys.text('test')
           ]
-        });
-      }))
-    ]), onSuccess, onFailure);
+        ),
+        tinyApis.sAssertContentStructure(ApproxStructure.build(function (s, str) {
+          return s.element('body', {
+            children: [
+              s.element('p', {
+                children: [
+                  s.text(str.is('\u00a0test'))
+                ]
+              })
+            ]
+          });
+        }))
+      ]),
+
+      tinyApis.sSetContent(''),
+
+      Log.stepsAsStep('TBA', 'NonBreaking: Add text to editor, click on the nbsp button, and assert content is correct', [
+        tinyApis.sSetContent('test'),
+        tinyApis.sSetCursor([0, 0], 4),
+        tinyUi.sClickOnToolbar('click on nbsp button', 'button[aria-label="Nonbreaking space"]'),
+        tinyApis.sAssertContentStructure(ApproxStructure.build(function (s, str) {
+          return s.element('body', {
+            children: [
+              s.element('p', {
+                children: [
+                  s.text(str.is('test\u00a0'))
+                ]
+              })
+            ]
+          });
+        }))
+      ]),
+
+      tinyApis.sSetContent(''),
+
+      // Log.stepsAsStep('TBA', 'NonBreaking: Add content to editor, click on the nbsp button then type some text, and assert content is correct', [
+      //   tinyApis.sSetContent('test'),
+      //   tinyUi.sClickOnToolbar('click on nbsp button', 'button[aria-label="Nonbreaking space"]'),
+      //   RealKeys.sSendKeysOn(
+      //     'iframe => body => p',
+      //     [
+      //       RealKeys.text('test')
+      //     ]
+      //   ),
+      //   tinyApis.sAssertContentStructure(ApproxStructure.build(function (s, str) {
+      //     return s.element('body', {
+      //       children: [
+      //         s.element('p', {
+      //           children: [
+      //             s.text(str.is('test\u00a0test'))
+      //           ]
+      //         })
+      //       ]
+      //     });
+      //   }))
+      // ]),
+
+      // tinyApis.sSetContent(''),
+
+      Log.stepsAsStep('TBA', 'NonBreaking: Click on the nbsp button then type a space, and assert content is correct', [
+        tinyUi.sClickOnToolbar('click on nbsp button', 'button[aria-label="Nonbreaking space"]'),
+        RealKeys.sSendKeysOn(
+          'iframe => body => p',
+          [
+            RealKeys.text(' ')
+          ]
+        ),
+        tinyApis.sAssertContentStructure(ApproxStructure.build(function (s, str) {
+          return s.element('body', {
+            children: [
+              s.element('p', {
+                children: [
+                  s.text(str.is('\u00a0\u00a0'))
+                ]
+              })
+            ]
+          });
+        }))
+      ]),
+
+      Log.stepsAsStep('TBA', 'NonBreaking: Add text to editor, click on the nbsp button and add content plus a space, and assert content is correct', [
+        tinyApis.sSetContent('test'),
+        tinyUi.sClickOnToolbar('click on nbsp button', 'button[aria-label="Nonbreaking space"]'),
+        RealKeys.sSendKeysOn(
+          'iframe => body => p',
+          [
+            RealKeys.text('test ')
+          ]
+        ),
+        tinyApis.sAssertContentStructure(ApproxStructure.build(function (s, str) {
+          return s.element('body', {
+            children: [
+              s.element('p', {
+                children: [
+                  s.text(str.is('test test\u00a0'))
+                ]
+              })
+            ]
+          });
+        }))
+      ]),
+
+    ], onSuccess, onFailure);
   }, {
     plugins: 'nonbreaking',
     toolbar: 'nonbreaking',
