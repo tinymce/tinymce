da8fc31b32a22202bab426e8026cc41df06680a1
James Johnson <james.johnson@ephox.com>
2019-07-24 12:10:33 +1000

    TINY-3389: Avoid complex inline scripts

--- modules/tinymce/src/themes/silver/test/ts/phantom/components/customeditor/BasicCustomEditorTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/phantom/components/customeditor/BasicCustomEditorTest.ts
@@ -1,39 +1,48 @@
 // TODO: Expose properly through alloy.
-import { ApproxStructure, Assertions, Logger, Step, Waiter } from '@ephox/agar';
+import { ApproxStructure, Assertions, Logger, Step, Waiter, Cleaner } from '@ephox/agar';
 import { GuiFactory, TestHelpers } from '@ephox/alloy';
 import { UnitTest } from '@ephox/bedrock';
-import { Global } from '@ephox/katamari';
+import { Global, Cell } from '@ephox/katamari';
 import { renderCustomEditor } from 'tinymce/themes/silver/ui/dialog/CustomEditor';
 import { RepresentingSteps } from '../../../module/ReperesentingSteps';
 import Scripts from 'tinymce/core/api/Scripts';
+import Delay from 'tinymce/core/api/util/Delay';
+import Promise from 'tinymce/core/api/util/Promise';
+import { Element, Class } from '@ephox/sugar';
+import { HTMLElement } from '@ephox/dom-globals';
 
-UnitTest.asynctest('CustomEditor component Test', (success, failure) => {
-  // for this test we have to check the internal state of another script so we store it in globals
-  Global.BasicCustomEditorTest = {
-    resolveInit: false,
-    customEditorValue: 'zztop'
-  };
-  // Create a fake TinyMCE global with just the Scripts
+declare const tinymce: { Scripts: Scripts };
+
+const install = () => {
+  const origTiny = Global.tinymce;
   Global.tinymce = {
     Scripts
   };
+  const uninstall = () => {
+    Global.tinymce = origTiny;
+  };
+  return uninstall;
+};
 
-  const dataScript = `data:text/javascript,tinymce.Scripts.add('BasicCustomEditorTest', function (e) {
-    var global = typeof window !== 'undefined' ? window : Function('return this;')();
-    return new Promise((resolve) => {
-    var intervalId = setInterval(() => {
-      if (global.BasicCustomEditorTest.resolveInit) {
-        clearInterval(intervalId);
-        e.className = 'my-custom-editor';
+UnitTest.asynctest('CustomEditor component Test', (success, failure) => {
+  const cleanup = Cleaner();
+  cleanup.add(install());
+  const resolveInit = Cell(false);
+  const customEditorValue = Cell('zztop');
+
+  tinymce.Scripts.add('BasicCustomEditorTest', (e: HTMLElement) => new Promise((resolve) => {
+    const intervalId = Delay.setInterval(() => {
+      if (resolveInit.get()) {
+        Delay.clearInterval(intervalId);
+        Class.add(Element.fromDom(e), 'my-custom-editor');
         resolve({
-          setValue: function(s) { global.BasicCustomEditorTest.customEditorValue = s; },
-          getValue: function() { return global.BasicCustomEditorTest.customEditorValue; },
-          destroy: function() {}
+          setValue(s: string) { customEditorValue.set(s); },
+          getValue() { return customEditorValue.get(); },
+          destroy() {}
         });
       }
     }, 500);
-  });
-});`;
+  }));
 
   TestHelpers.GuiSetup.setup(
     (store, doc, body) => {
@@ -43,7 +52,7 @@ UnitTest.asynctest('CustomEditor component Test', (success, failure) => {
           name: 'customeditor',
           tag: 'textarea',
           scriptId: 'BasicCustomEditorTest',
-          scriptUrl: dataScript,
+          scriptUrl: '/custom/404', // using the cache
           settings: undefined
          })
       );
@@ -74,7 +83,7 @@ UnitTest.asynctest('CustomEditor component Test', (success, failure) => {
         Logger.t(
           'Set to initialised',
           Step.sync(() => {
-            Global.BasicCustomEditorTest.resolveInit = true;
+            resolveInit.set(true);
           })
         ),
 
@@ -104,7 +113,6 @@ UnitTest.asynctest('CustomEditor component Test', (success, failure) => {
         )
       ];
     },
-    success,
-    failure
+    cleanup.wrap(success), cleanup.wrap(failure)
   );
 });
