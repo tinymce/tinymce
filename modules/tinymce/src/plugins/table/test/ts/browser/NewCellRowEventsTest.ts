386a272edd541f6c0334d28df972b7925d775d29
Dylan Just <dylan.just@ephox.com>
2019-08-07 10:06:37 +1000

    Fix lint error

--- modules/tinymce/src/plugins/table/test/ts/browser/NewCellRowEventsTest.ts
+++ modules/tinymce/src/plugins/table/test/ts/browser/NewCellRowEventsTest.ts
@@ -11,8 +11,6 @@ UnitTest.asynctest('browser.tinymce.plugins.table.NewCellRowEventsTest', (succes
   Plugin();
   SilverTheme();
 
-
-
   suite.test('TestCase-TBA: Table: Table newcell/newrow events', function (editor) {
     const cells = [];
     const rows = [];
