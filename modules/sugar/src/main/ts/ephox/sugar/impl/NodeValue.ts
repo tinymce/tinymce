785f3d47aa002924f47ce6a3bcbae4181e34c078
Dylan Just <dylan.just@ephox.com>
2019-08-13 16:04:11 +1000

    Formatting code.

--- modules/sugar/src/main/ts/ephox/sugar/impl/NodeValue.ts
+++ modules/sugar/src/main/ts/ephox/sugar/impl/NodeValue.ts
@@ -4,17 +4,21 @@ import { Node } from '@ephox/dom-globals';
 
 export default function (is: (e: Element<Node>) => boolean, name: string) {
   const get = function (element: Element): string {
-    if (!is(element)) { throw new Error('Can only get ' + name + ' value of a ' + name + ' node'); }
+    if (!is(element)) {
+      throw new Error('Can only get ' + name + ' value of a ' + name + ' node');
+    }
     return getOption(element).getOr('');
   };
 
   const getOption = function (element: Element<Node>): Option<string> {
-    return is(element) ? Option.from((element.dom() as Node).nodeValue) : Option.none<string>();
+    return is(element) ? Option.from(element.dom().nodeValue) : Option.none<string>();
   };
 
   const set = function (element: Element<Node>, value: string): void {
-    if (!is(element)) { throw new Error('Can only set raw ' + name + ' value of a ' + name + ' node'); }
-    (element.dom() as Node).nodeValue = value;
+    if (!is(element)) {
+      throw new Error('Can only set raw ' + name + ' value of a ' + name + ' node');
+    }
+    element.dom().nodeValue = value;
   };
 
   return {
