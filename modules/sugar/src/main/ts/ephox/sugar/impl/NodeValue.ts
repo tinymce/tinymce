193dfcb25a7b319760630fd4e23bc29aa1a0d495
Dylan Just <dylan.just@ephox.com>
2019-08-13 13:51:13 +1000

    TINY-3984: Remove legacy code path in Sugar's NodeValue class

--- modules/sugar/src/main/ts/ephox/sugar/impl/NodeValue.ts
+++ modules/sugar/src/main/ts/ephox/sugar/impl/NodeValue.ts
@@ -1,37 +1,20 @@
-import { PlatformDetection } from '@ephox/sand';
 import { Option } from '@ephox/katamari';
 import Element from '../api/node/Element';
 import { Node } from '@ephox/dom-globals';
 
 export default function (is: (e: Element<Node>) => boolean, name: string) {
-  const get = function (element: Element<Node>) {
-    if (!is(element)) {
-      throw new Error('Can only get ' + name + ' value of a ' + name + ' node');
-    }
+  const get = function (element: Element): string {
+    if (!is(element)) { throw new Error('Can only get ' + name + ' value of a ' + name + ' node'); }
     return getOption(element).getOr('');
   };
 
-  const getOptionIE10 = function (element: Element<Node>) {
-    // Prevent IE10 from throwing exception when setting parent innerHTML clobbers (TBIO-451).
-    try {
-      return getOptionSafe(element);
-    } catch (e) {
-      return Option.none<string>();
-    }
+  const getOption = function (element: Element<Node>): Option<string> {
+    return is(element) ? Option.from((element.dom() as Node).nodeValue) : Option.none<string>();
   };
 
-  const getOptionSafe = function (element: Element<Node>) {
-    return is(element) ? Option.from(element.dom().nodeValue) : Option.none<string>();
-  };
-
-  const browser = PlatformDetection.detect().browser;
-  const getOption = browser.isIE() && browser.version.major === 10 ? getOptionIE10 : getOptionSafe;
-
-  const set = function (element: Element<Node>, value: string) {
-    if (!is(element)) {
-      throw new Error('Can only set raw ' + name + ' value of a ' + name + ' node');
-    }
-    element.dom().nodeValue = value;
+  const set = function (element: Element<Node>, value: string): void {
+    if (!is(element)) { throw new Error('Can only set raw ' + name + ' value of a ' + name + ' node'); }
+    (element.dom() as Node).nodeValue = value;
   };
 
   return {
