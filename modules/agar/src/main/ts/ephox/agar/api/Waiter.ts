cbfd6f9f331939db4de3b65a62127cbeb9b0c1c1
Dylan Just <dylan.just@ephox.com>
2019-08-17 14:27:57 +1000

    Default the timeout values in Waiter.

--- modules/agar/src/main/ts/ephox/agar/api/Waiter.ts
+++ modules/agar/src/main/ts/ephox/agar/api/Waiter.ts
@@ -1,24 +1,24 @@
 import * as Guard from './Guard';
 import { Step } from './Step';
 
-const sTryUntilPredicate = function <T>(label: string, predicate: (value: T) => boolean, interval: number, amount: number) {
+const sTryUntilPredicate = function <T>(label: string, predicate: (value: T) => boolean, interval: number = 10, amount: number = 1000) {
   const guard = Guard.tryUntil<T, T>(label, interval, amount);
   return Step.control(Step.stateful<T, T>((value: T, next, die) => {
     predicate(value) ? next(value) : die('predicate did not succeed');
   }), guard);
 };
 
-const sTryUntil = function <T, U>(label: string, step: Step<T, U>, interval: number, amount: number) {
+const sTryUntil = function <T, U>(label: string, step: Step<T, U>, interval: number = 10, amount: number = 1000) {
   const guard = Guard.tryUntil<T, U>(label, interval, amount);
   return Step.control(step, guard);
 };
 
-const sTryUntilNot = function <T, U>(label: string, step: Step<T, U>, interval: number, amount: number) {
+const sTryUntilNot = function <T, U>(label: string, step: Step<T, U>, interval: number = 10, amount: number = 1000) {
   const guard = Guard.tryUntilNot<T, U>(label, interval, amount);
   return Step.control(step, guard);
 };
 
-const sTimeout = function <T, U>(label: string, step: Step<T, U>, limit: number) {
+const sTimeout = function <T, U>(label: string, step: Step<T, U>, limit: number = 1000) {
   const guard = Guard.timeout<T, U>(label, limit);
   return Step.control(step, guard);
 };
