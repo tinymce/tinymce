ae2473ef53fec6b0556dbd6e932e730b9079d26c
Dylan Just <dylan.just@ephox.com>
2019-09-11 12:56:05 +1000

    Fix types to work when we remove the "= any" default on Option.none

--- modules/tinymce/src/themes/mobile/main/ts/ios/view/Devices.ts
+++ modules/tinymce/src/themes/mobile/main/ts/ios/view/Devices.ts
@@ -5,7 +5,7 @@
  * For commercial licenses see https://www.tiny.cloud/
  */
 
-import { Option, Options } from '@ephox/katamari';
+import { Options } from '@ephox/katamari';
 
 /*
 
@@ -43,8 +43,19 @@ landscape : 459
 
 */
 
-const findDevice = function (deviceWidth, deviceHeight) {
-  const devices = [
+export interface Keyboard {
+  portrait: number;
+  landscape: number;
+}
+
+export interface Device {
+  width: number;
+  height: number;
+  keyboard: Keyboard;
+}
+
+export const findDevice = function (deviceWidth, deviceHeight): Keyboard {
+  const devices: Device[] = [
     // iPhone 4 class
     { width: 320, height: 480, keyboard: { portrait: 300, landscape: 240 } },
     // iPhone 5 class
@@ -59,13 +70,7 @@ const findDevice = function (deviceWidth, deviceHeight) {
     { width: 1024, height: 1366, keyboard: { portrait: 380, landscape: 460 } }
   ];
 
-  return Options.findMap(devices, function (device) {
-    return deviceWidth <= device.width && deviceHeight <= device.height ?
-        Option.some(device.keyboard) :
-        Option.none();
-  }).getOr({ portrait: deviceHeight / 5, landscape: deviceWidth / 4 });
-};
-
-export default {
-  findDevice
+  return Options.findMap<Device, Keyboard>(devices, (device) =>
+    Options.someIf(deviceWidth <= device.width && deviceHeight <= device.height, device.keyboard)
+  ).getOr({ portrait: deviceHeight / 5, landscape: deviceWidth / 4 });
 };
