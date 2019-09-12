10d3f6231bd4463e74ac8f1b72be699451adb681
Dylan Just <dylan.just@ephox.com>
2019-09-12 10:01:31 +1000

    Numbers are faster.

--- modules/katamari/src/test/ts/atomic/api/data/OptionsSomeIfTest.ts
+++ modules/katamari/src/test/ts/atomic/api/data/OptionsSomeIfTest.ts
@@ -3,6 +3,6 @@ import Jsc from '@ephox/wrap-jsverify';
 import { UnitTest, assert } from '@ephox/bedrock';
 
 UnitTest.test('OptionSomeIfTest', () => {
-  Jsc.property('false -> none', 'string', (s: string) => Jsc.eq(true, Options.someIf<string>(false, s).isNone()));
-  Jsc.property('true -> some', 'string', (s: string) => Jsc.eq(s, Options.someIf<string>(true, s).getOrDie()));
+  Jsc.property('false -> none', 'number', (n: number) => Jsc.eq(true, Options.someIf<number>(false, n).isNone()));
+  Jsc.property('true -> some', 'number', (n: number) => Jsc.eq(n, Options.someIf<number>(true, n).getOrDie()));
 });
