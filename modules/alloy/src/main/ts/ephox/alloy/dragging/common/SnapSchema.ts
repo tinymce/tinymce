efb02535ec4d3c21122d1a9292b2ea23a767cdb4
Joshua Haines <josh.j.haines@gmail.com>
2019-09-19 11:11:53 +1000

    TINY-4078: remove duplicate code

--- modules/alloy/src/main/ts/ephox/alloy/dragging/common/SnapSchema.ts
+++ modules/alloy/src/main/ts/ephox/alloy/dragging/common/SnapSchema.ts
@@ -1,18 +1,12 @@
 import { FieldSchema } from '@ephox/boulder';
-import { window } from '@ephox/dom-globals';
-import { VisualViewport } from '@ephox/sugar';
 
 import * as Fields from '../../data/Fields';
-import { Bounds } from '../../alien/Boxes';
-
-const defaultLazyViewport = (): Bounds => {
-  return VisualViewport.getBounds(window);
-};
+import * as Boxes from '../../alien/Boxes';
 
 export default FieldSchema.optionObjOf('snaps', [
   FieldSchema.strict('getSnapPoints'),
   Fields.onHandler('onSensor'),
   FieldSchema.strict('leftAttr'),
   FieldSchema.strict('topAttr'),
-  FieldSchema.defaulted('lazyViewport', defaultLazyViewport)
+  FieldSchema.defaulted('lazyViewport', Boxes.win)
 ]);
