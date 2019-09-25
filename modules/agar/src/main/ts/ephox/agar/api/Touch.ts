4ecb68e493a38d23abcbb10a1d82b32cf7319112
Lee Newson <lee.newson@tiny.cloud>
2019-09-25 10:10:33 +1000

    TINY-4181: Added new touch module to agar to simulate touch events

---
+++ modules/agar/src/main/ts/ephox/agar/api/Touch.ts
@@ -0,0 +1,86 @@
+import { Element, Focus } from '@ephox/sugar';
+import { Step, TestLogs } from '@ephox/agar';
+
+import { DieFn, NextFn } from '../pipe/Pipe';
+import * as Touches from '../touch/Touches';
+import { Chain, Wrap } from './Chain';
+import * as UiFinder from './UiFinder';
+
+const triggerWith = <T>(container: Element, selector: string, action: (ele: Element) => void) => {
+  return Step.async<T>((next, die) => {
+    const element = UiFinder.findIn(container, selector);
+    element.fold(() => {
+      die(new Error('Could not find element: ' + selector));
+    }, (elem) => {
+      action(elem);
+      next();
+    });
+  });
+};
+
+const trueTap = (elem: Element) => {
+  // The closest event queue to a true tap event
+  Focus.focus(elem);
+  Touches.touchstart(elem);
+  Touches.touchend(elem);
+};
+
+const tap = (elem: Element) => {
+  Touches.touchstart(elem);
+  Touches.touchend(elem);
+};
+
+const sTap = (element: Element) => Step.sync(() => tap(element));
+
+const sTrueTapOn = <T>(container: Element, selector: string) => {
+  return triggerWith<T>(container, selector, trueTap);
+};
+
+const sTapOn = <T>(container: Element, selector: string) => {
+  return triggerWith<T>(container, selector, tap);
+};
+
+const cTapOn = (selector: string): Chain<Element, Element> => {
+  return Chain.on((container: Element, next: NextFn<Wrap<Element>>, die: DieFn, initLogs: TestLogs) => {
+    triggerWith(container, selector, tap)({}, (v, newLogs) => {
+      next(Chain.wrap(container), newLogs);
+    }, (err, newLogs) => die(err, newLogs), initLogs);
+  });
+};
+
+const cTouchStartAt = function (dx: number, dy: number) {
+  return Chain.op(Touches.touchstartAt(dx, dy));
+};
+
+const cTouchEndAt = function (dx: number, dy: number) {
+  return Chain.op(Touches.touchendAt(dx, dy));
+};
+
+const cTouchMoveTo = function (dx: number, dy: number) {
+  return Chain.op(Touches.touchmoveTo(dx, dy));
+};
+
+const point = Touches.point;
+
+const cTrueTap = Chain.op(trueTap);
+const cTap = Chain.op(tap);
+const cTouchMove = Chain.op(Touches.touchmove);
+const cTouchStart = Chain.op(Touches.touchstart);
+const cTouchEnd = Chain.op(Touches.touchend);
+
+export {
+  point,
+
+  sTap,
+  sTapOn,
+  sTrueTapOn,
+
+  cTap,
+  cTapOn,
+  cTouchStart,
+  cTouchStartAt,
+  cTouchMove,
+  cTouchMoveTo,
+  cTouchEnd,
+  cTouchEndAt
+};
---
+++ modules/agar/src/main/ts/ephox/agar/touch/Touches.ts
@@ -0,0 +1,84 @@
+import { Document, Touch, TouchEvent, UIEvent, window } from '@ephox/dom-globals';
+import { Element, Location } from '@ephox/sugar';
+
+const point = (type: string, element: Element, x: number, y: number) => {
+  const touch = {
+    identifier: Date.now(),
+    target: element.dom(),
+    clientX: x,
+    clientY: y,
+    pageX: x,
+    pageY: y,
+    radiusX: 2.5,
+    radiusY: 2.5,
+    rotationAngle: 10,
+    force: 0.5,
+  };
+
+  // Adapted from https://stackoverflow.com/a/42447620/11275515
+  if (typeof TouchEvent === 'function' && typeof Touch === 'function') {
+    // @ts-ignore
+    const touchAction = new Touch(touch);
+    const ev: TouchEvent = new TouchEvent(type, {
+      cancelable: true,
+      bubbles: true,
+      view: window,
+      touches: [ touchAction ],
+      targetTouches: [],
+      changedTouches: [ touchAction ]
+    });
+    element.dom().dispatchEvent(ev);
+  } else {
+    // No native touch event support, so we need to simulate with a UIEvent
+    let ev: any;
+    if (typeof UIEvent === 'function') {
+      ev = new UIEvent(type, {
+        cancelable: true,
+        bubbles: true,
+        view: window
+      } as any);
+    } else {
+      // IE 11 doesn't support the UIEvent constructor, so we need to fallback to using createEvent
+      ev = (<Document> element.dom().ownerDocument).createEvent('UIEvent');
+      ev.initUIEvent(type, true, true, window, null);
+    }
+    // Patch in the touch properties
+    ev.touches = [ touch ];
+    ev.targetTouches = [];
+    ev.changedTouches = [ touch ];
+    element.dom().dispatchEvent(ev);
+  }
+};
+
+const touch = (eventType: string) => {
+  return (element: Element) => {
+    const position = Location.absolute(element);
+    point(eventType, element, position.left(), position.top());
+  };
+};
+
+const touchAt = (eventType: string) => {
+  return (dx: number, dy: number) => {
+    return (element: Element) => {
+      const position = Location.absolute(element);
+      point(eventType, element, position.left() + dx, position.top() + dy);
+    };
+  };
+};
+
+const touchstart = touch('touchstart');
+const touchstartAt = touchAt('touchstart');
+const touchend = touch('touchend');
+const touchendAt = touchAt('touchend');
+const touchmove = touch('touchmove');
+const touchmoveTo = touchAt('touchmove');
+
+export {
+  touchstart,
+  touchstartAt,
+  touchend,
+  touchendAt,
+  touchmove,
+  touchmoveTo,
+  point
+};
