a324f26273c67415e630253297939ce4bf46d9f4
Millie Macdonald <millie.macdonald@ephox.com>
2019-09-26 13:48:25 +1000

    TINY-4107 Rotate submenu caret

--- modules/tinymce/src/themes/silver/main/ts/ui/menus/item/build/NestedMenuItem.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/menus/item/build/NestedMenuItem.ts
@@ -3,13 +3,13 @@ import { Menu } from '@ephox/bridge';
 import { Fun, Option } from '@ephox/katamari';
 import { UiFactoryBackstageProviders } from 'tinymce/themes/silver/backstage/Backstage';
 import ItemResponse from '../ItemResponse';
-import { renderSubmenuCaret } from '../structure/ItemSlices';
+import { renderSubmenuCaret, renderDownwardsCaret } from '../structure/ItemSlices';
 import { renderItemStructure } from '../structure/ItemStructure';
 import { buildData, renderCommonItem } from './CommonMenuItem';
 
 // Note, this does not create a valid SketchSpec.
 const renderNestedItem = (spec: Menu.NestedMenuItem, itemResponse: ItemResponse, providersBackstage: UiFactoryBackstageProviders, renderIcons: boolean = true, isHorizontalMenu: boolean): ItemTypes.ItemSpec => {
-  const caret = renderSubmenuCaret(providersBackstage.icons);
+  const caret = isHorizontalMenu ? renderDownwardsCaret(providersBackstage.icons) : renderSubmenuCaret(providersBackstage.icons);
   const getApi = (component: AlloyComponent): Menu.NestedMenuItemInstanceApi => {
     return {
       isDisabled: () => Disabling.isDisabled(component),
