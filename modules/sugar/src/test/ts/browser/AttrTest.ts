bd82e80ed48e922aad1480a20eb76b81a56ea9ff
Dylan Just <dylan.just@ephox.com>
2019-10-04 18:52:16 +1000

    Converting tests to new Assert.

--- modules/sugar/src/test/ts/browser/AttrTest.ts
+++ modules/sugar/src/test/ts/browser/AttrTest.ts
@@ -1,8 +1,13 @@
 import * as Attr from 'ephox/sugar/api/properties/Attr';
 import Element from 'ephox/sugar/api/node/Element';
 import Div from 'ephox/sugar/test/Div';
-import { UnitTest, assert } from '@ephox/bedrock';
+import { UnitTest, Assert } from '@ephox/bedrock';
 import { Node, Comment, HTMLDivElement, HTMLSpanElement } from '@ephox/dom-globals';
+import { OptionInstances, Option } from '@ephox/katamari';
+import { Testable } from '@ephox/dispute';
+
+const { tOption } = OptionInstances;
+const { tString } = Testable;
 
 UnitTest.test('AttrTest', function () {
   const c = Div();
@@ -14,7 +19,7 @@ UnitTest.test('AttrTest', function () {
       // expected
       return;
     }
-    assert.fail('function did not throw an error');
+    Assert.fail('function did not throw an error');
   };
 
   const checkTypeErr = function (e) {
@@ -24,27 +29,28 @@ UnitTest.test('AttrTest', function () {
     checkErr(Attr.remove, e, 'id');
 
     // has just returns false now, no point in errors
-    assert.eq(false, Attr.has(e, 'id'));
+    Assert.eq('does not have key', false, Attr.has(e, 'id'));
   };
 
   const check = function (k, v1, v2) {
-    assert.eq(false, Attr.has(c, k));
-    assert.eq(undefined, Attr.get(c, k));
-    assert.eq(true, Attr.getOpt(c, k).isNone());
+    Assert.eq('has', false, Attr.has(c, k));
+    Assert.eq('get', undefined, Attr.get(c, k));
+    Assert.eq('getOpt', Option.none(), Attr.getOpt(c, k), tOption());
+
     Attr.set(c, k, v1);
-    assert.eq(true, Attr.has(c, k));
-    assert.eq(v1, Attr.get(c, k));
-    assert.eq(true, Attr.getOpt(c, k).isSome());
-    assert.eq(v1, Attr.getOpt(c, k).getOr('X'));
+    Assert.eq('has', true, Attr.has(c, k));
+    Assert.eq('get', v1, Attr.get(c, k));
+    Assert.eq('getOpt', Option.some(v1), Attr.getOpt(c, k), tOption());
+
     Attr.set(c, k, v2);
-    assert.eq(true, Attr.has(c, k));
-    assert.eq(v2, Attr.get(c, k));
-    assert.eq(true, Attr.getOpt(c, k).isSome());
-    assert.eq(v2, Attr.getOpt(c, k).getOr('X'));
+    Assert.eq('has', true, Attr.has(c, k));
+    Assert.eq('get', v2, Attr.get(c, k));
+    Assert.eq('getOpt', Option.some(v2), Attr.getOpt(c, k), tOption());
+
     Attr.remove(c, k);
-    assert.eq(false, Attr.has(c, k));
-    assert.eq(undefined, Attr.get(c, k));
-    assert.eq(true, Attr.getOpt(c, k).isNone());
+    Assert.eq('has', false, Attr.has(c, k));
+    Assert.eq('get', undefined, Attr.get(c, k));
+    Assert.eq('getOpt', Option.none(), Attr.getOpt(c, k), tOption());
   };
 
   // setting a non-simple value
@@ -59,21 +65,21 @@ UnitTest.test('AttrTest', function () {
 
   check('name', 'black', 'blue');
 
-  assert.eq(true, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div></div>')));
-  assert.eq(true, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div>Dog</div>')));
-  assert.eq(true, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div><span id="cat"></span></div>')));
-  assert.eq(false, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div name="container"><span id="cat"></span></div>')));
+  Assert.eq('hasNone', true, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div></div>')));
+  Assert.eq('hasNone', true, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div>Dog</div>')));
+  Assert.eq('hasNone', true, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div><span id="cat"></span></div>')));
+  Assert.eq('hasNone', false, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div name="container"><span id="cat"></span></div>')));
 
   /*
    * Note: IE returns true for an empty style attribute.
-   * assert.eq(false, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div style=""><span id="cat"></span></div>')));
+   * Assert.eq(false, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div style=""><span id="cat"></span></div>')));
    *
    */
-  assert.eq(false, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div style="display: block;"><span id="cat"></span></div>')));
+  Assert.eq('hasNone', false, Attr.hasNone(Element.fromHtml<HTMLDivElement>('<div style="display: block;"><span id="cat"></span></div>')));
 
-  assert.eq({id: 'cat'}, Attr.clone(Element.fromHtml<HTMLSpanElement>('<span id="cat"></span>')));
-  assert.eq({'name': 'foo', 'data-ephox-foo': 'bar'}, Attr.clone(Element.fromHtml<HTMLSpanElement>('<span name="foo" data-ephox-foo="bar"></span>')));
+  Assert.eq('clone', {id: 'cat'}, Attr.clone(Element.fromHtml<HTMLSpanElement>('<span id="cat"></span>')));
+  Assert.eq('clone', {'name': 'foo', 'data-ephox-foo': 'bar'}, Attr.clone(Element.fromHtml<HTMLSpanElement>('<span name="foo" data-ephox-foo="bar"></span>')));
 
   Attr.set(c, 'tabindex', -1);
-  assert.eq('-1', Attr.get(c, 'tabindex'));
+  Assert.eq('get', '-1', Attr.get(c, 'tabindex'));
 });
