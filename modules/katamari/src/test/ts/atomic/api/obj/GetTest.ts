b908395729a96559ce25ab4392b1063caa42beb8
Dylan Just <dylan.just@ephox.com>
2019-10-04 15:06:57 +1000

    Adopt new Bedrock assertions and Dispute library. Add Option instances.

--- modules/katamari/src/test/ts/atomic/api/obj/GetTest.ts
+++ modules/katamari/src/test/ts/atomic/api/obj/GetTest.ts
@@ -1,9 +1,40 @@
 import * as Obj from 'ephox/katamari/api/Obj';
-import { UnitTest, assert } from '@ephox/bedrock';
+import { Option } from 'ephox/katamari/api/Option';
+import { tOption } from 'ephox/katamari/api/OptionInstances';
+import { UnitTest, Assert } from '@ephox/bedrock';
+import { Testable } from '@ephox/dispute';
+
+const { tArray, tNumber } = Testable;
 
 UnitTest.test('GetTest', function () {
-  assert.eq(1, Obj.get({ a: 1 }, 'a').getOrUndefined());
-  assert.eq(false, Obj.get({ a: null }, 'a').isSome());
-  assert.eq(false, Obj.get({ a: undefined }, 'a').isSome());
-  assert.eq(undefined, Obj.get(<any> { a: 1 }, 'b').getOrUndefined());
+  Assert.eq(
+    'Key exists',
+    Option.some(3),
+    Obj.get({ a: 3 }, 'a'),
+    tOption(tNumber)
+  );
+  Assert.eq(
+    'Key with null value does not exist',
+    Option.none(),
+    Obj.get({ a: null }, 'a'),
+    tOption()
+  );
+  Assert.eq(
+    'Key with undefined value does not exist',
+    Option.none(),
+    Obj.get({ a: undefined }, 'a'),
+    tOption()
+  );
+  Assert.eq(
+    'Unknown key does not exist',
+    Option.none(),
+    Obj.get(<any> { a: 1 }, 'b'),
+    tOption()
+  );
+  Assert.eq(
+    'array option array number',
+    [Option.none<Array<number>>(), Option.some([1, 8, 3, 9]), Option.some([8, 9])],
+    [Option.none<Array<number>>(), Option.some([1, 8, 3, 9]), Option.some([8, 9])],
+    tArray(tOption(tArray(tNumber)))
+  );
 });
