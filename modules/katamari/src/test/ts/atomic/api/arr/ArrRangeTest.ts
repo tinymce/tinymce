f78bb8b2b9fca33ef8c246a1c683637294fecca5
Dylan Just <dylan.just@ephox.com>
2019-10-25 22:18:35 +1000

    Port ArrRangeTest to fast-check.

--- modules/katamari/src/test/ts/atomic/api/arr/ArrRangeTest.ts
+++ modules/katamari/src/test/ts/atomic/api/arr/ArrRangeTest.ts
@@ -1,38 +1,26 @@
 import * as Arr from 'ephox/katamari/api/Arr';
 import * as Fun from 'ephox/katamari/api/Fun';
-import Jsc from '@ephox/wrap-jsverify';
-import { UnitTest, assert } from '@ephox/bedrock-client';
+import { UnitTest, Assert } from '@ephox/bedrock-client';
+import * as fc from 'fast-check';
+import { Testable as T } from '@ephox/dispute';
 
-UnitTest.test('ArrRangeTest', function () {
-  const check = function (expected, input, f) {
+UnitTest.test('Arr.range: unit tests', () => {
+  const check = (expected, input, f) => {
     const actual = Arr.range(input, f);
-    assert.eq(expected, actual);
+    Assert.eq('range', expected, actual, T.tArray(T.tNumber));
   };
 
-  check([ ], 0, Fun.constant(10));
-  check([ 10 ], 1, Fun.constant(10));
-  check([ 10, 20, 30 ], 3, function (x) { return 10 * (x + 1); });
+  check([], 0, Fun.constant(10));
+  check([10], 1, Fun.constant(10));
+  check([10, 20, 30], 3, (x) => 10 * (x + 1));
+});
 
-  Jsc.property(
-    'Range(num, Fun.identity) should Arr.forall(x === index) && length === num',
-    Jsc.nat,
-    function (num) {
+UnitTest.test('Arr.range: property tests', () => {
+  fc.assert(fc.property(
+    fc.nat(30),
+    (num) => {
       const range = Arr.range(num, Fun.identity);
-      return Arr.forall(range, function (x, i) {
-        return x === i;
-      }) && range.length === num;
-    }
-  );
-
-  Jsc.property(
-    'Range(num, Fun.constant(c)) should Arr.forall(x === c) && length === num',
-    Jsc.nat,
-    Jsc.json,
-    function (num, c) {
-      const range = Arr.range(num, Fun.constant(c));
-      return Arr.forall(range, function (x, i) {
-        return x === c;
-      }) && range.length === num;
-    }
-  );
+      Assert.eq('length', range.length, num, T.tNumber);
+      return Arr.forall(range, (x, i) => x === i);
+    }));
 });
