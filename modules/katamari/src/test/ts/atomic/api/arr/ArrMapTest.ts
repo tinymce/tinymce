d9bb5c4efc0881c98c5d538d058c66db27799515
Dylan Just <dylan.just@ephox.com>
2019-10-25 12:51:58 +1000

    Use new Assert. Refactor.

--- modules/katamari/src/test/ts/atomic/api/arr/ArrMapTest.ts
+++ modules/katamari/src/test/ts/atomic/api/arr/ArrMapTest.ts
@@ -1,8 +1,8 @@
 import * as Arr from 'ephox/katamari/api/Arr';
 import * as Fun from 'ephox/katamari/api/Fun';
-import { UnitTest, assert } from '@ephox/bedrock-client';
+import { Assert, UnitTest } from '@ephox/bedrock-client';
 import * as fc from 'fast-check';
-import { Eq } from '@ephox/dispute';
+import { Testable as T } from '@ephox/dispute';
 
 const dbl = (x) => x * 2;
 
@@ -10,13 +10,9 @@ const plus3 = (x) => x + 3;
 
 UnitTest.test('Arr.map: unit tests', () => {
 
-  const check = (expected, C, input, f) => {
-    assert.eq(expected, C.map(input, f));
-  };
-
   const checkA = (expected, input, f) => {
-    check(expected, Arr, input, f);
-    check(expected, Arr, Object.freeze(input.slice()), f);
+    Assert.eq('map', expected, Arr.map(input, f));
+    Assert.eq('map frozen', expected, Arr.map(Object.freeze(input.slice()), f));
   };
 
   checkA([], [], dbl);
@@ -24,23 +20,25 @@ UnitTest.test('Arr.map: unit tests', () => {
   checkA([4, 6, 10], [2, 3, 5], dbl);
 });
 
-UnitTest.test('Arr.map: map id xs = xs', () => {
+UnitTest.test('Arr.map: functor laws', () => {
   fc.assert(fc.property(fc.array(fc.nat()), (xs) =>
-    Eq.eqArray(Eq.eqNumber).eq(
+    Assert.eq(
+      'map id = id',
+      Fun.identity(xs),
       Arr.map(xs, Fun.identity),
-      xs
+      T.tArray(T.tNumber)
     )
   ));
-});
 
-UnitTest.test('Arr.map: map f . g = map f . map g', () => {
   const f = plus3;
   const g = dbl;
 
   fc.assert(fc.property(fc.array(fc.nat()), (xs) =>
-    Eq.eqArray(Eq.eqNumber).eq(
+    Assert.eq(
+      'map (f . g) = map f . map g',
+      Arr.map(xs, Fun.compose(f, g)),
       Arr.map(Arr.map(xs, g), f),
-      Arr.map(xs, Fun.compose(f, g))
+      T.tArray(T.tNumber)
     )
   ));
 });
