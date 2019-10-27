271dd88a09b65c7947a64d6f575847aedd0b5529
Dylan Just <dylan.just@ephox.com>
2019-10-27 21:06:53 +1000

    Test failed on NaN, so just use an integer.

--- modules/katamari/src/test/ts/atomic/api/option/OptionGetOrTest.ts
+++ modules/katamari/src/test/ts/atomic/api/option/OptionGetOrTest.ts
@@ -4,11 +4,11 @@ import { Assert, UnitTest } from '@ephox/bedrock-client';
 import fc from 'fast-check';
 
 UnitTest.test('Option.getOr', () => {
-  fc.assert(fc.property(fc.anything(), (x) => {
+  fc.assert(fc.property(fc.integer(), (x) => {
     Assert.eq('none', x, Option.none().getOr(x));
     Assert.eq('none', x, Option.none().getOrThunk(() => x));
   }));
-  fc.assert(fc.property(fc.anything(), fc.anything(), (x, y) => {
+  fc.assert(fc.property(fc.integer(), fc.integer(), (x, y) => {
     Assert.eq('some', x, Option.some(x).getOr(y));
     Assert.eq('some', x, Option.some(x).getOrThunk(Fun.die('boom')));
   }));
