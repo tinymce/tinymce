b1b597c2b9ba5d483cf7689c7c7e02345685179d
Dylan Just <dylan.just@ephox.com>
2019-10-27 16:17:48 +1000

    Inductive case for Obj.size

--- modules/katamari/src/test/ts/atomic/api/arr/SizeTest.ts
+++ modules/katamari/src/test/ts/atomic/api/arr/SizeTest.ts
@@ -1,7 +1,8 @@
 import * as Obj from 'ephox/katamari/api/Obj';
-import { UnitTest, assert } from '@ephox/bedrock-client';
+import { UnitTest, assert, Assert } from '@ephox/bedrock-client';
+import fc from 'fast-check';
 
-UnitTest.test('SizeTest', function () {
+UnitTest.test('Obj.size: unit tests', function () {
   const check = function (expected, input) {
     assert.eq(expected, Obj.size(input));
   };
@@ -9,6 +10,18 @@ UnitTest.test('SizeTest', function () {
   check(0, {});
   check(1, { a: 'a' });
   check(3, { a: 'a', b: 'b', c: 'c' });
+});
 
-  // INVESTIGATE: Find some way to use Jsc that isn't just re-implementing
+UnitTest.test('Obj.size: inductive case', () => {
+  fc.assert(fc.property(
+    fc.dictionary(fc.asciiString(), fc.integer()),
+    fc.asciiString(),
+    fc.integer(),
+    (obj, k, v) => {
+    const objWithoutK = Obj.filter(obj, (x, i) => i !== k);
+    Assert.eq('Adding key adds 1 to size',
+      Obj.size(objWithoutK) + 1,
+      Obj.size({k: v, ...objWithoutK})
+    );
+  }));
 });
