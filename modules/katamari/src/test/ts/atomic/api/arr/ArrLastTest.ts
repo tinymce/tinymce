18502818f012d8edbcb5a3d75969d14310b25743
Dylan Just <dylan.just@ephox.com>
2019-11-01 10:36:05 +1000

    Arrow functions.

--- modules/katamari/src/test/ts/atomic/api/arr/ArrLastTest.ts
+++ modules/katamari/src/test/ts/atomic/api/arr/ArrLastTest.ts
@@ -5,11 +5,11 @@ import { tOption } from 'ephox/katamari/api/OptionInstances';
 import { Testable as T } from '@ephox/dispute';
 import fc from 'fast-check';
 
-UnitTest.test('Arr.last: empty', function () {
+UnitTest.test('Arr.last: empty', () => {
   Assert.eq('empty', Option.none<number>(), Arr.last<number>([]), tOption(T.tNumber));
 });
 
-UnitTest.test('Arr.last: nonEmpty', function () {
+UnitTest.test('Arr.last: nonEmpty', () => {
   fc.assert(fc.property(fc.array(fc.integer()), fc.integer(), (init, last) => {
     const arr = init.concat([last]);
     Assert.eq('nonEmpty', Option.some(last), Arr.last(arr), tOption(T.tNumber));
