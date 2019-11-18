9db5ca002d4c527faa22ac77327edd1b81f8d7bc
Joshua Haines <josh.j.haines@gmail.com>
2019-11-18 19:03:53 +1000

    move other table items as well

--- modules/tinymce/src/plugins/table/main/ts/ui/Buttons.ts
+++ modules/tinymce/src/plugins/table/main/ts/ui/Buttons.ts
@@ -14,7 +14,7 @@ const addButtons = (editor: Editor, selectionTargets: SelectionTargets) => {
   editor.ui.registry.addMenuButton('table', {
     tooltip: 'Table',
     icon: 'table',
-    fetch: (callback) => callback('inserttable tableprops deletetable | cell row column advtablesort')
+    fetch: (callback) => callback('inserttable advtablesort tableprops deletetable | cell row column')
   });
 
   const cmd = (command) => () => editor.execCommand(command);
--- modules/tinymce/src/themes/silver/main/ts/ui/menus/menubar/Integration.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/menus/menubar/Integration.ts
@@ -25,7 +25,7 @@ const defaultMenus = {
   insert: { title: 'Insert', items: 'image link media addcomment pageembed template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor toc | insertdatetime' },
   format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | formats blockformats fontformats fontsizes align | forecolor backcolor | removeformat' },
   tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | a11ycheck code wordcount' },
-  table: { title: 'Table', items: 'inserttable tableprops deletetable row column cell advtablesort' },
+  table: { title: 'Table', items: 'inserttable advtablesort tableprops deletetable row column cell' },
   help: { title: 'Help', items: 'help' }
 };
 
