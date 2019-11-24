272a4c56ed6abf2de59fa29becdaaf7e0260ffce
Dylan Just <>
2019-11-25 07:34:17 +1000

    Add tests for KAssert

--- modules/katamari-assertions/src/test/ts/atomic/api/KAssertTest.ts
+++ modules/katamari-assertions/src/test/ts/atomic/api/KAssertTest.ts
@@ -8,6 +8,8 @@ const { tNumber } = Testable;
 
 const tBoom = <T> () => Testable.testable(Eq.eq(Fun.die('⊥')), Pprint.pprint(Fun.die('⊥')));
 
+const twoDifferentNumbers = fc.tuple(fc.integer(), fc.integer()).filter(([a, b]) => a !== b);
+
 UnitTest.test('KAssert.eqError: success', () => {
   fc.assert(fc.property(fc.integer(), (i) => {
     KAssert.eqError('eq', i, Result.error(i));
@@ -17,27 +19,27 @@ UnitTest.test('KAssert.eqError: success', () => {
 });
 
 UnitTest.test('KAssert.eqError: failure', () => {
-  fc.assert(fc.property(fc.integer(), (i) => {
+  fc.assert(fc.property(twoDifferentNumbers, ([a, b]) => {
     Assert.throws('should throw if numbers differ #1', () => {
-      KAssert.eqError('eq', i + 1, Result.error(i));
+      KAssert.eqError('eq', a, Result.error(b));
     });
     Assert.throws('should throw if numbers differ #2', () => {
-      KAssert.eqError('eq', i + 1, Result.error(i), tBoom());
+      KAssert.eqError('eq', a, Result.error(b), tBoom());
     });
     Assert.throws('should throw if numbers differ #2', () => {
-      KAssert.eqError('eq', i + 1, Result.error(i), tBoom(), tNumber);
+      KAssert.eqError('eq', a, Result.error(b), tBoom(), tNumber);
     });
   }));
 
   fc.assert(fc.property(fc.integer(), fc.string(), (i, s) => {
     Assert.throws('should throw if value #1', () => {
-      KAssert.eqError('eq', i + 1, Result.value(s));
+      KAssert.eqError('eq', i, Result.value(s));
     });
     Assert.throws('should throw if value #2', () => {
-      KAssert.eqError('eq', i + 1, Result.value(s), tBoom());
+      KAssert.eqError('eq', i, Result.value(s), tBoom());
     });
     Assert.throws('should throw if value #3', () => {
-      KAssert.eqError('eq', i + 1, Result.value(s), tBoom(), tBoom());
+      KAssert.eqError('eq', i, Result.value(s), tBoom(), tBoom());
     });
   }));
 });
@@ -51,27 +53,27 @@ UnitTest.test('KAssert.eqValue: success', () => {
 });
 
 UnitTest.test('KAssert.eqValue: failure', () => {
-  fc.assert(fc.property(fc.integer(), (i) => {
+  fc.assert(fc.property(twoDifferentNumbers, ([a, b]) => {
     Assert.throws('should throw if numbers differ #1', () => {
-      KAssert.eqValue('eq', i + 1, Result.value(i));
+      KAssert.eqValue('eq', a, Result.value(b));
     });
     Assert.throws('should throw if numbers differ #2', () => {
-      KAssert.eqValue('eq', i + 1, Result.value(i), tNumber);
+      KAssert.eqValue('eq', a, Result.value(b), tNumber);
     });
     Assert.throws('should throw if numbers differ #2', () => {
-      KAssert.eqValue('eq', i + 1, Result.value(i), tNumber, tBoom());
+      KAssert.eqValue('eq', a, Result.value(b), tNumber, tBoom());
     });
   }));
 
   fc.assert(fc.property(fc.integer(), fc.string(), (i, s) => {
     Assert.throws('should throw if error #1', () => {
-      KAssert.eqValue('eq', i + 1, Result.error(s));
+      KAssert.eqValue('eq', i, Result.error(s));
     });
     Assert.throws('should throw if error #2', () => {
-      KAssert.eqValue('eq', i + 1, Result.error(s), tBoom());
+      KAssert.eqValue('eq', i, Result.error(s), tBoom());
     });
     Assert.throws('should throw if error #3', () => {
-      KAssert.eqValue('eq', i + 1, Result.error(s), tBoom(), tBoom());
+      KAssert.eqValue('eq', i, Result.error(s), tBoom(), tBoom());
     });
   }));
 });
@@ -81,12 +83,58 @@ UnitTest.test('KAssert.eqResult: success', () => {
     KAssert.eqResult('eq', Result.value(i), Result.value(i));
     KAssert.eqResult('eq', Result.value(i), Result.value(i), tNumber);
     KAssert.eqResult('eq', Result.value(i), Result.value(i), tNumber, tBoom());
-
     KAssert.eqResult('eq', Result.error(i), Result.error(i));
     KAssert.eqResult('eq', Result.error(i), Result.error(i), tBoom());
     KAssert.eqResult('eq', Result.error(i), Result.error(i), tBoom(), tNumber);
   }));
 });
 
-// TODO: KAssert.eqResult: fail
+UnitTest.test('KAssert.eqResult: fail', () => {
+  fc.assert(fc.property(twoDifferentNumbers, ([a, b]) => {
+    Assert.throws('value(a) != (value(!a)) #1', () => {
+      KAssert.eqResult('eq', Result.value(a), Result.value(b));
+    });
+    Assert.throws('value(a) != (value(!a)) #2', () => {
+      KAssert.eqResult('eq', Result.value(a), Result.value(b), tNumber);
+    });
+    Assert.throws('value(a) != (value(!a)) #2', () => {
+      KAssert.eqResult('eq', Result.value(a), Result.value(b), tNumber, tBoom());
+    });
+  }));
+
+  fc.assert(fc.property(twoDifferentNumbers, ([a, b]) => {
+    Assert.throws('error(a) != (error(!a)) #1', () => {
+      KAssert.eqResult('eq', Result.error(a), Result.error(b));
+    });
+    Assert.throws('error(a) != (error(!a)) #2', () => {
+      KAssert.eqResult('eq', Result.error(a), Result.error(b), tBoom());
+    });
+    Assert.throws('result(a) != (result(!a)) #2', () => {
+      KAssert.eqResult('eq', Result.error(a), Result.error(b), tBoom(), tNumber);
+    });
+  }));
+
+  fc.assert(fc.property(fc.integer(), fc.string(), (i, s) => {
+    Assert.throws('value != error #1', () => {
+      KAssert.eqResult('eq', Result.value(i), Result.error(s));
+    });
+    Assert.throws('value != error #2', () => {
+      KAssert.eqResult('eq', Result.value(i), Result.error(s), tBoom());
+    });
+    Assert.throws('value != error #3', () => {
+      KAssert.eqResult('eq', Result.value(i), Result.error(s), tBoom());
+    });
+
+    Assert.throws('error != value #1', () => {
+      KAssert.eqResult('eq', Result.error(i), Result.value(s));
+    });
+    Assert.throws('error != value #2', () => {
+      KAssert.eqResult('eq', Result.error(i), Result.value(s), tBoom());
+    });
+    Assert.throws('error != value #3', () => {
+      KAssert.eqResult('eq', Result.error(i), Result.value(s), tBoom(), tBoom());
+    });
+  }));
+});
+
 // TODO: KAssert.eqOption, eqSome, eqNone
