9946527bbde876eff3880baaebd4820675e5d83b
Dylan Just <>
2019-11-24 18:54:09 +1000

    Rewrite Num.cap in terms of Math.min/max

--- modules/katamari/src/main/ts/ephox/katamari/api/Num.ts
+++ modules/katamari/src/main/ts/ephox/katamari/api/Num.ts
@@ -3,6 +3,5 @@ export const cycleBy = (value: number, delta: number, min: number, max: number):
   if (r > max) { return min; } else { return r < min ? max : r; }
 };
 
-export const cap = (value: number, min: number, max: number): number => {
-  if (value <= min) { return min; } else { return value >= max ? max : value; }
-};
+export const cap = (value: number, min: number, max: number): number =>
+  Math.min(Math.max(value, min), max);
