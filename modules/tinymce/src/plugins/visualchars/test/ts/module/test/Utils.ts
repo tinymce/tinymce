425a859780fd7d839593c57636e9ba3473e79a12
Lee Newson <lee.newson@tiny.cloud>
2019-12-06 14:50:18 +1000

    TINY-4507: Fixed an issue where visual chars was converting plain text into dom elements

--- modules/tinymce/src/plugins/visualchars/test/ts/module/test/Utils.ts
+++ modules/tinymce/src/plugins/visualchars/test/ts/module/test/Utils.ts
@@ -1,36 +1,43 @@
-import { ApproxStructure } from '@ephox/agar';
+import { ApproxStructure, StructAssert } from '@ephox/agar';
 
-const sAssertSpanStruct = ApproxStructure.build(function (s, str) {
+const sAssertStruct = (paraStruct: StructAssert[]) => ApproxStructure.build((s, str) => {
   return s.element('body', {
     children: [
       s.element('p', {
-        children: [
-          s.text(str.is('a')),
-          s.element('span', {}),
-          s.element('span', {}),
-          s.text(str.is('b'))
-        ]
+        children: paraStruct
       })
     ]
   });
 });
 
-const sAssertNbspStruct = ApproxStructure.build(function (s, str) {
-  return s.element('body', {
-    children: [
-      s.element('p', {
-        children: [
-          s.text(str.is('a')),
-          s.text(str.is('\u00a0')),
-          s.text(str.is('\u00a0')),
-          s.text(str.is('b'))
-        ]
-      })
-    ]
-  });
-});
+const sAssertSpanStruct = sAssertStruct(ApproxStructure.build((s, str) => {
+  return [
+    s.text(str.is('a')),
+    s.element('span', {
+      children: [
+        s.text(str.is('\u00a0')),
+      ]
+    }),
+    s.element('span', {
+      children: [
+        s.text(str.is('\u00a0')),
+      ]
+    }),
+    s.text(str.is('b'))
+  ];
+}));
+
+const sAssertNbspStruct = sAssertStruct(ApproxStructure.build((s, str) => {
+  return [
+    s.text(str.is('a')),
+    s.text(str.is('\u00a0')),
+    s.text(str.is('\u00a0')),
+    s.text(str.is('b'))
+  ];
+}));
 
 export {
+  sAssertStruct,
   sAssertNbspStruct,
   sAssertSpanStruct
 };
