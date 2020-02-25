9120772be77ea820b2173d8ae9e88b857e954c6b
Dylan Just <>
2020-02-26 09:25:23 +1000

    Change how selectors are exported.

--- modules/tinymce/src/plugins/help/test/ts/module/Selectors.ts
+++ modules/tinymce/src/plugins/help/test/ts/module/Selectors.ts
@@ -1,3 +1,5 @@
-export const dialog = '[role="dialog"]';
-export const toolbarHelpButton = 'button';
-export const pluginsTab = '[role="tab"]:contains(Plugins)';
+export const selectors = {
+  dialog: '[role="dialog"]',
+  toolbarHelpButton: 'button',
+  pluginsTab: '[role="tab"]:contains(Plugins)'
+};
