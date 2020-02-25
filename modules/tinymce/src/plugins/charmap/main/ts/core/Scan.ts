8e86c2ff4197407474f67560a0bb63d85e1ca439
Matthew Litwin <matthew.litwin@mheducation.com>
2020-02-24 16:47:20 -0800

    Character Map Plugin: Support supplementary characters
    Supplementary characters (Unicode characters greater than U+FFFF) are truncated
    when adding them to the charmap.
    
    Thus a character like 𝅘𝅥𝅮 (0x1d160 "Musical Symbol Eighth Note"), is displayed and entered into text as 텠 (0xd160 a Hangul syllables).
    
    The underlying cause is in Scan.ts which uses String.fromCharCode(), where per https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode "Numbers greater than 0xFFFF are truncated".
    
    The fix is to use String.fromCodePoint() which supports supplementary characters - BUT:
    that's ES2015. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCodePoint has a polyfile, which I've adapted as a local function.

--- modules/tinymce/src/plugins/charmap/main/ts/core/Scan.ts
+++ modules/tinymce/src/plugins/charmap/main/ts/core/Scan.ts
@@ -14,8 +14,41 @@ export interface CharItem {
   text: string;
 }
 
+// Extract codepoint a la ES2015 String.fromCodePoint
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCodePoint
+// Unwrapped the polyfill into a local function. typescript and lint.
+function stringfromCodePoint(_): string {
+  const codeUnits = [];
+  let codeLen = 0;
+  let result = '';
+  for (let index = 0, len = arguments.length; index !== len; ++index) {
+    let codePoint = +arguments[index];
+    // correctly handles all cases including `NaN`, `-Infinity`, `+Infinity`
+    // The surrounding `!(...)` is required to correctly handle `NaN` cases
+    // The (codePoint>>>0) === codePoint clause handles decimals and negatives
+    if (!(codePoint < 0x10FFFF && (codePoint >>> 0) === codePoint)) {
+      throw RangeError('Invalid code point: ' + codePoint);
+    }
+    if (codePoint <= 0xFFFF) { // BMP code point
+      codeLen = codeUnits.push(codePoint);
+    } else { // Astral code point; split in surrogate halves
+      // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+      codePoint -= 0x10000;
+      codeLen = codeUnits.push(
+        (codePoint >> 10) + 0xD800,  // highSurrogate
+        (codePoint % 0x400) + 0xDC00 // lowSurrogate
+      );
+    }
+    if (codeLen >= 0x3fff) {
+      result += String.fromCharCode.apply(null, codeUnits);
+      codeUnits.length = 0;
+    }
+  }
+  return result + String.fromCharCode.apply(null, codeUnits);
+}
+
 const charMatches = (charCode: number, name: string, lowerCasePattern: string): boolean => {
-  if (Strings.contains(String.fromCharCode(charCode).toLowerCase(), lowerCasePattern)) {
+  if (Strings.contains(stringfromCodePoint(charCode).toLowerCase(), lowerCasePattern)) {
     return true;
   } else {
     return Strings.contains(name.toLowerCase(), lowerCasePattern) || Strings.contains(name.toLowerCase().replace(/\s+/g, ''), lowerCasePattern);
@@ -34,8 +67,8 @@ const scan = (group: CharMap, pattern: string): CharItem[] => {
   return Arr.map(matches, (m) => {
     return {
       text: m[1],
-      value: String.fromCharCode(m[0]),
-      icon: String.fromCharCode(m[0])
+      value: stringfromCodePoint(m[0]),
+      icon: stringfromCodePoint(m[0])
     };
   });
 };
