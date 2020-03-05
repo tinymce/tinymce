3b4a1e91d87de0811af9bad9adae4659114ed4e9
Luke Trouton <luke.trouton@tiny.cloud>
2020-03-06 08:27:53 +1000

    TINY-4742: Add test file

---
+++ modules/tinymce/src/core/test/ts/browser/init/InitContentBodyParserTest.ts
@@ -0,0 +1,36 @@
+import { Log, Pipeline } from '@ephox/agar';
+import { UnitTest } from '@ephox/bedrock-client';
+import { TinyApis, TinyLoader } from '@ephox/mcagar';
+import Editor from 'tinymce/core/api/Editor';
+import Theme from 'tinymce/themes/silver/Theme';
+
+UnitTest.asynctest('browser.tinymce.core.init.InitContentParserSelectionTest', (success, failure) => {
+  Theme();
+
+  TinyLoader.setupLight((editor, onSuccess, onFailure) => {
+    const tinyApis = TinyApis(editor);
+
+    Pipeline.async({}, [
+      Log.stepsAsStep('TINY-4742', 'Insert content to activate node filters, check content is in editor', [
+        tinyApis.sFocus(),
+        tinyApis.sSetContent('<p style="color: rgb(255, 0, 0);">abc</p>'),
+        // If an exception occurs, then no content will be inserted into the editor
+        tinyApis.sAssertContent('<p>abc</p>'),
+      ]),
+    ], onSuccess, onFailure);
+  }, {
+    theme: 'silver',
+    base_url: '/project/tinymce/js/tinymce',
+    setup: (ed: Editor) => {
+      ed.on('Init', () => {
+        ed.parser.addNodeFilter('p', (nodes) => {
+          nodes.forEach((node) => {
+            // Remove style attributes from node
+            node.attr('style', null);
+            node.attr('data-mce-style', null);
+          });
+        });
+      }, true);
+    },
+  }, success, failure);
+});
