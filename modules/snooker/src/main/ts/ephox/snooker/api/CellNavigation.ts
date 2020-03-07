cc1fd45cfdca1897e99fcc71589f3143b53ad996
Dylan Just <dylan.just@tiny.cloud>
2020-03-07 19:14:52 +1000

    Unthunking

--- modules/snooker/src/main/ts/ephox/snooker/api/CellNavigation.ts
+++ modules/snooker/src/main/ts/ephox/snooker/api/CellNavigation.ts
@@ -1,4 +1,4 @@
-import { Arr, Fun } from '@ephox/katamari';
+import { Arr, Option } from '@ephox/katamari';
 import { Compare, Element } from '@ephox/sugar';
 import { CellLocation } from './CellLocation';
 import * as TableLookup from './TableLookup';
@@ -7,19 +7,12 @@ import * as TableLookup from './TableLookup';
  * Identify the index of the current cell within all the cells, and
  * a list of the cells within its table.
  */
-const detect = function (current: Element, isRoot?: (e: Element) => boolean) {
+const detect = function (current: Element, isRoot?: (e: Element) => boolean): Option<{ all: Element[]; index: number }> {
   return TableLookup.table(current, isRoot).bind(function (table) {
     const all = TableLookup.cells(table);
-    const index = Arr.findIndex(all, function (x) {
-      return Compare.eq(current, x);
-    });
+    const index = Arr.findIndex(all, (x) => Compare.eq(current, x));
 
-    return index.map(function (ind) {
-      return {
-        index: Fun.constant(ind),
-        all: Fun.constant(all)
-      };
-    });
+    return index.map((index) => ({ index, all }));
   });
 };
 
@@ -31,19 +24,19 @@ const next = function (current: Element, isRoot?: (e: Element) => boolean) {
   return detection.fold(function () {
     return CellLocation.none(current);
   }, function (info) {
-    return info.index() + 1 < info.all().length ? CellLocation.middle(current, info.all()[info.index() + 1]) : CellLocation.last(current);
+    return info.index + 1 < info.all.length ? CellLocation.middle(current, info.all[info.index + 1]) : CellLocation.last(current);
   });
 };
 
 /*
  * Identify the CellLocation of the cell when navigating back from current
  */
-const prev = function (current: Element, isRoot?: (e: Element) => boolean) {
+const prev = function (current: Element, isRoot?: (e: Element) => boolean): CellLocation {
   const detection = detect(current, isRoot);
   return detection.fold(function () {
     return CellLocation.none();
   }, function (info) {
-    return info.index() - 1 >= 0 ? CellLocation.middle(current, info.all()[info.index() - 1]) : CellLocation.first(current);
+    return info.index - 1 >= 0 ? CellLocation.middle(current, info.all[info.index - 1]) : CellLocation.first(current);
   });
 };
 
