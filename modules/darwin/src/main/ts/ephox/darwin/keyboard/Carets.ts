4dd554240f581483fcfe81039222e14270240080
Dylan Just <dylan.just@tiny.cloud>
2020-03-08 13:07:23 +1000

    Remove usages of Struct.immutableBag. Some unthunking.

--- modules/darwin/src/main/ts/ephox/darwin/keyboard/Carets.ts
+++ modules/darwin/src/main/ts/ephox/darwin/keyboard/Carets.ts
@@ -1,82 +1,70 @@
-import { Struct } from '@ephox/katamari';
-
 export interface Carets {
-  left: () => number;
-  top: () => number;
-  right: () => number;
-  bottom: () => number;
+  readonly left: number;
+  readonly top: number;
+  readonly right: number;
+  readonly bottom: number;
 }
 
-interface CaretsIn {
-  left: number;
-  top: number;
-  right: number;
-  bottom: number;
-}
-
-const nu: (data: CaretsIn) => Carets = Struct.immutableBag(['left', 'top', 'right', 'bottom'], []);
-
-const moveDown = function (caret: Carets, amount: number) {
-  return nu({
-    left: caret.left(),
-    top: caret.top() + amount,
-    right: caret.right(),
-    bottom: caret.bottom() + amount
-  });
+const moveDown = function (caret: Carets, amount: number): Carets {
+  return {
+    left: caret.left,
+    top: caret.top + amount,
+    right: caret.right,
+    bottom: caret.bottom + amount
+  };
 };
 
-const moveUp = function (caret: Carets, amount: number) {
-  return nu({
-    left: caret.left(),
-    top: caret.top() - amount,
-    right: caret.right(),
-    bottom: caret.bottom() - amount
-  });
+const moveUp = function (caret: Carets, amount: number): Carets {
+  return {
+    left: caret.left,
+    top: caret.top - amount,
+    right: caret.right,
+    bottom: caret.bottom - amount
+  };
 };
 
-const moveBottomTo = function (caret: Carets, bottom: number) {
-  const height = caret.bottom() - caret.top();
-  return nu({
-    left: caret.left(),
+const moveBottomTo = function (caret: Carets, bottom: number): Carets {
+  const height = caret.bottom - caret.top;
+  return {
+    left: caret.left,
     top: bottom - height,
-    right: caret.right(),
+    right: caret.right,
     bottom
-  });
+  };
 };
 
-const moveTopTo = function (caret: Carets, top: number) {
-  const height = caret.bottom() - caret.top();
-  return nu({
-    left: caret.left(),
+const moveTopTo = function (caret: Carets, top: number): Carets {
+  const height = caret.bottom - caret.top;
+  return ({
+    left: caret.left,
     top,
-    right: caret.right(),
+    right: caret.right,
     bottom: top + height
   });
 };
 
-const translate = function (caret: Carets, xDelta: number, yDelta: number) {
-  return nu({
-    left: caret.left() + xDelta,
-    top: caret.top() + yDelta,
-    right: caret.right() + xDelta,
-    bottom: caret.bottom() + yDelta
-  });
+const translate = function (caret: Carets, xDelta: number, yDelta: number): Carets {
+  return {
+    left: caret.left + xDelta,
+    top: caret.top + yDelta,
+    right: caret.right + xDelta,
+    bottom: caret.bottom + yDelta
+  };
 };
 
-const getTop = function (caret: Carets) {
-  return caret.top();
+const getTop = function (caret: Carets): number {
+  return caret.top;
 };
 
-const getBottom = function (caret: Carets) {
-  return caret.bottom();
+const getBottom = function (caret: Carets): number {
+  return caret.bottom;
 };
 
 const toString = function (caret: Carets) {
-  return '(' + caret.left() + ', ' + caret.top() + ') -> (' + caret.right() + ', ' + caret.bottom() + ')';
+  return '(' + caret.left + ', ' + caret.top + ') -> (' + caret.right + ', ' + caret.bottom + ')';
 };
 
-export const Carets = {
-  nu,
+export {
   moveUp,
   moveDown,
   moveBottomTo,
