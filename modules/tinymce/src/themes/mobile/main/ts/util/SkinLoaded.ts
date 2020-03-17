791e94a63b7fa659e527349508986d2ccef88296
Dylan Just <dylan.just@tiny.cloud>
2020-03-17 21:16:41 +1000

    Whitespace

--- modules/tinymce/src/themes/mobile/main/ts/util/SkinLoaded.ts
+++ modules/tinymce/src/themes/mobile/main/ts/util/SkinLoaded.ts
@@ -4,6 +4,7 @@
  * For LGPL see License.txt in the project root for license information.
  * For commercial licenses see https://www.tiny.cloud/
  */
+
 import Editor from 'tinymce/core/api/Editor';
 
 const fireSkinLoaded = (editor: Editor) => (): void => {
