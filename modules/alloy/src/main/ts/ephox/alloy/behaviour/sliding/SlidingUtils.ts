f5c3c371b2ac80b15cd6fa013446a882c98d5758
James Johnson <james.johnson@tiny.cloud>
2020-04-06 15:36:27 +1000

    TINY-4780: Wrapped a line

--- modules/alloy/src/main/ts/ephox/alloy/behaviour/sliding/SlidingUtils.ts
+++ modules/alloy/src/main/ts/ephox/alloy/behaviour/sliding/SlidingUtils.ts
@@ -1,4 +1,8 @@
 import { AlloyComponent } from '../../api/component/ComponentApi';
 import { SlidingConfig } from './SlidingTypes';
 
-export const getAnimationRoot = (component: AlloyComponent, slideConfig: SlidingConfig) => slideConfig.getAnimationRoot.fold(() => component.element(), (get) => get(component));
+export const getAnimationRoot = (component: AlloyComponent, slideConfig: SlidingConfig) =>
+  slideConfig.getAnimationRoot.fold(
+    () => component.element(),
+    (get) => get(component)
+  );
