95ef9565eb3702dd2dd65e486e393201ab9bd588
Lee Newson <lee.newson@tiny.cloud>
2020-04-16 12:34:09 +1000

    TINY-4838: Fixed a regression caused by accidentally dropping an argument when removing formatting

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/color/TextColorFormattingTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/color/TextColorFormattingTest.ts
@@ -1,9 +1,9 @@
-import { ApproxStructure, Pipeline, Log } from '@ephox/agar';
+import { ApproxStructure, Log, Pipeline } from '@ephox/agar';
 import { UnitTest } from '@ephox/bedrock-client';
-import { TinyApis, TinyLoader, TinyUi } from '@ephox/mcagar';
-import SilverTheme from 'tinymce/themes/silver/Theme';
 import { Unicode } from '@ephox/katamari';
+import { TinyApis, TinyLoader, TinyUi } from '@ephox/mcagar';
 import Env from 'tinymce/core/api/Env';
+import SilverTheme from 'tinymce/themes/silver/Theme';
 
 UnitTest.asynctest('TextColorFormattingTest', (success, failure) => {
   if (Env.browser.isIE()) {
@@ -62,6 +62,25 @@ UnitTest.asynctest('TextColorFormattingTest', (success, failure) => {
               ]
             }),
             s.text(str.is('world'))
+          ]
+        })
+      ]
+    }));
+
+  const forecolorTextStruct = ApproxStructure.build((s, str) =>
+    s.element('body', {
+      children: [
+        s.element('p', {
+          children: [
+            s.element('span', {
+              styles: {
+                color: str.is('rgb(53, 152, 219)')
+              },
+              children: [
+                s.text(str.is('Hello'))
+              ]
+            }),
+            s.text(str.is(Unicode.nbsp + 'world'))
           ]
         })
       ]
@@ -129,7 +148,20 @@ UnitTest.asynctest('TextColorFormattingTest', (success, failure) => {
         tinyUi.sClickOnUi('click red color', 'div[title="Red"]'),
         tinyApis.sAssertContentStructure(backcolorTitleStruct),
       ]),
-    ], onSuccess, onFailure );
+      Log.stepsAsStep('TINY-4838', 'TextColor: Remove forecolor with collapsed selection', [
+        tinyApis.sFocus(),
+        tinyApis.sSetContent(`Hello${Unicode.nbsp}world`),
+        tinyApis.sSetSelection([ 0, 0 ], 2, [ 0, 0 ], 2),
+        tinyUi.sClickOnToolbar('click forecolor', '[aria-label="Text color"] > .tox-tbtn + .tox-split-button__chevron'),
+        tinyUi.sWaitForUi('wait for color swatch to open', '.tox-swatches'),
+        tinyUi.sClickOnUi('select blue color', 'div[data-mce-color="#3598DB"]'),
+        tinyApis.sAssertContentStructure(forecolorTextStruct),
+        tinyUi.sClickOnToolbar('click forecolor', '[aria-label="Text color"] > .tox-tbtn + .tox-split-button__chevron'),
+        tinyUi.sWaitForUi('wait for color swatch to open', '.tox-swatches'),
+        tinyUi.sClickOnUi('select remove color', 'div[title="Remove color"]'),
+        tinyApis.sAssertContent('<p>Hello&nbsp;world</p>')
+      ])
+    ], onSuccess, onFailure);
 
   }, {
     plugins: '',
