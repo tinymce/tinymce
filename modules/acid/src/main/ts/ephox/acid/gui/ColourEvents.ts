e6f0d8696de2175358cc684ef99d48c3794cfe5d
Lee Newson <lee.newson@tiny.cloud>
2020-05-08 23:01:49 +1000

    TINY-5970: Use the correct base type for custom events

--- modules/acid/src/main/ts/ephox/acid/gui/ColourEvents.ts
+++ modules/acid/src/main/ts/ephox/acid/gui/ColourEvents.ts
@@ -1,15 +1,15 @@
-import { EventFormat, SliderTypes } from '@ephox/alloy';
+import { CustomEvent, SliderTypes } from '@ephox/alloy';
 import { Id } from '@ephox/katamari';
 
 const fieldsUpdate = Id.generate('rgb-hex-update');
 const sliderUpdate = Id.generate('slider-update');
 const paletteUpdate = Id.generate('palette-update');
 
-export interface SliderUpdateEvent extends EventFormat {
+export interface SliderUpdateEvent extends CustomEvent {
   value: () => SliderTypes.SliderValueY;
 }
 
-export interface PaletteUpdateEvent extends EventFormat {
+export interface PaletteUpdateEvent extends CustomEvent {
   value: () => SliderTypes.SliderValueXY;
 }
 
