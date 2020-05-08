tinymce/tinymce

commit f40614b6aefd1790ad3a6bec39720b8f00b6f29e

Unthunking

modules/acid/src/main/ts/ephox/acid/gui/ColourEvents.ts
@@ -1,16 +1,16 @@
-import { Id, Fun } from '@ephox/katamari';
+import { Id } from '@ephox/katamari';
 import { SliderTypes, EventFormat } from '@ephox/alloy';
 
-const fieldsUpdate = Fun.constant(Id.generate('rgb-hex-update'));
-const sliderUpdate = Fun.constant(Id.generate('slider-update'));
-const paletteUpdate = Fun.constant(Id.generate('palette-update'));
+const fieldsUpdate = Id.generate('rgb-hex-update');
+const sliderUpdate = Id.generate('slider-update');
+const paletteUpdate = Id.generate('palette-update');
 
 export interface SliderUpdateEvent extends EventFormat {
-  value: () => SliderTypes.SliderValueY;
+  value: SliderTypes.SliderValueY;
 }
 
 export interface PaletteUpdateEvent extends EventFormat {
-  value: () => SliderTypes.SliderValueXY;
+  value: SliderTypes.SliderValueXY;
 }
 
 export {

modules/acid/src/main/ts/ephox/acid/gui/ColourPicker.ts
@@ -12,15 +12,15 @@ import * as RgbForm from './components/RgbForm';
 import * as SaturationBrightnessPalette from './components/SaturationBrightnessPalette';
 
 export interface ColourPickerDetail extends Sketcher.SingleSketchDetail {
-  dom: RawDomSchema;
-  onValidHex: (component: AlloyComponent) => void;
-  onInvalidHex: (component: AlloyComponent) => void;
+  readonly dom: RawDomSchema;
+  readonly onValidHex: (component: AlloyComponent) => void;
+  readonly onInvalidHex: (component: AlloyComponent) => void;
 }
 
 export interface ColourPickerSpec extends Sketcher.SingleSketchSpec {
-  dom: RawDomSchema;
-  onValidHex?: (component: AlloyComponent) => void;
-  onInvalidHex?: (component: AlloyComponent) => void;
+  readonly dom: RawDomSchema;
+  readonly onValidHex?: (component: AlloyComponent) => void;
+  readonly onInvalidHex?: (component: AlloyComponent) => void;
 }
 
 export interface ColourPickerSketcher extends Sketcher.SingleSketch<ColourPickerSpec> {
@@ -35,7 +35,7 @@ const makeFactory = (
     const sbPalette = SaturationBrightnessPalette.paletteFactory(translate, getClass);
 
     const state = {
-      paletteRgba: Fun.constant(Cell(RgbaColour.red))
+      paletteRgba: Cell(RgbaColour.red)
     };
 
     const memPalette = Memento.record(
@@ -48,7 +48,7 @@ const makeFactory = (
     const updatePalette = (anyInSystem: AlloyComponent, hex: Hex) => {
       memPalette.getOpt(anyInSystem).each((palette) => {
         const rgba = RgbaColour.fromHex(hex);
-        state.paletteRgba().set(rgba);
+        state.paletteRgba.set(rgba);
         sbPalette.setRgba(palette, rgba);
       });
     };
@@ -68,8 +68,8 @@ const makeFactory = (
     const paletteUpdates = () => {
       const updates = [updateFields];
       return (form: AlloyComponent, simulatedEvent: SimulatedEvent<ColourEvents.PaletteUpdateEvent>) => {
-        const value = simulatedEvent.event().value();
-        const oldRgb = state.paletteRgba().get();
+        const value = simulatedEvent.event().value;
+        const oldRgb = state.paletteRgba.get();
         const hsvColour = HsvColour.fromRgb(oldRgb);
         const newHsvColour = HsvColour.hsvColour(hsvColour.hue, value.x(), (100 - value.y()));
         const rgb = RgbaColour.fromHsv(newHsvColour);
@@ -81,7 +81,7 @@ const makeFactory = (
     const sliderUpdates = () => {
       const updates = [updatePalette, updateFields];
       return (form: AlloyComponent, simulatedEvent: SimulatedEvent<ColourEvents.SliderUpdateEvent>) => {
-        const value = simulatedEvent.event().value();
+        const value = simulatedEvent.event().value;
         const hex = calcHex(value.y());
         runUpdates(form, hex, updates);
       };
@@ -99,8 +99,8 @@ const makeFactory = (
       behaviours: Behaviour.derive([
         AddEventsBehaviour.config('colour-picker-events', [
           // AlloyEvents.run(ColourEvents.fieldsUpdate(), fieldsUpdates()),
-          AlloyEvents.run(ColourEvents.paletteUpdate(), paletteUpdates()),
-          AlloyEvents.run(ColourEvents.sliderUpdate(), sliderUpdates())
+          AlloyEvents.run(ColourEvents.paletteUpdate, paletteUpdates()),
+          AlloyEvents.run(ColourEvents.sliderUpdate, sliderUpdates())
         ]),
         Composing.config({
           find: (comp) => {

modules/acid/src/main/ts/ephox/acid/gui/components/HueSlider.ts
@@ -45,7 +45,7 @@ const sliderFactory = (translate: (key: string) => string, getClass: (key: strin
     ]),
 
     onChange: (slider: AlloyComponent, _thumb: any, value: any) => {
-      AlloyTriggers.emitWith(slider, sliderUpdate(), {
+      AlloyTriggers.emitWith(slider, sliderUpdate, {
         value
       });
     }

modules/acid/src/main/ts/ephox/acid/gui/components/RgbForm.ts
@@ -227,7 +227,7 @@ const rgbFormFactory = (
       copyRgbToForm(form, rgb);
       setValueRgb(rgb);
 
-      AlloyTriggers.emitWith(form, ColourEvents.fieldsUpdate(), {
+      AlloyTriggers.emitWith(form, ColourEvents.fieldsUpdate, {
         hex
       });
 

modules/acid/src/main/ts/ephox/acid/gui/components/SaturationBrightnessPalette.ts
@@ -75,7 +75,7 @@ const paletteFactory = (_translate: (key: string) => string, getClass: (key: str
     });
 
     const onChange = (slider: AlloyComponent, _thumb: AlloyComponent, value: number | SliderTypes.SliderValue) => {
-      AlloyTriggers.emitWith(slider, ColourEvents.paletteUpdate(), {
+      AlloyTriggers.emitWith(slider, ColourEvents.paletteUpdate, {
         value
       });
     };

commit 20f9cb5f96468b1ff7763ba0e02de308e9d55b5c

TINY-5970: Fixed colour picker regression introduced in f40614b

modules/acid/src/main/ts/ephox/acid/gui/ColourEvents.ts
@@ -1,16 +1,16 @@
+import { EventFormat, SliderTypes } from '@ephox/alloy';
 import { Id } from '@ephox/katamari';
-import { SliderTypes, EventFormat } from '@ephox/alloy';
 
 const fieldsUpdate = Id.generate('rgb-hex-update');
 const sliderUpdate = Id.generate('slider-update');
 const paletteUpdate = Id.generate('palette-update');
 
 export interface SliderUpdateEvent extends EventFormat {
-  value: SliderTypes.SliderValueY;
+  value: () => SliderTypes.SliderValueY;
 }
 
 export interface PaletteUpdateEvent extends EventFormat {
-  value: SliderTypes.SliderValueXY;
+  value: () => SliderTypes.SliderValueXY;
 }
 
 export {

modules/acid/src/main/ts/ephox/acid/gui/ColourPicker.ts
@@ -68,7 +68,7 @@ const makeFactory = (
     const paletteUpdates = () => {
       const updates = [ updateFields ];
       return (form: AlloyComponent, simulatedEvent: SimulatedEvent<ColourEvents.PaletteUpdateEvent>) => {
-        const value = simulatedEvent.event().value;
+        const value = simulatedEvent.event().value();
         const oldRgb = state.paletteRgba.get();
         const hsvColour = HsvColour.fromRgb(oldRgb);
         const newHsvColour = HsvColour.hsvColour(hsvColour.hue, value.x(), (100 - value.y()));
@@ -81,7 +81,7 @@ const makeFactory = (
     const sliderUpdates = () => {
       const updates = [ updatePalette, updateFields ];
       return (form: AlloyComponent, simulatedEvent: SimulatedEvent<ColourEvents.SliderUpdateEvent>) => {
-        const value = simulatedEvent.event().value;
+        const value = simulatedEvent.event().value();
         const hex = calcHex(value.y());
         runUpdates(form, hex, updates);
       };