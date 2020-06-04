4a50b341f30e7c493815c562f85c4be6e82e6f1d
Dylan Just <dylan.just@tiny.cloud>
2020-06-04 20:17:44 +1000

    Refactor

--- modules/sugar/src/test/ts/browser/ShadowDomTest.ts
+++ modules/sugar/src/test/ts/browser/ShadowDomTest.ts
@@ -2,18 +2,21 @@ import { Assert, UnitTest } from '@ephox/bedrock-client';
 import Element from 'ephox/sugar/api/node/Element';
 import * as Body from 'ephox/sugar/api/node/Body';
 import * as Insert from 'ephox/sugar/api/dom/Insert';
-import { Element as DomElement, ShadowRoot, window } from '@ephox/dom-globals';
+import { Element as DomElement, ShadowRoot } from '@ephox/dom-globals';
 import * as SelectorFind from 'ephox/sugar/api/search/SelectorFind';
 
-UnitTest.test('ShadowDom - SelectorFind.descendant', () => {
+const mkShadow = (): Element<ShadowRoot> => {
   const body = Body.body();
   const e = Element.fromHtml<DomElement>('<div />');
   Insert.append(body, e);
 
   const shadow: ShadowRoot = e.dom().attachShadow({ mode: 'open' });
-  const ss: Element<ShadowRoot> = Element.fromDom(shadow);
+  return Element.fromDom(shadow);
+};
+
+UnitTest.test('ShadowDom - SelectorFind.descendant', () => {
+  const ss = mkShadow();
   const inner = Element.fromHtml('<div><p>hello<span id="frog">iamthefrog</span></p></div>');
-  (window as any).myshadow = shadow;
   Insert.append(ss, inner);
 
   const frog: Element<DomElement> = SelectorFind.descendant(ss, '#frog').getOrDie('Element not found');
