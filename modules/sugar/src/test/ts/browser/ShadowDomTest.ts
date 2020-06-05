35eb10892739b9c76c787d3dacf945d5e138db5d
Dylan Just <dylan.just@tiny.cloud>
2020-06-05 11:22:45 +1000

    Clean up after tests.

--- modules/sugar/src/test/ts/browser/ShadowDomTest.ts
+++ modules/sugar/src/test/ts/browser/ShadowDomTest.ts
@@ -5,6 +5,7 @@ import * as Insert from 'ephox/sugar/api/dom/Insert';
 import { Element as DomElement, ShadowRoot } from '@ephox/dom-globals';
 import * as SelectorFind from 'ephox/sugar/api/search/SelectorFind';
 import fc, { Arbitrary } from 'fast-check';
+import * as Remove from 'ephox/sugar/api/dom/Remove';
 
 const shadowDomTest = (name: string, fn: () => void) => {
   if (DomElement.prototype.hasOwnProperty('attachShadow')) {
@@ -12,13 +13,19 @@ const shadowDomTest = (name: string, fn: () => void) => {
   }
 };
 
-const mkShadow = (): Element<ShadowRoot> => {
+const withShadow = (f: (shadow: Element<ShadowRoot>) => void): void => {
   const body = Body.body();
   const e = Element.fromHtml<DomElement>('<div />');
   Insert.append(body, e);
 
   const shadow: ShadowRoot = e.dom().attachShadow({ mode: 'open' });
-  return Element.fromDom(shadow);
+  const shadowE: Element<ShadowRoot> = Element.fromDom(shadow);
+
+  try {
+    f(shadowE);
+  } finally {
+    Remove.remove(e);
+  }
 };
 
 const htmlBlockTagName = (): Arbitrary<string> =>
@@ -31,12 +38,13 @@ const htmlInlineTagName = (): Arbitrary<string> =>
 
 shadowDomTest('ShadowDom - SelectorFind.descendant', () => {
   fc.assert(fc.property(htmlBlockTagName(), htmlInlineTagName(), fc.hexaString(), (block, inline, text) => {
-    const ss = mkShadow();
-    const id = 'theid';
-    const inner = Element.fromHtml(`<${block}><p>hello<${inline} id="${id}">${text}</${inline}></p></${block}>`);
-    Insert.append(ss, inner);
+    withShadow((ss) => {
+      const id = 'theid';
+      const inner = Element.fromHtml(`<${block}><p>hello<${inline} id="${id}">${text}</${inline}></p></${block}>`);
+      Insert.append(ss, inner);
 
-    const frog: Element<DomElement> = SelectorFind.descendant(ss, `#${id}`).getOrDie('Element not found');
-    Assert.eq('textcontent', text, frog.dom().textContent);
+      const frog: Element<DomElement> = SelectorFind.descendant(ss, `#${id}`).getOrDie('Element not found');
+      Assert.eq('textcontent', text, frog.dom().textContent);
+    });
   }));
 });
