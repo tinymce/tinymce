b797a645f84758029afc98c4940864c6557934cc
Dylan Just <dylan.just@tiny.cloud>
2020-06-12 13:19:51 +1000

    Use the StyleSheetLoaderRegistry in the theme.

---
+++ modules/tinymce/src/themes/mobile/main/ts/style/StyleSheetLoaderRegistry.ts
@@ -0,0 +1,35 @@
+import { document, Document as DomDocument, Node as DomNode, ShadowRoot } from '@ephox/dom-globals';
+import { StyleSheetLoader, StyleSheetLoaderSettings } from 'tinymce/core/api/dom/StyleSheetLoader';
+import { Element, ShadowDom } from '@ephox/sugar';
+import DOMUtils from 'tinymce/core/api/dom/DOMUtils';
+import { Option } from '@ephox/katamari';
+
+export interface StyleSheetLoaderRegistry {
+  readonly forElement: (referenceElement: DomNode, settings: Partial<StyleSheetLoaderSettings>) => StyleSheetLoader;
+}
+
+const create = (): StyleSheetLoaderRegistry => {
+
+  const map = new WeakMap<DomDocument | ShadowRoot, StyleSheetLoader>();
+
+  const forElement = (referenceElement: DomNode, settings: Partial<StyleSheetLoaderSettings>) => {
+    const root = ShadowDom.getRootNode(Element.fromDom(referenceElement));
+
+    const rootDom = root.dom();
+    if (rootDom === document) {
+      return DOMUtils.DOM.styleSheetLoader;
+    } else {
+      return Option.from(map.get(rootDom)).getOrThunk(() => {
+        const sl = StyleSheetLoader(rootDom, settings);
+        map.set(rootDom, sl);
+        return sl;
+      });
+    }
+  };
+
+  return {
+    forElement
+  };
+};
+
+export const instance = create();
