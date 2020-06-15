34743b5537fce797fac383f1aa51d5dd95034a47
Dylan Just <dylan.just@tiny.cloud>
2020-06-15 19:27:15 +1000

    Change withTimeout to produce an Option. Returns none if it times out.

--- modules/katamari/src/main/ts/ephox/katamari/api/LazyValues.ts
+++ modules/katamari/src/main/ts/ephox/katamari/api/LazyValues.ts
@@ -1,5 +1,7 @@
 import { LazyValue } from './LazyValue';
 import * as AsyncValues from '../async/AsyncValues';
+import { Option } from './Option';
+import * as Fun from './Fun';
 import { clearTimeout, setTimeout } from '@ephox/dom-globals';
 
 /** par :: [LazyValue a] -> LazyValue [a] */
@@ -7,14 +9,19 @@ export const par = function <T> (lazyValues: LazyValue<T>[]) {
   return AsyncValues.par(lazyValues, LazyValue.nu);
 };
 
-export const withTimeout = <T> (baseFn: (completer: (value: T) => void) => void, ifTimeout: () => T, timeout: number): LazyValue<T> =>
+/**
+ * Produces a LazyValue that may time out.
+ * If it times out, it produces an Option.none.
+ * If it completes before the timeout, it produces an Option.some.
+ */
+export const withTimeout = <T>(baseFn: (completer: (value: T) => void) => void, timeout: number): LazyValue<Option<T>> =>
   LazyValue.nu((completer) => {
-    const done = (r: T) => {
+    const done = (r: Option<T>) => {
       clearTimeout(timeoutRef);
       completer(r);
     };
     const timeoutRef = setTimeout(() => {
-      done(ifTimeout());
+      done(Option.none());
     }, timeout);
-    baseFn(done);
+    baseFn(Fun.compose(done, Option.some));
   });
