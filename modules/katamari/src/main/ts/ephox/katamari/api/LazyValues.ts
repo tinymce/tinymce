ae3944dc10dfd966bf01986e3b07a3e97795aba6
Dylan Just <dylan.just@tiny.cloud>
2020-06-15 16:08:52 +1000

    Remove signature.

--- modules/katamari/src/main/ts/ephox/katamari/api/LazyValues.ts
+++ modules/katamari/src/main/ts/ephox/katamari/api/LazyValues.ts
@@ -7,7 +7,7 @@ export const par = function <T> (lazyValues: LazyValue<T>[]) {
   return AsyncValues.par(lazyValues, LazyValue.nu);
 };
 
-export const withTimeout: <T>(baseFn: (completer: (value: T) => void) => void, ifTimeout: () => T, timeout: number) => LazyValue<T> = <T> (baseFn: (completer: (value: T) => void) => void, ifTimeout: () => T, timeout: number): LazyValue<T> =>
+export const withTimeout = <T> (baseFn: (completer: (value: T) => void) => void, ifTimeout: () => T, timeout: number): LazyValue<T> =>
   LazyValue.nu((completer) => {
     const done = (r: T) => {
       clearTimeout(timeoutRef);
