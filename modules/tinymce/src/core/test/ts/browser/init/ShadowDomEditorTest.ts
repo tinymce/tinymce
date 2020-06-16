8cb1bda0d750f4bc8bb441f93abeadc7918ed5f0
Dylan Just <dylan.just@tiny.cloud>
2020-06-16 20:53:59 +1000

    Tweak on test exclusion to match existing tests.

--- modules/tinymce/src/core/test/ts/browser/init/ShadowDomEditorTest.ts
+++ modules/tinymce/src/core/test/ts/browser/init/ShadowDomEditorTest.ts
@@ -11,70 +11,68 @@ const isSkin = (ss: StyleSheet) => ss.href !== null && Strings.contains(ss.href,
 
 UnitTest.asynctest('Skin stylesheets should be loaded in ShadowRoot when editor is in ShadowRoot', (success, failure) => {
 
-  if (ShadowDom.isSupported()) {
-    Theme();
-
-    const shadowHost = Element.fromTag('div', document);
-    Insert.append(Body.body(), shadowHost);
-    const sr = Element.fromDom(shadowHost.dom().attachShadow({ mode: 'open' }));
-    const editorDiv = Element.fromTag('div', document);
-    Insert.append(sr, editorDiv);
-
-    TinyLoader.setupFromElement((editor: Editor, onSuccess, onFailure) => {
-      Pipeline.async({}, [
-        Step.sync(() => {
-          Assert.eq(
-            'There should not be any skin stylesheets in the document',
-            false,
-            Arr.exists(document.styleSheets, isSkin)
-          );
-          Assert.eq(
-            'There should be a skin stylesheet in the ShadowRoot',
-            true,
-            Arr.exists(sr.dom().styleSheets, isSkin)
-          );
-          Remove.remove(shadowHost);
-        })
-      ], onSuccess, onFailure);
-    }, {
-      toolbar_sticky: false,
-      base_url: '/project/tinymce/js/tinymce'
-    }, editorDiv, success, failure);
-  } else {
-    success();
+  if (!ShadowDom.isSupported()) {
+    return success();
   }
+
+  Theme();
+
+  const shadowHost = Element.fromTag('div', document);
+  Insert.append(Body.body(), shadowHost);
+  const sr = Element.fromDom(shadowHost.dom().attachShadow({ mode: 'open' }));
+  const editorDiv = Element.fromTag('div', document);
+  Insert.append(sr, editorDiv);
+
+  TinyLoader.setupFromElement((editor: Editor, onSuccess, onFailure) => {
+    Pipeline.async({}, [
+      Step.sync(() => {
+        Assert.eq(
+          'There should not be any skin stylesheets in the document',
+          false,
+          Arr.exists(document.styleSheets, isSkin)
+        );
+        Assert.eq(
+          'There should be a skin stylesheet in the ShadowRoot',
+          true,
+          Arr.exists(sr.dom().styleSheets, isSkin)
+        );
+        Remove.remove(shadowHost);
+      })
+    ], onSuccess, onFailure);
+  }, {
+    toolbar_sticky: false,
+    base_url: '/project/tinymce/js/tinymce'
+  }, editorDiv, success, failure);
 });
 
 UnitTest.asynctest('Only one skin stylesheet should be loaded for multiple editors in a ShadowRoot', (success, failure) => {
-  if (ShadowDom.isSupported()) {
-    Theme();
-
-    const shadowHost = Element.fromTag('div', document);
-    Insert.append(Body.body(), shadowHost);
-    const sr = Element.fromDom(shadowHost.dom().attachShadow({ mode: 'open' }));
-
-    const mkEditor = () => {
-      const editorDiv = Element.fromTag('div', document);
-      Insert.append(sr, editorDiv);
-      return McEditor.cFromElement(editorDiv, { base_url: '/project/tinymce/js/tinymce' });
-    };
-
-    const nc = NamedChain.asChain([
-      NamedChain.write('editor1', mkEditor()),
-      NamedChain.write('editor2', mkEditor()),
-      NamedChain.write('editor3', mkEditor()),
-      Chain.op(() => {
-        Assert.eq(
-          'There should only be 1 skin stylesheet in the ShadowRoot',
-          1,
-          Arr.filter(sr.dom().styleSheets, isSkin).length
-        );
-      }),
-      NamedChain.read('editor1', McEditor.cRemove),
-      NamedChain.read('editor2', McEditor.cRemove),
-      NamedChain.read('editor3', McEditor.cRemove)
-    ]);
-
-    Pipeline.async({}, [ Chain.asStep({}, [ nc ]) ], () => success(), failure);
+  if (!ShadowDom.isSupported()) {
+    return success();
   }
+
+  Theme();
+  const shadowHost = Element.fromTag('div', document);
+  Insert.append(Body.body(), shadowHost);
+  const sr = Element.fromDom(shadowHost.dom().attachShadow({ mode: 'open' }));
+  const mkEditor = () => {
+    const editorDiv = Element.fromTag('div', document);
+    Insert.append(sr, editorDiv);
+    return McEditor.cFromElement(editorDiv, { base_url: '/project/tinymce/js/tinymce' });
+  };
+  const nc = NamedChain.asChain([
+    NamedChain.write('editor1', mkEditor()),
+    NamedChain.write('editor2', mkEditor()),
+    NamedChain.write('editor3', mkEditor()),
+    Chain.op(() => {
+      Assert.eq(
+        'There should only be 1 skin stylesheet in the ShadowRoot',
+        1,
+        Arr.filter(sr.dom().styleSheets, isSkin).length
+      );
+    }),
+    NamedChain.read('editor1', McEditor.cRemove),
+    NamedChain.read('editor2', McEditor.cRemove),
+    NamedChain.read('editor3', McEditor.cRemove)
+  ]);
+  Pipeline.async({}, [ Chain.asStep({}, [ nc ]) ], () => success(), failure);
 });
