bf55b8e86a8f2d7dbb7e9f1264c80e30b8467c1c
Dylan Just <dylan.just@tiny.cloud>
2020-06-16 17:50:35 +1000

    Test that multiple editors in one ShadowRoot only load the skin stylesheet once.

--- modules/tinymce/src/core/test/ts/browser/init/ShadowDomEditorTest.ts
+++ modules/tinymce/src/core/test/ts/browser/init/ShadowDomEditorTest.ts
@@ -1,11 +1,14 @@
 import { Pipeline, Step } from '@ephox/agar';
-import { document, StyleSheet } from '@ephox/dom-globals';
+import { document, setTimeout, StyleSheet } from '@ephox/dom-globals';
 import { TinyLoader } from '@ephox/mcagar';
 import Editor from 'tinymce/core/api/Editor';
 import Theme from 'tinymce/themes/silver/Theme';
 import { ShadowDom, Element, Insert, Body, Remove } from '@ephox/sugar';
 import { UnitTest, Assert } from '@ephox/bedrock-client';
-import { Arr, Strings } from '@ephox/katamari';
+import { Arr, Futures, Global, Strings } from '@ephox/katamari';
+import { Future } from '@ephox/katamari/lib/main/ts/ephox/katamari/api/Future';
+
+const isSkin = (ss: StyleSheet) => ss.href !== null && Strings.contains(ss.href, 'skin.min.css');
 
 UnitTest.asynctest('Skin stylesheets should be loaded in ShadowRoot when editor is in ShadowRoot', (success, failure) => {
 
@@ -19,7 +22,6 @@ UnitTest.asynctest('Skin stylesheets should be loaded in ShadowRoot when editor 
     Insert.append(sr, editorDiv);
 
     TinyLoader.setupFromElement((editor: Editor, onSuccess, onFailure) => {
-      const isSkin = (ss: StyleSheet) => ss.href !== null && Strings.contains(ss.href, 'skin.min.css');
       Pipeline.async({}, [
         Step.sync(() => {
           Assert.eq(
@@ -39,6 +41,52 @@ UnitTest.asynctest('Skin stylesheets should be loaded in ShadowRoot when editor 
       toolbar_sticky: false,
       base_url: '/project/tinymce/js/tinymce'
     }, editorDiv, success, failure);
+  } else {
+    success();
+  }
+});
+
+UnitTest.asynctest('Only one skin stylesheet should be loaded for multiple editors in a ShadowRoot', (success, failure) => {
+  if (ShadowDom.isSupported()) {
+    Theme();
+
+    const shadowHost = Element.fromTag('div', document);
+    Insert.append(Body.body(), shadowHost);
+    const sr = Element.fromDom(shadowHost.dom().attachShadow({ mode: 'open' }));
+
+    const createF = () => Future.nu((done) => {
+      const editorDiv = Element.fromTag('div', document);
+      Insert.append(sr, editorDiv);
+
+      Global.tinymce.init({
+        target: editorDiv.dom(),
+        base_url: '/project/tinymce/js/tinymce',
+        setup: (editor) => {
+          editor.on('SkinLoaded', () => {
+            setTimeout(() => {
+              done(editor);
+            }, 0);
+          });
+        }
+      });
+    });
+
+    Futures.traverse([1, 2, 3], createF).get((editors: Editor[]) => {
+      Pipeline.async({}, [
+        Step.sync(() => {
+          Assert.eq(
+            'There should only be 1 skin stylesheet in the ShadowRoot',
+            1,
+            Arr.filter(sr.dom().styleSheets, isSkin).length
+          );
+          Remove.remove(shadowHost);
+          Arr.each(editors, (e) => e.remove());
+          TinyLoader.removeTinymceElements();
+        })
+      ], () => {
+        success()
+      }, failure);
+    });
   } else {
     success();
   }
