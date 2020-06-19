ae4da93c0b86e79f41273821f085599ede579e4a
Dylan Just <dylan.just@tiny.cloud>
2020-06-19 15:43:16 +1000

    Add some attributes to elements in WithHelpers, to help debug test failures.

--- modules/sugar/src/test/ts/module/ephox/sugar/test/WithHelpers.ts
+++ modules/sugar/src/test/ts/module/ephox/sugar/test/WithHelpers.ts
@@ -1,8 +1,16 @@
 import Element from 'ephox/sugar/api/node/Element';
-import { document, Element as DomElement, HTMLIFrameElement, ShadowRoot, Window } from '@ephox/dom-globals';
+import {
+  document,
+  Element as DomElement,
+  HTMLElement,
+  HTMLIFrameElement,
+  ShadowRoot,
+  Window
+} from '@ephox/dom-globals';
 import * as Insert from 'ephox/sugar/api/dom/Insert';
 import * as Body from 'ephox/sugar/api/node/Body';
 import * as Remove from 'ephox/sugar/api/dom/Remove';
+import * as Attr from 'ephox/sugar/api/properties/Attr';
 
 export const withNormalElement = (f: (d: Element<DomElement>) => void): void => {
   const div = Element.fromTag('div');
@@ -12,18 +20,21 @@ export const withNormalElement = (f: (d: Element<DomElement>) => void): void => 
   Remove.remove(div);
 };
 
-const withShadowElementInMode = (mode: 'open' | 'closed', f: (sr: Element<ShadowRoot>, innerDiv: Element<DomElement>, shadowHost: Element<DomElement>) => void) => {
-  const shadowHost = Element.fromTag('div', document);
+const withShadowElementInMode = (mode: 'open' | 'closed', f: (sr: Element<ShadowRoot>, innerDiv: Element<HTMLElement>, shadowHost: Element<HTMLElement>) => void) => {
+  const shadowHost: Element<HTMLElement> = Element.fromTag('div', document);
+  Attr.set(shadowHost, 'data-blah', 'shadow-host');
+
   Insert.append(Body.body(), shadowHost);
-  const sr = Element.fromDom(shadowHost.dom().attachShadow({ mode }));
-  const innerDiv = Element.fromTag('div', document);
+  const sr: Element<ShadowRoot> = Element.fromDom(shadowHost.dom().attachShadow({ mode }));
+  const innerDiv: Element<HTMLElement> = Element.fromTag('div', document);
+  Attr.set(innerDiv, 'data-blah', 'div-in-shadow-root');
 
   Insert.append(sr, innerDiv);
   f(sr, innerDiv, shadowHost);
   Remove.remove(shadowHost);
 };
 
-export const withShadowElement = (f: (shadowRoot: Element<ShadowRoot>, innerDiv: Element<DomElement>, shadowHost: Element<DomElement>) => void): void => {
+export const withShadowElement = (f: (shadowRoot: Element<ShadowRoot>, innerDiv: Element<HTMLElement>, shadowHost: Element<HTMLElement>) => void): void => {
   withShadowElementInMode('open', f);
   withShadowElementInMode('closed', f);
 };
