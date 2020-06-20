5c9e99257f4c0f98f65f2fa46a6c24e9346bf30f
Dylan Just <dylan.just@tiny.cloud>
2020-06-20 20:49:15 +1000

    Remove Theme call to avoid compile error.

--- modules/mcagar/src/test/ts/browser/api/TinyLoaderErrorTest.ts
+++ modules/mcagar/src/test/ts/browser/api/TinyLoaderErrorTest.ts
@@ -1,10 +1,7 @@
 import { UnitTest } from '@ephox/bedrock-client';
 import * as TinyLoader from 'ephox/mcagar/api/TinyLoader';
-import Theme from 'tinymce/lib/themes/silver/main/ts/Theme';
 
 UnitTest.asynctest('TinyLoader should fail (instead of timeout) when exception is thrown in callback function', (success, failure) => {
-  Theme();
-
   TinyLoader.setup(() => {
     throw new Error('boo!');
   }, { base_url: '/project/tinymce/js/tinymce' }, failure, success);
