fbc50ddcea0e0d573a593821bfff44af6a425c2f
Dylan Just <dylan.just@tiny.cloud>
2020-06-22 16:27:41 +1000

    Skip unsupported browsers in withShadowElementInMode

--- modules/sugar/src/test/ts/module/ephox/sugar/test/WithHelpers.ts
+++ modules/sugar/src/test/ts/module/ephox/sugar/test/WithHelpers.ts
@@ -1,8 +1,17 @@
 import Element from 'ephox/sugar/api/node/Element';
-import { document, Element as DomElement, HTMLIFrameElement, ShadowRoot, Window } from '@ephox/dom-globals';
+import {
+  document,
+  Element as DomElement,
+  HTMLElement,
+  HTMLIFrameElement,
+  ShadowRoot,
+  Window
+} from '@ephox/dom-globals';
 import * as Insert from 'ephox/sugar/api/dom/Insert';
 import * as Body from 'ephox/sugar/api/node/Body';
 import * as Remove from 'ephox/sugar/api/dom/Remove';
+import * as Attr from 'ephox/sugar/api/properties/Attr';
+import { ShadowDom } from '@ephox/sugar';
 
 export const withNormalElement = (f: (d: Element<DomElement>) => void): void => {
   const div = Element.fromTag('div');
@@ -12,22 +21,26 @@ export const withNormalElement = (f: (d: Element<DomElement>) => void): void => 
   Remove.remove(div);
 };
 
-const withShadowElementInMode = (mode: 'open' | 'closed', f: (sr: Element<ShadowRoot>, innerDiv: Element<DomElement>, shadowHost: Element<DomElement>) => void) => {
-  const shadowHost = Element.fromTag('div', document);
-  Insert.append(Body.body(), shadowHost);
-  const sr = Element.fromDom(shadowHost.dom().attachShadow({ mode }));
-  const innerDiv = Element.fromTag('div', document);
+const withShadowElementInMode = (mode: 'open' | 'closed', f: (sr: Element<ShadowRoot>, innerDiv: Element<HTMLElement>, shadowHost: Element<HTMLElement>) => void) => {
+  if (ShadowDom.isSupported()) {
+    const shadowHost: Element<HTMLElement> = Element.fromTag('div', document);
+    Attr.set(shadowHost, 'data-blah', 'shadow-host');
 
-  Insert.append(sr, innerDiv);
-  f(sr, innerDiv, shadowHost);
-  Remove.remove(shadowHost);
+    Insert.append(Body.body(), shadowHost);
+    const sr: Element<ShadowRoot> = Element.fromDom(shadowHost.dom().attachShadow({ mode }));
+    const innerDiv: Element<HTMLElement> = Element.fromTag('div', document);
+    Attr.set(innerDiv, 'data-blah', 'div-in-shadow-root');
+
+    Insert.append(sr, innerDiv);
+    f(sr, innerDiv, shadowHost);
+    Remove.remove(shadowHost);
+  }
 };
 
-export const withShadowElement = (f: (shadowRoot: Element<ShadowRoot>, innerDiv: Element<DomElement>, shadowHost: Element<DomElement>) => void): void => {
+export const withShadowElement = (f: (shadowRoot: Element<ShadowRoot>, innerDiv: Element<HTMLElement>, shadowHost: Element<HTMLElement>) => void): void => {
   withShadowElementInMode('open', f);
   withShadowElementInMode('closed', f);
 };
-
 
 export const withIframe = (f: (div: Element<DomElement>, iframe: Element<HTMLIFrameElement>, cw: Window) => void): void => {
   const iframe = Element.fromTag('iframe');
