52156560aca27f6deb70d5dcd14d59424008f359
Lee Newson <lee.newson@tiny.cloud>
2020-07-03 13:55:10 +1000

    TINY-6073: Fixed `editor.selection.setContent()` API not running parser filters

--- tests/tinymce/dom/Selection.js
+++ tests/tinymce/dom/Selection.js
@@ -75,6 +75,15 @@ ModuleLoader.require([
 		editor.selection.setContent('<div>test</div>');
 		equal(editor.getContent(), '<div>test</div>', 'Set contents at selection');
 
+		// Insert XSS at selection
+		editor.setContent('<p>text</p>');
+		rng = editor.dom.createRng();
+		rng.setStart(editor.getBody(), 0);
+		rng.setEnd(editor.getBody(), 1);
+		editor.selection.setRng(rng);
+		editor.selection.setContent('<img src="a" onerror="alert(1)" />');
+		equal(editor.getContent(), '<img src="a" />', 'Set XSS at selection');
+
 		// Set contents at selection (collapsed)
 		editor.setContent('<p>text</p>');
 		rng = editor.dom.createRng();
