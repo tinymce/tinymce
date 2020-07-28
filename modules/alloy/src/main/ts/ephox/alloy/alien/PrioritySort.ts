8d59f39d3250df7fc606a3cd56fe3a26391d1b30
Lee Newson <lee.newson@tiny.cloud>
2020-07-28 16:50:23 +1000

    TINY-6275: Removed the thunk from Alloy ComponentEvents

--- modules/alloy/src/main/ts/ephox/alloy/alien/PrioritySort.ts
+++ modules/alloy/src/main/ts/ephox/alloy/alien/PrioritySort.ts
@@ -1,10 +1,10 @@
 import { Arr, Result } from '@ephox/katamari';
 
-const sortKeys = <T extends Record<string, () => any>, K extends keyof T>(label: string, keyName: K, array: T[], order: string[]): Result<T[], Error[]> => {
+const sortKeys = <T extends Record<string, any>, K extends keyof T>(label: string, keyName: K, array: T[], order: string[]): Result<T[], Error[]> => {
   try {
     const sorted = Arr.sort(array, (a, b) => {
-      const aKey: string = a[keyName]();
-      const bKey: string = b[keyName]();
+      const aKey: string = a[keyName];
+      const bKey: string = b[keyName];
       const aIndex = order.indexOf(aKey);
       const bIndex = order.indexOf(bKey);
       if (aIndex === -1) {
