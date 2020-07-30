658b0555dcc1f2ebf90c6f3e1108ba166cd0c101
Tyler Kelly <tyler.kelly@tiny.cloud>
2020-07-30 16:14:12 +1000

    ghdocs#1619: Add docs for XHR send API

--- modules/tinymce/src/core/main/ts/api/util/XHR.ts
+++ modules/tinymce/src/core/main/ts/api/util/XHR.ts
@@ -15,7 +15,7 @@ export interface XHRSettings {
   async?: boolean;
   content_type?: string;
   crossDomain?: boolean;
-  data?: string;
+  data?: any;
   requestheaders?: Record<string, { key: string; value: string}>;
   scope?: {};
   type?: string;
@@ -35,7 +35,7 @@ interface XHR extends Observable<XHREventMap> {
 }
 
 /**
- * This class enables you to send XMLHTTPRequests cross browser.
+ * This API allows you to send XMLHTTPRequests cross browser.
  * @class tinymce.util.XHR
  * @mixes tinymce.util.Observable
  * @static
@@ -59,10 +59,152 @@ const XHR: XHR = {
 
   /**
    * Sends a XMLHTTPRequest.
-   * Consult the TinyMCE Documentation for details on what settings this method takes.
    *
    * @method send
-   * @param {Object} settings Object will target URL, callbacks and other info needed to make the request.
+   * @param {Object} settings Object will target URL, callbacks and other info needed to make the request.<br />
+   * <div>
+   * <table>
+   * <thead>
+   * <tr style="text-align: center;">
+   * <th>Setting</th>
+   * <th>Required/<br />Optional</th>
+   * <th>Type</th>
+   * <th>Description</th>
+   * </tr>
+   * </thead>
+   * <tbody>
+   * <tr>
+   * <td>
+   * <p><code>url</code></p>
+   * </td>
+   * <td>Required</td>
+   * <td>
+   * <p><code>string</code></p>
+   * </td>
+   * <td>Address to send the request to such as the back-end server.</td>
+   * </tr>
+   * <tr>
+   * <td>
+   * <p><code>async<br /></code></p>
+   * </td>
+   * <td>Optional</td>
+   * <td>
+   * <p><code>boolean</code></p>
+   * </td>
+   * <td>When <code>false</code>, the request will be synchronous. Set to <code>true</code> by default.</td>
+   * </tr>
+   * <tr>
+   * <td>
+   * <p><code>content_type<br /></code></p>
+   * </td>
+   * <td>Optional</td>
+   * <td>
+   * <p><code>string</code></p>
+   * </td>
+   * <td>Used to define the mime-type of the data. Similar to
+   * <a href="https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/overrideMimeType">
+   * the XMLHttpRequest.overrideMimeType method</a>.</td>
+   * </tr>
+   * <tr>
+   * <td>
+   * <p><code>crossDomain<br /></code></p>
+   * </td>
+   * <td>Optional</td>
+   * <td>
+   * <p><code>boolean</code></p>
+   * </td>
+   * <td>When <code>true</code>, withCredentials property will be set to <code>true</code>.
+   * For information on the withCredentials property, see:
+   * <a href="https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/withCredentials">
+   * MDN Web Docs - XMLHttpRequest.withCredentials</a>.</td>
+   * </tr>
+   * <tr>
+   * <td>
+   * <p><code>data<br /></code></p>
+   * </td>
+   * <td>Optional</td>
+   * <td>
+   * <p><code>any</code></p>
+   * </td>
+   * <td>The data to be sent to the specified URL (<code>url</code>). For information on valid data inputs,
+   *  see: <a href="https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/send#Syntax">
+   *  MDN Web Docs - XMLHttpRequest.send() Syntax</a></td>
+   * </tr>
+   * <tr>
+   * <td>
+   * <p><code>requestheaders</code></p>
+   * </td>
+   * <td>Optional</td>
+   * <td>
+   * <p><code>Record&lt;string, { key: string; value: string }&gt;</code></p>
+   * </td>
+   * <td>Allows for the definition of additional
+   *  <a href="https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/setRequestHeader">
+   *  header-values for the request header</a>.</td>
+   * </tr>
+   * <tr>
+   * <td>
+   * <p><code>type</code></p>
+   * </td>
+   * <td>Optional</td>
+   * <td>
+   * <p><code>string</code></p>
+   * </td>
+   * <td>
+   * <p>The <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods">HTTP request method</a>,
+   *  such as: <code>'GET'</code>, <code>'POST'</code>, <code>'PUT'</code> and <code>'DELETE'</code>.</p>
+   * <p>By default:</p>
+   * <ul>
+   * <li>If no data is provided with the request, the <code>'GET'</code> method will be used.</li>
+   * <li>If data is provided with the request, the <code>'POST'</code> method will be used.</li>
+   * </ul>
+   * </td>
+   * </tr>
+   * <tr>
+   * <td>
+   * <p><code>error_scope</code></p>
+   * </td>
+   * <td>Optional</td>
+   * <td>
+   * <p><code>object</code></p>
+   * </td>
+   * <td>Sets the <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this">
+   * '<code>this</code>'</a> operator of the error callback function.</td>
+   * </tr>
+   * <tr>
+   * <td>
+   * <p><code>success_scope<br /></code></p>
+   * </td>
+   * <td>Optional</td>
+   * <td>
+   * <p><code>object</code></p>
+   * </td>
+   * <td>Sets the <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this">
+   * '<code>this</code>'</a> operator of the success callback function.</td>
+   * </tr>
+   * <tr>
+   * <td>
+   * <p><code>error<br /></code></p>
+   * </td>
+   * <td>Optional</td>
+   * <td>
+   * <p><code>callback function</code></p>
+   * </td>
+   * <td>The callback function called when an error occurs.</td>
+   * </tr>
+   * <tr>
+   * <td>
+   * <p><code>success</code></p>
+   * </td>
+   * <td>Optional</td>
+   * <td>
+   * <p><code>callback function</code></p>
+   * </td>
+   * <td>The callback function called when the request is successful.</td>
+   * </tr>
+   * </tbody>
+   * </table>
+   * </div>
    */
   send(settings: XHRSettings) {
     let xhr, count = 0;
