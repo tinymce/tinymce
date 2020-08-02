22e71a2a76f4ef65f9ee1e038dc847ba4d55ef5b
Lee Newson <lee.newson@tiny.cloud>
2020-08-02 17:24:44 +1000

    TINY-6286: Fixed a regression causing list toolbar items to not show the active state

--- modules/tinymce/src/plugins/lists/test/ts/browser/ListPropertiesTest.ts
+++ modules/tinymce/src/plugins/lists/test/ts/browser/ListPropertiesTest.ts
@@ -52,7 +52,7 @@ UnitTest.asynctest('tinymce.lists.browser.ListPropertiesTest', (success, failure
 
   const sAssertListPropMenuNotVisible = UiFinder.sNotExists(Body.body(), contentMenuSelector);
 
-  TinyLoader.setupLight((editor, onSuccess, onFailure) => {
+  TinyLoader.setup((editor, onSuccess, onFailure) => {
     const tinyApis = TinyApis(editor);
     const tinyUi = TinyUi(editor);
 
@@ -107,11 +107,30 @@ UnitTest.asynctest('tinymce.lists.browser.ListPropertiesTest', (success, failure
         sUpdateStart('1', '10'),
         tinyUi.sSubmitDialog('[role=dialog]'),
         tinyApis.sAssertContent('<ol start="10"><li>Root Item<ol><li>Item 1</li><li>Item 2</li></ol></li></ol>')
+      ]),
+      Log.stepsAsStep('TINY-6286', 'List properties menu item disabled for UL', [
+        tinyApis.sSetContent('<ul><li>Item 1</li><li>Item 2</li></ul>'),
+        tinyApis.sSetCursor([ 0, 0, 0 ], 0),
+        tinyUi.sClickOnMenu('Open custom menu', '.tox-mbtn:contains("Custom")'),
+        tinyUi.sWaitForUi('Check menu item is disabled', '.tox-collection__item--state-disabled:contains("List properties")'),
+        tinyUi.sClickOnMenu('Close custom menu', '.tox-mbtn:contains("Custom")')
+      ]),
+      Log.stepsAsStep('TINY-6286', 'List properties menu item enabled for OL', [
+        tinyApis.sSetContent('<ol><li>Item 1</li><li>Item 2</li></ol>'),
+        tinyApis.sSetCursor([ 0, 0, 0 ], 0),
+        tinyUi.sClickOnMenu('Open custom menu', '.tox-mbtn:contains("Custom")'),
+        tinyUi.sWaitForUi('Check menu item is enabled', '.tox-collection__item:contains("List properties"):not(.tox-collection__item--state-disabled)'),
+        tinyUi.sClickOnMenu('Close custom menu', '.tox-mbtn:contains("Custom")')
       ])
     ], onSuccess, onFailure);
   }, {
     plugins: 'lists',
     contextmenu: 'lists bold',
+    menu: {
+      custom: { title: 'Custom', items: 'listprops' }
+    },
+    menubar: 'custom',
+    toolbar: false,
     indent: false,
     theme: 'silver',
     base_url: '/project/tinymce/js/tinymce'
