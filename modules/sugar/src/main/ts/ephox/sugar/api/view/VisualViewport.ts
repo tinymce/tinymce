2b1488716ded987c42278a15169f4341d2d55d4b
Lee Newson <lee.newson@tiny.cloud>
2020-08-09 10:18:26 +1000

    TINY-3511: Fixed the visual viewport bind function not working, which also caused fullscreen plugin issues

--- modules/sugar/src/main/ts/ephox/sugar/api/view/VisualViewport.ts
+++ modules/sugar/src/main/ts/ephox/sugar/api/view/VisualViewport.ts
@@ -25,6 +25,7 @@ interface VisualViewport {
   readonly scale: number;
   readonly addEventListener: (event: string, handler: EventListenerOrEventListenerObject) => void;
   readonly removeEventListener: (event: string, handler: EventListenerOrEventListenerObject) => void;
+  readonly dispatchEvent: (evt: Event) => boolean;
 }
 
 const get = (_win?: Window): Option<VisualViewport> => {
@@ -64,7 +65,7 @@ const getBounds = (_win?: Window): Bounds => {
 };
 
 const bind = (name: string, callback: EventHandler, _win?: Window) => get(_win).map((visualViewport) => {
-  const handler = (e: Event) => fromRawEvent(e);
+  const handler = (e: Event) => callback(fromRawEvent(e));
   visualViewport.addEventListener(name, handler);
 
   return {
