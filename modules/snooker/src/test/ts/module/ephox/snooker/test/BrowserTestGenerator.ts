bb61d480b843aa3740eca0cec80f58f0cb6fee2d
Millie Macdonald <millie.macdonald@tiny.cloud>
2020-09-27 23:24:17 +1000

    TINY-6050 Switch ModificationOperationsTest to use real elements

---
+++ modules/snooker/src/test/ts/module/ephox/snooker/test/BrowserTestGenerator.ts
@@ -0,0 +1,42 @@
+import { Html, SugarElement, SugarNode } from '@ephox/sugar';
+import { SimpleGenerators } from 'ephox/snooker/api/Generators';
+
+export default function (): SimpleGenerators {
+  let cellCounter = 0;
+  let colCounter = 0;
+  let replaceCounter = 0;
+
+  const makeElem = (tag: string, content: string) => {
+    const elem = SugarElement.fromTag(tag);
+    Html.set(elem, content);
+    return elem;
+  };
+
+  const cell = function () {
+    const r = '?_' + cellCounter;
+    cellCounter++;
+    return makeElem('td', r);
+  };
+
+  const col = () => {
+    const r = '?_' + colCounter;
+    colCounter++;
+    return makeElem('col', r);
+  };
+
+  const replace = function (cell: SugarElement) {
+    const tag = SugarNode.name(cell);
+    const r = 'h(' + tag + ')_' + replaceCounter;
+    replaceCounter++;
+    return makeElem(tag, r);
+  };
+
+  return {
+    cell,
+    gap: () => makeElem('td', '*'),
+    row: () => makeElem('tr', ''),
+    colgroup: () => makeElem('colgroup', ''),
+    col,
+    replace
+  };
+}
