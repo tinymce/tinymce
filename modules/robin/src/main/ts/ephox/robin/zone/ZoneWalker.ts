bc642d863fc5c05a72306d413e52e31f95f2c4e5
Neil Ashford <neil.ashford@tiny.cloud>
2020-10-08 13:02:38 +1000

    TINY-6412: Refactor ZoneWalker part 6
    Here we delete the ZoneWalkerState ADT / combine the analyse + process functions.
    As the traversal logic got pulled up to the walk function in the last two
    commits I noticed that the true "state" we were really passing around was the
    Traverse object. The ZoneWalkerState was only ever created to get passed from
    analyse to process. That's been taken out now.

--- modules/robin/src/main/ts/ephox/robin/zone/ZoneWalker.ts
+++ modules/robin/src/main/ts/ephox/robin/zone/ZoneWalker.ts
@@ -1,109 +1,55 @@
 import { Universe } from '@ephox/boss';
-import { Adt, Fun, Optional } from '@ephox/katamari';
-import { Gather, Transition, Traverse } from '@ephox/phoenix';
+import { Fun, Optional } from '@ephox/katamari';
+import { Gather, Traverse } from '@ephox/phoenix';
 import { ZonePosition } from '../api/general/ZonePosition';
 import { ZoneViewports } from '../api/general/ZoneViewports';
 import { WordDecisionItem } from '../words/WordDecision';
 import { LanguageZones, ZoneDetails } from './LanguageZones';
 
-interface ZoneWalkerState<E> {
-  fold: <T> (
-    inline: (item: E, mode: Transition, lang: Optional<string>) => T,
-    text: (item: E, mode: Transition) => T,
-    empty: (item: E, mode: Transition) => T,
-    boundary: (item: E, mode: Transition, lang: Optional<string>) => T,
-  ) => T;
-  match: <T> (branches: {
-    inline: (item: E, mode: Transition, lang: Optional<string>) => T;
-    text: (item: E, mode: Transition) => T;
-    empty: (item: E, mode: Transition) => T;
-    boundary: (item: E, mode: Transition, lang: Optional<string>) => T;
-  }) => T;
-  log: (label: string) => void;
-}
-
-const adt: {
-  inline: <E> (item: E, mode: Transition, lang: Optional<string>) => ZoneWalkerState<E>;
-  text: <E> (item: E, mode: Transition) => ZoneWalkerState<E>;
-  empty: <E> (item: E, mode: Transition) => ZoneWalkerState<E>;
-  boundary: <E> (item: E, mode: Transition, lang: Optional<string>) => ZoneWalkerState<E>;
-} = Adt.generate([
-  // an inline element, so use the lang to identify if a new zone is needed
-  { inline: [ 'item', 'mode', 'lang' ] },
-  { text: [ 'item', 'mode' ] },
-  // things like <img>, <br>
-  { empty: [ 'item', 'mode' ] },
-  // things like boundary tags
-  { boundary: [ 'item', 'mode', 'lang' ] }
-]);
-
-const analyse = <E, D> (
+const process = <E, D> (
   universe: Universe<E, D>,
+  stack: LanguageZones<E>,
+  transform: (universe: Universe<E, D>, item: E) => WordDecisionItem<E>,
+  viewport: ZoneViewports<E>,
   traverse: Traverse<E>
-): ZoneWalkerState<E> => {
+): Optional<Traverse<E>> => {
   // Find if the current item has a lang property on it.
   const currentLang = universe.property().isElement(traverse.item) ?
     Optional.from(universe.attrs().get(traverse.item, 'lang')) :
     Optional.none<string>();
 
   if (universe.property().isText(traverse.item)) {
-    return adt.text(traverse.item, traverse.mode);
+    stack.addDetail(transform(universe, traverse.item));
+    return Optional.some(traverse);
   } else if (universe.property().isBoundary(traverse.item)) {
-    return adt.boundary(traverse.item, traverse.mode, currentLang);
-  } else if (universe.property().isEmptyTag(traverse.item)) {
-    return adt.empty(traverse.item, traverse.mode);
-  } else {
-    return adt.inline(traverse.item, traverse.mode, currentLang);
-  }
-};
-
-const process = <E, D> (
-  universe: Universe<E, D>,
-  outcome: ZoneWalkerState<E>,
-  stack: LanguageZones<E>,
-  transform: (universe: Universe<E, D>, item: E) => WordDecisionItem<E>,
-  viewport: ZoneViewports<E>
-): Optional<Traverse<E>> => outcome.fold(
-  (aItem, aMode, aLang) => {
-    // inline(aItem, aMode, aLang)
-    const opening = aMode === Gather.advance;
-    (opening ? stack.openInline : stack.closeInline)(aLang, aItem);
-    return Optional.some({ item: aItem, mode: aMode });
-  },
-  (aItem, aMode) => {
-    const detail = transform(universe, aItem);
-    // text (aItem, aMode)
-    stack.addDetail(detail);
-    return Optional.some({ item: aItem, mode: aMode });
-  },
-  (aItem, aMode) => {
-    // empty (aItem, aMode)
-    stack.addEmpty(aItem);
-    return Optional.some({ item: aItem, mode: aMode });
-  },
-  (aItem, aMode, aLang) => {
-    // Use boundary positions to assess whether we have moved out of the viewport.
-    const position = viewport.assess(aItem);
+    const position = viewport.assess(traverse.item);
     return ZonePosition.cata(position,
       (_aboveBlock) => {
-        // We are before the viewport, so skip
-        // Only sidestep if we hadn't already tried it. Otherwise, we'll loop forever.
-        if (aMode !== Gather.backtrack) {
-          return Optional.some({ item: aItem, mode: Gather.sidestep });
+        // We are above the viewport, so skip
+        // Only sidestep if we haven't already tried it. Otherwise, we'll loop forever.
+        if (traverse.mode !== Gather.backtrack) {
+          return Optional.some({ item: traverse.item, mode: Gather.sidestep });
         } else {
           return Optional.none();
         }
       },
       (_inBlock) => {
         // We are in the viewport, so process normally
-        const opening = aMode === Gather.advance;
-        (opening ? stack.openBoundary : stack.closeBoundary)(aLang, aItem);
-        return Optional.some({ item: aItem, mode: aMode });
+        const opening = traverse.mode === Gather.advance;
+        (opening ? stack.openBoundary : stack.closeBoundary)(currentLang, traverse.item);
+        return Optional.some(traverse);
       },
       (_belowBlock) => Optional.none() // We've gone past the end of the viewport, so stop completely
     );
-  },
-);
+  } else if (universe.property().isEmptyTag(traverse.item)) {
+    stack.addEmpty(traverse.item);
+    return Optional.some(traverse);
+  } else {
+    const opening = traverse.mode === Gather.advance;
+    (opening ? stack.openInline : stack.closeInline)(currentLang, traverse.item);
+    return Optional.some(traverse);
+  }
+};
 
 const walk = <E, D> (
   universe: Universe<E, D>,
@@ -127,16 +73,14 @@ const walk = <E, D> (
 
   // INVESTIGATE: Make the language zone stack immutable *and* performant
   const stack = LanguageZones.nu<E>(defaultLang);
-  let state = Optional.some({ item: start, mode: Gather.advance })
-    .filter(shouldContinue)
-    .map(Fun.curry(analyse, universe));
+  let state = Optional.some<Traverse<E>>({ item: start, mode: Gather.advance })
+    .filter(shouldContinue);
 
   while (state.isSome()) {
     state = state
-      .bind((state) => process(universe, state, stack, transform, viewport))
+      .bind(Fun.curry(process, universe, stack, transform, viewport))
       .bind((traverse) => Gather.walk(universe, traverse.item, traverse.mode, Gather.walkers().right()))
-      .filter(shouldContinue)
-      .map(Fun.curry(analyse, universe));
+      .filter(shouldContinue);
   }
 
   if (universe.property().isText(finish)) {
