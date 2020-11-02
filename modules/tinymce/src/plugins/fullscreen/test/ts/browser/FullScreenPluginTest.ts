d5f8a282e0ad76314ec92fe279bfe1c969ad95cd
Luke Trouton <luke.trouton@tiny.cloud>
2020-11-02 13:19:28 +1000

    TINY-6280: Rewrite test to use named chains

--- modules/tinymce/src/plugins/fullscreen/test/ts/browser/FullScreenPluginTest.ts
+++ modules/tinymce/src/plugins/fullscreen/test/ts/browser/FullScreenPluginTest.ts
@@ -1,7 +1,7 @@
-import { Assertions, Chain, Guard, Log, Pipeline, UiFinder } from '@ephox/agar';
+import { Assertions, Chain, Log, NamedChain, Pipeline, UiFinder } from '@ephox/agar';
 import { UnitTest } from '@ephox/bedrock-client';
 import { Cell } from '@ephox/katamari';
-import { Editor as McEditor, TinyLoader } from '@ephox/mcagar';
+import { ApiChains, Editor as McEditor, TinyLoader } from '@ephox/mcagar';
 import { Attribute, Classes, Css, Html, SelectorFind, SugarBody, SugarElement, SugarShadowDom } from '@ephox/sugar';
 import Editor from 'tinymce/core/api/Editor';
 
@@ -12,32 +12,38 @@ import SilverTheme from 'tinymce/themes/silver/Theme';
 const getContentContainer = (editor: Editor) =>
   SugarShadowDom.getContentContainer(SugarShadowDom.getRootNode(SugarElement.fromDom(editor.getElement())));
 
-const cCloseOnlyWindow = Chain.control(
-  Chain.op((editor: Editor) => {
-    const dialogs = () => UiFinder.findAllIn(getContentContainer(editor), '[role="dialog"]');
-    Assertions.assertEq('One window exists', 1, dialogs().length);
-    editor.windowManager.close();
-    Assertions.assertEq('No windows exist', 0, dialogs().length);
-  }),
-  Guard.addLogging('Close window')
+const cCloseOnlyWindow = Chain.label(
+  'Close window',
+  Chain.fromChains([
+    NamedChain.read('editor',
+      Chain.op((editor: Editor) => {
+        const dialogs = () => UiFinder.findAllIn(getContentContainer(editor), '[role="dialog"]');
+        Assertions.assertEq('One window exists', 1, dialogs().length);
+        editor.windowManager.close();
+        Assertions.assertEq('No windows exist', 0, dialogs().length);
+      })
+    )
+  ])
 );
 
 const cWaitForDialog = (ariaLabel: string) =>
-  Chain.control(
+  Chain.label(
+    'Looking for dialog with an aria-label: ' + ariaLabel,
     Chain.fromChains([
-      Chain.mapper(getContentContainer),
-      UiFinder.cWaitFor('Waiting for dialog', '[role="dialog"]'),
-      Chain.op((dialog) => {
-        if (Attribute.has(dialog, 'aria-labelledby')) {
-          const labelledby = Attribute.get(dialog, 'aria-labelledby');
-          const dialogLabel = SelectorFind.descendant<HTMLLabelElement>(dialog, '#' + labelledby).getOrDie('Could not find labelledby');
-          Assertions.assertEq('Checking label text', ariaLabel, Html.get(dialogLabel));
-        } else {
-          throw new Error('Dialog did not have an aria-labelledby');
-        }
-      })
-    ]),
-    Guard.addLogging('Looking for dialog with an aria-label: ' + ariaLabel)
+      NamedChain.direct('editor', Chain.mapper(getContentContainer), 'contentContainer'),
+      NamedChain.direct('contentContainer', UiFinder.cWaitFor('Waiting for dialog', '[role="dialog"]'), 'dialog'),
+      NamedChain.read('dialog',
+        Chain.op((dialog) => {
+          if (Attribute.has(dialog, 'aria-labelledby')) {
+            const labelledby = Attribute.get(dialog, 'aria-labelledby');
+            const dialogLabel = SelectorFind.descendant<HTMLLabelElement>(dialog, '#' + labelledby).getOrDie('Could not find labelledby');
+            Assertions.assertEq('Checking label text', ariaLabel, Html.get(dialogLabel));
+          } else {
+            throw new Error('Dialog did not have an aria-labelledby');
+          }
+        })
+      )
+    ])
   );
 
 UnitTest.asynctest('browser.tinymce.plugins.fullscreen.FullScreenPluginTest', (success, failure) => {
@@ -51,7 +57,9 @@ UnitTest.asynctest('browser.tinymce.plugins.fullscreen.FullScreenPluginTest', (s
     Chain.label(
       `${label}: fullscreen API and event state should return ${state}`,
       Chain.fromChains([
-        Chain.op((editor) => Assertions.assertEq('Editor isFullscreen', state, editor.plugins.fullscreen.isFullscreen())),
+        NamedChain.read('editor', Chain.op((editor) => {
+          Assertions.assertEq('Editor isFullscreen', state, editor.plugins.fullscreen.isFullscreen());
+        })),
         Chain.op(() => Assertions.assertEq('FullscreenStateChanged event', state, lastEventArgs.get().state))
       ])
     );
@@ -60,11 +68,11 @@ UnitTest.asynctest('browser.tinymce.plugins.fullscreen.FullScreenPluginTest', (s
     const selector = shouldExist ? 'root:.tox-fullscreen' : 'root::not(.tox-fullscreen)';
     return Chain.label(
       `${label}: Body and Html should ${shouldExist ? '' : 'not'} have "tox-fullscreen" class`,
-      Chain.fromIsolatedChains([
-        Chain.inject(SugarBody.body()),
-        UiFinder.cFindIn(selector),
-        Chain.inject(SugarElement.fromDom(document.documentElement)),
-        UiFinder.cFindIn(selector)
+      Chain.fromChains([
+        NamedChain.writeValue('docBody', SugarBody.body()),
+        NamedChain.read('docBody', UiFinder.cFindIn(selector)),
+        NamedChain.writeValue('docHTML', SugarElement.fromDom(document.documentElement)),
+        NamedChain.read('docHTML', UiFinder.cFindIn(selector))
       ])
     );
   };
@@ -73,36 +81,39 @@ UnitTest.asynctest('browser.tinymce.plugins.fullscreen.FullScreenPluginTest', (s
     Chain.label(
       `${label}: Editor container and sink should ${shouldExist ? '' : 'not'} have "tox-fullscreen" class and z-index`,
       Chain.fromChains([
-        Chain.fromIsolatedChains([
-          Chain.mapper((editor: Editor) => SugarElement.fromDom(editor.getContainer())),
-          UiFinder.cFindIn(shouldExist ? 'root:.tox-fullscreen' : 'root::not(.tox-fullscreen)'),
+        NamedChain.direct('editor', Chain.mapper((editor: Editor) => SugarElement.fromDom(editor.getContainer())), 'editorContainer'),
+        NamedChain.read('editorContainer', UiFinder.cFindIn(shouldExist ? 'root:.tox-fullscreen' : 'root::not(.tox-fullscreen)')),
+        NamedChain.read('editorContainer',
           Chain.op((container) => {
             Assertions.assertEq('Editor container z-index', shouldExist ? '1200' : 'auto', Css.get(container, 'z-index'));
           })
-        ]),
-        Chain.fromIsolatedChains([
-          Chain.mapper(getContentContainer),
-          UiFinder.cFindIn('.tox-silver-sink.tox-tinymce-aux'),
+        ),
+        NamedChain.direct('editor', Chain.mapper(getContentContainer), 'contentContainer'),
+        NamedChain.direct('contentContainer', UiFinder.cFindIn('.tox-silver-sink.tox-tinymce-aux'), 'sink'),
+        NamedChain.read('sink',
           Chain.op((sink) => {
             Assertions.assertEq('Editor sink z-index', shouldExist ? '1201' : '1300', Css.get(sink, 'z-index'));
           })
-        ])
+        )
       ])
     );
 
   const cAssertShadowHostState = (label: string, shouldExist: boolean) =>
     Chain.label(
       `${label}: Shadow host should ${shouldExist ? '' : 'not'} have "tox-fullscreen" and "tox-shadowhost" classes and z-index`,
-      Chain.op((editor: Editor) => {
-        if (SugarShadowDom.isInShadowRoot(SugarElement.fromDom(editor.getElement()))) {
-          const host = SugarShadowDom.getShadowRoot(SugarElement.fromDom(editor.getElement()))
-            .map(SugarShadowDom.getShadowHost)
-            .getOrDie('Expected shadow host');
+      NamedChain.read('editor',
+        Chain.op((editor: Editor) => {
+          if (SugarShadowDom.isInShadowRoot(SugarElement.fromDom(editor.getElement()))) {
+            const host = SugarShadowDom.getShadowRoot(SugarElement.fromDom(editor.getElement()))
+              .map(SugarShadowDom.getShadowHost)
+              .getOrDie('Expected shadow host');
 
-          Assertions.assertEq('Shadow host classes', shouldExist, Classes.hasAll(host, [ 'tox-fullscreen', 'tox-shadowhost' ]));
-          Assertions.assertEq('Shadow host z-index', shouldExist ? '1200' : 'auto', Css.get(host, 'z-index'));
-        }
-      }));
+            Assertions.assertEq('Shadow host classes', shouldExist, Classes.hasAll(host, [ 'tox-fullscreen', 'tox-shadowhost' ]));
+            Assertions.assertEq('Shadow host z-index', shouldExist ? '1200' : 'auto', Css.get(host, 'z-index'));
+          }
+        })
+      )
+    );
 
   const cAssertPageState = (label: string, shouldExist: boolean) =>
     Chain.fromChains([
@@ -114,30 +125,30 @@ UnitTest.asynctest('browser.tinymce.plugins.fullscreen.FullScreenPluginTest', (s
   TinyLoader.setupInBodyAndShadowRoot((editor: Editor, onSuccess, onFailure) => {
     Pipeline.async({}, [
       Log.chainsAsStep('TBA', 'FullScreen: Toggle fullscreen on, open link dialog, insert link, close dialog and toggle fullscreen off', [
-        Chain.inject(editor),
-        Chain.fromParent(Chain.identity, [
+        NamedChain.asChain([
+          NamedChain.writeValue('editor', editor),
           cAssertPageState('Before fullscreen command', false),
-          Chain.op((editor: Editor) => editor.execCommand('mceFullScreen', true)),
+          NamedChain.read('editor', ApiChains.cExecCommand('mceFullScreen', true)),
           cAssertApiAndLastEvent('After fullscreen command', true),
           cAssertPageState('After fullscreen command', true),
-          Chain.op((editor: Editor) => editor.execCommand('mceLink', true)),
+          NamedChain.read('editor', ApiChains.cExecCommand('mceLink', true)),
           cWaitForDialog('Insert/Edit Link'),
           cCloseOnlyWindow,
           cAssertPageState('After window is closed', true),
-          Chain.op((editor: Editor) => editor.execCommand('mceFullScreen')),
+          NamedChain.read('editor', ApiChains.cExecCommand('mceFullScreen', null)),
           cAssertApiAndLastEvent('After fullscreen toggled', false),
           cAssertPageState('After fullscreen toggled', false)
         ])
       ]),
       Log.chainsAsStep('TBA', 'FullScreen: Toggle fullscreen and remove editor should clean up classes', [
-        Chain.inject(editor),
-        Chain.fromParent(Chain.identity, [
-          Chain.op((editor: Editor) => editor.execCommand('mceFullScreen', true)),
+        NamedChain.asChain([
+          NamedChain.writeValue('editor', editor),
+          NamedChain.read('editor', ApiChains.cExecCommand('mceFullScreen', true)),
           cAssertApiAndLastEvent('After fullscreen command', true),
-          cAssertPageState('After fullscreen command', true)
-        ]),
-        McEditor.cRemove,
-        cAssertHtmlAndBodyState('After editor is closed', false)
+          cAssertPageState('After fullscreen command', true),
+          NamedChain.read('editor', McEditor.cRemove),
+          cAssertHtmlAndBodyState('After editor is closed', false)
+        ])
       ])
     ], onSuccess, onFailure);
   }, {
