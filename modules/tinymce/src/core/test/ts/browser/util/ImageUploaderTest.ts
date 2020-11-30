1b69a110ff9350df5987ad412c02f319ed8a1b15
JamesToohey <james@toohey.io>
2020-11-30 14:47:09 +1100

    Minor test improvements

--- modules/tinymce/src/core/test/ts/browser/util/ImageUploaderTest.ts
+++ modules/tinymce/src/core/test/ts/browser/util/ImageUploaderTest.ts
@@ -17,6 +17,7 @@ UnitTest.asynctest('browser.tinymce.core.util.ImageUploaderTest', (success, fail
     const blob = new Blob([ JSON.stringify({ hello: 'world' }) ]);
 
     const image1 = cache.create({ blob, base64: 'test' });
+    const image2 = cache.create({ blob, base64: 'test-two' });
 
     const cUploadImages = (images: BlobInfo[], openNotification: boolean) => Chain.async((_value, next, die) => {
       const uploader = ImageUploader(editor);
@@ -24,7 +25,7 @@ UnitTest.asynctest('browser.tinymce.core.util.ImageUploaderTest', (success, fail
     });
 
     const cAssertUploadResultSuccess = (expectedLength: number, expectedUrl: string) => Chain.op((uploadResults: UploadResult[]) => {
-      Assert.eq('Test', expectedLength, uploadResults.length);
+      Assert.eq('Upload results length matches expected length', expectedLength, uploadResults.length);
       Arr.each(uploadResults, (uploadResult) => {
         Assert.eq('Url is Image.png', expectedUrl, uploadResult.url);
         Assert.eq('Upload result status is true upon success', true, uploadResult.status);
@@ -32,8 +33,8 @@ UnitTest.asynctest('browser.tinymce.core.util.ImageUploaderTest', (success, fail
     });
 
     const cAssertUploadResultFailure = (expectedLength: number, expectedUrl?: string, errorMsg?: string) => Chain.op((uploadResults: UploadResult[]) => {
-      Assert.eq('Test', expectedLength, uploadResults.length);
-      Arr.each( uploadResults, (uploadResult) => {
+      Assert.eq('Upload results length matches expected length', expectedLength, uploadResults.length);
+      Arr.each(uploadResults, (uploadResult) => {
         Assert.eq('Url is empty string upon failure', expectedUrl, uploadResult.url);
         Assert.eq('Upload result status is false upon failure', false, uploadResult.status);
         Assert.eq('Upload result error message matches failure message', errorMsg, uploadResult.error.message);
@@ -43,22 +44,34 @@ UnitTest.asynctest('browser.tinymce.core.util.ImageUploaderTest', (success, fail
     Pipeline.async({}, [
       Log.chainsAsStep('TINY-4601', 'Image upload success', [
         Chain.inject(editor),
-        ApiChains.cSetSetting('images_upload_handler', (_blobInfo: BlobInfo[], success) => success('https://tiny.cloud/image.png')),
+        ApiChains.cSetSetting('images_upload_handler', (_blobInfo: BlobInfo, success) => success('https://tiny.cloud/image.png')),
         cUploadImages([ image1 ], true),
         cAssertUploadResultSuccess(1, 'https://tiny.cloud/image.png')
       ]),
+      Log.chainsAsStep('TINY-4601', 'Multiple image upload success', [
+        Chain.inject(editor),
+        ApiChains.cSetSetting('images_upload_handler', (_blobInfo: BlobInfo, success) => success('https://tiny.cloud/image.png')),
+        cUploadImages([ image1, image2 ], true),
+        cAssertUploadResultSuccess(2, 'https://tiny.cloud/image.png')
+      ]),
       Log.chainsAsStep('TINY-4601', 'Image upload failure', [
         Chain.inject(editor),
-        ApiChains.cSetSetting('images_upload_handler', (_blobInfo: BlobInfo[], success, failure) => failure('Error msg')),
+        ApiChains.cSetSetting('images_upload_handler', (_blobInfo: BlobInfo, _success, failure) => failure('Error msg')),
         cUploadImages([ image1 ], true),
         cAssertUploadResultFailure(1, '', 'Error msg')
       ]),
       Log.chainsAsStep('TINY-4601', 'Image upload failure for empty array', [
         Chain.inject(editor),
-        ApiChains.cSetSetting('images_upload_handler', (_blobInfo: BlobInfo[], success, failure) => failure('Error msg')),
+        ApiChains.cSetSetting('images_upload_handler', (_blobInfo: BlobInfo, _success, failure) => failure('Error msg')),
         cUploadImages([ ], true),
         cAssertUploadResultFailure(0)
-      ])
+      ]),
+      Log.chainsAsStep('TINY-4601', 'Multiple image upload failure', [
+        Chain.inject(editor),
+        ApiChains.cSetSetting('images_upload_handler', (_blobInfo: BlobInfo, _success, failure) => failure('Error msg')),
+        cUploadImages([ image1, image2 ], true),
+        cAssertUploadResultFailure(2, '', 'Error msg')
+      ]),
     ], onSuccess, onFailure);
   }, {
     base_url: '/project/tinymce/js/tinymce'
