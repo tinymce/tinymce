7d2ef14f8a68d2390993dc7f853f2ec909539c71
Lee Newson <lee.newson@tiny.cloud>
2021-01-11 21:14:38 +1000

    TINY-6870: Switch codesample plugin to use BDD style tests

--- modules/tinymce/src/plugins/codesample/test/ts/browser/ChangeCodeSampleTest.ts
+++ modules/tinymce/src/plugins/codesample/test/ts/browser/ChangeCodeSampleTest.ts
@@ -1,36 +1,28 @@
-import { Log, Pipeline } from '@ephox/agar';
-import { UnitTest } from '@ephox/bedrock-client';
-import { TinyLoader } from '@ephox/mcagar';
-import { SugarElement } from '@ephox/sugar';
-import CodePlugin from 'tinymce/plugins/codesample/Plugin';
-import SilverTheme from 'tinymce/themes/silver/Theme';
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyDom, TinyHooks } from '@ephox/mcagar';
+
+import Editor from 'tinymce/core/api/Editor';
+import Plugin from 'tinymce/plugins/codesample/Plugin';
+import Theme from 'tinymce/themes/silver/Theme';
 import * as TestUtils from '../module/CodeSampleTestUtils';
 
-UnitTest.asynctest('browser.tinymce.plugins.codesample.ChangeLanguageCodeSampleTest', (success, failure) => {
-
-  CodePlugin();
-  SilverTheme();
-
-  TinyLoader.setupLight((editor, onSuccess, onFailure) => {
-
-    const docBody = SugarElement.fromDom(document.body);
-    const editorBody = editor.contentDocument.body;
-    const jsContent = 'var foo = "bar";';
-
-    Pipeline.async({},
-      Log.steps('TBA', 'CodeSample: Open the dialog and check it has the right initial values. Set the codesample content, submit and check the editor content changes correctly. Re-open the dialog and check the dialog language and content is correct.', [
-        TestUtils.sOpenDialogAndAssertInitial(editor, docBody, 'markup', ''),
-        TestUtils.sSetLanguage('javascript'),
-        TestUtils.sSetTextareaContent(jsContent),
-        TestUtils.sSubmitDialog(docBody),
-        TestUtils.sAssertEditorContents(editorBody, 'javascript', jsContent, 'pre.language-javascript'),
-        TestUtils.sOpenDialogAndAssertInitial(editor, docBody, 'javascript', jsContent)
-      ])
-      , onSuccess, onFailure);
-  }, {
+describe('browser.tinymce.plugins.codesample.ChangeLanguageCodeSampleTest', () => {
+  const hook = TinyHooks.bddSetupLight<Editor>({
     plugins: 'codesample',
-    theme: 'silver',
     toolbar: 'codesample',
     base_url: '/project/tinymce/js/tinymce'
-  }, success, failure);
+  }, [ Plugin, Theme ]);
+
+  const jsContent = 'var foo = "bar";';
+
+  it('TBA: Open the dialog and check it has the right initial values. Set the codesample content, submit and check the editor content changes correctly. Re-open the dialog and check the dialog language and content is correct.', async () => {
+    const editor = hook.editor();
+    await TestUtils.pOpenDialogAndAssertInitial(editor, 'markup', '');
+    TestUtils.setLanguage('javascript');
+    TestUtils.setTextareaContent(jsContent);
+    await TestUtils.pSubmitDialog(editor);
+    await TestUtils.pAssertEditorContents(TinyDom.body(editor), 'javascript', jsContent, 'pre.language-javascript');
+    await TestUtils.pOpenDialogAndAssertInitial(editor, 'javascript', jsContent);
+    await TestUtils.pCancelDialog(editor);
+  });
 });
