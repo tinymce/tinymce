35a215b02040e960509fb799f3d560a41821eb6c
Lee Newson <lee.newson@tiny.cloud>
2021-01-11 16:21:22 +1000

    TINY-6870: Switch anchor plugin to use BDD style tests

--- modules/tinymce/src/plugins/anchor/test/ts/module/Helpers.ts
+++ modules/tinymce/src/plugins/anchor/test/ts/module/Helpers.ts
@@ -1,30 +1,29 @@
-import { Log, Step, Waiter } from '@ephox/agar';
-import { TinyUi, TinyApis } from '@ephox/mcagar';
+import { Waiter } from '@ephox/agar';
+import { TinyAssertions, TinyUiActions } from '@ephox/mcagar';
 
-const sType = (text: string) =>
-  Log.step('TBA', 'Add anchor id', Step.sync(() => {
-    const elm: any = document.querySelector('div[role="dialog"].tox-dialog  input');
-    elm.value = text;
-  }));
+import Editor from 'tinymce/core/api/Editor';
 
-const sAddAnchor = (tinyApis: TinyApis, tinyUi: TinyUi, id: string, useCommand: boolean = false) =>
-  Log.stepsAsStep('TBA', 'Add anchor', [
-    useCommand ? tinyApis.sExecCommand('mceAnchor') : tinyUi.sClickOnToolbar('click anchor button', 'button[aria-label="Anchor"]'),
-    tinyUi.sWaitForPopup('wait for window', 'div[role="dialog"].tox-dialog  input'),
-    sType(id),
-    tinyUi.sClickOnUi('click on Save btn', 'div.tox-dialog__footer button.tox-button:not(.tox-button--secondary)')
-  ]);
+const type = (text: string): void => {
+  const elm: HTMLInputElement = document.querySelector('div[role="dialog"].tox-dialog  input');
+  elm.value = text;
+};
 
-const sAssertAnchorPresence = (tinyApis: TinyApis, numAnchors: number, selector: string = 'a.mce-item-anchor') => {
+const pAddAnchor = async (editor: Editor, id: string, useCommand: boolean = false): Promise<void> => {
+  useCommand ? editor.execCommand('mceAnchor') : TinyUiActions.clickOnToolbar(editor, 'button[aria-label="Anchor"]');
+  await TinyUiActions.pWaitForPopup(editor, 'div[role="dialog"].tox-dialog  input');
+  type(id);
+  TinyUiActions.clickOnUi(editor, 'div.tox-dialog__footer button.tox-button:not(.tox-button--secondary)');
+};
+
+const pAssertAnchorPresence = (editor: Editor, numAnchors: number, selector: string = 'a.mce-item-anchor'): Promise<void> => {
   const expected = {};
   expected[selector] = numAnchors;
-  return Waiter.sTryUntil('wait for anchor',
-    tinyApis.sAssertContentPresence(expected)
+  return Waiter.pTryUntil('wait for anchor',
+    () => TinyAssertions.assertContentPresence(editor, expected)
   );
 };
 
 export {
-  sType,
-  sAddAnchor,
-  sAssertAnchorPresence
+  pAddAnchor,
+  pAssertAnchorPresence
 };
