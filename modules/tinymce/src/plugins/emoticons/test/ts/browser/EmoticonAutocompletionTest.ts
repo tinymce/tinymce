310be4f16940f1d4a4df4ffb8f81efe1364c6508
Lee Newson <lee.newson@tiny.cloud>
2021-01-12 07:13:36 +1000

    TINY-6870: Switch emoticons plugin to use BDD style tests

--- modules/tinymce/src/plugins/emoticons/test/ts/browser/EmoticonAutocompletionTest.ts
+++ modules/tinymce/src/plugins/emoticons/test/ts/browser/EmoticonAutocompletionTest.ts
@@ -1,39 +1,32 @@
-import { Keyboard, Keys, Log, Pipeline, UiFinder } from '@ephox/agar';
-import { UnitTest } from '@ephox/bedrock-client';
-import { TinyApis, TinyLoader } from '@ephox/mcagar';
-import { SugarBody, SugarElement } from '@ephox/sugar';
-import EmoticonsPlugin from 'tinymce/plugins/emoticons/Plugin';
-import SilverTheme from 'tinymce/themes/silver/Theme';
+import { Keyboard, Keys } from '@ephox/agar';
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyAssertions, TinyDom, TinyHooks, TinySelections, TinyUiActions } from '@ephox/mcagar';
 
-UnitTest.asynctest('browser.tinymce.plugins.emoticons.AutocompletionTest', (success, failure) => {
+import Editor from 'tinymce/core/api/Editor';
+import Plugin from 'tinymce/plugins/emoticons/Plugin';
+import Theme from 'tinymce/themes/silver/Theme';
 
-  EmoticonsPlugin();
-  SilverTheme();
-
-  TinyLoader.setupLight((editor, onSuccess, onFailure) => {
-    const tinyApis = TinyApis(editor);
-    const eDoc = SugarElement.fromDom(editor.getDoc());
-
-    // NOTE: This is almost identical to charmap
-    Pipeline.async({},
-      Log.steps('TBA', 'Emoticons: Autocomplete, trigger an autocomplete and check it appears', [
-        tinyApis.sFocus(),
-        tinyApis.sSetContent('<p>:ha</p>'),
-        tinyApis.sSetCursor([ 0, 0 ], 3),
-        Keyboard.sKeypress(eDoc, 'a'.charCodeAt(0), { }),
-        UiFinder.sWaitForVisible('Waiting for autocomplete menu', SugarBody.body(), '.tox-autocompleter .tox-collection__item'),
-        Keyboard.sKeydown(eDoc, Keys.right(), { }),
-        Keyboard.sKeydown(eDoc, Keys.right(), { }),
-        Keyboard.sKeydown(eDoc, Keys.enter(), { }),
-        tinyApis.sAssertContent('<p>😂</p>')
-      ])
-      , onSuccess, onFailure);
-  }, {
+describe('browser.tinymce.plugins.emoticons.AutocompletionTest', () => {
+  const hook = TinyHooks.bddSetupLight<Editor>({
     plugins: 'emoticons',
     toolbar: 'emoticons',
-    theme: 'silver',
     base_url: '/project/tinymce/js/tinymce',
     emoticons_database_url: '/project/tinymce/src/plugins/emoticons/test/js/test-emojis.js',
     emoticons_database_id: 'tinymce.plugins.emoticons.test-emojis.js'
-  }, success, failure);
+  }, [ Plugin, Theme ], true);
+
+  // NOTE: This is almost identical to charmap
+  it('TBA: Autocomplete, trigger an autocomplete and check it appears', async () => {
+    const editor = hook.editor();
+    const editorDoc = TinyDom.document(editor);
+
+    editor.setContent('<p>:ha</p>');
+    TinySelections.setCursor(editor, [ 0, 0 ], 3);
+    Keyboard.activeKeypress(editorDoc, 'a'.charCodeAt(0), { });
+    await TinyUiActions.pWaitForPopup(editor, '.tox-autocompleter .tox-collection__item');
+    Keyboard.activeKeydown(editorDoc, Keys.right(), { });
+    Keyboard.activeKeydown(editorDoc, Keys.right(), { });
+    Keyboard.activeKeydown(editorDoc, Keys.enter(), { });
+    TinyAssertions.assertContent(editor, '<p>😂</p>');
+  });
 });
