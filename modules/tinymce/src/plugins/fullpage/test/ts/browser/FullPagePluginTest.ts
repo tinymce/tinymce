e92632162034d22534bb878198c5af0ff86a167a
Lee Newson <lee.newson@tiny.cloud>
2021-01-12 12:10:26 +1000

    TINY-6870: Switch fullpage plugin to use BDD style tests

--- modules/tinymce/src/plugins/fullpage/test/ts/browser/FullPagePluginTest.ts
+++ modules/tinymce/src/plugins/fullpage/test/ts/browser/FullPagePluginTest.ts
@@ -1,82 +1,96 @@
-import { Assertions, GeneralSteps, Log, Logger, Pipeline, Step, Waiter } from '@ephox/agar';
-import { UnitTest } from '@ephox/bedrock-client';
-import { LegacyUnit, TinyLoader } from '@ephox/mcagar';
+import { Waiter } from '@ephox/agar';
+import { afterEach, describe, it } from '@ephox/bedrock-client';
+import { TinyAssertions, TinyHooks } from '@ephox/mcagar';
+import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
 import Plugin from 'tinymce/plugins/fullpage/Plugin';
 import Theme from 'tinymce/themes/silver/Theme';
 
-UnitTest.asynctest('browser.tinymce.plugins.fullpage.FullPagePluginTest', (success, failure) => {
-  const suite = LegacyUnit.createSuite<Editor>();
+describe('browser.tinymce.plugins.fullpage.FullPagePluginTest', () => {
+  const hook = TinyHooks.bddSetupLight<Editor>({
+    plugins: 'fullpage',
+    indent: false,
+    base_url: '/project/tinymce/js/tinymce',
+    theme: 'silver',
+    protect: [
+      /<!--([\s\S]*?)-->/g
+    ]
+  }, [ Plugin, Theme ]);
 
-  Plugin();
-  Theme();
+  afterEach(() => {
+    const editor = hook.editor();
+    editor.getBody().removeAttribute('dir');
+  });
 
-  const teardown = (editor) => {
-    editor.getBody().rtl = '';
-  };
-
-  suite.test('TestCase-TBA: FullPage: Keep header/footer intact', (editor) => {
-    const normalizeHTML = (html) => {
+  it('TBA: Keep header/footer intact', () => {
+    const editor = hook.editor();
+    const normalizeHTML = (html: string) => {
       return html.replace(/\s/g, '');
     };
 
     editor.setContent('<html><body><p>Test</p>');
-    LegacyUnit.equal(normalizeHTML(editor.getContent()), '<html><body><p>Test</p>', 'Invalid HTML content is still editable.');
+    assert.equal(normalizeHTML(editor.getContent()), '<html><body><p>Test</p>', 'Invalid HTML content is still editable.');
 
     editor.setContent('<html><body><p>Test</p></body></html>');
-    LegacyUnit.equal(normalizeHTML(editor.getContent()), '<html><body><p>Test</p></body></html>', 'Header/footer is intact.');
+    assert.equal(normalizeHTML(editor.getContent()), '<html><body><p>Test</p></body></html>', 'Header/footer is intact.');
   });
 
-  suite.test('TestCase-TBA: FullPage: Default header/footer', (editor) => {
+  it('TBA: Default header/footer', () => {
+    const editor = hook.editor();
     editor.setContent('<p>Test</p>');
-    LegacyUnit.equal(
+    assert.equal(
       editor.getContent(),
       '<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\n<p>Test</p>\n</body>\n</html>',
       'Invalid HTML content is still editable.'
     );
   });
 
-  suite.test('TestCase-TBA: FullPage: Parse body attributes', (editor) => {
+  it('TBA: Parse body attributes', () => {
+    const editor = hook.editor();
     editor.setContent('<html><body><p>Test</p></body></html>');
-    LegacyUnit.equal(editor.getBody().style.color, '', 'No color on body.');
-    LegacyUnit.equal(editor.getBody().dir, '', 'No dir on body.');
-    LegacyUnit.equal(editor.dom.getStyle(editor.getBody().firstChild, 'display', true), 'block', 'No styles added to iframe document');
+    assert.equal(editor.getBody().style.color, '', 'No color on body.');
+    assert.equal(editor.getBody().dir, '', 'No dir on body.');
+    assert.equal(editor.dom.getStyle(editor.getBody().firstChild, 'display', true), 'block', 'No styles added to iframe document');
 
     editor.setContent('<html><body style="color:#FF0000"><p>Test</p></body></html>');
-    LegacyUnit.equal(editor.getBody().style.color.length > 0, true, 'Color added to body');
+    assert.isAbove(editor.getBody().style.color.length, 0, 'Color added to body');
 
     editor.setContent('<html><body dir="rtl"><p>Test</p></body></html>');
-    LegacyUnit.equal(editor.getBody().dir, 'rtl', 'Dir added to body');
+    assert.equal(editor.getBody().dir, 'rtl', 'Dir added to body');
 
     editor.setContent('<html><body><p>Test</p></body></html>');
-    LegacyUnit.equal(editor.getBody().style.color, '', 'No color on body.');
-    LegacyUnit.equal(editor.getBody().dir, '', 'No dir on body.');
-    LegacyUnit.equal(editor.dom.getStyle(editor.getBody().firstChild, 'display', true), 'block', 'No styles added to iframe document');
+    assert.equal(editor.getBody().style.color, '', 'No color on body.');
+    assert.equal(editor.getBody().dir, '', 'No dir on body.');
+    assert.equal(editor.dom.getStyle(editor.getBody().firstChild, 'display', true), 'block', 'No styles added to iframe document');
   });
 
-  suite.test('TestCase-TBA: FullPage: fullpage_hide_in_source_view: false', (editor) => {
+  it('TBA: fullpage_hide_in_source_view: false', () => {
+    const editor = hook.editor();
     editor.settings.fullpage_hide_in_source_view = false;
     editor.setContent('<html><body><p>1</p></body></html>');
-    LegacyUnit.equal(editor.getContent({ source_view: true }), '<html><body>\n<p>1</p>\n</body></html>');
+    assert.equal(editor.getContent({ source_view: true }), '<html><body>\n<p>1</p>\n</body></html>');
   });
 
-  suite.test('TestCase-TBA: FullPage: fullpage_hide_in_source_view: false', (editor) => {
+  it('TBA: fullpage_hide_in_source_view: true', () => {
+    const editor = hook.editor();
     editor.settings.fullpage_hide_in_source_view = true;
     editor.setContent('<html><body><p>1</p></body></html>');
-    LegacyUnit.equal(editor.getContent({ source_view: true }), '<p>1</p>');
+    assert.equal(editor.getContent({ source_view: true }), '<p>1</p>');
   });
 
-  suite.test('TestCase-TBA: FullPage: link elements', (editor) => {
+  it('TBA: link elements', () => {
+    const editor = hook.editor();
     editor.setContent('<html><head><link rel="stylesheet" href="a.css"><link rel="something"></head><body><p>c</p></body></html>');
-    LegacyUnit.equal(
+    assert.equal(
       editor.getContent(),
       '<html><head><link rel="stylesheet" href="a.css"><link rel="something"></head><body>\n<p>c</p>\n</body></html>'
     );
   });
 
-  suite.test('TestCase-TBA: FullPage: add/remove stylesheets', (editor) => {
-    const hasLink = (href) => {
+  it('TBA: add/remove stylesheets', () => {
+    const editor = hook.editor();
+    const hasLink = (href: string) => {
       const links = editor.getDoc().getElementsByTagName('link');
 
       for (let i = 0; i < links.length; i++) {
@@ -89,16 +103,16 @@ UnitTest.asynctest('browser.tinymce.plugins.fullpage.FullPagePluginTest', (succe
     };
 
     editor.setContent('<html><head><link rel="stylesheet" href="a.css"></head><body><p>c</p></body></html>');
-    LegacyUnit.equal(hasLink('a.css'), true);
-    LegacyUnit.equal(hasLink('b.css'), false);
-    LegacyUnit.equal(hasLink('c.css'), false);
+    assert.isTrue(hasLink('a.css'));
+    assert.isFalse(hasLink('b.css'));
+    assert.isFalse(hasLink('c.css'));
 
     editor.setContent(
       '<html><head><link rel="stylesheet" href="a.css"><link rel="stylesheet" href="b.css"></head><body><p>c</p></body></html>'
     );
-    LegacyUnit.equal(hasLink('a.css'), true);
-    LegacyUnit.equal(hasLink('b.css'), true);
-    LegacyUnit.equal(hasLink('c.css'), false);
+    assert.isTrue(hasLink('a.css'));
+    assert.isTrue(hasLink('b.css'));
+    assert.isFalse(hasLink('c.css'));
 
     editor.setContent(
       '<html><head>' +
@@ -108,95 +122,67 @@ UnitTest.asynctest('browser.tinymce.plugins.fullpage.FullPagePluginTest', (succe
       '</head>' +
       '<body><p>c</p></body></html>'
     );
-    LegacyUnit.equal(hasLink('a.css'), true);
-    LegacyUnit.equal(hasLink('b.css'), true);
-    LegacyUnit.equal(hasLink('c.css'), true);
+    assert.isTrue(hasLink('a.css'));
+    assert.isTrue(hasLink('b.css'));
+    assert.isTrue(hasLink('c.css'));
 
     editor.setContent('<html><head><link rel="stylesheet" href="a.css"></head><body><p>c</p></body></html>');
-    LegacyUnit.equal(hasLink('a.css'), true);
-    LegacyUnit.equal(hasLink('b.css'), false);
-    LegacyUnit.equal(hasLink('c.css'), false);
+    assert.isTrue(hasLink('a.css'));
+    assert.isFalse(hasLink('b.css'));
+    assert.isFalse(hasLink('c.css'));
 
     editor.setContent('<html><head></head><body><p>c</p></body></html>');
-    LegacyUnit.equal(hasLink('a.css'), false);
-    LegacyUnit.equal(hasLink('b.css'), false);
-    LegacyUnit.equal(hasLink('c.css'), false);
+    assert.isFalse(hasLink('a.css'));
+    assert.isFalse(hasLink('b.css'));
+    assert.isFalse(hasLink('c.css'));
   });
 
-  const sParseStyles = (editor) => {
-    return Logger.t('Parse styles', GeneralSteps.sequence([
-      Step.sync(() => {
-        editor.setContent('<html><head><style>p {text-transform: uppercase}</style></head><body dir="rtl"><p>Test</p></body></html>');
-      }),
-      Waiter.sTryUntil(
-        'Expected styles were added',
-        Step.sync(() => {
-          Assertions.assertEq('Styles added to iframe document', 'uppercase', editor.dom.getStyle(editor.getBody().firstChild, 'text-transform', true));
-          Assertions.assertEq('Styles not added to actual element', '', editor.dom.getStyle(editor.getBody().firstChild, 'text-transform', false));
-        }
-        ), 10, 3000)
-    ]));
-  };
+  it('Parse styles', async () => {
+    const editor = hook.editor();
+    editor.setContent('<html><head><style>p {text-transform: uppercase}</style></head><body dir="rtl"><p>Test</p></body></html>');
+    await Waiter.pTryUntil(
+      'Expected styles were added',
+      () => {
+        assert.equal(editor.dom.getStyle(editor.getBody().firstChild, 'text-transform', true), 'uppercase', 'Styles added to iframe document');
+        assert.equal(editor.dom.getStyle(editor.getBody().firstChild, 'text-transform', false), '', 'Styles not added to actual element');
+      }
+    );
+  });
 
-  const sProtectConditionalCommentsInHeadFoot = (editor) => {
-    return Logger.t('Set and assert styles were added to iframe document', GeneralSteps.sequence([
-      Step.sync(() => {
-        editor.setContent([
-          '<!DOCTYPE html>',
-          '<html>',
-          '<!--[if mso]>',
-          '<body class="mso-container" style="background-color:#FFFFFF;">',
-          '<![endif]-->',
-          '<!--[if !mso]><!-->',
-          '<body class="clean-body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #FFFFFF">',
-          '<!--<![endif]--><p>text</p>',
-          '</body>',
-          '</html>'
-        ].join('\n'));
-      }),
-      Step.sync(() => {
-        const expectedContent = [
-          '<!DOCTYPE html>',
-          '<html>',
-          '<!--[if mso]>',
-          '<body class="mso-container" style="background-color:#FFFFFF;">',
-          '<![endif]-->',
-          '<!--[if !mso]><!-->',
-          '<body class="clean-body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #FFFFFF">',
-          '<!--<![endif]--><p>text</p>',
-          '</body>',
-          '</html>'
-        ].join('\n');
+  it('protect conditional comments in head and foot', () => {
+    const editor = hook.editor();
+    editor.setContent([
+      '<!DOCTYPE html>',
+      '<html>',
+      '<!--[if mso]>',
+      '<body class="mso-container" style="background-color:#FFFFFF;">',
+      '<![endif]-->',
+      '<!--[if !mso]><!-->',
+      '<body class="clean-body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #FFFFFF">',
+      '<!--<![endif]--><p>text</p>',
+      '</body>',
+      '</html>'
+    ].join('\n'));
 
-        Assertions.assertHtml('Styles added to iframe document', expectedContent, editor.getContent());
-      })
-    ]));
-  };
+    const expectedContent = [
+      '<!DOCTYPE html>',
+      '<html>',
+      '<!--[if mso]>',
+      '<body class="mso-container" style="background-color:#FFFFFF;">',
+      '<![endif]-->',
+      '<!--[if !mso]><!-->',
+      '<body class="clean-body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #FFFFFF">',
+      '<!--<![endif]--><p>text</p>',
+      '</body>',
+      '</html>'
+    ].join('\n');
 
-  TinyLoader.setupLight((editor, onSuccess, onFailure) => {
-    Pipeline.async({}, [
-      Log.stepsAsStep('TBA', 'FullPage: Test full page header, footer, body attributes, hide in source view and adding and removing stylesheets',
-        [
-          sParseStyles(editor),
-          sProtectConditionalCommentsInHeadFoot(editor)
-        ].concat(suite.toSteps(editor))
-      ),
-      Log.stepsAsStep('TINY-6541', 'FullPage: Text content should not be modified', [
-        Step.sync(() => {
-          editor.setContent('some plain text');
-          Assertions.assertEq('should return plain text content', 'some plain text', editor.getContent({ format: 'text' }));
-        })
-      ])
-    ], onSuccess, onFailure);
+    TinyAssertions.assertContent(editor, expectedContent);
+  });
 
-    teardown(editor);
-  }, {
-    plugins: 'fullpage',
-    indent: false,
-    base_url: '/project/tinymce/js/tinymce',
-    theme: 'silver',
-    protect: [
-      /<!--([\s\S]*?)-->/g
-    ]
-  }, success, failure);
+  it('TINY-6541: Text content should not be modified', () => {
+    const editor = hook.editor();
+    editor.setContent('some plain text');
+    assert.equal(editor.getContent({ format: 'text' }), 'some plain text', 'should return plain text content');
+  });
 });
