bfa195346f0139619b8bbc04e5fd621e76febe64
JamesToohey <james@toohey.io>
2021-01-20 16:34:13 +1000

    TINY-6715: Improvements from review

--- modules/snooker/src/test/ts/module/ephox/snooker/test/MockStructs.ts
+++ modules/snooker/src/test/ts/module/ephox/snooker/test/MockStructs.ts
@@ -1,6 +1,6 @@
 import { Arr } from '@ephox/katamari';
 import { SugarElement, SugarNode, TextContent } from '@ephox/sugar';
-import { Structs } from 'ephox/snooker/api/Main';
+import * as Structs from 'ephox/snooker/api/Structs';
 
 // Creates an elementNew (or returns from elements array if it already exists)
 const getElementNew = (elements: Structs.ElementNew[], tagName: keyof HTMLElementTagNameMap, text: string, isNew: boolean): Structs.ElementNew => {
@@ -12,10 +12,7 @@ const getElementNew = (elements: Structs.ElementNew[], tagName: keyof HTMLElemen
 
   return Arr.find(elements,
     (elementNew) => SugarNode.name(elementNew.element) === tagName && TextContent.get(elementNew.element) === text
-  ).fold(
-    () => createAndAppendElement(),
-    (elementNew) => elementNew
-  );
+  ).getOrThunk(createAndAppendElement);
 };
 
 export {
