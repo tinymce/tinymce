5e482c44243b9966cd3dfd71ea888140927d7181
James Toohey <james@toohey.io>
2021-02-03 16:55:41 +1000

    TINY-6870: Switch visualblocks plugin to use BDD style tests (#6424)

--- modules/tinymce/src/plugins/visualblocks/test/ts/browser/PreviewFormatTest.ts
+++ modules/tinymce/src/plugins/visualblocks/test/ts/browser/PreviewFormatTest.ts
@@ -1,45 +1,58 @@
-import { Assertions, Log, Pipeline, Step, Waiter } from '@ephox/agar';
-import { UnitTest } from '@ephox/bedrock-client';
-import { TinyApis, TinyLoader } from '@ephox/mcagar';
+import { Waiter } from '@ephox/agar';
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyDom, TinyHooks } from '@ephox/mcagar';
 import { Class, Css, SugarElement } from '@ephox/sugar';
+import { assert } from 'chai';
 
-import VisualBlocksPlugin from 'tinymce/plugins/visualblocks/Plugin';
+import Editor from 'tinymce/core/api/Editor';
+import Plugin from 'tinymce/plugins/visualblocks/Plugin';
 import Theme from 'tinymce/themes/silver/Theme';
 
-UnitTest.asynctest('browser.tinymce.plugins.visualblocks.PreviewFormatsTest', (success, failure) => {
-  Theme();
-  VisualBlocksPlugin();
-
-  const sWaitForVisualBlocks = (editor) => {
-    return Waiter.sTryUntil('Wait for background css to be applied to first element', Step.sync(() => {
-      const p = SugarElement.fromDom(editor.getBody().firstChild);
-      const background = Css.get(p, 'background-image');
-      Assertions.assertEq('Paragraph should have a url background', true, background.indexOf('url(') === 0);
-    }));
-  };
-
-  TinyLoader.setupLight((editor, onSuccess, onFailure) => {
-    const tinyApis = TinyApis(editor);
-
-    Pipeline.async({},
-      Log.steps('TBA', 'VisualBlocks: Toggle on/off visualblocks and compute previews', [
-        tinyApis.sExecCommand('mceVisualBlocks'),
-        sWaitForVisualBlocks(editor),
-        Step.sync(() => {
-          Assertions.assertEq('Visual blocks class should exist', true, Class.has(SugarElement.fromDom(editor.getBody()), 'mce-visualblocks'));
-          Assertions.assertEq('Should not have a border property', true, editor.formatter.getCssText('h1').indexOf('border:1px dashed') === -1);
-        }),
-        tinyApis.sExecCommand('mceVisualBlocks'),
-        Step.sync(() => {
-          Assertions.assertEq('Visual blocks class should not exist', false, Class.has(SugarElement.fromDom(editor.getBody()), 'mce-visualblocks'));
-          Assertions.assertEq('Should not have a border property', true, editor.formatter.getCssText('h1').indexOf('border:1px dashed') === -1);
-          Assertions.assertEq('Visual blocks class should still not exist', false, Class.has(SugarElement.fromDom(editor.getBody()), 'mce-visualblocks'));
-        })
-      ])
-      , onSuccess, onFailure);
-  }, {
+describe('browser.tinymce.plugins.visualblocks.PreviewFormatsTest', () => {
+  const hook = TinyHooks.bddSetupLight<Editor>({
     plugins: 'visualblocks',
     toolbar: 'visualblocks',
-    base_url: '/project/tinymce/js/tinymce'
-  }, success, failure);
+    base_url: '/project/tinymce/js/tinymce',
+    content_style: `
+      h1 {
+        border: 2px solid black;
+      }
+      .mce-visualblocks h1 {
+        border: 13px solid black;
+      }
+    `
+  }, [ Plugin, Theme ]);
+
+  const pWaitForVisualBlocks = (editor: Editor, waitUntilEnabled: boolean = true) =>
+    Waiter.pTryUntil('Wait for background css to be applied to first element', () => {
+      const p = TinyDom.fromDom(editor.getBody().firstChild);
+      const background = Css.get(p, 'background-image');
+      if (waitUntilEnabled) {
+        assert.include(background, 'url(', 'Paragraph should have a url background');
+        assert.isTrue(Class.has(TinyDom.body(editor), 'mce-visualblocks'), 'Visual blocks class should exist');
+      } else {
+        assert.notInclude(background, 'url(', 'Paragraph should not have a url background');
+        assert.isFalse(Class.has(TinyDom.body(editor), 'mce-visualblocks'), 'Visual blocks class should not exist');
+      }
+    });
+
+  const assertBorderWidth = (element: HTMLElement, expectedWidth: string) => {
+    // we need to use `border-left-width` rather than the short hand `border-width` for the computed value to be valid in Firefox
+    assert.equal(Css.get(SugarElement.fromDom(element), 'border-left-width'), expectedWidth);
+  };
+
+  it('TBA: Toggle on/off visualblocks and compute previews', async () => {
+    const editor = hook.editor();
+    editor.setContent('<h1>something</h1>');
+    const h1 = editor.dom.select('h1')[0];
+    assertBorderWidth(h1, '2px');  // content style is overriden so that all h1s have a 1px border
+
+    editor.execCommand('mceVisualBlocks');
+    await pWaitForVisualBlocks(editor);
+    assertBorderWidth(h1, '13px');
+
+    editor.execCommand('mceVisualBlocks');
+    await pWaitForVisualBlocks(editor, false);
+    assertBorderWidth(h1, '2px');
+  });
 });
--- modules/tinymce/src/plugins/visualblocks/test/ts/browser/VisualBlocksSanityTest.ts
+++ modules/tinymce/src/plugins/visualblocks/test/ts/browser/VisualBlocksSanityTest.ts
@@ -1,49 +1,42 @@
-import { ApproxStructure, Log, Pipeline } from '@ephox/agar';
-import { UnitTest } from '@ephox/bedrock-client';
-import { TinyApis, TinyLoader, TinyUi } from '@ephox/mcagar';
+import { ApproxStructure } from '@ephox/agar';
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyAssertions, TinyHooks, TinyUiActions } from '@ephox/mcagar';
 
-import VisualBlocksPlugin from 'tinymce/plugins/visualblocks/Plugin';
+import Editor from 'tinymce/core/api/Editor';
+import Plugin from 'tinymce/plugins/visualblocks/Plugin';
 import Theme from 'tinymce/themes/silver/Theme';
 
-UnitTest.asynctest(
-  'browser.tinymce.plugins.visualblocks.VisualBlocksSanityTest', (success, failure) => {
+describe('browser.tinymce.plugins.visualblocks.VisualBlocksSanityTest', () => {
+  const hook = TinyHooks.bddSetupLight<Editor>({
+    plugins: 'visualblocks',
+    toolbar: 'visualblocks',
+    base_url: '/project/tinymce/js/tinymce'
+  }, [ Plugin, Theme ]);
 
-    Theme();
-    VisualBlocksPlugin();
-
-    TinyLoader.setupLight((editor, onSuccess, onFailure) => {
-      const tinyUi = TinyUi(editor);
-      const tinyApis = TinyApis(editor);
-
-      Pipeline.async({}, Log.steps('TBA', 'VisualBlocks: Assert visual blocks are not present, click on the visual blocks button and assert they are present, click on the button again and assert they are not present', [
-        tinyApis.sAssertContentStructure(ApproxStructure.build((s, str, arr) => {
-          return s.element('body', {
-            classes: [
-              arr.not('mce-visualblocks')
-            ]
-          });
-        })),
-        tinyUi.sClickOnToolbar('click visualblocks button', 'button'),
-        tinyApis.sAssertContentStructure(ApproxStructure.build((s, str, arr) => {
-          return s.element('body', {
-            classes: [
-              arr.has('mce-visualblocks')
-            ]
-          });
-        })),
-        tinyUi.sClickOnToolbar('click visualblocks button', 'button'),
-        tinyApis.sAssertContentStructure(ApproxStructure.build((s, str, arr) => {
-          return s.element('body', {
-            classes: [
-              arr.not('mce-visualblocks')
-            ]
-          });
-        }))
-      ]), onSuccess, onFailure);
-    }, {
-      plugins: 'visualblocks',
-      toolbar: 'visualblocks',
-      base_url: '/project/tinymce/js/tinymce'
-    }, success, failure);
-  }
-);
+  it('TBA: Assert visual blocks are not present, click on the visual blocks button and assert they are present, click on the button again and assert they are not present', () => {
+    const editor = hook.editor();
+    TinyAssertions.assertContentStructure(editor, ApproxStructure.build((s, _str, arr) => {
+      return s.element('body', {
+        classes: [
+          arr.not('mce-visualblocks')
+        ]
+      });
+    }));
+    TinyUiActions.clickOnToolbar(editor, 'button');
+    TinyAssertions.assertContentStructure(editor, ApproxStructure.build((s, _str, arr) => {
+      return s.element('body', {
+        classes: [
+          arr.has('mce-visualblocks')
+        ]
+      });
+    }));
+    TinyUiActions.clickOnToolbar(editor, 'button');
+    TinyAssertions.assertContentStructure(editor, ApproxStructure.build((s, _str, arr) => {
+      return s.element('body', {
+        classes: [
+          arr.not('mce-visualblocks')
+        ]
+      });
+    }));
+  });
+});
