e8367b8767f3d25a9399fd87745cf9e041b3a547
Lee Newson <lee.newson@tiny.cloud>
2021-02-08 08:14:03 +1000

    Reword comment
    Co-authored-by: James Toohey <james@toohey.io>

--- modules/snooker/src/main/ts/ephox/snooker/api/CellMutations.ts
+++ modules/snooker/src/main/ts/ephox/snooker/api/CellMutations.ts
@@ -4,7 +4,7 @@ import * as CellUtils from '../util/CellUtils';
 
 const halve = (main: SugarElement, other: SugarElement): void => {
   const colspan = CellUtils.getSpan(main, 'colspan');
-  // Don't set a width on the new cell if we have a colspan as we can only safely do that for cells
+  // Only set width on the new cell if we have a colspan of 1 (or no colspan) as we can only safely do that for cells
   // that are a single column, since we don't know the individual column widths for a cell with a colspan.
   // Instead, we'll rely on the adjustments/postAction logic to set the widths based on other cells in the column
   if (colspan === 1) {
