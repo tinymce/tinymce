db2d9a6996a4e6dd21c18207401ee5ebe0cdd0ce
Viktor Pyshnenko <viktor.pyshnenko@tiny.cloud>
2021-03-19 08:56:30 +0200

    TINY-7006: expand using TinyContenActions.keystroke over entire test file

--- modules/tinymce/src/plugins/table/test/ts/browser/TabKeyNavigationTest.ts
+++ modules/tinymce/src/plugins/table/test/ts/browser/TabKeyNavigationTest.ts
@@ -1,6 +1,6 @@
 import { Keys } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
-import { LegacyUnit, TinyAssertions, TinyHooks } from '@ephox/mcagar';
+import { LegacyUnit, TinyAssertions, TinyContentActions, TinyHooks } from '@ephox/mcagar';
 import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
@@ -25,25 +25,26 @@ describe('browser.tinymce.plugins.table.TabKeyNavigationTest', () => {
     editor.setContent('<table><tbody><tr><td>A1</td><td>A2</td></tr><tr><td>B1</td><td>B2</td></tr></tbody></table><p>x</p>');
 
     LegacyUnit.setSelection(editor, 'td', 0);
-    editor.fire('keydown', { keyCode: Keys.tab() } as KeyboardEvent);
+    TinyContentActions.keystroke(editor, Keys.tab());
     assert.equal(editor.selection.getStart().innerHTML, 'A2');
 
     LegacyUnit.setSelection(editor, 'td', 0);
-    editor.fire('keydown', { keyCode: Keys.tab(), shiftKey: true } as KeyboardEvent);
+    TinyContentActions.keystroke(editor, Keys.tab(), {shiftKey: true});
     assert.equal(editor.selection.getStart().innerHTML, 'A1');
 
     LegacyUnit.setSelection(editor, 'td:nth-child(2)', 0);
-    editor.fire('keydown', { keyCode: Keys.tab(), shiftKey: true } as KeyboardEvent);
+    TinyContentActions.keystroke(editor, Keys.tab(), {shiftKey: true});
     assert.equal(editor.selection.getStart().innerHTML, 'A1');
 
     LegacyUnit.setSelection(editor, 'tr:nth-child(2) td:nth-child(2)', 0);
-    editor.fire('keydown', { keyCode: Keys.tab() } as KeyboardEvent);
+    TinyContentActions.keystroke(editor, Keys.tab());
     assert.equal(editor.selection.getStart(true).nodeName, 'TD');
     TinyAssertions.assertContent(editor,
       '<table><tbody><tr><td>A1</td><td>A2</td></tr><tr><td>B1</td><td>B2' +
       '</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td></tr></tbody></table><p>x</p>'
     );
   });
+
   it('TINY-7006: Fire TableModified event when rows are added via the Tab key', () => {
     const editor = hook.editor();
 
