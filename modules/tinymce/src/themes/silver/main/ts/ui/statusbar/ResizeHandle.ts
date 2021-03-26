5c6d52cecd965233ac70d043674a35d75d9fe641
Neil Ashford <neil.ashford@tiny.cloud>
2021-03-26 11:56:34 +1000

    TINY-4823: Add license header to new source files

--- modules/tinymce/src/themes/silver/main/ts/ui/statusbar/ResizeHandle.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/statusbar/ResizeHandle.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Tiny Technologies, Inc. All rights reserved.
+ * Licensed under the LGPL or a commercial license.
+ * For LGPL see License.txt in the project root for license information.
+ * For commercial licenses see https://www.tiny.cloud/
+ */
+
 import { AlloyEvents, Behaviour, Dragging, Focusing, NativeEvents, SimpleSpec, SimulatedEvent, Tabstopping } from '@ephox/alloy';
 import { Optional } from '@ephox/katamari';
 import { EventArgs, SugarPosition } from '@ephox/sugar';
