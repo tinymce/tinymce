e13ef2aa5320746a2a1f0a135b781efc74b6366a
Pieter-Jan Van Robays <pieterjan.vanrobays@gmail.com>
2021-04-09 10:37:19 +0200

    simplify tests add SilverFixedToolbarContainerTargetNotInlineTest.ts

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/SilverFixedToolbarContainerPriorityTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/SilverFixedToolbarContainerPriorityTest.ts
@@ -1,6 +1,6 @@
-﻿import { ApproxStructure, Assertions } from '@ephox/agar';
+﻿import { Assertions } from '@ephox/agar';
 import { after, before, describe, it } from '@ephox/bedrock-client';
-import { TinyHooks } from '@ephox/mcagar';
+import { TinyHooks, TinyUiActions } from '@ephox/mcagar';
 import { Insert, Remove, SugarBody, SugarElement } from '@ephox/sugar';
 
 import Editor from 'tinymce/core/api/Editor';
@@ -27,99 +27,12 @@ describe('browser.tinymce.themes.silver.editor.SilverFixedToolbarContainerPriori
     base_url: '/project/tinymce/js/tinymce'
   }, [ Theme ]);
 
-  it('Check basic structure', () => {
+  it('Check priority of fixed_toolbar_container(_target) setting', async () => {
     const editor = hook.editor();
-    editor.setContent('fixed_toolbar_container test');
+    editor.setContent('fixed_toolbar_container_target priority test');
     editor.focus();
-    Assertions.assertStructure(
-      'Container structure',
-      ApproxStructure.build((s, str, arr) => s.element('div', {
-        classes: [],
-        children: [
-          s.element('div', {
-            classes: [ arr.has('tox'), arr.has('tox-tinymce'), arr.has('tox-tinymce-inline') ],
-            children: [
-              s.element('div', {
-                classes: [ arr.has('tox-editor-container') ],
-                children: [
-                  s.element('div', {
-                    classes: [ arr.has('tox-editor-header') ],
-                    children: [
-                      s.element('div', {
-                        classes: [ arr.has('tox-menubar') ],
-                        attrs: { role: str.is('menubar') },
-                        children: [
-                          s.element('button', {
-                            classes: [ arr.has('tox-mbtn'), arr.has('tox-mbtn--select') ],
-                            children: [
-                              s.element('span', {
-                                classes: [ arr.has('tox-mbtn__select-label') ],
-                                html: str.is('File')
-                              }),
-                              s.element('div', {
-                                classes: [ arr.has('tox-mbtn__select-chevron') ],
-                                children: [
-                                  s.element('svg', {})
-                                ]
-                              })
-                            ]
-                          })
-                        ]
-                      }),
 
-                      s.element('div', {
-                        classes: [ arr.has('tox-toolbar-overlord') ],
-                        attrs: { role: str.is('group') },
-                        children: [
-                          s.element('div', {
-                            classes: [ arr.has('tox-toolbar__primary') ],
-                            attrs: { role: str.is('group') },
-                            children: [
-                              s.element('div', {
-                                classes: [ arr.has('tox-toolbar__group') ],
-                                children: [
-                                  s.element('button', {
-                                    classes: [ arr.has('tox-tbtn'), arr.not('tox-btn--enabled') ],
-                                    attrs: {
-                                      'aria-label': str.is('Undo')
-                                    }
-                                  }),
-                                  s.element('button', {
-                                    classes: [ arr.has('tox-tbtn'), arr.not('tox-btn--enabled') ],
-                                    attrs: {
-                                      'aria-label': str.is('Bold')
-                                    }
-                                  })
-                                ]
-                              })
-                            ]
-                          })
-                        ]
-                      })
-                    ]
-                  })
-                ]
-              }),
-              s.element('div', {
-                classes: [ arr.has('tox-throbber') ]
-              })
-            ]
-          }),
-          s.element('div', {
-            classes: [ arr.has('tox'), arr.has('tox-silver-sink'), arr.has('tox-tinymce-aux') ]
-          })
-        ]
-      })),
-      toolbarWithId
-    );
-
-    Assertions.assertStructure(
-      'empty structure',
-      ApproxStructure.build((s) => s.element('div', {
-        classes: [],
-        children: []
-      })),
-      toolbar
-    );
+    await TinyUiActions.pWaitForUi(editor, '#toolbar .tox-editor-header');
+    Assertions.assertPresence('Check that only one toolbar exists', { '.tox-editor-header': 1 }, SugarBody.body());
   });
 });
--- modules/tinymce/src/themes/silver/test/ts/browser/editor/SilverFixedToolbarContainerTargetNotInlineTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/SilverFixedToolbarContainerTargetNotInlineTest.ts
@@ -6,7 +6,7 @@ import { Insert, Remove, SugarBody, SugarElement } from '@ephox/sugar';
 import Editor from 'tinymce/core/api/Editor';
 import Theme from 'tinymce/themes/silver/Theme';
 
-describe('browser.tinymce.themes.silver.editor.SilverFixedToolbarContainerTargetTest', () => {
+describe('browser.tinymce.themes.silver.editor.SilverFixedToolbarContainerTargetNotInlineTest', () => {
   const toolbar: SugarElement<HTMLDivElement> = SugarElement.fromHtml('<div style="margin: 50px 0;"></div>');
   before(() => {
     Insert.append(SugarBody.body(), toolbar);
@@ -17,19 +17,19 @@ describe('browser.tinymce.themes.silver.editor.SilverFixedToolbarContainerTarget
   });
 
   const hook = TinyHooks.bddSetup<Editor>({
-    inline: true,
+    inline: false,
     fixed_toolbar_container_target: toolbar.dom,
     menubar: 'file',
     toolbar: 'undo bold',
     base_url: '/project/tinymce/js/tinymce'
   }, [ Theme ]);
 
-  it('Check fixed_toolbar_container_target setting for inline editor', async () => {
+  it('Check fixed_toolbar_container_target setting is ignored when not an inline editor', async () => {
     const editor = hook.editor();
     editor.setContent('fixed_toolbar_container_target test');
     editor.focus();
 
     await TinyUiActions.pWaitForUi(editor, '.tox-editor-header');
-    Assertions.assertPresence('Check that the inline toolbar exists', { '.tox-tinymce-inline': 1 }, SugarBody.body());
+    Assertions.assertHtml('Check that the inline toolbar is still empty', '', toolbar.dom.innerHTML);
   });
 });
