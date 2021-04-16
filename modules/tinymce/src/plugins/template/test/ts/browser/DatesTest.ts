96e71fac5d17d213f6516777e93b49aa47a41f0e
Viktor Pyshnenko <viktor.pyshnenko@tiny.cloud>
2021-04-16 21:34:01 +0300

    TINY-7319: BDD conversion: Template

--- modules/tinymce/src/plugins/template/test/ts/browser/DatesTest.ts
+++ modules/tinymce/src/plugins/template/test/ts/browser/DatesTest.ts
@@ -1,99 +1,117 @@
-import { Log, Logger, Mouse, Pipeline, Step, UiFinder, Waiter } from '@ephox/agar';
-import { UnitTest } from '@ephox/bedrock-client';
-import { TinyApis, TinyLoader } from '@ephox/mcagar';
-import { SugarElement } from '@ephox/sugar';
-import TemplatePlugin from 'tinymce/plugins/template/Plugin';
-import SilverTheme from 'tinymce/themes/silver/Theme';
+import { UiFinder, Waiter } from '@ephox/agar';
+import { afterEach, describe, it } from '@ephox/bedrock-client';
+import { Obj } from '@ephox/katamari';
+import { TinyAssertions, TinyHooks, TinyUiActions } from '@ephox/mcagar';
+import { SugarBody } from '@ephox/sugar';
 
-UnitTest.asynctest('browser.tinymce.plugins.template.DatesTest', (success, failure) => {
+import Editor from 'tinymce/core/api/Editor';
+import Plugin from 'tinymce/plugins/template/Plugin';
+import Theme from 'tinymce/themes/silver/Theme';
 
-  TemplatePlugin();
-  SilverTheme();
+const insertTemplate = async (editor: Editor) => {
+  const toolbarButtonSelector = '[role="toolbar"] button[aria-label="Insert template"]';
+  const dialogSelector = 'div.tox-dialog';
 
-  TinyLoader.setupLight((editor, onSuccess, onFailure) => {
-    const tinyApis = TinyApis(editor);
+  TinyUiActions.clickOnToolbar(editor, toolbarButtonSelector);
+  await TinyUiActions.pWaitForDialog(editor);
+  TinyUiActions.submitDialog(editor);
+  await Waiter.pTryUntil('Dialog should close', () => UiFinder.notExists(SugarBody.body(), dialogSelector));
+};
 
-    const docBody = SugarElement.fromDom(document.body);
-    const dialogSelector = 'div.tox-dialog';
-    const toolbarButtonSelector = '[role="toolbar"] button[aria-label="Insert template"]';
-
-    const sDeleteSetting = (key) => Logger.t('Deleting Setting ' + key, Step.sync(() => {
-      delete editor.settings[key];
-    }));
-
-    Pipeline.async({}, [
-      Log.stepsAsStep('TBA', 'Template: Test cdate in snippet with default class', [
-        tinyApis.sSetSetting('templates', [{ title: 'a', description: 'b', content: '<p class="cdate">x</p>' }]),
-        tinyApis.sSetSetting('template_cdate_format', 'fake date'),
-        Mouse.sClickOn(SugarElement.fromDom(editor.getContainer()), toolbarButtonSelector),
-        UiFinder.sWaitForVisible('Waited for dialog to be visible', docBody, dialogSelector),
-        Mouse.sClickOn(docBody, 'button.tox-button:contains(Save)'),
-        Waiter.sTryUntil('Dialog should close', UiFinder.sNotExists(docBody, dialogSelector)),
-        tinyApis.sAssertContent('<p class="cdate">fake date</p>'),
-        tinyApis.sSetContent('')
-      ]),
-
-      Log.stepsAsStep('TBA', 'Template: Test cdate in snippet with custom class', [
-        tinyApis.sSetSetting('template_cdate_classes', 'customCdateClass'),
-        tinyApis.sSetSetting('templates', [{ title: 'a', description: 'b', content: '<p class="customCdateClass">x</p>' }]),
-        tinyApis.sSetSetting('template_cdate_format', 'fake date'),
-        Mouse.sClickOn(SugarElement.fromDom(editor.getContainer()), toolbarButtonSelector),
-        UiFinder.sWaitForVisible('Waited for dialog to be visible', docBody, dialogSelector),
-        Mouse.sClickOn(docBody, 'button.tox-button:contains(Save)'),
-        Waiter.sTryUntil('Dialog should close', UiFinder.sNotExists(docBody, dialogSelector)),
-        tinyApis.sAssertContent('<p class="customCdateClass">fake date</p>'),
-        sDeleteSetting('template_cdate_classes'),
-        sDeleteSetting('templates'),
-        sDeleteSetting('template_cdate_format'),
-        tinyApis.sSetContent('')
-      ]),
-
-      Log.stepsAsStep('TBA', 'Template: Test mdate updates with each serialization', [
-        tinyApis.sSetSetting(
-          'templates',
-          [{ title: 'a', description: 'b', content: '<div class="mceTmpl"><p class="mdate"></p><p class="cdate"></p></div>' }]
-        ),
-        tinyApis.sSetSetting('template_mdate_format', 'fake modified date'),
-        tinyApis.sSetSetting('template_cdate_format', 'fake created date'),
-        Mouse.sClickOn(SugarElement.fromDom(editor.getContainer()), toolbarButtonSelector),
-        UiFinder.sWaitForVisible('Waited for dialog to be visible', docBody, dialogSelector),
-        Mouse.sClickOn(docBody, 'button.tox-button:contains(Save)'),
-        Waiter.sTryUntil('Dialog should close', UiFinder.sNotExists(docBody, dialogSelector)),
-        tinyApis.sAssertContent('<div class="mceTmpl"><p class="mdate">fake modified date</p><p class="cdate">fake created date</p></div>'),
-        tinyApis.sSetSetting('template_mdate_format', 'changed modified date'),
-        tinyApis.sAssertContent('<div class="mceTmpl"><p class="mdate">changed modified date</p><p class="cdate">fake created date</p></div>'),
-        sDeleteSetting('templates'),
-        sDeleteSetting('template_mdate_format'),
-        sDeleteSetting('template_cdate_template'),
-        tinyApis.sSetContent('')
-      ]),
-
-      Log.stepsAsStep('TBA', 'Template: Test mdate updates with each serialization with custom class', [
-        tinyApis.sSetSetting('template_mdate_classes', 'modified'),
-        tinyApis.sSetSetting(
-          'templates',
-          [{ title: 'a', description: 'b', content: '<div class="mceTmpl"><p class="modified"></p><p class="cdate"></p></div>' }]
-        ),
-        tinyApis.sSetSetting('template_mdate_format', 'fake modified date'),
-        tinyApis.sSetSetting('template_cdate_format', 'fake created date'),
-        Mouse.sClickOn(SugarElement.fromDom(editor.getContainer()), toolbarButtonSelector),
-        UiFinder.sWaitForVisible('Waited for dialog to be visible', docBody, dialogSelector),
-        Mouse.sClickOn(docBody, 'button.tox-button:contains(Save)'),
-        Waiter.sTryUntil('Dialog should close', UiFinder.sNotExists(docBody, dialogSelector)),
-        tinyApis.sAssertContent('<div class="mceTmpl"><p class="modified">fake modified date</p><p class="cdate">fake created date</p></div>'),
-        tinyApis.sSetSetting('template_mdate_format', 'changed modified date'),
-        tinyApis.sAssertContent('<div class="mceTmpl"><p class="modified">changed modified date</p><p class="cdate">fake created date</p></div>'),
-        sDeleteSetting('template_mdate_classes'),
-        sDeleteSetting('templates'),
-        sDeleteSetting('template_mdate_format'),
-        sDeleteSetting('template_cdate_template')
-      ])
-    ], onSuccess, onFailure);
-  }, {
-    theme: 'silver',
+describe('browser.tinymce.plugins.template.DatesTest', () => {
+  const hook = TinyHooks.bddSetup<Editor>({
     plugins: 'template',
     toolbar: 'template',
-    indent: false,
     base_url: '/project/tinymce/js/tinymce'
-  }, success, failure);
+  }, [ Plugin, Theme ]);
+
+  let settings = new Set<string>();
+  const addSettings = (config: Record<string, any>) => {
+    const editor = hook.editor();
+    Obj.each(config, (val, key) => {
+      editor.settings[key] = val;
+      settings.add(key);
+    });
+  };
+  const delSettings = () => {
+    const editor = hook.editor();
+    settings.forEach((key) => delete editor.settings[key]);
+    settings = new Set<string>();
+  };
+
+  afterEach(() => {
+    const editor = hook.editor();
+    delSettings();
+    editor.setContent('');
+  });
+
+  it('TBA: Template: Test cdate in snippet with default class', async () => {
+    const editor = hook.editor();
+    addSettings({
+      templates: [{ title: 'a', description: 'b', content: '<p class="cdate">x</p>' }],
+      template_cdate_format: 'fake date',
+    });
+    await insertTemplate(editor);
+    TinyAssertions.assertContent(editor, '<p class="cdate">fake date</p>');
+  });
+
+  it('TBA: Template: Test cdate in snippet with custom class', async () => {
+    const editor = hook.editor();
+    addSettings({
+      template_cdate_classes: 'customCdateClass',
+      templates: [{ title: 'a', description: 'b', content: '<p class="customCdateClass">x</p>' }],
+      template_cdate_format: 'fake date'
+    });
+    await insertTemplate(editor);
+    TinyAssertions.assertContent(editor,
+      '<p class="customCdateClass">fake date</p>'
+    );
+  });
+
+  it('TBA: Template: Test mdate updates with each serialization', async () => {
+    const editor = hook.editor();
+    addSettings({
+      template_mdate_format: 'fake modified date',
+      template_cdate_format: 'fake created date',
+      templates: [{ title: 'a', description: 'b', content: '<div class="mceTmpl"><p class="mdate"></p><p class="cdate"></p></div>' }]
+    });
+    await insertTemplate(editor);
+    TinyAssertions.assertContent(editor, [
+      '<div class="mceTmpl">',
+      '<p class="mdate">fake modified date</p>',
+      '<p class="cdate">fake created date</p>',
+      '</div>'
+    ].join('\n'));
+    addSettings({ template_mdate_format: 'changed modified date' });
+    TinyAssertions.assertContent(editor, [
+      '<div class="mceTmpl">',
+      '<p class="mdate">changed modified date</p>',
+      '<p class="cdate">fake created date</p>',
+      '</div>'
+    ].join('\n'));
+  });
+
+  it('TBA: Template: Test mdate updates with each serialization with custom class', async () => {
+    const editor = hook.editor();
+    addSettings({
+      template_mdate_classes: 'modified',
+      template_mdate_format: 'fake modified date',
+      template_cdate_format: 'fake created date',
+      templates: [{ title: 'a', description: 'b', content: '<div class="mceTmpl"><p class="modified"></p><p class="cdate"></p></div>' }]
+    });
+    await insertTemplate(editor);
+    TinyAssertions.assertContent(editor, [
+      '<div class="mceTmpl">',
+      '<p class="modified">fake modified date</p>',
+      '<p class="cdate">fake created date</p>',
+      '</div>'
+    ].join('\n'));
+    addSettings({ template_mdate_format: 'changed modified date' });
+    TinyAssertions.assertContent(editor, [
+      '<div class="mceTmpl">',
+      '<p class="modified">changed modified date</p>',
+      '<p class="cdate">fake created date</p>',
+      '</div>'
+    ].join('\n'));
+  });
 });
