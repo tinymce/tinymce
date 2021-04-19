e35e7aa28635ce79171f94b4fb6f4406e7801191
Viktor Pyshnenko <viktor.pyshnenko@tiny.cloud>
2021-04-19 15:42:14 +0300

    TINY-7319: InvalidUrlTest

--- modules/tinymce/src/plugins/template/test/ts/browser/InvalidUrlTest.ts
+++ modules/tinymce/src/plugins/template/test/ts/browser/InvalidUrlTest.ts
@@ -1,45 +1,37 @@
-import { Log, Pipeline, UiFinder, Waiter } from '@ephox/agar';
-import { UnitTest } from '@ephox/bedrock-client';
-import { TinyApis, TinyLoader, TinyUi } from '@ephox/mcagar';
-import { SugarElement } from '@ephox/sugar';
-import TemplatePlugin from 'tinymce/plugins/template/Plugin';
-import SilverTheme from 'tinymce/themes/silver/Theme';
+import { UiFinder, Waiter } from '@ephox/agar';
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyAssertions, TinyHooks, TinyUiActions } from '@ephox/mcagar';
+import { SugarBody } from '@ephox/sugar';
 
-UnitTest.asynctest('browser.tinymce.plugins.template.InvalidUrlTest', (success, failure) => {
+import Editor from 'tinymce/core/api/Editor';
+import Plugin from 'tinymce/plugins/template/Plugin';
+import Theme from 'tinymce/themes/silver/Theme';
 
-  TemplatePlugin();
-  SilverTheme();
+const dialogSelector = 'div.tox-dialog';
+const alertDialogSelector = 'div.tox-dialog.tox-alert-dialog';
+const toolbarButtonSelector = '[role="toolbar"] button[aria-label="Insert template"]';
 
-  TinyLoader.setupLight((editor, onSuccess, onFailure) => {
-    const tinyApis = TinyApis(editor);
-    const tinyUi = TinyUi(editor);
-
-    const docBody = SugarElement.fromDom(document.body);
-    const alertDialogSelector = 'div.tox-dialog.tox-alert-dialog';
-    const dialogSelector = 'div.tox-dialog';
-    const toolbarButtonSelector = 'button[aria-label="Insert template"]';
-
-    Pipeline.async({}, [
-      Log.stepsAsStep('TBA', 'Template: Test loading in snippet from file that does not exist', [
-        tinyApis.sSetContent(''),
-        tinyApis.sSetSetting('templates', [{ title: 'invalid', description: 'b', url: '/custom/404' }, { title: 'a', description: 'a', content: '<strong>c</strong>' }]),
-        tinyUi.sClickOnToolbar('Click template button', toolbarButtonSelector),
-        tinyUi.sWaitForPopup('Waited for dialog to be visible', dialogSelector),
-        tinyUi.sWaitForPopup('Waited for alert dialog to be visible', alertDialogSelector),
-        tinyUi.sClickOnUi('Click on OK button', 'button.tox-button:contains(OK)'),
-        Waiter.sTryUntil('Alert dialog should close', UiFinder.sNotExists(docBody, alertDialogSelector)),
-        tinyUi.sClickOnUi('Click on Save button (should be disabled)', 'button.tox-button:contains(Save)'),
-        Waiter.sTryUntil('Dialog should not close', UiFinder.sExists(docBody, dialogSelector)),
-        tinyUi.sClickOnUi('Click on Cancel button', 'button.tox-button:contains(Cancel)'),
-        Waiter.sTryUntil('Dialog should close', UiFinder.sNotExists(docBody, dialogSelector)),
-        tinyApis.sAssertContent('')
-      ])
-    ], onSuccess, onFailure);
-  }, {
-    theme: 'silver',
+describe('browser.tinymce.plugins.template.InvalidUrlTest', () => {
+  const hook = TinyHooks.bddSetup<Editor>({
     plugins: 'template',
     toolbar: 'template',
-    indent: false,
     base_url: '/project/tinymce/js/tinymce'
-  }, success, failure);
+  }, [ Plugin, Theme ]);
+
+  it('TBA: Template: Test loading in snippet from file that does not exist', async () => {
+    const editor = hook.editor();
+    editor.setContent('');
+    editor.settings.templates = [{ title: 'invalid', description: 'b', url: '/custom/404' }, { title: 'a', description: 'a', content: '<strong>c</strong>' }];
+    TinyUiActions.clickOnToolbar(editor, toolbarButtonSelector);
+    await TinyUiActions.pWaitForDialog(editor);
+    await TinyUiActions.pWaitForPopup(editor, alertDialogSelector);
+    // Click on Save button (should be disabled)
+    TinyUiActions.clickOnUi(editor, 'button.tox-button:contains(OK)');
+    await Waiter.pTryUntil('Alert dialog should close', () => UiFinder.notExists(SugarBody.body(), alertDialogSelector));
+    TinyUiActions.submitDialog(editor);
+    await Waiter.pTryUntil('Dialog should not close', () => UiFinder.exists(SugarBody.body(), dialogSelector));
+    TinyUiActions.cancelDialog(editor);
+    await Waiter.pTryUntil('Dialog should close', () => UiFinder.notExists(SugarBody.body(), dialogSelector));
+    TinyAssertions.assertContent(editor, '');
+  });
 });
