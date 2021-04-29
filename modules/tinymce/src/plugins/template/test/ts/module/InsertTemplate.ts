902e996cddab137a144b50f8a50c481ee79474c4
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2021-04-29 09:36:25 +0300

    TINY-7319: BDD conversion: Template (#6742)

---
+++ modules/tinymce/src/plugins/template/test/ts/module/InsertTemplate.ts
@@ -0,0 +1,23 @@
+import { UiFinder, Waiter } from '@ephox/agar';
+import { Type } from '@ephox/katamari';
+import { TinyUiActions } from '@ephox/mcagar';
+import { SugarBody, SugarElement } from '@ephox/sugar';
+
+import Editor from 'tinymce/core/api/Editor';
+
+const toolbarButtonSelector = '[role="toolbar"] button[aria-label="Insert template"]';
+const dialogSelector = 'div.tox-dialog';
+
+const pInsertTemplate = async (editor: Editor, assertFn?: (elm: SugarElement<Node>) => Promise<void>) => {
+  TinyUiActions.clickOnToolbar(editor, toolbarButtonSelector);
+  const dialogEl = await TinyUiActions.pWaitForDialog(editor);
+  if (Type.isFunction(assertFn)) {
+    await assertFn(dialogEl);
+  }
+  TinyUiActions.submitDialog(editor);
+  await Waiter.pTryUntil('Dialog should close', () => UiFinder.notExists(SugarBody.body(), dialogSelector));
+};
+
+export {
+  pInsertTemplate
+};
