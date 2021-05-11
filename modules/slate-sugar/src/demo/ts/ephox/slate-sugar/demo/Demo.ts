1ef4b1751e92b231e5d9f431e88e4822e0b225e2
Viktor Pyshnenko <viktor.pyshnenko@tiny.cloud>
2021-05-11 14:18:16 +0300

    TINY-7381: PredicateFilter.descendants should return array not wrapped in MayBe

--- modules/slate-sugar/src/demo/ts/ephox/slate-sugar/demo/Demo.ts
+++ modules/slate-sugar/src/demo/ts/ephox/slate-sugar/demo/Demo.ts
@@ -12,16 +12,16 @@ tinymce.init({
     const api = Maybes.getOrDie(Api.getModelApi(editor));
     const root = Body.getBody(api);
 
-    const [ p ] = Maybes.getOrDie(PredicateFilter.descendants(api, root, ({ node }) =>
-      ModelNodeType.isElement(api, node) ? node.type === 'p' : false));
+    const [ p ] = PredicateFilter.descendants(api, root, ({ node }) =>
+      ModelNodeType.isElement(api, node) ? node.type === 'p' : false);
 
     const div = Maybes.getOrDie(PredicateFind.closest(api, p, ({ node }) =>
       ModelNodeType.isElement(api, node) ? node.class === 'content' : false, (el) => el === root));
 
-    const textLocs = Maybes.getOrDie(PredicateFilter.descendants(api, p, ({ node }) => ModelNodeType.isText(api, node)));
+    const textLocs = PredicateFilter.descendants(api, p, ({ node }) => ModelNodeType.isText(api, node));
     const [ bye, hello ] = textLocs.reverse();
-    const [ time ] = Maybes.getOrDie(PredicateFilter.descendants(api, div, ({ node }) => ModelNodeType.isInline(api, node)));
-    const blocks = Maybes.getOrDie(PredicateFilter.descendants(api, root, ({ node }) => ModelNodeType.isBlock(api, node)));
+    const [ time ] = PredicateFilter.descendants(api, div, ({ node }) => ModelNodeType.isInline(api, node));
+    const blocks = PredicateFilter.descendants(api, root, ({ node }) => ModelNodeType.isBlock(api, node));
 
     NodeTransforms.setPropsAtPath(api, SlateLoc.toPathArray(api, p), { style: 'font-weight: bold' });
     blocks.forEach((loc) => NodeTransforms.setPropsAtPath(api, SlateLoc.toPathArray(api, loc), { style: 'border: 1px solid green' }));
--- modules/slate-sugar/src/main/ts/ephox/slate-sugar/api/PredicateFilter.ts
+++ modules/slate-sugar/src/main/ts/ephox/slate-sugar/api/PredicateFilter.ts
@@ -1,8 +1,7 @@
-import { Maybes, Maybe } from '@ephox/katamari';
 import { ModelApi, SlateLoc } from './Api';
 
-const descendants = (api: ModelApi, scope: SlateLoc, predicate: (loc: SlateLoc) => boolean): Maybe<SlateLoc[]> =>
-  Maybes.from(api.predicateFilter.descendants(scope, predicate));
+const descendants = (api: ModelApi, scope: SlateLoc, predicate: (loc: SlateLoc) => boolean): SlateLoc[] =>
+  api.predicateFilter.descendants(scope, predicate);
 
 export {
   descendants
