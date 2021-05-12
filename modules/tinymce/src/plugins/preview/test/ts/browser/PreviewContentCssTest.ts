983eee961da2df7eceaa6ecc52c5d93c69d2371d
JamesToohey <james.toohey@tiny.cloud>
2021-05-12 11:01:29 +1000

    TINY-6870: BDD style browser tests for preview plugin

--- modules/tinymce/src/plugins/preview/test/ts/browser/PreviewContentCssTest.ts
+++ modules/tinymce/src/plugins/preview/test/ts/browser/PreviewContentCssTest.ts
@@ -1,41 +1,34 @@
-import { Log, Logger, Pipeline, Step } from '@ephox/agar';
-import { Assert, UnitTest } from '@ephox/bedrock-client';
-import { TinyApis, TinyLoader } from '@ephox/mcagar';
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyHooks } from '@ephox/mcagar';
+import { assert } from 'chai';
+
 import Editor from 'tinymce/core/api/Editor';
 import * as IframeContent from 'tinymce/plugins/preview/core/IframeContent';
+import Plugin from 'tinymce/plugins/preview/Plugin';
+import Theme from 'tinymce/themes/silver/Theme';
 
-import PreviewPlugin from 'tinymce/plugins/preview/Plugin';
-import SilverTheme from 'tinymce/themes/silver/Theme';
-
-UnitTest.asynctest('browser.tinymce.plugins.preview.PreviewContentCssTest', (success, failure) => {
-
-  PreviewPlugin();
-  SilverTheme();
-
-  const sAssertIframeHtmlContains = (editor: Editor, text: string) => Logger.t('Assert Iframe Html contains ' + text, Step.sync(() => {
-    const actual = IframeContent.getPreviewHtml(editor);
-    const regexp = new RegExp(text);
-
-    Assert.eq('Should be same html', true, regexp.test(actual));
-  }));
-
-  TinyLoader.setupLight((editor, onSuccess, onFailure) => {
-    const tinyApis = TinyApis(editor);
-    const contentCssUrl = editor.documentBaseURI.toAbsolute('/project/tinymce/js/tinymce/skins/content/default/content.css');
-
-    Pipeline.async({},
-      Log.steps('TBA', 'Preview: Set content, set content_css_cors and assert link elements. Delete setting and assert crossOrigin attr is removed', [
-        tinyApis.sSetContent('<p>hello world</p>'),
-        tinyApis.sSetSetting('content_css_cors', true),
-        sAssertIframeHtmlContains(editor, `<link type="text/css" rel="stylesheet" href="${contentCssUrl}" crossorigin="anonymous">`),
-        tinyApis.sSetSetting('content_css_cors', false),
-        sAssertIframeHtmlContains(editor, `<link type="text/css" rel="stylesheet" href="${contentCssUrl}">`)
-      ])
-      , onSuccess, onFailure);
-  }, {
-    theme: 'silver',
+describe('browser.tinymce.plugins.preview.PreviewContentCssTest', () => {
+  const hook = TinyHooks.bddSetupLight<Editor>({
     plugins: 'preview',
     base_url: '/project/tinymce/js/tinymce',
     content_css: '/project/tinymce/js/tinymce/skins/content/default/content.css'
-  }, success, failure);
+  }, [ Theme, Plugin ]);
+
+  const assertIframeHtmlContains = (editor: Editor, text: string) => {
+    const actual = IframeContent.getPreviewHtml(editor);
+    const regexp = new RegExp(text);
+
+    assert.isTrue(regexp.test(actual), 'Should be the same html');
+  };
+
+  it('TBA: Set content, set content_css_cors and assert link elements. Delete setting and assert crossOrigin attr is removed', () => {
+    const editor = hook.editor();
+    const contentCssUrl = editor.documentBaseURI.toAbsolute('/project/tinymce/js/tinymce/skins/content/default/content.css');
+
+    editor.setContent('<p>hello world</p>');
+    editor.settings.content_css_cors = true;
+    assertIframeHtmlContains(editor, `<link type="text/css" rel="stylesheet" href="${contentCssUrl}" crossorigin="anonymous">`);
+    editor.settings.content_css_cors = false;
+    assertIframeHtmlContains(editor, `<link type="text/css" rel="stylesheet" href="${contentCssUrl}">`);
+  });
 });
--- modules/tinymce/src/plugins/preview/test/ts/browser/PreviewSanityTest.ts
+++ modules/tinymce/src/plugins/preview/test/ts/browser/PreviewSanityTest.ts
@@ -1,46 +1,39 @@
-import { GeneralSteps, Keyboard, Keys, Log, Logger, Pipeline, UiFinder, Waiter } from '@ephox/agar';
-import { UnitTest } from '@ephox/bedrock-client';
-import { TinyApis, TinyDom, TinyLoader, TinyUi } from '@ephox/mcagar';
+import { Keyboard, Keys, UiFinder, Waiter } from '@ephox/agar';
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyDom, TinyHooks, TinyUiActions } from '@ephox/mcagar';
 import { SugarElement } from '@ephox/sugar';
 
-import PreviewPlugin from 'tinymce/plugins/preview/Plugin';
-import SilverTheme from 'tinymce/themes/silver/Theme';
+import Editor from 'tinymce/core/api/Editor';
+import Plugin from 'tinymce/plugins/preview/Plugin';
+import Theme from 'tinymce/themes/silver/Theme';
 
-UnitTest.asynctest('browser.tinymce.plugins.preview.PreviewSanityTest', (success, failure) => {
-
-  PreviewPlugin();
-  SilverTheme();
-
-  const dialogSelector = 'div[role="dialog"]';
-  const docBody = SugarElement.fromDom(document.body);
-  const doc = TinyDom.fromDom(document);
-
-  TinyLoader.setupLight((editor, onSuccess, onFailure) => {
-    const tinyApis = TinyApis(editor);
-    const tinyUi = TinyUi(editor);
-
-    const sOpenDialog = () => GeneralSteps.sequence(Logger.ts('Open dialog and wait for it to be visible', [
-      tinyUi.sClickOnToolbar('click on preview toolbar', 'button'),
-      tinyUi.sWaitForPopup('wait for preview popup', '[role="dialog"] iframe')
-    ]));
-
-    Pipeline.async({},
-      Log.steps('TBA', 'Preview: Set content, open dialog, click Close to close dialog. Open dialog, press escape and assert dialog closes', [
-        tinyApis.sSetContent('<strong>a</strong>'),
-
-        sOpenDialog(),
-        tinyUi.sClickOnUi('Click on Close button', '.tox-button:not(.tox-button--secondary)'),
-        Waiter.sTryUntil('Dialog should close', UiFinder.sNotExists(docBody, dialogSelector)),
-
-        sOpenDialog(),
-        Keyboard.sKeydown(doc, Keys.escape(), { }),
-        Waiter.sTryUntil('Dialog should close on esc', UiFinder.sNotExists(docBody, dialogSelector))
-      ])
-      , onSuccess, onFailure);
-  }, {
-    theme: 'silver',
+describe('browser.tinymce.plugins.preview.PreviewSanityTest', () => {
+  const hook = TinyHooks.bddSetupLight<Editor>({
     plugins: 'preview',
     toolbar: 'preview',
     base_url: '/project/tinymce/js/tinymce'
-  }, success, failure);
+  }, [ Theme, Plugin ]);
+
+  const dialogSelector = 'div[role="dialog"]';
+  const docBody = SugarElement.fromDom(document.body);
+
+  const openDialog = async (editor: Editor) => {
+    TinyUiActions.clickOnToolbar(editor, 'button');
+    await TinyUiActions.pWaitForPopup(editor, '[role="dialog"] iframe');
+  };
+
+  it('TBA: Set content, open dialog, click Close to close dialog. Open dialog, press escape and assert dialog closes', async () => {
+    const editor = hook.editor();
+    editor.setContent('<strong>a</strong>');
+
+    await openDialog(editor);
+    // Close dialog with button
+    TinyUiActions.clickOnUi(editor, '.tox-button:not(.tox-button--secondary)');
+    await Waiter.pTryUntil('Dialog should close', () => UiFinder.notExists(docBody, dialogSelector));
+
+    await openDialog(editor);
+    // Close dialog with escape key
+    Keyboard.keydown(Keys.escape(), { }, TinyDom.fromDom(document));
+    await Waiter.pTryUntil('Dialog should close on esc', () => UiFinder.notExists(docBody, dialogSelector));
+  });
 });
