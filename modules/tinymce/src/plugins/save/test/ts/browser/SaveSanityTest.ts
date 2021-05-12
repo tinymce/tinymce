564008297bac2dab6a85085b9a543b177889e9f1
JamesToohey <james.toohey@tiny.cloud>
2021-05-12 15:26:40 +1000

    Split test and use TinyContentActions

--- modules/tinymce/src/plugins/save/test/ts/browser/SaveSanityTest.ts
+++ modules/tinymce/src/plugins/save/test/ts/browser/SaveSanityTest.ts
@@ -1,6 +1,6 @@
-import { Keyboard, Keys } from '@ephox/agar';
+import { Keys } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
-import { TinyDom, TinyHooks, TinySelections, TinyUiActions } from '@ephox/mcagar';
+import { TinyContentActions, TinyHooks, TinySelections, TinyUiActions } from '@ephox/mcagar';
 
 import Editor from 'tinymce/core/api/Editor';
 import Plugin from 'tinymce/plugins/save/Plugin';
@@ -13,13 +13,17 @@ describe('browser.tinymce.plugins.save.SaveSanityTest', () => {
     base_url: '/project/tinymce/js/tinymce',
   }, [ Theme, Plugin ]);
 
-  it('TBA: Assert Save button is disabled when editor is opened. Add content and assert Save button is enabled', async () => {
+  it('TBA: Assert Save button is disabled when editor is opened.', async () => {
     const editor = hook.editor();
     // button is disabled
     await TinyUiActions.pWaitForUi(editor, 'button.tox-tbtn--disabled[aria-label="Save"]');
+  });
+
+  it('TBA: Add content and assert Save button is enabled.', async () => {
+    const editor = hook.editor();
     editor.setContent('<p>a</p>');
     TinySelections.setCursor(editor, [ 0, 0 ], 1);
-    Keyboard.activeKeystroke(TinyDom.document(editor), Keys.enter());
+    TinyContentActions.keystroke(editor, Keys.enter());
     // button no longer disabled
     await TinyUiActions.pWaitForUi(editor, 'button[aria-label="Save"]:not(.tox-tbtn--disabled)');
   });
