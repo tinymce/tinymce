54c5aeca0d658356f89d8a6d7dc7a2a267656b77
techtangents <167879+techtangents@users.noreply.github.com>
2021-05-18 10:51:11 +1000

    BDD conversion for ArrArrayLikeTest.ts

--- modules/katamari/src/test/ts/atomic/api/arr/ArrArrayLikeTest.ts
+++ modules/katamari/src/test/ts/atomic/api/arr/ArrArrayLikeTest.ts
@@ -1,4 +1,5 @@
-import { Assert, UnitTest } from '@ephox/bedrock-client';
+import { context, describe } from '@ephox/bedrock-client';
+import { assert } from 'chai';
 import * as Arr from 'ephox/katamari/api/Arr';
 
 const arrayLike: ArrayLike<number> = {
@@ -11,11 +12,28 @@ const arrayLike: ArrayLike<number> = {
   5: 5
 };
 
-UnitTest.test('ArrArrayLikeTest', () => {
-  Assert.eq('eq', 3, Arr.indexOf(arrayLike, 3).getOrDie('test should have found a 3'));
-  Assert.eq('eq', true, Arr.contains(arrayLike, 3));
-  Assert.eq('eq', [ 1, 2, 3, 4, 5, 6 ], Arr.map(arrayLike, ((n) => n + 1)));
-  Assert.eq('eq', 3, Arr.find(arrayLike, ((n) => n === 3)).getOrDie('test should have found a 3'));
-  Assert.eq('eq', 0, Arr.head(arrayLike).getOrDie('array like object should have a head'));
-  Assert.eq('eq', 5, Arr.last(arrayLike).getOrDie('array like object should have a last'));
+describe('ArrArrayLikeTest', () => {
+  context('indexOf', () => {
+    assert.deepEqual(Arr.indexOf(arrayLike, 3).getOrDie('test should have found a 3'), 3);
+  });
+
+  context('contains', () => {
+    assert.deepEqual(Arr.contains(arrayLike, 3), true);
+  });
+
+  context('map', () => {
+    assert.deepEqual(Arr.map(arrayLike, ((n) => n + 1)), [ 1, 2, 3, 4, 5, 6 ]);
+  });
+
+  context('find', () => {
+    assert.deepEqual(Arr.find(arrayLike, ((n) => n === 3)).getOrDie('test should have found a 3'), 3);
+  });
+
+  context('head', () => {
+    assert.deepEqual(Arr.head(arrayLike).getOrDie('array like object should have a head'), 0);
+  });
+
+  context('last', () => {
+    assert.deepEqual(Arr.last(arrayLike).getOrDie('array like object should have a last'), 5);
+  });
 });
