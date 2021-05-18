7f7a121ce8c0bb427f63cf5fdd010f54b083185e
techtangents <167879+techtangents@users.noreply.github.com>
2021-05-18 16:34:15 +1000

    Tweak tests

--- modules/katamari/src/test/ts/atomic/api/arr/ExistsTest.ts
+++ modules/katamari/src/test/ts/atomic/api/arr/ExistsTest.ts
@@ -11,7 +11,7 @@ const bottom = () => {
 
 describe('atomic.katamari.api.arr.ExistsTest', () => {
   it('Arr.exists: unit test', () => {
-    const check = (expected, input: any[], f) => {
+    const check = (expected: boolean, input: number[], f: (b: number) => boolean) => {
       assert.deepEqual(Arr.exists(input, f), expected);
       assert.deepEqual(Arr.exists(Object.freeze(input.slice()), f), expected);
     };
@@ -19,26 +19,26 @@ describe('atomic.katamari.api.arr.ExistsTest', () => {
     check(true, [ 1, 2, 3 ], eqc(1));
     check(false, [ 2, 3, 4 ], eqc(1));
 
-    assert.deepEqual(Arr.exists([], bottom), false);
-    assert.deepEqual(Arr.exists([], always), false);
+    assert.isFalse(Arr.exists([], bottom));
+    assert.isFalse(Arr.exists([], always));
   });
 
   it('Element exists in middle of array', () => {
     fc.assert(fc.property(fc.array(fc.integer()), fc.integer(), fc.array(fc.integer()), (prefix, element, suffix) => {
       const arr2 = Arr.flatten([ prefix, [ element ], suffix ]);
-      assert.deepEqual(Arr.exists(arr2, eqc(element)), true);
+      assert.isTrue(Arr.exists(arr2, eqc(element)));
     }));
   });
 
   it('Element exists in singleton array of itself', () => {
     fc.assert(fc.property(fc.array(fc.integer()), (i) => {
-      assert.deepEqual(Arr.exists([ i ], eqc(i)), true);
+      assert.isTrue(Arr.exists([ i ], eqc(i)));
     }));
   });
 
   it('Element does not exist in empty array', () => {
     fc.assert(fc.property(fc.array(fc.integer()), (i) => {
-      assert.deepEqual(Arr.exists([], eqc(i)), false);
+      assert.isFalse(Arr.exists([], eqc(i)));
     }));
   });
 
@@ -49,7 +49,7 @@ describe('atomic.katamari.api.arr.ExistsTest', () => {
   it('Element exists in non-empty array when predicate always returns true', () => {
     fc.assert(fc.property(fc.array(fc.integer()), fc.integer(), (xs, x) => {
       const arr = Arr.flatten([ xs, [ x ]]);
-      return Arr.exists(arr, always);
+      assert.isTrue(Arr.exists(arr, always));
     }));
   });
 });
