de5235202fe34b164e2613625ddf2c4fe59509a5
Lee Newson <lee.newson@tiny.cloud>
2021-05-20 10:14:48 +1000

    TINY-6870: Converted the paste plugin to use BDD style tests (#6832)

--- modules/tinymce/src/plugins/paste/test/ts/browser/PasteBinTest.ts
+++ modules/tinymce/src/plugins/paste/test/ts/browser/PasteBinTest.ts
@@ -1,18 +1,27 @@
-import { Assertions, Chain, Guard, Log, Pipeline } from '@ephox/agar';
-import { UnitTest } from '@ephox/bedrock-client';
-import { Obj } from '@ephox/katamari';
+import { before, describe, it } from '@ephox/bedrock-client';
+import { Arr } from '@ephox/katamari';
 import { McEditor } from '@ephox/mcagar';
+import { assert } from 'chai';
 
+import Editor from 'tinymce/core/api/Editor';
+import { RawEditorSettings } from 'tinymce/core/api/SettingsTypes';
 import { getPasteBinParent, PasteBin } from 'tinymce/plugins/paste/core/PasteBin';
-import PastePlugin from 'tinymce/plugins/paste/Plugin';
+import Plugin from 'tinymce/plugins/paste/Plugin';
 import Theme from 'tinymce/themes/silver/Theme';
 
-UnitTest.asynctest('browser.tinymce.plugins.paste.PasteBin', (success, failure) => {
+interface TestCase {
+  readonly label: string;
+  readonly content: string;
+  readonly result: string;
+}
 
-  Theme();
-  PastePlugin();
+describe('browser.tinymce.plugins.paste.PasteBin', () => {
+  before(() => {
+    Theme();
+    Plugin();
+  });
 
-  const cases = [
+  const cases: TestCase[] = [
     {
       label: 'TINY-1162: testing nested paste bins',
       content:
@@ -54,59 +63,37 @@ UnitTest.asynctest('browser.tinymce.plugins.paste.PasteBin', (success, failure) 
     }
   ];
 
-  const cCreateEditorFromSettings = (settings = {}, html?) => {
-    return Chain.control(
-      McEditor.cFromHtml(html, {
-        ...settings,
-        add_unload_trigger: false,
-        indent: false,
-        plugins: 'paste',
-        base_url: '/project/tinymce/js/tinymce'
-      }),
-      Guard.addLogging(`Create editor using settings ${settings}`)
-    );
+  const pCreateEditorFromSettings = (settings: RawEditorSettings = {}, html?: string) =>
+    McEditor.pFromHtml<Editor>(html, {
+      ...settings,
+      add_unload_trigger: false,
+      indent: false,
+      plugins: 'paste',
+      base_url: '/project/tinymce/js/tinymce'
+    });
+
+  const pCreateEditorFromHtml = (html: string, settings: RawEditorSettings) =>
+    pCreateEditorFromSettings(settings, html);
+
+  const assertCases = (editor: Editor, cases: TestCase[]) => {
+    const pasteBin = PasteBin(editor);
+    Arr.each(cases, (c) => {
+      getPasteBinParent(editor).appendChild(editor.dom.createFragment(c.content));
+      assert.equal(pasteBin.getHtml(), c.result, c.label);
+      pasteBin.remove();
+    });
   };
 
-  const cCreateEditorFromHtml = (html, settings) => {
-    return Chain.control(
-      cCreateEditorFromSettings(settings, html),
-      Guard.addLogging(`Create editor using ${html}`)
-    );
-  };
+  it('TBA: Create editor from settings and test nested and adjacent paste bins', async () => {
+    const editor = await pCreateEditorFromSettings();
+    assertCases(editor, cases);
+    McEditor.remove(editor);
+  });
 
-  const cRemoveEditor = () => {
-    return Chain.control(
-      McEditor.cRemove,
-      Guard.addLogging('Remove Editor')
-    );
-  };
-
-  const cAssertCases = (cases) => {
-    return Chain.control(
-      Chain.op((editor: any) => {
-        const pasteBin = PasteBin(editor);
-        Obj.each(cases, (c, i) => {
-          getPasteBinParent(editor).appendChild(editor.dom.createFragment(c.content));
-          Assertions.assertEq(c.label || 'Asserting paste bin case ' + i, c.result, pasteBin.getHtml());
-          pasteBin.remove();
-        });
-      }),
-      Guard.addLogging('Assert cases')
-    );
-  };
-
-  Pipeline.async({}, [
-    Chain.asStep({}, Log.chains('TBA', 'Paste: Create editor from settings and test nested and adjacent paste bins', [
-      cCreateEditorFromSettings(),
-      cAssertCases(cases),
-      cRemoveEditor()
-    ])),
-
-    // TINY-1208/TINY-1209: same cases, but for inline editor
-    Chain.asStep({}, Log.chains('TBA', 'Paste: Create editor from html and test nested and adjacent paste bins', [
-      cCreateEditorFromHtml('<div>some text</div>', { inline: true }),
-      cAssertCases(cases),
-      cRemoveEditor()
-    ]))
-  ], success, failure);
+  // TINY-1208/TINY-1209: same cases, but for inline editor
+  it('TBA: Create editor from html and test nested and adjacent paste bins', async () => {
+    const editor = await pCreateEditorFromHtml('<div>some text</div>', { inline: true });
+    assertCases(editor, cases);
+    McEditor.remove(editor);
+  });
 });
