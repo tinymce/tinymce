e48785b5822cac7cd45e5a35a7242f866f60fb9f
techtangents <167879+techtangents@users.noreply.github.com>
2021-05-26 19:59:50 +1000

    TINY-7469: Convert katamari fun/num/obj tests to BDD (#6845)

--- modules/katamari/src/test/ts/atomic/api/obj/MergerTest.ts
+++ modules/katamari/src/test/ts/atomic/api/obj/MergerTest.ts
@@ -1,146 +1,149 @@
-import { Assert, UnitTest } from '@ephox/bedrock-client';
+import { describe, it } from '@ephox/bedrock-client';
+import { assert } from 'chai';
 import fc from 'fast-check';
 import * as Arr from 'ephox/katamari/api/Arr';
 import * as Merger from 'ephox/katamari/api/Merger';
 import * as Obj from 'ephox/katamari/api/Obj';
 
-UnitTest.test('Merger', () => {
-  Assert.eq('eq', {}, Merger.merge({}, {}));
-  Assert.eq('eq', { a: 'A' }, Merger.merge({ a: 'A' }, {}));
-  Assert.eq('eq', { a: 'A' }, Merger.merge({ a: 'A' }, {}, {}));
-  Assert.eq('eq', { a: 'A' }, Merger.merge({ a: 'A' }, {}, {}));
-  Assert.eq('eq', { a: 'A' }, Merger.merge({}, { a: 'A' }, {}));
-  Assert.eq('eq', { a: 'A' }, Merger.merge({}, {}, { a: 'A' }));
+describe('atomic.katamari.api.obj.MergerTest', () => {
+  it('Merger', () => {
+    assert.deepEqual(Merger.merge({}, {}), {});
+    assert.deepEqual(Merger.merge({ a: 'A' }, {}), { a: 'A' });
+    assert.deepEqual(Merger.merge({ a: 'A' }, {}, {}), { a: 'A' });
+    assert.deepEqual(Merger.merge({ a: 'A' }, {}, {}), { a: 'A' });
+    assert.deepEqual(Merger.merge({}, { a: 'A' }, {}), { a: 'A' });
+    assert.deepEqual(Merger.merge({}, {}, { a: 'A' }), { a: 'A' });
 
-  Assert.eq('eq', { a: 'A', b: { bb: 'BB' }}, Merger.merge({ b: { bb: 'BB' }}, { a: 'A' }));
-  Assert.eq('eq', { a: 'A', b: { bb: 'BB' }}, Merger.merge({}, { a: 'A', b: { bb: 'BB' }}));
-  Assert.eq('eq', { a: 'A', b: { bb: 'BB' }}, Merger.merge({ b: { bb: 'B' }}, { a: 'A', b: { bb: 'BB' }}));
-  Assert.eq('eq', { a: 'A', b: { bb: 'BB' }}, Merger.merge({ b: { bb: 'BB' }}, { a: 'A' }));
-  Assert.eq('eq', { a: 'A', b: { bb: 'BB' }}, Merger.merge({ b: { bb: 'BB' }}, { a: 'A' }));
-  Assert.eq('eq', { a: 'A', b: { bb: 'BB' }}, Merger.merge({ b: { bb: 'BB' }}, { a: 'A' }));
+    assert.deepEqual(Merger.merge({ b: { bb: 'BB' }}, { a: 'A' }), { a: 'A', b: { bb: 'BB' }});
+    assert.deepEqual(Merger.merge({}, { a: 'A', b: { bb: 'BB' }}), { a: 'A', b: { bb: 'BB' }});
+    assert.deepEqual(Merger.merge({ b: { bb: 'B' }}, { a: 'A', b: { bb: 'BB' }}), { a: 'A', b: { bb: 'BB' }});
+    assert.deepEqual(Merger.merge({ b: { bb: 'BB' }}, { a: 'A' }), { a: 'A', b: { bb: 'BB' }});
+    assert.deepEqual(Merger.merge({ b: { bb: 'BB' }}, { a: 'A' }), { a: 'A', b: { bb: 'BB' }});
+    assert.deepEqual(Merger.merge({ b: { bb: 'BB' }}, { a: 'A' }), { a: 'A', b: { bb: 'BB' }});
 
-  Assert.eq('eq', {
-    a: 'A',
-    b: {
-      bb: 'BB',
-      bc: [ 1, 'bc' ],
-      bd: {
-        bdd: 'BDBD'
+    assert.deepEqual(Merger.merge({
+      a: 'A'
+    }, {
+      b: {
+        ba: 'BA'
       }
-    }
-  }, Merger.merge({
-    a: 'A'
-  }, {
-    b: {
-      ba: 'BA'
-    }
-  }, {
-    b: {
-      bb: 'BB',
-      bc: [ 1, 'bc' ],
-      bd: {
-        bdd: 'BDBD'
+    }, {
+      b: {
+        bb: 'BB',
+        bc: [ 1, 'bc' ],
+        bd: {
+          bdd: 'BDBD'
+        }
       }
-    }
-  }));
-
-  Assert.eq('eq', {
-    a: 'A',
-    b: {
-      ba: 'BA',
-      bb: 'BB',
-      bc: [ 1, 'bc' ],
-      bd: {
-        bdd: 'BDBD'
+    }), {
+      a: 'A',
+      b: {
+        bb: 'BB',
+        bc: [ 1, 'bc' ],
+        bd: {
+          bdd: 'BDBD'
+        }
       }
-    }
-  }, Merger.deepMerge({
-    a: 'A'
-  }, {
-    b: {
-      ba: 'BA'
-    }
-  }, {
-    b: {
-      bb: 'BB',
-      bc: [ 1, 'bc' ],
-      bd: {
-        bdd: 'BDBD'
-      }
-    }
-  }));
-});
-
-UnitTest.test('Merged with identity on left', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
-    Assert.eq('eq', obj, Merger.merge({}, obj));
-  }));
-});
-
-UnitTest.test('Merged with identity on right', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
-    Assert.eq('eq', obj, Merger.merge(obj, {}));
-  }));
-});
-
-UnitTest.test('Merged with itself is itself', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
-    Assert.eq('eq', obj, Merger.merge(obj, obj));
-  }));
-});
-
-UnitTest.test('Merge(a, Merge(b, c)) === Merge(Merge(a, b), c)', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), (a, b, c) => {
-    const one = Merger.merge(a, Merger.merge(b, c));
-    const other = Merger.merge(Merger.merge(a, b), c);
-    Assert.eq('eq', one, other);
-  }));
-});
-
-UnitTest.test('Merge(a, b) contains all the keys of b', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), (a, b) => {
-    const output = Merger.merge(a, b);
-    const keys = Obj.keys(b);
-    const oKeys = Obj.keys(output);
-    return Arr.forall(keys, (k) => {
-      return Arr.contains(oKeys, k);
     });
-  }));
-});
 
-UnitTest.test('Deep-merged with identity on left', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
-    Assert.eq('eq', obj, Merger.deepMerge({}, obj));
-  }));
-});
-
-UnitTest.test('Deep-merged with identity on right', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
-    Assert.eq('eq', obj, Merger.deepMerge(obj, {}));
-  }));
-});
-
-UnitTest.test('Deep-merged with itself is itself', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
-    Assert.eq('eq', obj, Merger.deepMerge(obj, obj));
-  }));
-});
-
-UnitTest.test('Deep-merge(a, Deep-merge(b, c)) === Deep-merge(Deep-merge(a, b), c)', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), (a, b, c) => {
-    const one = Merger.merge(a, Merger.merge(b, c));
-    const other = Merger.merge(Merger.merge(a, b), c);
-    Assert.eq('eq', one, other);
-  }));
-});
-
-UnitTest.test('Deep-merge(a, b) contains all the keys of b', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), (a, b) => {
-    const output = Merger.deepMerge(a, b);
-    const keys = Obj.keys(b);
-    const oKeys = Obj.keys(output);
-    return Arr.forall(keys, (k) => {
-      return Arr.contains(oKeys, k);
+    assert.deepEqual(Merger.deepMerge({
+      a: 'A'
+    }, {
+      b: {
+        ba: 'BA'
+      }
+    }, {
+      b: {
+        bb: 'BB',
+        bc: [ 1, 'bc' ],
+        bd: {
+          bdd: 'BDBD'
+        }
+      }
+    }), {
+      a: 'A',
+      b: {
+        ba: 'BA',
+        bb: 'BB',
+        bc: [ 1, 'bc' ],
+        bd: {
+          bdd: 'BDBD'
+        }
+      }
     });
-  }));
+  });
+
+  it('Merged with identity on left', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
+      assert.deepEqual(Merger.merge({}, obj), obj);
+    }));
+  });
+
+  it('Merged with identity on right', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
+      assert.deepEqual(Merger.merge(obj, {}), obj);
+    }));
+  });
+
+  it('Merged with itself is itself', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
+      assert.deepEqual(Merger.merge(obj, obj), obj);
+    }));
+  });
+
+  it('Merge(a, Merge(b, c)) === Merge(Merge(a, b), c)', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), (a, b, c) => {
+      const one = Merger.merge(a, Merger.merge(b, c));
+      const other = Merger.merge(Merger.merge(a, b), c);
+      assert.deepEqual(other, one);
+    }));
+  });
+
+  it('Merge(a, b) contains all the keys of b', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), (a, b) => {
+      const output = Merger.merge(a, b);
+      const keys = Obj.keys(b);
+      const oKeys = Obj.keys(output);
+      return Arr.forall(keys, (k) => {
+        return Arr.contains(oKeys, k);
+      });
+    }));
+  });
+
+  it('Deep-merged with identity on left', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
+      assert.deepEqual(Merger.deepMerge({}, obj), obj);
+    }));
+  });
+
+  it('Deep-merged with identity on right', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
+      assert.deepEqual(Merger.deepMerge(obj, {}), obj);
+    }));
+  });
+
+  it('Deep-merged with itself is itself', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), (obj) => {
+      assert.deepEqual(Merger.deepMerge(obj, obj), obj);
+    }));
+  });
+
+  it('Deep-merge(a, Deep-merge(b, c)) === Deep-merge(Deep-merge(a, b), c)', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), (a, b, c) => {
+      const one = Merger.merge(a, Merger.merge(b, c));
+      const other = Merger.merge(Merger.merge(a, b), c);
+      assert.deepEqual(other, one);
+    }));
+  });
+
+  it('Deep-merge(a, b) contains all the keys of b', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.json()), fc.dictionary(fc.asciiString(), fc.json()), (a, b) => {
+      const output = Merger.deepMerge(a, b);
+      const keys = Obj.keys(b);
+      const oKeys = Obj.keys(output);
+      return Arr.forall(keys, (k) => {
+        return Arr.contains(oKeys, k);
+      });
+    }));
+  });
 });
--- modules/katamari/src/test/ts/atomic/api/obj/ObjMapTest.ts
+++ modules/katamari/src/test/ts/atomic/api/obj/ObjMapTest.ts
@@ -1,79 +1,82 @@
-import { Assert, UnitTest } from '@ephox/bedrock-client';
+import { describe, it } from '@ephox/bedrock-client';
+import { assert } from 'chai';
 import fc from 'fast-check';
 import * as Arr from 'ephox/katamari/api/Arr';
 import * as Fun from 'ephox/katamari/api/Fun';
 import * as Obj from 'ephox/katamari/api/Obj';
 
-UnitTest.test('Obj.map: unit test', () => {
-  const dbl = (x) => x * 2;
+describe('atomic.katamari.api.obj.ObjMapTest', () => {
+  it('unit test', () => {
+    const dbl = (x) => x * 2;
 
-  const addDot = (x) => x + '.';
+    const addDot = (x) => x + '.';
 
-  const tupleF = (x, i) => ({
-    k: i + 'b',
-    v: x + 'b'
+    const tupleF = (x, i) => ({
+      k: i + 'b',
+      v: x + 'b'
+    });
+
+    const check = (expected, input, f) => {
+      assert.deepEqual(Obj.map(input, f), expected);
+    };
+
+    const checkT = (expected, input, f) => {
+      assert.deepEqual(Obj.tupleMap(input, f), expected);
+    };
+
+    check({}, {}, dbl);
+    check({ a: 'a.' }, { a: 'a' }, addDot);
+    check({ a: 'a.', b: 'b.', c: 'c.' }, { a: 'a', b: 'b', c: 'c' }, addDot);
+
+    checkT({}, {}, tupleF);
+    checkT({ ab: 'ab' }, { a: 'a' }, tupleF);
+    checkT({ ab: 'ab', bb: 'bb', cb: 'cb' }, { a: 'a', b: 'b', c: 'c' }, tupleF);
+
+    const stringify = (x, i) => i + ' :: ' + x;
+
+    const checkMapToArray = (expected, input, f) => {
+      assert.deepEqual(Obj.mapToArray(input, f), expected);
+    };
+
+    checkMapToArray([], {}, stringify);
+    checkMapToArray([ 'a :: a' ], { a: 'a' }, stringify);
+    checkMapToArray([ 'a :: a', 'b :: b', 'c :: c' ], { a: 'a', b: 'b', c: 'c' }, stringify);
   });
 
-  const check = (expected, input, f) => {
-    Assert.eq('check', expected, Obj.map(input, f));
-  };
+  it('map id obj = obj', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.integer()), (obj) => {
+      const actual = Obj.map(obj, Fun.identity);
+      assert.deepEqual(actual, obj);
+    }));
+  });
 
-  const checkT = (expected, input, f) => {
-    Assert.eq(' checkt', expected, Obj.tupleMap(input, f));
-  };
+  it('map constant obj means that values(obj) are all the constant', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.integer()), fc.integer(), (obj, x) => {
+      const output = Obj.map(obj, Fun.constant(x));
+      const values = Obj.values(output);
+      return Arr.forall(values, (v) => v === x);
+    }));
+  });
 
-  check({}, {}, dbl);
-  check({ a: 'a.' }, { a: 'a' }, addDot);
-  check({ a: 'a.', b: 'b.', c: 'c.' }, { a: 'a', b: 'b', c: 'c' }, addDot);
+  it('tupleMap obj (x, i) -> { k: i, v: x }', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.integer()), (obj) => {
+      const output = Obj.tupleMap(obj, (x, i) => ({ k: i, v: x }));
+      assert.deepEqual(output, obj);
+    }));
+  });
 
-  checkT({}, {}, tupleF);
-  checkT({ ab: 'ab' }, { a: 'a' }, tupleF);
-  checkT({ ab: 'ab', bb: 'bb', cb: 'cb' }, { a: 'a', b: 'b', c: 'c' }, tupleF);
+  it('mapToArray is symmetric with tupleMap', () => {
+    fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.integer()), (obj) => {
+      const array = Obj.mapToArray(obj, (x, i) => ({ k: i, v: x }));
 
-  const stringify = (x, i) => i + ' :: ' + x;
+      const aKeys = Arr.map(array, (x) => x.k);
+      const aValues = Arr.map(array, (x) => x.v);
 
-  const checkMapToArray = (expected, input, f) => {
-    Assert.eq('checkMapToArray', expected, Obj.mapToArray(input, f));
-  };
+      const keys = Obj.keys(obj);
+      const values = Obj.values(obj);
 
-  checkMapToArray([], {}, stringify);
-  checkMapToArray([ 'a :: a' ], { a: 'a' }, stringify);
-  checkMapToArray([ 'a :: a', 'b :: b', 'c :: c' ], { a: 'a', b: 'b', c: 'c' }, stringify);
-});
-
-UnitTest.test('Obj.map: map id obj = obj', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.integer()), (obj) => {
-    const actual = Obj.map(obj, Fun.identity);
-    Assert.eq('map id', obj, actual);
-  }));
-});
-
-UnitTest.test('map constant obj means that values(obj) are all the constant', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.integer()), fc.integer(), (obj, x) => {
-    const output = Obj.map(obj, Fun.constant(x));
-    const values = Obj.values(output);
-    return Arr.forall(values, (v) => v === x);
-  }));
-});
-
-UnitTest.test('tupleMap obj (x, i) -> { k: i, v: x }', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.integer()), (obj) => {
-    const output = Obj.tupleMap(obj, (x, i) => ({ k: i, v: x }));
-    Assert.eq('tupleMap', obj, output);
-  }));
-});
-
-UnitTest.test('mapToArray is symmetric with tupleMap', () => {
-  fc.assert(fc.property(fc.dictionary(fc.asciiString(), fc.integer()), (obj) => {
-    const array = Obj.mapToArray(obj, (x, i) => ({ k: i, v: x }));
-
-    const aKeys = Arr.map(array, (x) => x.k);
-    const aValues = Arr.map(array, (x) => x.v);
-
-    const keys = Obj.keys(obj);
-    const values = Obj.values(obj);
-
-    Assert.eq('same keys', Arr.sort(keys), Arr.sort(aKeys));
-    Assert.eq('same values', Arr.sort(values), Arr.sort(aValues));
-  }));
+      assert.deepEqual(Arr.sort(aKeys), Arr.sort(keys));
+      assert.deepEqual(Arr.sort(aValues), Arr.sort(values));
+    }));
+  });
 });
