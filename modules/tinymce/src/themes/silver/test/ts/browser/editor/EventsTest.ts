5a7eceb59567a8f853f2f5b067c76a2d397dbc94
Diogo Goncalves Silva <diogo.goncalves-silva@tiny.cloud>
2021-07-16 08:26:17 +1000

    TINY-7399: refactor tests

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/EventsTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/EventsTest.ts
@@ -15,21 +15,20 @@ describe('browser.tinymce.themes.silver.editor.EventsTest', () => {
 
   const hook1 = TinyHooks.bddSetupLight(settings, [ Theme ]);
   const hook2 = TinyHooks.bddSetupLight(settings, [ Theme ]);
+  const menuSelector = '[role="menu"]';
 
-  const waitToOpen = (selector: string) => {
-    return Waiter.pTryUntil(`Wait ${selector} to open`, () => UiFinder.exists(SugarBody.body(), selector));
-  };
+  const pWaitMenuToOpen = () =>
+    Waiter.pTryUntil('Wait for a menu to open', () => UiFinder.exists(SugarBody.body(), menuSelector));
 
-  const waitToClose = (selector: string) => {
-    return Waiter.pTryUntil(`Wait ${selector} to close`, () => UiFinder.notExists(SugarBody.body(), selector));
-  };
+  const pWaitMenuToClose = () =>
+    Waiter.pTryUntil('Wait for all menus to close', () => UiFinder.notExists(SugarBody.body(), menuSelector));
 
   it('TINY-7399: Clicking on the editor should close the menu', async () => {
     const editor = hook1.editor();
     TinyUiActions.clickOnMenu(editor, '[role="menuitem"]');
-    await waitToOpen('[role="menu"]');
+    await pWaitMenuToOpen();
     TinyContentActions.trueClick(editor);
-    await waitToClose('[role="menu"]');
+    await pWaitMenuToClose();
   });
 
   it('TINY-7399: Clicking on the editor should close the context menu', async () => {
@@ -37,7 +36,7 @@ describe('browser.tinymce.themes.silver.editor.EventsTest', () => {
     TinyContentActions.trueClick(editor);
     await TinyUiActions.pTriggerContextMenu(editor, 'p', '[role="menu"]');
     TinyContentActions.trueClick(editor);
-    await waitToClose('[role="menu"]');
+    await pWaitMenuToClose();
   });
 
   it('TINY-7399: Clicking on the editor should close other editors menus', async () => {
@@ -45,9 +44,9 @@ describe('browser.tinymce.themes.silver.editor.EventsTest', () => {
     const editor2 = hook2.editor();
 
     TinyUiActions.clickOnMenu(editor1, '[role="menuitem"]');
-    await waitToOpen('[role="menu"]');
+    await pWaitMenuToOpen();
     TinyContentActions.trueClick(editor2);
-    await waitToClose('[role="menu"]');
+    await pWaitMenuToClose();
   });
 
   it('TINY-7399: Clicking on the editor should close other editors context menus', async () => {
@@ -56,7 +55,7 @@ describe('browser.tinymce.themes.silver.editor.EventsTest', () => {
 
     await TinyUiActions.pTriggerContextMenu(editor1, 'p', '[role="menu"]');
     TinyContentActions.trueClick(editor2);
-    await waitToClose('[role="menu"]');
+    await pWaitMenuToClose();
   });
 
   it('TINY-7399: Opening a menu in the editor should close other editor\'s menu', async () => {
@@ -64,9 +63,9 @@ describe('browser.tinymce.themes.silver.editor.EventsTest', () => {
     const editor2 = hook2.editor();
 
     TinyUiActions.clickOnMenu(editor1, '[role="menuitem"]');
-    await waitToOpen('[role="menu"]');
+    await pWaitMenuToOpen();
     Mouse.trueClickOn(TinyDom.container(editor2), '[role="menuitem"]');
-    await waitToOpen('[role="menu"]');
+    await pWaitMenuToClose();
 
     const menus = UiFinder.findAllIn(SugarBody.body(), '[role="menu"]');
     assert.lengthOf(menus, 1, 'Should have one menu open');
