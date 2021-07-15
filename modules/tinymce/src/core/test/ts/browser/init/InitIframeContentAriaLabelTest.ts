a7f8da2b26b109eab1b52963f33239331ee26261
Diogo Goncalves Silva <diogo.goncalves-silva@tiny.cloud>
2021-07-15 16:28:52 +1000

    TINY-1264: refactor tests with pFromSettings

--- modules/tinymce/src/core/test/ts/browser/init/InitIframeContentAriaLabelTest.ts
+++ modules/tinymce/src/core/test/ts/browser/init/InitIframeContentAriaLabelTest.ts
@@ -1,33 +1,30 @@
 import { describe, it } from '@ephox/bedrock-client';
-import { TinyHooks } from '@ephox/mcagar';
+import { McEditor } from '@ephox/mcagar';
 import { Attribute, SugarElement } from '@ephox/sugar';
 import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
-import Theme from 'tinymce/themes/silver/Theme';
 
 describe('browser.tinymce.core.init.InitIframeContentAriaLabelTest', () => {
   const defaultIframeTitle = 'Rich Text Area. Press ALT-0 for help.';
   const customIframeTitle = 'Cupidatat magna aliquip.';
-  const hookWithDefaultIframeTitle = TinyHooks.bddSetupLight<Editor>({
-    base_url: '/project/tinymce/js/tinymce',
-  }, [ Theme ]);
-  const hookWithContentAriaLabel = TinyHooks.bddSetupLight<Editor>({
-    base_url: '/project/tinymce/js/tinymce',
-    content_aria_label: customIframeTitle
-  }, [ Theme ]);
 
-  it('TINY-1264: Should use the default iframe title when content_aria_label is not set', () => {
-    const editor = hookWithDefaultIframeTitle.editor();
+  it('TINY-1264: Should use the default iframe title when iframe_aria_text is not set', async () => {
+    const editor = await McEditor.pFromSettings<Editor>({
+      base_url: '/project/tinymce/js/tinymce'
+    });
     const iframe = SugarElement.fromDom(editor.iframeElement);
-
     assert.equal(Attribute.get(iframe, 'title'), defaultIframeTitle);
+    McEditor.remove(editor);
   });
 
-  it('TINY-1264: Should use content_aria_label as iframe title', () => {
-    const editor = hookWithContentAriaLabel.editor();
+  it('TINY-1264: Should use iframe_aria_text as the iframe title', async () => {
+    const editor = await McEditor.pFromSettings<Editor>({
+      base_url: '/project/tinymce/js/tinymce',
+      iframe_aria_text: customIframeTitle
+    });
     const iframe = SugarElement.fromDom(editor.iframeElement);
-
     assert.equal(Attribute.get(iframe, 'title'), customIframeTitle);
+    McEditor.remove(editor);
   });
 });
