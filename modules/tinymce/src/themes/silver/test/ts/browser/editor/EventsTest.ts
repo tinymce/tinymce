d9670fc9192ca4faa6ed5c5a2b1777a640293a5d
James Toohey <james.toohey@tiny.cloud>
2021-07-21 15:05:14 +1000

    TINY-7399: Attempt to fix flaking test (#7028)

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/EventsTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/EventsTest.ts
@@ -1,42 +1,50 @@
 import { Mouse, UiFinder, Waiter } from '@ephox/agar';
-import { describe, it } from '@ephox/bedrock-client';
+import { before, describe, it } from '@ephox/bedrock-client';
 import { McEditor, TinyContentActions, TinyDom, TinyUiActions } from '@ephox/mcagar';
 import { SugarBody } from '@ephox/sugar';
-import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
+import Theme from 'tinymce/themes/silver/Theme';
 
 describe('browser.tinymce.themes.silver.editor.EventsTest', () => {
   const settings = {
     base_url: '/project/tinymce/js/tinymce',
     plugins: 'link',
-    menubar: 'file'
+    menubar: 'file edit'
   };
 
-  const menuSelector = '[role="menu"]';
-  const menuItemSelector = '[role="menuitem"]';
+  const contextMenuItemSelector = '[role="menuitem"]:contains("Link...")';
+  const fileMenuSelector = '[role="menuitem"]:contains("New document")';
+  const editMenuSelector = '[role="menuitem"]:contains("Cut")';
+  const fileMenuButtonSelector = 'button:contains("File")';
+  const editMenuButtonSelector = 'button:contains("Edit")';
 
-  const pWaitMenuToOpen = () =>
-    Waiter.pTryUntil('Wait for a menu to open', () => UiFinder.exists(SugarBody.body(), menuSelector));
+  before(() => {
+    Theme();
+  });
 
-  const pWaitMenuToClose = () =>
-    Waiter.pTryUntil('Wait for all menus to close', () => UiFinder.notExists(SugarBody.body(), menuSelector));
+  const pAssertMenuOpen = (selector: string) =>
+    Waiter.pTryUntil('Wait for a specific menu to open', () => UiFinder.exists(SugarBody.body(), selector));
+
+  const pAssertMenuClosed = (selector: string) =>
+    Waiter.pTryUntil('Wait for a specific menu to be closed', () => UiFinder.notExists(SugarBody.body(), selector));
 
   it('TINY-7399: Clicking on the editor should close the menu', async () => {
     const editor = await McEditor.pFromSettings<Editor>(settings);
-    TinyUiActions.clickOnMenu(editor, menuItemSelector);
-    await pWaitMenuToOpen();
+    TinyUiActions.clickOnMenu(editor, fileMenuButtonSelector);
+    await pAssertMenuOpen(fileMenuSelector);
     TinyContentActions.trueClick(editor);
-    await pWaitMenuToClose();
+    await pAssertMenuClosed(fileMenuSelector);
     McEditor.remove(editor);
   });
 
   it('TINY-7399: Clicking on the editor should close the context menu', async () => {
     const editor = await McEditor.pFromSettings<Editor>(settings);
     TinyContentActions.trueClick(editor);
-    await TinyUiActions.pTriggerContextMenu(editor, 'p', menuSelector);
+    await TinyUiActions.pTriggerContextMenu(editor, 'p', contextMenuItemSelector);
+    await pAssertMenuOpen(contextMenuItemSelector);
     TinyContentActions.trueClick(editor);
-    await pWaitMenuToClose();
+    await pAssertMenuClosed(contextMenuItemSelector);
     McEditor.remove(editor);
   });
 
@@ -44,10 +52,10 @@ describe('browser.tinymce.themes.silver.editor.EventsTest', () => {
     const editor1 = await McEditor.pFromSettings<Editor>(settings);
     const editor2 = await McEditor.pFromSettings<Editor>(settings);
 
-    TinyUiActions.clickOnMenu(editor1, menuItemSelector);
-    await pWaitMenuToOpen();
+    TinyUiActions.clickOnMenu(editor1, fileMenuButtonSelector);
+    await pAssertMenuOpen(fileMenuSelector);
     TinyContentActions.trueClick(editor2);
-    await pWaitMenuToClose();
+    await pAssertMenuClosed(fileMenuSelector);
     McEditor.remove(editor1);
     McEditor.remove(editor2);
   });
@@ -56,9 +64,10 @@ describe('browser.tinymce.themes.silver.editor.EventsTest', () => {
     const editor1 = await McEditor.pFromSettings<Editor>(settings);
     const editor2 = await McEditor.pFromSettings<Editor>(settings);
 
-    await TinyUiActions.pTriggerContextMenu(editor1, 'p', menuSelector);
+    await TinyUiActions.pTriggerContextMenu(editor1, 'p', contextMenuItemSelector);
+    await pAssertMenuOpen(contextMenuItemSelector);
     TinyContentActions.trueClick(editor2);
-    await pWaitMenuToClose();
+    await pAssertMenuClosed(contextMenuItemSelector);
     McEditor.remove(editor1);
     McEditor.remove(editor2);
   });
@@ -67,13 +76,14 @@ describe('browser.tinymce.themes.silver.editor.EventsTest', () => {
     const editor1 = await McEditor.pFromSettings<Editor>(settings);
     const editor2 = await McEditor.pFromSettings<Editor>(settings);
 
-    TinyUiActions.clickOnMenu(editor1, menuItemSelector);
-    await pWaitMenuToOpen();
-    Mouse.trueClickOn(TinyDom.container(editor2), menuItemSelector);
-    await pWaitMenuToClose();
+    // Open file menu in first editor
+    TinyUiActions.clickOnMenu(editor1, fileMenuButtonSelector);
+    await pAssertMenuOpen(fileMenuSelector);
+    // Open edit menu in second editor
+    Mouse.trueClickOn(TinyDom.container(editor2), editMenuButtonSelector);
+    await pAssertMenuClosed(fileMenuSelector);
+    await pAssertMenuOpen(editMenuSelector);
 
-    const menus = UiFinder.findAllIn(SugarBody.body(), menuSelector);
-    assert.lengthOf(menus, 1, 'Should have one menu open');
     McEditor.remove(editor1);
     McEditor.remove(editor2);
   });
