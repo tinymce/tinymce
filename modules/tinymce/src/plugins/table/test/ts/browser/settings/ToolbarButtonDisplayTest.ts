4bc0e82d7e4b8b315a02eb153b5ae0c69ced4826
Hannes Frimmel Moström <hannes.mostrom@tiny.cloud>
2021-07-28 03:32:12 +0200

    TINY-7737: Test improvements.

--- modules/tinymce/src/plugins/table/test/ts/browser/settings/ToolbarButtonDisplayTest.ts
+++ modules/tinymce/src/plugins/table/test/ts/browser/settings/ToolbarButtonDisplayTest.ts
@@ -1,7 +1,5 @@
-import { UiFinder } from '@ephox/agar';
 import { context, describe, it } from '@ephox/bedrock-client';
 import { TinyHooks, TinySelections, TinyUiActions } from '@ephox/mcagar';
-import { SugarBody } from '@ephox/sugar';
 
 import Editor from 'tinymce/core/api/Editor';
 import Plugin from 'tinymce/plugins/table/Plugin';
@@ -16,35 +14,34 @@ describe('browser.tinymce.plugins.table.ToolbarButtonDisplayTest', () => {
 
   const editorContent = '<p>A</p><table><tbody><tr><td>B</td></tr></tbody></table><p>C</p>';
 
-  const displayTest = async (startPath: number[], startOffset: number, endPath: number[], endOffset: number, shouldBeEnabled: boolean) => {
+  const pDisplayTest = async (startPath: number[], startOffset: number, endPath: number[], endOffset: number, shouldBeEnabled: boolean) => {
     const editor = hook.editor();
     editor.setContent(editorContent);
     await TinyUiActions.pWaitForUi(editor, '.tox-tbtn--disabled[title="Table caption"]');
     TinySelections.setSelection(editor, startPath, startOffset, endPath, endOffset);
 
     if (shouldBeEnabled) {
-      await TinyUiActions.pWaitForUi(editor, '.tox-tbtn[title="Table caption"]');
-      UiFinder.notExists(SugarBody.body(), '.tox-tbtn--disabled[title="Table caption"]');
+      await TinyUiActions.pWaitForUi(editor, '.tox-tbtn[title="Table caption"]:not(.tox-tbtn-disabled)');
     } else {
       await TinyUiActions.pWaitForUi(editor, '.tox-tbtn--disabled[title="Table caption"]');
     }
   };
 
-  context('Ensure table buttons are enabled when appropriate', () => {
-    it('TINY-7737: When the whole selection is in the table', async () => {
-      await displayTest([ 1, 0, 0, 0 ], 0, [ 1, 0, 0, 0 ], 1, true);
-    });
+  context('Ensure table caption buttons are enabled when appropriate', () => {
+    it('TINY-7737: When the whole selection is in the table', async () =>
+      pDisplayTest([ 1, 0, 0, 0 ], 0, [ 1, 0, 0, 0 ], 1, true)
+    );
 
-    it('TINY-7737: When the selection starts outside the table, but ends in the table', async () => {
-      await displayTest([ 1, 0, 0, 0 ], 0, [ 2 ], 1, false);
-    });
+    it('TINY-7737: When the selection starts outside the table, but ends in the table', async () =>
+      pDisplayTest([ 1, 0, 0, 0 ], 0, [ 2 ], 1, false)
+    );
 
-    it('TINY-7737: When the selection starts inside the table, but ends outside the table', async () => {
-      await displayTest([ 0 ], 0, [ 1, 0, 0, 0 ], 1, false);
-    });
+    it('TINY-7737: When the selection starts inside the table, but ends outside the table', async () =>
+      pDisplayTest([ 0 ], 0, [ 1, 0, 0, 0 ], 1, false)
+    );
 
-    it('TINY-7737: When the selection starts and ends outside of the table', async () => {
-      await displayTest([ 0 ], 0, [ 2 ], 1, false);
-    });
+    it('TINY-7737: When the selection starts and ends outside of the table', async () =>
+      pDisplayTest([ 0 ], 0, [ 2 ], 1, false)
+    );
   });
 });
