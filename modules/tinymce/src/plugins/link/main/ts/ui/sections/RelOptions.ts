0299584e3f2d6d49c2ce94621fc6f8b56a1b37f8
Lee Newson <lee.newson@tiny.cloud>
2021-08-07 18:51:04 +1000

    Added types for the link plugin and removed unused leftClickedOnAHref function

--- modules/tinymce/src/plugins/link/main/ts/ui/sections/RelOptions.ts
+++ modules/tinymce/src/plugins/link/main/ts/ui/sections/RelOptions.ts
@@ -12,14 +12,14 @@ import Editor from 'tinymce/core/api/Editor';
 import * as Settings from '../../api/Settings';
 import { ListOptions } from '../../core/ListOptions';
 import * as Utils from '../../core/Utils';
-import { ListItem } from '../DialogTypes';
+import { ListItem, UserListItem } from '../DialogTypes';
 
 const getRels = (editor: Editor, initialTarget: Optional<string>): Optional<ListItem[]> => {
   const list = Settings.getRelList(editor);
   if (list.length > 0) {
     const isTargetBlank = Optionals.is(initialTarget, '_blank');
     const enforceSafe = Settings.allowUnsafeLinkTarget(editor) === false;
-    const safeRelExtractor = (item) => Utils.applyRelTargetRules(ListOptions.getValue(item), isTargetBlank);
+    const safeRelExtractor = (item: UserListItem) => Utils.applyRelTargetRules(ListOptions.getValue(item), isTargetBlank);
     const sanitizer = enforceSafe ? ListOptions.sanitizeWith(safeRelExtractor) : ListOptions.sanitize;
     return sanitizer(list);
   }
