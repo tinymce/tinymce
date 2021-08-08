0e6324c7a7c4eec81a7908e6e314060368ada7d2
Lee Newson <lee.newson@tiny.cloud>
2021-08-08 11:30:51 +1000

    Added types for the noneditable plugin

--- modules/tinymce/src/plugins/noneditable/main/ts/Plugin.ts
+++ modules/tinymce/src/plugins/noneditable/main/ts/Plugin.ts
@@ -9,7 +9,7 @@ import PluginManager from 'tinymce/core/api/PluginManager';
 
 import * as FilterContent from './core/FilterContent';
 
-export default () => {
+export default (): void => {
   PluginManager.add('noneditable', (editor) => {
     FilterContent.setup(editor);
   });
