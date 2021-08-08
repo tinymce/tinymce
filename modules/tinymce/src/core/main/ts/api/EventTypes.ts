df6832e0d078949bbda8692e1e4ed087470a4978
Lee Newson <lee.newson@tiny.cloud>
2021-08-08 17:02:56 +1000

    Added types for the toc plugin

--- modules/tinymce/src/core/main/ts/api/EventTypes.ts
+++ modules/tinymce/src/core/main/ts/api/EventTypes.ts
@@ -8,6 +8,7 @@
 import { GetContentArgs, SetContentArgs } from '../content/ContentTypes';
 import { UndoLevel } from '../undo/UndoManagerTypes';
 import Editor from './Editor';
+import { ParserArgs } from './html/DomParser';
 import { Dialog } from './ui/Ui';
 import { NativeEventMap } from './util/EventDispatcher';
 import { InstanceApi } from './WindowManager';
@@ -46,6 +47,9 @@ export interface AfterProgressStateEvent { state: boolean }
 export interface PlaceholderToggleEvent { state: boolean }
 
 export interface LoadErrorEvent { message: string }
+
+export interface PreProcessEvent extends ParserArgs { node: Element }
+export interface PostProcessEvent extends ParserArgs { content: string }
 
 export interface EditorEventMap extends Omit<NativeEventMap, 'blur' | 'focus'> {
   'activate': { relatedTarget: Editor };
@@ -104,6 +108,8 @@ export interface EditorEventMap extends Omit<NativeEventMap, 'blur' | 'focus'> {
   'tap': TouchEvent;
   'longpress': TouchEvent;
   'longpresscancel': { };
+  'PreProcess': PreProcessEvent;
+  'PostProcess': PostProcessEvent;
 }
 
 export interface EditorManagerEventMap {
