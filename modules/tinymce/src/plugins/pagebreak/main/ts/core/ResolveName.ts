eb816d0d41c4a9ae9ab906bf637ef6b0d7094851
Lee Newson <lee.newson@tiny.cloud>
2021-08-08 11:32:33 +1000

    Added return types for the pagebreak plugin

--- modules/tinymce/src/plugins/pagebreak/main/ts/core/ResolveName.ts
+++ modules/tinymce/src/plugins/pagebreak/main/ts/core/ResolveName.ts
@@ -9,7 +9,7 @@ import Editor from 'tinymce/core/api/Editor';
 
 import * as FilterContent from './FilterContent';
 
-const setup = (editor: Editor) => {
+const setup = (editor: Editor): void => {
   editor.on('ResolveName', (e) => {
     if (e.target.nodeName === 'IMG' && editor.dom.hasClass(e.target, FilterContent.pageBreakClass)) {
       e.name = 'pagebreak';
