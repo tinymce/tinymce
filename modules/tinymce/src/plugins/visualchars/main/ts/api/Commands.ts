4dba75bf03a659de82faaae9c42d5619242c8021
Lee Newson <lee.newson@tiny.cloud>
2021-08-08 17:15:21 +1000

    Added return types for the visualchars plugin

--- modules/tinymce/src/plugins/visualchars/main/ts/api/Commands.ts
+++ modules/tinymce/src/plugins/visualchars/main/ts/api/Commands.ts
@@ -5,9 +5,13 @@
  * For commercial licenses see https://www.tiny.cloud/
  */
 
+import { Cell } from '@ephox/katamari';
+
+import Editor from 'tinymce/core/api/Editor';
+
 import * as Actions from '../core/Actions';
 
-const register = (editor, toggleState) => {
+const register = (editor: Editor, toggleState: Cell<boolean>): void => {
   editor.addCommand('mceVisualChars', () => {
     Actions.toggleVisualChars(editor, toggleState);
   });
