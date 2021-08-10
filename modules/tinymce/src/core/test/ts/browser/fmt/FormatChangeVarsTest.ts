ecdbf794a4c0c7676f7800345b1ad76ab01fc6bc
Neil Ashford <neil.ashford@tiny.cloud>
2021-08-10 19:26:56 +1000

    TINY-7713: Test fixes

--- modules/tinymce/src/core/test/ts/browser/fmt/FormatChangeVarsTest.ts
+++ modules/tinymce/src/core/test/ts/browser/fmt/FormatChangeVarsTest.ts
@@ -5,11 +5,12 @@ import { TinyHooks, TinySelections } from '@ephox/mcagar';
 import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
+import Theme from 'tinymce/themes/silver/Theme';
 
 describe('browser.tinymce.core.fmt.FormatChangeVarsTest', () => {
   const hook = TinyHooks.bddSetupLight<Editor>({
     base_url: '/project/tinymce/js/tinymce'
-  }, []);
+  }, [ Theme ]);
 
   const events = {
     general: [] as boolean[],
