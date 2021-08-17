db6f00770f031cc4dc915a21d9e735261ca2e7c9
Diogo Goncalves Silva <diogo.goncalves-silva@tiny.cloud>
2021-08-17 16:00:01 +1000

    TINY-7746: alloy test

--- modules/alloy/src/test/ts/browser/position/SelectionInDocPositionTest.ts
+++ modules/alloy/src/test/ts/browser/position/SelectionInDocPositionTest.ts
@@ -1,7 +1,8 @@
 import { Chain, Cursors, NamedChain } from '@ephox/agar';
 import { UnitTest } from '@ephox/bedrock-client';
 import { Optional, Result } from '@ephox/katamari';
-import { Css, Html, SugarElement } from '@ephox/sugar';
+import { Css, Html, Scroll, SugarElement, Traverse } from '@ephox/sugar';
+import { assert } from 'chai';
 
 import * as GuiFactory from 'ephox/alloy/api/component/GuiFactory';
 import * as GuiSetup from 'ephox/alloy/api/testhelpers/GuiSetup';
@@ -9,6 +10,26 @@ import { Container } from 'ephox/alloy/api/ui/Container';
 import * as ChainUtils from 'ephox/alloy/test/ChainUtils';
 import * as PositionTestUtils from 'ephox/alloy/test/PositionTestUtils';
 import * as Sinks from 'ephox/alloy/test/Sinks';
+
+const toDomRange = (cursorRange: Cursors.CursorRange): Range => {
+  const doc = Traverse.owner(cursorRange.start);
+  const range = doc.dom.createRange();
+  range.setStart(cursorRange.start.dom, cursorRange.soffset);
+  range.setEnd(cursorRange.finish.dom, cursorRange.foffset);
+  return range;
+};
+
+const cAssertBelowSelection = NamedChain.bundle((data: any) => {
+  const root = data.inline.element;
+  const popup = data.popup;
+  const path = data.path;
+  const range = toDomRange(Cursors.calculate(root, path));
+
+  const selectionBox = range.getBoundingClientRect();
+  const popupBox = popup.element.dom.getBoundingClientRect();
+  assert.approximately(popupBox.y, selectionBox.bottom, 5);
+  return Result.value(data);
+});
 
 UnitTest.asynctest('SelectionInDocPositionTest', (success, failure) => {
 
@@ -133,6 +154,47 @@ UnitTest.asynctest('SelectionInDocPositionTest', (success, failure) => {
           PositionTestUtils.cTestSink(
             'Fixed, Selected: 13rd paragraph, large scroll, no editor scroll',
             'fixed'
+          ),
+
+          ChainUtils.cLogging(
+            'Setting selection to 5th-7th paragraph and scrolling there',
+            [
+              NamedChain.writeValue('path', Cursors.path({
+                startPath: [ 5 ],
+                soffset: 0,
+                finishPath: [ 8 ],
+                foffset: 0
+              })),
+              NamedChain.bundle((data: any) => {
+                const root = data.inline.element;
+                // Scroll so the 7th paragraph is at the top
+                const range = Cursors.calculate(root, Cursors.path({
+                  startPath: [ 7 ],
+                  soffset: 0,
+                  finishPath: [ 8 ],
+                  foffset: 0
+                }));
+                Scroll.intoView(range.start, true);
+                return Result.value(data);
+              }),
+
+              // Update the anchor
+              NamedChain.write('anchor', cSetupAnchor)
+            ]
+          ),
+
+          PositionTestUtils.cTestSink(
+            'Relative, Selected: 5th-7th paragraph, no scroll, some editor scroll',
+            'relative'
+          ),
+          PositionTestUtils.cTestSink(
+            'Fixed, Selected: 5th-7th paragraph, no scroll, some editor scroll',
+            'fixed'
+          ),
+
+          ChainUtils.cLogging(
+            'Relative, Selected: 5th-7th paragraph, assert below selection',
+            [ cAssertBelowSelection ]
           )
         ])
       ])
