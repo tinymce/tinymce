8da12b6a1cf3f81776e35bebd671a9b2ca0e37e6
Diogo Goncalves Silva <diogo.goncalves-silva@tiny.cloud>
2021-08-18 13:48:50 +1000

    TINY-7746: move functions + test relative and fixed

--- modules/alloy/src/test/ts/browser/position/SelectionInDocPositionTest.ts
+++ modules/alloy/src/test/ts/browser/position/SelectionInDocPositionTest.ts
@@ -11,26 +11,6 @@ import * as ChainUtils from 'ephox/alloy/test/ChainUtils';
 import * as PositionTestUtils from 'ephox/alloy/test/PositionTestUtils';
 import * as Sinks from 'ephox/alloy/test/Sinks';
 
-const toDomRange = (cursorRange: Cursors.CursorRange): Range => {
-  const doc = Traverse.owner(cursorRange.start);
-  const range = doc.dom.createRange();
-  range.setStart(cursorRange.start.dom, cursorRange.soffset);
-  range.setEnd(cursorRange.finish.dom, cursorRange.foffset);
-  return range;
-};
-
-const cAssertBelowSelection = NamedChain.bundle((data: any) => {
-  const root = data.inline.element;
-  const popup = data.popup;
-  const path = data.path;
-  const range = toDomRange(Cursors.calculate(root, path));
-
-  const selectionBox = range.getBoundingClientRect();
-  const popupBox = popup.element.dom.getBoundingClientRect();
-  assert.approximately(popupBox.y, selectionBox.bottom, 5);
-  return Result.value(data);
-});
-
 UnitTest.asynctest('SelectionInDocPositionTest', (success, failure) => {
 
   GuiSetup.setup((_store, _doc, _body) => {
@@ -77,6 +57,27 @@ UnitTest.asynctest('SelectionInDocPositionTest', (success, failure) => {
         );
       }
     }));
+
+    const toDomRange = (cursorRange: Cursors.CursorRange): Range => {
+      const doc = Traverse.owner(cursorRange.start);
+      const range = doc.dom.createRange();
+      range.setStart(cursorRange.start.dom, cursorRange.soffset);
+      range.setEnd(cursorRange.finish.dom, cursorRange.foffset);
+      return range;
+    };
+
+    const cAssertBelowSelection = NamedChain.bundle((data: any) => {
+      const root = data.inline.element;
+      const popup = data.popup;
+      const path = data.path;
+      const range = toDomRange(Cursors.calculate(root, path));
+
+      const selectionBox = range.getBoundingClientRect();
+      const popupBox = popup.element.dom.getBoundingClientRect();
+      assert.isAtLeast(popupBox.y, selectionBox.bottom);
+      assert.approximately(popupBox.y, selectionBox.bottom, 5);
+      return Result.value(data);
+    });
 
     return [
       Chain.asStep({}, [
@@ -187,11 +188,14 @@ UnitTest.asynctest('SelectionInDocPositionTest', (success, failure) => {
             'Relative, Selected: 5th-7th paragraph, no scroll, some editor scroll',
             'relative'
           ),
+          ChainUtils.cLogging(
+            'Relative, Selected: 5th-7th paragraph, assert below selection',
+            [ cAssertBelowSelection ]
+          ),
           PositionTestUtils.cTestSink(
             'Fixed, Selected: 5th-7th paragraph, no scroll, some editor scroll',
             'fixed'
           ),
-
           ChainUtils.cLogging(
             'Relative, Selected: 5th-7th paragraph, assert below selection',
             [ cAssertBelowSelection ]
