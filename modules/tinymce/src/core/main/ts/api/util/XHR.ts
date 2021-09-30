f8f252a6fab5c4016f6d7d75f1015b3ed86978ef
James Toohey <james.toohey@tiny.cloud>
2021-09-30 11:28:46 +1000

    TINY-8023: Fix bad formatting (#7239)

--- modules/tinymce/src/core/main/ts/api/util/XHR.ts
+++ modules/tinymce/src/core/main/ts/api/util/XHR.ts
@@ -35,13 +35,12 @@ interface XHR extends Observable<XHREventMap> {
 
 /**
  * This API allows you to send XMLHTTPRequests cross browser. Extends the Observable class.
- * <br>
- * <em><code>XHR</code> has been deprecated and marked for removal in TinyMCE 6.0.</em>
  *
  * @deprecated
  * @class tinymce.util.XHR
  * @mixes tinymce.util.Observable
  * @static
+ * @summary XHR has been deprecated and marked for removal in TinyMCE 6.0.
  * @example
  * // Sends a low level Ajax request
  * tinymce.util.XHR.send({
