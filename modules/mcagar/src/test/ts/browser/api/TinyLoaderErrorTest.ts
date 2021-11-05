a883d4b729cca560d88f366b648fb65a3133591a
JamesToohey <james.toohey@tiny.cloud>
2021-11-05 15:08:16 +1000

    Update TinyLoaderErrorTest.ts

--- modules/mcagar/src/test/ts/browser/api/TinyLoaderErrorTest.ts
+++ modules/mcagar/src/test/ts/browser/api/TinyLoaderErrorTest.ts
@@ -1,9 +1,24 @@
-import { UnitTest } from '@ephox/bedrock-client';
+import { Waiter } from '@ephox/agar';
+import { describe, it } from '@ephox/bedrock-client';
+import { assert } from 'chai';
 
 import * as TinyLoader from 'ephox/mcagar/api/pipeline/TinyLoader';
 
-UnitTest.asynctest('TinyLoader should fail (instead of timeout) when exception is thrown in callback function', (success, failure) => {
-  TinyLoader.setup(() => {
-    throw new Error('boo!');
-  }, { base_url: '/project/tinymce/js/tinymce' }, failure, success);
+describe('TinyLoaderErrorTest', () => {
+  it('should fail (instead of timeout) when exception is thrown in callback function', async () => {
+    let failed = false;
+
+    TinyLoader.setup(
+      () => {
+        throw new Error('boo!');
+      },
+      { base_url: '/project/tinymce/js/tinymce' },
+      () => assert.fail(),  // If the success callback is called, fail this test
+      () => failed = true
+    );
+
+    await Waiter.pTryUntil('Wait for the TinyLoader failure callback', () => {
+      assert.isTrue(failed);
+    });
+  });
 });
