66862659ee71bfcea9d05b177efd865e24f5d8b4
Luke Trouton <luke.trouton@tiny.cloud>
2021-11-17 16:39:33 +1000

    TINY-7493: Change to use ?? operator for simplicity

--- modules/tinymce/src/core/main/ts/api/html/StyleUtils.ts
+++ modules/tinymce/src/core/main/ts/api/html/StyleUtils.ts
@@ -5,7 +5,7 @@
  * For commercial licenses see https://www.tiny.cloud/
  */
 
-import { Arr, Obj, Strings, Type } from '@ephox/katamari';
+import { Arr, Obj, Strings } from '@ephox/katamari';
 
 import DOMUtils from '../dom/DOMUtils';
 
@@ -49,11 +49,9 @@ const hasStyleConflict = (dom: DOMUtils, node: Node, parentNode: Node): boolean 
   const parentNodeStyleProps = getStyleProps(dom, parentNode);
 
   const valueMismatch = (prop: string) => {
-    const nodeValue = dom.getStyle(node, prop);
-    const parentValue = dom.getStyle(parentNode, prop);
-    const isValidNodeValue = Type.isNonNullable(nodeValue) && Strings.isNotEmpty(nodeValue);
-    const isValidParentValue = Type.isNonNullable(parentValue) && Strings.isNotEmpty(parentValue);
-    return isValidNodeValue && isValidParentValue && nodeValue !== parentValue;
+    const nodeValue = dom.getStyle(node, prop) ?? '';
+    const parentValue = dom.getStyle(parentNode, prop) ?? '';
+    return Strings.isNotEmpty(nodeValue) && Strings.isNotEmpty(parentValue) && nodeValue !== parentValue;
   };
 
   return Arr.exists(nodeStyleProps, (nodeStyleProp) => {
