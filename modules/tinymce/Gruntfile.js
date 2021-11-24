b6c4d10d9aea012f8e30bebff934abde6528e4a9
Lee Newson <lee.newson@tiny.cloud>
2021-11-24 14:25:11 +1000

    TINY-8253: Update to output ES2018 syntax (#7414)

--- modules/tinymce/Gruntfile.js
+++ modules/tinymce/Gruntfile.js
@@ -176,17 +176,16 @@ module.exports = function (grunt) {
       }
     },
 
-    uglify: Object.assign(
+    terser: Object.assign(
       {
         options: {
+          ecma: 2018,
           output: {
             comments: 'all',
             ascii_only: true
           },
           compress: {
-            passes: 2,
-            // TINY-7720: Disable merge_vars as it has a bug that causes errors on IE 11
-            merge_vars: false
+            passes: 2
           }
         },
         core: {
@@ -201,7 +200,7 @@ module.exports = function (grunt) {
           options: {
             mangle: false,
             compress: false,
-            beautify: {
+            output: {
               indent_level: 2
             }
           },
@@ -892,7 +891,7 @@ module.exports = function (grunt) {
   });
   grunt.loadTasks('tools/tasks');
 
-  grunt.registerTask('emoji', ['emojis', 'uglify:emoticons-raw']);
+  grunt.registerTask('emoji', ['emojis', 'terser:emoticons-raw']);
 
   grunt.registerTask('prodBuild', [
     'shell:tsc',
@@ -902,7 +901,7 @@ module.exports = function (grunt) {
     'rollup',
     'concat',
     'copy',
-    'uglify'
+    'terser'
   ]);
 
   grunt.registerTask('prod', [
