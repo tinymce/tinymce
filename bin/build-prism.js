01b3c0269e0f50e529cd82abdd9a7b36b04dc529
Lee Newson <lee.newson@tiny.cloud>
2021-12-09 16:43:57 +1000

    TINY-8293: Replace the prismjs wrapper with prism directly couple with some patches (#7452)

---
+++ bin/build-prism.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+// The list of core and plugin files from prism to use in the prism.js bundle
+const files = [
+  'components/prism-core.js',
+
+  'components/prism-clike.js',
+  'components/prism-markup-templating.js',
+
+  'components/prism-c.js',
+  'components/prism-cpp.js',
+  'components/prism-csharp.js',
+  'components/prism-css.js',
+  'components/prism-java.js',
+  'components/prism-javascript.js',
+  'components/prism-markup.js',
+  'components/prism-php.js',
+  'components/prism-python.js',
+  'components/prism-ruby.js'
+];
+
+// Add the header to store any original Prism reference and to wrap the prism init in an IIFE
+let content = `const prismjs = function(global, module, exports) {
+// preserve the global if it has already been loaded
+const oldprism = window.Prism;
+window.Prism = { manual: true };
+`;
+
+// Load in the core and any plugins/languages that should be used
+files.forEach((file) => {
+  content += fs.readFileSync(path.resolve(__dirname, '../node_modules/prismjs', file)).toString() + '\n';
+});
+
+// Restore the original reference and expose prism as an ES6 module
+content += `// restore the original Prism reference
+window.Prism = oldprism;
+return Prism;
+}(undefined, undefined, undefined);
+export default prismjs;`
+
+// Write the new prism.js file
+fs.writeFileSync(path.resolve(__dirname, '../node_modules/prismjs/prism.js'), content);
