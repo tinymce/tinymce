4db42eb5ef7af5e15702c5aac710266b3c51900b
Andrew Herron <thespyder@programmer.net>
2021-12-10 14:10:43 +1000

    TINY-8333: ImagePanel now loads the image correctly and supports changing the zoom level via an optional data property

--- modules/bridge/src/main/ts/ephox/bridge/components/dialog/ImagePanel.ts
+++ modules/bridge/src/main/ts/ephox/bridge/components/dialog/ImagePanel.ts
@@ -1,19 +1,26 @@
 import { FieldSchema, StructureSchema } from '@ephox/boulder';
+import { Optional } from '@ephox/katamari';
 
 import { FormComponent, formComponentFields, FormComponentSpec } from './FormComponent';
 
 export interface ImagePanelSpec extends FormComponentSpec {
   type: 'imagepanel';
+  width?: string;
+  height?: string;
 }
 
 export interface ImagePanel extends FormComponent {
   type: 'imagepanel';
+  width: Optional<string>;
+  height: Optional<string>;
 }
 
-export const imagePanelSchema = StructureSchema.objOf(formComponentFields);
+export const imagePanelSchema = StructureSchema.objOf(formComponentFields.concat([
+  FieldSchema.optionString('width'),
+  FieldSchema.optionString('height'),
+]));
 
 export const imagePanelDataProcessor = StructureSchema.objOf([
   FieldSchema.requiredString('url'),
-  FieldSchema.requiredNumber('width'),
-  FieldSchema.requiredNumber('height'),
+  FieldSchema.optionNumber('zoom'),
 ]);
--- modules/tinymce/src/themes/silver/main/ts/ui/dialog/ImagePanel.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/dialog/ImagePanel.ts
@@ -5,10 +5,10 @@
  * For commercial licenses see https://www.tiny.cloud/
  */
 
-import { AlloyComponent, Behaviour, Representing, SimpleSpec } from '@ephox/alloy';
+import { AlloyComponent, Behaviour, Memento, Representing, SimpleSpec } from '@ephox/alloy';
 import { Dialog } from '@ephox/bridge';
-import { Cell } from '@ephox/katamari';
-import { Attribute, Css, SugarElement, Traverse } from '@ephox/sugar';
+import { Optional, Singleton } from '@ephox/katamari';
+import { Attribute, Css, Height, SugarElement, Width } from '@ephox/sugar';
 
 import { ComposingConfigs } from '../alien/ComposingConfigs';
 
@@ -16,59 +16,129 @@ type ImagePanelSpec = Omit<Dialog.ImagePanel, 'type'>;
 
 interface ImagePanelData {
   url: string;
-  width: number;
-  height: number;
+  zoom: Optional<number>;
 }
 
-export const renderImagePanel = (spec: ImagePanelSpec): SimpleSpec => {
-  const cachedValue = Cell(null);
-
-  // TODO: how to set the image URL?
-  const setValue = (frameComponent: AlloyComponent, data: ImagePanelData) => {
-    const [ background, foreground ] = Traverse.children(frameComponent.element) as SugarElement[];
-    Attribute.set(foreground, 'src', data.url);
-    const position = {
-      top: '50px',
-      left: '50px',
-      width: data.width + 'px',
-      height: data.height + 'px'
-    };
-    Css.setAll(foreground, position);
-    Css.setAll(background, position);
-    cachedValue.set(data);
+// Does this belong somewhere else?
+const loadImage = (image: SugarElement<HTMLImageElement>): Promise<SugarElement<HTMLImageElement>> => new Promise((resolve) => {
+  const loaded = () => {
+    image.dom.removeEventListener('load', loaded);
+    resolve(image);
   };
+
+  if (image.dom.complete) {
+    resolve(image);
+  } else {
+    image.dom.addEventListener('load', loaded);
+  }
+});
+
+const calculateImagePosition = (panelWidth: number, panelHeight: number, imageWidth: number, imageHeight: number, zoom: number) => {
+  const width = imageWidth * zoom;
+  const height = imageHeight * zoom;
+  const left = Math.max(0, panelWidth / 2 - width / 2);
+  const top = Math.max(0, panelHeight / 2 - height / 2);
+
+  return {
+    left: left.toString() + 'px',
+    top: top.toString() + 'px',
+    width: width.toString() + 'px',
+    height: height.toString() + 'px',
+  };
+};
+
+const zoomToFit = (panel: SugarElement, img: SugarElement) => {
+  const panelW = Width.get(panel);
+  const panelH = Height.get(panel);
+  const width = img.dom.naturalWidth;
+  const height = img.dom.naturalHeight;
+  return Math.min((panelW) / width, (panelH) / height, 1);
+};
+
+export const renderImagePanel = (spec: ImagePanelSpec): SimpleSpec => {
+  const cachedData = Singleton.value<ImagePanelData>();
+
+  const memImage = Memento.record({
+    dom: {
+      tag: 'img',
+      classes: [ 'tox-imagepanel__image' ],
+    },
+  });
+
+  const memContainer = Memento.record({
+    dom: {
+      tag: 'div',
+      classes: [ 'tox-imagepanel__container' ],
+      attributes: {
+        role: 'presentation'
+      },
+    },
+    components: [
+      memImage.asSpec()
+    ]
+  });
+
+  const setValue = (frameComponent: AlloyComponent, data: ImagePanelData) => {
+    const repaintImg = (img) => {
+      const zoom = data.zoom.getOrThunk(() => {
+        const z = zoomToFit(frameComponent.element, img);
+        data.zoom = Optional.some(z);
+        return z;
+      }
+      );
+      const position = calculateImagePosition(
+        Width.get(frameComponent.element),
+        Height.get(frameComponent.element),
+        img.dom.naturalWidth,
+        img.dom.naturalHeight,
+        zoom
+      );
+      memContainer.getOpt(frameComponent).each((container) => {
+        Css.setAll(container.element, position);
+      });
+      cachedData.set(data);
+    };
+
+    memImage.getOpt(frameComponent).each((imageComponent) => {
+      const img = imageComponent.element;
+      if (data.url !== Attribute.get(img, 'src')) {
+        Attribute.set(img, 'src', data.url);
+      }
+
+      loadImage(img).then(repaintImg);
+    });
+  };
+
+  const styles: Record<string, string> = {};
+  spec.width.each((w) => styles.width = w);
+  spec.height.each((h) => styles.height = h);
+
   return {
     dom: {
       tag: 'div',
       classes: [ 'tox-imagepanel' ],
+      styles,
       attributes: {
         role: 'presentation'
       }
     },
     components: [
-      {
-        dom: {
-          tag: 'div',
-          classes: [ 'tox-imagepanel__bg' ],
-          attributes: {
-            role: 'presentation'
-          }
-        },
-        name: spec.name + '-background'
-      },
-      {
-        dom: {
-          tag: 'img',
-          classes: [ 'tox-imagepanel__image' ],
-        },
-      }
+      memContainer.asSpec(),
     ],
     behaviours: Behaviour.derive([
       ComposingConfigs.self(),
       Representing.config({
         store: {
           mode: 'manual',
-          getValue: () => cachedValue.get(),
+          getValue: () => {
+            const value: Record<string, string | number> = {};
+            // if data hasn't been set yet, it doesn't seem to matter what we return
+            cachedData.on((data) => {
+              value.url = data.url;
+              data.zoom.each((z) => value.zoom = z);
+            });
+            return value;
+          },
           setValue
         }
       }),
