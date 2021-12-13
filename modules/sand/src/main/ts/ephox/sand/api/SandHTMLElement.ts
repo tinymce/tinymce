6f0537eb3541a9ead6d938805e9ae0d807fc20d2
Andrew Herron <thespyder@programmer.net>
2021-12-13 11:20:40 +1000

    Avoid toString in Sand HTMLElement

--- modules/sand/src/main/ts/ephox/sand/api/SandHTMLElement.ts
+++ modules/sand/src/main/ts/ephox/sand/api/SandHTMLElement.ts
@@ -1,27 +1,7 @@
-import { Resolve, Type } from '@ephox/katamari';
-
-import * as Global from '../util/Global';
-
-const getPrototypeOf = Object.getPrototypeOf;
-
-/*
- * IE9 and above
- *
- * MDN no use on this one, but here's the link anyway:
- * https://developer.mozilla.org/en/docs/Web/API/HTMLElement
- */
-const sandHTMLElement = (scope: Window) => {
-  return Global.getOrDie('HTMLElement', scope) as typeof HTMLElement;
-};
+import { Type } from '@ephox/katamari';
 
 const isPrototypeOf = (x: any): x is HTMLElement => {
-  // use Resolve to get the window object for x and just return undefined if it can't find it.
-  // undefined scope later triggers using the global window.
-  const scope: Window | undefined = Resolve.resolve('ownerDocument.defaultView', x);
-
-  // TINY-7374: We can't rely on looking at the owner window HTMLElement as the element may have
-  // been constructed in a different window and then appended to the current window document.
-  return Type.isObject(x) && (sandHTMLElement(scope).prototype.isPrototypeOf(x) || /^HTML\w*Element$/.test(getPrototypeOf(x).constructor.name));
+  return Type.isObject(x) && (/^HTML\w*Element$/.test(x.constructor.name));
 };
 
 export {
