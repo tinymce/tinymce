e2bab635f65213f4defc2a43f995df80ced8b70f
Lee Newson <lee.newson@tiny.cloud>
2021-12-15 06:48:42 +1000

    TINY-8334: fixed an issue with patching footer buttons

--- modules/tinymce/src/themes/silver/main/ts/ui/window/SilverDialogFooter.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/window/SilverDialogFooter.ts
@@ -5,11 +5,9 @@
  * For commercial licenses see https://www.tiny.cloud/
  */
 
-import {
-  AlloyComponent, AlloySpec, Behaviour, Container, DomFactory, Memento, ModalDialog, Reflecting, Replacing, SketchSpec
-} from '@ephox/alloy';
+import { AlloyComponent, AlloySpec, Behaviour, Container, DomFactory, Memento, ModalDialog, Reflecting, Replacing, SketchSpec } from '@ephox/alloy';
 import { Dialog } from '@ephox/bridge';
-import { Arr, Optional, Optionals } from '@ephox/katamari';
+import { Arr, Optional } from '@ephox/katamari';
 
 import { UiFactoryBackstage } from '../../backstage/Backstage';
 import { renderFooterButton } from '../general/Button';
@@ -30,12 +28,9 @@ export interface FooterState {
   readonly buttons: Dialog.DialogFooterButton[];
 }
 
-const lookupFromSpec = (compInSystem: AlloyComponent, spec: Dialog.DialogFooterButton): Optional<AlloyComponent> =>
-  compInSystem.getSystem().getByUid(spec.uid).toOptional();
-
 const lookupByName = (compInSystem: AlloyComponent, footerButtons: Dialog.DialogFooterButton[], buttonName: string): Optional<AlloyComponent> =>
   Arr.find(footerButtons, (spec) => spec.name === buttonName)
-    .bind((spec) => lookupFromSpec(compInSystem, spec));
+    .bind((spec) => compInSystem.getSystem().getByUid(spec.uid).toOptional());
 
 const makeGroup = (edge: string): SketchSpec => Container.sketch({
   dom: {
@@ -49,16 +44,25 @@ const makeGroup = (edge: string): SketchSpec => Container.sketch({
 });
 
 const renderFooter = (initSpec: WindowFooterSpec, dialogId: string, backstage: UiFactoryBackstage) => {
+  const patchGroup = (group: AlloyComponent, buttons: Dialog.DialogFooterButton[], prevButtons: Dialog.DialogFooterButton[]) => {
+    const diffs = Diff.diffItems(buttons, prevButtons);
+    return Arr.bind(diffs, (diff, index) => {
+      return Diff.applyDiff(group, diff, index, (spec) => renderFooterButton(spec, spec.type, backstage));
+    });
+  };
+
   const updateState = (comp: AlloyComponent, data: WindowFooterSpec, state: Optional<FooterState>) => {
-    const buttons = Optionals.lift2(memStartContainer.getOpt(comp), memEndContainer.getOpt(comp), (start, end) => {
-      // Determine what's changed and apply the changes
-      const prevButtons: Dialog.DialogFooterButton[] = state.map((s) => s.buttons).getOr([]);
-      const diffs = Diff.diffItems(data.buttons, prevButtons);
-      return Arr.bind(diffs, (diff, index) => {
-        const group = diff.item.align === 'start' ? start : end;
-        return Diff.applyDiff(group, diff, index, (spec) => renderFooterButton(spec, spec.type, backstage));
-      });
-    }).getOr(data.buttons);
+    const prevButtons: Dialog.DialogFooterButton[] = state.map((s) => s.buttons).getOr([]);
+
+    const isStart = (button: Dialog.DialogFooterButton) => button.align === 'start';
+    const groupButtons = Arr.partition(data.buttons, isStart);
+    const prevGroupButtons = Arr.partition(prevButtons, isStart);
+
+    // Determine what's changed and apply the changes
+    const buttons = [
+      ...patchGroup(memStartContainer.get(comp), groupButtons.pass, prevGroupButtons.pass),
+      ...patchGroup(memEndContainer.get(comp), groupButtons.fail, prevGroupButtons.fail)
+    ];
 
     return Optional.some<FooterState>({
       lookupByName: (buttonName) => lookupByName(comp, buttons, buttonName),
