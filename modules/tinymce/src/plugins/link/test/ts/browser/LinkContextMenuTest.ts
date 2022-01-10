1e7c5570660cbce45d3e0a5b5c3899343dd78e24
Arvin Han <arvinhjt0718@gmail.com>
2022-01-10 14:39:24 +1100

    TINY-2293: Link context menu now test focus on menu element WIP

--- modules/tinymce/src/plugins/link/test/ts/browser/LinkContextMenuTest.ts
+++ modules/tinymce/src/plugins/link/test/ts/browser/LinkContextMenuTest.ts
@@ -1,22 +1,32 @@
 import { describe, it } from '@ephox/bedrock-client';
 import { TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
+import {FocusTools, Keys } from '@ephox/agar';
+import { SugarDocument } from '@ephox/sugar';
+
 
 import Editor from 'tinymce/core/api/Editor';
 import Plugin from 'tinymce/plugins/link/Plugin';
 
-describe('browser.tinymce.core.content.ContextMenuTest', () => {
+describe('browser.tinymce.plugins.link.LinkContextMenuTest', () => {
   const hook = TinyHooks.bddSetup<Editor>({
     plugins: 'link',
     toolbar: 'link',
     base_url: '/project/tinymce/js/tinymce',
   }, [ Plugin ]);
 
+  const pAssertFocusOnItem = (label: string, selector: string) =>
+    FocusTools.pTryOnSelector(`Focus should be on: ${label}`, SugarDocument.getDocument(), selector);
+  
+  const pressDownArrowKey = (editor: Editor) => TinyUiActions.keydown(editor, Keys.down());
+
   it('TBA: Context menu show up on link', async () => {
     const editor = hook.editor();
     editor.setContent('<p><a href="http://tiny.cloud">tiny</a></p>');
     await TinyUiActions.pTriggerContextMenu(editor, 'a[href="http://tiny.cloud"]', '.tox-silver-sink [role="menuitem"]');
-    await TinyUiActions.pWaitForUi(editor, '[title="Link..."]');
-    await TinyUiActions.pWaitForUi(editor, '[title="Remove link"]');
-    await TinyUiActions.pWaitForUi(editor, '[title="Open link"]');
+    await pAssertFocusOnItem('Link...', '.tox-collection__item:contains("Link...")');
+    pressDownArrowKey(editor);
+    await pAssertFocusOnItem('Remove link', '.tox-collection__item:contains("Remove link")');
+    pressDownArrowKey(editor);
+    await pAssertFocusOnItem('Open link', '.tox-collection__item:contains("Open link")');
   });
 });
