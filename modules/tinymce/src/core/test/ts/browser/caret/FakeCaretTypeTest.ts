2ede61349643975648a9b8a94285245224e13f3f
Spocke <spocke@moxiecode.com>
2022-01-14 07:33:25 +0100

    TINY-8169: added new approach for adding noneditable fake carets

---
+++ modules/tinymce/src/core/test/ts/browser/caret/FakeCaretTypeTest.ts
@@ -0,0 +1,30 @@
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyHooks, TinySelections, TinyAssertions, TinyContentActions } from '@ephox/wrap-mcagar';
+
+import Editor from 'tinymce/core/api/Editor';
+import TablePlugin from 'tinymce/plugins/table/Plugin';
+
+describe('browser.tinymce.core.FakeCaretTypeTest', () => {
+  const hook = TinyHooks.bddSetupLight<Editor>({
+    base_url: '/project/tinymce/js/tinymce',
+    indent: false
+  }, [ TablePlugin ]);
+
+  it('typing after a noneditable block should remove the fake caret', () => {
+    const editor = hook.editor();
+    editor.setContent('<div contenteditable="false">a</div>');
+    TinySelections.setCursor(editor, [], 1);
+    TinyAssertions.assertContentPresence(editor, {
+      'p[data-mce-caret="after"]': 1,
+      'div.mce-visual-caret': 1
+    });
+
+    TinyContentActions.type(editor, 'b');
+
+    TinyAssertions.assertContent(editor, '<div contenteditable="false">a</div><p>b</p>');
+    TinyAssertions.assertContentPresence(editor, {
+      'p[data-mce-caret="after"]': 0,
+      'div.mce-visual-caret': 0
+    });
+  });
+});
