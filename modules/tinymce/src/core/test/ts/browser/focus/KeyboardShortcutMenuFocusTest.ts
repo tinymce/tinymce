123cdd64df3364e63377ee1adc4c7929360fa1c3
Arvin Han <arvinhjt0718@gmail.com>
2022-01-14 19:20:45 +1100

    TINY-2884: Add keyboard menu focus test:

---
+++ modules/tinymce/src/core/test/ts/browser/focus/KeyboardShortcutMenuFocusTest.ts
@@ -0,0 +1,29 @@
+import { FocusTools, Keys } from '@ephox/agar';
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyUiActions, TinyContentActions } from '@ephox/mcagar';
+import { SugarDocument } from '@ephox/sugar';
+import { TinyHooks } from '@ephox/wrap-mcagar';
+
+import Editor from 'tinymce/core/api/Editor';
+
+describe('browser.tinymce.core.undo.UndoKeyboardShortcutTest', () => {
+
+  const hook = TinyHooks.bddSetup<Editor>({
+    base_url: '/project/tinymce/js/tinymce'
+  }, [], true);
+
+  const pressDownArrowKey = (editor: Editor) => TinyUiActions.keydown(editor, Keys.down());
+
+  const pAssertFocusOnItem = (label: string, selector: string) =>
+    FocusTools.pTryOnSelector(`Focus should be on: ${label}`, SugarDocument.getDocument(), selector);
+
+  it('shoud undo and redo action', async () => {
+    const editor = hook.editor();
+    TinyContentActions.keystroke(editor, 120, { alt: true });
+    await pAssertFocusOnItem('File', '.tox-mbtn--select:contains("File")');
+    TinyUiActions.keystroke(editor, 32);
+    await pAssertFocusOnItem('New document', '.tox-menu-nav__js:contains("New document")');
+    pressDownArrowKey(editor);
+    await pAssertFocusOnItem('Print...', '.tox-menu-nav__js:contains("Print...")');
+  });
+});
