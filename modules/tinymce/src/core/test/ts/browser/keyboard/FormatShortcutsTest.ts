6671e558330a7cbf56d88042694fac4ec8133f09
Arvin Han <arvinhjt0718@gmail.com>
2022-01-20 16:26:21 +1100

    delete useless comment

--- modules/tinymce/src/core/test/ts/browser/keyboard/FormatShortcutsTest.ts
+++ modules/tinymce/src/core/test/ts/browser/keyboard/FormatShortcutsTest.ts
@@ -17,84 +17,84 @@ describe('browser.tinymce.core.keyboard.FormatShortcutsTest', () => {
   it('TINY-2884: should set the selection to be bold', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 66, { meta: true });
     TinyAssertions.assertContent(editor, '<p><strong>abc</strong></p>');
   });
   it('TINY-2884: should set the selection to be italic', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 73, { meta: true });
     TinyAssertions.assertContent(editor, '<p><em>abc</em></p>');
   });
   it('TINY-2884: should set the selection to be underline', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 85, { meta: true });
     TinyAssertions.assertContent(editor, '<p><span style="text-decoration: underline;">abc</span></p>');
   });
   it('TINY-2884: should set the selection to be H1', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 49, { ctrl: true, alt: true });
     TinyAssertions.assertContent(editor, '<h1>abc</h1>');
   });
   it('TINY-2884: should set the selection to be H2', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 50, { ctrl: true, alt: true });
     TinyAssertions.assertContent(editor, '<h2>abc</h2>');
   });
   it('TINY-2884: should set the selection to be H3', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 51, { ctrl: true, alt: true });
     TinyAssertions.assertContent(editor, '<h3>abc</h3>');
   });
   it('TINY-2884: should set the selection to be H4', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 52, { ctrl: true, alt: true });
     TinyAssertions.assertContent(editor, '<h4>abc</h4>');
   });
   it('TINY-2884: should set the selection to be H5', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 53, { ctrl: true, alt: true });
     TinyAssertions.assertContent(editor, '<h5>abc</h5>');
   });
   it('TINY-2884: should set the selection to be H6', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 54, { ctrl: true, alt: true });
     TinyAssertions.assertContent(editor, '<h6>abc</h6>');
   });
   it('TINY-2884: should set the selection to be Paragraph', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 55, { ctrl: true, alt: true });
     TinyAssertions.assertContent(editor, '<p>abc</p>');
   });
   it('TINY-2884: should set the selection to be Div', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 56, { ctrl: true, alt: true });
     TinyAssertions.assertContent(editor, '<div>abc</div>');
   });
   it('TINY-2884: should set the selection to be address', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3); // Selects `tent` in the paragraph
+    TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 3);
     TinyContentActions.keystroke(editor, 57, { ctrl: true, alt: true });
     TinyAssertions.assertContent(editor, '<address>abc</address>');
   });
