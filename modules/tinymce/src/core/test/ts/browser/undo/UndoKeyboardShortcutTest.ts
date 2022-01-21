119c593504831ea9c5e78ecc58d22c224c7f5199
Arvin Han <arvinhjt0718@gmail.com>
2022-01-21 14:58:28 +1100

    TINY-2884: Add system detec for Undo keyboard test

--- modules/tinymce/src/core/test/ts/browser/undo/UndoKeyboardShortcutTest.ts
+++ modules/tinymce/src/core/test/ts/browser/undo/UndoKeyboardShortcutTest.ts
@@ -1,5 +1,6 @@
 import { describe, it } from '@ephox/bedrock-client';
 import { TinyAssertions, TinyContentActions } from '@ephox/mcagar';
+import { PlatformDetection } from '@ephox/sand';
 import { TinyHooks } from '@ephox/wrap-mcagar';
 
 import Editor from 'tinymce/core/api/Editor';
@@ -8,15 +9,38 @@ describe('browser.tinymce.core.undo.UndoKeyboardShortcutTest', () => {
   const hook = TinyHooks.bddSetupLight<Editor>({
     base_url: '/project/tinymce/js/tinymce'
   }, [], true);
-
+  const SelectAll = (editor: Editor) => {
+    const platform = PlatformDetection.detect().os.current;
+    if (platform === 'macOS') {
+      TinyContentActions.keystroke(editor, 'A'.charCodeAt(0), { meta: true });
+    } else {
+      TinyContentActions.keystroke(editor, 'A'.charCodeAt(0), { ctrl: true });
+    }
+  };
+  const Undo = (editor: Editor) => {
+    const platform = PlatformDetection.detect().os.current;
+    if (platform === 'macOS') {
+      TinyContentActions.keystroke(editor, 'Z'.charCodeAt(0), { meta: true });
+    } else {
+      TinyContentActions.keystroke(editor, 'Z'.charCodeAt(0), { ctrl: true });
+    }
+  };
+  const Redo = (editor: Editor) => {
+    const platform = PlatformDetection.detect().os.current;
+    if (platform === 'macOS') {
+      TinyContentActions.keystroke(editor, 'Y'.charCodeAt(0), { meta: true });
+    } else {
+      TinyContentActions.keystroke(editor, 'Y'.charCodeAt(0), { ctrl: true });
+    }
+  };
   it('TINY-2884: shoud undo and redo action', () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
-    TinyContentActions.keystroke(editor, 'A'.charCodeAt(0), { meta: true });
+    SelectAll(editor);
     editor.execCommand('Delete');
-    TinyContentActions.keystroke(editor, 'Z'.charCodeAt(0), { meta: true });
+    Undo(editor);
     TinyAssertions.assertContent(editor, '<p>abc</p>');
-    TinyContentActions.keystroke(editor, 'Y'.charCodeAt(0), { meta: true });
+    Redo(editor);
     TinyAssertions.assertContent(editor, '');
   });
 });
