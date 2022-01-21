97af24b9101e1d3783fd3965bf6ccde53e199f91
Sasha Averin <noxuhax@setka.io>
2022-01-21 11:18:31 +0300

    TINY-8371: Add `Powered by Tiny` logo and animation to statusbar

--- modules/tinymce/src/themes/silver/main/ts/ui/statusbar/Statusbar.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/statusbar/Statusbar.ts
@@ -20,7 +20,10 @@ const renderStatusbar = (editor: Editor, providersBackstage: UiFactoryBackstageP
 
   const renderBranding = (): SimpleSpec => {
     const label = I18n.translate([ 'Powered by {0}', 'Tiny' ]);
-    const linkHtml = `<a href="https://www.tiny.cloud/?utm_campaign=editor_referral&amp;utm_medium=poweredby&amp;utm_source=tinymce&amp;utm_content=v5" rel="noopener" target="_blank" tabindex="-1" aria-label="${label}">${label}</a>`;
+    const logo = `<svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M10.574 0c2.72.016 5.407 2.314 5.407 5.664 0 0 .08 4.05-.088 5.175-.376 2.559-2.256 4.327-4.847 4.776-2.336.464-3.72.733-4.167.823-.192.04-1.04.155-1.408.155-2.848 0-5.431-2.168-5.471-5.664 0 0 0-3.733.032-4.719C.128 3.66 1.864 1.597 5.167.937L9.374.106a6.57 6.57 0 0 1 1.2-.106Zm1.904 2.975-6.4 1.263V6.78l-2.559.506v6.34l6.4-1.263V9.82l2.559-.505v-6.34Zm-2.56 3.048V9.82l-3.84.758V6.78l3.84-.757Z"/>
+    </svg>`;
+    const linkHtml = `<a href="https://www.tiny.cloud/?utm_campaign=editor_referral&amp;utm_medium=poweredby&amp;utm_source=tinymce&amp;utm_content=v5" rel="noopener" target="_blank" tabindex="-1" aria-label="${label}">${ logo }<span>${label}</span></a>`;
     return {
       dom: {
         tag: 'span',
