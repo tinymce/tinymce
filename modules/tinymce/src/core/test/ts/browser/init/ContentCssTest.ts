38aae238c23a988360fe799daa1b3bd9e19f5069
Spocke <spocke@moxiecode.com>
2022-01-26 09:20:13 +0100

    TINY-8431: fixed a few more test that where state dependent

--- modules/tinymce/src/core/test/ts/browser/init/ContentCssTest.ts
+++ modules/tinymce/src/core/test/ts/browser/init/ContentCssTest.ts
@@ -6,40 +6,39 @@ import EditorManager from 'tinymce/core/api/EditorManager';
 import { appendContentCssFromSettings } from 'tinymce/core/init/ContentCss';
 
 describe('browser.tinymce.core.init.ContentCssTest', () => {
-  const baseUrl = EditorManager.documentBaseURL.replace(/\/$/, '');
-  const skinsBaseUrl = EditorManager.baseURL + '/skins/content';
+  // Needs to be thunked since other tests might mutate these before the tests gets executed
+  const baseUrl = () => EditorManager.documentBaseURL.replace(/\/$/, '');
+  const skinsBaseUrl = () => EditorManager.baseURL + '/skins/content';
 
-  const testContentCss = (label: string, expectedContentCss: string[], inputContentCss: string[] | string | boolean, inline: boolean = false) => {
-    it(label, () => {
-      const editor = new Editor('id', {
-        content_css: inputContentCss,
-        inline
-      }, EditorManager);
+  const testContentCss = (expectedContentCss: string[], inputContentCss: string[] | string | boolean, inline: boolean = false) => {
+    const editor = new Editor('id', {
+      content_css: inputContentCss,
+      inline
+    }, EditorManager);
 
-      appendContentCssFromSettings(editor);
+    appendContentCssFromSettings(editor);
 
-      assert.deepEqual(editor.contentCSS, expectedContentCss, label);
-    });
+    assert.deepEqual(editor.contentCSS, expectedContentCss);
   };
 
-  testContentCss('Expected empty array on empty input', [], []);
-  testContentCss('Expected empty array on boolean false value', [], false);
-  testContentCss('Expected default content css on undefined value', [ `${skinsBaseUrl}/default/content.css` ], undefined);
-  testContentCss('Expected array with absolute url from .css file name', [ `${baseUrl}/test.css` ], 'test.css');
-  testContentCss('Expected array with absolute url from relative string', [ `${baseUrl}/content/test.css` ], '/content/test.css');
-  testContentCss('Expected array with absolute url from array with relative string', [ `${baseUrl}/content/test.css` ], [ '/content/test.css' ]);
-  testContentCss('Expected array with two absolute urls from an array of relative strings',
-    [ `${baseUrl}/content/a.css`, `${baseUrl}/content/b.css` ],
+  it('Expected empty array on empty input', () => testContentCss([], []));
+  it('Expected empty array on boolean false value', () => testContentCss([], false));
+  it('Expected default content css on undefined value', () => testContentCss([ `${skinsBaseUrl()}/default/content.css` ], undefined));
+  it('Expected array with absolute url from .css file name', () => testContentCss([ `${baseUrl()}/test.css` ], 'test.css'));
+  it('Expected array with absolute url from relative string', () => testContentCss([ `${baseUrl()}/content/test.css` ], '/content/test.css'));
+  it('Expected array with absolute url from array with relative string', () => testContentCss([ `${baseUrl()}/content/test.css` ], [ '/content/test.css' ]));
+  it('Expected array with two absolute urls from an array of relative strings', () => testContentCss(
+    [ `${baseUrl()}/content/a.css`, `${baseUrl()}/content/b.css` ],
     [ '/content/a.css', '/content/b.css' ]
-  );
-  testContentCss('Expected array with absolute url from absolute string', [ 'http://localhost/a/b/test.css' ], 'http://localhost/a/b/test.css');
-  testContentCss('Expected array with absolute url from string with skin name', [ `${skinsBaseUrl}/document/content.css` ], 'document');
-  testContentCss('Expected array with absolute url from array with skin name', [ `${skinsBaseUrl}/document/content.css` ], [ 'document' ]);
-  testContentCss('Expected array with absolute url from string with skin name with dash', [ `${skinsBaseUrl}/business-letter/content.css` ], 'business-letter');
-  testContentCss('Expected array with absolute url from a comma separated list of css files',
-    [ `${baseUrl}/a.css`, `${baseUrl}/b.css` ],
+  ));
+  it('Expected array with absolute url from absolute string', () => testContentCss([ 'http://localhost/a/b/test.css' ], 'http://localhost/a/b/test.css'));
+  it('Expected array with absolute url from string with skin name', () => testContentCss([ `${skinsBaseUrl()}/document/content.css` ], 'document'));
+  it('Expected array with absolute url from array with skin name', () => testContentCss([ `${skinsBaseUrl()}/document/content.css` ], [ 'document' ]));
+  it('Expected array with absolute url from string with skin name with dash', () => testContentCss([ `${skinsBaseUrl()}/business-letter/content.css` ], 'business-letter'));
+  it('Expected array with absolute url from a comma separated list of css files', () => testContentCss(
+    [ `${baseUrl()}/a.css`, `${baseUrl()}/b.css` ],
     'a.css,b.css'
-  );
+  ));
 
-  testContentCss('Expected array without content skins for inline editors', [ `${baseUrl}/document` ], 'document', true);
+  it('Expected array without content skins for inline editors', () => testContentCss([ `${baseUrl()}/document` ], 'document', true));
 });
