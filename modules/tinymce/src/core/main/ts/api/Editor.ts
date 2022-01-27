b69238eb9152f719d8b2e410976ea9c62a9cc529
QAverified <matthew.thackwell@tiny.cloud>
2022-01-27 16:48:58 +1000

    Feature/TINY-8088, removed !! from editor files (#7558)

--- modules/tinymce/src/core/main/ts/api/Editor.ts
+++ modules/tinymce/src/core/main/ts/api/Editor.ts
@@ -261,6 +261,7 @@ class Editor implements EditorObservable {
     const self = this;
 
     this.id = id;
+    this.hidden = false;
     const normalizedOptions = normalizeOptions(editorManager.defaultOptions, options);
 
     this.options = createOptions(self, normalizedOptions);
@@ -652,7 +653,7 @@ class Editor implements EditorObservable {
    * @return {Boolean} True/false if the editor is hidden or not.
    */
   public isHidden() {
-    return !!this.hidden;
+    return this.hidden;
   }
 
   /**
