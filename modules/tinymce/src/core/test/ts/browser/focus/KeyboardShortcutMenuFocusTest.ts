7c91f0e22441844e9236c7393bd8e514e39968a0
Arvin Han <arvinhjt0718@gmail.com>
2022-01-31 19:47:00 +1100

    Update KeyboardShortcutMenuFocusTest.ts, use `Arr`, move focus to editor config

--- modules/tinymce/src/core/test/ts/browser/focus/KeyboardShortcutMenuFocusTest.ts
+++ modules/tinymce/src/core/test/ts/browser/focus/KeyboardShortcutMenuFocusTest.ts
@@ -1,46 +1,32 @@
 import { FocusTools, Keys } from '@ephox/agar';
 import { context, describe, it } from '@ephox/bedrock-client';
+import { Arr } from '@ephox/katamari';
 import { SugarDocument } from '@ephox/sugar';
-import { McEditor, TinyContentActions, TinyUiActions } from '@ephox/wrap-mcagar';
-
-import Editor from 'tinymce/core/api/Editor';
-import { RawEditorOptions } from 'tinymce/core/api/OptionTypes';
+import { TinyContentActions, TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
 
 describe('browser.tinymce.core.focus.KeyboardShortcutMenuFocusTest', () => {
 
-  const pTestFocus = async (options: RawEditorOptions) => {
-    const editor = await McEditor.pFromSettings<Editor>({
-      statusbar: false,
-      ...options,
-      base_url: '/project/tinymce/js/tinymce'
+  Arr.each([
+    { label: 'classic mode', options: {}},
+    { label: 'inline mode', options: { inline: true }},
+    { label: 'class bottom', options: { toolbar_location: 'bottom' }}
+  ], (tester) => {
+    context(tester.label, () => {
+      const hook = TinyHooks.bddSetup({
+        statusbar: false,
+        ...tester.options,
+        base_url: '/project/tinymce/js/tinymce'
+      }, [], true);
+
+      it('TINY-2884: Pressing Alt+F9 focuses the element path and escape from the toolbar will focus the editor', async () => {
+        const editor = hook.editor();
+        const doc = SugarDocument.getDocument();
+
+        TinyContentActions.keystroke(editor, 120, { alt: true });
+
+        await FocusTools.pTryOnSelector('Assert menubar is focused', doc, 'div[role=menubar] .tox-mbtn');
+        TinyUiActions.keystroke(editor, Keys.escape());
+      });
     });
-    // currently for me i need to actually focus on the browser window for the test to pass, not sure if there's a bug somewhere
-    editor.focus();
-
-    // const editorDoc = TinyDom.document(editor);
-    const doc = SugarDocument.getDocument();
-
-    // Timeout before shortcut, inline mode seems to have a slower render time
-    setTimeout(() => {
-      TinyContentActions.keystroke(editor, 120, { alt: true });
-    }, 500);
-
-    await FocusTools.pTryOnSelector('Assert menubar is focused', doc, 'div[role=menubar] .tox-mbtn');
-    TinyUiActions.keystroke(editor, Keys.escape());
-    McEditor.remove(editor);
-  };
-
-  context('TINY-2884: Pressing Alt+F9 focuses the menubar and escape from the toolbar will focus the editor', () => {
-    it('classic mode', () =>
-      pTestFocus({ })
-    );
-
-    it('inline mode', () =>
-      pTestFocus({ inline: true })
-    );
-
-    it('class bottom', () =>
-      pTestFocus({ toolbar_location: 'bottom' })
-    );
   });
 });
