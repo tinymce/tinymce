4c154f8facdc7e90982fd93b323dba3a8bc61f9b
Arvin Han <arvinhjt0718@gmail.com>
2022-01-31 18:59:27 +1100

    Update KeyboardShortcutElementPathFocusTest.ts

--- modules/tinymce/src/core/test/ts/browser/focus/KeyboardShortcutElementPathFocusTest.ts
+++ modules/tinymce/src/core/test/ts/browser/focus/KeyboardShortcutElementPathFocusTest.ts
@@ -1,36 +1,32 @@
 import { FocusTools, Keys } from '@ephox/agar';
 import { context, describe, it } from '@ephox/bedrock-client';
+import { Arr } from '@ephox/katamari';
 import { SugarDocument } from '@ephox/sugar';
-import { McEditor, TinyContentActions, TinyUiActions } from '@ephox/wrap-mcagar';
-
-import Editor from 'tinymce/core/api/Editor';
-import { RawEditorOptions } from 'tinymce/core/api/OptionTypes';
+import { TinyContentActions, TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
 
 describe('browser.tinymce.core.focus.KeyboardShortcutElementPathFocusTest ', () => {
 
-  const pTestFocus = async (options: RawEditorOptions) => {
-    const editor = await McEditor.pFromSettings<Editor>({
-      menubar: false,
-      ...options,
-      base_url: '/project/tinymce/js/tinymce'
+  Arr.each([
+    { label: 'classic mode', options: {}},
+    { label: 'class bottom', options: { toolbar_location: 'bottom' }}
+  ], (tester) => {
+    context(tester.label, () => {
+      const hook = TinyHooks.bddSetup({
+        menubar: false,
+        ...tester.options,
+        base_url: '/project/tinymce/js/tinymce'
+      });
+
+      it('TINY-2884: Pressing Alt+F11 focuses the element path and escape from the toolbar will focus the editor', async () => {
+        const editor = hook.editor();
+        const doc = SugarDocument.getDocument();
+
+        TinyContentActions.keystroke(editor, 122, { alt: true });
+
+        await FocusTools.pTryOnSelector('Assert element path is focused', doc, 'div[role=navigation] .tox-statusbar__path-item');
+        TinyUiActions.keystroke(editor, Keys.escape());
+      });
     });
-
-    const doc = SugarDocument.getDocument();
-
-    TinyContentActions.keystroke(editor, 122, { alt: true });
-
-    await FocusTools.pTryOnSelector('Assert element path is focused', doc, 'div[role=navigation] .tox-statusbar__path-item');
-    TinyUiActions.keystroke(editor, Keys.escape());
-    McEditor.remove(editor);
-  };
-
-  context('TINY-2884: Pressing Alt+F11 focuses the element path and escape from the toolbar will focus the editor', () => {
-    it('classic mode', () =>
-      pTestFocus({ })
-    );
-
-    it('class bottom', () =>
-      pTestFocus({ toolbar_location: 'bottom' })
-    );
   });
+
 });
