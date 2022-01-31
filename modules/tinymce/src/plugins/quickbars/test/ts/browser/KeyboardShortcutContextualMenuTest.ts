882302783c25ebc690fbd3478ad77df0499f25a4
Arvin Han <arvinhjt0718@gmail.com>
2022-01-31 20:36:18 +1100

    Update KeyboardShortcutContextualMenuTest.ts revert the focus and timeout

--- modules/tinymce/src/plugins/quickbars/test/ts/browser/KeyboardShortcutContextualMenuTest.ts
+++ modules/tinymce/src/plugins/quickbars/test/ts/browser/KeyboardShortcutContextualMenuTest.ts
@@ -1,7 +1,7 @@
 import { FocusTools, Keys } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
 import { SugarDocument } from '@ephox/sugar';
-import { TinyHooks, TinySelections, TinyContentActions, McEditor, TinyUiActions } from '@ephox/wrap-mcagar';
+import { TinyHooks, TinySelections, TinyContentActions, TinyUiActions } from '@ephox/wrap-mcagar';
 
 import Editor from 'tinymce/core/api/Editor';
 import Plugin from 'tinymce/plugins/quickbars/Plugin';
@@ -13,18 +13,21 @@ describe('browser.tinymce.plugins.quickbars.KeyboardShortcutContextualMenuTest',
     quickbars_selection_toolbar: 'bold italic underline | formatselect | bullist numlist | blockquote quicklink',
     contextmenu: 'undo redo | inserttable | cell row column deletetable | help',
     base_url: '/project/tinymce/js/tinymce',
-  }, [ Plugin ], true);
+  }, [ Plugin ]);
 
   const doc = SugarDocument.getDocument();
 
   it('TINY-2884: keypress should focus on contextual menu', async () => {
     const editor = hook.editor();
     editor.setContent('<p>aaaaaaa</p>');
+    editor.focus();
     TinySelections.setSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 4);
-    TinyContentActions.keystroke(editor, 120, { ctrl: true });
+    // safari would need this or else won't pass
+    setTimeout(() => {
+      TinyContentActions.keystroke(editor, 120, { ctrl: true });
+    }, 40);
     await FocusTools.pTryOnSelector('Assert toolbar is focused', doc, 'div[role=toolbar] .tox-tbtn');
     TinyUiActions.keystroke(editor, Keys.escape());
     await FocusTools.pTryOnSelector('Assert editor is focused', doc, 'iframe');
-    McEditor.remove(editor);
   });
 });
