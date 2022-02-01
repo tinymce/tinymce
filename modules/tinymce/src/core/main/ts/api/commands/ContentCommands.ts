8de8dda41601cdad0e3ba77e88a59966d31046cb
Spocke <spocke@moxiecode.com>
2022-02-01 11:28:58 +0100

    TINY-7829: implemented some more native commands and added more tests

--- modules/tinymce/src/core/main/ts/api/commands/ContentCommands.ts
+++ modules/tinymce/src/core/main/ts/api/commands/ContentCommands.ts
@@ -16,6 +16,18 @@ export const registerCommands = (editor: Editor): void => {
       editor.selection.moveToBookmark(bm);
     },
 
+    insertImage: (_command, _ui, value) => {
+      InsertContent.insertAtCaret(editor, editor.dom.createHTML('img', { src: value }));
+    },
+
+    insertHorizontalRule: () => {
+      editor.execCommand('mceInsertContent', false, '<hr>');
+    },
+
+    insertText: (_command, _ui, value) => {
+      InsertContent.insertAtCaret(editor, editor.dom.encode(value));
+    },
+
     insertHTML: (_command, _ui, value) => {
       InsertContent.insertAtCaret(editor, value);
     },
@@ -34,10 +46,6 @@ export const registerCommands = (editor: Editor): void => {
 
     mceNewDocument: () => {
       editor.setContent('');
-    },
-
-    InsertHorizontalRule: () => {
-      editor.execCommand('mceInsertContent', false, '<hr />');
     }
   });
 };
--- modules/tinymce/src/core/main/ts/api/commands/NewlineCommands.ts
+++ modules/tinymce/src/core/main/ts/api/commands/NewlineCommands.ts
@@ -11,6 +11,10 @@ import Editor from '../Editor';
 
 export const registerCommands = (editor: Editor): void => {
   editor.editorCommands.addCommands({
+    insertParagraph: () => {
+      InsertNewLine.insert(editor);
+    },
+
     mceInsertNewLine: (_command, _ui, value) => {
       InsertNewLine.insert(editor, value);
     },
