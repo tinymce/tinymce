63b6c710d2f02469e4de81da70d4a7f1252053eb
Viktor Pyshnenko <viktor.pyshnenko@tiny.cloud>
2022-02-02 09:44:46 +0200

    TINY-8351: move fire('SetContent') out of insertContent

--- modules/tinymce/src/core/main/ts/content/InsertContentImpl.ts
+++ modules/tinymce/src/core/main/ts/content/InsertContentImpl.ts
@@ -255,7 +255,6 @@ export const insertHtmlAtCaret = (editor: Editor, value: string, details: Insert
   if (details.paste === true && InsertList.isListFragment(editor.schema, fragment) && InsertList.isParentBlockLi(dom, parentNode)) {
     rng = InsertList.insertAtCaret(serializer, dom, selection.getRng(), fragment);
     selection.setRng(rng);
-    // editor.fire('SetContent', args);
     return;
   }
 
