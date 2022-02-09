a87c7232eb6499cc4fe7bec0906abae52b47e2f4
Ben Tran <ben.tran@tiny.cloud>
2022-02-10 10:03:40 +1030

    TINY-8101: Extended test

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/core/UndoRedoTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/core/UndoRedoTest.ts
@@ -1,4 +1,4 @@
-import { Keys, UiFinder } from '@ephox/agar';
+import { ApproxStructure, Assertions, Keys, UiFinder, Waiter } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
 import { SugarBody } from '@ephox/sugar';
 import { TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
@@ -17,6 +17,22 @@ describe('browser.tinymce.themes.silver.editor.core.UndoRedoTest', () => {
 
   const pAssertMenuItemDisabled = (editor, item: string, state: boolean = true) =>
     TinyUiActions.pWaitForUi(editor, `div[title="${item}"][role="menuitem"][aria-disabled="${state}"]`);
+
+  const pAssertUiDisabled = async (editor: Editor, disabled: boolean) => {
+    const overlord = UiFinder.findIn(SugarBody.body(), '.tox-toolbar-overlord').getOrDie();
+    await Waiter.pTryUntil(
+      'Waiting for toolbar state',
+      () => Assertions.assertStructure('Toolbar should be in disabled state', ApproxStructure.build((s, str, arr) =>
+        s.element('div', {
+          classes: [
+            arr.has('tox-toolbar-overlord'),
+            disabled ? arr.has('tox-tbtn--disabled') : arr.not('tox-tbtn--disabled')
+          ],
+          attrs: { 'aria-disabled': str.is(disabled ? 'true' : 'false') }
+        })
+      ), overlord)
+    );
+  };
 
   it('TINY-8101: Undo/redo should be disabled by default', async () => {
     const editor = hook.editor();
@@ -49,10 +65,21 @@ describe('browser.tinymce.themes.silver.editor.core.UndoRedoTest', () => {
     TinyUiActions.keydown(hook.editor(), Keys.escape());
   });
 
-  it('TINY-8101: Undo/redo are always disabled in readonly mode', () => {
+  it('TINY-8101: Undo/redo are always disabled in readonly mode', async () => {
     const editor = hook.editor();
+    // 1. switch to readonly mode
     editor.mode.set('readonly');
+    await pAssertUiDisabled(editor, true);
     assertToolbarButtonDisabled('Redo');
     assertToolbarButtonDisabled('Undo');
+
+    // 2. switch back to design mode. Expect undo to be re-enabled
+    editor.mode.set('design');
+    await pAssertUiDisabled(editor, false);
+    assertToolbarButtonDisabled('Redo', false);
+    assertToolbarButtonDisabled('Undo');
+    TinyUiActions.clickOnMenu(editor, 'button:contains("Edit")');
+    await pAssertMenuItemDisabled(editor, 'Redo', false);
+    await pAssertMenuItemDisabled(editor, 'Undo');
   });
 });
