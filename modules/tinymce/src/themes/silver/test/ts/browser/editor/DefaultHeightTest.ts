b1cda5189e782e9b36ad3c8d67122eb1de870583
Ben Tran <ben.tran@tiny.cloud>
2022-02-15 10:41:58 +1030

    TINY-6860: Updated tests

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/DefaultHeightTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/DefaultHeightTest.ts
@@ -9,44 +9,46 @@ describe('browser.tinymce.themes.silver.editor.DefaultHeightTest', () => {
     base_url: '/project/tinymce/js/tinymce'
   };
 
+  const assertEditorHeight = (editor: Editor, expected: number) => assert.equal(editor.getContainer().offsetHeight, expected);
+
   it('TINY-8443: Default height should be 400px', async () => {
     const editor = await McEditor.pFromSettings<Editor>(baseSettings);
-    assert.equal(editor.getContainer().offsetHeight, 400);
+    assertEditorHeight(editor, 400);
     McEditor.remove(editor);
   });
 
-  it('TINY-8443: Option heigh takes precedence', async () => {
-    const editor = await McEditor.pFromSettings<Editor>({ height: 500, ...baseSettings });
-    assert.equal(editor.getContainer().offsetHeight, 500);
+  it('TINY-8443: Height option takes precedence', async () => {
+    const editor = await McEditor.pFromSettings<Editor>({ ...baseSettings, height: 500 });
+    assertEditorHeight(editor, 500);
     McEditor.remove(editor);
   });
 
-  it('TINY-8443: CSS height takes precendence in the absence of option height', async () => {
+  it('TINY-8443: CSS height takes precedence in the absence of the height option when larger than the default height', async () => {
     let editor = await McEditor.pFromHtml<Editor>('<div style="height: 600px"></div>', baseSettings);
-    assert.equal(editor.getContainer().offsetHeight, 600);
+    assertEditorHeight(editor, 600);
     McEditor.remove(editor);
 
     // Test with another height unit
     editor = await McEditor.pFromHtml<Editor>('<div style="height: 50rem"></div>', baseSettings);
-    assert.equal(editor.getContainer().offsetHeight, 800);
+    assertEditorHeight(editor, 800);
     McEditor.remove(editor);
   });
 
-  it('TINY-8443: Use default height if CSS height is less and option height is not specified', async () => {
+  it('TINY-8443: Use default height if CSS height is less and the height option is not specified', async () => {
     const editor = await McEditor.pFromHtml<Editor>('<div style="height: 200px"></div>', baseSettings);
-    assert.equal(editor.getContainer().offsetHeight, 400);
+    assertEditorHeight(editor, 400);
     McEditor.remove(editor);
   });
 
-  it('TINY-8443: Option height should take precedence over CSS height', async () => {
-    const editor = await McEditor.pFromHtml<Editor>('<div style="height: 600px"></div>', { height: 721, ...baseSettings });
-    assert.equal(editor.getContainer().offsetHeight, 721);
+  it('TINY-8443: The height option should take precedence over CSS height', async () => {
+    const editor = await McEditor.pFromHtml<Editor>('<div style="height: 600px"></div>', { ...baseSettings, height: 721 });
+    assertEditorHeight(editor, 721);
     McEditor.remove(editor);
   });
 
-  it('TINY-8443: Default min-height to 100px', async () => {
-    const editor = await McEditor.pFromSettings<Editor>({ height: 80, ...baseSettings });
-    assert.equal(editor.getContainer().offsetHeight, 100);
+  it('TINY-8443: The min-height option defaults to 100px', async () => {
+    const editor = await McEditor.pFromSettings<Editor>({ ...baseSettings, height: 80 });
+    assertEditorHeight(editor, 100);
     McEditor.remove(editor);
   });
 });
