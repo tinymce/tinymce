95a7df12581c4fdd1fd7ed09412997c06db2c701
Viktor Pyshnenko <viktor.pyshnenko@tiny.cloud>
2022-02-18 13:09:16 +0200

    TINY-8397: check UI hasn't been rendered before open dialog

--- modules/tinymce/src/themes/silver/test/ts/browser/window/SilverDialogBeforeEditorRenderedTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/window/SilverDialogBeforeEditorRenderedTest.ts
@@ -1,4 +1,6 @@
+import { UiFinder } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
+import { SugarBody } from '@ephox/sugar';
 import { TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
 
 import Editor from 'tinymce/core/api/Editor';
@@ -11,14 +13,14 @@ describe('browser.tinymce.themes.silver.window.SilverDialogBeforeEditorRenderedT
 
   it('TINY-8397: Open dialog while the inline editor is hidden should not throw an exception', async () => {
     const editor = hook.editor();
+    UiFinder.notExists(SugarBody.body(), '.tox-tinymce-aux');
     editor.windowManager.open({
-        title: 'Test Dialog',
-        body: {
-          type: 'panel',
-          items: []
-        },
-        buttons: []
-      });
+      title: 'Test Dialog',
+      body: {
+        type: 'panel',
+        items: []
+      },
+      buttons: []
     });
     await TinyUiActions.pWaitForDialog(editor, 'div[role="dialog"].tox-dialog');
     TinyUiActions.closeDialog(editor);
