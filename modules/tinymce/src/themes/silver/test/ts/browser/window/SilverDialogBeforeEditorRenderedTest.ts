b5b14216107a5ae67b610bc2a0427d5dd8c63daf
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2022-02-18 12:34:00 +0200

    TINY-8397: open dialog
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>

--- modules/tinymce/src/themes/silver/test/ts/browser/window/SilverDialogBeforeEditorRenderedTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/window/SilverDialogBeforeEditorRenderedTest.ts
@@ -11,6 +11,15 @@ describe('browser.tinymce.themes.silver.window.SilverDialogBeforeEditorRenderedT
 
   it('TINY-8397: Open dialog while the inline editor is hidden should not throw an exception', async () => {
     const editor = hook.editor();
+    editor.windowManager.open({
+        title: 'Test Dialog',
+        body: {
+          type: 'panel',
+          items: []
+        },
+        buttons: []
+      });
+    });
     await TinyUiActions.pWaitForDialog(editor, 'div[role="dialog"].tox-dialog');
     TinyUiActions.closeDialog(editor);
   });
