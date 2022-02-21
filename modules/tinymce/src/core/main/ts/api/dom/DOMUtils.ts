1f87c8dd2eafc8983eb9132ae07c32bf0c563e84
tiny-ben-tran <97494111+tiny-ben-tran@users.noreply.github.com>
2022-02-21 11:32:23 +1030

    TINY-8102: Fixed invalid HTML tags used in the deprecated comments (#7641)

--- modules/tinymce/src/core/main/ts/api/dom/DOMUtils.ts
+++ modules/tinymce/src/core/main/ts/api/dom/DOMUtils.ts
@@ -1808,7 +1808,7 @@ const DOMUtils = (doc: Document, settings: Partial<DOMUtilsSettings> = {}): DOMU
     /**
      * Fires the specified event name and optional object on the specified target.
      * <br>
-     * <em>Deprecated in TinyMCE 6.0 and has been marked for removal in TinyMCE 7.0. Use <code>dispatch<code> instead.</em>
+     * <em>Deprecated in TinyMCE 6.0 and has been marked for removal in TinyMCE 7.0. Use <code>dispatch</code> instead.</em>
      *
      * @method fire
      * @param {Node/Document/Window} target Target element or object to fire event on.
--- modules/tinymce/src/core/main/ts/api/util/EventDispatcher.ts
+++ modules/tinymce/src/core/main/ts/api/util/EventDispatcher.ts
@@ -128,7 +128,7 @@ class EventDispatcher<T> {
   /**
    * Fires the specified event by name.
    * <br>
-   * <em>Deprecated in TinyMCE 6.0 and has been marked for removal in TinyMCE 7.0. Use <code>dispatch<code> instead.</em>
+   * <em>Deprecated in TinyMCE 6.0 and has been marked for removal in TinyMCE 7.0. Use <code>dispatch</code> instead.</em>
    *
    * @method fire
    * @param {String} name Name of the event to fire.
