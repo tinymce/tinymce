e69181f03f28e813ec972ecd8c24a8a10f194d3b
Mike Chau <mike.chau@tiny.cloud>
2022-02-25 08:36:57 +1000

    TINY-8441: link conversions

--- modules/tinymce/src/core/main/ts/api/EditorManager.ts
+++ modules/tinymce/src/core/main/ts/api/EditorManager.ts
@@ -305,7 +305,7 @@ const EditorManager: EditorManager = {
   /**
    * Initializes a set of editors. This method will create editors based on various settings.
    * <br /><br />
-   * For information on basic usage of <code>init</code>, see: <a href="https://www.tiny.cloud/docs/general-configuration-guide/basic-setup/">Basic setup</a>.
+   * For information on basic usage of <code>init</code>, see: <a href="https://www.tiny.cloud/docs/tinymce/6/basic-setup.html">Basic setup</a>.
    *
    * @method init
    * @param {Object} options Options object to be passed to each editor instance.
@@ -360,7 +360,7 @@ const EditorManager: EditorManager = {
       if (Env.browser.isIE() || Env.browser.isEdge()) {
         ErrorReporter.initError(
           'TinyMCE does not support the browser you are using. For a list of supported' +
-          ' browsers please see: https://www.tinymce.com/docs/get-started/system-requirements/'
+          ' browsers please see: https://www.tiny.cloud/docs/tinymce/6/support.html#_supported_web_browsers'
         );
         return [];
       } else if (isQuirksMode) {
