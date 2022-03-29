3326308f0d4b0aaabd38427d5b89586e6ff93e33
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2022-03-29 08:52:28 +0300

    TINY-8215: triple click selection implementation

--- modules/tinymce/src/core/main/ts/selection/MultiClickSelection.ts
+++ modules/tinymce/src/core/main/ts/selection/MultiClickSelection.ts
@@ -1,44 +1,82 @@
 import { SugarElement } from '@ephox/sugar';
 
 import Editor from '../api/Editor';
-import * as CaretFinder from '../caret/CaretFinder';
 import { CaretPosition } from '../caret/CaretPosition';
-import { isTextBlock } from '../dom/ElementType';
+import { isBlock } from '../dom/ElementType';
+import { isCaretCandidate } from '../caret/CaretCandidate';
 import * as NodeType from '../dom/NodeType';
 import * as RangeNormalizer from './RangeNormalizer';
+import DomTreeWalker from '../api/dom/TreeWalker';
 
-const isTextBlockNode = (node: Node): node is Element => NodeType.isElement(node) && isTextBlock(SugarElement.fromDom(node));
+const isBr = NodeType.isBr;
+const isText = NodeType.isText;
 
-const normalizeSelection = (editor: Editor) => {
-  const rng = editor.selection.getRng();
-  const startPos = CaretPosition.fromRangeStart(rng);
-  const endPos = CaretPosition.fromRangeEnd(rng);
+const isBoundary = (node: Node): boolean => {
+  return isBr(node) || isBlock(SugarElement.fromDom(node));
+}
 
+const walkBackwardWhile = (startNode: Node, rootNode: Node): Node => {
+  const walker = new DomTreeWalker(startNode, rootNode);
+  let result: Node = startNode;
+  for (let next = walker.prev(); next && !isBoundary(next); next = walker.prev()){
+    if(isCaretCandidate(next)){
+      result = next
+    }
+  }
+  return result
+}
+
+const walkForwardWhile = (startNode: Node, rootNode: Node): Node => {
+  const walker = new DomTreeWalker(startNode, rootNode);
+  let result: Node = startNode;
+  for (let next = startNode; next && !isBoundary(next); next = walker.next()){
+    if(isCaretCandidate(next)){
+      result = next
+    }
+  }
+  return result
+}
+
+const findClosestBlockRange = (startRng: Range, rootNode: Node) => {
+  const startPos = CaretPosition.fromRangeStart(startRng);
+  let clickNode = startPos.container()
   if (CaretPosition.isElementPosition(startPos)) {
-    const container = startPos.container();
-    if (isTextBlockNode(container)) {
-      CaretFinder.firstPositionIn(container).each((pos) => rng.setStart(pos.container(), pos.offset()));
-    }
+    clickNode = clickNode.childNodes[startPos.offset()]
   }
 
-  if (CaretPosition.isElementPosition(endPos)) {
-    const container = startPos.container();
-    if (isTextBlockNode(container)) {
-      CaretFinder.lastPositionIn(container).each((pos) => rng.setEnd(pos.container(), pos.offset()));
-    }
+  const startNode = walkBackwardWhile(clickNode, rootNode)
+  const endNode = walkForwardWhile(clickNode, rootNode)
+
+  const rng = document.createRange();
+  if (isText(startNode)) {
+    rng.setStart(startNode, 0);
+  } else {
+    rng.setStartBefore(startNode);
   }
 
+  if (isText(endNode)) {
+    rng.setEnd(endNode, endNode.data.length);
+  } else {
+    rng.setEndAfter(endNode);
+  }
+  return rng
+}
+
+const onTripleClickSelect = (editor) => {
+  const rng = findClosestBlockRange(editor.selection.getRng(), editor.getBody())
   editor.selection.setRng(RangeNormalizer.normalize(rng));
-};
+}
 
 const setup = (editor: Editor) => {
-  editor.on('click', (e) => {
+  editor.on('mousedown', (e) => {
     if (e.detail >= 3) {
-      normalizeSelection(editor);
+      e.preventDefault();
+      onTripleClickSelect(editor)
     }
   });
 };
 
 export {
+  findClosestBlockRange,
   setup
 };
