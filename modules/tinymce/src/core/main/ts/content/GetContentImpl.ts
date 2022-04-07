e6ea992d08f6ce54334cab9522937fc9e7bfa501
Hannes Frimmel Moström <hannes.mostrom@tiny.cloud>
2022-04-07 04:39:21 +0200

    TINY-8578: Newlines for more than one empty line.

--- modules/tinymce/src/core/main/ts/content/GetContentImpl.ts
+++ modules/tinymce/src/core/main/ts/content/GetContentImpl.ts
@@ -23,7 +23,8 @@ const getContentFromBody = (editor: Editor, args: GetContentArgs, body: HTMLElem
     content = Tools.trim(TrimHtml.trimExternal(editor.serializer, body.innerHTML));
   } else if (args.format === 'text') {
     // return empty string for text format when editor is empty to avoid bogus elements being returned in content
-    content = editor.dom.isEmpty(body) ? '' : Zwsp.trim(body.innerText || body.textContent);
+    content = Zwsp.trim(body.innerText || body.textContent);
+    content = '\n' === content ? '' : content;
   } else if (args.format === 'tree') {
     content = editor.serializer.serialize(body, args);
   } else {
