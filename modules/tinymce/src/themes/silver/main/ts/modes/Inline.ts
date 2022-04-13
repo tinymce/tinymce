10532bb1df2f44f5da68c368535387e9099d5f45
Ben Tran <ben.tran@tiny.cloud>
2022-04-13 16:52:41 +0930

    TINY-8594: Removed comment

--- modules/tinymce/src/themes/silver/main/ts/modes/Inline.ts
+++ modules/tinymce/src/themes/silver/main/ts/modes/Inline.ts
@@ -106,9 +106,6 @@ const render = (editor: Editor, uiComponents: RenderUiComponents, rawUiConfig: R
     editor.nodeChanged();
   };
 
-  // In certain circumstances we need to delay the render function until the next
-  // event loop to ensure things like the current selection are correct.
-
   editor.on('show', render);
   editor.on('hide', ui.hide);
 
