a623272e24261f2693b43c42c68f8ada184bac70
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2022-04-19 12:57:39 +0200

    add undo case to the test

--- modules/tinymce/src/core/test/ts/browser/api/commands/NewlineCommandsTest.ts
+++ modules/tinymce/src/core/test/ts/browser/api/commands/NewlineCommandsTest.ts
@@ -20,11 +20,18 @@ describe('browser.tinymce.core.api.commands.NewlineCommandTest', () => {
 
   it('TINY-8606: InsertParagraph command ', () => {
     const editor = hook.editor();
+    editor.undoManager.clear();
     editor.setContent('<p>This is a test</p>');
     TinySelections.setSelection(editor, [ 0, 0 ], 'This'.length, [ 0, 0 ], 'This is a '.length);
     editor.execCommand('InsertParagraph');
     TinyAssertions.assertContent(editor, '<p>This</p><p>test</p>');
     TinyAssertions.assertCursor(editor, [ 1, 0 ], 0);
+    editor.undoManager.undo();
+
+    setTimeout(() => {
+      TinyAssertions.assertContent(editor, '<p>This is a test</p>');
+      TinyAssertions.assertCursor(editor, [ 1, 0 ], 0);
+    }, 0);
   });
 
   it('TINY-7829: mceInsertNewLine command', () => {
