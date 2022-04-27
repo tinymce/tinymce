10b6cbea4f2634f44446d9476b17ae4e598ef20f
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2022-04-27 18:35:44 +0300

    TINY-8601: remove 'it' from title test
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>

--- modules/tinymce/src/core/test/ts/browser/keyboard/ArrowKeysInlineBoundariesTest.ts
+++ modules/tinymce/src/core/test/ts/browser/keyboard/ArrowKeysInlineBoundariesTest.ts
@@ -338,7 +338,7 @@ describe('browser.tinymce.core.keyboard.ArrowKeysInlineBoundariesTest', () => {
   });
 
   context('Arrow keys when selection is not collapsed', () => {
-    it('TINY-8601: It should move caret before selected inline boundary node when arrow left', () => {
+    it('TINY-8601: should move caret before selected inline boundary node when arrow left', () => {
       const editor = hook.editor();
       editor.setContent('<p>test <span class="mce-annotation">span</span> selection</p>', { format: 'raw' });
       TinySelections.setSelection(editor, [ 0 ], 1, [ 0 ], 2, true);
