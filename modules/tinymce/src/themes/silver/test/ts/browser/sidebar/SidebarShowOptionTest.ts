27b5d8c8ca12b0fed79ffa51595446c9677dc7ad
Ben Tran <ben.tran@tiny.cloud>
2022-05-25 22:34:39 +0930

    TINY-8710: Updated tests

--- modules/tinymce/src/themes/silver/test/ts/browser/sidebar/SidebarShowOptionTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/sidebar/SidebarShowOptionTest.ts
@@ -1,17 +1,16 @@
 
-import { Waiter } from '@ephox/agar';
 import { TestHelpers } from '@ephox/alloy';
-import {describe, it } from '@ephox/bedrock-client';
+import { beforeEach, describe, it } from '@ephox/bedrock-client';
 import { Sidebar } from '@ephox/bridge';
 import { SugarElement, Traverse } from '@ephox/sugar';
 import { McEditor } from '@ephox/wrap-mcagar';
-
 interface EventLog {
   readonly name: string;
   readonly index: number;
 }
 
 describe('browser.tinymce.core.options.SidebarShowOptionTest', () => {
+  const store = TestHelpers.TestStore();
   const settingsFactory = (store: TestHelpers.TestStore) => {
     const logEvent = (name: string) => (api: Sidebar.SidebarInstanceApi) => {
       const index = Traverse.findIndex(SugarElement.fromDom(api.element())).getOr(-1);
@@ -36,52 +35,51 @@ describe('browser.tinymce.core.options.SidebarShowOptionTest', () => {
     };
   };
 
+  beforeEach(() => {
+    store.clear();
+  });
+
   it('TINY-8710: Show no sidebar on init if not set', async () => {
-    const store = TestHelpers.TestStore();
     const editor = await McEditor.pFromSettings({
       ...settingsFactory(store),
     });
-    await Waiter.pTryUntil('Checking initial events', () => store.assertEq('Asserting initial show of sidebars', []));
+    store.assertEq('Asserting initial show of sidebars', []);
     McEditor.remove(editor);
   });
 
   it('TINY-8710: Show a sidebar on init', async () => {
-    const store = TestHelpers.TestStore();
     const editor = await McEditor.pFromSettings({
       ...settingsFactory(store),
       sidebar_show: 'sidebarone'
     });
-    await Waiter.pTryUntil('Checking initial events', () => store.assertEq('Asserting initial show of sidebars', [
+    store.assertEq('Asserting initial show of sidebars', [
       {
         name: 'sidebarone:show',
         index: 0
       },
-    ]));
+    ]);
     McEditor.remove(editor);
   });
 
-  it('TINY-8710: Show just one sidebar on init', async () => {
-    const store = TestHelpers.TestStore();
+  it('TINY-8710: Show a different sidebar on init', async () => {
     const editor = await McEditor.pFromSettings({
       ...settingsFactory(store),
       sidebar_show: 'sidebartwo'
     });
-    await Waiter.pTryUntil('Checking initial events', () => store.assertEq('Asserting initial show of sidebars', [
+    store.assertEq('Asserting initial show of sidebars', [
       {
         name: 'sidebartwo:show',
         index: 1
       },
-    ]));
+    ]);
     McEditor.remove(editor);
   });
 
-  it('TINY-8710: Show no side bar if name does not exist', async () => {
-    const store = TestHelpers.TestStore();
+  it('TINY-8710: Show no sidebar if the name does not exist', async () => {
     const editor = await McEditor.pFromSettings({
       ...settingsFactory(store),
-      sidebar_show: 'sidebartwo'
     });
-    await Waiter.pTryUntil('Checking initial events', () => store.assertEq('Asserting initial show of sidebars', []));
+    store.assertEq('Asserting initial show of sidebars', []);
     McEditor.remove(editor);
   });
 });
