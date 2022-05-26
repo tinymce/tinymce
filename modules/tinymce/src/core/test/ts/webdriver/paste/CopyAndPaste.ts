b273d9df0cf0c530268c1b919fb2d0bc61dcc6ac
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2022-05-26 09:24:41 +0200

    TINY-7719: refacto test

--- modules/tinymce/src/core/test/ts/webdriver/paste/CopyAndPaste.ts
+++ modules/tinymce/src/core/test/ts/webdriver/paste/CopyAndPaste.ts
@@ -1,6 +1,6 @@
-import { GeneralSteps, Pipeline, RealClipboard, RealMouse, Waiter } from '@ephox/agar';
-import { UnitTest } from '@ephox/bedrock-client';
-import { TinyApis, TinyHooks, TinyUi } from '@ephox/mcagar';
+import { RealClipboard, RealMouse } from '@ephox/agar';
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyApis, TinyAssertions, TinyHooks, TinyUi } from '@ephox/mcagar';
 
 import Editor from 'tinymce/core/api/Editor';
 
@@ -10,35 +10,29 @@ const hook = TinyHooks.bddSetup<Editor>({
   statusbar: false
 }, []);
 
-UnitTest.asynctest('TINY-7719 :: Test wrapped element are preserved in copy and paste', (success, failure) => {
-  const editor = hook.editor();
-  const ui = TinyUi(editor);
-  const api = TinyApis(editor);
+describe('webdriver.tinymce.core.paste.CopyAndPaste', () => {
+  it('TINY-7719: Test wrapped elements are preserved in copy and paste', async () => {
+    const editor = hook.editor();
+    const ui = TinyUi(editor);
+    const api = TinyApis(editor);
 
-  const sTestVersion = () => GeneralSteps.sequence([
-    api.sSetContent(
+    editor.setContent(
       '<pre>abc</pre>' +
       '<h1>something</h1>' +
       '<p>abc def</p>'
-    ),
-    api.sSetSelection([ 0, 0 ], 0, [ 0, 0 ], 3),
-    ui.sClickOnMenu('Click Edit menu', 'button:contains("Edit")'),
-    ui.sWaitForUi('Wait for dropdown', '*[role="menu"]'),
-    RealMouse.sClickOn('div[title="Copy"]'),
-    api.sSetSelection([ 1, 0 ], 0, [ 1, 0 ], 3),
-    RealClipboard.sPaste('iframe => body => h1'),
-    Waiter.sTryUntil(
-      'Cut is async now, so need to wait for content',
-      api.sAssertContent(
-        '<pre>abc</pre>\n' +
-        '<pre>abc</pre>\n' +
-        '<h1>ething</h1>\n' +
-        '<p>abc def</p>'
-      )
-    )
-  ]);
+    );
+    api.setSelection([ 0, 0 ], 0, [ 0, 0 ], 3);
+    ui.clickOnMenu('button:contains("Edit")');
+    await ui.pWaitForUi('*[role="menu"]');
+    await RealMouse.pClickOn('div[title="Copy"]');
+    api.setSelection([ 1, 0 ], 0, [ 1, 0 ], 3);
+    await RealClipboard.pPaste('iframe => body => h1');
+    TinyAssertions.assertContent(editor,
+      '<pre>abc</pre>\n' +
+      '<pre>abc</pre>\n' +
+      '<h1>ething</h1>\n' +
+      '<p>abc def</p>'
+    );
 
-  Pipeline.async({}, [
-    sTestVersion()
-  ], success, failure);
+  });
 });
