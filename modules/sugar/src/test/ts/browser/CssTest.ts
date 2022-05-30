885aba79fe9b4bbb1015d416fd52be65826bddc3
Ben Tran <ben.tran@tiny.cloud>
2022-05-31 09:14:50 +0930

    TINY-8750: Removed comment

--- modules/sugar/src/test/ts/browser/CssTest.ts
+++ modules/sugar/src/test/ts/browser/CssTest.ts
@@ -41,7 +41,6 @@ UnitTest.test('CssTest', () => {
 
     // NOTE: Safari, Firefox 71+ and Chromium 102+ seem to support styles for math ml tags, so the Css.copy(m, c2) clobbers the previous style
     const browser = PlatformDetection.detect().browser;
-    // Would cover other Chromium based browsers such as Chrome and Edge
     if (browser.isSafari() || browser.isFirefox() && browser.version.major >= 71 || browser.isChromium() && browser.version.major >= 102) {
       Css.copy(c, c2);
     }
