78f7b68abd364004e579c8ede1e79fcbdfbae677
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2022-06-06 09:14:36 +0200

    TINY-7719: add comment to test

--- modules/tinymce/src/core/test/ts/webdriver/paste/CopyAndPasteTest.ts
+++ modules/tinymce/src/core/test/ts/webdriver/paste/CopyAndPasteTest.ts
@@ -15,6 +15,9 @@ describe('webdriver.tinymce.core.paste.CopyAndPasteTest', () => {
 
   const pCopyAndPaste = async (editor: Editor, source: Selection, target: Selection): Promise<void> => {
     TinySelections.setSelection(editor, ...source);
+    // at the moment: RealClipboard.pCopy('iframe => body'), doesn't work in with all browser (see https://github.com/webdriverio/webdriverio/issues/622)
+    // chrome, chrome-headless, firefox-headless -> it doesn't work
+    // firefox -> it works
     TinyUiActions.clickOnMenu(editor, 'button:contains("Edit")');
     await TinyUiActions.pWaitForUi(editor, '*[role="menu"]');
     await RealMouse.pClickOn('div[title="Copy"]');
