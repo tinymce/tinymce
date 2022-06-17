c14bd0da20ec078b345c44a6ccc0c43781c9a61e
MitchCTiny <89239746+MitchCTiny@users.noreply.github.com>
2022-06-17 17:40:10 +1000

    TINY-8057: CommandOptions interface

--- modules/tinymce/src/plugins/link/main/ts/api/Commands.ts
+++ modules/tinymce/src/plugins/link/main/ts/api/Commands.ts
@@ -3,12 +3,12 @@ import Editor from 'tinymce/core/api/Editor';
 import * as Dialog from '../ui/Dialog';
 import * as Options from './Options';
 
-interface Dialog {
+interface CommandOptions {
   readonly dialog?: boolean;
 }
 
 const register = (editor: Editor): void => {
-  editor.addCommand('mceLink', (_ui, value?: Dialog) => {
+  editor.addCommand('mceLink', (_ui, value?: CommandOptions) => {
     if (value?.dialog === true || !Options.useQuickLink(editor)) {
       Dialog.open(editor);
     } else {
