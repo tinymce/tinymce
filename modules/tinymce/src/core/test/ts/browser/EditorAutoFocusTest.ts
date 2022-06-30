ae7e3b9a8cf212f2ec8eafa1e28ce3c443a78d70
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2022-06-30 13:23:30 +0300

    TINY-8785: follow naming conventions

--- modules/tinymce/src/core/test/ts/browser/EditorAutoFocusTest.ts
+++ modules/tinymce/src/core/test/ts/browser/EditorAutoFocusTest.ts
@@ -6,38 +6,38 @@ import 'tinymce';
 import Editor from 'tinymce/core/api/Editor';
 import EditorManager from 'tinymce/core/api/EditorManager';
 
-const isInViewport = (editor: Editor) => {
-  const containerRect = editor.getContentAreaContainer().getBoundingClientRect();
-  const innerRect = editor.selection.getNode().getBoundingClientRect();
-  const top = containerRect.top + innerRect.top + 1;
-  return top > 0 && top + innerRect.height < window.innerHeight;
-};
-
-const setupEditorAutoFocus = (id: string) => {
-  return new Promise((resolve) => {
-    EditorManager.init({
-      selector: 'textarea',
-      base_url: '/project/tinymce/js/tinymce/',
-      toolbar: '',
-      menubar: false,
-      statusbar: false,
-      height: 1000,
-      auto_focus: id,
-      init_instance_callback: (editor: Editor) => {
-        editor.on('focus', resolve);
-      }
-    });
-  });
-};
-
-const testEditorAutoFocus = async (id: string) => {
-  await setupEditorAutoFocus(id);
-  const editor = EditorManager.get(id);
-  assert.isTrue(editor.hasFocus());
-  assert.isTrue(isInViewport(editor));
-};
-
 describe('browser.tinymce.core.EditorAutoFocusTest', () => {
+  const isInViewport = (editor: Editor) => {
+    const containerRect = editor.getContentAreaContainer().getBoundingClientRect();
+    const innerRect = editor.selection.getNode().getBoundingClientRect();
+    const top = containerRect.top + innerRect.top + 1;
+    return top > 0 && top + innerRect.height < window.innerHeight;
+  };
+
+  const pSetupEditorAutoFocus = (id: string) => {
+    return new Promise((resolve) => {
+      EditorManager.init({
+        selector: 'textarea',
+        base_url: '/project/tinymce/js/tinymce/',
+        toolbar: '',
+        menubar: false,
+        statusbar: false,
+        height: 1000,
+        auto_focus: id,
+        init_instance_callback: (editor: Editor) => {
+          editor.on('focus', resolve);
+        }
+      });
+    });
+  };
+
+  const pTestEditorAutoFocus = async (id: string) => {
+    await pSetupEditorAutoFocus(id);
+    const editor = EditorManager.get(id);
+    assert.isTrue(editor.hasFocus());
+    assert.isTrue(isInViewport(editor));
+  };
+
   before(() => {
     Insert.append(SugarBody.body(), SugarElement.fromHtml(`<div>
     <textarea id="mce_0">Editor_0</textarea>
@@ -51,14 +51,14 @@ describe('browser.tinymce.core.EditorAutoFocusTest', () => {
   });
 
   it('TINY-8785: should autofocus the first editor', async () => {
-    await testEditorAutoFocus('mce_0');
+    await pTestEditorAutoFocus('mce_0');
   });
 
   it('TINY-8785: should autofocus the second editor', async () => {
-    await testEditorAutoFocus('mce_1');
+    await pTestEditorAutoFocus('mce_1');
   });
 
   it('TINY-8785: should autofocus the third editor', async () => {
-    await testEditorAutoFocus('mce_2');
+    await pTestEditorAutoFocus('mce_2');
   });
 });
