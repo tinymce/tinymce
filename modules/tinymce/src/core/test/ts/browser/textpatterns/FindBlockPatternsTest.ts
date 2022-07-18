ce39467a4c17e86670410a196252cb78e6a04952
tiny-ben-tran <ben.tran@tiny.cloud>
2022-07-18 13:55:56 +0930

    TINY-8778: New `text_patterns_lookup` option (#7969)
    * TINY-8778: Initial POC for adding text_patterns_lookup for more flexibility
    
    * TINY-8778: Fixed bug and added tests for block pattern
    
    * TINY-8778: Updated test titles
    
    * TINY-8778: Lookup patterns take precedence
    
    * TINY-8778: Added changelog entry
    
    * TINY-8778: Updated test titles
    
    * TINY-8778: Fixed lint error
    
    * Update modules/tinymce/src/core/main/ts/api/Options.ts
    
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>
    
    * Update modules/tinymce/src/core/main/ts/textpatterns/core/BlockPattern.ts
    
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>
    
    * TINY-8778: Changed option type
    
    * TINY-8778: Moved checking to Keyboard.ts
    
    * Update modules/tinymce/src/core/main/ts/textpatterns/keyboard/Keyboard.ts
    
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>
    
    * Update modules/tinymce/src/core/test/ts/browser/textpatterns/FindBlockPatternTest.ts
    
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>
    
    * TINY-8778: Updated tests
    
    * TINY-8778: Renamed FindBlockPatternTest
    
    * TINY-8778: Fixed lint
    
    * TINY-8778: Fixed failed tests
    
    * TINY-8778: Clean up FindBlockPatternsTest
    
    * TINY:8778: Moved checking for selection collapsed up
    
    * Update modules/tinymce/src/core/main/ts/textpatterns/core/Pattern.ts
    
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>
    
    * Update modules/tinymce/src/core/main/ts/textpatterns/core/Pattern.ts
    
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>
    
    * TINY-8778: More changes
    
    * TINY-8778: Moved help functions inside describe
    
    * TINY-8778: Fixed types
    
    Co-authored-by: Morgan Smith <morgan.smith@tiny.cloud>
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>

---
+++ modules/tinymce/src/core/test/ts/browser/textpatterns/FindBlockPatternsTest.ts
@@ -0,0 +1,199 @@
+import { context, describe, it } from '@ephox/bedrock-client';
+import { TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
+import { assert } from 'chai';
+
+import Editor from 'tinymce/core/api/Editor';
+import * as BlockPattern from 'tinymce/core/textpatterns/core/BlockPattern';
+
+import { getPatternSetFor } from '../../module/test/TextPatternsUtils';
+
+// Similar to modules/tinymce/src/core/test/ts/atomic/textpatterns/FindBlockPatternsTest.ts
+// but uses DOM and includes tests for text_patterns_lookup
+describe('browser.tinymce.textpatterns.FindBlockPatternsTest', () => {
+
+  context('no text_patterns_lookup', () => {
+    const hook = TinyHooks.bddSetupLight<Editor>({
+      text_patterns: [
+        { start: '*', end: '*', format: 'italic' },
+        { start: '**', end: '**', format: 'bold' },
+        { start: '#', format: 'h1' },
+        { start: '##', format: 'h2' },
+        { start: '###', format: 'h3' }
+      ],
+      base_url: '/project/tinymce/js/tinymce'
+    }, []);
+
+    const getPatternSet = getPatternSetFor(hook);
+
+    // TODO: This may not be the expected behaviour, depending on the decisions that being made in TINY-4303
+    it('TBA: # triggers heading match when block tag matches forced_root_block', () => {
+      const editor = hook.editor();
+      // For block patterns to execute, the block tag must be the same as the
+      // forced root block. We aren't sure why this constraint exists.
+      editor.setContent('<p># Heading</p>');
+      TinySelections.setCursor(editor, [ 0, 0 ], '# Heading'.length);
+      const matches = BlockPattern.findPatterns(editor, getPatternSet());
+      assert.deepEqual(
+        matches,
+        [
+          {
+            pattern: {
+              type: 'block-format',
+              start: '#',
+              format: 'h1'
+            },
+            range: { start: [ 0, 0 ], end: [ 0, 0 ] }
+          }
+        ],
+        'Checking block pattern matches'
+      );
+    });
+
+    it('TINY-8778: * does not trigger a match', () => {
+      const editor = hook.editor();
+      editor.setContent('<p>* No match');
+      TinySelections.setCursor(editor, [ 0, 0 ], 1);
+      const matches = BlockPattern.findPatterns(editor, getPatternSet());
+      assert.deepEqual(matches, []);
+    });
+
+    // TODO: This may not be the expected behaviour, depending on the decisions that being made in TINY-4303
+    it('TBA: # does not trigger heading match when block tag does not match forced_root_block', () => {
+      const editor = hook.editor();
+      // For block patterns to execute, the block tag must be the same as the
+      // forced root block. We aren't sure why this constraint exists.
+      editor.setContent('<div># Heading</div>');
+      TinySelections.setCursor(editor, [ 0, 0 ], '# Heading'.length);
+      const matches = BlockPattern.findPatterns(editor, getPatternSet());
+      assert.deepEqual(
+        matches,
+        [],
+        'Checking block pattern matches - do not match for incorrect block tag type'
+      );
+    });
+  });
+
+  context('with text_patterns_lookup', () => {
+    const hook = TinyHooks.bddSetupLight<Editor>({
+      text_patterns: [
+        { start: '*', end: '*', format: 'italic' },
+        { start: '**', end: '**', format: 'bold' },
+        { start: '#', format: 'h1' },
+        { start: '##', format: 'h2' },
+        { start: '###', format: 'h3' },
+        { start: '' }
+      ],
+      text_patterns_lookup: (_ctx) => [
+        { start: '####', format: 'h4' },
+        { start: 'TBA', cmd: 'mceInsertContent', value: 'To be announced' },
+        { start: '###', cmd: 'mceInsertContent', value: 'h3 heading' }
+      ],
+      base_url: '/project/tinymce/js/tinymce'
+    }, []);
+
+    const getPatternSet = getPatternSetFor(hook);
+
+    it('TINY-8778: Match a heading format which is extending the existing text patterns', () => {
+      const editor = hook.editor();
+      editor.setContent('<p>#### wow</p>');
+      TinySelections.setCursor(editor, [ 0, 0 ], 4);
+      const matches = BlockPattern.findPatterns(editor, getPatternSet());
+      assert.deepEqual(
+        matches,
+        [
+          {
+            pattern: {
+              type: 'block-format',
+              start: '####',
+              format: 'h4'
+            },
+            range: {
+              start: [ 0, 0 ],
+              end: [ 0, 0 ]
+            }
+          }
+        ],
+        'Checking block pattern matches'
+      );
+    });
+
+    it('TINY-8778: Match a command pattern', () => {
+      const editor = hook.editor();
+      editor.setContent('<p><b>TBA Bold heading</b></p>');
+      TinySelections.setCursor(editor, [ 0, 0, 0 ], 3);
+      const matches = BlockPattern.findPatterns(editor, getPatternSet());
+      assert.deepEqual(
+        matches,
+        [
+          {
+            pattern: {
+              type: 'block-command',
+              start: 'TBA',
+              cmd: 'mceInsertContent',
+              value: 'To be announced'
+            },
+            range: {
+              start: [ 0, 0 ],
+              end: [ 0, 0 ]
+            }
+          }
+        ],
+        'Checking block pattern matches'
+      );
+    });
+
+    // TODO: This maybe need to be fixed, depending on the decisions that being made in TINY-4303
+    it('TINY-8778: Match a heading where its block tag matches forced_root_block but its parent block does not', () => {
+      const editor = hook.editor();
+      editor.setContent('<div><p>#### New heading type</p></div>');
+      TinySelections.setCursor(editor, [ 0, 0, 0 ], 4);
+      const patterns = getPatternSet();
+      const matches = BlockPattern.findPatterns(editor, patterns);
+      assert.deepEqual(matches, [
+        {
+          pattern: {
+            type: 'block-format',
+            start: '####',
+            format: 'h4'
+          },
+          range: {
+            start: [ 0, 0, 0 ],
+            end: [ 0, 0, 0 ]
+          }
+        }
+      ],
+      'Checking block pattern matches'
+      );
+    });
+
+    // TODO: This may not be the expected behaviour, depending on the decisions that being made in TINY-4303
+    it('TBA: Does not trigger a match if the block tag does not match forced_root_block', () => {
+      const editor = hook.editor();
+      editor.setContent('<div>#### New heading type</div>');
+      TinySelections.setCursor(editor, [ 0, 0 ], 4);
+      const matches = BlockPattern.findPatterns(editor, getPatternSet());
+      assert.deepEqual(matches, [], 'Checking block pattern matches do not match for incorrect block tag type');
+    });
+
+    it('TINY-8778: Lookup patterns take precendence over block patterns', () => {
+      const editor = hook.editor();
+      editor.setContent('<p>### is a new heading</p>');
+      TinySelections.setCursor(editor, [ 0, 0 ], 4);
+      const matches = BlockPattern.findPatterns(editor, getPatternSet());
+      assert.deepEqual(matches, [
+        {
+          pattern: {
+            type: 'block-command',
+            start: '###',
+            cmd: 'mceInsertContent',
+            value: 'h3 heading'
+          },
+          range: {
+            start: [ 0, 0 ],
+            end: [ 0, 0 ]
+          }
+        }
+      ]);
+    });
+  });
+});
