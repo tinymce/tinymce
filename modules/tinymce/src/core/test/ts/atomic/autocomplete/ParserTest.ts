580e45e144b8b0c7dd042dc601b1a4428f45e0fe
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2022-07-25 08:19:41 +0300

    TINY-8887: roll back to wildcard importd

--- modules/tinymce/src/core/test/ts/atomic/autocomplete/ParserTest.ts
+++ modules/tinymce/src/core/test/ts/atomic/autocomplete/ParserTest.ts
@@ -1,45 +1,45 @@
 import { describe, it } from '@ephox/bedrock-client';
 import { assert } from 'chai';
 
-import { findTrigger } from 'tinymce/core/autocomplete/AutocompleteContext';
+import * as AutocompleteContext from 'tinymce/core/autocomplete/AutocompleteContext';
 
 describe('atomic.tinymce.core.autocomplete.AutocompleteContext', () => {
   it('should find the one char trigger', () => {
-    assert.equal(findTrigger('', 0, '@').getOr(null), -1);
-    assert.equal(findTrigger('abc', 0, '@').getOr(null), -1);
-    assert.equal(findTrigger('@abc', 0, '@').getOr(null), -1);
-    assert.equal(findTrigger('@abc', 2, '@').getOr(null), 0);
-    assert.equal(findTrigger('@abc', 40, '@').getOr(null), 0);
+    assert.equal(AutocompleteContext.findTrigger('', 0, '@').getOr(null), -1);
+    assert.equal(AutocompleteContext.findTrigger('abc', 0, '@').getOr(null), -1);
+    assert.equal(AutocompleteContext.findTrigger('@abc', 0, '@').getOr(null), -1);
+    assert.equal(AutocompleteContext.findTrigger('@abc', 2, '@').getOr(null), 0);
+    assert.equal(AutocompleteContext.findTrigger('@abc', 40, '@').getOr(null), 0);
     // Multiple trigger entries
-    assert.equal(findTrigger('@def@abc', 40, '@').getOr(null), 4);
-    assert.equal(findTrigger('@def@abc', 4, '@').getOr(null), 0);
+    assert.equal(AutocompleteContext.findTrigger('@def@abc', 40, '@').getOr(null), 4);
+    assert.equal(AutocompleteContext.findTrigger('@def@abc', 4, '@').getOr(null), 0);
     // Whitespace
-    assert.equal(findTrigger('@abc x', 40, '@').getOr(null), null);
-    assert.equal(findTrigger('@abc\u00a0x', 40, '@').getOr(null), null);
-    assert.equal(findTrigger('@abc\tx', 40, '@').getOr(null), null);
-    assert.equal(findTrigger('@abc\nx', 40, '@').getOr(null), null);
-    assert.equal(findTrigger('@abc 123', 4, '@').getOr(null), 0);
+    assert.equal(AutocompleteContext.findTrigger('@abc x', 40, '@').getOr(null), null);
+    assert.equal(AutocompleteContext.findTrigger('@abc\u00a0x', 40, '@').getOr(null), null);
+    assert.equal(AutocompleteContext.findTrigger('@abc\tx', 40, '@').getOr(null), null);
+    assert.equal(AutocompleteContext.findTrigger('@abc\nx', 40, '@').getOr(null), null);
+    assert.equal(AutocompleteContext.findTrigger('@abc 123', 4, '@').getOr(null), 0);
     // Fake caret
-    assert.equal(findTrigger('@abc\uFFEFx', 40, '@').getOr(null), 0);
+    assert.equal(AutocompleteContext.findTrigger('@abc\uFFEFx', 40, '@').getOr(null), 0);
   });
 
   it(' should find the multi-char trigger', () => {
-    assert.equal(findTrigger('', 0, '@@').getOr(null), -1);
-    assert.equal(findTrigger('abc', 0, '@@').getOr(null), -1);
-    assert.equal(findTrigger('@@abc', 0, '@@').getOr(null), -1);
-    assert.equal(findTrigger('@@abc', 2, '@@').getOr(null), 0);
-    assert.equal(findTrigger('@@abc', 40, '@@').getOr(null), 0);
+    assert.equal(AutocompleteContext.findTrigger('', 0, '@@').getOr(null), -1);
+    assert.equal(AutocompleteContext.findTrigger('abc', 0, '@@').getOr(null), -1);
+    assert.equal(AutocompleteContext.findTrigger('@@abc', 0, '@@').getOr(null), -1);
+    assert.equal(AutocompleteContext.findTrigger('@@abc', 2, '@@').getOr(null), 0);
+    assert.equal(AutocompleteContext.findTrigger('@@abc', 40, '@@').getOr(null), 0);
     // Multiple trigger entries
-    assert.equal(findTrigger('@@def@@abc', 40, '@@').getOr(null), 5);
-    assert.equal(findTrigger('@@def@@abc', 5, '@@').getOr(null), 0);
+    assert.equal(AutocompleteContext.findTrigger('@@def@@abc', 40, '@@').getOr(null), 5);
+    assert.equal(AutocompleteContext.findTrigger('@@def@@abc', 5, '@@').getOr(null), 0);
     // Whitespace
-    assert.equal(findTrigger('@@abc x', 40, '@@').getOr(null), null);
-    assert.equal(findTrigger('@@abc\u00a0x', 40, '@@').getOr(null), null);
-    assert.equal(findTrigger('@@abc\tx', 40, '@@').getOr(null), null);
-    assert.equal(findTrigger('@@abc\nx', 40, '@@').getOr(null), null);
-    assert.equal(findTrigger('@@abc 123', 5, '@@').getOr(null), 0);
-    assert.equal(findTrigger('abc @@@123', 40, '@@@').getOr(null), 4);
+    assert.equal(AutocompleteContext.findTrigger('@@abc x', 40, '@@').getOr(null), null);
+    assert.equal(AutocompleteContext.findTrigger('@@abc\u00a0x', 40, '@@').getOr(null), null);
+    assert.equal(AutocompleteContext.findTrigger('@@abc\tx', 40, '@@').getOr(null), null);
+    assert.equal(AutocompleteContext.findTrigger('@@abc\nx', 40, '@@').getOr(null), null);
+    assert.equal(AutocompleteContext.findTrigger('@@abc 123', 5, '@@').getOr(null), 0);
+    assert.equal(AutocompleteContext.findTrigger('abc @@@123', 40, '@@@').getOr(null), 4);
     // Fake caret
-    assert.equal(findTrigger('@@abc\uFFEFx', 40, '@@').getOr(null), 0);
+    assert.equal(AutocompleteContext.findTrigger('@@abc\uFFEFx', 40, '@@').getOr(null), 0);
   });
 });
