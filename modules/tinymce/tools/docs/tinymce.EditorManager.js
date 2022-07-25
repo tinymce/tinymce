4923748d33eb455692b7188679269ea7f882a5ef
Andrew Herron <thespyder@programmer.net>
2022-07-25 16:14:01 +1000

    TINY-8591: Move the EditorManager class to a standalone file, better reflecting how it's intended to be used

---
+++ modules/tinymce/tools/docs/tinymce.EditorManager.js
@@ -0,0 +1,11 @@
+/**
+ * This is the base class for the <a href="tinymce.root.html>TinyMCE</a> global, a factory for tinymce.Editor instances.
+ *
+ * It is not generally used directly - the global <a href="tinymce.root.html>TinyMCE</a> is the only one expected to be created.
+ *
+ * @example
+ * tinymce.EditorManager.init({});
+ *
+ * @class tinymce.EditorManager
+ * @static
+ */
