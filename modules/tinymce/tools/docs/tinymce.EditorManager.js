a914bfb41378d3d7cd7882124ff2c8a03cc6fd0f
Andrew Herron <thespyder@programmer.net>
2022-07-27 21:53:35 +1000

    TINY-8591: Tweak to link syntax

--- modules/tinymce/tools/docs/tinymce.EditorManager.js
+++ modules/tinymce/tools/docs/tinymce.EditorManager.js
@@ -1,7 +1,7 @@
 /**
- * This is the base class for the <a href="tinymce.root.html>TinyMCE</a> global, a factory for tinymce.Editor instances.
+ * This is the base class for the link:../tinymce.root/[`tinymce`] global, a factory for link:../tinymce.editor/[`tinymce.Editor`] instances.
  *
- * It is not generally used directly - the global <a href="tinymce.root.html>TinyMCE</a> is the only one expected to be created.
+ * It is not generally used directly - the global link:../tinymce.root/[`tinymce`] is the only one expected to be created.
  *
  * @example
  * tinymce.EditorManager.init({});
