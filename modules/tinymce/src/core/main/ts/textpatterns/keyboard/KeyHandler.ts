aa792a4761eecf387e5b0b11f088c80ce0419b29
tiny-ben-tran <ben.tran@tiny.cloud>
2022-07-28 08:26:17 +0930

    TINY-8778: Fixed `text_patterns_lookup` being called more than expected when matching patterns (#7991)
    * TINY-8778: Reduced the number of text_patterns_lookup being called for matching inline patterns
    
    * TINY-8778: text_patterns_lookup should only be called once when pressing space or enter key
    
    * TINY-8778: Refactorred
    
    * TINY-8778: Updated tests
    
    * TINY-8778: Removed blank line
    
    * TINY-8778: Cleanup
    
    * TINY-8779: Fixed whole content of a block being passed to inline pattern
    
    * TINY-8778: Cleaned up
    
    * TINY-8778: Fixed type
    
    * TINY-8778: Fixed types
    
    * TINY-8778: Updated tests
    
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>

--- modules/tinymce/src/core/main/ts/textpatterns/keyboard/KeyHandler.ts
+++ modules/tinymce/src/core/main/ts/textpatterns/keyboard/KeyHandler.ts
@@ -5,55 +5,65 @@ import Editor from '../../api/Editor';
 import VK from '../../api/util/VK';
 import * as BlockPattern from '../core/BlockPattern';
 import * as InlinePattern from '../core/InlinePattern';
-import { InlinePatternSet, PatternSet } from '../core/PatternTypes';
+import { PatternSet } from '../core/PatternTypes';
 import * as Utils from '../utils/Utils';
 
 const handleEnter = (editor: Editor, patternSet: PatternSet): boolean => {
-  // Find any matches
-  // IMPORTANT: We need to get normalized match results since undoing and redoing the editor state
-  // via undoManager.extra() will result in the DOM being normalized.
-  const inlineMatches = InlinePattern.findPatterns(editor, patternSet, true, false);
-  const blockMatches = BlockPattern.findPatterns(editor, patternSet, true);
-  if (blockMatches.length > 0 || inlineMatches.length > 0) {
-    editor.undoManager.add();
-    editor.undoManager.extra(
-      () => {
-        editor.execCommand('mceInsertNewLine');
-      },
-      () => {
-        // create a cursor position that we can move to avoid the inline formats
-        editor.insertContent(Unicode.zeroWidth);
-        InlinePattern.applyMatches(editor, inlineMatches);
-        BlockPattern.applyMatches(editor, blockMatches);
-        // find the spot before the cursor position
-        const range = editor.selection.getRng();
-        const spot = textBefore(range.startContainer, range.startOffset, editor.dom.getRoot());
-        editor.execCommand('mceInsertNewLine');
-        // clean up the cursor position we used to preserve the format
-        spot.each((s) => {
-          const node = s.container;
-          if (node.data.charAt(s.offset - 1) === Unicode.zeroWidth) {
-            node.deleteData(s.offset - 1, 1);
-            Utils.cleanEmptyNodes(editor.dom, node.parentNode, (e: Node) => e === editor.dom.getRoot());
-          }
-        });
-      }
-    );
-    return true;
-  }
-  return false;
+  const rng = editor.selection.getRng();
+  return Utils.getParentBlock(editor, rng).map((block) => {
+    const offset = Math.max(0, rng.startOffset);
+    const dynamicPatternSet = Utils.resolveFromDynamicPatterns(patternSet, block, block.textContent ?? '');
+    // IMPORTANT: We need to get normalized match results since undoing and redoing the editor state
+    // via undoManager.extra() will result in the DOM being normalized.
+    const inlineMatches = InlinePattern.findPatterns(editor, block, rng.startContainer, offset, dynamicPatternSet, true);
+    const blockMatches = BlockPattern.findPatterns(editor, block, dynamicPatternSet, true);
+    if (blockMatches.length > 0 || inlineMatches.length > 0) {
+      editor.undoManager.add();
+      editor.undoManager.extra(
+        () => {
+          editor.execCommand('mceInsertNewLine');
+        },
+        () => {
+          // create a cursor position that we can move to avoid the inline formats
+          editor.insertContent(Unicode.zeroWidth);
+          InlinePattern.applyMatches(editor, inlineMatches);
+          BlockPattern.applyMatches(editor, blockMatches);
+          // find the spot before the cursor position
+          const range = editor.selection.getRng();
+          const spot = textBefore(range.startContainer, range.startOffset, editor.dom.getRoot());
+          editor.execCommand('mceInsertNewLine');
+          // clean up the cursor position we used to preserve the format
+          spot.each((s) => {
+            const node = s.container;
+            if (node.data.charAt(s.offset - 1) === Unicode.zeroWidth) {
+              node.deleteData(s.offset - 1, 1);
+              Utils.cleanEmptyNodes(editor.dom, node.parentNode, (e: Node) => e === editor.dom.getRoot());
+            }
+          });
+        }
+      );
+      return true;
+    }
+    return false;
+  }).getOr(false);
 };
 
 const handleInlineKey = (
   editor: Editor,
-  patternSet: InlinePatternSet
+  patternSet: PatternSet
 ): void => {
-  const inlineMatches = InlinePattern.findPatterns(editor, patternSet, false, true);
-  if (inlineMatches.length > 0) {
-    editor.undoManager.transact(() => {
-      InlinePattern.applyMatches(editor, inlineMatches);
-    });
-  }
+  const rng = editor.selection.getRng();
+  Utils.getParentBlock(editor, rng).map((block) => {
+    const offset = Math.max(0, rng.startOffset - 1);
+    const beforeText = Utils.getBeforeText(editor.dom, block, rng.startContainer, offset);
+    const dynamicPatternSet = Utils.resolveFromDynamicPatterns(patternSet, block, beforeText);
+    const inlineMatches = InlinePattern.findPatterns(editor, block, rng.startContainer, offset, dynamicPatternSet, false);
+    if (inlineMatches.length > 0) {
+      editor.undoManager.transact(() => {
+        InlinePattern.applyMatches(editor, inlineMatches);
+      });
+    }
+  });
 };
 
 const checkKeyEvent = <T>(codes: T[], event: KeyboardEvent, predicate: (code: T, event: KeyboardEvent) => boolean): boolean => {
