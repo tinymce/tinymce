3f7a669fe4284e760fc504e584664f391088cb17
Hamza Benkhaldoun <hamza0867@gmail.com>
2022-07-31 23:29:05 +0100

    TINY-8940: simplify test by using UiFinder.notExists

--- modules/tinymce/src/plugins/link/test/ts/browser/ContextToolbarTest.ts
+++ modules/tinymce/src/plugins/link/test/ts/browser/ContextToolbarTest.ts
@@ -1,8 +1,7 @@
-import { Mouse, UiFinder } from '@ephox/agar';
+import { Mouse, UiFinder, Waiter } from '@ephox/agar';
 import { describe, it, before, after } from '@ephox/bedrock-client';
 import { SugarBody } from '@ephox/sugar';
 import { TinyDom, TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
-import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
 import Plugin from 'tinymce/plugins/link/Plugin';
@@ -44,19 +43,13 @@ describe('browser.tinymce.plugins.link.ContextToolbarTest', () => {
     await UiFinder.pWaitForState<HTMLInputElement>('check link content', SugarBody.body(), '.tox-toolbar input', (ele) => ele.dom.value === 'http://www.google.com');
   });
 
-  it('TINY-8940: toolbar doesn\'t show on non link elements', () => {
+  it('TINY-8940: toolbar doesn\'t show on non link elements', async () => {
     const editor = hook.editor();
     editor.options.set('link_context_toolbar', true);
     editor.setContent('<p>google</p>');
     Mouse.trueClickOn(TinyDom.body(editor), 'p');
-    // You might be wondering why I did not use UiFinder.notExists.
-    // And the reason being simply it doesn't work in this case.
-    // I am not sure why, but you can put a  link in the editor content
-    // and click on it. The tollbar will show as it should, while the call
-    // UiFinder.notExists(SugarBody.body(), '.tox-toolbar') will still pass
-    // when it should fail since the toolbar does exist
-    const isNotToolbarFound = UiFinder.findIn(SugarBody.body(), '.tox-toolbar').isError();
-    assert.isTrue(isNotToolbarFound, 'There is no toolbar when clicking on non link elements');
+    await Waiter.pWait(100);
+    UiFinder.notExists(SugarBody.body(), '.tox-pop__dialog .tox-toolbar');
     editor.setContent('');
   });
 
