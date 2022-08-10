dfb271abd2971779d6e3bd170de6c427b80c5e73
Hamza Benkhaldoun <hamza0867@gmail.com>
2022-08-10 16:52:12 +0100

    TINY-8874: fix CI

--- modules/katamari/src/test/ts/browser/RepeatableTest.ts
+++ modules/katamari/src/test/ts/browser/RepeatableTest.ts
@@ -1,9 +1,12 @@
-import { Waiter } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
 import { assert } from 'chai';
 
 import { Optional } from 'ephox/katamari/api/Optional';
 import * as Singleton from 'ephox/katamari/api/Singleton';
+
+const delay = (ms: number): Promise<never> => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 describe('browser.katamari.RepeatableTests', () => {
   it('Make a repeatable then clean it', async () => {
@@ -13,14 +16,14 @@ describe('browser.katamari.RepeatableTests', () => {
     intervalId.set(() => {
       counter++;
     });
-    await Waiter.pWait(201);
+    await delay(250);
     assert.strictEqual(counter, 2);
     const currentId = intervalId.get().getOrNull();
     assert.isNotNull(currentId);
     intervalId.set(() => {
       counter--;
     });
-    await Waiter.pWait(201);
+    await delay(250);
     const newId = intervalId.get().getOrNull();
     assert.isNotNull(currentId);
     assert.notStrictEqual(currentId, newId);
@@ -29,7 +32,7 @@ describe('browser.katamari.RepeatableTests', () => {
     intervalId.clear();
     assert.isFalse(intervalId.isSet());
     assert.strictEqual(intervalId.get(), Optional.none());
-    await Waiter.pWait(101);
+    await delay(150);
     assert.strictEqual(counter, 0);
   });
 });
