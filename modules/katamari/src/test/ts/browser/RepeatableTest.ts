c5bf3d84fb9dc01548243a93711f7ef56343741b
Hamza Benkhaldoun <hamza0867@gmail.com>
2022-08-12 11:22:50 +0100

    TINY-8874: add assertions around time in RepeatableTest

--- modules/katamari/src/test/ts/browser/RepeatableTest.ts
+++ modules/katamari/src/test/ts/browser/RepeatableTest.ts
@@ -13,26 +13,35 @@ describe('browser.katamari.RepeatableTests', () => {
     const intervalId = Singleton.repeatable(100);
     assert.strictEqual(intervalId.get(), Optional.none());
     let counter = 0;
+    let start = Date.now();
     intervalId.set(() => {
       counter++;
     });
     await delay(250);
+    let end = Date.now();
     assert.strictEqual(counter, 2);
+    assert.isAbove(end - start, 200);
     const currentId = intervalId.get().getOrNull();
     assert.isNotNull(currentId);
+    start = Date.now();
     intervalId.set(() => {
       counter--;
     });
     await delay(250);
+    end = Date.now();
     const newId = intervalId.get().getOrNull();
     assert.isNotNull(currentId);
     assert.notStrictEqual(currentId, newId);
     assert.strictEqual(counter, 0);
     assert.isTrue(intervalId.isSet());
+    assert.isAbove(end - start, 200);
     intervalId.clear();
     assert.isFalse(intervalId.isSet());
     assert.strictEqual(intervalId.get(), Optional.none());
-    await delay(150);
+    start = Date.now();
+    await delay(150); // Waiting to make sure that interval did not run again
+    end = Date.now();
+    assert.isAbove(end - start, 100);
     assert.strictEqual(counter, 0);
   });
 });
