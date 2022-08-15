bff7d527abed3bbad5e36f0478d268c4b5d060c9
Andrew Herron <thespyder@programmer.net>
2022-08-15 11:02:32 +0000

    TINY-8591: Corrected whitespace

--- modules/tinymce/tools/docs/tinymce.EditorManager.js
+++ modules/tinymce/tools/docs/tinymce.EditorManager.js
@@ -1,10 +1,10 @@
 /**
  * This is the base class for the link:../tinymce.root/[`tinymce`] global.
-*
-* This class is a factory for link:../tinymce.editor/[`tinymce.Editor`] instances.
-*
-* And the link:../tinymce.root/[`tinymce`] global is the only
-* EditorManager instance the class is expected to create.
+ *
+ * This class is a factory for link:../tinymce.editor/[`tinymce.Editor`] instances.
+ *
+ * And the link:../tinymce.root/[`tinymce`] global is the only
+ * EditorManager instance the class is expected to create.
  *
  * @example
  * tinymce.EditorManager.init({});
