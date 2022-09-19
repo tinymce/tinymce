82df9fed1b4d8c35ef34f3f71d1c8133350801ae
Sasha Averin <noxuhax@setka.io>
2022-09-19 19:19:08 +0400

    TINY-8819: Lint fixes

--- modules/tinymce/src/themes/silver/main/ts/Render.ts
+++ modules/tinymce/src/themes/silver/main/ts/Render.ts
@@ -163,12 +163,11 @@ const setup = (editor: Editor): RenderInfo => {
 
     const menubarCollection = shouldHavePromotion ? [ partPromotion, partMenubar ] : [ partMenubar ];
 
-
     return OuterContainer.parts.header({
       dom: {
         tag: 'div',
         classes: [ 'tox-editor-header' ]
-          .concat((hasMultipleToolbar || hasToolbar || hasMenubar) ? [] : ['tox-editor-header--empty']),
+          .concat((hasMultipleToolbar || hasToolbar || hasMenubar) ? [] : [ 'tox-editor-header--empty' ]),
         ...verticalDirAttributes
       },
       components: Arr.flatten<AlloySpec>([
