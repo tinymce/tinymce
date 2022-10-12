07be1aba160ef902ac4227ca046d01ad7dd40cbd
Ben Tran <ben.tran@tiny.cloud>
2022-10-12 16:45:07 +1030

    TINY-9190: Removed types

--- modules/tinymce/src/plugins/quickbars/main/ts/insert/Toolbars.ts
+++ modules/tinymce/src/plugins/quickbars/main/ts/insert/Toolbars.ts
@@ -1,5 +1,5 @@
 import { Fun } from '@ephox/katamari';
-import { Attribute, PredicateFind, SelectorExists, SelectorFind, SugarElement, SugarNode } from '@ephox/sugar';
+import { Attribute, PredicateExists, SelectorExists, SelectorFind, SugarElement, SugarNode } from '@ephox/sugar';
 
 import Editor from 'tinymce/core/api/Editor';
 
@@ -9,19 +9,19 @@ const addToEditor = (editor: Editor): void => {
   const insertToolbarItems = Options.getInsertToolbarItems(editor);
   if (insertToolbarItems.length > 0) {
     editor.ui.registry.addContextToolbar('quickblock', {
-      predicate: (node: Node) => {
+      predicate: (node) => {
         const sugarNode = SugarElement.fromDom(node);
         const textBlockElementsMap = editor.schema.getTextBlockElements();
-        const isRoot = (elem: SugarElement<Node>) => elem.dom === editor.getBody();
-        const isFakeCaret = (elem: SugarElement<Node>) =>
-          Attribute.has(elem, 'data-mce-bogus') || SelectorExists.ancestor(elem, '[data-mce-bogus="all"]', (el: SugarElement<Node>) => isRoot(el));
+        const isRoot = (elem) => elem.dom === editor.getBody();
+        const isFakeCaret = (elem) =>
+          Attribute.has(elem, 'data-mce-bogus') || SelectorExists.ancestor(elem, '[data-mce-bogus="all"]', (el) => isRoot(el));
 
         return SelectorFind.closest(sugarNode, 'table', isRoot).fold(
-          () => PredicateFind.closest(
+          () => PredicateExists.closest(
             sugarNode,
-            (elem: SugarElement<Node>) => !isFakeCaret(elem) && SugarNode.name(elem) in textBlockElementsMap && editor.dom.isEmpty(elem.dom),
+            (elem) => !isFakeCaret(elem) && SugarNode.name(elem) in textBlockElementsMap && editor.dom.isEmpty(elem.dom),
             isRoot
-          ).isSome(),
+          ),
           Fun.never
         );
       },
