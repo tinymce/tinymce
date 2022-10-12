054231834efa41e0a40652db44c10457aac36b76
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2022-10-12 12:58:33 +0200

    TINY-8881: refactor

--- modules/tinymce/src/core/main/ts/caret/ClosestCaretCandidate.ts
+++ modules/tinymce/src/core/main/ts/caret/ClosestCaretCandidate.ts
@@ -1,4 +1,4 @@
-import { Arr, Optional } from '@ephox/katamari';
+import { Arr, Fun, Optional } from '@ephox/katamari';
 import { Compare, SugarElement, Traverse } from '@ephox/sugar';
 
 import { getClientRects, NodeClientRect } from '../dom/Dimensions';
@@ -102,9 +102,8 @@ const closestChildCaretCandidateNodeRect = (children: ChildNode[], clientX: numb
 
 const traverseUp = (rootElm: SugarElement<Node>, scope: SugarElement<Element>, clientX: number, clientY: number): Optional<NodeClientRect> => {
   const helper = (scope: SugarElement<Element>, prevScope: Optional<SugarElement<Element>>): Optional<NodeClientRect> => {
-    const childNodesWithoutGhost = Arr.filter(Arr.from(scope.dom.childNodes), (node) =>
-      !(node as HTMLElement)?.classList?.contains('mce-drag-container')
-    );
+    const isDragGhostContainer = (node: ChildNode) => NodeType.isElement(node) && node.classList.contains('mce-drag-container');
+    const childNodesWithoutGhost = Arr.filter(scope.dom.childNodes, Fun.not(isDragGhostContainer));
 
     return prevScope.fold(
       () => closestChildCaretCandidateNodeRect(childNodesWithoutGhost, clientX, clientY),
