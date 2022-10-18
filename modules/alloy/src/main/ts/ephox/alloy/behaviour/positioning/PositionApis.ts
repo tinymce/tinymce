c0b37339b9bfc074487c64f13263579abeecb267
Morgan Smith <morgan.smith@tiny.cloud>
2022-10-18 17:41:09 +1000

    TINY-9226: Removing unused custom "placer" function - unnecessarily confusing configuration

--- modules/alloy/src/main/ts/ephox/alloy/behaviour/positioning/PositionApis.ts
+++ modules/alloy/src/main/ts/ephox/alloy/behaviour/positioning/PositionApis.ts
@@ -61,14 +61,13 @@ const positionWithinBounds = (component: AlloyComponent, posConfig: PositioningC
     // (bottom and right) will be using the wrong dimensions
     const origin = posConfig.useFixed() ? getFixedOrigin() : getRelativeOrigin(component);
 
-    const placer = anchorage.placement;
+    const placement = anchorage.placement;
 
     const getBounds = bounds.map(Fun.constant).or(posConfig.getBounds);
 
-    placer(component, anchorage, origin).each((anchoring) => {
+    placement(component, anchorage, origin).each((anchoring) => {
       // Place the element and then update the state for the placee
-      const doPlace = anchoring.placer.getOr(place);
-      const newState = doPlace(component, origin, anchoring, getBounds, placee, placeeState, placeeDetail.transition);
+      const newState = place(component, origin, anchoring, getBounds, placee, placeeState, placeeDetail.transition);
       posState.set(placee.uid, newState);
     });
 
