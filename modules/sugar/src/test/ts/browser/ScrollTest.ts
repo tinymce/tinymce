4354b3e6ac9cc4e78b02166892709c3062b77d47
mkzhx <mkzh.org@gmail.com>
2022-10-25 12:22:16 +1000

    TINY-9203: Allow scrollbar to be either hidden or visible when on Win10/11 FF (#8211)
    - in LocationTest and ScrollTest in sugar
    
    (cherry picked from commit 6d4c160bf30ab5a9fc0342d9f585fd15fd875778)

--- modules/sugar/src/test/ts/browser/ScrollTest.ts
+++ modules/sugar/src/test/ts/browser/ScrollTest.ts
@@ -135,7 +135,9 @@ UnitTest.asynctest('ScrollTest', (success, failure) => {
     const cX = Math.round(center.left);
     const cY = Math.round(center.top);
 
-    const noVisibleScrollbarBrowser = platform.os.isOSX() || (platform.browser.isFirefox() && platform.os.isLinux());
+    // TINY-9203: due to Win11 FF adopting native hidden scrollbar behavior and current inability to distinguish between Win10 and Win11
+    // (both os.version.major === 10), allow scrollbar to be either hidden or visible when on Win10/11 FF
+    const noVisibleScrollbarBrowser = platform.os.isMacOS() || (platform.browser.isFirefox() && platform.os.isLinux()) || (platform.browser.isFirefox() && platform.os.isWindows() && platform.os.version.major >= 10);
     assert.eq(true, scrollBarWidth > 5 && scrollBarWidth < 50 || (noVisibleScrollbarBrowser && scrollBarWidth === 0), 'scroll bar width, got=' + scrollBarWidth);
 
     scrollCheck(0, 0, 0, 0, doc, 'start pos');
