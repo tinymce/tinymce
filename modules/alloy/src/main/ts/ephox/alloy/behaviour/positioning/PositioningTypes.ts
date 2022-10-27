f8e17b8d6da720893a54549773482cdac16b0ffb
Morgan Smith <morgan.smith@tiny.cloud>
2022-10-28 00:16:32 +1000

    TINY-9226: Streamlining PositionApis with more consistent naming and less thunking

--- modules/alloy/src/main/ts/ephox/alloy/behaviour/positioning/PositioningTypes.ts
+++ modules/alloy/src/main/ts/ephox/alloy/behaviour/positioning/PositioningTypes.ts
@@ -31,7 +31,7 @@ export interface PlacementDetail {
 export interface PositioningBehaviour extends Behaviour.AlloyBehaviour<PositioningConfigSpec, PositioningConfig> {
   readonly config: (config: PositioningConfigSpec) => Behaviour.NamedConfiguredBehaviour<PositioningConfigSpec, PositioningConfig>;
   readonly position: (component: AlloyComponent, placee: AlloyComponent, spec: PlacementSpec) => void;
-  readonly positionWithinBounds: (component: AlloyComponent, placee: AlloyComponent, spec: PlacementSpec, bounds: Optional<Bounds>) => void;
+  readonly positionWithinBounds: (component: AlloyComponent, placee: AlloyComponent, spec: PlacementSpec, optWithinBounds: Optional<Bounds>) => void;
   readonly getMode: (component: AlloyComponent) => string;
   readonly reset: (component: AlloyComponent, placee: AlloyComponent) => void;
 }
