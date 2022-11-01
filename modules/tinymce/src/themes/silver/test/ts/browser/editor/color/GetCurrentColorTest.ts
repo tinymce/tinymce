aa9221c683cc24d906c2dec6b19296a58f380134
HAFRMO <61967464+HAFRMO@users.noreply.github.com>
2022-11-01 03:30:11 +0100

    TINY-9213: Color picker dialog now defaults to approriate color  (#8191)
    * TINY-9213: Color picker dialog now defaults to approriate color ( defaulting to detecting text color )
    
    * TINY-9213: Improvements.
    
    * TINY-9213: Spelling correction.
    
    * TINY-9213: Improvements to code.
    
    * TINY-9213: updated changelog.
    
    * TINY-9213: Improved tests
    
    * TINY-9213: Now canceling dialogs in tests.

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/color/GetCurrentColorTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/color/GetCurrentColorTest.ts
@@ -20,13 +20,13 @@ describe('browser.tinymce.themes.silver.editor.color.GetCurrentColorTest', () =>
     const editor = hook.editor();
     editor.setContent('<p style="color: blue;">hello <span style="color: red;">world</span></p>');
     TinySelections.setCursor(editor, [ 0, 1, 0 ], 2);
-    assertCurrentColor(editor, 'forecolor', 'should return red', 'red');
+    assertCurrentColor(editor, 'forecolor', 'should return red', '#FF0000');
   });
 
   it('TBA: getCurrentColor should return the first found backcolor, not the parent color', () => {
     const editor = hook.editor();
     editor.setContent('<p style="background-color: red;">hello <span style="background-color: blue;">world</span></p>');
     TinySelections.setCursor(editor, [ 0, 1, 0 ], 2);
-    assertCurrentColor(editor, 'hilitecolor', 'should return blue', 'blue');
+    assertCurrentColor(editor, 'hilitecolor', 'should return blue', '#0000FF');
   });
 });
