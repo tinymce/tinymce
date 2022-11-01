1f6da1d4c47eef419cf776eb65ab18c4756d1fbd
HAFRMO <61967464+HAFRMO@users.noreply.github.com>
2022-11-01 03:30:27 +0100

    TINY-7981: getContent text no longer untoggles radio inputs. (#8220)
    * TINY-7981: getContent text no longer untoggles radio inputs.
    
    * TINY-7981: Fixup

--- modules/tinymce/src/core/test/ts/browser/content/EditorGetContentTextFormatTest.ts
+++ modules/tinymce/src/core/test/ts/browser/content/EditorGetContentTextFormatTest.ts
@@ -10,10 +10,26 @@ describe('browser.tinymce.core.content.EditorGetContentTextFormatTest', () => {
     base_url: '/project/tinymce/js/tinymce'
   }, []);
 
-  it('get text format content should trim zwsp', () => {
+  const assertSelectedRadioButtons = (editor: Editor, nrOfInputs: number, shouldBeSelected: number) => {
+    const total = editor.getBody().querySelectorAll('input').length;
+    const selected = editor.getBody().querySelectorAll('input:checked').length;
+
+    assert.equal(total, nrOfInputs, 'Should have the right amount of inputs');
+    assert.equal(selected, shouldBeSelected, 'Should have exactly one radio button');
+  };
+
+  it('TBA: get text format content should trim zwsp', () => {
     const editor = hook.editor();
     editor.setContent('<p>' + Zwsp.ZWSP + 'a</p>');
     const html = editor.getContent({ format: 'text' });
     assert.equal(html, 'a', 'Should be expected html');
+  });
+
+  it('TINY-7981: inputs should not be deselected', () => {
+    const editor = hook.editor();
+    editor.setContent('<p><input name="group-name" type="radio">Option 1<input checked="checked" name="group-name" type="radio">Option 2<input name="group-name" type="radio">Option 3</p>');
+    const html = editor.getContent({ format: 'text' });
+    assert.equal(html, 'Option 1Option 2Option 3', 'Should be expected text');
+    assertSelectedRadioButtons(editor, 3, 1);
   });
 });
