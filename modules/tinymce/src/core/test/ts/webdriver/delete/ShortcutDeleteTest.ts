6590fa59b8c28f36ef05ed82e037163d3978e97f
Mike Zhu <mkzh.org@gmail.com>
2022-11-17 16:26:22 +1000

    TINY-9302: Move tests out of context block in ShortcutDeleteTest

--- modules/tinymce/src/core/test/ts/webdriver/delete/ShortcutDeleteTest.ts
+++ modules/tinymce/src/core/test/ts/webdriver/delete/ShortcutDeleteTest.ts
@@ -1,5 +1,5 @@
 import { ApproxStructure, RealKeys } from '@ephox/agar';
-import { context, describe, it } from '@ephox/bedrock-client';
+import { describe, it } from '@ephox/bedrock-client';
 import { PlatformDetection } from '@ephox/sand';
 import { TinyAssertions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
 
@@ -10,63 +10,60 @@ describe('webdriver.tinymce.core.delete.ShortcutDeleteTest', () => {
     base_url: '/project/tinymce/js/tinymce',
     indent: false
   }, [], true);
+  const platform = PlatformDetection.detect();
+  const os = platform.os;
+  const browser = platform.browser;
 
-  context('Ranged backspace/delete formatted elements without selection using keyboard shortcuts', () => {
-    const platform = PlatformDetection.detect();
-    const os = platform.os;
-    const browser = platform.browser;
-
-    it('Ctrl/Alt + Backspace a formatted word', async () => {
-      const modifiers = os.isMacOS() ? { alt: true } : { ctrl: true };
-      const editor = hook.editor();
-      editor.setContent('<p><strong><em><span style="text-decoration: underline;">abc</span></em></strong></p>');
-      TinySelections.setCursor(editor, [ 0, 0, 0, 0, 0 ], 3);
-      await RealKeys.pSendKeysOn('iframe => body', [ RealKeys.combo(modifiers, 'Backspace') ]);
-      TinyAssertions.assertContentStructure(editor,
-        ApproxStructure.build((s, str, _arr) => {
-          return s.element('body', {
-            children: [
-              s.element('p', {
-                // firefox preserves formats
-                children: browser.isFirefox()
-                  ? [
-                    s.element('strong', {
-                      children: [
-                        s.element('em', {
-                          children: [
-                            s.element('span', {
-                              attrs: {
-                                style: str.is('text-decoration: underline;')
-                              },
-                              children: [
-                                s.element('span', {
-                                  attrs: {
-                                    'id': str.is('_mce_caret'),
-                                    'data-mce-bogus': str.is('1')
-                                  }
-                                })
-                              ]
-                            })
-                          ]
-                        })
-                      ]
-                    })
-                  ]
-                  : [
-                    s.element('span', {
-                      attrs: {
-                        'id': str.is('_mce_caret'),
-                        'data-mce-bogus': str.is('1')
-                      }
-                    })
-                  ]
-              })
-            ]
-          });
-        })
-      );
-      const selPath = browser.isFirefox() ? [ 0, 0, 0, 0, 0, 0 ] : [ 0, 0, 0 ];
-      TinyAssertions.assertSelection(editor, selPath, 0, selPath, 0);
-    });
+  it('Ctrl/Alt + Backspace a formatted word', async () => {
+    const modifiers = os.isMacOS() ? { alt: true } : { ctrl: true };
+    const editor = hook.editor();
+    editor.setContent('<p><strong><em><span style="text-decoration: underline;">abc</span></em></strong></p>');
+    TinySelections.setCursor(editor, [ 0, 0, 0, 0, 0 ], 3);
+    await RealKeys.pSendKeysOn('iframe => body', [ RealKeys.combo(modifiers, 'Backspace') ]);
+    TinyAssertions.assertContentStructure(editor,
+      ApproxStructure.build((s, str, _arr) => {
+        return s.element('body', {
+          children: [
+            s.element('p', {
+              // firefox preserves formats
+              children: browser.isFirefox()
+                ? [
+                  s.element('strong', {
+                    children: [
+                      s.element('em', {
+                        children: [
+                          s.element('span', {
+                            attrs: {
+                              style: str.is('text-decoration: underline;')
+                            },
+                            children: [
+                              s.element('span', {
+                                attrs: {
+                                  'id': str.is('_mce_caret'),
+                                  'data-mce-bogus': str.is('1')
+                                }
+                              })
+                            ]
+                          })
+                        ]
+                      })
+                    ]
+                  })
+                ]
+                : [
+                  s.element('span', {
+                    attrs: {
+                      'id': str.is('_mce_caret'),
+                      'data-mce-bogus': str.is('1')
+                    }
+                  })
+                ]
+            })
+          ]
+        });
+      })
+    );
+    const selPath = browser.isFirefox() ? [ 0, 0, 0, 0, 0, 0 ] : [ 0, 0, 0 ];
+    TinyAssertions.assertSelection(editor, selPath, 0, selPath, 0);
   });
 });
