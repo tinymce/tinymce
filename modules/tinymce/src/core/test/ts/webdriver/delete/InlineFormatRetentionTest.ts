b36587bb20048d433144fc1322ffcab6e6c7433c
Mike Zhu <mkzh.org@gmail.com>
2022-11-18 13:33:11 +1000

    TINY-9302: Use native backspace/delete in InlineFormatRetentionTest
    - set initial content to two p blocks to not unexpectedly trigger BlockRangeDelete override

--- modules/tinymce/src/core/test/ts/webdriver/delete/InlineFormatRetentionTest.ts
+++ modules/tinymce/src/core/test/ts/webdriver/delete/InlineFormatRetentionTest.ts
@@ -2,10 +2,8 @@ import { RealKeys } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
 import { PlatformDetection } from '@ephox/sand';
 import { TinyAssertions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
-import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
-import * as InlineFormatDelete from 'tinymce/core/delete/InlineFormatDelete';
 
 describe('webdriver.tinymce.core.delete.InlineFormatRetentionTest', () => {
   const hook = TinyHooks.bddSetupLight<Editor>({
@@ -15,28 +13,27 @@ describe('webdriver.tinymce.core.delete.InlineFormatRetentionTest', () => {
 
   const browser = PlatformDetection.detect().browser;
 
-  const doBackspace = (editor: Editor) => {
-    const returnVal = InlineFormatDelete.backspaceDelete(editor, false);
-    returnVal.each((apply) => apply());
-    assert.isTrue(returnVal.isSome(), 'Should return true since the operation should have done something');
+  const doNativeBackspace = async () => {
+    await RealKeys.pSendKeysOn('iframe => body', [ RealKeys.backspace() ]);
   };
 
   it('TINY-9302: Backspace entire selection of block containing underlined text and then typing will produce underlined text will not produce unexpected formats', async () => {
     const editor = hook.editor();
-    editor.setContent('<p><span style="text-decoration: underline;">abc</span></p>');
+    // second paragraph to ensure BlockRangeDelete override not triggered when everything is selected
+    editor.setContent('<p><span style="text-decoration: underline;">abc</span></p><p>&nbsp;</p>');
     TinySelections.setSelection(editor, [ 0, 0, 0 ], 0, [ 0, 0, 0 ], 3);
-    doBackspace(editor);
-    // firefox natively preserves formats
-    TinyAssertions.assertContent(editor, browser.isFirefox() ? '<p><span style="text-decoration: underline;">&nbsp;</span></p>' : '');
+    await doNativeBackspace();
+    // firefox natively preserves formats for block deletion
+    TinyAssertions.assertContent(editor, browser.isFirefox() ? '<p><span style="text-decoration: underline;">&nbsp;</span></p><p>&nbsp;</p>' : '<p>&nbsp;</p><p>&nbsp;</p>');
     await RealKeys.pSendKeysOn('iframe => body', [ RealKeys.text('d') ]);
-    TinyAssertions.assertContent(editor, browser.isFirefox() ? '<p><span style="text-decoration: underline;">d<br></span></p>' : '<p><span style="text-decoration: underline;">d</span></p>');
+    TinyAssertions.assertContent(editor, browser.isFirefox() ? '<p><span style="text-decoration: underline;">d<br></span></p><p>&nbsp;</p>' : '<p><span style="text-decoration: underline;">d</span></p><p>&nbsp;</p>');
   });
 
   it('TINY-9302: Backspace partial selection of underlined text within block then typing will not produce unexpected formats', async () => {
     const editor = hook.editor();
     editor.setContent('<p>a<span style="text-decoration: underline;">bcd</span>e</p>');
     TinySelections.setSelection(editor, [ 0, 1, 0 ], 0, [ 0, 2 ], 1);
-    doBackspace(editor);
+    await doNativeBackspace();
     TinyAssertions.assertContent(editor, '<p>a</p>');
     await RealKeys.pSendKeysOn('iframe => body', [ RealKeys.text('d') ]);
     // chrome and safari disregards caret format when surrounded by unformatted text
