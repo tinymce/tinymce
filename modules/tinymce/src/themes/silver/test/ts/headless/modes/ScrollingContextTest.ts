d8ff1f57e5b6109b601dfa028a394fb9ab3bb0c4
Morgan Smith <morgan.smith@tiny.cloud>
2022-11-21 16:38:24 +1000

    TINY-9226: Tests for ScrollingContext.detect

--- modules/tinymce/src/themes/silver/test/ts/headless/modes/ScrollingContextTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/headless/modes/ScrollingContextTest.ts
@@ -1,35 +1,97 @@
+import { Cursors } from '@ephox/agar';
 import { context, describe, it } from '@ephox/bedrock-client';
-import { SugarElement } from '@ephox/sugar';
+import { Css, InsertAll, SugarElement } from '@ephox/sugar';
 import { assert } from 'chai';
 
 import * as ScrollingContext from 'tinymce/themes/silver/modes/ScrollingContext';
 
 describe('headless.modes.ScrollingContextTest', () => {
   context('isScroller', () => {
-    it('overflow default - not a scroller', () => {
+    it('TINY-9226: overflow default - not a scroller', () => {
       const div = SugarElement.fromHtml('<div>A</div>');
       assert.isFalse(ScrollingContext.isScroller(div), 'Should not be a scroller');
     });
 
-    it('overflow: visible - not a scroller', () => {
+    it('TINY-9226: overflow: visible - not a scroller', () => {
       const div = SugarElement.fromHtml('<div style="overflow: visible;">A</div>');
       assert.isFalse(ScrollingContext.isScroller(div), 'Should not be a scroller');
     });
 
-    it('overflow: auto - a scroller', () => {
+    it('TINY-9226: overflow: auto - a scroller', () => {
       const div = SugarElement.fromHtml('<div style="overflow: auto;">A</div>');
       assert.isTrue(ScrollingContext.isScroller(div), 'Should be a scroller');
     });
 
-    it('overflow: scroll - a scroller', () => {
+    it('TINY-9226: overflow: scroll - a scroller', () => {
       const div = SugarElement.fromHtml('<div style="overflow: scroll;">A</div>');
       assert.isTrue(ScrollingContext.isScroller(div), 'Should be a scroller');
     });
   });
 
-  // context('detect', () => {
+  context('detect', () => {
 
-  // });
+    const makeWith = (nodeName: string, styles: Record<string, string>, children: SugarElement<Node>[]): SugarElement<Node> => {
+      const parent = SugarElement.fromTag(nodeName);
+      Css.setAll(parent, styles);
+      InsertAll.append(parent, children);
+      return parent;
+    };
+
+    it('TINY-9226: no scrolling contexts', () => {
+      const situation = makeWith('div', { }, [
+        makeWith('div', { }, [
+          makeWith('div', { }, [
+            makeWith('div', { }, [
+
+            ])
+          ])
+        ])
+      ]);
+
+      const target = Cursors.follow(situation, [ 0, 0, 0 ]).getOrDie() as SugarElement<HTMLElement>;
+
+      const optActual = ScrollingContext.detect(target);
+      assert.isTrue(optActual.isNone(), 'There should be no scrolling context');
+    });
+
+    it('TINY-9226: several scrolling contexts', () => {
+      const situation = makeWith('div', {
+        overflow: 'auto'
+      }, [
+        makeWith('div', {
+          overflow: 'auto'
+        }, [
+          makeWith('div', {
+            overflow: 'scroll'
+          }, [
+            makeWith('div', { }, [
+
+            ])
+          ])
+        ])
+      ]);
+
+      const follow = (path: number[]): SugarElement<HTMLElement> =>
+        Cursors.follow(situation, path).getOrDie() as SugarElement<HTMLElement>;
+
+      const target = follow([ 0, 0, 0 ]);
+
+      const optActual = ScrollingContext.detect(target);
+      optActual.fold(
+        () => assert.fail('Should have found a scrolling context'),
+        (actual) => assert.deepEqual(
+          actual,
+          {
+            element: follow([ 0, 0 ]),
+            others: [
+              follow([ 0 ]),
+              follow([ ])
+            ]
+          }
+        )
+      );
+    });
+  });
 
   // context('getBoundsFrom', () => {
 
