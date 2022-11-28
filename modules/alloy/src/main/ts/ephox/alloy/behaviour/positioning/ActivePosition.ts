b98f3d7c73aeff1072026f19853c0d67a644f0cb
Aleksandr Yakunichev <hi@ya.codes>
2022-11-28 22:11:00 +0100

    TINY-9148: Add a comment about width: 100% to ActivePosition.ts

--- modules/alloy/src/main/ts/ephox/alloy/behaviour/positioning/ActivePosition.ts
+++ modules/alloy/src/main/ts/ephox/alloy/behaviour/positioning/ActivePosition.ts
@@ -6,6 +6,9 @@ const getStyles = (positioningType: PositioningType): Record<string, string> => 
   if (positioningType === 'relative') {
     return { position: 'relative' };
   } else if (positioningType === 'absolute') {
+    // Elements with `position: absolute` and without any content collapse, 
+    // so theirs width equals 0. That differs from `position: relative` behaviour,
+    // which preserves containers width.
     return { position: 'absolute', width: '100%' };
   } else {
     return { };
