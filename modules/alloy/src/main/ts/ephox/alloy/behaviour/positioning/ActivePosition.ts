64cc31f03547bfb128a689cb5f4d8e0825cf83a1
Aleksandr Yakunichev <hi@ya.codes>
2022-11-28 22:27:07 +0100

    TINY-9148: Fix eslint errors

--- modules/alloy/src/main/ts/ephox/alloy/behaviour/positioning/ActivePosition.ts
+++ modules/alloy/src/main/ts/ephox/alloy/behaviour/positioning/ActivePosition.ts
@@ -1,12 +1,12 @@
 import { DomDefinitionDetail } from '../../dom/DomDefinition';
 import * as DomModification from '../../dom/DomModification';
-import { PositioningConfig, PositioningType  } from './PositioningTypes';
+import { PositioningConfig, PositioningType } from './PositioningTypes';
 
 const getStyles = (positioningType: PositioningType): Record<string, string> => {
   if (positioningType === 'relative') {
     return { position: 'relative' };
   } else if (positioningType === 'absolute') {
-    // Elements with `position: absolute` and without any content collapse, 
+    // Elements with `position: absolute` and without any content collapse,
     // so theirs width equals 0. That differs from `position: relative` behaviour,
     // which preserves containers width.
     return { position: 'absolute', width: '100%' };
@@ -15,7 +15,7 @@ const getStyles = (positioningType: PositioningType): Record<string, string> => 
   }
 };
 
-const exhibit = (base: DomDefinitionDetail, posConfig: PositioningConfig): DomModification.DomModification => 
+const exhibit = (base: DomDefinitionDetail, posConfig: PositioningConfig): DomModification.DomModification =>
   DomModification.nu({
     classes: [ ],
     styles: getStyles(posConfig.usePositioningType()),
