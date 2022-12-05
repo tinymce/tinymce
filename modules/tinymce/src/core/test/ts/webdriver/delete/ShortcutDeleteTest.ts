f3fc89c18cb24934bbe56147d4f33a8264673ee9
Mike Zhu <mkzh.org@gmail.com>
2022-12-05 16:43:48 +1000

    TINY-9302: Add nested caret test in ShortcutDeleteTest
    - add 'data-mce-type' to carets in ApproxStructure assertions
    - add zwsp as child to carets in ApproxStructure assertions
    - replace numerical offsets with .length where applicable in setCursor

--- modules/tinymce/src/core/test/ts/webdriver/delete/ShortcutDeleteTest.ts
+++ modules/tinymce/src/core/test/ts/webdriver/delete/ShortcutDeleteTest.ts
@@ -5,6 +5,7 @@ import { TinyAssertions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
 
 import Editor from 'tinymce/core/api/Editor';
 import * as InlineFormatDelete from 'tinymce/core/delete/InlineFormatDelete';
+import * as Zwsp from 'tinymce/core/text/Zwsp';
 
 type BackspaceDeleteModifier = { ctrl: true } | { alt: true } | { meta: true };
 
@@ -29,7 +30,7 @@ describe('webdriver.tinymce.core.delete.ShortcutDeleteTest', () => {
     InlineFormatDelete.refreshCaret(editor);
   };
 
-  const assertStructureAndSelectionFormattedWord = (editor: Editor): void => {
+  const assertStructureAndCursorFormattedWord = (editor: Editor): void => {
     TinyAssertions.assertContentStructure(editor,
       ApproxStructure.build((s, str, _arr) => {
         return s.element('body', {
@@ -50,8 +51,12 @@ describe('webdriver.tinymce.core.delete.ShortcutDeleteTest', () => {
                               s.element('span', {
                                 attrs: {
                                   'id': str.is('_mce_caret'),
-                                  'data-mce-bogus': str.is('1')
-                                }
+                                  'data-mce-bogus': str.is('1'),
+                                  'data-mce-type': str.is('format-caret')
+                                },
+                                children: [
+                                  s.text(str.is(Zwsp.ZWSP))
+                                ]
                               })
                             ]
                           })
@@ -64,8 +69,12 @@ describe('webdriver.tinymce.core.delete.ShortcutDeleteTest', () => {
                   s.element('span', {
                     attrs: {
                       'id': str.is('_mce_caret'),
-                      'data-mce-bogus': str.is('1')
-                    }
+                      'data-mce-bogus': str.is('1'),
+                      'data-mce-type': str.is('format-caret')
+                    },
+                    children: [
+                      s.text(str.is(Zwsp.ZWSP))
+                    ]
                   })
                 ]
             })
@@ -74,7 +83,48 @@ describe('webdriver.tinymce.core.delete.ShortcutDeleteTest', () => {
       })
     );
     const selPath = browser.isFirefox() ? [ 0, 0, 0, 0, 0, 0 ] : [ 0, 0, 0 ];
-    TinyAssertions.assertSelection(editor, selPath, 0, selPath, 0);
+    TinyAssertions.assertCursor(editor, selPath, 0);
+  };
+
+  const assertStructureAndCursorFormattedCaret = (editor: Editor): void => {
+    TinyAssertions.assertContentStructure(editor,
+      ApproxStructure.build((s, str, _arr) => {
+        return s.element('body', {
+          children: [
+            s.element('p', {
+              children: [
+                s.element('span', {
+                  attrs: {
+                    'id': str.is('_mce_caret'),
+                    'data-mce-bogus': str.is('1'),
+                    'data-mce-type': str.is('format-caret')
+                  },
+                  children: [
+                    s.element('strong', {
+                      children: [
+                        s.element('em', {
+                          children: [
+                            s.element('span', {
+                              attrs: {
+                                style: str.is('text-decoration: underline;')
+                              },
+                              children: [
+                                s.text(str.is(Zwsp.ZWSP))
+                              ]
+                            })
+                          ]
+                        })
+                      ]
+                    })
+                  ]
+                })
+              ]
+            })
+          ]
+        });
+      })
+    );
+    TinyAssertions.assertCursor(editor, [ 0, 0, 0, 0, 0, 0 ], 0);
   };
 
   const assertContentDeletionThenTyping = (editor: Editor): void =>
@@ -87,9 +137,9 @@ describe('webdriver.tinymce.core.delete.ShortcutDeleteTest', () => {
   it('Ctrl/Alt + Backspace at the end of a formatted word', async () => {
     const editor = hook.editor();
     editor.setContent('<p><strong><em><span style="text-decoration: underline;">abc</span></em></strong></p>');
-    TinySelections.setCursor(editor, [ 0, 0, 0, 0, 0 ], 3);
+    TinySelections.setCursor(editor, [ 0, 0, 0, 0, 0 ], 'abc'.length);
     await doBackspaceDelete(editor, 'Backspace', ctrlModifier);
-    assertStructureAndSelectionFormattedWord(editor);
+    assertStructureAndCursorFormattedWord(editor);
   });
 
   it('Ctrl/Alt + Delete at the start of a formatted word', async () => {
@@ -97,13 +147,13 @@ describe('webdriver.tinymce.core.delete.ShortcutDeleteTest', () => {
     editor.setContent('<p><strong><em><span style="text-decoration: underline;">abc</span></em></strong></p>');
     TinySelections.setCursor(editor, [ 0, 0, 0, 0, 0 ], 0);
     await doBackspaceDelete(editor, 'Delete', ctrlModifier);
-    assertStructureAndSelectionFormattedWord(editor);
+    assertStructureAndCursorFormattedWord(editor);
   });
 
   it('TINY-9302: Ctrl/Alt + Backspace at the end of an underlined word then typing will not produce unexpected formats', async () => {
     const editor = hook.editor();
     editor.setContent('<p><span style="text-decoration: underline;">abc</span></p>');
-    TinySelections.setCursor(editor, [ 0, 0, 0 ], 3);
+    TinySelections.setCursor(editor, [ 0, 0, 0 ], 'abc'.length);
     await doBackspaceDelete(editor, 'Backspace', ctrlModifier);
     await RealKeys.pSendKeysOn('iframe => body', [ RealKeys.text('d') ]);
     assertContentDeletionThenTyping(editor);
@@ -116,5 +166,50 @@ describe('webdriver.tinymce.core.delete.ShortcutDeleteTest', () => {
     await doBackspaceDelete(editor, 'Delete', ctrlModifier);
     await RealKeys.pSendKeysOn('iframe => body', [ RealKeys.text('d') ]);
     assertContentDeletionThenTyping(editor);
+  });
+
+  it('TINY-9302: Nested carets are not created when an ancestor caret already exists', async () => {
+    const editor = hook.editor();
+    editor.setContent('<p>abc</p>');
+    TinySelections.setCursor(editor, [ 0, 0 ], 'abc'.length);
+    await doBackspaceDelete(editor, 'Backspace', ctrlModifier);
+    TinyAssertions.assertContentStructure(editor,
+      ApproxStructure.build((s, str, _arr) => {
+        return s.element('body', {
+          children: [
+            s.element('p', {
+              children: [
+                s.element('span', {
+                  attrs: {
+                    'id': str.is('_mce_caret'),
+                    'data-mce-bogus': str.is('1'),
+                    'data-mce-type': str.is('format-caret')
+                  },
+                  children: [
+                    s.text(str.is(Zwsp.ZWSP))
+                  ]
+                })
+              ]
+            })
+          ]
+        });
+      })
+    );
+    TinyAssertions.assertCursor(editor, [ 0, 0, 0 ], 0);
+
+    // apply formats within caret so that it becomes an ancestor caret relative to
+    // selection start after deletion
+    editor.execCommand('Underline');
+    editor.execCommand('Italic');
+    editor.execCommand('Bold');
+    assertStructureAndCursorFormattedCaret(editor);
+
+    // type a character for deletion
+    await RealKeys.pSendKeysOn('iframe => body', [ RealKeys.text('d') ]);
+    TinyAssertions.assertContent(editor, '<p><strong><em><span style="text-decoration: underline;">d</span></em></strong></p>');
+
+    // assert that a new caret is not created at cursor after deletion
+    await doBackspaceDelete(editor, 'Backspace', ctrlModifier);
+    assertStructureAndCursorFormattedCaret(editor);
   });
 });
