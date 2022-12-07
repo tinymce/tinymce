4648899294d6952b0ab96918aee60cf6871bacb5
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2022-12-07 10:42:14 +0100

    TINY-9429: refactor and add some keybord actions

--- modules/tinymce/src/themes/silver/main/ts/ui/core/complex/BespokeNumberInput.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/core/complex/BespokeNumberInput.ts
@@ -15,14 +15,17 @@ interface BespokeSelectApi {
 }
 
 const createBespokeNumberInput = (editor: Editor, _backstage: UiFactoryBackstage, spec: NumberInputSpec): SketchSpec => {
-  const currentValue = Cell('');
   const currentComp: Cell<Optional<AlloyComponent>> = Cell(Optional.none());
+
+  const getValueFromCurrentComp = (comp: Cell<Optional<AlloyComponent>>): string => comp
+    .get().map((alloyComp) => Representing.getValue(alloyComp)).getOr('');
 
   const onSetup = onSetupEvent(editor, 'NodeChange', (api: BespokeSelectApi) => {
     const comp = api.getComponent();
     currentComp.set(Optional.some(comp));
     spec.updateText(comp);
   });
+
   const getApi = (comp: AlloyComponent): BespokeSelectApi => ({ getComponent: Fun.constant(comp) });
   const editorOffCell = Cell(Fun.noop);
 
@@ -31,7 +34,7 @@ const createBespokeNumberInput = (editor: Editor, _backstage: UiFactoryBackstage
   const isValidValue = (value: number): boolean => value >= 0;
 
   const changeValue = (f: (v: number, step: number) => number): void => {
-    const text = currentValue.get();
+    const text = getValueFromCurrentComp(currentComp);
     const value = parseFloat(text.match(/^[\d\.]+/)?.join('') ?? '0');
     const unitRegexp = new RegExp(`(?<=${value})\\D+$`);
     const unit = text.match(unitRegexp)?.join('') ?? '';
@@ -39,9 +42,11 @@ const createBespokeNumberInput = (editor: Editor, _backstage: UiFactoryBackstage
     const newValueWithUnit = `${isValidValue(newValue) ? newValue : value}${unit}`;
 
     spec.onAction(newValueWithUnit);
-    currentValue.set(newValueWithUnit);
     currentComp.get().each((comp) => Representing.setValue(comp, newValueWithUnit));
   };
+
+  const decrease = () => changeValue((n, s) => n - s);
+  const increase = () => changeValue((n, s) => n + s);
 
   const buttonStyles = {
     'width': '20px',
@@ -64,8 +69,9 @@ const createBespokeNumberInput = (editor: Editor, _backstage: UiFactoryBackstage
           tag: 'button',
           styles: buttonStyles,
           innerHtml: '-',
+          classes: [ 'minus' ]
         },
-        action: () => changeValue((n, s) => n - s)
+        action: decrease
       }),
       Input.sketch({
         inputStyles: {
@@ -80,21 +86,42 @@ const createBespokeNumberInput = (editor: Editor, _backstage: UiFactoryBackstage
           AddEventsBehaviour.config('menubutton-update-display-text', [
             AlloyEvents.run<UpdateMenuTextEvent>(updateMenuText, (comp, se) => {
               Representing.setValue(comp, se.event.text);
-              currentValue.set(se.event.text);
+            }),
+            AlloyEvents.run(NativeEvents.focusout(), (_comp, se) => {
+              spec.onAction(se.event.target.dom.value);
             }),
             AlloyEvents.run(NativeEvents.change(), (_comp, se) => {
               spec.onAction(se.event.target.dom.value);
             })
-          ])
+          ]),
+          Keying.config({
+            mode: 'special',
+            onEnter: (comp) => {
+              comp.element.dom.blur();
+              return Optional.some(true);
+            },
+            onUp: (comp) => {
+              increase();
+              // TOFIX: now it preserve the focus but it put the selection at the end of the input
+              Focus.focusInside(comp.element);
+              return Optional.some(true);
+            },
+            onDown: (comp) => {
+              decrease();
+              Focus.focusInside(comp.element);
+              return Optional.some(true);
+            }
+          })
         ])
       }),
       Button.sketch({
         dom: {
           tag: 'button',
           styles: buttonStyles,
-          innerHtml: '+'
+          innerHtml: '+',
+          classes: [ 'plus' ]
         },
-        action: () => changeValue((n, s) => n + s)
+        action: increase
       })
     ],
     behaviours: Behaviour.derive([
