b4e705394f6c25a1825cc53f868b76697721e192
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2022-12-12 08:04:47 +0100

    TINY-9429: refactor test

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/toolbar/NumberInputTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/toolbar/NumberInputTest.ts
@@ -1,7 +1,7 @@
-import { UiControls } from '@ephox/agar';
+import { FocusTools, Keys, UiControls } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
-import { SugarElement } from '@ephox/sugar';
-import { TinyAssertions, TinyHooks, TinySelections, TinyUiActions } from '@ephox/wrap-mcagar';
+import { SugarElement, SugarShadowDom } from '@ephox/sugar';
+import { TinyAssertions, TinyDom, TinyHooks, TinySelections, TinyUiActions } from '@ephox/wrap-mcagar';
 
 import Editor from 'tinymce/core/api/Editor';
 
@@ -10,24 +10,6 @@ describe('browser.tinymce.themes.silver.throbber.ThrobberEditorTest', () => {
     base_url: '/project/tinymce/js/tinymce',
     toolbar: [ 'undo', 'fontsizeinput', 'redo' ]
   }, []);
-
-  const pressEnter = (editor: Editor, target: HTMLElement, _evt?: any) => {
-
-    target.dispatchEvent(new KeyboardEvent('keydown', {
-      code: 'Enter',
-      key: 'Enter',
-      charCode: 13,
-      keyCode: 13,
-      view: window,
-      bubbles: true
-    }));
-
-    // const dom = editor.dom;
-    // evt = Tools.extend({ keyCode: 13, shiftKey: false }, evt);
-    // dom.dispatch(target, 'keydown', evt);
-    // dom.dispatch(target, 'keypress', evt);
-    // dom.dispatch(target, 'keyup', evt);
-  };
 
   it('TINY-9429: plus and minus should increase and decrease font size of the current selection', () => {
     const editor = hook.editor();
@@ -41,7 +23,7 @@ describe('browser.tinymce.themes.silver.throbber.ThrobberEditorTest', () => {
     TinyAssertions.assertContent(editor, '<p>a<span style="font-size: 16px;">b</span>c</p>');
   });
 
-  it('TINY-9429: should be possible to change the font size from the input', () => {
+  it('TINY-9429: should be possible to change the font size from the input', async () => {
     const editor = hook.editor();
     editor.setContent('<p>abc</p>');
     TinySelections.setSelection(editor, [ 0, 0 ], 1, [ 0, 0 ], 2);
@@ -50,7 +32,10 @@ describe('browser.tinymce.themes.silver.throbber.ThrobberEditorTest', () => {
 
     const input: SugarElement<HTMLInputElement> = TinyUiActions.clickOnToolbar(editor, '.tox-number-input input');
     UiControls.setValue(input, '15px');
-    pressEnter(editor, input.dom);
+    const root = SugarShadowDom.getRootNode(TinyDom.targetElement(editor));
+    FocusTools.setFocus(root, '.tox-number-input input');
+    await FocusTools.pTryOnSelector('Focus should start on', root, '.tox-number-input input');
+    TinyUiActions.keystroke(editor, Keys.enter(), { shiftKey: false });
 
     TinyAssertions.assertContent(editor, '<p>a<span style="font-size: 15px;">b</span>c</p>');
   });
