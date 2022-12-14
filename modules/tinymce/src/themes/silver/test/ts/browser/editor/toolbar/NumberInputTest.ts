3e7d2f7844921999255a0143dba5061b819c4a87
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2022-12-14 14:54:40 +0100

    TINY-9429: test refactor

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/toolbar/NumberInputTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/toolbar/NumberInputTest.ts
@@ -13,19 +13,19 @@ describe('browser.tinymce.themes.silver.throbber.NumberInputTest', () => {
 
   it('TINY-9429: plus and minus should increase and decrease font size of the current selection', () => {
     const editor = hook.editor();
-    editor.setContent('<p>abc</p>');
+    editor.setContent('<p style="font-size: 16px;">abc</p>');
     TinySelections.setSelection(editor, [ 0, 0 ], 1, [ 0, 0 ], 2);
 
     TinyUiActions.clickOnToolbar(editor, '.tox-number-input .plus');
-    TinyAssertions.assertContent(editor, '<p>a<span style="font-size: 17px;">b</span>c</p>');
+    TinyAssertions.assertContent(editor, '<p style="font-size: 16px;">a<span style="font-size: 17px;">b</span>c</p>');
 
     TinyUiActions.clickOnToolbar(editor, '.tox-number-input .minus');
-    TinyAssertions.assertContent(editor, '<p>a<span style="font-size: 16px;">b</span>c</p>');
+    TinyAssertions.assertContent(editor, '<p style="font-size: 16px;">a<span style="font-size: 16px;">b</span>c</p>');
   });
 
   it('TINY-9429: should be possible to change the font size from the input', async () => {
     const editor = hook.editor();
-    editor.setContent('<p>abc</p>');
+    editor.setContent('<p style="font-size: 16px;">abc</p>');
     TinySelections.setSelection(editor, [ 0, 0 ], 1, [ 0, 0 ], 2);
 
     TinyUiActions.clickOnToolbar(editor, '.tox-number-input input');
@@ -37,41 +37,41 @@ describe('browser.tinymce.themes.silver.throbber.NumberInputTest', () => {
     await FocusTools.pTryOnSelector('Focus should should be on input', root, '.tox-number-input input');
     TinyUiActions.keystroke(editor, Keys.enter());
 
-    TinyAssertions.assertContent(editor, '<p>a<span style="font-size: 15px;">b</span>c</p>');
+    TinyAssertions.assertContent(editor, '<p style="font-size: 16px;">a<span style="font-size: 15px;">b</span>c</p>');
   });
 
   it('TINY-9429: when input is selected arrow up should increase the size and arrow down decrease it', async () => {
     const editor = hook.editor();
     const root = SugarShadowDom.getRootNode(TinyDom.targetElement(editor));
-    editor.setContent('<p>abc</p>');
+    editor.setContent('<p style="font-size: 16px;">abc</p>');
     TinySelections.setSelection(editor, [ 0, 0 ], 1, [ 0, 0 ], 2);
 
     FocusTools.setFocus(root, '.tox-number-input input');
     await FocusTools.pTryOnSelector('Focus should should be on input', root, '.tox-number-input input');
     TinyUiActions.keystroke(editor, Keys.up());
-    TinyAssertions.assertContent(editor, '<p>a<span style="font-size: 17px;">b</span>c</p>');
+    TinyAssertions.assertContent(editor, '<p style="font-size: 16px;">a<span style="font-size: 17px;">b</span>c</p>');
 
     FocusTools.setFocus(root, '.tox-number-input input');
     await FocusTools.pTryOnSelector('Focus should should be on input', root, '.tox-number-input input');
     TinyUiActions.keystroke(editor, Keys.down());
-    TinyAssertions.assertContent(editor, '<p>a<span style="font-size: 16px;">b</span>c</p>');
+    TinyAssertions.assertContent(editor, '<p style="font-size: 16px;">a<span style="font-size: 16px;">b</span>c</p>');
 
     const input: SugarElement<HTMLInputElement> = TinyUiActions.clickOnToolbar(editor, '.tox-number-input input');
     UiControls.setValue(input, '2em');
     FocusTools.setFocus(root, '.tox-number-input input');
     await FocusTools.pTryOnSelector('Focus should should be on input', root, '.tox-number-input input');
     TinyUiActions.keystroke(editor, Keys.enter());
-    TinyAssertions.assertContent(editor, '<p>a<span style="font-size: 2em;">b</span>c</p>');
+    TinyAssertions.assertContent(editor, '<p style="font-size: 16px;">a<span style="font-size: 2em;">b</span>c</p>');
 
     FocusTools.setFocus(root, '.tox-number-input input');
     await FocusTools.pTryOnSelector('Focus should should be on input', root, '.tox-number-input input');
     TinyUiActions.keystroke(editor, Keys.up());
-    TinyAssertions.assertContent(editor, '<p>a<span style="font-size: 2.1em;">b</span>c</p>');
+    TinyAssertions.assertContent(editor, '<p style="font-size: 16px;">a<span style="font-size: 2.1em;">b</span>c</p>');
 
     FocusTools.setFocus(root, '.tox-number-input input');
     await FocusTools.pTryOnSelector('Focus should should be on input', root, '.tox-number-input input');
     TinyUiActions.keystroke(editor, Keys.down());
-    TinyAssertions.assertContent(editor, '<p>a<span style="font-size: 2em;">b</span>c</p>');
+    TinyAssertions.assertContent(editor, '<p style="font-size: 16px;">a<span style="font-size: 2em;">b</span>c</p>');
   });
 
   it('TINY-9429: should navigate correclty via keyboard', async () => {
