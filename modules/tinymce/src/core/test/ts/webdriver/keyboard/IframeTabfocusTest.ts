47b0caa95f6b7d0d4806c528e46e70d41150317e
tiny-ben-tran <ben.tran@tiny.cloud>
2023-01-06 08:03:35 +1030

    Update modules/tinymce/src/core/test/ts/webdriver/keyboard/IframeTabfocusTest.ts
    Co-authored-by: spocke <spocke@moxiecode.com>

--- modules/tinymce/src/core/test/ts/webdriver/keyboard/IframeTabfocusTest.ts
+++ modules/tinymce/src/core/test/ts/webdriver/keyboard/IframeTabfocusTest.ts
@@ -39,7 +39,7 @@ describe('webdriver.tinymce.core.keyboard.IframeTabfocusTest', () => {
   });
 
   context('Highlight editor content area on focus', () => {
-    const asssertIsHighlighted = (editor: Editor) => assert.isTrue(Class.has(TinyDom.container(editor), 'tox-edit-focus'), 'Editor container should has tox-edit-focus class');
+    const assertIsHighlighted = (editor: Editor) => assert.isTrue(Class.has(TinyDom.container(editor), 'tox-edit-focus'), 'Editor container should has tox-edit-focus class');
     const asssertIsNotHighlighted = (editor: Editor) => assert.isFalse(Class.has(TinyDom.container(editor), 'tox-edit-focus'), 'Editor container should not has tox-edit-focus class');
 
     const hook = TinyHooks.bddSetupLight<Editor>({
