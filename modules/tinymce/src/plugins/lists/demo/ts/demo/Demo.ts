094c870f11c8b2e7f7c2d061c4785f10cb23916d
spocke <spocke@moxiecode.com>
2023-01-13 14:00:03 +0100

    TINY-9458: Editable lists in noneditable root (#8373)
    * TINY-9458: list operations should not apply to lists in a noneditable root
    
    * TINY-9458: updated changelog

--- modules/tinymce/src/plugins/lists/demo/ts/demo/Demo.ts
+++ modules/tinymce/src/plugins/lists/demo/ts/demo/Demo.ts
@@ -5,7 +5,7 @@ declare let tinymce: TinyMCE;
 tinymce.init({
   selector: 'textarea.tinymce',
   plugins: 'lists code',
-  toolbar: 'numlist bullist | code',
+  toolbar: 'numlist bullist | outdent indent | code',
   height: 600,
   contextmenu: 'lists'
 });
