98b40b5fd44881c757c681e2855006cd2f69bdd6
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2023-02-06 13:43:42 +0100

    TINY-9523: add test

---
+++ modules/tinymce/src/themes/silver/test/ts/browser/view/ViewButtonsTest.ts
@@ -0,0 +1,108 @@
+import { TestStore } from '@ephox/agar';
+import { context, describe, it } from '@ephox/bedrock-client';
+import { Arr, Fun } from '@ephox/katamari';
+import { TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
+
+import Editor from 'tinymce/core/api/Editor';
+
+describe('browser.tinymce.themes.silver.view.ViewButtonsTest', () => {
+  context('Iframe mode', () => {
+    const store = TestStore();
+    const hook = TinyHooks.bddSetup<Editor>({
+      base_url: '/project/tinymce/js/tinymce',
+      toolbar_mode: 'floating',
+      toolbar: Arr.range(10, Fun.constant('bold | italic ')).join(''),
+      width: 500,
+      setup: (editor: Editor) => {
+        editor.ui.registry.addView('myview1', {
+          buttons: [
+            {
+              type: 'group',
+              buttons: [
+                {
+                  name: 'initAtNormal',
+                  type: 'togglableIconButton',
+                  text: 'init-at-normal',
+                  icon: 'fullscreen',
+                  toggledIcon: 'info',
+                  onAction: (statusApi) => {
+                    store.add(`myview1:init-at-normal-with-${statusApi.getStatus()}`);
+                    const newStatus = statusApi.getStatus() === 'normal' ? 'toggled' : 'normal';
+                    statusApi.setStatus(newStatus);
+                  },
+                  initialStatus: 'normal'
+                },
+                {
+                  name: 'initAtToggled',
+                  type: 'togglableIconButton',
+                  text: 'init-at-toggled',
+                  icon: 'help',
+                  toggledIcon: 'cut',
+                  onAction: (statusApi) => {
+                    store.add(`myview1:init-at-toggled-with-${statusApi.getStatus()}`);
+                    const newStatus = statusApi.getStatus() === 'normal' ? 'toggled' : 'normal';
+                    statusApi.setStatus(newStatus);
+                  },
+                  initialStatus: 'toggled'
+                }
+              ]
+            },
+            {
+              name: 'initAtToggledNotChangable',
+              type: 'togglableIconButton',
+              text: 'init-at-toggled-not-changable',
+              icon: 'help',
+              toggledIcon: 'cut',
+              onAction: (statusApi) => {
+                store.add(`myview1:init-at-toggled-not-changable-with-${statusApi.getStatus()}`);
+              },
+              initialStatus: 'toggled'
+            }
+          ],
+          onShow: (api: any) => {
+            api.getContainer().innerHTML = '<button>myview1</button>';
+            api.getContainer().querySelector('button')?.focus();
+          },
+          onHide: store.adder('myview1:hide')
+        });
+
+        editor.ui.registry.addContextToolbar('test-context', {
+          predicate: (node) => node.nodeName.toLowerCase() === 'img',
+          items: 'bold'
+        });
+      }
+    }, []);
+
+    const toggleView = (name: string) => {
+      const editor = hook.editor();
+      editor.execCommand('ToggleView', false, name);
+    };
+
+    const clickViewButton = (editor: Editor, tooltip: string) => TinyUiActions.clickOnUi(editor, `.tox-view button[title='${tooltip}']`);
+
+    it('TINY-9523: tooglable button api give the current status and alow user to change it', () => {
+      const editor = hook.editor();
+
+      store.clear();
+
+      toggleView('myview1');
+      clickViewButton(editor, 'init-at-normal');
+      clickViewButton(editor, 'init-at-normal');
+
+      clickViewButton(editor, 'init-at-toggled');
+      clickViewButton(editor, 'init-at-toggled');
+
+      clickViewButton(editor, 'init-at-toggled-not-changable');
+      clickViewButton(editor, 'init-at-toggled-not-changable');
+
+      store.assertEq('Should trigger fullscreen', [
+        'myview1:init-at-normal-with-normal',
+        'myview1:init-at-normal-with-toggled',
+        'myview1:init-at-toggled-with-toggled',
+        'myview1:init-at-toggled-with-normal',
+        'myview1:init-at-toggled-not-changable-with-toggled',
+        'myview1:init-at-toggled-not-changable-with-toggled'
+      ]);
+    });
+  });
+});
