c4199207033c4dcf4d7ccfec32201cbf94efd945
Mike Zhu <mkzh.org@gmail.com>
2023-02-06 15:05:17 +1000

    TINY-9244: Add test
    - name: "TINY-9244: Parsed html should be shown when previewing templates"

--- modules/tinymce/src/plugins/template/test/ts/browser/TemplateSanityTest.ts
+++ modules/tinymce/src/plugins/template/test/ts/browser/TemplateSanityTest.ts
@@ -1,7 +1,9 @@
 import { UiFinder } from '@ephox/agar';
 import { afterEach, beforeEach, describe, it } from '@ephox/bedrock-client';
 import { Type } from '@ephox/katamari';
+import { SugarElement } from '@ephox/sugar';
 import { TinyAssertions, TinyHooks } from '@ephox/wrap-mcagar';
+import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
 import Plugin from 'tinymce/plugins/template/Plugin';
@@ -26,6 +28,25 @@ describe('browser.tinymce.plugins.template.TemplateSanityTest', () => {
   afterEach(() => {
     cleanupSettings();
   });
+
+  const waitUntilIframeLoaded = async (dialogEl: SugarElement<Node>): Promise<void> => {
+    await UiFinder.pWaitForState<HTMLIFrameElement>('iframe is loaded', dialogEl, 'iframe', (elm) => {
+      const iframeDoc = elm.dom.contentDocument || elm.dom.contentWindow?.document;
+      return Type.isNonNullable(iframeDoc?.body.firstChild);
+    });
+  };
+
+  const assertPreviewContent = async (dialogEl: SugarElement<Node>, contentSelector: string): Promise<void> => {
+    await waitUntilIframeLoaded(dialogEl);
+    UiFinder.findIn<HTMLIFrameElement>(dialogEl, 'iframe').fold(
+      () => assert.fail('Preview iframe not found'),
+      (iframe) => {
+        const iframeDoc = iframe.dom.contentDocument || iframe.dom.contentWindow?.document;
+        const iframeBody = iframeDoc?.body;
+        UiFinder.exists(SugarElement.fromDom(iframeBody as Node), contentSelector);
+      }
+    );
+  };
 
   it('TBA: Test basic template insertion', async () => {
     const editor = hook.editor();
@@ -52,10 +73,7 @@ describe('browser.tinymce.plugins.template.TemplateSanityTest', () => {
       templates: [{ title: 'a', description: '<strong>b</strong>', url: '/project/tinymce/src/plugins/template/test/html/test_template.html' }]
     });
     await pInsertTemplate(editor, async (dialogEl) => {
-      await UiFinder.pWaitForState<HTMLIFrameElement>('iframe is loaded', dialogEl, 'iframe', (elm) => {
-        const iframeDoc = elm.dom.contentDocument || elm.dom.contentWindow?.document;
-        return Type.isNonNullable(iframeDoc?.body.firstChild);
-      });
+      await waitUntilIframeLoaded(dialogEl);
       UiFinder.exists(dialogEl, 'p:contains("<strong>b</strong>")');
     });
     TinyAssertions.assertContent(editor, '<p><em>this is external</em></p>');
@@ -77,5 +95,19 @@ describe('browser.tinymce.plugins.template.TemplateSanityTest', () => {
     });
     editor.execCommand('mceInsertTemplate', false, '<p>{$first+name}</p>');
     TinyAssertions.assertContent(editor, '<p>Tester</p>');
+  });
+
+  it('TINY-9244: Parsed html should be shown when previewing templates', async () => {
+    const editor = hook.editor();
+    const unparsedHtml = '<img src="error" onerror="console.log(`unparsed html`)">';
+    const parsedHtml = '<p><img src="error"></p>';
+    const parsedPreviewHtmlSelector = 'p > img[src="error"][data-mce-src="error"]';
+    addSettings({
+      templates: [{ title: 'a', description: 'b', content: unparsedHtml }],
+    });
+    await pInsertTemplate(editor, async (dialogEl) => {
+      await assertPreviewContent(dialogEl, parsedPreviewHtmlSelector);
+    });
+    TinyAssertions.assertContent(editor, parsedHtml);
   });
 });
