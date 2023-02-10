b6cbd8cd4eb5f399b6876ba7c9286b6eb18eaf2f
Hamza Benkhaldoun <hamza0867@gmail.com>
2023-02-10 10:12:21 +0100

    TINY-9532: revert change to code demo page

--- modules/tinymce/src/plugins/code/demo/ts/demo/Demo.ts
+++ modules/tinymce/src/plugins/code/demo/ts/demo/Demo.ts
@@ -5,43 +5,8 @@ declare let tinymce: TinyMCE;
 tinymce.init({
   selector: 'textarea.tinymce',
   plugins: 'code',
-  toolbar: 'code tree',
-  height: 600,
-  setup: (ed) => {
-    ed.ui.registry.addButton('tree', {
-      text: 'Tree',
-      onAction: () => {
-        ed.windowManager.open({
-          title: 'Tree',
-          body: {
-            type: 'panel',
-            items: [
-              {
-                type: 'tree',
-                items: [
-                  {
-                    type: 'directory',
-                    title: 'Dir',
-                    id: '1',
-                    children: [{ type: 'leaf', title: 'File 1', id: '2' }]
-                  },
-                  {
-                    type: 'leaf', title: 'File 2', id: '3'
-                  }
-                ]
-              }
-            ]
-          },
-          buttons: [
-            {
-              type: 'cancel',
-              text: 'Cancel'
-            }
-          ]
-        });
-      }
-    });
-  }
+  toolbar: 'code',
+  height: 600
 });
 
 export {};
