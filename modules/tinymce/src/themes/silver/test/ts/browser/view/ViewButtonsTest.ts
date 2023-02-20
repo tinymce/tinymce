f995ab7d8f402e648e0f63dbd750e2bdaed7fae4
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2023-02-20 09:21:37 +0100

    TINY-9523: expand the configuration capability of the view header (#8410)
    * TINY-9523: first implementation of a configurable modal header
    
    * TINY-9523: fix types and schema
    
    * TINY-9523: now is possible to add custom button with incon and text
    
    * TINY-9523: first implementation of group button
    
    * TINY-9523: refactor
    
    * TINY-9523: fix classes
    
    * TINY-9523: fix
    
    * TINY-9523: fix css
    
    * TINY-9523: now ViewButton also takes iconButton
    
    * TINY-9523: add togglable buttons to view bar
    
    * TINY-9523: add button group
    
    * TINY-9523: add style for group
    
    * TINY-9523: add comment
    
    * TINY-9523: remove empty line
    
    * TINY-9523: remove most of the modal changes
    
    * TINY-9523: remove type for dialog
    
    * TINY-9523: type fix
    
    * TINY-9523: now view types are inherit from toolbar
    
    * TINY-9523: changed onAction param from newStatus to statusApi
    
    * TINY-9523: fixes
    
    * TINY-9523: remove useless change
    
    * TINY-9523: remove resolved TODO
    
    * TINY-9523: moved tooglableIconButton in a new file in view dir
    
    * TINY-9523: add missin space at the end
    
    * TINY-9523: add test
    
    * TINY-9523: add togglable button to dialog footer
    
    * TINY-9523: fix
    
    * TINY-9523: add changelog entries
    
    * TINY-9523: simplified togglable icon button interface for view
    
    * TINY-9523: simplified togglable icon button for dialog
    
    * TINY-9523: fix test
    
    * TINY-9523: remove useless if
    
    * TINY-9523: fix test
    
    * TINY-9523: refactor view button
    
    * TINY-9523: fix changelog
    
    * TINY-9523: add view demo
    
    * TINY-9523: refactor type
    
    * TINY-9523: add borderless property
    
    * TINY-9523: fix
    
    * TINY-9523: fix test
    
    * TINY-9523: fix tooltip for `togglableIconButton`
    
    * TINY-9523: fix type for `togglableIconButton`
    
    * TINY-9523: add an empty line at the end of the file
    
    * TINY-9523: remove name from ViewTogglableIconButtonSpec
    
    * TINY-9523: removed `showIconAndText`
    
    * TINY-9523: refactor view and toolbar button API
    
    * TINY-9523: fix
    
    * TINY-9523: now `renderTogglableButton` also render viewNormalButton
    
    * TINY-9523: now active is optional and defaulted to false
    
    * TINY-9523: remove unused export
    
    * TINY-9523: refactoring
    
    * TINY-9523: fix style
    
    * TINY-9523: rename from `togglableButton` to `toggleButton`
    
    * TINY-9523: fix style for button enable
    
    * TINY-9523: fix style

---
+++ modules/tinymce/src/themes/silver/test/ts/browser/view/ViewButtonsTest.ts
@@ -0,0 +1,84 @@
+import { UiFinder } from '@ephox/agar';
+import { context, describe, it } from '@ephox/bedrock-client';
+import { Arr, Fun } from '@ephox/katamari';
+import { TinyDom, TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
+import { assert } from 'chai';
+
+import Editor from 'tinymce/core/api/Editor';
+
+describe('browser.tinymce.themes.silver.view.ViewButtonsTest', () => {
+  context('Iframe mode', () => {
+    const hook = TinyHooks.bddSetup<Editor>({
+      base_url: '/project/tinymce/js/tinymce',
+      toolbar_mode: 'floating',
+      toolbar: Arr.range(10, Fun.constant('bold | italic ')).join(''),
+      width: 500,
+      setup: (editor: Editor) => {
+        let buttonWithToggle = false;
+        editor.ui.registry.addView('myview1', {
+          buttons: [
+            {
+              type: 'group',
+              buttons: [
+                {
+                  type: 'togglebutton',
+                  text: 'button-with-toggle',
+                  tooltip: 'button-with-toggle',
+                  icon: 'cut',
+                  onAction: (api) => {
+                    buttonWithToggle = !buttonWithToggle;
+                    api.setIcon(buttonWithToggle ? 'fullscreen' : 'cut');
+                  }
+                }
+              ]
+            },
+            {
+              type: 'togglebutton',
+              text: 'button-without-toggle',
+              tooltip: 'button-without-toggle',
+              icon: 'help',
+              onAction: Fun.noop
+            }
+          ],
+          onShow: (api: any) => {
+            api.getContainer().innerHTML = '<button>myview1</button>';
+            api.getContainer().querySelector('button')?.focus();
+          },
+          onHide: Fun.noop
+        });
+
+        editor.ui.registry.addContextToolbar('test-context', {
+          predicate: (node) => node.nodeName.toLowerCase() === 'img',
+          items: 'bold'
+        });
+      }
+    }, []);
+
+    const toggleView = (name: string) => {
+      const editor = hook.editor();
+      editor.execCommand('ToggleView', false, name);
+    };
+
+    const clickViewButton = (editor: Editor, tooltip: string) => TinyUiActions.clickOnUi(editor, `.tox-view button[title='${tooltip}']`);
+
+    const getSvg = (editor: Editor, name: string) => UiFinder.findIn<HTMLElement>(TinyDom.container(editor), `.tox-view button[title='${name}'] svg`).getOrDie().dom.innerHTML;
+
+    it('TINY-9523: tooglable button can be toggled with the correct implementation', () => {
+      const editor = hook.editor();
+
+      toggleView('myview1');
+
+      const initialButtonWithToggleButtonSvg = getSvg(editor, 'button-with-toggle');
+      clickViewButton(editor, 'button-with-toggle');
+      assert.notEqual(getSvg(editor, 'button-with-toggle'), initialButtonWithToggleButtonSvg, 'after the first toggle icon should change');
+      clickViewButton(editor, 'button-with-toggle');
+      assert.equal(getSvg(editor, 'button-with-toggle'), initialButtonWithToggleButtonSvg, 'after the second toggle icon should return to the old value');
+
+      const initialbuttonWithoutToggleButtonSvg = getSvg(editor, 'button-without-toggle');
+      clickViewButton(editor, 'button-without-toggle');
+      assert.equal(getSvg(editor, 'button-without-toggle'), initialbuttonWithoutToggleButtonSvg, 'click should not toggle icon');
+      clickViewButton(editor, 'button-without-toggle');
+      assert.equal(getSvg(editor, 'button-without-toggle'), initialbuttonWithoutToggleButtonSvg, 'click should not toggle icon');
+    });
+  });
+});
