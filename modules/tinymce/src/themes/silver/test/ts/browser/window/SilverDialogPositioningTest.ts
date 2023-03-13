91d5f95cdc12ae824ba2516b10a4f6d53a6fd26c
MitchCTiny <89239746+MitchCTiny@users.noreply.github.com>
2023-03-13 12:13:47 +1000

    TINY-9588: Assert dialog within window

--- modules/tinymce/src/themes/silver/test/ts/browser/window/SilverDialogPositioningTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/window/SilverDialogPositioningTest.ts
@@ -1,8 +1,9 @@
 import { UiFinder, Waiter } from '@ephox/agar';
 import { context, describe, it } from '@ephox/bedrock-client';
 import { Arr } from '@ephox/katamari';
-import { SugarBody } from '@ephox/sugar';
+import { SugarBody, SugarElement, Traverse } from '@ephox/sugar';
 import { TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
+import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
 import { Dialog } from 'tinymce/core/api/ui/Ui';
@@ -15,16 +16,20 @@ describe('browser.tinymce.themes.silver.window.SilverDialogPositioningTest', () 
     { label: 'Inline Mode', inline: true },
     { label: 'Iframe Mode', inline: false }
   ], (editorMode) => Arr.each([
+    { label: 'LTR Direction', directionality: 'ltr' },
+    { label: 'RTL Direction', directionality: 'rtl' },
+  ], (editorDirectionality) => Arr.each([
     { label: 'Floating Mode', mode: 'floating' },
     { label: 'Sliding Mode', mode: 'sliding' },
     { label: 'Wrap Mode', mode: 'wrap' }
   ], (toolbarMode) => Arr.each([
-    { label: 'Toolbar Top', location: 'top' },
-    { label: 'Toolbar Bottom', location: 'bottom' }
-  ], (toolbarLocation) => context(`${editorMode.label} ${toolbarMode.label} ${toolbarLocation.label}`, () => {
+    { label: 'Location Top', location: 'top' },
+    { label: 'Location Bottom', location: 'bottom' }
+  ], (toolbarLocation) => context(`${editorMode.label} ${editorDirectionality.label} Toolbar ${toolbarMode.label} ${toolbarLocation.label}`, () => {
     const inlineHook = TinyHooks.bddSetupLight<Editor>({
       base_url: '/project/tinymce/js/tinymce',
       inline: editorMode.inline,
+      directionality: editorDirectionality.directionality,
       toolbar_mode: toolbarMode.mode,
       toolbar_location: toolbarLocation.location,
       toolbar: 'bold italic underline | forecolor backcolor outdent indent',
@@ -61,14 +66,26 @@ describe('browser.tinymce.themes.silver.window.SilverDialogPositioningTest', () 
 
     const openDialog = (editor: Editor, params: WindowParams) => DialogUtils.open(editor, dialogSpec, params);
 
-    it(`TINY-9588: Inline 'toolbar' Dialog Positioning`, async () => {
-      const editor = inlineHook.editor();
+    const getComponent = (selector: string) => {
+      const component = UiFinder.findIn(SugarBody.body(), selector).getOrDie();
+      return Traverse.parent(component).getOr(component) as SugarElement<HTMLElement>;
+    };
 
+    const assertWithinViewableArea = (dialog: SugarElement<HTMLElement>) => {
+      const { left, right, top, bottom } = dialog.dom.getBoundingClientRect();
+      if (left < 0 || right < 0 || top < 0 || bottom < 0) {
+        assert.fail('Dialog is outside of the viewable area');
+      }
+    };
+
+    it(`TINY-9588: Inline toolbar Dialog Positioning`, async () => {
+      const editor = inlineHook.editor();
       const api = openDialog(editor, { inline: 'toolbar' });
       await TinyUiActions.pWaitForDialog(editor);
-      debugger;
+      const dialog = getComponent('.tox-dialog-inline');
+      assertWithinViewableArea(dialog);
       api.close();
       await Waiter.pTryUntil('Wait for dialog to close', () => UiFinder.notExists(SugarBody.body(), '[role="dialog"]'));
     });
-  }))));
+  })))));
 });
