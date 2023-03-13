85341c4ab78d3c114b9a161782359ca9fe10f006
MitchCTiny <89239746+MitchCTiny@users.noreply.github.com>
2023-03-13 13:45:33 +1000

    TINY-9588: Add modal test, remove toolbar mode tests

--- modules/tinymce/src/themes/silver/test/ts/browser/window/SilverDialogPositioningTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/window/SilverDialogPositioningTest.ts
@@ -1,7 +1,7 @@
 import { UiFinder, Waiter } from '@ephox/agar';
 import { context, describe, it } from '@ephox/bedrock-client';
 import { Arr } from '@ephox/katamari';
-import { SugarBody, SugarElement, Traverse } from '@ephox/sugar';
+import { SugarBody, SugarElement } from '@ephox/sugar';
 import { TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
 import { assert } from 'chai';
 
@@ -19,18 +19,13 @@ describe('browser.tinymce.themes.silver.window.SilverDialogPositioningTest', () 
     { label: 'LTR Direction', directionality: 'ltr' },
     { label: 'RTL Direction', directionality: 'rtl' },
   ], (editorDirectionality) => Arr.each([
-    { label: 'Floating Mode', mode: 'floating' },
-    { label: 'Sliding Mode', mode: 'sliding' },
-    { label: 'Wrap Mode', mode: 'wrap' }
-  ], (toolbarMode) => Arr.each([
     { label: 'Location Top', location: 'top' },
     { label: 'Location Bottom', location: 'bottom' }
-  ], (toolbarLocation) => context(`${editorMode.label} ${editorDirectionality.label} Toolbar ${toolbarMode.label} ${toolbarLocation.label}`, () => {
+  ], (toolbarLocation) => context(`${editorMode.label} ${editorDirectionality.label} Toolbar ${toolbarLocation.label}`, () => {
     const inlineHook = TinyHooks.bddSetupLight<Editor>({
       base_url: '/project/tinymce/js/tinymce',
       inline: editorMode.inline,
       directionality: editorDirectionality.directionality,
-      toolbar_mode: toolbarMode.mode,
       toolbar_location: toolbarLocation.location,
       toolbar: 'bold italic underline | forecolor backcolor outdent indent',
       menubar: false
@@ -64,28 +59,25 @@ describe('browser.tinymce.themes.silver.window.SilverDialogPositioningTest', () 
       onSubmit: (api) => api.close(),
     };
 
-    const openDialog = (editor: Editor, params: WindowParams) => DialogUtils.open(editor, dialogSpec, params);
-
-    const getComponent = (selector: string) => {
-      const component = UiFinder.findIn(SugarBody.body(), selector).getOrDie();
-      return Traverse.parent(component).getOr(component) as SugarElement<HTMLElement>;
-    };
-
-    const assertWithinViewableArea = (dialog: SugarElement<HTMLElement>) => {
+    const pTestOpenDialogAssertPosition = async (params: WindowParams) => {
+      const editor = inlineHook.editor();
+      const api = DialogUtils.open(editor, dialogSpec, params);
+      await TinyUiActions.pWaitForDialog(editor);
+      const dialog = UiFinder.findIn(SugarBody.body(), '.tox-dialog').getOrDie() as SugarElement<HTMLElement>;
       const { left, right, top, bottom } = dialog.dom.getBoundingClientRect();
       if (left < 0 || right > window.innerWidth || top < 0 || bottom > window.innerHeight) {
         assert.fail('Dialog is outside of the viewable area');
       }
-    };
-
-    it(`TINY-9588: Inline toolbar Dialog Positioning`, async () => {
-      const editor = inlineHook.editor();
-      const api = openDialog(editor, { inline: 'toolbar' });
-      await TinyUiActions.pWaitForDialog(editor);
-      const dialog = getComponent('.tox-dialog-inline');
-      assertWithinViewableArea(dialog);
       api.close();
       await Waiter.pTryUntil('Wait for dialog to close', () => UiFinder.notExists(SugarBody.body(), '[role="dialog"]'));
-    });
-  })))));
+    };
+
+    it('TINY-9588: Modal dialog positioning', async () =>
+      await pTestOpenDialogAssertPosition({})
+    );
+
+    it('TINY-9588: Inline toolbar dialog positioning', async () =>
+      await pTestOpenDialogAssertPosition({ inline: 'toolbar' })
+    );
+  }))));
 });
