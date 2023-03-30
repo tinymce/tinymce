ef14cdcb1d6e3919c46e5731733aa9fb39be74fe
Mike Zhu <mkzh.org@gmail.com>
2023-03-30 22:11:27 +1000

    TINY-9746: Implement Enter key override as per TINY-9453 investigation

--- modules/tinymce/src/core/main/ts/keyboard/EnterKey.ts
+++ modules/tinymce/src/core/main/ts/keyboard/EnterKey.ts
@@ -1,8 +1,16 @@
+import { Fun, Optional } from '@ephox/katamari';
+import { PlatformDetection } from '@ephox/sand';
+
 import Editor from '../api/Editor';
 import { EditorEvent } from '../api/util/EventDispatcher';
 import VK from '../api/util/VK';
+import { Bookmark } from '../bookmark/BookmarkTypes';
+import * as NodeType from '../dom/NodeType';
 import * as InsertNewLine from '../newline/InsertNewLine';
 import { endTypingLevelIgnoreLocks } from '../undo/TypingState';
+
+const platform = PlatformDetection.detect();
+const isIOSSafari = platform.os.isiOS() && platform.browser.isSafari();
 
 const handleEnterKeyEvent = (editor: Editor, event: EditorEvent<KeyboardEvent>) => {
   if (event.isDefaultPrevented()) {
@@ -18,9 +26,39 @@ const handleEnterKeyEvent = (editor: Editor, event: EditorEvent<KeyboardEvent>) 
 };
 
 const setup = (editor: Editor): void => {
+  let bookmark: Optional<Bookmark> = Optional.none();
+  let shouldOverrideKeyup = false;
+
+  const iOSSafariKeydownOverride = (editor: Editor): void => {
+    const rng = editor.selection.getRng();
+    if (rng.collapsed && NodeType.isText(rng.commonAncestorContainer)) {
+      bookmark = Optional.some(editor.selection.getBookmark());
+      editor.undoManager.add();
+      shouldOverrideKeyup = true;
+    }
+  };
+
+  const iOSSafariKeyupOverride = (editor: Editor, event: EditorEvent<KeyboardEvent>): void => {
+    editor.undoManager.undo();
+    bookmark.fold(Fun.noop, (b) => editor.selection.moveToBookmark(b));
+    handleEnterKeyEvent(editor, event);
+    shouldOverrideKeyup = false;
+    bookmark = Optional.none();
+  };
+
   editor.on('keydown', (event: EditorEvent<KeyboardEvent>) => {
     if (event.keyCode === VK.ENTER) {
-      handleEnterKeyEvent(editor, event);
+      if (isIOSSafari) {
+        iOSSafariKeydownOverride(editor);
+      } else {
+        handleEnterKeyEvent(editor, event);
+      }
+    }
+  });
+
+  editor.on('keyup', (event: EditorEvent<KeyboardEvent>) => {
+    if (event.keyCode === VK.ENTER && shouldOverrideKeyup) {
+      iOSSafariKeyupOverride(editor, event);
     }
   });
 };
