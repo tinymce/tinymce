83f8eb42dc3b37370052bd6386fb7e1e0a6c87b6
Hamza Benkhaldoun <hamza0867@gmail.com>
2023-04-11 15:39:05 +0000

    TINY-9723: Bug with Table Icon in More Drawer

--- modules/alloy/src/main/ts/ephox/alloy/ui/schema/FloatingToolbarButtonSchema.ts
+++ modules/alloy/src/main/ts/ephox/alloy/ui/schema/FloatingToolbarButtonSchema.ts
@@ -66,6 +66,7 @@ const parts: () => PartType.PartTypeAdt[] = Fun.constant([
         toolbarBehaviours: Behaviour.derive([
           Keying.config({
             mode: 'cyclic',
+            selector: '.tox-toolbar__group',
             onEscape: (comp) => {
               AlloyParts.getPart(comp, detail, 'button').each(Focusing.focus);
               // Don't return true here, as we need to allow the sandbox to handle the escape to close the overflow
