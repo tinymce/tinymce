4809a60f0a1ea31d57d26328b813bdf86702d430
Aleksandr Yakunichev <hi@ya.codes>
2023-04-11 19:12:10 +0200

    TINY-9730: Fix esling errors

--- modules/tinymce/src/plugins/accordion/main/ts/core/Actions.ts
+++ modules/tinymce/src/plugins/accordion/main/ts/core/Actions.ts
@@ -1,15 +1,19 @@
-import Editor from 'tinymce/core/api/Editor';
 import { Id } from '@ephox/katamari';
+
+import Editor from 'tinymce/core/api/Editor';
 
 import * as Events from '../api/Events';
 
 const insertAccordion = (editor: Editor): void => {
   const container = editor.selection.getNode();
   if (container.nodeName === 'SUMMARY') {
-    const body = editor.dom.getNext(container, 'div.mce-accordion-body')
-    console.log(body);
-    if (!body) return;
-    if (!body.lastChild) return;
+    const body = editor.dom.getNext(container, 'div.mce-accordion-body');
+    if (!body) {
+      return;
+    }
+    if (!body.lastChild) {
+      return;
+    }
     editor.selection.setCursorLocation(body.lastChild, 1);
   }
 
@@ -19,7 +23,9 @@ const insertAccordion = (editor: Editor): void => {
   editor.insertContent(`<details data-mce-id="${uid}" class="mce-accordion" open="open"><summary class="mce-accordion-summary">${summaryText}</summary><div class="mce-accordion-body"><p>${bodyText}</p></div></details>`);
 
   const details = editor.dom.select(`[data-mce-id="${uid}"]`)[0];
-  if (!details) return;
+  if (!details) {
+    return;
+  }
   details.removeAttribute('data-mce-id');
 
   const summary = editor.dom.select(`summary`, details)[0];
