fe7cd662921eca8325800c19a9e91fc8ed77b6ac
Hamza Benkhaldoun <hamza0867@gmail.com>
2023-04-11 16:06:53 +0000

    TINY-9723: empty commit to retrigger CI

--- modules/tinymce/src/plugins/code/demo/ts/demo/Demo.ts
+++ modules/tinymce/src/plugins/code/demo/ts/demo/Demo.ts
@@ -4,9 +4,13 @@ declare let tinymce: TinyMCE;
 
 tinymce.init({
   selector: 'textarea.tinymce',
-  plugins: 'code',
-  toolbar: 'code',
-  height: 600
+  // plugins: 'code',
+  // toolbar: 'code',
+  plugins: 'a11ychecker advlist advtable anchor autolink autosave charmap advcode codesample directionality emoticons fullscreen help image insertdatetime link lists media mediaembed nonbreaking pagebreak powerpaste preview quickbars save searchreplace table visualblocks visualchars wordcount',
+  toolbar: 'redo undo | styles | bold italic underline strikethrough forecolor backcolor | removeformat alignleft aligncenter alignright alignjustify | bullist numlist indent outdent | fontfamily fontsize | ltr rtl | anchor | link image media table codesample | code fullscreen',
+
+  height: 600,
+  width: 1000
 });
 
 export {};
