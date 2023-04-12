c14cda0661ead541540ad69fdec05f65a9fe3a50
Aleksandr Yakunichev <hi@ya.codes>
2023-04-12 20:01:48 +0200

    TINY-9730: Disable the insert button if the cursor is in SUMMARY element

--- modules/tinymce/src/plugins/accordion/main/ts/ui/Buttons.ts
+++ modules/tinymce/src/plugins/accordion/main/ts/ui/Buttons.ts
@@ -1,9 +1,24 @@
 import Editor from 'tinymce/core/api/Editor';
+import { Menu, Toolbar } from '@ephox/bridge';
+
+const isSummary = (node: Node): node is HTMLElement =>
+  node.nodeName === 'SUMMARY';
+
+const isInSummary = (editor: Editor): boolean => {
+  const node = editor.selection.getNode();
+  return isSummary(node) || Boolean(editor.dom.getParent(node, isSummary));
+};
+
+const onSetup = (editor: Editor) => (buttonApi: Toolbar.ToolbarButtonInstanceApi | Menu.MenuItemInstanceApi) => {
+  const f = () => buttonApi.setEnabled(!isInSummary(editor));
+  editor.on('NodeChange', f);
+  return () => editor.off('NodeChange', f);
+};
 
 const register = (editor: Editor): void => {
   const onAction = () => editor.execCommand('InsertAccordion');
-  editor.ui.registry.addButton('accordion', { icon: 'accordion', text: 'Accordion', onAction });
-  editor.ui.registry.addMenuItem('accordion', { icon: 'accordion', text: 'Accordion', onAction });
+  editor.ui.registry.addButton('accordion', { icon: 'accordion', text: 'Accordion', onSetup: onSetup(editor), onAction });
+  editor.ui.registry.addMenuItem('accordion', { icon: 'accordion', text: 'Accordion', onSetup: onSetup(editor), onAction });
 };
 
 export { register };
