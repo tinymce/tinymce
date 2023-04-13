58854025ba40a293699e17ae38e16f91e2d580a8
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2023-04-13 07:37:52 +0200

    TINY-9754: now fontsizeinput revert to old value instead of setting a default value (#8628)
    * TINY-9754: now inserting an invalid unit in `fontsizeinput` it revert the value to the old one
    
    * TINY-9754: add changelog entry
    
    * TINY-9754: add a default value that is different from 0
    
    * TINY-9754: fix test + typo
    
    * TINY-9754: fix changelog
    
    * TINY-9754: change changelog
    
    * TINY-9754: refactor
    
    * TINY-9754: remove empty line

--- modules/tinymce/src/themes/silver/main/ts/ui/core/complex/BespokeNumberInput.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/core/complex/BespokeNumberInput.ts
@@ -1,10 +1,9 @@
 import { Keys } from '@ephox/agar';
 import { AddEventsBehaviour, AlloyComponent, AlloyEvents, AlloySpec, Behaviour, Button, Focusing, FocusInsideModes, Input, Keying, Memento, NativeEvents, Representing } from '@ephox/alloy';
 import { Arr, Cell, Fun, Id, Optional } from '@ephox/katamari';
-import { Dimension, Focus, SugarElement, Traverse } from '@ephox/sugar';
+import { Focus, SugarElement, Traverse } from '@ephox/sugar';
 
 import Editor from 'tinymce/core/api/Editor';
-import * as Options from 'tinymce/themes/silver/api/Options';
 import { UiFactoryBackstage } from 'tinymce/themes/silver/backstage/Backstage';
 
 import { renderIconFromPack } from '../../button/ButtonSlices';
@@ -34,25 +33,18 @@ const createBespokeNumberInput = (editor: Editor, backstage: UiFactoryBackstage,
 
   const customEvents = Id.generate('custom-number-input-events');
 
-  const isValidValue = (value: number): boolean => value >= 0;
-
   const changeValue = (f: (v: number, step: number) => number, fromInput: boolean, focusBack: boolean): void => {
     const text = getValueFromCurrentComp(currentComp);
-    const parsedText = Dimension.parse(text, [ 'unsupportedLength', 'empty' ]);
-    const value = parsedText.map((res) => res.value).getOr(0);
-    const defaultUnit = Options.getFontSizeInputDefaultUnit(editor);
-    const unit = parsedText.map((res) => res.unit).filter((u) => u !== '').getOr(defaultUnit);
 
-    const newValue = f(value, spec.getConfigFromUnit(unit).step);
-    const newValueWithUnit = `${isValidValue(newValue) ? newValue : value}${unit}`;
+    const newValue = spec.getNewValue(text, f);
 
-    const lenghtDelta = `${value}${unit}`.length - `${newValueWithUnit}`.length;
+    const lenghtDelta = text.length - `${newValue}`.length;
     const oldStart = currentComp.map((comp) => comp.element.dom.selectionStart - lenghtDelta);
     const oldEnd = currentComp.map((comp) => comp.element.dom.selectionEnd - lenghtDelta);
 
-    spec.onAction(newValueWithUnit, focusBack);
+    spec.onAction(newValue, focusBack);
     currentComp.each((comp) => {
-      Representing.setValue(comp, newValueWithUnit);
+      Representing.setValue(comp, newValue);
       if (fromInput) {
         oldStart.each((oldStart) => comp.element.dom.selectionStart = oldStart);
         oldEnd.each((oldEnd) => comp.element.dom.selectionEnd = oldEnd);
