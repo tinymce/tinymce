95a20de7087d18eb2bd6626a0a6b503b8d61ae56
Aleksandr Yakunichev <hi@ya.codes>
2023-04-17 17:05:22 +0200

    TINY-9731: Fix event types exports

--- modules/tinymce/src/plugins/accordion/main/ts/api/Events.ts
+++ modules/tinymce/src/plugins/accordion/main/ts/api/Events.ts
@@ -5,9 +5,9 @@ export type ToggledAccordionEvent = EditorEvent<{ element: HTMLDetailsElement; s
 export type ToggledAllAccordionsEvent = EditorEvent<{ elements: HTMLDetailsElement[]; state: boolean }>;
 
 export const fireToggleAccordionEvent =
-  (editor: Editor, element: HTMLDetailsElement, state: boolean): EditorEvent<ToggledAccordionEvent> =>
+  (editor: Editor, element: HTMLDetailsElement, state: boolean): ToggledAccordionEvent =>
     editor.dispatch('ToggledAccordion', { element, state });
 
 export const fireToggleAllAccordionsEvent =
-  (editor: Editor, elements: HTMLDetailsElement[], state: boolean): EditorEvent<ToggledAllAccordionsEvent> =>
+  (editor: Editor, elements: HTMLDetailsElement[], state: boolean): ToggledAllAccordionsEvent =>
     editor.dispatch('ToggledAllAccordions', { elements, state });
