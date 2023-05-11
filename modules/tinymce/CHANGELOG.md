2e4dadd62e3b7e3b7f1b590e1eeddb45dc490d15
Spocke <spocke@moxiecode.com>
2023-05-11 12:29:05 +0200

    TINY-9818: Added test and changelog item

--- modules/tinymce/CHANGELOG.md
+++ modules/tinymce/CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - New `help_accessibility` option which displays the keyboard shortcut to access the help functionality in the statusbar. #TINY-9379
 - New `accordion` plugin with the `InsertAccordion` command. #TINY-9730
 - New `details_initial_state` and `details_serialized_state` options. #TINY-9732
+- New `document_write` option that initializes the editor iframe using `document.write` instead of `srcdoc`. #TINY-9818
 
 ### Improved
 - Screen readers are now able to announce the highlighted menu item of link comboboxes. #TINY-9280
---
+++ modules/tinymce/src/core/test/ts/browser/init/InitDocumentWriteTest.ts
@@ -0,0 +1,21 @@
+import { describe, it } from '@ephox/bedrock-client';
+import { SelectorFind } from '@ephox/sugar';
+import { TinyDom, TinyHooks } from '@ephox/wrap-mcagar';
+import { assert } from 'chai';
+
+import Editor from 'tinymce/core/api/Editor';
+
+describe('browser.tinymce.core.init.InitDocumentWriteTest', () => {
+  const hook = TinyHooks.bddSetup<Editor>({
+    base_url: '/project/tinymce/js/tinymce',
+    document_write: true
+  }, [], true);
+
+  it('TINY-9818: Should initialize the editor', () => {
+    const editor = hook.editor();
+    const ifr = SelectorFind.descendant<HTMLIFrameElement>(TinyDom.container(editor), 'iframe').getOrDie('Could not find iframe');
+    assert.isEmpty(ifr.dom.srcdoc, 'Should not have srcdoc');
+    SelectorFind.descendant<HTMLLinkElement>(TinyDom.documentElement(editor), 'link').getOrDie('Could not find link');
+  });
+});
+
