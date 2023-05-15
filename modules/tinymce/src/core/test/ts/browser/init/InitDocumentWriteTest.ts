33446a777224a1600b7013bc0277fab017969ece
Spocke <spocke@moxiecode.com>
2023-05-15 09:10:30 +0200

    TINY-9818: Switched to UiFinder

--- modules/tinymce/src/core/test/ts/browser/init/InitDocumentWriteTest.ts
+++ modules/tinymce/src/core/test/ts/browser/init/InitDocumentWriteTest.ts
@@ -1,5 +1,5 @@
+import { UiFinder } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
-import { SelectorFind } from '@ephox/sugar';
 import { TinyDom, TinyHooks } from '@ephox/wrap-mcagar';
 import { assert } from 'chai';
 
@@ -13,9 +13,9 @@ describe('browser.tinymce.core.init.InitDocumentWriteTest', () => {
 
   it('TINY-9818: Should initialize the editor', () => {
     const editor = hook.editor();
-    const ifr = SelectorFind.descendant<HTMLIFrameElement>(TinyDom.container(editor), 'iframe').getOrDie('Could not find iframe');
-    assert.isEmpty(ifr.dom.srcdoc, 'Should not have srcdoc');
-    SelectorFind.descendant<HTMLLinkElement>(TinyDom.documentElement(editor), 'link').getOrDie('Could not find link');
+    const ifr = UiFinder.findIn<HTMLIFrameElement>(TinyDom.container(editor), 'iframe').getOrDie();
+    assert.isEmpty(ifr.dom.srcdoc, 'Should not have srcdoc set to a non empty string');
+    UiFinder.exists(TinyDom.documentElement(editor), 'link');
   });
 });
 
