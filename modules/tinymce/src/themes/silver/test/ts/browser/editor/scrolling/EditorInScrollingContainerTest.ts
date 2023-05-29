255de2b32ddb5fa77cb925d4885c886bfe1fe0a6
shanmen-tiny <shanmen.ng@tiny.cloud>
2023-05-29 08:15:43 +0800

    TINY-9718: Update changelog and remove comment

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/scrolling/EditorInScrollingContainerTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/scrolling/EditorInScrollingContainerTest.ts
@@ -172,9 +172,6 @@ describe('browser.tinymce.themes.silver.editor.scrolling.EditorInScrollingContai
             // The first time it appears, it doesn't get fade-in, so we just look for non-fadeout
             // for now.
             arr.not(ui.editor.stickyHeaderInvisible.className),
-            // TINY-9408: In classic mode, the transition class isn't going away because
-            // there is another transition style clobbering it. Reinstate the `arr.not` check
-            // when the bug is fixed.
             arr.not(ui.editor.stickyHeaderTransitioning.className)
           ]
         })),
@@ -191,9 +188,6 @@ describe('browser.tinymce.themes.silver.editor.scrolling.EditorInScrollingContai
         ApproxStructure.build((s, str, arr) => s.element('div', {
           classes: [
             arr.has(ui.editor.stickyHeaderInvisible.className),
-            // TINY-9408: In classic mode, the transition class isn't going away because
-            // there is another transition style clobbering it. Reinstate the `arr.not` check
-            // when the bug is fixed.
             arr.not(ui.editor.stickyHeaderTransitioning.className)
           ]
         })),
