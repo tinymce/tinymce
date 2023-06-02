f0e7e0244a0d8bad4db8ed0920bb4bd40e164ae4
Mike Zhu <mkzh.org@gmail.com>
2023-06-02 12:20:26 +1000

    TINY-9928: Remove fallback string literal and use loading from English file as fallback
    - add test that fallback is the same as the html loaded from English file

--- modules/tinymce/src/plugins/help/test/ts/browser/KeyboardNavTabI18nTest.ts
+++ modules/tinymce/src/plugins/help/test/ts/browser/KeyboardNavTabI18nTest.ts
@@ -1,7 +1,8 @@
 import { Mouse, UiFinder } from '@ephox/agar';
-import { before, describe, it } from '@ephox/bedrock-client';
-import { SugarBody } from '@ephox/sugar';
+import { before, context, describe, it } from '@ephox/bedrock-client';
+import { SugarBody, SugarElement } from '@ephox/sugar';
 import { McEditor, TinyUiActions } from '@ephox/wrap-mcagar';
+import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
 import { RawEditorOptions } from 'tinymce/core/api/OptionTypes';
@@ -18,18 +19,41 @@ describe('browser.tinymce.plugins.help.KeyboardNavTabI18nTest', () => {
     base_url: '/project/tinymce/js/tinymce'
   };
 
-  const testLanguage = async (firstWord: string, language?: string): Promise<void> => {
-    const editor = await McEditor.pFromSettings<Editor>({ ...baseSettings, language });
+  const createEditorWithLanguage = (language?: string): Promise<Editor> => McEditor.pFromSettings<Editor>({ ...baseSettings, language });
+
+  const openHelpDialogKeyboardNavigationTab = async (editor: Editor): Promise<SugarElement<Element>> => {
     editor.execCommand('mceHelp');
     const dialogEl = await TinyUiActions.pWaitForDialog(editor);
     Mouse.trueClickOn(SugarBody.body(), `.tox-dialog__body-nav-item:nth-child(2)`);
-    UiFinder.exists(dialogEl, `h1:contains("${firstWord}")`);
-    McEditor.remove(editor);
+    return dialogEl;
   };
 
-  it('TINY-9633: Can load English by default', () => testLanguage('Begin'));
+  context('Test language load', () => {
+    const testLanguage = async (firstWord: string, language?: string): Promise<void> => {
+      const editor = await createEditorWithLanguage(language);
+      const dialogEl = await openHelpDialogKeyboardNavigationTab(editor);
+      UiFinder.exists(dialogEl, `h1:contains("${firstWord}")`);
+      McEditor.remove(editor);
+    };
 
-  it('TINY-9633: Can load German translation', () => testLanguage('Grundlagen', 'de'));
+    it('TINY-9633: Can load English by default', () => testLanguage('Begin'));
+    it('TINY-9633: Can load German translation', () => testLanguage('Grundlagen', 'de'));
+    it('TINY-9633: Loads English fallback when invalid language code is specified', () => testLanguage('Begin', 'invalid'));
+  });
 
-  it('TINY-9633: Loads English fallback when invalid language code is specified', () => testLanguage('Begin', 'invalid'));
+  it('TINY-9920: Fallback HTML is the same as the one loaded from English HTML file', async () => {
+    const getKeyboardNavHtml = async (language: string): Promise<string> => {
+      const editor = await createEditorWithLanguage(language);
+      const dialogEl = await openHelpDialogKeyboardNavigationTab(editor);
+      const keyboardNavContentEl = UiFinder.findIn(dialogEl, '.tox-dialog__body-content [role="document"]')
+        .getOrThunk(() => assert.fail('Could not find Keyboard Navigation content element'));
+      McEditor.remove(editor);
+      return keyboardNavContentEl.dom.innerHTML;
+    };
+
+    const fallbackHtml = await getKeyboardNavHtml('invalid');
+    const englishHtml = await getKeyboardNavHtml('en');
+
+    assert.equal(fallbackHtml, englishHtml, 'Fallback HTML should be the same as the one loaded from English HTML file');
+  });
 });
