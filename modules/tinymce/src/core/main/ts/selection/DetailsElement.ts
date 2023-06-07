a525d02d2ed9d6c31672feb72ea94316277f7454
Mike Zhu <mkzh.org@gmail.com>
2023-06-07 17:05:58 +1000

    TINY-9951: Implement workaround that wraps all summary children in a span
    - add workaround for when entire summary content is selected

--- modules/tinymce/src/core/main/ts/selection/DetailsElement.ts
+++ modules/tinymce/src/core/main/ts/selection/DetailsElement.ts
@@ -63,7 +63,8 @@ const isDetails = (node: Node | null | undefined): boolean => node?.nodeName ===
 
 // TINY-9950: Firefox has some situations where its native behavior does not match what we expect, so
 // we have to perform Firefox-specific overrides.
-const browser = PlatformDetection.detect().browser;
+const platform = PlatformDetection.detect();
+const browser = platform.browser;
 const isFirefox = browser.isFirefox();
 const isSafari = browser.isSafari();
 
@@ -78,7 +79,8 @@ const preventDeletingSummary = (editor: Editor): void => {
       const endElement = editor.selection.getEnd();
       const rng = editor.selection.getRng();
       const isBackspace = e.keyCode === VK.BACKSPACE;
-      const isBackspaceAndCaretAtStart = isBackspace && isCaretInTheBeginning(rng, node);
+      const isCaretAtStart = isCaretInTheBeginning(rng, node);
+      const isBackspaceAndCaretAtStart = isBackspace && isCaretAtStart;
       const isDelete = e.keyCode === VK.DELETE;
       const isDeleteAndCaretAtEnd = isDelete && isCaretInTheEnding(rng, node);
       const isCollapsed = editor.selection.isCollapsed();
@@ -111,30 +113,42 @@ const preventDeletingSummary = (editor: Editor): void => {
         e.preventDefault();
         removeNode(editor, node);
         CaretFinder.firstPositionIn(nextNode).each(setCaretToPosition(editor));
+      } else if (isSafari && isSummary(node)) {
+        // TINY-9951: Safari has a bug where upon pressing Backspace/Delete when the caret is directly within the summary,
+        // all content is removed and the caret is prevented from being placed back into the summary.
+        e.preventDefault();
+
+        if (!isCollapsed && rng.startOffset === 0 && rng.endOffset === node.textContent?.length || DeleteUtils.willDeleteLastPositionInElement(isDelete, CaretPosition.fromRangeStart(rng), node)) {
+          PaddingBr.fillWithPaddingBr(SugarElement.fromDom(node));
+        } else {
+          // Wrap all summary children in a temporary container to execute Backspace/Delete there, then unwrap
+          const selection = editor.selection.getSel();
+          let { anchorNode, anchorOffset, focusNode, focusOffset } = selection ?? {};
+          const applySelection = () => {
+            if (Type.isNonNullable(anchorNode) && Type.isNonNullable(anchorOffset) && Type.isNonNullable(focusNode) && Type.isNonNullable(focusOffset)) {
+              selection?.setBaseAndExtent(anchorNode, anchorOffset, focusNode, focusOffset);
+            }
+          };
+          const updateSelection = () => {
+            anchorNode = selection?.anchorNode;
+            anchorOffset = selection?.anchorOffset;
+            focusNode = selection?.focusNode;
+            focusOffset = selection?.focusOffset;
+          };
+
+          const container = editor.dom.create('span');
+          Arr.each(node.childNodes, (child) => container.appendChild(child));
+          node.appendChild(container);
+          applySelection();
+          editor.execCommand(isBackspace ? 'Delete' : 'ForwardDelete');
+          updateSelection();
+          Arr.each(container.childNodes, (child) => node.appendChild(child));
+          editor.dom.remove(container);
+          applySelection();
+        }
       }
     }
   });
-
-  if (isSafari) {
-    // TINY-9951: Safari has a bug where upon pressing Backspace/Delete when the caret is directly within the summary,
-    // all content is removed and the caret is prevented from being placed back into the summary.
-    editor.on('keydown', (e) => {
-      const node = editor.selection.getNode();
-      const isDelete = e.keyCode === VK.DELETE;
-      if ((e.keyCode === VK.BACKSPACE || isDelete) && isSummary(node)) {
-        if (isDelete && isCaretInTheBeginning(editor.selection.getRng(), node)) {
-          e.preventDefault();
-        } else {
-          const fromPos = CaretPosition.fromRangeStart(editor.selection.getRng());
-          if (e.altKey || e.metaKey || DeleteUtils.willDeleteLastPositionInElement(isDelete, fromPos, node)) {
-            PaddingBr.fillWithPaddingBr(SugarElement.fromDom(node));
-          } else {
-            editor.selection.getBookmark();
-          }
-        }
-      }
-    });
-  }
 };
 
 const setup = (editor: Editor): void => {
