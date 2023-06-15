3b464e431d5ec47492f94d9571d197b871223603
shanmen-tiny <shanmen.ng@tiny.cloud>
2023-06-15 13:34:36 +0800

    TINY-9379: Add keyboard shortcut indicator in statusbar

--- modules/tinymce/src/core/main/ts/api/OptionTypes.ts
+++ modules/tinymce/src/core/main/ts/api/OptionTypes.ts
@@ -122,6 +122,7 @@ interface BaseEditorOptions {
   formats?: Formats;
   format_noneditable_selector?: string;
   height?: number | string;
+  help_accessibility?: boolean;
   hidden_input?: boolean;
   highlight_on_focus?: boolean;
   icons?: string;
