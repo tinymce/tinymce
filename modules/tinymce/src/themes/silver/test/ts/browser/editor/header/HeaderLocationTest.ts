239a71f382f8890de6de37449e3160e4e06c27a7
shanmen-tiny <shanmen.ng@tiny.cloud>
2023-06-22 15:04:10 +0800

    TINY-9888: Update tests that asserts editor container structure to include bottom anchorbar

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/header/HeaderLocationTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/header/HeaderLocationTest.ts
@@ -36,6 +36,9 @@ describe('browser.tinymce.themes.silver.editor.header.HeaderLocationTest', () =>
               ]
             }),
             s.element('div', {
+              classes: [ arr.has('tox-bottom-anchorbar') ]
+            }),
+            s.element('div', {
               classes: [ arr.has('tox-statusbar') ]
             }),
           ]
--- modules/tinymce/src/themes/silver/test/ts/module/StickyHeaderUtils.ts
+++ modules/tinymce/src/themes/silver/test/ts/module/StickyHeaderUtils.ts
@@ -21,6 +21,14 @@ const statusbar = (s: ApproxStructure.StructApi, arr: ApproxStructure.ArrayApi):
     })
   ];
 
+const bottomAnchorbar = (s: ApproxStructure.StructApi, arr: ApproxStructure.ArrayApi): StructAssert[] =>
+  // should not change
+  [
+    s.element('div', {
+      classes: [ arr.has('tox-bottom-anchorbar') ]
+    })
+  ];
+
 const staticPartsInner = (s: ApproxStructure.StructApi, arr: ApproxStructure.ArrayApi): StructAssert[] =>
   // should not change
   [
@@ -157,8 +165,8 @@ const pAssertEditorContainer = async (isToolbarTop: boolean, expectedPart: Appro
       ApproxStructure.build((s, str, arr) => s.element('div', {
         classes: [ arr.has('tox-editor-container') ],
         children: isToolbarTop ?
-          expectedPart(s, str, arr).concat(staticPartsOuter(s, arr)).concat(statusbar(s, arr)) :
-          staticPartsOuter(s, arr).concat(expectedPart(s, str, arr)).concat(statusbar(s, arr))
+          expectedPart(s, str, arr).concat(staticPartsOuter(s, arr)).concat(bottomAnchorbar(s, arr)).concat(statusbar(s, arr)) :
+          staticPartsOuter(s, arr).concat(expectedPart(s, str, arr)).concat(bottomAnchorbar(s, arr)).concat(statusbar(s, arr))
       })),
       container
     )
@@ -174,8 +182,8 @@ const pScrollAndAssertStructure = async (isToolbarTop: boolean, scrollYDelta: nu
       ApproxStructure.build((s, str, arr) => s.element('div', {
         classes: [ arr.has('tox-editor-container') ],
         children: isToolbarTop ?
-          expectedPart(s, str, arr).concat(staticPartsOuter(s, arr)).concat(statusbar(s, arr)) :
-          staticPartsOuter(s, arr).concat(expectedPart(s, str, arr)).concat(statusbar(s, arr))
+          expectedPart(s, str, arr).concat(staticPartsOuter(s, arr)).concat(bottomAnchorbar(s, arr)).concat(statusbar(s, arr)) :
+          staticPartsOuter(s, arr).concat(expectedPart(s, str, arr)).concat(bottomAnchorbar(s, arr)).concat(statusbar(s, arr))
       })),
       container
     )
