0b57e46767d1b8af5dad53e54698be9a6dafcc00
mkzhx <mkzh.org@gmail.com>
2023-09-15 11:49:32 +1000

    TINY-10215: Reimplement Levels.createFromEditor (#2)

---
+++ modules/tinymce/src/core/main/ts/dom/TrimBody.ts
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) Tiny Technologies, Inc. All rights reserved.
+ * Licensed under the LGPL or a commercial license.
+ * For LGPL see License.txt in the project root for license information.
+ * For commercial licenses see https://www.tiny.cloud/
+ */
+
+import { Arr, Type } from '@ephox/katamari';
+import { Attribute, Remove, SugarElement } from '@ephox/sugar';
+
+import * as Zwsp from '../text/Zwsp';
+
+const getTemporaryNodeSelector = (tempAttrs: string[]): string =>
+  `${tempAttrs.length === 0 ? '' : `${Arr.map(tempAttrs, (attr) => `[${attr}]`).join(',')},`}[data-mce-bogus="all"]`;
+
+const getTemporaryNodes = (body: HTMLElement, tempAttrs: string[]): NodeListOf<Element> =>
+  body.querySelectorAll(getTemporaryNodeSelector(tempAttrs));
+
+const createCommentWalker = (body: HTMLElement): TreeWalker =>
+  // TINY-10215: IE11's implementation of createTreeWalker is consistent with DOM Level 2, which requires a fourth
+  // entityReferenceExpansion boolean argument to Document.createTreeWalker(). Modern browsers silently ignore this
+  // fourth argument.
+  // https://www.w3.org/TR/DOM-Level-2-Traversal-Range/traversal.html#NodeIteratorFactory-createTreeWalker
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  document.createTreeWalker(body, NodeFilter.SHOW_COMMENT, null, false);
+
+const hasComments = (body: HTMLElement): boolean =>
+  createCommentWalker(body).nextNode() !== null;
+
+const hasTemporaryNodes = (body: HTMLElement, tempAttrs: string[]): boolean =>
+  body.querySelector(getTemporaryNodeSelector(tempAttrs)) !== null;
+
+const trimTemporaryNodes = (body: HTMLElement, tempAttrs: string[]): void => {
+  Arr.each(getTemporaryNodes(body, tempAttrs), (elm) => {
+    const element = SugarElement.fromDom(elm);
+    if (Attribute.get(element, 'data-mce-bogus') === 'all') {
+      Remove.remove(element);
+    } else {
+      Arr.each(tempAttrs, (attr) => {
+        if (Attribute.has(element, attr)) {
+          Attribute.remove(element, attr);
+        }
+      });
+    }
+  });
+};
+
+const removeCommentsContainingZwsp = (body: HTMLElement): void => {
+  const walker = createCommentWalker(body);
+  let nextNode = walker.nextNode();
+  while (nextNode !== null) {
+    const comment = walker.currentNode as Comment;
+    nextNode = walker.nextNode();
+    // TINY-10215: Use String.prototype.indexOf() as IE11 does not support String.prototype.includes().
+    if (Type.isString(comment.nodeValue) && comment.nodeValue.indexOf(Zwsp.ZWSP) !== -1) {
+      Remove.remove(SugarElement.fromDom(comment));
+    }
+  }
+};
+
+export {
+  hasComments,
+  hasTemporaryNodes,
+  removeCommentsContainingZwsp,
+  trimTemporaryNodes
+};
