ad017d7dcd40a47d1234e4855c28d3ae9fbc4f8d
HAFRMO <61967464+HAFRMO@users.noreply.github.com>
2023-09-18 04:57:19 +0200

    TINY-9911: Load from javascript instead of css url links. (#9008)
    * TINY-9911: Load from javascript instead of css url links.
    
    * TINY-9911: Updated code to be more in line with requests
    
    * TINY-9911: Removed changelog change.
    
    * TINY-9911: Changed resource management.
    
    * TINY-9911: Fixed mistake.
    
    * TINY-9911: Removed no-longer-needed returns.

--- modules/tinymce/src/themes/silver/main/ts/ui/skin/Loader.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/skin/Loader.ts
@@ -4,6 +4,9 @@ import { SugarElement, SugarShadowDom } from '@ephox/sugar';
 import DOMUtils from 'tinymce/core/api/dom/DOMUtils';
 import StyleSheetLoader from 'tinymce/core/api/dom/StyleSheetLoader';
 import Editor from 'tinymce/core/api/Editor';
+import { TinyMCE } from 'tinymce/core/api/Tinymce';
+
+declare let tinymce: TinyMCE;
 
 import * as Options from '../../api/Options';
 import * as SkinLoaded from './SkinLoaded';
@@ -14,26 +17,51 @@ const loadStylesheet = (editor: Editor, stylesheetUrl: string, styleSheetLoader:
   return styleSheetLoader.load(stylesheetUrl);
 };
 
-const loadUiSkins = (editor: Editor, skinUrl: string): Promise<void> => {
-  const skinUiCss = skinUrl + '/skin.min.css';
-  return loadStylesheet(editor, skinUiCss, editor.ui.styleSheetLoader);
+const loadRawCss = (editor: Editor, key: string, css: string, styleSheetLoader: StyleSheetLoader): void => {
+  // Ensure the stylesheet is cleaned up when the editor is destroyed
+  editor.on('remove', () => styleSheetLoader.unloadRawCss(key));
+  return styleSheetLoader.loadRawCss(key, css);
 };
 
-const loadShadowDomUiSkins = (editor: Editor, skinUrl: string): Promise<void> => {
-  const isInShadowRoot = SugarShadowDom.isInShadowRoot(SugarElement.fromDom(editor.getElement()));
-  if (isInShadowRoot) {
-    const shadowDomSkinCss = skinUrl + '/skin.shadowdom.min.css';
-    return loadStylesheet(editor, shadowDomSkinCss, DOMUtils.DOM.styleSheetLoader);
+const loadUiSkins = async (editor: Editor, skinUrl: string): Promise<void> => {
+  const skinUiCss = skinUrl + '/skin.css';
+  const css = await tinymce.Resource.get(skinUiCss);
+
+  if (Type.isString(css)) {
+    loadRawCss(editor, skinUiCss, css, editor.ui.styleSheetLoader);
   } else {
-    return Promise.resolve();
+    const skinUiCss = skinUrl + '/skin.min.css';
+    return loadStylesheet(editor, skinUiCss, editor.ui.styleSheetLoader);
   }
 };
 
-const loadSkin = (isInline: boolean, editor: Editor): Promise<void> => {
-  const skinUrl = Options.getSkinUrl(editor);
+const loadShadowDomUiSkins = async (editor: Editor, skinUrl: string): Promise<void> => {
+  const isInShadowRoot = SugarShadowDom.isInShadowRoot(SugarElement.fromDom(editor.getElement()));
+  if (isInShadowRoot) {
 
+    const shadowDomSkinCss = skinUrl + '/skin.shadowdom.css';
+    const css = await tinymce.Resource.get(shadowDomSkinCss);
+
+    if (Type.isString(css)) {
+      loadRawCss(editor, shadowDomSkinCss, css, DOMUtils.DOM.styleSheetLoader);
+      return Promise.resolve();
+    } else {
+      const shadowDomSkinCss = skinUrl + '/skin.shadowdom.min.css';
+      return loadStylesheet(editor, shadowDomSkinCss, DOMUtils.DOM.styleSheetLoader);
+    }
+  }
+};
+
+const loadUrlSkin = async (isInline: boolean, editor: Editor): Promise<void> => {
+  const skinUrl = Options.getSkinUrl(editor);
   if (skinUrl) {
-    editor.contentCSS.push(skinUrl + (isInline ? '/content.inline' : '/content') + '.min.css');
+    const css = await tinymce.Resource.get(skinUrl + (isInline ? '/content.inline' : '/content') + '.css');
+
+    if (Type.isString(css)) {
+      loadRawCss(editor, skinUrl, css, editor.ui.styleSheetLoader);
+    } else {
+      editor.contentCSS.push(skinUrl + (isInline ? '/content.inline' : '/content') + '.min.css');
+    }
   }
 
   // In Modern Inline, this is explicitly called in editor.on('focus', ...) as well as in render().
@@ -42,10 +70,12 @@ const loadSkin = (isInline: boolean, editor: Editor): Promise<void> => {
     return Promise.all([
       loadUiSkins(editor, skinUrl),
       loadShadowDomUiSkins(editor, skinUrl)
-    ]).then(SkinLoaded.fireSkinLoaded(editor), SkinLoaded.fireSkinLoadError(editor, 'Skin could not be loaded'));
-  } else {
-    return Promise.resolve(SkinLoaded.fireSkinLoaded(editor)());
+    ]).then();
   }
+};
+
+const loadSkin = (isInline: boolean, editor: Editor): Promise<void> => {
+  return loadUrlSkin(isInline, editor).then(SkinLoaded.fireSkinLoaded(editor), SkinLoaded.fireSkinLoadError(editor, 'Skin could not be loaded'));
 };
 
 const iframe = Fun.curry(loadSkin, false);
