8f3758420a6db58c1269cd78fd18133729b25bf5
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2023-09-19 22:31:54 +0300

    TINY-10154: remove filter for summary headings

--- modules/tinymce/src/core/main/ts/selection/DetailsElement.ts
+++ modules/tinymce/src/core/main/ts/selection/DetailsElement.ts
@@ -1,9 +1,7 @@
 import { Arr } from '@ephox/katamari';
 
 import Editor from '../api/Editor';
-import AstNode from '../api/html/Node';
 import * as Options from '../api/Options';
-import * as AstNodeType from '../html/AstNodeType';
 
 const preventSummaryToggle = (editor: Editor): void => {
   editor.on('click', (e) => {
@@ -12,21 +10,6 @@ const preventSummaryToggle = (editor: Editor): void => {
     }
   });
 };
-
-// If the `summary` element contains multiple heading elements,
-// merge their contents into the first heading element.
-const mergeSummaryHeadings = (details: AstNode) =>
-  Arr.find(details.children(), AstNodeType.isSummary)
-    .each((summary) => {
-      if (summary.firstChild && AstNodeType.isHeading(summary.firstChild) && summary.children().length > 1) {
-        const h = summary.firstChild;
-        for (let node = h.next; node;) {
-          const next = node.next;
-          h.append(node);
-          node = next;
-        }
-      }
-    });
 
 const filterDetails = (editor: Editor): void => {
   editor.parser.addNodeFilter('details', (elms) => {
@@ -37,7 +20,6 @@ const filterDetails = (editor: Editor): void => {
       } else if (initialStateOption === 'collapsed') {
         details.attr('open', null);
       }
-      mergeSummaryHeadings(details);
     });
   });
 
