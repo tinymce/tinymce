d90241a6e94e0e7eb5e795bb29677808587ca72a
Spocke <spocke@moxiecode.com>
2023-10-11 18:47:51 +0200

    hints for esbuild/vite to remove import/exports

--- modules/tinymce/src/core/main/ts/content/EditorContent.ts
+++ modules/tinymce/src/core/main/ts/content/EditorContent.ts
@@ -1,4 +1,4 @@
-import { Content, GetContentArgs, SetContentArgs } from './ContentTypes';
+import { type Content, type GetContentArgs, type SetContentArgs } from './ContentTypes';
 import { getContent } from './GetContent';
 import { setContent } from './SetContent';
 
