6961779c31c58113cedc7812b041b5a3703a6d47
Mike Zhu <mike@mkzh.org>
2023-10-23 15:24:49 +1000

    TINY-10147: Refactor DropdownAriaLabelTest

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/bespoke/DropdownAriaLabelTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/bespoke/DropdownAriaLabelTest.ts
@@ -1,6 +1,7 @@
 import { UiFinder } from '@ephox/agar';
 import { afterEach, describe, it } from '@ephox/bedrock-client';
-import { Attribute, SugarBody } from '@ephox/sugar';
+import { Fun } from '@ephox/katamari';
+import { Attribute, SugarBody, SugarElement } from '@ephox/sugar';
 import { TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
 import { assert } from 'chai';
 
@@ -21,91 +22,115 @@ describe('browser.tinymce.themes.silver.editor.bespoke.DropdownAriaLabelTest', (
     toolbar: 'align fontfamily fontsize blocks styles'
   });
 
-  const testDropdownAriaLabel = (scenario: Scenario) => async () => {
-    const isAlign = scenario.menuLabel === 'Align';
-    const makeLabel = (item: string) => `${scenario.menuLabel} ${isAlign ? item.toLowerCase() : item}`;
-
+  const testDropdownAriaLabel = (
+    initialItem: string,
+    finalItem: string,
+    makeLabel: (item: string) => string,
+    pOpenMenu: (buttonSelector: string) => Promise<void>,
+    pWaitForMenu: (editor: Editor, itemSelector: string) => Promise<SugarElement<Element>>
+  ) => async () => {
     const editor = hook.editor();
-    const buttonSelector = `button[title="${makeLabel(scenario.initialItem)}"]`;
+    const buttonSelector = `button[title="${makeLabel(initialItem)}"]`;
     const button = UiFinder.findIn(SugarBody.body(), `.tox-toolbar__group ${buttonSelector}`).getOrDie();
-    if (isAlign) {
-      await MenuUtils.pOpenAlignMenu(scenario.menuLabel);
-    } else {
-      await MenuUtils.pOpenMenuWithSelector(scenario.menuLabel, buttonSelector);
-    }
-    const itemSelector = `div[role="menuitemcheckbox"][title="${scenario.finalItem}"]`;
-    if (scenario.menuLabel === 'Formats') {
-      const submenuSelector = 'div[title="Blocks"]';
-      await TinyUiActions.pWaitForUi(editor, submenuSelector);
-      TinyUiActions.clickOnUi(editor, submenuSelector);
-    } else {
-      await TinyUiActions.pWaitForUi(editor, itemSelector);
-    }
-    assert.equal(Attribute.get(button, 'aria-label'), makeLabel(scenario.initialItem));
+
+    await pOpenMenu(buttonSelector);
+    const itemSelector = `div[role="menuitemcheckbox"][title="${finalItem}"]`;
+    await pWaitForMenu(editor, itemSelector);
+    assert.equal(Attribute.get(button, 'aria-label'), makeLabel(initialItem));
     TinyUiActions.clickOnUi(editor, itemSelector);
-    assert.equal(Attribute.get(button, 'aria-label'), makeLabel(scenario.finalItem));
+    assert.equal(Attribute.get(button, 'aria-label'), makeLabel(finalItem));
   };
+
+  const testStandardDropdownAriaLabel = (scenario: Scenario) =>
+    testDropdownAriaLabel(
+      scenario.initialItem,
+      scenario.finalItem,
+      (item) => `${scenario.menuLabel} ${item}`,
+      Fun.curry(MenuUtils.pOpenMenuWithSelector, scenario.menuLabel),
+      TinyUiActions.pWaitForUi
+    );
+
+  const testAlignDropdownAriaLabel = (scenario: Scenario) =>
+    testDropdownAriaLabel(
+      scenario.initialItem,
+      scenario.finalItem,
+      (item) => `${scenario.menuLabel} ${item.toLowerCase()}`,
+      () => MenuUtils.pOpenAlignMenu(scenario.menuLabel),
+      TinyUiActions.pWaitForUi
+    );
+
+  const testFormatsDropdownAriaLabel = (scenario: Scenario) =>
+    testDropdownAriaLabel(
+      scenario.initialItem,
+      scenario.finalItem,
+      (item) => `${scenario.menuLabel} ${item}`,
+      Fun.curry(MenuUtils.pOpenMenuWithSelector, scenario.menuLabel),
+      (editor) => {
+        const submenuSelector = 'div[title="Blocks"]';
+        return TinyUiActions.pWaitForUi(editor, submenuSelector).then(() => TinyUiActions.clickOnUi(editor, submenuSelector));
+      }
+    );
 
   afterEach(() => {
     const editor = hook.editor();
     editor.setContent('');
   });
 
-  it('TINY-10147: Align menu should not update aria-label if displayed text does not change', testDropdownAriaLabel({
+  it('TINY-10147: Align menu should not update aria-label if displayed text does not change', testAlignDropdownAriaLabel({
     menuLabel: 'Align',
     initialItem: 'Left',
     finalItem: 'Left'
   }));
 
-  it('TINY-10147: Align menu should update aria-label if displayed text changes', testDropdownAriaLabel({
+  it('TINY-10147: Align menu should update aria-label if displayed text changes', testAlignDropdownAriaLabel({
     menuLabel: 'Align',
     initialItem: 'Left',
     finalItem: 'Right'
   }));
 
-  it('TINY-10147: Font family menu should not update aria-label if displayed text does not change', testDropdownAriaLabel({
+  it('TINY-10147: Font family menu should not update aria-label if displayed text does not change', testStandardDropdownAriaLabel({
     menuLabel: 'Fonts',
     initialItem: 'Verdana',
     finalItem: 'Verdana'
   }));
 
-  it('TINY-10147: Font family menu should update aria-label if displayed text changes', testDropdownAriaLabel({
+  it('TINY-10147: Font family menu should update aria-label if displayed text changes', testStandardDropdownAriaLabel({
     menuLabel: 'Fonts',
     initialItem: 'Verdana',
     finalItem: 'Arial'
   }));
 
-  it('TINY-10147: Font size menu should not update aria-label if displayed text does not change', testDropdownAriaLabel({
+  it('TINY-10147: Font size menu should not update aria-label if displayed text does not change', testStandardDropdownAriaLabel({
     menuLabel: 'Font sizes',
     initialItem: '12pt',
     finalItem: '12pt'
   }));
 
-  it('TINY-10147: Font size menu should update aria-label if displayed text changes', testDropdownAriaLabel({
+  it('TINY-10147: Font size menu should update aria-label if displayed text changes', testStandardDropdownAriaLabel({
     menuLabel: 'Font sizes',
     initialItem: '12pt',
     finalItem: '8pt'
   }));
 
-  it('TINY-10147: Blocks menu should not update aria-label if displayed text does not change', testDropdownAriaLabel({
+  it('TINY-10147: Blocks menu should not update aria-label if displayed text does not change', testStandardDropdownAriaLabel({
     menuLabel: 'Blocks',
     initialItem: 'Paragraph',
     finalItem: 'Paragraph'
   }));
 
-  it('TINY-10147: Blocks menu should update aria-label if displayed text changes', testDropdownAriaLabel({
+  it('TINY-10147: Blocks menu should update aria-label if displayed text changes', testStandardDropdownAriaLabel({
     menuLabel: 'Blocks',
     initialItem: 'Paragraph',
     finalItem: 'Heading 1'
   }));
 
-  it('TINY-10147: Styles menu should not update aria-label if displayed text does not change', testDropdownAriaLabel({
+  it('TINY-10147: Styles menu should not update aria-label if displayed text does not change', testFormatsDropdownAriaLabel({
     menuLabel: 'Formats',
     initialItem: 'Paragraph',
     finalItem: 'Paragraph'
   }));
 
-  it('TINY-10147: Styles menu should update aria-label if displayed text changes', testDropdownAriaLabel({
+  it('TINY-10147: Styles menu should update aria-label if displayed text changes', testFormatsDropdownAriaLabel({
     menuLabel: 'Formats',
     initialItem: 'Paragraph',
     finalItem: 'Div'
