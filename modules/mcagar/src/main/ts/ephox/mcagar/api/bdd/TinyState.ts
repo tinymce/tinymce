01999ec50216cbd689b829ad7297930cf1f9a339
spocke <spocke@moxiecode.com>
2023-10-24 08:31:44 +0200

    TINY-10304: Fixed broken non-editable root advlist test (#9091) (#9104)
    * TINY-10304: Fixed broken non-editable root advlist test (#9091)
    
    * TINY-10304: Properly toggle editable root state
    
    * TINY-10304: Try using the specific editor container
    
    * TINY-10304: dump the whole page state
    
    * TINY-10304: added missing AdvList plugin to hook init
    
    * TINY-10304: Restored sync test and re-enabled all tests
    
    * TINY-10304: Added changelog to mcagar
    
    * TINY-10304: Added v6.5 API checking for mcagar TinyState

--- modules/mcagar/src/main/ts/ephox/mcagar/api/bdd/TinyState.ts
+++ modules/mcagar/src/main/ts/ephox/mcagar/api/bdd/TinyState.ts
@@ -1,14 +1,22 @@
 import { Editor as EditorType } from '../../alien/EditorTypes';
 
+const setEditableRoot = <T extends EditorType = EditorType>(editor: T, state: boolean) => {
+  if (editor.setEditableRoot) {
+    editor.setEditableRoot(state);
+  } else {
+    throw new Error('setEditableRoot requires at least TinyMCE v6.5');
+  }
+};
+
 export const withNoneditableRootEditor = <T extends EditorType = EditorType>(editor: T, f: (editor: T) => void): void => {
-  editor.getBody().contentEditable = 'false';
+  setEditableRoot(editor, false);
   f(editor);
-  editor.getBody().contentEditable = 'true';
+  setEditableRoot(editor, true);
 };
 
 export const withNoneditableRootEditorAsync = async <T extends EditorType = EditorType>(editor: T, f: (editor: T) => Promise<void>): Promise<void> => {
-  editor.getBody().contentEditable = 'false';
+  setEditableRoot(editor, false);
   await f(editor);
-  editor.getBody().contentEditable = 'true';
+  setEditableRoot(editor, true);
 };
 
