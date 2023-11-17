3d7e1382e5c2bb5084789b8996d3a8ec93d46be9
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2023-11-17 15:29:40 +0200

    TINY-10177: move Safari branching to the test itself

--- modules/tinymce/src/plugins/accordion/test/ts/browser/QuirksTest.ts
+++ modules/tinymce/src/plugins/accordion/test/ts/browser/QuirksTest.ts
@@ -1,4 +1,4 @@
-import { before, describe, it } from '@ephox/bedrock-client';
+import { describe, it } from '@ephox/bedrock-client';
 import { PlatformDetection } from '@ephox/sand';
 import { TinyAssertions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
 
@@ -17,17 +17,16 @@ describe('browser.tinymce.plugins.accordion.QuirksTest', () => {
     },
     [ Plugin ]
   );
-  before(function () {
-    if (!browser.isSafari()) {
-      this.skip();
-    }
-  });
-
   it('TINY-10177: should override the selection to the beginning of `summary` after clicking on it.', () => {
     const editor = hook.editor();
     editor.setContent('<p>Hello</p> ' + AccordionUtils.createAccordion());
+    if (browser.isSafari()) {
     // set selection before the `summary`
-    TinySelections.setCursor(editor, [ 1 ], 0);
+      TinySelections.setCursor(editor, [ 1 ], 0);
+    } else {
+    // set selection at the beginning of the `summary`
+      TinySelections.setCursor(editor, [ 1, 0 ], 0);
+    }
     const event = { target: editor.dom.select('summary')[0] } as unknown as MouseEvent;
     editor.dispatch('click', event );
     TinyAssertions.assertCursor(editor, [ 1, 0 ], 0);
