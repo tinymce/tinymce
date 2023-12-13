1e1927c4d396b7a0b839483e41f5a2e313cfd03c
shanmen-tiny <shanmen.ng@tiny.cloud>
2023-12-13 11:49:41 +0800

    TINY-9275: Fix failing test

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/TooltipTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/TooltipTest.ts
@@ -1,6 +1,6 @@
 import { FocusTools, Keys, Mouse, UiFinder, Waiter } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
-import { SugarBody } from '@ephox/sugar';
+import { SugarBody, SugarElement } from '@ephox/sugar';
 import { TinyDom, TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
 import { assert } from 'chai';
 
@@ -15,11 +15,12 @@ describe('browser.tinymce.themes.silver.editor.Tooltip', () => {
 
   const tooltipSelector = '.tox-silver-sink .tox-tooltip__body';
   const insertLinkSelector = 'button.tox-tbtn[aria-label="Insert/edit link"]';
-  const assertTooltip = (text: string) => {
-    const tooltip = UiFinder.findIn(SugarBody.body(), tooltipSelector).getOrDie();
+  const assertTooltip = async (editor: Editor, text: string) => {
+    const tooltip = await TinyUiActions.pWaitForUi(editor, tooltipSelector) as SugarElement<HTMLElement>;
     assert.equal(tooltip.dom.textContent, text);
   };
 
+  //TODO: TINY-10465 - Improve tests
   it('TINY-9275: should be rendered after a delay on mouse over', async () => {
     const editor = hook.editor();
 
@@ -28,15 +29,15 @@ describe('browser.tinymce.themes.silver.editor.Tooltip', () => {
     // Not expecting the tooltip to be rendered immedilately
     UiFinder.notExists(SugarBody.body(), tooltipSelector);
     // This is a slightly higher than the default delay for showing a tooltip to take renderinging time into account
-    await Waiter.pWait(1000);
-    assertTooltip('Font size 12pt');
+    await assertTooltip(editor, 'Font size 12pt');
 
     // Then move mouse over the insert link button
     const insertLinkButton = UiFinder.findIn(TinyDom.container(editor), insertLinkSelector).getOrDie();
     Mouse.mouseOver(insertLinkButton);
+
+    await Waiter.pWait(500);
     // There is a small delay between hiding the current tooltip and showing another one
-    await Waiter.pWait(200);
-    assertTooltip('Insert/edit link');
+    await assertTooltip(editor, 'Insert/edit link');
 
     // Mouseovering another element doesn't trigger `mouseout` event on tooltip
     Mouse.mouseOut(insertLinkButton);
@@ -52,11 +53,11 @@ describe('browser.tinymce.themes.silver.editor.Tooltip', () => {
     FocusTools.setFocus(TinyDom.container(editor), insertLinkSelector);
     // Wait for the tooltip to be visible. Notice that the waiting is significant less
     await Waiter.pWait(500);
-    assertTooltip('Insert/edit link');
+    await assertTooltip(editor, 'Insert/edit link');
 
     // Navigate to another button in the toolbar
     TinyUiActions.keydown(editor, Keys.tab());
-    await Waiter.pWait(300);
-    assertTooltip('Font size 12pt');
+    await Waiter.pWait(500);
+    await assertTooltip(editor, 'Font size 12pt');
   });
 });
