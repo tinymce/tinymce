2f1a7a3158282b305d88134446da3fa6d5965cb4
shanmen-tiny <shanmen.ng@tiny.cloud>
2023-12-13 12:09:57 +0800

    TINY-9275: Fix test container

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/TooltipTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/TooltipTest.ts
@@ -32,7 +32,7 @@ describe('browser.tinymce.themes.silver.editor.Tooltip', () => {
     await assertTooltip(editor, 'Font size 12pt');
 
     // Then move mouse over the insert link button
-    const insertLinkButton = UiFinder.findIn(TinyDom.container(editor), insertLinkSelector).getOrDie();
+    const insertLinkButton = UiFinder.findIn(SugarBody.body(), insertLinkSelector).getOrDie();
     Mouse.mouseOver(insertLinkButton);
 
     await Waiter.pWait(500);
@@ -50,7 +50,7 @@ describe('browser.tinymce.themes.silver.editor.Tooltip', () => {
   it('TINY-9275: should be rendered immediately when navigating by keyboard', async () => {
     const editor = hook.editor();
     // Focus on the first toolbar button
-    FocusTools.setFocus(TinyDom.container(editor), insertLinkSelector);
+    FocusTools.setFocus(SugarBody.body(), insertLinkSelector);
     // Wait for the tooltip to be visible. Notice that the waiting is significant less
     await Waiter.pWait(500);
     await assertTooltip(editor, 'Insert/edit link');
