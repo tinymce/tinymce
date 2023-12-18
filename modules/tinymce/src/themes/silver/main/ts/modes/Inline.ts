cf739bf3b21753ff78f1844cb2de7c67d155a734
Daniel Oakman <141111365+danoaky-tiny@users.noreply.github.com>
2023-12-18 21:33:49 +1100

    TINY-10482: Wait for skin to be loaded before setting inline mode ui

--- modules/tinymce/src/themes/silver/main/ts/modes/Inline.ts
+++ modules/tinymce/src/themes/silver/main/ts/modes/Inline.ts
@@ -155,21 +155,26 @@ const render = (editor: Editor, uiRefs: ReadyUiReferences, rawUiConfig: RenderUi
     // NOTE: In UiRefs, dialogUi and popupUi refer to the same thing if ui_mode: combined
     Attachment.attachSystem(uiContainer, uiRefs.dialogUi.mothership);
 
-    // Unlike menubar below which uses OuterContainer directly, this level of abstraction is
-    // required because of the different types of toolbars available (e.g. multiple vs single)
-    setToolbar(editor, uiRefs, rawUiConfig, backstage);
+    // TINY-10482: Just like in iframe mode, we need to wait for the skin to be loaded before setting the toolbar/menubar.
+    // Without this, there's the occasional chance that the toolbar/menubar could be set before the skin has finished
+    // loading, which causes CSS issues.
+    editor.on('SkinLoaded', () => {
+      // Unlike menubar below which uses OuterContainer directly, this level of abstraction is
+      // required because of the different types of toolbars available (e.g. multiple vs single)
+      setToolbar(editor, uiRefs, rawUiConfig, backstage);
 
-    OuterContainer.setMenubar(
-      mainUi.outerContainer,
-      identifyMenus(editor, rawUiConfig)
-    );
+      OuterContainer.setMenubar(
+        mainUi.outerContainer,
+        identifyMenus(editor, rawUiConfig)
+      );
 
-    // Initialise the toolbar - set initial positioning then show
-    ui.show();
+      // Initialise the toolbar - set initial positioning then show
+      ui.show();
 
-    setupEvents(editor, targetElm, ui, toolbarPersist);
+      setupEvents(editor, targetElm, ui, toolbarPersist);
 
-    editor.nodeChanged();
+      editor.nodeChanged();
+    });
   };
 
   editor.on('show', render);
