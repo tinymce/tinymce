1cf86d4e15bff9264582866db9053b7fd762b02f
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2024-01-18 13:28:24 +0100

    TINY-10580: test with `approximately`

--- modules/tinymce/src/themes/silver/test/ts/browser/editor/header/InlineHeaderTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/editor/header/InlineHeaderTest.ts
@@ -91,6 +91,7 @@ describe('browser.tinymce.themes.silver.editor.header.InlineHeaderTest', () => {
     const editorTargetRect = editor.dom.getRect(editorTarget.dom);
     const editorTargetTop = editorTargetRect.y;
 
-    assert.isAtLeast(editorTargetTop, toolbarBottom, 'toolbarBottom should be above editorTargetTop');
+    // assert.isAtLeast(editorTargetTop, toolbarBottom, 'toolbarBottom should be above editorTargetTop');
+    assert.approximately(editorTargetTop, toolbarBottom, 5, 'toolbarBottom should be above editorTargetTop');
   });
 });
