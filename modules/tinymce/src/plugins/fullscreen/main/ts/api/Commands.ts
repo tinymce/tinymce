47345d2dffc5f48bacfcf630d5ab717d83737d64
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2024-01-22 12:41:33 +0200

    TINY-10335: Handle immediate sink on fullscreen toggle (#9268)
    * TINY-10335: set sink to fixed position on fullscreen toggled
    
    * TINY-10335: fix typescript error
    
    * Update modules/tinymce/src/plugins/fullscreen/main/ts/core/Actions.ts
    
    Co-authored-by: spocke <spocke@moxiecode.com>
    
    * Update modules/tinymce/src/plugins/fullscreen/main/ts/core/Actions.ts
    
    Co-authored-by: spocke <spocke@moxiecode.com>
    
    * TINY-10335: add test
    
    ---------
    
    Co-authored-by: spocke <spocke@moxiecode.com>

--- modules/tinymce/src/plugins/fullscreen/main/ts/api/Commands.ts
+++ modules/tinymce/src/plugins/fullscreen/main/ts/api/Commands.ts
@@ -4,7 +4,7 @@ import Editor from 'tinymce/core/api/Editor';
 
 import * as Actions from '../core/Actions';
 
-const register = (editor: Editor, fullscreenState: Cell<Actions.ScrollInfo | null>): void => {
+const register = (editor: Editor, fullscreenState: Cell<Actions.FullScreenInfo | null>): void => {
   editor.addCommand('mceFullScreen', () => {
     Actions.toggleFullscreen(editor, fullscreenState);
   });
