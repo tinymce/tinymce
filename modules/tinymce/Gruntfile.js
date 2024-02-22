cf8259cd13c3a5de7932d81eaa9d8d50d8fb5d47
Spocke <spocke@moxiecode.com>
2024-02-22 08:36:55 +0100

    TINY-10578: Updated distribution file generation to GPL license

--- modules/tinymce/Gruntfile.js
+++ modules/tinymce/Gruntfile.js
@@ -628,7 +628,7 @@ module.exports = function (grunt) {
             zip.addData('bower.json', jsonToBuffer({
               'name': 'tinymce',
               'description': 'Web based JavaScript HTML WYSIWYG editor control.',
-              'license': 'MIT',
+              'license': 'GPL-2.0-or-later',
               'keywords': keywords,
               'homepage': 'https://www.tiny.cloud/',
               'ignore': ['README.md', 'composer.json', 'package.json', '.npmignore', 'CHANGELOG.md']
@@ -646,7 +646,7 @@ module.exports = function (grunt) {
               'author': 'Ephox Corporation DBA Tiny Technologies, Inc',
               'main': 'tinymce.js',
               'types': 'tinymce.d.ts',
-              'license': 'MIT',
+              'license': 'GPL-2.0-or-later',
               'keywords': keywords,
               'homepage': 'https://www.tiny.cloud/',
               'bugs': { 'url': 'https://github.com/tinymce/tinymce/issues' }
@@ -656,7 +656,7 @@ module.exports = function (grunt) {
               'name': 'tinymce/tinymce',
               'version': packageData.version,
               'description': 'Web based JavaScript HTML WYSIWYG editor control.',
-              'license': ['MIT-only'],
+              'license': ['GPL-2.0-or-later'],
               'keywords': keywords,
               'homepage': 'https://www.tiny.cloud/',
               'type': 'component',
@@ -738,12 +738,12 @@ module.exports = function (grunt) {
           authors: 'Ephox Corporation DBA Tiny Technologies, Inc',
           owners: 'Ephox Corporation DBA Tiny Technologies, Inc',
           description: 'The best WYSIWYG editor! TinyMCE is a platform independent web based Javascript HTML WYSIWYG editor ' +
-          'control released as Open Source under MIT by Tiny Technologies, Inc. TinyMCE has the ability to convert HTML ' +
+          'control released as Open Source under GNU General Public License Version 2 or later by Tiny Technologies, Inc. TinyMCE has the ability to convert HTML ' +
           'TEXTAREA fields or other HTML elements to editor instances. TinyMCE is very easy to integrate ' +
           'into other Content Management Systems.',
           releaseNotes: 'Release notes for my package.',
           summary: 'TinyMCE is a platform independent web based Javascript HTML WYSIWYG editor ' +
-          'control released as Open Source under MIT by Tiny Technologies, Inc.',
+          'control released as Open Source under GNU General Public License Version 2 or later by Tiny Technologies, Inc.',
           projectUrl: 'https://www.tiny.cloud/',
           iconUrl: 'https://www.tiny.cloud/favicon-32x32.png',
           licenseUrl: 'https://www.tiny.cloud/license',
