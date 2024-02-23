e6f76e413bfe7d51ab454842ce35a8d3d293d756
shanmen-tiny <shanmen.ng@tiny.cloud>
2024-02-23 17:25:19 +1100

    TINY-10609: Revert changes made in `a4ec376` (#9404)

--- modules/tinymce/src/themes/silver/main/ts/ui/view/ViewButtons.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/view/ViewButtons.ts
@@ -7,33 +7,11 @@ import { renderReplaceableIconFromPack } from '../button/ButtonSlices';
 import { calculateClassesFromButtonType, IconButtonWrapper, renderCommonSpec } from '../general/Button';
 import { componentRenderPipeline } from '../menus/item/build/CommonMenuItem';
 import { ViewButtonClasses } from '../toolbar/button/ButtonClasses';
-import { ViewButtonWithoutGroup, ViewLabel } from './View';
+import { ViewButtonWithoutGroup } from './View';
 
 type Behaviours = Behaviour.NamedConfiguredBehaviour<any, any, any>[];
 
-const labelSizeClass = {
-  normal: 'tox-view__label--normal',
-  large: 'tox-view__label--large',
-};
-
-const renderLabel = (spec: ViewLabel, providers: UiFactoryBackstageProviders): SimpleOrSketchSpec => {
-  const sizeClass = labelSizeClass[spec.size];
-  return {
-    dom: {
-      tag: 'div',
-      classes: [ 'tox-view__label', sizeClass ]
-    },
-    components: [
-      GuiFactory.text(providers.translate(spec.text))
-    ]
-  };
-};
-
 export const renderButton = (spec: ViewButtonWithoutGroup, providers: UiFactoryBackstageProviders): SimpleOrSketchSpec => {
-  if (spec.type === 'label') {
-    return renderLabel(spec, providers);
-  }
-
   const isToggleButton = spec.type === 'togglebutton';
 
   const optMemIcon = spec.icon
