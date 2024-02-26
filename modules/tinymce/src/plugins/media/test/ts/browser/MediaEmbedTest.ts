6d9f6dfff15bdfa96ac360b974de0166a4b72dfb
Arvin Han <20640097+ArvinJ-H@users.noreply.github.com>
2024-02-26 13:16:45 +1100

    TINY-9154: Change media_url_resolver to use promises

--- modules/tinymce/src/plugins/media/test/ts/browser/MediaEmbedTest.ts
+++ modules/tinymce/src/plugins/media/test/ts/browser/MediaEmbedTest.ts
@@ -11,10 +11,16 @@ describe.skip('browser.tinymce.plugins.media.core.MediaEmbedTest', () => {
   const hook = TinyHooks.bddSetupLight<Editor>({
     plugins: [ 'media' ],
     toolbar: 'media',
-    media_url_resolver: (data: { url: string }, resolve: (response: { html: string }) => void) => {
-      resolve({
-        html: '<video width="300" height="150" ' +
-          'controls="controls">\n<source src="' + data.url + '" />\n</video>'
+    media_url_resolver: (data: { url: string }) => {
+      return new Promise<{ html: string }>((resolve, reject) => {
+        try {
+          resolve({
+            html: '<video width="300" height="150" ' +
+              'controls="controls">\n<source src="' + data.url + '" />\n</video>'
+          });
+        } catch (error) {
+          reject(error);
+        }
       });
     },
     base_url: '/project/tinymce/js/tinymce'
