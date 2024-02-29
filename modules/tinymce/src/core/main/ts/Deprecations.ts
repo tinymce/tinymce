93b581d669b957467432f2b0e55d45a4993d1411
ltrouton <46954949+ltrouton@users.noreply.github.com>
2024-02-29 10:23:56 +1000

    TINY-10694: Update deprecation/removed console message for 7.0 (#9432)

--- modules/tinymce/src/core/main/ts/Deprecations.ts
+++ modules/tinymce/src/core/main/ts/Deprecations.ts
@@ -3,30 +3,27 @@ import { Arr, Obj } from '@ephox/katamari';
 import { NormalizedEditorOptions, RawEditorOptions } from './api/OptionTypes';
 import Tools from './api/util/Tools';
 
+interface DeprecatedPlugin {
+  readonly name: string;
+  readonly replacedWith?: string;
+}
+
 const removedOptions = (
   'autoresize_on_init,content_editable_state,padd_empty_with_br,block_elements,' +
   'boolean_attributes,editor_deselector,editor_selector,elements,file_browser_callback_types,filepicker_validator_handler,' +
   'force_hex_style_colors,force_p_newlines,gecko_spellcheck,images_dataimg_filter,media_scripts,mode,move_caret_before_on_enter_elements,' +
   'non_empty_elements,self_closing_elements,short_ended_elements,special,spellchecker_select_languages,spellchecker_whitelist,' +
   'tab_focus,tabfocus_elements,table_responsive_width,text_block_elements,text_inline_elements,toolbar_drawer,types,validate,whitespace_elements,' +
-  'paste_enable_default_filters,paste_filter_drop,paste_word_valid_elements,paste_retain_style_properties,paste_convert_word_fake_lists'
-).split(',');
-
-const deprecatedOptions = (
+  'paste_enable_default_filters,paste_filter_drop,paste_word_valid_elements,paste_retain_style_properties,paste_convert_word_fake_lists,' +
   'template_cdate_classes,template_mdate_classes,template_selected_content_classes,template_preview_replace_values,template_replace_values,templates,template_cdate_format,template_mdate_format'
 ).split(',');
 
-const removedPlugins = 'bbcode,colorpicker,contextmenu,fullpage,legacyoutput,spellchecker,textcolor'.split(',');
+// const deprecatedOptions: string[] = ('').split(',');
+const deprecatedOptions: string[] = [];
 
-const deprecatedPlugins = [
-  {
-    name: 'template',
-    replacedWith: 'Advanced Template',
-  },
-  {
-    name: 'rtc',
-  },
-];
+const removedPlugins = 'bbcode,colorpicker,contextmenu,fullpage,legacyoutput,spellchecker,template,textcolor,rtc'.split(',');
+
+const deprecatedPlugins: DeprecatedPlugin[] = [];
 
 const getMatchingOptions = (options: RawEditorOptions, searchingFor: string[]): string[] => {
   const settingNames = Arr.filter(searchingFor, (setting) => Obj.has(options, setting));
@@ -75,8 +72,8 @@ const logRemovedWarnings = (rawOptions: RawEditorOptions, normalizedOptions: Nor
     const optionsMessage = hasRemovedOptions ? `\n\nOptions:${listJoiner}${removedOptions.join(listJoiner)}` : '';
     // eslint-disable-next-line no-console
     console.warn(
-      'The following deprecated features are currently enabled and have been removed in TinyMCE 6.0. These features will no longer work and should be removed from the TinyMCE configuration. ' +
-      'See https://www.tiny.cloud/docs/tinymce/6/migration-from-5x/ for more information.' +
+      'The following deprecated features are currently enabled and have been removed in TinyMCE 7.0. These features will no longer work and should be removed from the TinyMCE configuration. ' +
+      'See https://www.tiny.cloud/docs/tinymce/7/migration-from-6x/ for more information.' +
       themesMessage +
       pluginsMessage +
       optionsMessage
