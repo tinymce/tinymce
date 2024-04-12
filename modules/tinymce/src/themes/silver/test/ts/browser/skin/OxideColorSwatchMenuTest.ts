205d75316a0488f321468dcacb47560682db603e
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2024-04-12 10:11:41 +0300

    TINY-10806: remove role attribute from Color Swatch menu item

--- modules/tinymce/src/themes/silver/test/ts/browser/skin/OxideColorSwatchMenuTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/skin/OxideColorSwatchMenuTest.ts
@@ -149,6 +149,9 @@ describe('browser.tinymce.themes.silver.skin.OxideColorSwatchMenuTest', () => {
       'Checking menu structure for color swatches',
       ApproxStructure.build((s, str, arr) => s.element('div', {
         classes: [ arr.has('tox-menu') ],
+        attrs: {
+          role: str.is('menu')
+        },
         children: [
           s.element('div', {
             classes: [ arr.has('tox-swatches') ],
@@ -206,6 +209,9 @@ describe('browser.tinymce.themes.silver.skin.OxideColorSwatchMenuTest', () => {
       'Checking menu structure for color swatches',
       ApproxStructure.build((s, str, arr) => s.element('div', {
         classes: [ arr.has('tox-menu') ],
+        attrs: {
+          role: str.none('The role should not be present')
+        },
         children: [
           s.element('div', {
             classes: [ arr.has('tox-swatches') ],
