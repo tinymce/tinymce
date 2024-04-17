ca3c1968a713f5c195455c33be56be833e1c6881
shanmen-tiny <shanmen.ng@tiny.cloud>
2024-04-17 16:20:04 +0800

    TINY-10597: Use VK instead

--- modules/tinymce/src/plugins/fullscreen/main/ts/core/Keyboard.ts
+++ modules/tinymce/src/plugins/fullscreen/main/ts/core/Keyboard.ts
@@ -1,13 +1,14 @@
 import { Cell } from '@ephox/katamari';
 
 import Editor from 'tinymce/core/api/Editor';
+import VK from 'tinymce/core/api/util/VK';
 
 import { FullScreenInfo } from './Actions';
 
 const setup = (editor: Editor, fullscreenState: Cell<FullScreenInfo | null>): void => {
   editor.on('init', () => {
     editor.on('keydown', (e) => {
-      if (e.key === 'Tab' && fullscreenState.get()) {
+      if ((e.keyCode === VK.TAB || (e.keyCode === VK.TAB && e.shiftKey)) && fullscreenState.get()) {
         e.preventDefault();
       }
     });
