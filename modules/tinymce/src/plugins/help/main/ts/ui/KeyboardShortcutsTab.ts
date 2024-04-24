af1ec5635b9e318b8a346b27ba25dae4ecdf9143
mrel <mrel@netcompany.com>
2024-04-24 08:47:31 +0200

    TINY-6561 Add includes to shortcuts and check included shortcuts for help plugin

--- modules/tinymce/src/plugins/help/main/ts/ui/KeyboardShortcutsTab.ts
+++ modules/tinymce/src/plugins/help/main/ts/ui/KeyboardShortcutsTab.ts
@@ -4,17 +4,20 @@ import { Dialog } from 'tinymce/core/api/ui/Ui';
 
 import * as ConvertShortcut from '../alien/ConvertShortcut';
 import * as KeyboardShortcuts from '../data/KeyboardShortcuts';
+import Editor from 'tinymce/core/api/Editor';
 
 export interface ShortcutActionPairType {
   shortcuts: string[];
   action: string;
 }
 
-const tab = (): Dialog.TabSpec & { name: string } => {
-  const shortcutList = Arr.map(KeyboardShortcuts.shortcuts, (shortcut: ShortcutActionPairType) => {
-    const shortcutText = Arr.map(shortcut.shortcuts, ConvertShortcut.convertText).join(' or ');
+const tab = (editor: Editor): Dialog.TabSpec & { name: string } => {
+  const shortcutList = Arr.filter(Arr.map(KeyboardShortcuts.shortcuts, (shortcut: ShortcutActionPairType) => {
+    const existingShortcutPatterns = Arr.filter(shortcut.shortcuts, (pattern: string) => editor.shortcuts.includes(pattern));
+    if (existingShortcutPatterns.length === 0) return undefined;
+    const shortcutText = Arr.map(existingShortcutPatterns, ConvertShortcut.convertText).join(' or ');
     return [ shortcut.action, shortcutText ];
-  });
+  }), (shortcut: any) => shortcut !== undefined);
 
   const tablePanel: Dialog.TableSpec = {
     type: 'table',
