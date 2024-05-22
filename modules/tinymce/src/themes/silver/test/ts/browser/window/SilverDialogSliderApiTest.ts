f27df1ec5ca7dc31b5a880222eff4c1c1da11f67
Zuzanna Tomaszyk <zuzanna.tomaszyk@tiny.cloud>
2024-05-22 11:53:21 +0200

    TINY-10428: improve test

--- modules/tinymce/src/themes/silver/test/ts/browser/window/SilverDialogSliderApiTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/browser/window/SilverDialogSliderApiTest.ts
@@ -3,6 +3,7 @@ import { context, describe, it } from '@ephox/bedrock-client';
 import { Arr } from '@ephox/katamari';
 import { SugarDocument } from '@ephox/sugar';
 import { TinyHooks, TinyUiActions } from '@ephox/wrap-mcagar';
+import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
 import { Dialog } from 'tinymce/core/api/ui/Ui';
@@ -14,6 +15,8 @@ describe('browser.tinymce.themes.silver.window.SilverDialogSliderApiTest', () =>
   const hook = TinyHooks.bddSetupLight<Editor>({
     base_url: '/project/tinymce/js/tinymce'
   }, []);
+
+  const initialSliderValue = 50;
 
   const dialogSpec: Dialog.DialogSpec<{ sliderField: number }> = {
     title: 'Silver Dialog Test Slider Api',
@@ -30,7 +33,7 @@ describe('browser.tinymce.themes.silver.window.SilverDialogSliderApiTest', () =>
       ]
     },
     initialData: {
-      sliderField: 50
+      sliderField: initialSliderValue
     },
     onChange: () => {
       store.add('logOnChangeEvent');
@@ -45,13 +48,21 @@ describe('browser.tinymce.themes.silver.window.SilverDialogSliderApiTest', () =>
       it('TINY-10428: dialog onChange handler should fire when using arrow key on slider', async () => {
         store.clear();
         const editor = hook.editor();
-        DialogUtils.open(editor, dialogSpec, test.params);
+        const dialogApi = DialogUtils.open(editor, dialogSpec, test.params);
         await TinyUiActions.pWaitForDialog(editor);
 
+        assert.deepEqual(dialogApi.getData(), { sliderField: initialSliderValue }, 'Check initial dialog data');
         await FocusTools.pTryOnSelector('Focus should be on slider rail', SugarDocument.getDocument(), '.tox-slider__rail');
+
         TinyUiActions.keydown(editor, Keys.left());
 
+        assert.deepEqual(dialogApi.getData(), { sliderField: initialSliderValue - 1 }, 'Check dialog data after left arrow key');
         store.assertEq('Check if on change event is logged after using arrow key', [ 'logOnChangeEvent' ]);
+
+        TinyUiActions.keydown(editor, Keys.right());
+
+        assert.deepEqual(dialogApi.getData(), { sliderField: initialSliderValue }, 'Check dialog data after right arrow key');
+        store.assertEq('Check if on change event is logged after using arrow key', [ 'logOnChangeEvent', 'logOnChangeEvent' ]);
       });
     });
   });
