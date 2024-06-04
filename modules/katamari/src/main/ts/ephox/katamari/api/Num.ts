7cd7b50e0ee02318cba3b83b631117d0228e883c
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2024-06-04 08:58:55 +0200

    TINY-10946: replace `Math.random` with `crypto.getRandomValues`

--- modules/katamari/src/main/ts/ephox/katamari/api/Num.ts
+++ modules/katamari/src/main/ts/ephox/katamari/api/Num.ts
@@ -17,3 +17,7 @@ export const cycleBy = (value: number, delta: number, min: number, max: number):
 // ASSUMPTION: Max will always be larger than min
 export const clamp = (value: number, min: number, max: number): number =>
   Math.min(Math.max(value, min), max);
+
+// this is from this example: https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-insecure-randomness.md
+// quoting the link above: "Divide a random UInt32 by the maximum value (2^32 -1) to get a result between 0 and 1"
+export const random = (): number => window.self.crypto.getRandomValues(new Uint32Array(1))[0] / 4294967295;
