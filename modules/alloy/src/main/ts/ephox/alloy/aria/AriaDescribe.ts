054671e930597a1edf45994132c97b72b163514a
Shan <shanmen.ng@tiny.cloud>
2024-06-05 11:47:36 +0800

    TINY-10891: Add tooltips to element path (#9676)
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>

--- modules/alloy/src/main/ts/ephox/alloy/aria/AriaDescribe.ts
+++ modules/alloy/src/main/ts/ephox/alloy/aria/AriaDescribe.ts
@@ -1,17 +1,22 @@
-import { Fun, Id, Optional } from '@ephox/katamari';
+import { Id, Optional } from '@ephox/katamari';
 import { Attribute, SugarElement } from '@ephox/sugar';
 
 const describedBy = (describedElement: SugarElement<Element>, describeElement: SugarElement<Element>): void => {
   const describeId = Optional.from(Attribute.get(describedElement, 'id'))
-    .fold(() => {
-      const id = Id.generate('dialog-describe');
+    .getOrThunk(() => {
+      const id = Id.generate('aria');
       Attribute.set(describeElement, 'id', id);
       return id;
-    }, Fun.identity);
+    });
 
   Attribute.set(describedElement, 'aria-describedby', describeId);
 };
 
+const remove = (describedElement: SugarElement<Element>): void => {
+  Attribute.remove(describedElement, 'aria-describedby');
+};
+
 export {
-  describedBy
+  describedBy,
+  remove
 };
