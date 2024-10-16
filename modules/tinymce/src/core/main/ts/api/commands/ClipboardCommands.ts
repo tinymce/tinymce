f5ed9505c181a759e945adbd14935c69e3c9008b
Arvin Han <20640097+ArvinJ-H@users.noreply.github.com>
2024-10-16 12:18:06 +1100

    TINY-6023: Add fallback method

--- modules/tinymce/src/core/main/ts/api/commands/ClipboardCommands.ts
+++ modules/tinymce/src/core/main/ts/api/commands/ClipboardCommands.ts
@@ -4,20 +4,12 @@ import Env from '../Env';
 
 export const registerCommands = (editor: Editor): void => {
 
-  const handleClipboardError = (command: string, failed: boolean, message: string, err: string) => {
+  const handleClipboardError = () => {
 
-    let msg = message;
-
-    if (failed) {
-      msg = editor.translate(
-        `Your browser doesn't support direct access to the clipboard. ` +
+    let msg = editor.translate(
+      `Your browser doesn't support direct access to the clipboard. ` +
         'Please use the Ctrl+X/C/V keyboard shortcuts instead.'
-      );
-    }
-
-    if (err) {
-      msg += ' ' + err;
-    }
+    );
 
     if (Env.os.isMacOS() || Env.os.isiOS()) {
       msg = msg.replace(/Ctrl\+/g, '\u2318+');
@@ -26,60 +18,78 @@ export const registerCommands = (editor: Editor): void => {
     editor.notificationManager.open({ text: msg, type: 'error' });
   };
 
-  const copyToClipboard = async (text: string, command: string, failed: boolean) => {
+  const copyToClipboard = async (text: string) => {
     try {
       await navigator.clipboard.writeText(text);
+      console.log('copyToClipboard', text);
     } catch (err) {
-      handleClipboardError(command, failed, 'Failed to copy to clipboard', String(err));
+      handleClipboardError();
     }
   };
 
-  const pasteFromClipboard = async (command: string, failed: boolean) => {
+  const pasteFromClipboard = async () => {
     let text = '';
 
     try {
       text = await navigator.clipboard.readText();
+      console.log('pasteFromClipboard', text);
       editor.selection.setContent(text);
     } catch (err) {
-      handleClipboardError(command, failed, 'Failed to paste from clipboard', String(err));
+      handleClipboardError();
     }
     return text;
   };
 
-  const cutToClipboard = async (text: string, command: string, failed: boolean) => {
+  const cutToClipboard = async (text: string) => {
     try {
       await navigator.clipboard.writeText(text);
+      console.log('cutToClipboard', text);
       editor.selection.setContent('');
     } catch (err) {
-      handleClipboardError(command, failed, 'Failed to cut to clipboard', String(err));
+      handleClipboardError();
+    }
+  };
+
+  const fallbackClipboardCommand = (editor: Editor, command: string) => {
+    const doc = editor.getDoc();
+    let failed = false;
+
+    try {
+      doc.execCommand(command);
+    } catch (ex) {
+      failed = true;
+    }
+
+    // Chrome reports the paste command as supported, however older IE:s will return false for cut/paste
+    if (command === 'paste' && !doc.queryCommandEnabled(command)) {
+      failed = true;
+    }
+
+    if (failed || !doc.queryCommandSupported(command)) {
+      handleClipboardError();
     }
   };
 
   editor.editorCommands.addCommands({
     'Cut,Copy,Paste': (command) => {
-      let failed = false;
+      const clipboard = navigator.clipboard;
+      console.log('command', navigator.clipboard);
 
-      try {
+      if (clipboard) {
         switch (command) {
           case 'copy':
-            copyToClipboard(editor.selection.getContent(), command, failed);
+            copyToClipboard(editor.selection.getContent());
             break;
           case 'paste':
-            pasteFromClipboard(command, failed);
+            pasteFromClipboard();
             break;
           case 'cut':
-            cutToClipboard(editor.selection.getContent(), command, failed);
+            cutToClipboard(editor.selection.getContent());
             break;
         }
-      } catch (ex) {
-        // Command failed
-        failed = true;
+      } else {
+        fallbackClipboardCommand(editor, command);
       }
-
-      // Chrome reports the paste command as supported however older IE:s will return false for cut/paste
-      // if (command === 'paste' && !doc.queryCommandEnabled(command)) {
-      //   failed = true;
-      // }
     }
   });
 };
