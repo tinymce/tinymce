2df68262c0895b39623fa0e827e1a681d24cce74
Arvin Han <20640097+ArvinJ-H@users.noreply.github.com>
2024-10-16 23:00:27 +1100

    TINY-11217: Move test

--- modules/tinymce/src/core/test/ts/browser/fmt/ListIndentationFormatTest.ts
+++ modules/tinymce/src/core/test/ts/browser/fmt/ListIndentationFormatTest.ts
@@ -27,5 +27,93 @@ describe('browser.tinymce.core.fmt.ListItemFormatTest', () => {
       const expected = editor.getContent();
       TinyAssertions.assertContent(editor, expected);
     });
+
+    it('TINY-8961: Apply format to partial selection of list item', () => {
+      const editor = hook.editor();
+
+      editor.setContent('<ul><li>Item 1</li><li>Item 2</li></ul>');
+      TinySelections.setSelection(editor, [ 0, 0, 0 ], 2, [ 0, 0, 0 ], 4);
+      editor.formatter.apply('bold');
+      TinySelections.setCursor(editor, [ 0, 1 ], 1);
+
+      TinyContentActions.keydown(editor, Keys.tab());
+
+      const expected = editor.getContent();
+      TinyAssertions.assertContent(editor, expected);
+    });
+
+    it('TINY-8961: Apply format to nested lists', () => {
+      const editor = hook.editor();
+
+      editor.setContent('<ul><li>Item 1<ul><li>Subitem 1</li><li>Subitem 2</li></ul></li><li>Item 2</li></ul>');
+      TinySelections.setSelection(editor, [ 0 ], 0, [ 0 ], 1);
+      editor.formatter.apply('bold');
+      TinySelections.setCursor(editor, [ 0, 1 ], 1);
+
+      TinyContentActions.keydown(editor, Keys.tab());
+
+      const expected = editor.getContent();
+      TinyAssertions.assertContent(editor, expected);
+    });
+
+    it('TINY-8961: Apply format to list with mixed content', () => {
+      const editor = hook.editor();
+
+      editor.setContent('<ul><li>Item <em>1</em></li><li><strong>Item</strong> 2</li></ul>');
+      TinySelections.setSelection(editor, [ 0 ], 0, [ 0 ], 1);
+      editor.formatter.apply('underline');
+      TinySelections.setCursor(editor, [ 0, 1 ], 1);
+
+      TinyContentActions.keydown(editor, Keys.tab());
+
+      const expected = editor.getContent();
+      TinyAssertions.assertContent(editor, expected);
+    });
+
+    it('TINY-8961: Remove all formats from list', () => {
+      const editor = hook.editor();
+
+      editor.setContent('<ul><li><strong><em><u>Item 1</u></em></strong></li><li><strong><em><u>Item 2</u></em></strong></li></ul>');
+      TinySelections.setSelection(editor, [ 0 ], 0, [ 0 ], 1);
+      editor.formatter.remove('bold');
+      editor.formatter.remove('italic');
+      editor.formatter.remove('underline');
+      TinySelections.setCursor(editor, [ 0, 1 ], 1);
+
+      TinyContentActions.keydown(editor, Keys.tab());
+
+      const expected = editor.getContent();
+      TinyAssertions.assertContent(editor, expected);
+    });
+
+    it('TINY-8961: Apply format to ordered list', () => {
+      const editor = hook.editor();
+
+      editor.setContent('<ol><li>Item 1</li><li>Item 2</li></ol>');
+      TinySelections.setSelection(editor, [ 0 ], 0, [ 0 ], 1);
+      editor.formatter.apply('bold');
+      TinySelections.setCursor(editor, [ 0, 1 ], 1);
+
+      TinyContentActions.keydown(editor, Keys.tab());
+
+      const expected = editor.getContent();
+      TinyAssertions.assertContent(editor, expected);
+    });
+
+    it('TINY-8961: Apply custom format to list', () => {
+      const editor = hook.editor();
+
+      editor.formatter.register('custom_format', { inline: 'span', classes: 'custom-class' });
+      editor.setContent('<ul><li>Item 1</li><li>Item 2</li></ul>');
+      TinySelections.setSelection(editor, [ 0 ], 0, [ 0 ], 1);
+      editor.formatter.apply('custom_format');
+      TinySelections.setCursor(editor, [ 0, 1 ], 1);
+
+      TinyContentActions.keydown(editor, Keys.tab());
+
+      const expected = editor.getContent();
+      TinyAssertions.assertContent(editor, expected);
+      editor.formatter.unregister('custom_format');
+    });
   });
 });
