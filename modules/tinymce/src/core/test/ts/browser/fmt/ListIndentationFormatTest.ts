d14c0973911b310554cced1d82d532a7b3defffa
Arvin Han <20640097+ArvinJ-H@users.noreply.github.com>
2024-10-23 17:56:59 +1100

    TINY-11217: Added color and background color test, and verify content structure

--- modules/tinymce/src/core/test/ts/browser/fmt/ListIndentationFormatTest.ts
+++ modules/tinymce/src/core/test/ts/browser/fmt/ListIndentationFormatTest.ts
@@ -2,7 +2,7 @@ import { Keys } from '@ephox/agar';
 import { context, describe, it } from '@ephox/bedrock-client';
 import { PlatformDetection } from '@ephox/sand';
 import { Css, SugarElement } from '@ephox/sugar';
-import { TinyContentActions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
+import { TinyAssertions, TinyContentActions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
 import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
@@ -15,7 +15,7 @@ describe('browser.tinymce.core.fmt.ListIndentationFormatTest', () => {
     plugins: 'lists'
   }, [], true);
 
-  const assertListItemStyles = () => {
+  const assertListItemStyles = (unorderd: boolean) => {
     const sugarItem2 = SugarElement.fromDom(hook.editor().dom.select('li.testClass')[0]);
 
     assert.strictEqual(Css.get(sugarItem2, 'color'), 'rgb(34, 47, 62)', 'Color should be rgb(34, 47, 62)');
@@ -27,6 +27,13 @@ describe('browser.tinymce.core.fmt.ListIndentationFormatTest', () => {
         '-apple-system, "system-ui", "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif' :
         '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif',
       'Font family should match the expected value');
+
+    TinyAssertions.assertContentPresence(hook.editor(),
+      unorderd ? {
+        ul: 2,
+      } : {
+        ol: 2,
+      });
   };
 
   context('Apply list formatting', () => {
@@ -39,11 +46,13 @@ describe('browser.tinymce.core.fmt.ListIndentationFormatTest', () => {
       editor.formatter.apply('bold');
       editor.formatter.apply('italic');
       editor.formatter.apply('underline');
+      editor.formatter.apply('forecolor', { value: '#ff0000' });
+      editor.formatter.apply('hilitecolor', { value: '#ff0000' });
       TinySelections.setCursor(editor, [ 0, 1 ], 1);
 
       TinyContentActions.keydown(editor, Keys.tab());
 
-      assertListItemStyles();
+      assertListItemStyles(true);
     });
 
     it('TINY-11217: Apply format to partial selection of list item', () => {
@@ -52,11 +61,13 @@ describe('browser.tinymce.core.fmt.ListIndentationFormatTest', () => {
       editor.setContent('<ul><li>Item 1</li><li class="testClass">Item 2</li></ul>');
       TinySelections.setSelection(editor, [ 0, 0, 0 ], 2, [ 0, 0, 0 ], 4);
       editor.formatter.apply('bold');
+      editor.formatter.apply('forecolor', { value: '#ff0000' });
+      editor.formatter.apply('hilitecolor', { value: '#ff0000' });
       TinySelections.setCursor(editor, [ 0, 1 ], 1);
 
       TinyContentActions.keydown(editor, Keys.tab());
 
-      assertListItemStyles();
+      assertListItemStyles(true);
     });
 
     it('TINY-11217: Apply format to nested lists', () => {
@@ -65,11 +76,15 @@ describe('browser.tinymce.core.fmt.ListIndentationFormatTest', () => {
       editor.setContent('<ul><li>Item 1<ul><li>Subitem 1</li><li>Subitem 2</li></ul></li><li class="testClass">Item 2</li></ul>');
       TinySelections.setSelection(editor, [ 0 ], 0, [ 0 ], 1);
       editor.formatter.apply('bold');
+      editor.formatter.remove('italic');
+      editor.formatter.remove('underline');
+      editor.formatter.apply('forecolor', { value: '#ff0000' });
+      editor.formatter.apply('hilitecolor', { value: '#ff0000' });
       TinySelections.setCursor(editor, [ 0, 1 ], 1);
 
       TinyContentActions.keydown(editor, Keys.tab());
 
-      assertListItemStyles();
+      assertListItemStyles(true);
     });
 
     it('TINY-11217: Apply format to list with mixed content', () => {
@@ -82,7 +97,7 @@ describe('browser.tinymce.core.fmt.ListIndentationFormatTest', () => {
 
       TinyContentActions.keydown(editor, Keys.tab());
 
-      assertListItemStyles();
+      assertListItemStyles(true);
     });
 
     it('TINY-11217: Remove all formats from list', () => {
@@ -97,7 +112,7 @@ describe('browser.tinymce.core.fmt.ListIndentationFormatTest', () => {
 
       TinyContentActions.keydown(editor, Keys.tab());
 
-      assertListItemStyles();
+      assertListItemStyles(true);
     });
 
     it('TINY-11217: Apply format to ordered list', () => {
@@ -106,11 +121,15 @@ describe('browser.tinymce.core.fmt.ListIndentationFormatTest', () => {
       editor.setContent('<ol><li>Item 1</li><li class="testClass">Item 2</li></ol>');
       TinySelections.setSelection(editor, [ 0 ], 0, [ 0 ], 1);
       editor.formatter.apply('bold');
+      editor.formatter.remove('italic');
+      editor.formatter.remove('underline');
+      editor.formatter.apply('forecolor', { value: '#ff0000' });
+      editor.formatter.apply('hilitecolor', { value: '#ff0000' });
       TinySelections.setCursor(editor, [ 0, 1 ], 1);
 
       TinyContentActions.keydown(editor, Keys.tab());
 
-      assertListItemStyles();
+      assertListItemStyles(false);
     });
 
     it('TINY-11217: Apply custom format to list', () => {
@@ -124,7 +143,7 @@ describe('browser.tinymce.core.fmt.ListIndentationFormatTest', () => {
 
       TinyContentActions.keydown(editor, Keys.tab());
 
-      assertListItemStyles();
+      assertListItemStyles(true);
       editor.formatter.unregister('custom_format');
     });
   });
