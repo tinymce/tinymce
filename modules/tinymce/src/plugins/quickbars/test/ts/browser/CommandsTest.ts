9813fd788b742b2ae83d74293d47ddbf9926cdc6
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2024-11-06 12:38:04 +0100

    TINY-11399: refactor test

--- modules/tinymce/src/plugins/quickbars/test/ts/browser/CommandsTest.ts
+++ modules/tinymce/src/plugins/quickbars/test/ts/browser/CommandsTest.ts
@@ -1,14 +1,14 @@
+import { TestStore } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
 import { Arr } from '@ephox/katamari';
 import { TinyHooks } from '@ephox/wrap-mcagar';
-import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
 import QuickbarsPlugin from 'tinymce/plugins/quickbars/Plugin';
 
 describe('browser.tinymce.plugins.quickbars.CommandsTest', () => {
   Arr.each([ true, false ], (withPresetedCommand) => {
-    const store: string[] = [];
+    const store = TestStore();
     const hook = TinyHooks.bddSetupLight<Editor>({
       plugins: 'quickbars',
       inline: true,
@@ -16,21 +16,21 @@ describe('browser.tinymce.plugins.quickbars.CommandsTest', () => {
       menubar: false,
       setup: (ed: Editor) => {
         if (withPresetedCommand) {
-          ed.addCommand('QuickbarInsertImage', () => store.push('QII'));
+          ed.addCommand('QuickbarInsertImage', () => store.add('QII'));
         }
       },
       base_url: '/project/tinymce/js/tinymce'
     }, [ QuickbarsPlugin ], true);
 
-    it('TINY-11399: QuickbarInsertImage should be not overwrite by the plugin if it already exists', async () => {
+    it(`TINY-11399: QuickbarInsertImage should be not overwrite by the plugin if it already exists${withPresetedCommand ? ' (withPresetedCommand)' : ''}`, async () => {
       const editor = hook.editor();
-      assert.deepEqual(store, []);
+      store.assertEq('it should be empty at start', []);
       editor.execCommand('QuickbarInsertImage');
-      assert.deepEqual(store, withPresetedCommand ? [ 'QII' ] : []);
+      store.assertEq('if overwritte it should exec the overwritten command', withPresetedCommand ? [ 'QII' ] : []);
 
-      editor.addCommand('QuickbarInsertImage', () => store.push('QII setted after'));
+      editor.addCommand('QuickbarInsertImage', () => store.add('QII-setted-after'));
       editor.execCommand('QuickbarInsertImage');
-      assert.deepEqual(store, withPresetedCommand ? [ 'QII', 'QII setted after' ] : [ 'QII setted after' ]);
+      store.assertEq('it should exec the overwritten command', withPresetedCommand ? [ 'QII', 'QII-setted-after' ] : [ 'QII-setted-after' ]);
     });
   });
 });
