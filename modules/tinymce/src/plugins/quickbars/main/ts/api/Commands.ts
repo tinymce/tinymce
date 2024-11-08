140e5bcf1cd3bc3ec210af3d452093968c5da9e5
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2024-11-08 13:01:29 +0100

    TINY-11399: moved `QuickbarInsertImage` inside `PreInit`

--- modules/tinymce/src/plugins/quickbars/main/ts/api/Commands.ts
+++ modules/tinymce/src/plugins/quickbars/main/ts/api/Commands.ts
@@ -5,19 +5,21 @@ import * as Conversions from '../insert/Conversions';
 import * as Picker from '../insert/Picker';
 
 const register = (editor: Editor): void => {
-  if (!editor.queryCommandSupported('QuickbarInsertImage')) {
-    editor.addCommand('QuickbarInsertImage', () => {
-      Picker.pickFile(editor).then((files) => {
-        if (files.length > 0) {
-          const blob = files[0];
+  editor.on('PreInit', () => {
+    if (!editor.queryCommandSupported('QuickbarInsertImage')) {
+      editor.addCommand('QuickbarInsertImage', () => {
+        Picker.pickFile(editor).then((files) => {
+          if (files.length > 0) {
+            const blob = files[0];
 
-          Conversions.blobToBase64(blob).then((base64) => {
-            Actions.insertBlob(editor, base64, blob);
-          });
-        }
+            Conversions.blobToBase64(blob).then((base64) => {
+              Actions.insertBlob(editor, base64, blob);
+            });
+          }
+        });
       });
-    });
-  }
+    }
+  });
 };
 
 export {
