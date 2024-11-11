327d761b26f1c00bb6449d7bc9acd132b9eabaee
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2024-11-11 13:47:40 +0100

    TINY-11399: refactor

--- modules/tinymce/src/plugins/quickbars/test/ts/browser/CommandsTest.ts
+++ modules/tinymce/src/plugins/quickbars/test/ts/browser/CommandsTest.ts
@@ -4,25 +4,27 @@ import { Arr } from '@ephox/katamari';
 import { TinyHooks } from '@ephox/wrap-mcagar';
 
 import Editor from 'tinymce/core/api/Editor';
+import PluginManager from 'tinymce/core/api/PluginManager';
 import QuickbarsPlugin from 'tinymce/plugins/quickbars/Plugin';
 
 describe('browser.tinymce.plugins.quickbars.CommandsTest', () => {
+  const store = TestStore();
+  const FakePlugin = (): void => {
+    PluginManager.add('fake', (editor) => {
+      editor.addCommand('QuickbarInsertImage', () => store.add('QII'));
+    });
+  };
   Arr.each([ true, false ], (withPredefinedCommand) => {
-    const store = TestStore();
     const hook = TinyHooks.bddSetupLight<Editor>({
-      plugins: 'quickbars',
+      plugins: withPredefinedCommand ? 'fake quickbars' : 'quickbars',
       inline: true,
       toolbar: false,
       menubar: false,
-      setup: (ed: Editor) => {
-        if (withPredefinedCommand) {
-          ed.addCommand('QuickbarInsertImage', () => store.add('QII'));
-        }
-      },
       base_url: '/project/tinymce/js/tinymce'
-    }, [ QuickbarsPlugin ], true);
+    }, [ FakePlugin, QuickbarsPlugin ], true);
 
     it(`TINY-11399: QuickbarInsertImage should be not overwritten by the plugin if it already exists${withPredefinedCommand ? ' (withPredefinedCommand)' : ''}`, async () => {
+      store.clear();
       const editor = hook.editor();
       store.assertEq('it should be empty at start', []);
       editor.execCommand('QuickbarInsertImage');
