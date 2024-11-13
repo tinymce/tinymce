8fe7496fb1c558638dbd570e3162c61951ea4f6e
Spocke <spocke@moxiecode.com>
2024-11-13 07:12:00 +0100

    EPIC-117: Updated changelogs and added ticket ref

--- modules/tinymce/src/themes/silver/main/ts/ui/context/ContextFormGroup.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/context/ContextFormGroup.ts
@@ -18,7 +18,7 @@ export const createContextFormFieldFromParts = (
     Disabling.config({
       disabled: () => providers.checkUiComponentContext('mode:design').shouldDisable,
       onDisabled: (comp) => {
-        // TODO: Is this really the best way to move focus out of the input when it gets disabled
+        // TODO: Is this really the best way to move focus out of the input when it gets disabled #TINY-11527
         Focus.search(comp.element).each((focus) => {
           SelectorFind.ancestor<HTMLElement>(focus, '[tabindex="-1"]').each((parent) => {
             Focus.focus(parent);
