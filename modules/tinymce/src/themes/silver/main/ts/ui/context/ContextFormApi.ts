3349aec52146a19f37b50d6ef1dfc9c2404daf88
Spocke <spocke@moxiecode.com>
2024-11-18 10:23:17 +0100

    TINY-11559: Fixed focus onSetup issue

--- modules/tinymce/src/themes/silver/main/ts/ui/context/ContextFormApi.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/context/ContextFormApi.ts
@@ -8,13 +8,20 @@ import {
 import { InlineContent } from '@ephox/bridge';
 import { Singleton } from '@ephox/katamari';
 
+import * as ContextToolbarFocus from './ContextToolbarFocus';
 import { backSlideEvent } from './ContextUi';
 
 export const getFormApi = <T>(input: AlloyComponent): InlineContent.ContextFormInstanceApi<T> => {
   const valueState = Singleton.value<T>();
 
   return ({
-    setInputEnabled: (state: boolean) => Disabling.set(input, !state),
+    setInputEnabled: (state: boolean) => {
+      if (!state) {
+        ContextToolbarFocus.focusParent(input);
+      }
+
+      Disabling.set(input, !state);
+    },
     isInputEnabled: () => !Disabling.isDisabled(input),
     hide: () => {
       // Before we hide snapshot the current value since accessing the value of a form field after it's been detached will throw an error
