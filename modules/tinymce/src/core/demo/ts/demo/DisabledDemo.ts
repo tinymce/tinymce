3572a896576734d1dea28797cc5ce162860d3035
shanmen-tiny <shanmen.ng@tiny.cloud>
2024-11-22 09:28:58 +0800

    TINY-11488: Update disabled demo page

--- modules/tinymce/src/core/demo/ts/demo/DisabledDemo.ts
+++ modules/tinymce/src/core/demo/ts/demo/DisabledDemo.ts
@@ -14,15 +14,15 @@ export default (): void => {
   Html.set(toggleModeBtn, 'Toggle readonly mode');
   DomEvent.bind(toggleModeBtn, 'click', () => {
     tinymce.activeEditor?.mode.set(tinymce.activeEditor.mode.get() === 'readonly' ? 'design' : 'readonly');
-    TextContent.set(statusDiv, `Mode: ${tinymce.activeEditor?.mode.get()}, Disabled: ${tinymce.activeEditor?.options.get('disabled')}`);
+    TextContent.set(statusDiv, `Mode: ${tinymce.activeEditor?.mode.get()}, Enabled: ${tinymce.activeEditor?.options.get('enabled')}`);
   });
   Insert.before(container, toggleModeBtn);
 
   const toggleDisabledBtn = SugarElement.fromTag('button');
   Html.set(toggleDisabledBtn, 'Toggle disabled mode');
   DomEvent.bind(toggleDisabledBtn, 'click', () => {
-    tinymce.activeEditor?.options.set('disabled', !tinymce.activeEditor.options.get('disabled'));
-    TextContent.set(statusDiv, `Mode: ${tinymce.activeEditor?.mode.get()}, Disabled: ${tinymce.activeEditor?.options.get('disabled')}`);
+    tinymce.activeEditor?.options.set('enabled', !tinymce.activeEditor.options.get('enabled'));
+    TextContent.set(statusDiv, `Mode: ${tinymce.activeEditor?.mode.get()}, Enabled: ${tinymce.activeEditor?.options.get('enabled')}`);
   });
   Insert.before(container, toggleDisabledBtn);
 
@@ -35,7 +35,7 @@ export default (): void => {
     toolbar: 'bold italic underline strikethrough subscript superscript accordion addtemplate inserttemplate| fontfamily fontsize fontsizeinput | numlist bullist checklist mergetags footnotes footnotesupdate| typography permanentpen formatpainter removeformat forecolor backcolor | blockquote nonbreaking hr pagebreak | casechange styles blocks lineheight | ltr rtl outdent indent | align alignleft aligncenter alignright alignjustify alignnone | h1 h2 h3 h4 h5 h6 | copy cut paste pastetext selectall remove newdocument wordcount searchreplace | undo redo | save cancel restoredraft | fullscreen print preview export code help | template insertdatetime codesample emoticons charmap | anchor link unlink image media pageembed insertfile | visualblocks visualchars | table advtablerownumbering tableclass tablecellclass tablecellvalign tablecellborderwidth tablecellborderstyle tablecaption tablecellbackgroundcolor tablecellbordercolor tablerowheader tablecolheader',
     setup: (ed) => {
       ed.on('init', () => {
-        TextContent.set(statusDiv, `Mode: ${ed.mode.get()}, Disabled: ${ed.options.get('disabled')}`);
+        TextContent.set(statusDiv, `Mode: ${ed.mode.get()}, Enabled: ${ed.options.get('enabled')}`);
       });
     }
   });
