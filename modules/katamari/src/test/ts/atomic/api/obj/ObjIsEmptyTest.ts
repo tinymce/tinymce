b75c276fc9d10baaa5708ca2952d32d7f75141ea
Andrew Herron <thespyder@programmer.net>
2024-12-13 12:03:23 +1100

    TINY-11377: Converted `isEmpty` to an assertion function, improving cases with optional object properties

--- modules/katamari/src/test/ts/atomic/api/obj/ObjIsEmptyTest.ts
+++ modules/katamari/src/test/ts/atomic/api/obj/ObjIsEmptyTest.ts
@@ -4,6 +4,8 @@ import fc from 'fast-check';
 
 import * as Obj from 'ephox/katamari/api/Obj';
 
+const makePossiblyEmptyValue = (): { hasKey: boolean } | {} => ({ hasKey: true });
+
 describe('atomic.katamari.api.obj.ObjIsEmptyTest', () => {
   it('unit tests', () => {
     assert.isTrue(Obj.isEmpty({}));
@@ -19,5 +21,13 @@ describe('atomic.katamari.api.obj.ObjIsEmptyTest', () => {
         assert.isFalse(Obj.isEmpty(o));
       }
     ));
+  });
+
+  it('helps TypeScript understand the empty type', () => {
+    const maybeEmpty = makePossiblyEmptyValue();
+    if (!Obj.isEmpty(maybeEmpty)) {
+      const _emptyType: { hasKey: boolean } = maybeEmpty;
+      assert.isTrue(_emptyType.hasKey);
+    }
   });
 });
