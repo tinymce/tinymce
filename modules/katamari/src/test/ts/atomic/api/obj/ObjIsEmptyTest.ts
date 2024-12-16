dd7778e739c2b4d7d90f14a0e089aa910d3bdf67
Andrew Herron <thespyder@programmer.net>
2024-12-16 12:11:27 +1100

    TINY-11377: Added test for is empty

--- modules/katamari/src/test/ts/atomic/api/obj/ObjIsEmptyTest.ts
+++ modules/katamari/src/test/ts/atomic/api/obj/ObjIsEmptyTest.ts
@@ -4,7 +4,7 @@ import fc from 'fast-check';
 
 import * as Obj from 'ephox/katamari/api/Obj';
 
-const makePossiblyEmptyValue = (): { hasKey: boolean } | {} => ({ hasKey: true });
+const makePossiblyEmptyValue = (isEmpty: boolean): { hasKey: boolean } | {} => isEmpty ? {} : { hasKey: true };
 
 describe('atomic.katamari.api.obj.ObjIsEmptyTest', () => {
   it('unit tests', () => {
@@ -23,11 +23,24 @@ describe('atomic.katamari.api.obj.ObjIsEmptyTest', () => {
     ));
   });
 
-  it('helps TypeScript understand the empty type', () => {
-    const maybeEmpty = makePossiblyEmptyValue();
+  it('helps TypeScript understand the non-empty type', () => {
+    const maybeEmpty = makePossiblyEmptyValue(false);
     if (!Obj.isEmpty(maybeEmpty)) {
-      const _emptyType: { hasKey: boolean } = maybeEmpty;
-      assert.isTrue(_emptyType.hasKey);
+      const emptyType: { hasKey: boolean } = maybeEmpty;
+      assert.isTrue(emptyType.hasKey);
+    } else {
+      assert.fail('Expected non-empty object');
+    }
+  });
+
+  /* The compiler won't complain about this case even if the value is non-empty, but just for completeness */
+  it('helps TypeScript understand the empty type', () => {
+    const maybeEmpty = makePossiblyEmptyValue(true);
+    if (Obj.isEmpty(maybeEmpty)) {
+      const emptyType: {} = maybeEmpty;
+      assert.deepStrictEqual(emptyType, {});
+    } else {
+      assert.fail('Expected empty object');
     }
   });
 });
