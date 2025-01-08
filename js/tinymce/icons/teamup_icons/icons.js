3ea7550800197b620c69b13778e0edb4f2a71d5b
Cristian <cristian.buj@teamup.com>
2025-01-08 12:13:20 +0100

    add icons for dropbox & upload buttons

---
+++ js/tinymce/icons/teamup_icons/icons.js
@@ -0,0 +1,6 @@
+tinymce.IconManager.add('teamup_icons', {
+  icons: {
+    'event-tinymce-dropbox': '<svg width="24" height="24"><path d="M7 4 2 7.2l5 3.2 5.1-3.2zm10.2 0L12 7.2l5 3.2 5.1-3.2zM2 13.7l5 3.2 5.1-3.2-5-3.3zm15.2-3.3L12 13.7l5 3.2 5.1-3.2zM7 18l5 3.3 5-3.3-5-3.2z" fill-rule="nonzero"/></svg>',
+    'event-tinymce-upload': '<svg width="24" height="24"><g transform="translate(5 3)" fill-rule="nonzero"><path d="M4.1 13.3h6.2V7.2h4L7.3 0 0 7.2h4.1z"/><rect width="14.4" height="2.1" y="15.4" rx="1"/></g></svg>',
+  }
+});
