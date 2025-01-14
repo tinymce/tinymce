952db890882ec34cf8eaa758fe3dea0a4091a713
Hamza Benkhaldoun <hamza0867@gmail.com>
2025-01-14 17:54:42 +0300

    TINY-11707: Make changes as per review

--- modules/tinymce/src/plugins/link/test/ts/browser/AttributesPostprocessTest.ts
+++ modules/tinymce/src/plugins/link/test/ts/browser/AttributesPostprocessTest.ts
@@ -9,7 +9,7 @@ import Plugin from 'tinymce/plugins/link/Plugin';
 
 import { TestLinkUi } from '../module/TestLinkUi';
 
-describe('browser.tinymce.plugins.link.AttributesPostprocess', () => {
+describe('browser.tinymce.plugins.link.AttributesPostprocessTest', () => {
   const newRel = 'noopener noreferrer';
   const newHref = 'https://www.google.com';
   const attrsOverride = (attrs: any) => {
