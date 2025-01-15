b0289d0bd9ddad846ae45dcb37e46a807766627a
Andrew Herron <thespyder@programmer.net>
2025-01-15 17:38:07 +1100

    TINY-11177: Add real audio and video files to the routes using 0.01s clips from royalty-free sources: https://www.pexels.com https://www.bensound.com

--- modules/tinymce/src/plugins/media/test/ts/browser/PlaceholderTest.ts
+++ modules/tinymce/src/plugins/media/test/ts/browser/PlaceholderTest.ts
@@ -84,15 +84,15 @@ describe('browser.tinymce.plugins.media.core.PlaceholderTest', () => {
       ));
 
       it('TBA: Set and assert video placeholder structure', () => pTestPlaceholder(hook.editor(),
-        '/custom/tinymce/video.mp4',
+        '/custom/video.mp4',
         '<p><video controls="controls" width="300" height="150">\n' +
-        '<source src="custom/tinymce/video.mp4" type="video/mp4"></video></p>',
+        '<source src="custom/video.mp4" type="video/mp4"></video></p>',
         placeholderStructure
       ));
 
       it('TBA: Set and assert audio placeholder structure', () => pTestPlaceholder(hook.editor(),
-        '/custom/tinymce/audio.mp3',
-        '<p><audio src="custom/tinymce/audio.mp3" controls="controls"></audio></p>',
+        '/custom/audio.mp3',
+        '<p><audio src="custom/audio.mp3" controls="controls"></audio></p>',
         placeholderStructure
       ));
     });
