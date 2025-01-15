004422c9f1ad49cddb6bb6d45b29cb7c786bc828
Arvin Han <20640097+ArvinJ-H@users.noreply.github.com>
2025-01-15 18:46:35 +1100

    added 3 button

---
+++ modules/tinymce/src/core/demo/ts/demo/DummyDemo.ts
@@ -0,0 +1,97 @@
+import { icon } from '@ephox/bridge/src/main/ts/ephox/bridge/core/ComponentSchema';
+import { Optional } from '@ephox/katamari';
+
+import { Editor, RawEditorOptions, TinyMCE } from 'tinymce/core/api/PublicApi';
+
+declare let tinymce: TinyMCE;
+
+export default (): void => {
+
+  const loadCSS = (editor: Editor, rawCss: string) => {
+    // Use the editors UI stylesheet loader if it's available
+    const uiStyleSheetLoader = Optional.from(editor.ui?.styleSheetLoader).getOr(tinymce.DOM.styleSheetLoader);
+
+    uiStyleSheetLoader.loadRawCss('poc-skin', rawCss);
+    editor.on('remove', () => uiStyleSheetLoader.unloadRawCss('poc-skin'));
+  };
+
+  const settings = (selector: string, skin?: string): RawEditorOptions => ({
+    selector,
+    setup: (ed) => {
+      const Dialog = (editor: Editor): { open: () => void } => {
+        return {
+          open: () => {
+            editor.windowManager.open(
+              {
+                title: 'This is a dummy dialog',
+                size: 'medium',
+                body: {
+                  type: 'panel',
+                  items: [
+                    {
+                      type: 'input',
+                      name: 'name',
+                      label: 'Name',
+                      placeholder: 'Enter your name'
+                    }
+                  ]
+                },
+                buttons: [
+                  {
+                    type: 'cancel',
+                    name: 'close',
+                    text: 'Close',
+                    primary: true
+                  },
+                  {
+                    type: 'togglebutton',
+                    name: 'close',
+                    text: 'Close',
+                    primary: true,
+                    icon: 'copy',
+                    align: 'end'
+                  },
+                  {
+                    type: 'togglebutton',
+                    name: 'close',
+                    text: 'Close',
+                    primary: true,
+                    icon: 'format-code',
+                  }
+                ],
+                initialData: {}
+              }
+            );
+          }
+        };
+      };
+      ed.ui.registry.addButton('demoButton', {
+        text: 'Dummy',
+        onAction: Dialog(ed).open,
+      });
+
+      if (skin) {
+        ed.on('PostRender', () => {
+          loadCSS(ed, skin);
+        });
+      }
+    },
+    toolbar: 'demoButton',
+  });
+
+  tinymce.init(settings('textarea.default'));
+
+  const pinkSkin = `
+  :root {
+    --light-bg: hsl(350, 100%, 90%);
+    --dark-bg: hsl(350, 100%, 30%);
+  }`;
+  tinymce.init(settings('textarea.skin', pinkSkin ));
+
+  /*
+    Notes:
+      1. We will need to make sure that the skin css is loaded AFTER our css.
+      2. All of the CSS Custom Properties are global and public. User will see them and be able to change them easily.
+      3. As the CSS Custom Properties are global both editors will be skinned. Appying a skin will mean all editors on the website will be skinned the same way. Not sure if there is a work around this or if this is a negative or not
+  */
+};
