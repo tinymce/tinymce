dce8e47882eb71ab3c5cb7bf20c742917dd3d73a
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2025-02-27 09:23:31 +0100

    TINY-11890: fix behavior when a context form has `setInputEnabled(false)` on init (#10199)
    * TINY-11890: fix behavior when a context form has `setInputEnabled(false)` on init
    
    * TINY-11890: add changelog entry
    
    * TINY-11890: refactor
    
    * TINY-11890: typo
    
    * TINY-11890: typos + remove useless row

--- modules/tinymce/src/themes/silver/main/ts/ui/context/ContextFormTextInput.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/context/ContextFormTextInput.ts
@@ -1,6 +1,7 @@
 import { AddEventsBehaviour, AlloyComponent, AlloyEvents, Behaviour, Disabling, FormField, GuiFactory, Input, Keying, NativeEvents, SketchSpec } from '@ephox/alloy';
 import { InlineContent } from '@ephox/bridge';
 import { Cell, Fun, Optional } from '@ephox/katamari';
+import { SelectorFind } from '@ephox/sugar';
 
 import { UiFactoryBackstageProviders } from '../../backstage/Backstage';
 import * as UiState from '../../UiState';
@@ -55,7 +56,16 @@ export const renderContextFormTextInput = (
       AddEventsBehaviour.config('input-events', [
         onControlAttached<InlineContent.ContextFormInstanceApi<string>>({
           onSetup: ctx.onSetup,
-          getApi: ContextFormApi.getFormApi,
+          getApi: (comp) => {
+            const closestFocussableOpt = SelectorFind.ancestor(comp.element, '.tox-toolbar').bind((toolbar) =>
+              SelectorFind.descendant<HTMLButtonElement>(toolbar, 'button:enabled')
+            );
+
+            return closestFocussableOpt.fold(
+              () => ContextFormApi.getFormApi(comp),
+              (closestFocussable) => ContextFormApi.getFormApi(comp, closestFocussable)
+            );
+          },
           onBeforeSetup: Keying.focusIn
         }, editorOffCell),
         onControlDetached({ getApi: ContextFormApi.getFormApi }, editorOffCell),
