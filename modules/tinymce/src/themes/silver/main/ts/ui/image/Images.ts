10f9549e52daba49d9a85bc5bdc1038c3ba41161
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2025-03-06 09:57:06 +0100

    TINY-11847: test with a different layout

--- modules/tinymce/src/themes/silver/main/ts/ui/image/Images.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/image/Images.ts
@@ -1,6 +1,6 @@
 import { AddEventsBehaviour, AlloyEvents, Behaviour, SimpleSpec } from '@ephox/alloy';
-import { Arr, Optional } from '@ephox/katamari';
-import { SelectorFilter, SelectorFind } from '@ephox/sugar';
+import { Optional } from '@ephox/katamari';
+import { Class, Insert, SelectorFind, SugarElement } from '@ephox/sugar';
 
 export type ImageProvider = () => Record<string, string>;
 
@@ -12,29 +12,46 @@ interface ImageSpec {
   readonly label: Optional<string>;
 }
 
-const getSvgWithLoading = (url: string): string => `<svg width="48" height="48" xmlns="http://www.w3.org/2000/svg">
-  <style>
-    .ball {
-      animation: bounce 2s infinite;
-    }
+// const getSvgWithLoading = (url: string): string => `<svg width="48" height="48" xmlns="http://www.w3.org/2000/svg">
+//   <style>
+//     .ball {
+//       animation: bounce 2s infinite;
+//     }
 
-    @keyframes bounce {
-      0%, 100% {
-        cy: 30px;
-      }
-      50% {
-        cy: 170px;
-      }
-    }
-  </style>
-  <circle class="ball" cx="10" cy="5" r="5" fill="red" />
-  <image
-    width="48"
-    height="48"
-    preserveaspectratio="xMidYMid slice"
-    href="${url}"
-  />
-</svg>`;
+//     @keyframes bounce {
+//       0%, 100% {
+//         cy: 30px;
+//       }
+//       50% {
+//         cy: 170px;
+//       }
+//     }
+//   </style>
+//   <circle class="ball" cx="10" cy="5" r="5" fill="red" />
+//   <image
+//     width="48"
+//     height="48"
+//     preserveaspectratio="xMidYMid slice"
+//     href="${url}"
+//   />
+// </svg>`;
+
+const getInnerHTML = (url: string): string => `
+  <div style="width: 48px; height: 48px; display: flex; align-items: center;">
+    <img src="${url}" />
+  </div>
+`;
+
+const spinnerWrapperClass = 'tox-uc-loading-spinner-wrapper';
+const spinnerElementClass = 'tox-uc-loading-spinner';
+
+const addSpinnerElement = (loadingElement: SugarElement) => {
+  Class.add(loadingElement, spinnerWrapperClass);
+
+  const spinnerElement = SugarElement.fromTag('div');
+  Class.add(spinnerElement, spinnerElementClass);
+  Insert.append(loadingElement, spinnerElement);
+};
 
 const renderImage = (spec: ImageSpec, imageUrl: string): SimpleSpec => {
   return {
@@ -42,18 +59,18 @@ const renderImage = (spec: ImageSpec, imageUrl: string): SimpleSpec => {
       tag: spec.tag,
       attributes: spec.attributes ?? {},
       classes: spec.classes,
-      innerHtml: getSvgWithLoading(imageUrl),
+      innerHtml: getInnerHTML(imageUrl),
     },
     components: [],
     behaviours: Behaviour.derive([
       ...spec.behaviours ?? [],
       AddEventsBehaviour.config('render-image-events', [
         AlloyEvents.runOnAttached((component) => {
-          SelectorFind.descendant<SVGImageElement>(component.element, 'image').each((image) => {
+          addSpinnerElement(component.element);
+          SelectorFind.descendant<SVGImageElement>(component.element, 'img').each((image) => {
             image.dom.addEventListener('load', () => {
-              Arr.each(SelectorFilter.descendants<SVGAElement>(component.element, 'circle, style'), (e) => {
-                e.dom.remove();
-              });
+              // eslint-disable-next-line no-console
+              console.log('img loaded');
             });
           });
         })
