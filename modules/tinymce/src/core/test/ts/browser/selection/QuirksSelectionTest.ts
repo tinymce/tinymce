239d76fccde5756d512806b9de24c125478cfc36
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2025-04-17 17:47:26 +0200

    TINY-11676: test refactoring

--- modules/tinymce/src/core/test/ts/browser/selection/QuirksSelectionTest.ts
+++ modules/tinymce/src/core/test/ts/browser/selection/QuirksSelectionTest.ts
@@ -1,7 +1,7 @@
-import { RealMouse } from '@ephox/agar';
+import { UiFinder } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
 import { Unicode } from '@ephox/katamari';
-import { TinyAssertions, TinyHooks } from '@ephox/wrap-mcagar';
+import { TinyAssertions, TinyDom, TinyHooks } from '@ephox/wrap-mcagar';
 
 import Editor from 'tinymce/core/api/Editor';
 
@@ -16,7 +16,15 @@ describe('browser.tinymce.selection.QuirksSelectionTest', () => {
     const editor = hook.editor();
     const initialContent = '<p><strong><img style="display: block; margin-left: auto;" src="img.jpg" width="1" height="1"></strong></p>';
     editor.setContent(initialContent);
-    await RealMouse.pClickOn('iframe => body => p');
+    const imgElement = await UiFinder.pWaitFor<HTMLElement>('', TinyDom.body(editor), 'img');
+    const pRect = imgElement.dom.getBoundingClientRect();
+    const clientX = pRect.left + (pRect.width / 2);
+    const clientY = pRect.top + (pRect.height / 2);
+    const point = editor.getDoc().caretPositionFromPoint(clientX, clientY);
+    if (point) {
+      editor.selection.getSel()?.setBaseAndExtent(point.offsetNode, point.offset, point.offsetNode, point.offset);
+    }
+
     editor.execCommand('mceInsertNewLine');
     TinyAssertions.assertContent(editor, `<p>${Unicode.nbsp}</p>` + initialContent);
   });
