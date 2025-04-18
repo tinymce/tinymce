0443341389e9f94ae2ccfb6ebe1f947dcb4b2c7d
Hamza Benkhaldoun <hamza0867@gmail.com>
2025-04-18 21:58:08 +0100

    TINY-11629: Upgrade acid to eslint v9

--- modules/acid/src/main/ts/ephox/acid/api/Main.ts
+++ modules/acid/src/main/ts/ephox/acid/api/Main.ts
@@ -1,4 +1,5 @@
 import * as ColourPicker from '../gui/ColourPicker';
+
 import * as HexColour from './colour/HexColour';
 import * as HsvColour from './colour/HsvColour';
 import * as RgbaColour from './colour/RgbaColour';
