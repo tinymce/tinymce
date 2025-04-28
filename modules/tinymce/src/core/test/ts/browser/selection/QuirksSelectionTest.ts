9ba63e1ac86da8c9f550d1f3f416f6ddab120dbe
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2025-04-28 14:22:54 +0200

    TINY-11676: refactor tests

--- modules/tinymce/src/core/test/ts/browser/selection/QuirksSelectionTest.ts
+++ modules/tinymce/src/core/test/ts/browser/selection/QuirksSelectionTest.ts
@@ -1,6 +1,6 @@
 import { UiFinder, Waiter } from '@ephox/agar';
-import { describe, it } from '@ephox/bedrock-client';
-import { Arr, Unicode } from '@ephox/katamari';
+import { before, describe, it } from '@ephox/bedrock-client';
+import { Unicode } from '@ephox/katamari';
 import { PlatformDetection } from '@ephox/sand';
 import { TinyAssertions, TinyDom, TinyHooks } from '@ephox/wrap-mcagar';
 
@@ -14,30 +14,75 @@ describe('browser.tinymce.selection.QuirksSelectionTest', () => {
   }, []);
   const platform = PlatformDetection.detect();
 
-  Arr.each([ true, false ], (before) => {
-    it('TINY-11676: pressing enter after clicking on a `p` containg a floating `img` in FireFox should not duplicate the image ' + (before ? '(before)' : '(after)'), async function () {
-      // this test is needed only for Firefox
-      if (!platform.browser.isFirefox()) {
-        this.skip();
-      }
+  before(function () {
+    // these test is needed only for Firefox
+    if (!platform.browser.isFirefox()) {
+      this.skip();
+    }
+  });
 
-      const editor = hook.editor();
-      const initialContent = '<p><strong><img style="display: block; margin-left: auto; margin-right: auto;" src="img.jpg" width="1" height="1"></strong></p>';
-      editor.setContent(initialContent);
-      const imgElement = await UiFinder.pWaitFor<HTMLElement>('', TinyDom.body(editor), 'img');
-      const pRect = imgElement.dom.getBoundingClientRect();
-      const clientX = before ? pRect.left - 2 : pRect.left + 2;
-      const clientY = pRect.top;
-      const mouseDownEvent = new MouseEvent('mousedown', {
-        bubbles: true,
-        cancelable: true,
-        clientX,
-        clientY
-      });
-      TinyDom.body(editor).dom.dispatchEvent(mouseDownEvent);
-      await Waiter.pTryUntil('selection now should be on the `strong`', () => TinyAssertions.assertCursor(editor, [ 0, 0 ], before ? 0 : 1));
-      editor.execCommand('mceInsertNewLine');
-      TinyAssertions.assertContent(editor, before ? `<p>${Unicode.nbsp}</p>${initialContent}` : `${initialContent}<p>${Unicode.nbsp}</p>`);
+  const floatingCenterStyle = 'display: block; margin-left: auto; margin-right: auto;';
+  const floatLeftStyle = 'float: left;';
+  const floatRightStyle = 'float: right;';
+  const testSelection = async (position: 'before' | 'on' | 'after', style: string) => {
+    const editor = hook.editor();
+    const initialContent = `<p><strong><img style="${style}" src="img.jpg" width="10" height="10"></strong></p>`;
+    editor.setContent(initialContent);
+    const imgElement = await UiFinder.pWaitFor<HTMLElement>('', TinyDom.body(editor), 'img');
+    const pRect = imgElement.dom.getBoundingClientRect();
+    const clientX = pRect.left + {
+      before: -2,
+      on: 2,
+      after: 12
+    }[position];
+
+    const clientY = pRect.top;
+    const mouseDownEvent = new MouseEvent('mousedown', {
+      bubbles: true,
+      cancelable: true,
+      clientX,
+      clientY
     });
+    TinyDom.body(editor).dom.dispatchEvent(mouseDownEvent);
+    switch (position) {
+      case 'before':
+        await Waiter.pTryUntil('selection now should be on the `strong`', () => TinyAssertions.assertCursor(editor, [ 0, 0 ], 0));
+        break;
+      case 'on':
+        await Waiter.pTryUntil('selection now should be on the `img`', () => TinyAssertions.assertSelection(editor, [ 0, 0 ], 0, [ 0, 0 ], 1));
+        break;
+      case 'after':
+        await Waiter.pTryUntil('selection now should be on the `strong`', () => TinyAssertions.assertCursor(editor, [ 0, 0 ], 1));
+        break;
+    }
+
+    editor.execCommand('mceInsertNewLine');
+    switch (position) {
+      case 'before':
+        TinyAssertions.assertContent(editor, `<p>${Unicode.nbsp}</p>${initialContent}`);
+        break;
+      case 'after':
+        TinyAssertions.assertContent(editor, `${initialContent}<p>${Unicode.nbsp}</p>`);
+        break;
+      case 'on':
+        TinyAssertions.assertContent(editor, `<p>${Unicode.nbsp}</p><p>${Unicode.nbsp}</p>`);
+        break;
+    }
+  };
+
+  it('TINY-11676: clicking on a `p` containg a center floating `img` in FireFox before the image', async () => {
+    await testSelection('before', floatingCenterStyle);
+  });
+
+  it('TINY-11676: clicking on a `p` containg a center floating `img` in FireFox after the image', async () => {
+    await testSelection('after', floatingCenterStyle);
+  });
+
+  it('TINY-11676: clicking on a `p` containg a left floating `img` in FireFox on the image', async () => {
+    await testSelection('on', floatLeftStyle);
+  });
+
+  it('TINY-11676: clicking on a `p` containg a right floating `img` in FireFox on the image', async () => {
+    await testSelection('on', floatRightStyle);
   });
 });
