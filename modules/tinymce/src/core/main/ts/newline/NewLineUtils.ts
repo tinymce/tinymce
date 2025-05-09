fb81ec44f9057d7573ea88f93667b95dbef84c47
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-05-09 16:11:43 +0200

    TINY-12102: Fix new list item formatting

--- modules/tinymce/src/core/main/ts/newline/NewLineUtils.ts
+++ modules/tinymce/src/core/main/ts/newline/NewLineUtils.ts
@@ -1,5 +1,5 @@
 import { Arr, Fun, Obj, Optional, Optionals, Unicode } from '@ephox/katamari';
-import { Css, SugarElement } from '@ephox/sugar';
+import { Css, Insert, SugarElement, Traverse } from '@ephox/sugar';
 
 import DOMUtils from '../api/dom/DOMUtils';
 import DomTreeWalker from '../api/dom/TreeWalker';
@@ -38,6 +38,9 @@ const moveToCaretPosition = (editor: Editor, root: Node): void => {
 
     if (firstChild && /^(UL|OL|DL)$/.test(firstChild.nodeName)) {
       root.insertBefore(dom.doc.createTextNode(Unicode.nbsp), root.firstChild);
+    } else if (firstChild && dom.isEmpty(firstChild)) {
+      const element = Traverse.leftmostLeaf(SugarElement.fromDom(firstChild));
+      Insert.append(element, SugarElement.fromText(Unicode.nbsp));
     }
   }
 
