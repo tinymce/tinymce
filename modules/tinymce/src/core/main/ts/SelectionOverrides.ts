2606e1d216d2051bffbe7161c9bcdb0a3d911aa9
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2025-05-20 09:58:24 +0200

    TINY-11753: fix tab selection inside caption

--- modules/tinymce/src/core/main/ts/SelectionOverrides.ts
+++ modules/tinymce/src/core/main/ts/SelectionOverrides.ts
@@ -161,9 +161,10 @@ const SelectionOverrides = (editor: Editor): SelectionOverrides => {
     });
 
     editor.on('focusin', (e) => {
-      if (fakeCaret.isShowing() && editor.getBody().contains(e.target) && e.target !== editor.getBody() && !editor.dom.isEditable(e.target.parentNode)) {
-        fakeCaret.hide();
-
+      if (e.target.textContent && editor.getBody().contains(e.target) && e.target !== editor.getBody() && !editor.dom.isEditable(e.target.parentNode)) {
+        if (fakeCaret.isShowing()) {
+          fakeCaret.hide();
+        }
         if (!e.target.contains(editor.selection.getNode())) {
           editor.selection.select(e.target, true);
           editor.selection.collapse(true);
