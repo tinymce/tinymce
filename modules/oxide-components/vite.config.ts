7bfaef805ac26c98a12f99ad0cbf08536de315f7
Hamza Benkhaldoun <hamza0867@gmail.com>
2025-05-27 19:55:49 +0100

    TINY-12133: fix build setup for preact

--- modules/oxide-components/vite.config.ts
+++ modules/oxide-components/vite.config.ts
@@ -18,12 +18,15 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: [ 'react' ],
+      external: [ 'react', 'react-dom', 'react-dom/client', 'react/jsx-runtime' ],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          react: 'React',
+          'react': 'React',
+          'react-dom': 'ReactDOM',
+          'react-dom/client': 'ReactDOMClient',
+          'react/jsx-runtime': 'ReactJSXRuntime',
         },
       },
     },
