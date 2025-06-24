4d8d581a8bc0fa094d830263f0c71c48cb6fa541
Spocke <spocke@moxiecode.com>
2025-06-24 07:50:21 +0200

    TINY-12220: Removed unused function

--- modules/tinymce/src/core/main/ts/html/KeepHtmlComments.ts
+++ modules/tinymce/src/core/main/ts/html/KeepHtmlComments.ts
@@ -4,13 +4,3 @@ export const encodeData = (data: string): string =>
 export const decodeData = (data: string): string =>
   data.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&');
 
-export const encodeComments = (target: HTMLElement): void => {
-  const walker = document.createTreeWalker(target, NodeFilter.SHOW_COMMENT);
-  let node: Node | null;
-
-  while ((node = walker.nextNode())) {
-    const commentNode = node as Comment;
-    commentNode.data = encodeData(commentNode.data);
-  }
-};
-
