1b010db6201ab60a6188b8b2cf1788909b93dd43
Spocke <spocke@moxiecode.com>
2025-06-25 07:47:47 +0200

    TINY-12172: Code cleanup

--- modules/tinymce/src/core/main/ts/bookmark/NodeStructureBookmark.ts
+++ modules/tinymce/src/core/main/ts/bookmark/NodeStructureBookmark.ts
@@ -1,20 +1,84 @@
+import { Type } from '@ephox/katamari';
+
 import DOMUtils from '../api/dom/DOMUtils';
 import * as NodeType from '../dom/NodeType';
 
 import * as NormalizeBookmarkPoint from './NormalizeBookmarkPoint';
 
-// TODO: This is a clone of the list bookmark code if we move lists to core then de-duplicate this #TINY-12172
-
 const DOM = DOMUtils.DOM;
 
 export interface Bookmark {
-  startContainer: Node;
-  startOffset: number;
-  endContainer?: Node;
-  endOffset?: number;
+  readonly startContainer: Node;
+  readonly startOffset: number;
+  readonly endContainer?: Node;
+  readonly endOffset?: number;
 }
 
 const defaultMarker = () => DOM.create('span', { 'data-mce-type': 'bookmark' });
+
+const setupEndPoint = (container: Node, offset: number, createMarker: () => HTMLElement) => {
+  if (NodeType.isElement(container)) {
+    const offsetNode = createMarker();
+
+    if (container.hasChildNodes()) {
+      if (offset === container.childNodes.length) {
+        container.appendChild(offsetNode);
+      } else {
+        container.insertBefore(offsetNode, container.childNodes[offset]);
+      }
+    } else {
+      container.appendChild(offsetNode);
+    }
+
+    return { container: offsetNode, offset: 0 };
+  } else {
+    return { container, offset };
+  }
+};
+
+const restoreEndPoint = (container: Node, offset: number) => {
+  const nodeIndex = (container: Node): number => {
+    let node = container.parentNode?.firstChild;
+    let idx = 0;
+
+    while (node) {
+      if (node === container) {
+        return idx;
+      }
+
+      // Skip data-mce-type=bookmark nodes
+      if (!NodeType.isElement(node) || node.getAttribute('data-mce-type') !== 'bookmark') {
+        idx++;
+      }
+
+      node = node.nextSibling;
+    }
+
+    return -1;
+  };
+
+  if (NodeType.isElement(container) && Type.isNonNullable(container.parentNode)) {
+    const node = container;
+    offset = nodeIndex(container);
+    container = container.parentNode;
+    DOM.remove(node);
+
+    if (!container.hasChildNodes() && DOM.isBlock(container)) {
+      container.appendChild(DOM.create('br'));
+    }
+  }
+
+  return { container, offset };
+};
+
+const createNormalizedRange = (startContainer: Node, startOffset: number, endContainer: Node, endOffset: number): Range => {
+  const rng = DOM.createRng();
+
+  rng.setStart(startContainer, startOffset);
+  rng.setEnd(endContainer, endOffset);
+
+  return NormalizeBookmarkPoint.normalizeRange(rng);
+};
 
 /**
  * Returns a range bookmark. This will convert indexed bookmarks into temporary span elements with
@@ -25,98 +89,25 @@ const defaultMarker = () => DOM.create('span', { 'data-mce-type': 'bookmark' });
  * becomes: <p><b><span data-mce-type="bookmark">|</span></b><b data-mce-type="bookmark">|</span></b></p>
  */
 const createBookmark = (rng: Range, createMarker: () => HTMLElement = defaultMarker): Bookmark => {
-  const bookmark: Partial<Bookmark> = {};
+  const { container: startContainer, offset: startOffset } = setupEndPoint(rng.startContainer, rng.startOffset, createMarker);
 
-  const setupEndPoint = (start?: boolean) => {
-    let container = rng[start ? 'startContainer' : 'endContainer'];
-    let offset = rng[start ? 'startOffset' : 'endOffset'];
-
-    if (NodeType.isElement(container)) {
-      const offsetNode = createMarker();
-
-      if (container.hasChildNodes()) {
-        if (offset === container.childNodes.length) {
-          container.appendChild(offsetNode);
-        } else {
-          container.insertBefore(offsetNode, container.childNodes[offset]);
-        }
-      } else {
-        container.appendChild(offsetNode);
-      }
-
-      container = offsetNode;
-      offset = 0;
-    }
-
-    bookmark[start ? 'startContainer' : 'endContainer'] = container;
-    bookmark[start ? 'startOffset' : 'endOffset'] = offset;
-  };
-
-  setupEndPoint(true);
-
-  if (!rng.collapsed) {
-    setupEndPoint();
+  if (rng.collapsed) {
+    return { startContainer, startOffset };
+  } else {
+    const { container: endContainer, offset: endOffset } = setupEndPoint(rng.endContainer, rng.endOffset, createMarker);
+    return { startContainer, startOffset, endContainer, endOffset };
   }
-
-  return bookmark as Bookmark;
 };
 
 const resolveBookmark = (bookmark: Bookmark): Range => {
-  const restoreEndPoint = (start?: boolean) => {
-    const nodeIndex = (container: Node): number => {
-      let node = container.parentNode?.firstChild;
-      let idx = 0;
+  const { container: startContainer, offset: startOffset } = restoreEndPoint(bookmark.startContainer, bookmark.startOffset);
 
-      while (node) {
-        if (node === container) {
-          return idx;
-        }
-
-        // Skip data-mce-type=bookmark nodes
-        if (!NodeType.isElement(node) || node.getAttribute('data-mce-type') !== 'bookmark') {
-          idx++;
-        }
-
-        node = node.nextSibling;
-      }
-
-      return -1;
-    };
-
-    let container: Node | null | undefined = bookmark[start ? 'startContainer' : 'endContainer'];
-    let offset = bookmark[start ? 'startOffset' : 'endOffset'];
-
-    if (!container) {
-      return;
-    }
-
-    if (NodeType.isElement(container) && container.parentNode) {
-      const node = container;
-      offset = nodeIndex(container);
-      container = container.parentNode;
-      DOM.remove(node);
-
-      if (!container.hasChildNodes() && DOM.isBlock(container)) {
-        container.appendChild(DOM.create('br'));
-      }
-    }
-
-    bookmark[start ? 'startContainer' : 'endContainer'] = container;
-    bookmark[start ? 'startOffset' : 'endOffset'] = offset as number;
-  };
-
-  restoreEndPoint(true);
-  restoreEndPoint();
-
-  const rng = DOM.createRng();
-
-  rng.setStart(bookmark.startContainer, bookmark.startOffset);
-
-  if (bookmark.endContainer) {
-    rng.setEnd(bookmark.endContainer, bookmark.endOffset as number);
+  if (!Type.isUndefined(bookmark.endContainer) && !Type.isUndefined(bookmark.endOffset)) {
+    const { container: endContainer, offset: endOffset } = restoreEndPoint(bookmark.endContainer, bookmark.endOffset);
+    return createNormalizedRange(startContainer, startOffset, endContainer, endOffset);
+  } else {
+    return createNormalizedRange(startContainer, startOffset, startContainer, startOffset);
   }
-
-  return NormalizeBookmarkPoint.normalizeRange(rng);
 };
 
 export {
