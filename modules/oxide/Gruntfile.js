a70aa224351f69cd031b56887ee7b3ce2d6c0388
Hamza Benkhaldoun <hamza0867@gmail.com>
2025-06-25 21:59:25 +0100

    TINY-12142: Oxide now ships with ts definitions for css files

--- modules/oxide/Gruntfile.js
+++ modules/oxide/Gruntfile.js
@@ -2,6 +2,11 @@ module.exports = function (grunt) {
   grunt.loadTasks('./tasks');
 
   grunt.initConfig({
+    shell: {
+      generateTsDefinitions: {
+        command: 'node ./tasks/ts-skins.js ./build'
+      }
+    },
     // Clean specified directories
     clean: {
       build: ['./build'],
@@ -94,7 +99,8 @@ module.exports = function (grunt) {
     'compileLess',
     'cssmin',
     'generateJsSkins',
-    'addLicenseHeaders'
+    'addLicenseHeaders',
+    'shell:generateTsDefinitions'
   ]);
 
   grunt.registerTask('start', [
