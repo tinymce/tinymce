dce76a3edcf311fc6bd5519df62da01282a6ab2b
Kim <45845989+kimwoodfield@users.noreply.github.com>
2025-06-30 15:54:30 +1000

    Update modules/tinymce/src/core/test/ts/module/test/PasteEventUtils.ts
    Co-authored-by: Andrew Herron <thespyder@programmer.net>

--- modules/tinymce/src/core/test/ts/module/test/PasteEventUtils.ts
+++ modules/tinymce/src/core/test/ts/module/test/PasteEventUtils.ts
@@ -71,7 +71,7 @@ const pWaitForAndAssertInputEvents = async (beforeinputEvent: SingletonEvent<Inp
     inputEvent.on((e) => {
       assert.equal(e.inputType, 'insertFromPaste', 'beforeinput event type should be "insertFromPaste"');
       // TINY-11373: Chromium >= 129, e.data is no longer null when pasting plain text
-      // TINY-12342: Chromium <= 137, e.data is no longer null when pasting plain text
+      // TINY-12342: Chromium > 137, e.data once again null when pasting plain text
       if (isNative && browser.isChromium() && browser.version.major >= 129 && browser.version.major <= 137) {
         assert.isNotNull(e.data, 'input event data should not be null');
       } else {
