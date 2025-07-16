b9c3ca146f9b9e10b3095d7a25bafcd693b03c28
Hamza Benkhaldoun <hamza0867@gmail.com>
2025-07-16 13:22:28 +0100

    TINY-12513: Add visual regression testing with playwright (#10465)

--- modules/oxide-components/visual-regression-test-ci.mjs
+++ modules/oxide-components/visual-regression-test-ci.mjs
@@ -1,40 +1,14 @@
 import { exec, execSync } from 'child_process';
 import { setTimeout } from 'timers';
 
-let devServer;
-let exitCode = 0;
 
-const cleanUp = () => {
-  if (devServer) {
-    devServer.kill();
-  }
-  process.exit(exitCode);
-};
+const devServer = exec('yarn start --ci');
 
-process.on('SIGINT', cleanUp);
-process.on('SIGTERM', cleanUp);
-
-const waitForServer = async () => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 5000);
+// Wait for the server to start
+setTimeout(() => {
+  execSync('yarn playwright test', {
+    stdio: 'inherit'
   });
-};
-
-const runTests = async () => {
-  try {
-    devServer = exec('yarn start --ci' );
-    await waitForServer();
-    execSync('yarn playwright test', {
-      stdio: 'inherit'
-    });
-  } catch (error) {
-    console.error('Running tests failed:', error);
-    exitCode = 1;
-  } finally {
-    cleanUp();
-  }
-}
-
-await runTests();
+  devServer.kill()
+  process.exit(0)
+}, 5000);
