a9b80ed28374d4a12bff8584306f0cdda52f5961
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-07-25 13:50:50 +0200

    TINY-12211: move default image generation to oxide components

--- modules/oxide-components/src/main/ts/main.ts
+++ modules/oxide-components/src/main/ts/main.ts
@@ -1,10 +1,12 @@
 import { Button } from './button/Button.component';
+import * as DefaultAvatar from './defaultavatar/DefaultAvatar';
 import * as KeyboardNavigationTypes from './keynav/keyboard/NavigationTypes';
-import * as FocusHelpers from './utils/FocusHelpers';
 import * as KeyboardNavigationHooks from './keynav/KeyboardNavigationHooks';
+import * as FocusHelpers from './utils/FocusHelpers';
 
 export {
   Button,
+  DefaultAvatar,
   FocusHelpers,
   KeyboardNavigationHooks,
   KeyboardNavigationTypes
