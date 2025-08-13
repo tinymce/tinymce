602359c6efbc539e5ccb2e94f291d6b9f2f89abf
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-08-13 09:23:13 +0200

    TINY-12664: update imports

--- modules/persona/src/test/ts/atomic/DefaultAvatarTest.ts
+++ modules/persona/src/test/ts/atomic/DefaultAvatarTest.ts
@@ -1,7 +1,7 @@
 import { context, describe, it } from '@ephox/bedrock-client';
 import { Optional, Fun } from '@ephox/katamari';
 import { assert } from 'chai';
-import * as DefaultAvatar from 'persona/DefaultAvatar';
+import * as DefaultAvatar from 'tiny/persona/DefaultAvatar';
 
 const extractAttribute = (attribute: string, avatar: string): Optional<string> => {
   const match = avatar.match(new RegExp(`${attribute}="([^"]+)"`));
