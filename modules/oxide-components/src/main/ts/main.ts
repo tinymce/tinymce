5c391a358e43ebcbed3c9dc39ea93f3d148c0826
Hamza Benkhaldoun <hamza0867@gmail.com>
2025-08-14 14:13:29 +0100

    TINY-12346: Create IconButton component

--- modules/oxide-components/src/main/ts/main.ts
+++ modules/oxide-components/src/main/ts/main.ts
@@ -1,10 +1,12 @@
-import { Button } from './button/Button.component';
+import { Button } from './components/button/Button.component';
+import { IconButton } from './components/iconbutton/IconButton.component';
 import * as KeyboardNavigationTypes from './keynav/keyboard/NavigationTypes';
 import * as KeyboardNavigationHooks from './keynav/KeyboardNavigationHooks';
 import * as FocusHelpers from './utils/FocusHelpers';
 
 export {
   Button,
+  IconButton,
   FocusHelpers,
   KeyboardNavigationHooks,
   KeyboardNavigationTypes
