5d51310a26da4e6160c2fa0ff3057bd81c0c5c14
ltrouton <46954949+ltrouton@users.noreply.github.com>
2025-08-29 08:45:27 +1000

    TINY-12807: Enforce consistent import and export types (tinymce plugins) (#10568)
    Related Ticket: TINY-12807
    
    Description of Changes:
    
    - Update open-source plugins to adhere to import/export type rules
    
    NOTE: The PRs are too big to properly review. The eslint auto-fixer largely did most of the work so should be safe.
    
    yarn build​ works witout issues with a tinymce.d.ts​ still correctly generated
    
    Pre-checks:
    
    - [x] Changelog entry added
    - [x] Tests have been added (if applicable)
    - [x] Branch prefixed with `feature/`, `hotfix/`or `spike/`
    
    Review:
    
    - [x] Milestone set
    - [x] Docs ticket created (if applicable)
    
    GitHub issues (if applicable):

--- modules/bridge/src/main/ts/ephox/bridge/api/View.ts
+++ modules/bridge/src/main/ts/ephox/bridge/api/View.ts
@@ -1,5 +1,15 @@
 import { createView, type View, type ViewInstanceApi, type ViewSpec } from '../components/view/View';
 import type { ViewButton, ViewButtonsGroup, ViewButtonsGroupSpec, ViewButtonSpec, ViewToggleButton, ViewToggleButtonSpec } from '../components/view/ViewButton';
 
-export type { View, ViewSpec, ViewInstanceApi, ViewButton, ViewButtonsGroup, ViewButtonSpec, ViewButtonsGroupSpec, ViewToggleButton, ViewToggleButtonSpec };
+export type {
+  View,
+  ViewSpec,
+  ViewInstanceApi,
+  ViewButton,
+  ViewButtonsGroup,
+  ViewButtonSpec,
+  ViewButtonsGroupSpec,
+  ViewToggleButton,
+  ViewToggleButtonSpec
+};
 export { createView };
