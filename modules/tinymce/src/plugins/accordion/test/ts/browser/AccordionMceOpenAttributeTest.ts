f0d53d73d45e76358249b40e76e6354afdc3f4bc
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-08-29 14:59:10 +0200

    TINY-12316: handle mode switch

--- modules/tinymce/src/plugins/accordion/test/ts/browser/AccordionMceOpenAttributeTest.ts
+++ modules/tinymce/src/plugins/accordion/test/ts/browser/AccordionMceOpenAttributeTest.ts
@@ -1,5 +1,6 @@
 import { describe, it } from '@ephox/bedrock-client';
-import { TinyAssertions, TinyHooks } from '@ephox/wrap-mcagar';
+import { Insert, Remove, SugarBody, SugarElement } from '@ephox/sugar';
+import { TinyAssertions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
 
 import Editor from 'tinymce/core/api/Editor';
 import Plugin from 'tinymce/plugins/accordion/Plugin';
@@ -11,7 +12,7 @@ describe('browser.tinymce.plugins.accordion.AccordionMceOpenAttributeTest', () =
       toolbar: 'accordiontoggle',
       base_url: '/project/tinymce/js/tinymce',
     },
-    [Plugin]
+    [ Plugin ]
   );
 
   it('TINY-12315: `data-mce-open` attributes should be added/removed when entering/leaving readonly mode', () => {
@@ -35,11 +36,11 @@ describe('browser.tinymce.plugins.accordion.AccordionMceOpenAttributeTest', () =
 
     TinyAssertions.assertRawContent(editor,
       '<details><summary>Toggle accordion</summary><p>Hidden info</p></details>' +
-      '<details open=""><summary>Toggle accordion</summary><p>Expanded info</p></details>'
+      '<details open="open"><summary>Toggle accordion</summary><p>Expanded info</p></details>'
     );
   });
 
-  it.skip('TINY-12315: `data-mce-open` attribute should be added when setting the content in readonly editor', () => {
+  it('TINY-12315: `data-mce-open` attribute should be added when setting the content in readonly editor', () => {
     const editor = hook.editor();
     editor.mode.set('readonly');
     editor.setContent(
@@ -54,19 +55,33 @@ describe('browser.tinymce.plugins.accordion.AccordionMceOpenAttributeTest', () =
   });
 
   describe('readonly by default', () => {
-    // TODO: readonly: true is not working, and initialContent is not working as well
-    const readonlyHook = TinyHooks.bddSetup<Editor>(
+    const setupElement = () => {
+      const container = SugarElement.fromTag('div');
+      const editorElm = SugarElement.fromTag('textarea');
+      editorElm.dom.innerHTML =
+        '<details><summary>Toggle accordion</summary><p>Hidden info</p></details>' +
+        '<details open><summary>Toggle accordion</summary><p>Expanded info</p></details>';
+      Insert.append(container, editorElm);
+      Insert.append(SugarBody.body(), container);
+
+      return {
+        element: editorElm,
+        teardown: () => Remove.remove(container)
+      };
+    };
+
+    const readonlyHook = TinyHooks.bddSetupFromElement<Editor>(
       {
         plugins: 'accordion',
         toolbar: 'accordiontoggle',
         base_url: '/project/tinymce/js/tinymce',
-        readonly: true,
-        initialContent: '<h1>Hello</h1>'
+        readonly: true
       },
+      setupElement,
       [ Plugin ]
     );
 
-    it.only('TINY-12315: `data-mce-open` attribute should be added when editor is in readonly by default', () => {
+    it('TINY-12315: `data-mce-open` attribute should be added when editor is in readonly by default', () => {
       const editor = readonlyHook.editor();
       TinyAssertions.assertRawContent(editor,
         '<details data-mce-open="false"><summary>Toggle accordion</summary><p>Hidden info</p></details>' +
@@ -75,10 +90,29 @@ describe('browser.tinymce.plugins.accordion.AccordionMceOpenAttributeTest', () =
     });
   });
 
-  /* TODO: data-mce-open should be added when editor is in readonly by default */
-  /* TODO: data-mce-open should be added when content is set in readonly mode (see if possible) */
-  /* TODO: data-mce-open should be removed when entering design mode */
-  /* TODO: open should change it's value to `data-mce-open` when entering design mode */
+  it(`TINY-12315: Update the value of 'open' attribute, to reflect the value in 'data-mce-open' attribute when entering design mode`, () => { 
+    const editor = hook.editor();
+    editor.setContent(
+      '<details><summary>Toggle accordion</summary><p>Hidden info</p></details>' +
+      '<details open><summary>Toggle accordion</summary><p>Expanded info</p></details>'
+    );
+    editor.mode.set('readonly');
+
+    TinySelections.setCursor(editor, [ 0, 0, 0 ], 0);
+    editor.execCommand('ToggleAccordion');
+    TinySelections.setCursor(editor, [ 1, 0, 0 ], 0);
+    editor.execCommand('ToggleAccordion');
+    TinyAssertions.assertRawContent(editor,
+      '<details data-mce-open="false" open="open"><summary>Toggle accordion</summary><p>Hidden info</p></details>' +
+      '<details data-mce-open="true" data-mce-selected="1"><summary>Toggle accordion</summary><p>Expanded info</p></details>'
+    );
+    editor.mode.set('design');
+
+    TinyAssertions.assertRawContent(editor,
+      '<details><summary>Toggle accordion</summary><p>Hidden info</p></details>' +
+      '<details data-mce-selected="1" open="open"><summary>Toggle accordion</summary><p>Expanded info</p></details>'
+    );
+  });
   /* TODO: getContent in readonly mode should replace open with data-mce-open and remove data-mce-open */
   /* TODO: getContent in design mode should replace data-mce-open */
 });
