888c6c702a00233a125a7fda4dce68c5665fedd9
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-09-02 14:45:50 +0200

    TINY-12316: use serializer and deserializer

--- modules/tinymce/src/plugins/accordion/main/ts/core/readonly/Readonly.ts
+++ modules/tinymce/src/plugins/accordion/main/ts/core/readonly/Readonly.ts
@@ -3,6 +3,7 @@ import { SugarElement } from '@ephox/sugar';
 
 import Editor from 'tinymce/core/api/Editor';
 import { ExecCommandEvent } from 'tinymce/core/api/EventTypes';
+import AstNode from 'tinymce/core/api/html/Node';
 import { EditorEvent } from 'tinymce/core/api/util/EventDispatcher';
 
 import * as Utils from './Utils';
@@ -21,54 +22,53 @@ const setup = (editor: Editor): void => {
     }
   });
 
-  // Save snapshot of accordion state on entering readonly
+  editor.on('PreInit', () => {
+    const { serializer, parser } = editor;
+    const accordionTag = 'details';
+
+    /*
+      Using `editor.setContent` is possible in `readonly` mode. `setContent` may add new <details> elements
+      therefore we have to add `data-mce-open` attributes.
+
+      Purpose:
+        - add 'data-mce-open' attribute to <details> elements if the editor is in readonly mode
+    */
+    parser.addNodeFilter(accordionTag, (nodes) => {
+      if (editor.readonly) {
+        addTemporaryAttributes(nodes);
+      }
+    });
+
+    /*
+      Using `editor.getContent` in `readonly` mode we have to update <details> `open` attribute according to
+      `data-mce-open` attribute and remove `data-mce-open` attribute.
+
+      Purpose:
+      - in readonly mode: update <details> `open` attribute according to `data-mce-open` attribute value
+      - in readonly mode: remove `data-mce-open` attribute
+    */
+    serializer.addNodeFilter(accordionTag, (nodes) => {
+      if (editor.readonly) {
+        restoreNormalState(nodes);
+      }
+    });
+  });
+
   editor.on('SwitchMode', (event) => {
-    const editorBofy = SugarElement.fromDom(editor.getBody());
-    if (event.mode === 'readonly') {
-      addTemporaryAttributes(editorBofy);
-    } else {
-      restoreNormalState(editorBofy);
-    }
-  });
-
-  /*
-    Using `editor.setContent` is possible in `readonly` mode. `setContent` may add new <details> elements
-    therefore we have to add `data-mce-open` attributes.
-
-    TODO: Here I'm doing this after setting the content. I could also utilise `BeforeSetContent`
-    but this would require serializing the content, as in BeforeSetContent event `content` is a string.
-
-    TODO: Re-think. Adding a new element in readonly-mode and re-iterating over all the details could override data-mce-open
-    to the current open attribute value. Which could be tempered with in readonly mode.
-
-    TODO: Should we ignore SetContent events when format is raw?
-  */
-  editor.on('SetContent', () => {
     const editorBody = SugarElement.fromDom(editor.getBody());
-    if (editor.readonly) {
-      addTemporaryAttributes(editorBody);
-    }
-  });
-
-  /*
-    TODO: A lot of places overriding this event excludes partial selection, should I do this as well?
-    TODO: Should we Ignore GetContent override even when format is raw?
-  */
-  editor.on('GetContent', (event) => {
-    // TODO: I know this can't be done that way. Unfurtunately I can't parse the content here. So I will need to work on a string level...
-    if (editor.readonly) {
-      const currentContent = event.content;
-      const parsedContent = new DOMParser().parseFromString(currentContent, 'text/html');
-      restoreNormalState(SugarElement.fromDom(parsedContent));
-      event.content = parsedContent.body.innerHTML;
+    const details = Utils.getDetailsElements(editorBody);
+    if (event.mode === 'readonly') {
+      addTemporaryAttributes(details);
+    } else {
+      restoreNormalState(details);
     }
   });
 };
 
-const restoreNormalState = (scope: SugarElement<Node>) => {
+const restoreNormalState = (detailsElements: Array<SugarElement<HTMLDetailsElement> | AstNode>) => {
   Arr.each(
     // At this point every <details> should have data-mce-open attribute. But I will ignore those that don't - just in case.
-    Arr.filter(Utils.getDetailsElements(scope), Utils.hasMceOpenAttribute),
+    Arr.filter(detailsElements, Utils.hasMceOpenAttribute),
     (details) => {
       const mceOpen = Utils.getMceOpenAttribute(details);
       Utils.removeMceOpenAttribute(details);
@@ -81,9 +81,9 @@ const restoreNormalState = (scope: SugarElement<Node>) => {
   );
 };
 
-const addTemporaryAttributes = (scope: SugarElement<Node>) =>
+const addTemporaryAttributes = (detailsElements: Array<SugarElement<HTMLDetailsElement> | AstNode>) =>
   Arr.each(
-    Utils.getDetailsElements(scope),
+    detailsElements,
     (details) => Utils.setMceOpenAttribute(details, Utils.hasOpenAttribute(details))
   );
 
--- modules/tinymce/src/plugins/accordion/main/ts/core/readonly/Utils.ts
+++ modules/tinymce/src/plugins/accordion/main/ts/core/readonly/Utils.ts
@@ -1,4 +1,7 @@
+import { Arr } from '@ephox/katamari';
 import { Attribute, SelectorFilter, SugarElement } from '@ephox/sugar';
+
+import AstNode from 'tinymce/core/api/html/Node';
 
 const MCE_OPEN = 'data-mce-open';
 const OPEN = 'open';
@@ -6,26 +9,56 @@ const OPEN = 'open';
 const getDetailsElements = (scope: SugarElement<Node>): Array<SugarElement<HTMLDetailsElement>> =>
   SelectorFilter.descendants<HTMLDetailsElement>(scope, 'details');
 
-const hasOpenAttribute = (element: SugarElement<HTMLDetailsElement>): boolean =>
-  Attribute.has(element, OPEN);
+const hasOpenAttribute = (element: SugarElement<HTMLDetailsElement> | AstNode): boolean =>
+  hasAttribute(element, OPEN);
 
-const setOpenAttribute = (element: SugarElement<HTMLDetailsElement>): void =>
-  Attribute.set(element, OPEN, 'open');
+const setOpenAttribute = (element: SugarElement<HTMLDetailsElement> | AstNode): void =>
+  setAttribute(element, OPEN, 'open');
 
-const removeOpenAttribute = (element: SugarElement<HTMLDetailsElement>): void =>
-  Attribute.remove(element, OPEN);
+const removeOpenAttribute = (element: SugarElement<HTMLDetailsElement> | AstNode): void =>
+  removeAttribute(element, OPEN);
 
-const hasMceOpenAttribute = (element: SugarElement<HTMLDetailsElement>): boolean =>
-  Attribute.has(element, MCE_OPEN);
+const hasMceOpenAttribute = (element: SugarElement<HTMLDetailsElement> | AstNode): boolean =>
+  hasAttribute(element, MCE_OPEN);
 
-const setMceOpenAttribute = (element: SugarElement<HTMLDetailsElement>, value: boolean): void =>
-  Attribute.set(element, MCE_OPEN, value);
+const setMceOpenAttribute = (element: SugarElement<HTMLDetailsElement> | AstNode, value: boolean): void =>
+  setAttribute(element, MCE_OPEN, value);
 
-const removeMceOpenAttribute = (element: SugarElement<HTMLDetailsElement>): void =>
-  Attribute.remove(element, MCE_OPEN);
+const removeMceOpenAttribute = (element: SugarElement<HTMLDetailsElement> | AstNode): void =>
+  removeAttribute(element, MCE_OPEN);
 
-const getMceOpenAttribute = (element: SugarElement<HTMLDetailsElement>): boolean =>
-  Attribute.get(element, MCE_OPEN)?.toLowerCase() === 'true' ? true : false;
+const getMceOpenAttribute = (element: SugarElement<HTMLDetailsElement> | AstNode): boolean =>
+  getAttributeValue(element, MCE_OPEN)?.toLowerCase() === 'true' ? true : false;
+
+const hasAttribute = (element: SugarElement<Element> | AstNode, attribute: string): boolean => {
+  if (element instanceof AstNode) {
+    return Arr.exists(element.attributes ?? [], ({ name }) => name === attribute);
+  }
+  return Attribute.has(element, attribute);
+};
+
+const getAttributeValue = (element: SugarElement<Element> | AstNode, attribute: string): string | undefined => {
+  if (element instanceof AstNode) {
+    return element.attr(attribute);
+  }
+  return Attribute.get(element, attribute);
+};
+
+const removeAttribute = (element: SugarElement<Element> | AstNode, attribute: string) => {
+  if (element instanceof AstNode) {
+    element.attr(attribute, null);
+  } else {
+    Attribute.remove(element, attribute);
+  }
+};
+
+const setAttribute = (element: SugarElement<Element> | AstNode, attribute: string, value: string | boolean) => {
+  if (element instanceof AstNode) {
+    element.attr(attribute, value + '');
+  } else {
+    Attribute.set(element, attribute, value);
+  }
+};
 
 export {
   getDetailsElements,
