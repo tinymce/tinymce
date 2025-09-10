835f5106210c9893d779f77120eefbb2998693b6
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-09-10 13:45:30 +0200

    TINY-12315: Update import

--- modules/tinymce/src/core/main/ts/mode/Disabled.ts
+++ modules/tinymce/src/core/main/ts/mode/Disabled.ts
@@ -1,4 +1,4 @@
-import { Arr, type Optional, Strings } from '@ephox/katamari';
+import { Arr, Fun, type Optional, Strings } from '@ephox/katamari';
 import { Attribute, Compare, ContentEditable, SelectorFilter, SelectorFind, SugarElement, SugarNode } from '@ephox/sugar';
 
 import type Editor from '../api/Editor';
