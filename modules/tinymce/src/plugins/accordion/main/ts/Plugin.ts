5f685381ccf67a9ad2030e1269789d52420c8474
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-09-10 13:36:26 +0200

    TINY-12862: Normalize  attribute value for accordions

--- modules/tinymce/src/plugins/accordion/main/ts/Plugin.ts
+++ modules/tinymce/src/plugins/accordion/main/ts/Plugin.ts
@@ -1,7 +1,7 @@
 import PluginManager from 'tinymce/core/api/PluginManager';
 
 import * as Commands from './api/Commands';
-import * as FilterContent from './core/FilterContent';
+import * as FilterContent from './core/FilterContent/FilterContent';
 import * as Keyboard from './core/Keyboard';
 import * as Quirks from './core/Quirks';
 import * as Buttons from './ui/Buttons';
