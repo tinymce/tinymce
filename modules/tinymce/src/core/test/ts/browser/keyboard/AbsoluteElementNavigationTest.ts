1fa7b5adcd2f354c1074ba92cb022e4917645b49
Ben Tran <ben.tran@tiny.cloud>
2025-09-16 22:31:12 +0930

    Update demo

--- modules/tinymce/src/core/test/ts/browser/keyboard/AbsoluteElementNavigationTest.ts
+++ modules/tinymce/src/core/test/ts/browser/keyboard/AbsoluteElementNavigationTest.ts
@@ -4,12 +4,17 @@ import { Attribute, SugarElement } from '@ephox/sugar';
 import { TinyAssertions, TinyContentActions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
 import { assert } from 'chai';
 
-import Editor from 'tinymce/core/api/Editor';
+import type   Editor from 'tinymce/core/api/Editor';
 
-describe('browser.tinymce.core.FloatElementNavigationTest', () => {
+describe('browser.tinymce.core.AbsoluteElementNavigationTest', () => {
   const hook = TinyHooks.bddSetupLight<Editor>({
     base_url: '/project/tinymce/js/tinymce',
-    height: 300
+    height: 300,
+    setup: (ed: Editor) => {
+      ed.on('SelectionChange', () => {
+        console.log(ed.selection.getNode());
+      });
+    }
   }, [], true);
 
   const assertNode = (editor: Editor, f: (node: Node) => boolean) => {
@@ -17,7 +22,7 @@ describe('browser.tinymce.core.FloatElementNavigationTest', () => {
     assert.isTrue(f(node), 'Check selection is node');
   };
 
-  it('TINY-10326: Able to move cursor to and from a float element', async () => {
+  it.skip('TINY-10326: Move the caret around an absolute CEF element', async () => {
     const editor = hook.editor();
     editor.setContent(
       `<p>Tobias<span style="position: absolute; right: 30px;" contenteditable="false"> button </span><br>Linus</p>`
@@ -40,5 +45,30 @@ describe('browser.tinymce.core.FloatElementNavigationTest', () => {
     // Move the cursor to 'Tobias|'
     TinyContentActions.keydown(editor, Keys.left());
     TinyAssertions.assertCursor(editor, [ 0, 0 ],'Tobias'.length);
+  });
+
+  it('TINY-10326: Move the caret around an absolute element (not CEF)', async () => {
+    const editor = hook.editor();
+    editor.setContent(
+      `<p>Absolute <span style="position: absolute; right: 30px;">element</span>after</p>`
+    );
+
+    // Move the cursor to 'Tobias|'
+    TinySelections.setCursor(editor, [ 0, 0 ], 'Tobias'.length);
+    TinyContentActions.keydown(editor, Keys.right());
+    TinySelections.setCursor(editor, [0, 0, 0 , 0], 0);
+    // // Move back to the floated button
+    // assertNode(editor, (node) => Attribute.has(SugarElement.fromDom(node), 'data-mce-selected'));
+    // // Move the cursor to '|Linus'
+    // TinyContentActions.keydown(editor, Keys.right());
+    // TinyAssertions.assertCursor(editor, [ 0, 3 ], 0);
+
+    // // Move back to the floated button
+    // TinyContentActions.keydown(editor, Keys.left());
+    // assertNode(editor, (node) => Attribute.has(SugarElement.fromDom(node), 'data-mce-selected'));
+
+    // // Move the cursor to 'Tobias|'
+    // TinyContentActions.keydown(editor, Keys.left());
+    // TinyAssertions.assertCursor(editor, [ 0, 0 ],'Tobias'.length);
   });
 });
