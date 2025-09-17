d337b3a4eabc57492a5dedf182fb445e6fe4906d
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-09-17 15:23:26 +0200

    TINY-12316: prevent adding the undo level on blur

--- modules/tinymce/src/plugins/accordion/demo/ts/demo/Demo.ts
+++ modules/tinymce/src/plugins/accordion/demo/ts/demo/Demo.ts
@@ -11,6 +11,29 @@ tinymce.init({
   details_initial_state: 'inherited',
   details_serialize_state: 'inherited',
   readonly: true
+}).then((editors) => {
+  const editor = editors[0];
+
+  // Toggle readonly button functionality
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const toggleReadonlyBtn = document.getElementById('toggle-readonly')!;
+  toggleReadonlyBtn.addEventListener('click', () => {
+    const isReadonly = editor.mode.isReadOnly();
+    if (isReadonly) {
+      editor.mode.set('design');
+      toggleReadonlyBtn.textContent = 'Toggle readonly (currently: design)';
+    } else {
+      editor.mode.set('readonly');
+      toggleReadonlyBtn.textContent = 'Toggle readonly (currently: readonly)';
+    }
+  });
+
+  const printContentBtn = document.getElementById('print-content');
+  if (printContentBtn) {
+    printContentBtn.addEventListener('click', () => {
+      console.log('Editor content:', editor.getContent());
+    });
+  }
 });
 
 export {};
