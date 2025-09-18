3131887b691ac348b7c4d280a6265e120db18a86
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-09-18 10:59:42 +0200

    TINY-12316: cleanup

--- modules/tinymce/src/plugins/accordion/demo/ts/demo/Demo.ts
+++ modules/tinymce/src/plugins/accordion/demo/ts/demo/Demo.ts
@@ -5,35 +5,11 @@ declare let tinymce: TinyMCE;
 tinymce.init({
   selector: 'textarea.tinymce',
   license_key: 'gpl',
-  plugins: 'table lists image code accordion',
-  toolbar: 'undo redo | table | numlist bullist | image | accordion | code',
+  plugins: 'table lists image accordion code',
+  toolbar: 'table | numlist bullist | image | accordion | code',
   menu: { insert: { title: 'Insert', items: 'table | image | accordion' }},
   details_initial_state: 'inherited',
   details_serialize_state: 'inherited',
-  readonly: true
-}).then((editors) => {
-  const editor = editors[0];
-
-  // Toggle readonly button functionality
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const toggleReadonlyBtn = document.getElementById('toggle-readonly')!;
-  toggleReadonlyBtn.addEventListener('click', () => {
-    const isReadonly = editor.mode.isReadOnly();
-    if (isReadonly) {
-      editor.mode.set('design');
-      toggleReadonlyBtn.textContent = 'Toggle readonly (currently: design)';
-    } else {
-      editor.mode.set('readonly');
-      toggleReadonlyBtn.textContent = 'Toggle readonly (currently: readonly)';
-    }
-  });
-
-  const printContentBtn = document.getElementById('print-content');
-  if (printContentBtn) {
-    printContentBtn.addEventListener('click', () => {
-      console.log('Editor content:', editor.getContent());
-    });
-  }
 });
 
 export {};
