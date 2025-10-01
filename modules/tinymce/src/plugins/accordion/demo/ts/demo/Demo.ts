2c0aa7ada47d3722a1cc78f5f41eed0713be30a3
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-10-01 09:44:32 +0200

    TINY-12992: rollback demo changes

--- modules/tinymce/src/plugins/accordion/demo/ts/demo/Demo.ts
+++ modules/tinymce/src/plugins/accordion/demo/ts/demo/Demo.ts
@@ -6,7 +6,7 @@ tinymce.init({
   selector: 'textarea.tinymce',
   license_key: 'gpl',
   plugins: 'table lists image accordion code',
-  toolbar: 'undo redo | table | numlist bullist | image | accordion | code',
+  toolbar: 'table | numlist bullist | image | accordion | code',
   menu: { insert: { title: 'Insert', items: 'table | image | accordion' }},
   details_initial_state: 'inherited',
   details_serialize_state: 'inherited',
