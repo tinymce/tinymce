449f4311640eb1fa3089e41deb7e50eb4f333ec8
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-10-02 13:32:21 +0200

    TINY-12875: add boundries calculation

--- modules/oxide-components/src/main/ts/components/draggable/internals/calculations.ts
+++ modules/oxide-components/src/main/ts/components/draggable/internals/calculations.ts
@@ -1,7 +1,25 @@
-import type { Position, Shift } from './types';
+import type { Boundries, Position, Shift, Size } from './types';
 
 const delta = (start: Position, end: Position): { deltaX: number; deltaY: number } => ({ deltaX: end.x - start.x, deltaY: end.y - start.y });
 
 const round = (shift: Shift): Shift => ({ x: Math.round(shift.x), y: Math.round(shift.y) });
 
-export { delta, round };
+const clamp = (value: number, min: number, max: number): number => Math.min(max, Math.max(min, value));
+
+const boundries = (element: Position & Size, upperLeftCorner: Position, bottomRightCorner: Position): Boundries => {
+  const elementRight = element.x + element.width;
+  const elementBottom = element.y + element.height;
+
+  return {
+    shiftX: {
+      min: -(element.x - upperLeftCorner.x),
+      max: bottomRightCorner.x - elementRight
+    },
+    shiftY: {
+      min: -(element.y - upperLeftCorner.y),
+      max: bottomRightCorner.y - elementBottom
+    }
+  };
+};
+
+export { delta, round, clamp, boundries };
--- modules/oxide-components/src/test/ts/browser/components/draggable/Calculations.spec.ts
+++ modules/oxide-components/src/test/ts/browser/components/draggable/Calculations.spec.ts
@@ -1,4 +1,4 @@
-import { delta, round } from 'oxide-components/components/draggable/internals/calculations';
+import { delta, round, clamp, boundries } from 'oxide-components/components/draggable/internals/calculations';
 import { describe, expect, it } from 'vitest';
 
 describe('browser.draggable.calculations', () => {
@@ -37,6 +37,52 @@ describe('browser.draggable.calculations', () => {
 
       expect(result.x).toBe(-6);
       expect(result.y).toBe(-3);
+    });
+  });
+
+  describe('clamp', () => {
+    it('clamp', () => {
+      expect(clamp(5, 0, 10)).toBe(5);
+      expect(clamp(15, 0, 10)).toBe(10);
+      expect(clamp(-5, 0, 10)).toBe(0);
+      expect(clamp(0, 0, 10)).toBe(0);
+      expect(clamp(10, 0, 10)).toBe(10);
+    });
+  });
+
+  describe('boundries', () => {
+    it('should calculate max and min shift when left top is in 0, 0', () => {
+      const upperLeftCorner = { x: 0, y: 0 };
+      const bottomRightCorner = { x: 1500, y: 1500 };
+      const calculatedBoundries = boundries({ x: 50, y: 50, width: 300, height: 300 }, upperLeftCorner, bottomRightCorner);
+
+      expect(calculatedBoundries).toMatchObject({
+        shiftX: {
+          min: -50,
+          max: 1150
+        },
+        shiftY: {
+          min: -50,
+          max: 1150
+        }
+      });
+    });
+
+    it('should calculate max and min shift when left top is in 500, 500', () => {
+      const upperLeftCorner = { x: 500, y: 500 };
+      const bottomRightCorner = { x: 1500, y: 1500 };
+      const calculatedBoundries = boundries({ x: 700, y: 600, width: 50, height: 50 }, upperLeftCorner, bottomRightCorner);
+
+      expect(calculatedBoundries).toMatchObject({
+        shiftX: {
+          min: -200,
+          max: 750
+        },
+        shiftY: {
+          min: -100,
+          max: 850
+        }
+      });
     });
   });
 });
