112d19eceead66d571b6b582cd42f76246f731a1
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-10-02 16:21:28 +0200

    TINY-12875: update import

--- modules/oxide-components/src/main/ts/components/draggable/Draggable.stories.tsx
+++ modules/oxide-components/src/main/ts/components/draggable/Draggable.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
-import { Draggable, type DraggableProps } from './Draggable';
+import { Draggable } from './Draggable';
+import type { DraggableProps } from './internals/types';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
