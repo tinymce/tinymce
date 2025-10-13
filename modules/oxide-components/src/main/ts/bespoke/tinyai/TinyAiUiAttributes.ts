70261900583bde26397cb17e6dbabc4eb272a4e1
Hannes Frimmel Moström <hannes.mostrom@tiny.cloud>
2025-10-13 16:46:20 +0200

    TINY-12827: Moved in spinner

---
+++ modules/oxide-components/src/main/ts/bespoke/tinyai/TinyAiUiAttributes.ts
@@ -0,0 +1,11 @@
+const aiSpinner = 'tox-ai__spinner';
+
+const classArrayToClass = (classes: string[]): string => classes.join(' ');
+
+const classes = {
+  aiSpinner: classArrayToClass([ aiSpinner ]),
+};
+
+export {
+  classes
+};
