5ceea9e95d3b8b53d7de1dd2f020b330e19053fa
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-10-14 09:37:14 +0200

    TINY-13052: implement FloatingSidebar component

---
+++ modules/oxide-components/src/main/ts/components/floatingsidebar/FloatingSidebar.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, type FC, type PropsWithChildren, type ReactElement, type ReactNode } from 'react';
+
+import { Draggable } from '../../main';
+import { classes } from '../../utils/Styles';
+
+export interface FloatingSidebarProps extends PropsWithChildren {
+  isOpen?: boolean;
+}
+interface HeaderProps extends PropsWithChildren {};
+
+interface Slots {
+  header: ReactNode;
+  children: ReactNode;
+}
+
+const createSlots = (children: ReactNode): Slots => {
+  const header = (Array.isArray(children) ? children : [ children ]).find((child: ReactNode) => (child as ReactElement)?.type === Header);
+  const otherChildren = (Array.isArray(children) ? children : [ children ]).filter((child: ReactNode) => (child as ReactElement)?.type !== Header);
+
+  if (header === undefined) {
+    throw new Error('FloatingSidebar requires a header');
+  }
+
+  return { header, children: otherChildren };
+};
+
+const Root: FC<FloatingSidebarProps> = ({ isOpen = true, ...props }) => {
+  const { header, children } = createSlots(props.children);
+  const elementRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const element = elementRef.current;
+    if (element) {
+      isOpen ? element.showPopover() : element.hidePopover();
+    }
+  }, [ isOpen ]);
+
+  return (
+    <Draggable.Root ref={elementRef} className={classes([ 'tox-floating-sidebar' ])} popover="manual">
+      <aside className={classes([ 'tox-floating-sidebar__content-wrapper' ])}>
+        <Draggable.Handle>{ header }</Draggable.Handle>
+        { children }
+      </aside>
+    </Draggable.Root>
+  );
+};
+
+const Header: FC<HeaderProps> = ({ children }) => {
+  return (
+    <header className={classes([ 'tox-sidebar-content__header', 'tox-floating-sidebar__header' ])}>{ children }</header>
+  );
+};
+
+export { Root, Header };
