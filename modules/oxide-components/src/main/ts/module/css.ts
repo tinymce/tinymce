6f98d3e28721b9202e0244627a838ef625467a11
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-10-16 11:43:38 +0200

    TINY-13044: Add initial position to floating sidebar

--- modules/oxide-components/src/main/ts/module/css.ts
+++ modules/oxide-components/src/main/ts/module/css.ts
@@ -4,6 +4,7 @@ declare module 'csstype' {
   interface Properties {
     // Allow CSS custom property
     '--tox-private-floating-sidebar-requested-height'?: CSS.Property.Height;
-
+    '--tox-private-floating-sidebar-requested-top'?: CSS.Property.Top;
+    '--tox-private-floating-sidebar-requested-left'?: CSS.Property.Left;
   }
 }
