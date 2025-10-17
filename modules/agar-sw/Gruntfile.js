76e980b67a970776dc7ade442c74c02d0ba9146b
Spocke <spocke@moxiecode.com>
2025-10-17 10:01:59 +0200

    TINY-13084: Added compare task to verify that the Shared.ts file is the same

--- modules/agar-sw/Gruntfile.js
+++ modules/agar-sw/Gruntfile.js
@@ -24,11 +24,31 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.registerTask('compare-shared', 'Compare the two Shared.ts files between client and worker', function () {
+    const file1 = '../agar/src/main/ts/ephox/agar/http/Shared.ts';
+    const file2 = 'src/main/ts/core/Shared.ts';
+
+    if (!grunt.file.exists(file1)) {
+      grunt.fail.fatal(`File not found: ${file1}`);
+    }
+
+    if (!grunt.file.exists(file2)) {
+      grunt.fail.fatal(`File not found: ${file2}`);
+    }
+
+    const content1 = grunt.file.read(file1).trim();
+    const content2 = grunt.file.read(file2).trim();
+
+    if (content1 !== content2) {
+      grunt.fail.warn(`Shared.ts differ:\n- ${file1}\n- ${file2}`);
+    }
+  });
+
   require('load-grunt-tasks')(grunt, {
     requireResolution: true,
     config: "../../package.json",
     pattern: ['grunt-*', '@ephox/swag']
   });
 
-  grunt.registerTask('default', ['shell:tsc', 'rollup']);
+  grunt.registerTask('default', ['compare-shared', 'shell:tsc', 'rollup']);
 };
