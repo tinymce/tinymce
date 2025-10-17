6b352b24aa52f9666ef4d6f895196a3ce79debbf
kimwoodfield <kimwoodfield@live.com>
2025-10-17 14:27:40 +1000

    TINY-13071: Remove Storybook boilerplate comments

--- modules/oxide-components/src/main/ts/components/inlinetoolbar/InlineToolbar.stories.tsx
+++ modules/oxide-components/src/main/ts/components/inlinetoolbar/InlineToolbar.stories.tsx
@@ -9,10 +9,8 @@ import * as InlineToolbar from './InlineToolbar';
 const meta = {
   title: 'components/InlineToolbar',
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: [ 'autodocs', 'skip-visual-testing' ],
 } satisfies Meta;
 
