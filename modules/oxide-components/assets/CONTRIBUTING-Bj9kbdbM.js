bca023fa60dc796fa1fe75bafb28f41f5bb5332d
ephox <is-accounts@tiny.cloud>
2025-10-22 05:39:20 +0000

    Updates

--- modules/oxide-components/assets/CONTRIBUTING-Bj9kbdbM.js
+++ modules/oxide-components/assets/CONTRIBUTING-Bj9kbdbM.js
@@ -1,4 +1,4 @@
-import{j as n,b as s,f as i}from"./iframe-Cqy9OkYT.js";import{useMDXComponents as r}from"./index-P30P7Jg3.js";const a=`# Contributing to Oxide Components
+import{j as n,b as s,f as i}from"./iframe-V952qUM2.js";import{useMDXComponents as r}from"./index-BTbim6gC.js";const a=`# Contributing to Oxide Components
 
 \`oxide-components\` is a React component library for the TinyMCE rich text editor. This guide will help you understand the project structure, architecture, and development workflow.
 
