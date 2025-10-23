94b141a9b1443d4b8677fb631d84fe9854e32943
Spocke <spocke@moxiecode.com>
2025-10-23 09:47:04 +0200

    TINY-13132: Simplified the check

--- modules/oxide-components/src/test/ts/visual.spec.ts
+++ modules/oxide-components/src/test/ts/visual.spec.ts
@@ -4,8 +4,6 @@ import { expect, test } from '@playwright/test';
 // when we run `npm run build`
 import storybook from '../../../storybook-static/index.json' with { type: 'json' };
 
-declare const document: any;
-
 // Only run tests on stories, not other documentation pages.
 const stories = Object.values(storybook.entries).filter(
   (e) => e.type === 'story' && !e.tags.includes('skip-visual-testing')
@@ -23,16 +21,8 @@ for (const story of stories) {
 
     await page.goto(`/iframe.html?${params.toString()}`);
     await page.waitForSelector('#storybook-root');
+    await page.waitForSelector('body[class="sb-main-centered sb-show-main"]');
     await page.waitForLoadState('networkidle');
-
-    // Wait for final body classes state to avoid a flake where the screenshot would be made while a spinner is shown see #TINY-13132
-    await page.waitForFunction(() => {
-      // Code here needs to be inlined since this function is evaluated inside the frame
-      const requiredBodyClasses = [ 'sb-main-centered', 'sb-show-main' ];
-      const bodyClasses = document.body ? Array.from(document.body.classList) : [];
-
-      return requiredBodyClasses.length === bodyClasses.length && requiredBodyClasses.every((cls, i) => cls === bodyClasses[i]);
-    }, null, { polling: 10 });
 
     await expect(page).toHaveScreenshot(
       `${story.id}-${workerInfo.project.name}-${process.platform}.png`,
