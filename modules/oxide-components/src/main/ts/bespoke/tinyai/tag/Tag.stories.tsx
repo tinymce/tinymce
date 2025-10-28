48e98440d8b62e0a00317cfe7c3813ed3c10eb72
Hannes Frimmel Moström <hannes.mostrom@tiny.cloud>
2025-10-28 10:14:41 +0100

    TINY-12989: Undetected lint issue.

--- modules/oxide-components/src/main/ts/bespoke/tinyai/tag/Tag.stories.tsx
+++ modules/oxide-components/src/main/ts/bespoke/tinyai/tag/Tag.stories.tsx
@@ -6,15 +6,15 @@ import { Tag } from './Tag';
 const meta = {
   title: 'bespoke/tinyai/Tag',
   component: Tag,
-  parameters: { 
+  parameters: {
     layout: 'centered',
-  }, 
+  },
   tags: [ 'autodocs', 'skip-visual-testing' ],
 } satisfies Meta<typeof Tag>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
- 
+
 export const ClosableTag: Story = {
   args: {
     closeable: true,
