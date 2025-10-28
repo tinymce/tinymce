53cc74bd9ca3615641b0933dc1cab223b3c697ab
HAFRMO <61967464+HAFRMO@users.noreply.github.com>
2025-10-28 10:56:42 +0100

    TINY-12827: Moved in spinner (#10656)
    * TINY-12827: Moved in spinner
    
    * TINY-12827: Feedback
    
    * TINY-12827: Minor lint issue
    
    * TINY-12827: Minor fixup
    
    * TINY-12827: Minor mistake.

--- modules/oxide-components/src/main/ts/main.ts
+++ modules/oxide-components/src/main/ts/main.ts
@@ -1,9 +1,10 @@
+import { Spinner } from './bespoke/tinyai/spinner/Spinner';
 import { AutoResizingTextarea } from './components/autoresizingtextarea/AutoResizingTextarea';
 import { Button } from './components/button/Button';
+import * as ContextToolbar from './components/contexttoolbar/ContextToolbar';
 import * as Draggable from './components/draggable/Draggable';
 import * as FloatingSidebar from './components/floatingsidebar/FloatingSidebar';
 import { IconButton } from './components/iconbutton/IconButton';
-import * as ContextToolbar from './components/contexttoolbar/ContextToolbar';
 import * as KeyboardNavigationTypes from './keynav/keyboard/NavigationTypes';
 import * as KeyboardNavigationHooks from './keynav/KeyboardNavigationHooks';
 import * as FocusHelpers from './utils/FocusHelpers';
@@ -17,5 +18,6 @@ export {
   IconButton,
   ContextToolbar,
   KeyboardNavigationHooks,
-  KeyboardNavigationTypes
+  KeyboardNavigationTypes,
+  Spinner
 };
