fe04b3eefdea1e9ae19ef3ef673d184502af66d0
spocke <spocke@moxiecode.com>
2025-10-30 09:43:42 +0100

    TINY-13140: Added expand box component (#10690)
    Related Ticket: TINY-13140
    
    Description of Changes:
    
    - Added new expand box component
    
    Pre-checks:
    
    - [x] ~~Changelog entry added~~
    - [x] ~~Tests have been added (if applicable)~~
    - [x] Branch prefixed with `feature/`, `hotfix/` or `spike/`
    
    Review:
    
    - [x] Milestone set
    - [x] Docs ticket created (if applicable)
    
    GitHub issues (if applicable):

---
+++ modules/oxide-components/src/main/ts/components/expandablebox/ExpandableBox.tsx
@@ -0,0 +1,56 @@
+import { Type } from '@ephox/katamari';
+import { useLayoutEffect, useRef, useState, type FC, type PropsWithChildren } from 'react';
+
+import { Icon } from '../../internal/icon/Icon.component';
+import * as Bem from '../../utils/Bem';
+
+export interface ExpandableBoxProps extends PropsWithChildren {
+  /** Icon resolver */
+  readonly iconResolver: (icon: string) => string;
+  /** Max height the content can be before it becomes expandable */
+  readonly maxHeight?: number;
+  /** Expanded state */
+  readonly expanded?: boolean;
+  /** Callback for toggle button */
+  readonly onToggle?: () => void;
+  /** Text to render when the button is to expand the content */
+  readonly expandLabel?: string;
+  /** Text to render when the button is to collapse the content */
+  readonly collapseLabel?: string;
+}
+
+/** Expandable container box */
+export const ExpandableBox: FC<ExpandableBoxProps> = ({
+  iconResolver,
+  maxHeight = 80,
+  expanded = false,
+  onToggle,
+  expandLabel = 'Expand',
+  collapseLabel = 'Collapse',
+  children
+}) => {
+  const contentRef = useRef<HTMLDivElement | null>(null);
+  const [ overflowing, setOverflowing ] = useState(false);
+  const contentClass = Bem.element('tox-expandable-box', 'content', { expanded, overflowing: overflowing && !expanded });
+
+  useLayoutEffect(() => {
+    const contentEl = contentRef.current;
+    if (Type.isNonNullable(contentEl)) {
+      setOverflowing(contentEl.scrollHeight > maxHeight);
+    }
+  }, [ children, maxHeight ]);
+
+  return (
+    <div className={Bem.block('tox-expandable-box')}>
+      <div ref={contentRef} className={contentClass} style={{ maxHeight: expanded ? undefined : `${maxHeight}px` }}>
+        {children}
+      </div>
+      {
+        overflowing && <button type="button" className={Bem.element('tox-expandable-box', 'toggle-button')} onClick={() => onToggle?.()}>
+          <Icon resolver={iconResolver} icon={expanded ? 'chevron-up' : 'chevron-down'} />
+          {expanded ? collapseLabel : expandLabel}
+        </button>
+      }
+    </div>
+  );
+};
