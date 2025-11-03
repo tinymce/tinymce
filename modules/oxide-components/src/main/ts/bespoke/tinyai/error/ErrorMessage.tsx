89633a98b3ce83c83d44bcb0cfcf6c155d31259a
Hannes Frimmel Moström <hannes.mostrom@tiny.cloud>
2025-11-03 09:51:15 +0100

    TINY-12909: Added suggested role and aria attribute.

--- modules/oxide-components/src/main/ts/bespoke/tinyai/error/ErrorMessage.tsx
+++ modules/oxide-components/src/main/ts/bespoke/tinyai/error/ErrorMessage.tsx
@@ -7,7 +7,7 @@ interface ErrorMessageProps {
 }
 
 const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {
-  return <div className={classes([ 'tox-ai__error-message' ])}>{message}</div>;
+  return <div className={classes([ 'tox-ai__error-message' ])} role="alert" aria-live="polite">{message}</div>;
 };
 
 export default ErrorMessage;
