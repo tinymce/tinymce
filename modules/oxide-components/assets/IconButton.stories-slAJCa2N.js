d9531b9c40934b21ac311e2804c6dbe7369c2013
ephox <is-accounts@tiny.cloud>
2025-11-19 07:12:02 +0000

    Updates

--- modules/oxide-components/assets/IconButton.stories-slAJCa2N.js
+++ modules/oxide-components/assets/IconButton.stories-slAJCa2N.js
@@ -1,6 +1,6 @@
-import{I as y}from"./IconButton-CYYZC-cj.js";import{c as w}from"./Optional-Bk48EtMP.js";import"./iframe-lCE6g0Kq.js";import"./Button-BwzdkHOv.js";import"./Bem-BjVdC6ql.js";import"./Strings-ChBvWd27.js";const f=`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+import{I as y}from"./IconButton-B4tSzIvk.js";import{c as w}from"./Optional-xAorvXRI.js";import"./iframe-D9INKKzB.js";import"./Icon.component--SdvYtIj.js";import"./Button-T8TN8c0J.js";import"./Bem--Tv3ahAp.js";import"./Strings-Cpm3hB5w.js";const f=`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
   <path fill-rule="evenodd" clip-rule="evenodd" d="M15.7071 4.29289C15.3166 3.90237 14.6834 3.90237 14.2929 4.29289L6.58579 12L14.2929 19.7071C14.6834 20.0976 15.3166 20.0976 15.7071 19.7071C16.0976 19.3166 16.0976 18.6834 15.7071 18.2929L9.41421 12L15.7071 5.70711C16.0976 5.31658 16.0976 4.68342 15.7071 4.29289Z"/>
-</svg>`,L={title:"components/IconButton",component:y,parameters:{layout:"centered"},tags:["autodocs"],args:{icon:"leftArrow",resolver:w(f)}},r={args:{variant:"primary"}},a={args:{variant:"secondary"}},e={args:{variant:"outlined"}},o={args:{variant:"naked"}};var s,t,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+</svg>`,B={title:"components/IconButton",component:y,parameters:{layout:"centered"},tags:["autodocs"],args:{icon:"leftArrow",resolver:w(f)}},r={args:{variant:"primary"}},a={args:{variant:"secondary"}},e={args:{variant:"outlined"}},o={args:{variant:"naked"}};var s,t,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
   args: {
     variant: 'primary'
   }
@@ -16,4 +16,4 @@ import{I as y}from"./IconButton-CYYZC-cj.js";import{c as w}from"./Optional-Bk48E
   args: {
     variant: 'naked'
   }
-}`,...(v=(g=o.parameters)==null?void 0:g.docs)==null?void 0:v.source}}};const B=["Primary","Secondary","Outlined","Naked"];export{o as Naked,e as Outlined,r as Primary,a as Secondary,B as __namedExportsOrder,L as default};
+}`,...(v=(g=o.parameters)==null?void 0:g.docs)==null?void 0:v.source}}};const O=["Primary","Secondary","Outlined","Naked"];export{o as Naked,e as Outlined,r as Primary,a as Secondary,O as __namedExportsOrder,B as default};
--- modules/oxide-components/assets/STYLING-37ggzkod.js
+++ modules/oxide-components/assets/STYLING-37ggzkod.js
@@ -1,4 +1,4 @@
-import{j as e,b as i,c as r}from"./iframe-lCE6g0Kq.js";import{useMDXComponents as s}from"./index-DCjrDNV_.js";const a=`# Styling Oxide Components
+import{j as e,b as i,f as r}from"./iframe-D9INKKzB.js";import{useMDXComponents as s}from"./index-9FAvbE7-.js";const a=`# Styling Oxide Components
 
 To ensure backward compatibility with existing skins, all oxide-components should be styled inside **Oxide** library. 
 
