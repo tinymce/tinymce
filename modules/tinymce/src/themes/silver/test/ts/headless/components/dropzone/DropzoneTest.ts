6d8f4b2390d6a1a56a1d33c31bcc9caefa2fe532
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2025-11-20 10:04:34 +0100

    TINY-13278: refactor test

--- modules/tinymce/src/themes/silver/test/ts/headless/components/dropzone/DropzoneTest.ts
+++ modules/tinymce/src/themes/silver/test/ts/headless/components/dropzone/DropzoneTest.ts
@@ -1,6 +1,6 @@
 import { ApproxStructure, Assertions, UiFinder } from '@ephox/agar';
 import { AlloyTriggers, Composing, GuiFactory, NativeEvents, Representing } from '@ephox/alloy';
-import { describe, it } from '@ephox/bedrock-client';
+import { context, describe, it } from '@ephox/bedrock-client';
 import { Optional } from '@ephox/katamari';
 import { assert } from 'chai';
 
@@ -15,8 +15,8 @@ describe('headless.tinymce.themes.silver.components.dropzone.DropzoneTest', () =
       context: 'any',
       name: 'drop1',
       label: Optional.some('Dropzone Label'),
-      buttonLabel: Optional.some('Button Label'),
-      dropAreaLabel: Optional.some('Drop Area Label'),
+      buttonLabel: Optional.none(),
+      dropAreaLabel: Optional.none(),
       allowedFileTypes: Optional.none(),
       allowedFileExtensions: Optional.none(),
     }, TestProviders, Optional.none())
@@ -36,10 +36,10 @@ describe('headless.tinymce.themes.silver.components.dropzone.DropzoneTest', () =
               s.element('div', {
                 children: [
                   s.element('p', {
-                    html: str.is('Drop Area Label')
+                    html: str.is('Drop an image here')
                   }),
                   s.element('button', {
-                    html: str.is('Button Label<input type="file" accept="image/*" style="display: none;">')
+                    html: str.is('Browse for an image<input type="file" accept="image/*" style="display: none;">')
                   }),
                 ]
               })
@@ -91,5 +91,70 @@ describe('headless.tinymce.themes.silver.components.dropzone.DropzoneTest', () =
       { name: 'image11.PNG' },
       { name: 'image12.WEBP' }
     ], 'Checking value of dropzone');
+  });
+
+  context('TINY-13278: buttonLabel, dropAreaLabel, allowedFileTypes and allowedFileExtensions', () => {
+    const hook = GuiSetup.bddSetup((_store, _doc, _body) => GuiFactory.build(
+      renderDropZone({
+        context: 'any',
+        name: 'drop1',
+        label: Optional.some('Dropzone Label'),
+        buttonLabel: Optional.some('Button Label'),
+        dropAreaLabel: Optional.some('Drop Area Label'),
+        allowedFileTypes: Optional.some('plain/text'),
+        allowedFileExtensions: Optional.some([ 'txt' ]),
+      }, TestProviders, Optional.none())
+    ));
+
+    it('TINY-13278: Check basic structure', () => {
+      Assertions.assertStructure(
+        'Checking initial structure',
+        ApproxStructure.build((s, str, arr) => s.element('div', {
+          children: [
+            s.element('label', {
+              classes: [ arr.has('tox-label') ],
+              html: str.is('Dropzone Label')
+            }),
+            s.element('div', {
+              children: [
+                s.element('div', {
+                  children: [
+                    s.element('p', {
+                      html: str.is('Drop Area Label')
+                    }),
+                    s.element('button', {
+                      html: str.is('Button Label<input type="file" accept="plain/text" style="display: none;">')
+                    }),
+                  ]
+                })
+              ]
+            })
+          ]
+        })),
+        hook.component().element
+      );
+    });
+
+    it('TINY-13278: Check drop on zone', () => {
+      const component = hook.component();
+      const dropzone = UiFinder.findIn(component.element, '.tox-dropzone').getOrDie();
+      const zone = component.getSystem().getByDom(dropzone).getOrDie();
+      AlloyTriggers.emitWith(zone, NativeEvents.drop(), {
+        raw: {
+          dataTransfer: {
+            files: [
+              { name: 'text1.txt' },
+              { name: 'text2.doc' }
+            ]
+          }
+        }
+      });
+
+      const comp = Composing.getCurrent(component).getOrDie('Failed trying to get the zone from the container');
+      const filesValue = Representing.getValue(comp);
+      assert.deepEqual(filesValue, [
+        { name: 'text1.txt' }
+      ], 'Checking value of dropzone');
+    });
   });
 });
