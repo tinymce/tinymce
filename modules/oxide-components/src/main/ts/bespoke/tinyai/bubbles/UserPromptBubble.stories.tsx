fee667aaa4277391550371acb4dbfd54417cab4b
Hannes Frimmel Moström <hannes.mostrom@tiny.cloud>
2025-11-21 16:42:18 +0100

    TINY-12912: Updated visual tests.

--- modules/oxide-components/src/main/ts/bespoke/tinyai/bubbles/UserPromptBubble.stories.tsx
+++ modules/oxide-components/src/main/ts/bespoke/tinyai/bubbles/UserPromptBubble.stories.tsx
@@ -1,8 +1,13 @@
-import type { Meta, StoryObj } from '@storybook/react-vite';
+import type { Meta, ReactRenderer, StoryObj } from '@storybook/react-vite';
+import { classes } from 'oxide-components/utils/Styles';
+import type { PartialStoryFn } from 'storybook/internal/csf';
 
 import { UserPromptBubble } from './UserPromptBubble';
 
 const meta = {
+  decorators: [
+    (Story: PartialStoryFn<ReactRenderer>): JSX.Element => <div className={classes([ 'tox-ai' ])}><Story /></div>
+  ],
   title: 'bespoke/tinyai/UserPromptBubble',
   component: UserPromptBubble,
   parameters: {
