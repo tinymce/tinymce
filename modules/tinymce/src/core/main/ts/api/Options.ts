673d62567ff22ede7022bb2818d9baab28fcb329
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2025-11-27 11:49:52 +0100

    TINY-13319: fix error message

--- modules/tinymce/src/core/main/ts/api/Options.ts
+++ modules/tinymce/src/core/main/ts/api/Options.ts
@@ -951,7 +951,7 @@ const register = (editor: Editor): void => {
     processor: (value: unknown) => StructureSchema.asRaw<DocumentsFileTypes[]>('documents_file_types', documentsFileTypesOptionsSchema, value).fold<DocumentsFileTypesProcessorReturnType>(
       (_err) => ({
         valid: false,
-        message: 'Must be a non-empty array of objects matching the configuration schema: https://www.tiny.cloud/docs/tinymce/latest/content-filtering/#documents_file_types' // TODO: this is a placeholder
+        message: 'Must be a non-empty array of objects matching the configuration schema: https://www.tiny.cloud/docs/tinymce/latest/uploadcare/#documents-file-types'
       }),
       (val) => ({ valid: true, value: val })
     )
