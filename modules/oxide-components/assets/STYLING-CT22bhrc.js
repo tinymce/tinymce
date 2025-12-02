47e91372d21e95edc14def0244048d0c27d4d92d
ephox <is-accounts@tiny.cloud>
2025-12-02 23:48:14 +0000

    Updates

--- modules/oxide-components/assets/STYLING-CT22bhrc.js
+++ modules/oxide-components/assets/STYLING-CT22bhrc.js
@@ -1,4 +1,4 @@
-import{j as e,b as i,f as r}from"./iframe-C-RmNaWQ.js";import{useMDXComponents as s}from"./index-Dh5b8VzC.js";const a=`# Styling Oxide Components
+import{j as e,b as i,c as r}from"./iframe-DRzN_yWJ.js";import{useMDXComponents as s}from"./index-DVUUjhYJ.js";const a=`# Styling Oxide Components
 
 To ensure backward compatibility with existing skins, all oxide-components should be styled inside **Oxide** library. 
 
