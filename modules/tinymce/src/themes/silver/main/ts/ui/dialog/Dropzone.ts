f6703de9d4a4d2c9aacfaa1bb5d9dd3168e030a1
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2025-12-11 08:13:30 +0100

    TINY-13420: fix

--- modules/tinymce/src/themes/silver/main/ts/ui/dialog/Dropzone.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/dialog/Dropzone.ts
@@ -62,13 +62,12 @@ export const renderDropZone = (spec: DropZoneSpec, providersBackstage: UiFactory
   const handleFiles = (component: AlloyComponent, files: FileList | null | undefined) => {
     if (files) {
       const filteredFiles = filterByExtension(files, providersBackstage, spec.allowedFileExtensions);
+      Representing.setValue(component, filteredFiles);
+      AlloyTriggers.emitWith(component, formChangeEvent, { name: spec.name });
       if (filteredFiles.length === 0) {
         spec.errorHandler?.each((errorHandler) => {
           errorHandler('All inserted files have unallowed extensions');
         });
-      } else {
-        Representing.setValue(component, filteredFiles);
-        AlloyTriggers.emitWith(component, formChangeEvent, { name: spec.name });
       }
     }
   };
