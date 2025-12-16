6ad6056219e8a2a7e0423c124a117926b45d619f
ephox <is-accounts@tiny.cloud>
2025-12-16 23:32:05 +0000

    Updates

--- modules/oxide-components/assets/ErrorMessage.stories-D95f2RmF.js
+++
@@ -1,5 +0,0 @@
-import{j as o,f as n}from"./iframe-CxsFnUj6.js";const c=({message:t})=>o.jsx("div",{className:n(["tox-ai__error-message"]),role:"alert","aria-live":"polite",children:t}),m={title:"bespoke/tinyai/ErrorMessage",component:c,parameters:{layout:"centered"},tags:["autodocs","skip-visual-testing"]},e={args:{message:"Message"}};var s,r,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    message: 'Message'
-  }
-}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const d=["StandardErrorMessage"];export{e as StandardErrorMessage,d as __namedExportsOrder,m as default};
