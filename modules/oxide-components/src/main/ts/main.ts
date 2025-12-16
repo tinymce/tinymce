2c13e1f0f7e8750dc517cd677fef367600eae1cf
Zuzanna Tomaszyk <91727075+ztomaszyk@users.noreply.github.com>
2025-12-16 10:02:06 +0100

    TINY-13237: create input form component (#10766)
    Related Ticket: TINY-13237:
    
    Description of Changes:
    * Added a new `ToolbarInputForm` component for handling form inputs in toolbars
    * Support for labels, placeholders, and submit functionality
    * Added Storybook examples with "Example" and "URL" variants
    * Implemented visual test snapshots for the component
    
    Pre-checks:
    * [x] ~~Changelog entry added~~ (epic branch)
    * [x] Tests have been added (visual tests)
    * [x] Branch prefixed with `feature/`, `hotfix/` or `spike/`
    
    Review:
    * [x] ~~Milestone set~~ (epic branch)
    * [x] ~~Docs ticket created (if applicable)~~
    
    GitHub issues (if applicable):

--- modules/oxide-components/src/main/ts/main.ts
+++ modules/oxide-components/src/main/ts/main.ts
@@ -14,6 +14,7 @@ import { IconButton } from './components/iconbutton/IconButton';
 import * as Menu from './components/menu/Menu';
 import * as MenuRenderer from './components/menu/MenuRenderer';
 import { ToggleSwitch } from './components/toggleswitch/ToggleSwitch';
+import { ToolbarInputForm } from './components/toolbarInputForm/ToolbarInputForm';
 import { useUniverse } from './contexts/UniverseContext/Universe';
 import { UniverseProvider } from './contexts/UniverseContext/UniverseProvider';
 import type { UniverseResources } from './contexts/UniverseContext/UniverseTypes';
@@ -44,7 +45,8 @@ export {
   ToggleSwitch,
   UniverseProvider,
   useUniverse,
-  UserPromptBubble
+  UserPromptBubble,
+  ToolbarInputForm
 };
 
 export type { UniverseResources };
