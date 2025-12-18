ae8d1eee5b280e1e2f6f259b5f80b9f00c5209b5
ephox <is-accounts@tiny.cloud>
2025-12-18 08:43:21 +0000

    Updates

---
+++ modules/oxide-components/assets/Dropdown.stories-BneAhciN.js
@@ -0,0 +1,169 @@
+import{j as r}from"./iframe-BrcTUybY.js";import{B as Z}from"./Button-DApeBbrS.js";import{R as Y,T as $,C as ee}from"./Dropdown-DcGeo5MR.js";import"./UniverseProvider-BEzxNUDn.js";import"./Bem-CMgfceZl.js";import"./Optional-CHj922Ci.js";import"./Strings-D6HyETGk.js";import"./Fun--VEwoXIw.js";import"./Obj-BAtCA5NI.js";const me={title:"components/Dropdown",component:Y,argTypes:{side:{description:"On which side of the trigger button should the content container appear",value:"top",options:["top","bottom","left","right"]},align:{description:"Should the content container be aligned to the start, center or end of the trigger button.",value:"start",options:["start","center","end"]},gap:{description:"Gap between trigger button and dropdown",value:8,control:"number"},triggerEvents:{description:"Defines trigger element behavior. On default opens a dropdown on click but can be changed to open on hover and close on mouse leave.",value:["click"],options:[["click"],["hover"],["click","hover"]]}},parameters:{layout:"centered",docs:{description:{component:"A dropdown component. Contains a button and an anchored container that can be used for creating menus."}}},tags:["autodocs","skip-visual-testing","dropdown-visual-testing"]},e=f=>r.jsx("div",{style:{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh"},children:r.jsxs(Y,{side:f.side,align:f.align,children:[r.jsx($,{children:r.jsx(Z,{variant:"secondary",children:"Click me to toggle dropdown"})}),r.jsx(ee,{children:r.jsx("div",{style:{width:"400px",height:"300px",borderRadius:"8px"}})})]})}),n={args:{side:"top",align:"start"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},t={args:{side:"top",align:"center"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},s={args:{side:"top",align:"end"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},a={args:{side:"bottom",align:"start"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},o={args:{side:"bottom",align:"center"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},i={args:{side:"bottom",align:"end"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},c={args:{side:"left",align:"start"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},d={args:{side:"left",align:"center"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},m={args:{side:"left",align:"end"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},p={args:{side:"right",align:"start"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},l={args:{side:"right",align:"center"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e},g={args:{side:"right",align:"end"},parameters:{docs:{story:{inline:!1,iframeHeight:700}}},render:e};var h,u,y;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    side: 'top',
+    align: 'start'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(y=(u=n.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};var H,S,b;t.parameters={...t.parameters,docs:{...(H=t.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    side: 'top',
+    align: 'center'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(b=(S=t.parameters)==null?void 0:S.docs)==null?void 0:b.source}}};var v,x,C;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    side: 'top',
+    align: 'end'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(C=(x=s.parameters)==null?void 0:x.docs)==null?void 0:C.source}}};var E,R,j;a.parameters={...a.parameters,docs:{...(E=a.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    side: 'bottom',
+    align: 'start'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(j=(R=a.parameters)==null?void 0:R.docs)==null?void 0:j.source}}};var w,T,B;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    side: 'bottom',
+    align: 'center'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(B=(T=o.parameters)==null?void 0:T.docs)==null?void 0:B.source}}};var k,L,O;i.parameters={...i.parameters,docs:{...(k=i.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    side: 'bottom',
+    align: 'end'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(O=(L=i.parameters)==null?void 0:L.docs)==null?void 0:O.source}}};var D,_,A;c.parameters={...c.parameters,docs:{...(D=c.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    side: 'left',
+    align: 'start'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(A=(_=c.parameters)==null?void 0:_.docs)==null?void 0:A.source}}};var G,I,q;d.parameters={...d.parameters,docs:{...(G=d.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    side: 'left',
+    align: 'center'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(q=(I=d.parameters)==null?void 0:I.docs)==null?void 0:q.source}}};var z,F,J;m.parameters={...m.parameters,docs:{...(z=m.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    side: 'left',
+    align: 'end'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(J=(F=m.parameters)==null?void 0:F.docs)==null?void 0:J.source}}};var K,M,N;p.parameters={...p.parameters,docs:{...(K=p.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  args: {
+    side: 'right',
+    align: 'start'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(N=(M=p.parameters)==null?void 0:M.docs)==null?void 0:N.source}}};var P,Q,U;l.parameters={...l.parameters,docs:{...(P=l.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    side: 'right',
+    align: 'center'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(U=(Q=l.parameters)==null?void 0:Q.docs)==null?void 0:U.source}}};var V,W,X;g.parameters={...g.parameters,docs:{...(V=g.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    side: 'right',
+    align: 'end'
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 700
+      }
+    }
+  },
+  render
+}`,...(X=(W=g.parameters)==null?void 0:W.docs)==null?void 0:X.source}}};const pe=["TopStart","TopCenter","TopEnd","BottomStart","BottomCenter","BottomEnd","LeftStart","LeftCenter","LeftEnd","RightStart","RightCenter","RightEnd"];export{o as BottomCenter,i as BottomEnd,a as BottomStart,d as LeftCenter,m as LeftEnd,c as LeftStart,l as RightCenter,g as RightEnd,p as RightStart,t as TopCenter,s as TopEnd,n as TopStart,pe as __namedExportsOrder,me as default};
