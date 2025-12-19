1d0c9cb49cdba434d85711f623bdb1ce9e981915
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2025-12-19 09:42:46 +0100

    TINY-13548: add positioning config options to draggable

--- modules/oxide-components/src/main/ts/components/draggable/internals/types.ts
+++ modules/oxide-components/src/main/ts/components/draggable/internals/types.ts
@@ -1,18 +1,24 @@
 import type { Property } from 'csstype';
-import type { PropsWithChildren, HTMLAttributes } from 'react';
+import type { HTMLAttributes, PropsWithChildren } from 'react';
 
 export interface AllowedOverflow {
   horizontal: number;
   vertical: number;
 }
 
-export interface DraggableProps extends HTMLAttributes<HTMLDivElement> {
+export type DraggableProps = {
   // The popover attribute is missing from HTMLAttributes, it's released in React v19. We can remove this property once we upgrade to React v19.
   popover?: 'hint' | 'manual' | 'auto';
-  initialPosition?: CssPosition;
   declaredSize?: CssSize;
   allowedOverflow?: Partial<AllowedOverflow>;
-}
+} & PositioningProps & HTMLAttributes<HTMLDivElement>;
+
+type PositioningProps =
+  { anchor?: never; initialPosition?: { top: Property.Top; left: Property.Left }} |
+  { anchor: 'top-left'; initialPosition?: { top: Property.Top; left: Property.Left }} |
+  { anchor: 'top-right'; initialPosition?: { top: Property.Top; right: Property.Right }} |
+  { anchor: 'bottom-left'; initialPosition?: { bottom: Property.Bottom; left: Property.Left }} |
+  { anchor: 'bottom-right'; initialPosition?: { bottom: Property.Bottom; right: Property.Right }};
 
 export interface DraggableHandleProps extends PropsWithChildren { }
 
