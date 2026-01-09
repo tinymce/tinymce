93a6892b5971d4a95af45a9aa29fbc8aa00182ec
ephox <is-accounts@tiny.cloud>
2026-01-09 09:59:48 +0000

    Updates

---
+++ modules/oxide-components/assets/UserPromptBubble.stories-B6K5iqix.js
@@ -0,0 +1,5 @@
+import{j as t,c as m}from"./iframe-BdJ3TlxI.js";import{e as c}from"./Bem-DDAe-1K0.js";import"./Optional-DSwtTfFR.js";import"./Strings-DLAoeKw-.js";import"./Fun--VEwoXIw.js";import"./Obj-CCYZ-IyV.js";const o=({prompt:e})=>t.jsx("div",{tabIndex:-1,className:c("tox-ai","user-prompt__text"),children:e});try{o.displayName="UserPromptBubble",o.__docgenInfo={description:"",displayName:"UserPromptBubble",props:{prompt:{defaultValue:null,description:"",name:"prompt",required:!0,type:{name:"string"}}}}}catch{}const _={decorators:[e=>t.jsx("div",{className:m(["tox-ai"]),children:t.jsx(e,{})})],title:"bespoke/tinymceai/UserPromptBubble",component:o,parameters:{layout:"centered"},tags:["autodocs"]},r={args:{prompt:"Value"}};var s,a,p;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    prompt: 'Value'
+  }
+}`,...(p=(a=r.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};const x=["SimpleUserPromptBubble"];export{r as SimpleUserPromptBubble,x as __namedExportsOrder,_ as default};
