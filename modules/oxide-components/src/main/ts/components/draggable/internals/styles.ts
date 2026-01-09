0cbc53db7314ff880be2ebc8db92f7f4ab9d64cb
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2026-01-09 10:10:23 +0100

    TINY-13548: add positioning config options to draggable (#10809)
    Related Ticket: TINY-13548
    
    Description of Changes:
    
    `Draggable` component uses `transform` property to position itself while dragging. When dragging ends (pointerup) we used to translate `transform` into `top` and `left` properties. We do that to later wrap `top` and `left` values with css `min` to ensure that the element stays on the page on window resize.
    
    In this PR I introduced new prop for `Draggable` component called `origin`, with values `top-left | top-right | bottom-left | bottom-right`. This property allows us to modify the behavior described above. So for example:
    
    `<Draggable origin="top-right" />` will use css `top` and `right` properties to position itself on the page.
    
    This option enables us to provide more natural feeling while resizing the window with elements that are initially near the right edge of the viewport. Especially now, that we allow partial overflow ( see https://github.com/tinymce/tinymce/pull/10795 )
    
    Draggable:
    
    - Add new `origin` property with default value of `top-left`
    - Update documentation
    - Where needed, run tests for multiple origin values
    
    FloatingSidebar:
    
    - Add new `origin` property with default value of `top-right`
    - Change type of `initialPosition` property
    - Remove obsolete tests
    - Remove obsolete story
    - Adjust tests
    - Update documentation
    
    Pre-checks:
    
    ~~\- [ ] Changelog entry added~~
    
    - [x] Tests have been added (if applicable)
    - [x] Branch prefixed with `feature/`, `hotfix/` or `spike/`
    
    Review:
    
    ~~\- [ ] Milestone set~~
    ~~\- [ ] Docs ticket created (if applicable)~~
    
    GitHub issues (if applicable):

--- modules/oxide-components/src/main/ts/components/draggable/internals/styles.ts
+++ modules/oxide-components/src/main/ts/components/draggable/internals/styles.ts
@@ -1,24 +1,39 @@
-import { Fun, type Optional } from '@ephox/katamari';
+import { Fun, Type, type Optional } from '@ephox/katamari';
 
-import type { AllowedOverflow, CssPosition, CssSize, Position, Shift } from './types';
+import * as OriginPredicates from './OriginPredicates';
+import type { AllowedOverflow, Origin, CssPosition, CssSize, Position, Shift } from './types';
 
 const getTransform = (shift: Shift): string => `translate3d(${shift.x}px, ${shift.y}px, 0px)`;
 
-const normalizePosition = (position: CssPosition | Position): CssPosition =>
-  'x' in position ? { top: `${position.y}px`, left: `${position.x}px` } : position;
+const normalizePosition = (position: CssPosition | Position): CssPosition => ({
+  x: Type.isNumber(position.x) ? `${position.x}px` : position.x,
+  y: Type.isNumber(position.y) ? `${position.y}px` : position.y
+});
 
-const getPosition = (position: CssPosition, allowedOverflow: AllowedOverflow, size: Optional<CssSize>) =>
-  size.fold(
+const getPosition = (position: CssPosition, allowedOverflow: AllowedOverflow, origin: Origin, size: Optional<CssSize>) => {
+  const { x, y } = size.fold(
     Fun.constant(position),
     ({ width, height }) => ({
-      top: `min(${position.top}, calc(100% - (${height} * ${1 - allowedOverflow.vertical}))`,
-      left: `min(${position.left}, calc(100% - (${width} * ${1 - allowedOverflow.horizontal}))`,
+      x: `min(${position.x}, calc(100% - (${width} * ${1 - allowedOverflow.horizontal}))`,
+      y: `min(${position.y}, calc(100% - (${height} * ${1 - allowedOverflow.vertical}))`
     })
   );
 
-const getPositioningStyles = (shift: Shift, position: CssPosition | Position, allowedOverflow: AllowedOverflow, isDragging: boolean, declaredSize: Optional<CssSize>): React.CSSProperties =>
+  if (OriginPredicates.isTopLeft(origin)) {
+    return { top: y, left: x };
+  }
+  if (OriginPredicates.isTopRight(origin)) {
+    return { top: y, right: x };
+  }
+  if (OriginPredicates.isBottomLeft(origin)) {
+    return { bottom: y, left: x };
+  }
+  return { bottom: y, right: x };
+};
+
+const getPositioningStyles = (shift: Shift, position: CssPosition | Position, origin: Origin, allowedOverflow: AllowedOverflow, isDragging: boolean, declaredSize: Optional<CssSize>): React.CSSProperties =>
   isDragging ?
-    { transform: getTransform(shift), ...getPosition(normalizePosition(position), allowedOverflow, declaredSize) }
-    : getPosition(normalizePosition(position), allowedOverflow, declaredSize);
+    { transform: getTransform(shift), ...getPosition(normalizePosition(position), allowedOverflow, origin, declaredSize) }
+    : getPosition(normalizePosition(position), allowedOverflow, origin, declaredSize);
 
 export { getPositioningStyles };
