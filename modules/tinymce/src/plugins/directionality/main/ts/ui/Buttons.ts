ad22e0dde3669863e0f91a2e3ba5da259ff384df
MitchCTiny <mitchell.crompton@tiny.cloud>
2026-01-13 13:10:07 +1000

    TINY-13337: Buttons activate to reflect toggled directionality of block

--- modules/tinymce/src/plugins/directionality/main/ts/ui/Buttons.ts
+++ modules/tinymce/src/plugins/directionality/main/ts/ui/Buttons.ts
@@ -1,4 +1,5 @@
-import { Direction, SugarElement } from '@ephox/sugar';
+import { Optional, Strings } from '@ephox/katamari';
+import { PredicateExists, SugarElement, Traverse } from '@ephox/sugar';
 
 import type Editor from 'tinymce/core/api/Editor';
 import type { NodeChangeEvent } from 'tinymce/core/api/EventTypes';
@@ -6,13 +7,20 @@ import type { Toolbar } from 'tinymce/core/api/ui/Ui';
 import type { EditorEvent } from 'tinymce/core/api/util/EventDispatcher';
 
 const getNodeChangeHandler = (editor: Editor, dir: 'ltr' | 'rtl') => (api: Toolbar.ToolbarToggleButtonInstanceApi) => {
-  const nodeChangeHandler = (e: EditorEvent<NodeChangeEvent>) => {
-    const element = SugarElement.fromDom(e.element);
-    api.setActive(Direction.getDirection(element) === dir);
-    api.setEnabled(editor.selection.isEditable());
-  };
+  const getDirection = (elem: Element): Optional<string> =>
+    Optional.from(editor.dom.getStyle(elem, 'direction'))
+      .or(Optional.from(editor.dom.getAttrib(elem, 'dir')))
+      .filter(Strings.isNotEmpty);
+
+  const activeDirection = (elem: Element) =>
+    getDirection(elem)
+      .or(Optional.from(editor.dom.getParent(elem, editor.dom.isBlock)).bind(getDirection))
+      .exists((direction: string) => direction === dir);
+
+  const nodeChangeHandler = (e: EditorEvent<NodeChangeEvent>) =>
+    api.setActive(activeDirection(e.element));
+
   editor.on('NodeChange', nodeChangeHandler);
-  api.setEnabled(editor.selection.isEditable());
 
   return () => editor.off('NodeChange', nodeChangeHandler);
 };
@@ -21,13 +29,15 @@ const register = (editor: Editor): void => {
   editor.ui.registry.addToggleButton('ltr', {
     tooltip: 'Left to right',
     icon: 'ltr',
+    context: 'editable',
     onAction: () => editor.execCommand('mceDirectionLTR'),
-    onSetup: getNodeChangeHandler(editor, 'ltr')
+    onSetup: getNodeChangeHandler(editor, 'ltr'),
   });
 
   editor.ui.registry.addToggleButton('rtl', {
     tooltip: 'Right to left',
     icon: 'rtl',
+    context: 'editable',
     onAction: () => editor.execCommand('mceDirectionRTL'),
     onSetup: getNodeChangeHandler(editor, 'rtl')
   });
