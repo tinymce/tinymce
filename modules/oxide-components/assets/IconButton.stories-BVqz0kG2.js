489fd71a1cd26b14e6d84a8c44cd9f587a44cd6e
ephox <is-accounts@tiny.cloud>
2026-01-15 01:30:43 +0000

    Updates

---
+++ modules/oxide-components/assets/IconButton.stories-BVqz0kG2.js
@@ -0,0 +1,19 @@
+import{j as t}from"./iframe-Dmg9xr8O.js";import{I as w}from"./IconButton-Zn5fXC9V.js";import{U as x}from"./UniverseProvider-Bhg6zhei.js";import{g as h}from"./Obj-0QrmN1ba.js";import"./Button-BS00URxD.js";import"./Bem-A_X-XUhj.js";import"./Optional-DGH8Y1w3.js";import"./Strings-CkeB91LW.js";import"./Fun--VEwoXIw.js";import"./Universe-DVkDv2uJ.js";const S={"left-arrow":`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M15.7071 4.29289C15.3166 3.90237 14.6834 3.90237 14.2929 4.29289L6.58579 12L14.2929 19.7071C14.6834 20.0976 15.3166 20.0976 15.7071 19.7071C16.0976 19.3166 16.0976 18.6834 15.7071 18.2929L9.41421 12L15.7071 5.70711C16.0976 5.31658 16.0976 4.68342 15.7071 4.29289Z"/>
+</svg>`},k={getIcon:s=>h(S,s).getOrDie("Failed to get icon")},_={title:"components/IconButton",component:w,decorators:[s=>t.jsx(x,{resources:k,children:t.jsx(s,{})})],parameters:{layout:"centered"},tags:["autodocs"],args:{icon:"left-arrow"}},r={args:{variant:"primary"}},e={args:{variant:"secondary"}},a={args:{variant:"outlined"}},o={args:{variant:"naked"}};var n,c,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary'
+  }
+}`,...(i=(c=r.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var m,d,p;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(p=(d=e.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var u,g,l;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    variant: 'outlined'
+  }
+}`,...(l=(g=a.parameters)==null?void 0:g.docs)==null?void 0:l.source}}};var v,f,y;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    variant: 'naked'
+  }
+}`,...(y=(f=o.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};const D=["Primary","Secondary","Outlined","Naked"];export{o as Naked,a as Outlined,r as Primary,e as Secondary,D as __namedExportsOrder,_ as default};
