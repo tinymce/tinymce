8b72c65acc516d2ff400ecf9e36b524fea6294f7
Sorita Heng <69398037+soritaheng@users.noreply.github.com>
2026-01-29 10:26:53 +1000

    Revert "TINY-13190: Add tests"
    This reverts commit 6e0042b4764ecfb86c22c8257c5bbb18f2fc03ae.

--- modules/tinymce/src/plugins/preview/test/ts/browser/PreviewContentCssTest.ts
+++ modules/tinymce/src/plugins/preview/test/ts/browser/PreviewContentCssTest.ts
@@ -1,90 +1,33 @@
-import { context, describe, it, before, after } from '@ephox/bedrock-client';
-import { Arr, Fun } from '@ephox/katamari';
-import { McEditor, TinyHooks } from '@ephox/wrap-mcagar';
+import { describe, it } from '@ephox/bedrock-client';
+import { TinyHooks } from '@ephox/wrap-mcagar';
 import { assert } from 'chai';
 
 import type Editor from 'tinymce/core/api/Editor';
-import type { TinyMCE } from 'tinymce/core/api/Tinymce';
 import * as IframeContent from 'tinymce/plugins/preview/core/IframeContent';
 import Plugin from 'tinymce/plugins/preview/Plugin';
 
-declare const tinymce: TinyMCE;
-
 describe('browser.tinymce.plugins.preview.PreviewContentCssTest', () => {
-  context('No bundling', () => {
-    const hook = TinyHooks.bddSetupLight<Editor>({
-      plugins: 'preview',
-      base_url: '/project/tinymce/js/tinymce',
-      content_css: '/project/tinymce/js/tinymce/skins/content/default/content.css'
-    }, [ Plugin ]);
+  const hook = TinyHooks.bddSetupLight<Editor>({
+    plugins: 'preview',
+    base_url: '/project/tinymce/js/tinymce',
+    content_css: '/project/tinymce/js/tinymce/skins/content/default/content.css'
+  }, [ Plugin ]);
 
-    const assertIframeHtmlContains = (editor: Editor, text: string) => {
-      const actual = IframeContent.getPreviewHtml(editor);
-      const regexp = new RegExp(text);
+  const assertIframeHtmlContains = (editor: Editor, text: string) => {
+    const actual = IframeContent.getPreviewHtml(editor);
+    const regexp = new RegExp(text);
 
-      assert.match(actual, regexp, 'Should be the same html');
-    };
+    assert.match(actual, regexp, 'Should be the same html');
+  };
 
-    it('TBA: Set content, set content_css_cors and assert link elements. Delete setting and assert crossOrigin attr is removed', () => {
-      const editor = hook.editor();
-      const contentCssUrl = editor.documentBaseURI.toAbsolute('/project/tinymce/js/tinymce/skins/content/default/content.css');
+  it('TBA: Set content, set content_css_cors and assert link elements. Delete setting and assert crossOrigin attr is removed', () => {
+    const editor = hook.editor();
+    const contentCssUrl = editor.documentBaseURI.toAbsolute('/project/tinymce/js/tinymce/skins/content/default/content.css');
 
-      editor.setContent('<p>hello world</p>');
-      editor.options.set('content_css_cors', true);
-      assertIframeHtmlContains(editor, `<link type="text/css" rel="stylesheet" href="${contentCssUrl}" crossorigin="anonymous">`);
-      editor.options.set('content_css_cors', false);
-      assertIframeHtmlContains(editor, `<link type="text/css" rel="stylesheet" href="${contentCssUrl}">`);
-    });
-  });
-
-  context('Bundling', () => {
-    Arr.each([
-      { label: 'Single bundled content CSS', contentCss: [ 'dark' ] },
-      { label: 'Multiple bundled content CSS', contentCss: [ 'dark', 'default' ] },
-    ], ({ label, contentCss }) => {
-      context(label, () => {
-        const baseUrl = '/project/tinymce/js/tinymce';
-        const defaultSkinUiContentUrl = `${baseUrl}/skins/ui/oxide/content.js`;
-        const settings = {
-          plugins: 'preview',
-          base_url: baseUrl,
-          content_css: contentCss
-        };
-
-        const resourceKeys = Arr.map(contentCss, (item) => `content/${item}/content.css`);
-        const resourceUrls = Arr.map(contentCss, (item) => `${baseUrl}/skins/content/${item}/content.js`);
-
-        before(async () => {
-          const scriptsLoaded = tinymce.ScriptLoader.loadScripts([ ...resourceUrls, defaultSkinUiContentUrl ])
-            .catch((errors) => {
-              assert.fail(`Unable to load scripts: ${errors}`);
-            });
-          await scriptsLoaded;
-
-          for (const key of resourceKeys) {
-            assert.isTrue(tinymce.Resource.has(key));
-          }
-        });
-
-        after(() => {
-          Arr.each(resourceUrls, (url) => tinymce.ScriptLoader.remove(url));
-          Arr.each(resourceKeys, tinymce.Resource.unload);
-        });
-
-        it(`TINY-13190: Load bundled ${contentCss} in Preview Iframe`, async () => {
-          const editor = await McEditor.pFromSettings<Editor>({
-            ...settings
-          });
-          const cleanup = Fun.noop;
-
-          for (const key of resourceKeys) {
-            assert.include(IframeContent.getPreviewHtml(editor), `<style type="text/css">${tinymce.Resource.get(key)}</style>`);
-          }
-
-          McEditor.remove(editor);
-          cleanup();
-        });
-      });
-    });
+    editor.setContent('<p>hello world</p>');
+    editor.options.set('content_css_cors', true);
+    assertIframeHtmlContains(editor, `<link type="text/css" rel="stylesheet" href="${contentCssUrl}" crossorigin="anonymous">`);
+    editor.options.set('content_css_cors', false);
+    assertIframeHtmlContains(editor, `<link type="text/css" rel="stylesheet" href="${contentCssUrl}">`);
   });
 });
