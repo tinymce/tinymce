d7d3dcc1ec26e10f995ea457a373bb8b0aafb8d7
Spocke <spocke@moxiecode.com>
2026-01-30 15:10:59 +0100

    TINY-13689: Increased timeout to 10s

--- modules/agar/src/main/ts/ephox/agar/http/MockHttpHook.ts
+++ modules/agar/src/main/ts/ephox/agar/http/MockHttpHook.ts
@@ -1,8 +1,6 @@
 import { after, before } from '@ephox/bedrock-client';
 import { Singleton } from '@ephox/katamari';
 
-import * as Waiter from '../api/Waiter';
-
 import * as HttpHandler from './HttpHandler';
 import * as MockClient from './MockClient';
 import type * as Shared from './Shared';
@@ -23,6 +21,9 @@ export const mockHttpHook = <T>(
   const state = Singleton.value<T>();
 
   before(async function () {
+    // The default timeout is 2s, that isn't enough on slow connections
+    this.timeout(10000);
+
     if (!window.isSecureContext) {
       this.skip();
     }
@@ -33,11 +34,9 @@ export const mockHttpHook = <T>(
       ...config,
       handler: (request, abortSignal) => HttpHandler.resolveRequest(requestHandlers, request, abortSignal)
     });
-    await Waiter.pWait(100);
   });
 
   after(async () => {
-    await Waiter.pWait(100);
     await MockClient.stopMocking();
   });
 
