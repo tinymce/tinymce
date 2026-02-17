47da3d42236a06a5feacd8072770feb7c722c210
kimwoodfield <kimwoodfield@live.com>
2026-02-18 09:46:54 +1000

    TINY-13470: Delete ToggleSwitch component

--- modules/oxide-components/src/main/ts/main.ts
+++ modules/oxide-components/src/main/ts/main.ts
@@ -16,7 +16,6 @@ import { IconButton } from './components/iconbutton/IconButton';
 import * as Menu from './components/menu/Menu';
 import * as MenuRenderer from './components/menu/MenuRenderer';
 import { SegmentedControl } from './components/segmentedcontrol/SegmentedControl';
-import { ToggleSwitch } from './components/toggleswitch/ToggleSwitch';
 import { ToolbarInputForm } from './components/toolbarInputForm/ToolbarInputForm';
 import { useUniverse } from './contexts/UniverseContext/Universe';
 import { UniverseProvider } from './contexts/UniverseContext/UniverseProvider';
@@ -50,7 +49,6 @@ export {
   Menu,
   MenuRenderer,
   SegmentedControl,
-  ToggleSwitch,
   UniverseProvider,
   useUniverse,
   UserPromptBubble,
