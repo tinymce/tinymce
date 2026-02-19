f286f9360955419ec35009d9af896e3e2bf9b6e0
ephox <is-accounts@tiny.cloud>
2026-02-19 15:09:10 +0000

    Updates

---
+++ modules/oxide-components/assets/IconButton.stories-BBJeAnsg.js
@@ -0,0 +1,19 @@
+import{j as t}from"./iframe-DLYGhbkQ.js";import{I as n}from"./IconButton-BNRFua_c.js";import{U as i}from"./UniverseProvider-DT0FiHTz.js";import{g as c}from"./Obj-BYXNADYo.js";import"./preload-helper-PPVm8Dsz.js";import"./Button-Dh2ZR_Tw.js";import"./Bem-BJplfvtK.js";import"./Optional-CuHnBc-7.js";import"./Strings-CwVUhXDr.js";import"./Fun--VEwoXIw.js";import"./Icon-BwE2LRuv.js";const m={"left-arrow":`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M15.7071 4.29289C15.3166 3.90237 14.6834 3.90237 14.2929 4.29289L6.58579 12L14.2929 19.7071C14.6834 20.0976 15.3166 20.0976 15.7071 19.7071C16.0976 19.3166 16.0976 18.6834 15.7071 18.2929L9.41421 12L15.7071 5.70711C16.0976 5.31658 16.0976 4.68342 15.7071 4.29289Z"/>
+</svg>`},d={getIcon:s=>c(m,s).getOrDie("Failed to get icon")},k={title:"components/IconButton",component:n,decorators:[s=>t.jsx(i,{resources:d,children:t.jsx(s,{})})],parameters:{layout:"centered"},tags:["autodocs"],args:{icon:"left-arrow"}},r={args:{variant:"primary"}},e={args:{variant:"secondary"}},a={args:{variant:"outlined"}},o={args:{variant:"naked"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary'
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'outlined'
+  }
+}`,...a.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'naked'
+  }
+}`,...o.parameters?.docs?.source}}};const j=["Primary","Secondary","Outlined","Naked"];export{o as Naked,a as Outlined,r as Primary,e as Secondary,j as __namedExportsOrder,k as default};
--- modules/oxide-components/assets/client-ByXc8kw3.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./iframe-CqtGEMvu.js";var r={},t;function a(){if(t)return r;t=1;var e=o();return r.createRoot=e.createRoot,r.hydrateRoot=e.hydrateRoot,r}var n=a();export{n as c};
