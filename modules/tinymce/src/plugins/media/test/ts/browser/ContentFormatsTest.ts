44fcf148a080181b321f13d504b6070f4bbb0809
Mitchell Crompton <mitchell.crompton@tiny.cloud>
2026-03-09 14:10:00 +1100

    TINY-9655: Prevent valid iframe, script, style, and meta elements from being removed by DomPurify (#10950)
    * TINY-9655: Restore iframe content after DOMPurify sanitization
    
    * TINY-9655: Prevent valid iframe, script, meta, and style elements from being removed by DomPurify
    
    * TINY-9655: Update tests which expect iframe to be removed by DomPurify
    
    * TINY-9655: Update tests which expect valid script tags to be removed by DomPurify
    
    * TINY-9655: Add changelogs
    
    * TINY-9655: Add test
    
    * TINY-9655: Remove handling of style and meta tags as unnecessary
    
    * TINY-9655: Fix lint error
    
    * TINY-9655: Apply suggestions

--- modules/tinymce/src/plugins/media/test/ts/browser/ContentFormatsTest.ts
+++ modules/tinymce/src/plugins/media/test/ts/browser/ContentFormatsTest.ts
@@ -76,8 +76,7 @@ describe('browser.tinymce.plugins.media.ContentFormatsTest', () => {
     McEditor.remove(editor);
   });
 
-  it('TBA: Iframe with innerHTML retained as is with xss_sanitization: false', async () => {
-    // TINY-8363: Iframe with innerHTML is removed by DOMPurify, so disable sanitization for this test
+  it('TINY-8363: Iframe with innerHTML retained as is with xss_sanitization: false', async () => {
     const editor = await McEditor.pFromSettings<Editor>({
       ...settings,
       xss_sanitization: false
@@ -87,6 +86,17 @@ describe('browser.tinymce.plugins.media.ContentFormatsTest', () => {
     );
     TinyAssertions.assertContent(editor,
       '<p><iframe src="320x240.ogg" width="300" height="150" sandbox="" allowfullscreen="allowfullscreen">text<a href="#">link</a></iframe></p>'
+    );
+    McEditor.remove(editor);
+  });
+
+  it('TINY-9655: Iframe with innerHTML has contents removed', async () => {
+    const editor = await McEditor.pFromSettings<Editor>(settings);
+    editor.setContent(
+      '<iframe src="320x240.ogg" allowfullscreen>text<a href="#">link</a></iframe>'
+    );
+    TinyAssertions.assertContent(editor,
+      '<p><iframe src="320x240.ogg" width="300" height="150" sandbox="" allowfullscreen="allowfullscreen"></iframe></p>'
     );
     McEditor.remove(editor);
   });
@@ -168,7 +178,7 @@ describe('browser.tinymce.plugins.media.ContentFormatsTest', () => {
     );
     testXss(
       '<p><video><script><svg onload="javascript:alert(1)"></svg></s' + 'cript></video>',
-      '<p><video width="300" height="150"></video></p>'
+      '<p><video width="300" height="150">\n<script></s' + 'cript>\n</video></p>'
     );
     testXss(
       '<p><audio><noscript><svg onload="javascript:alert(1)"></svg></noscript></audio>',
@@ -176,8 +186,8 @@ describe('browser.tinymce.plugins.media.ContentFormatsTest', () => {
     );
     testXss(
       '<p><audio><script><svg onload="javascript:alert(1)"></svg></s' + 'cript></audio>',
-      '<p><audio></audio></p>'
+      '<p><audio>\n<script></s' + 'cript>\n</audio></p>'
     );
-    testXss('<p><audio><script><svg></svg></script></audio>', '<p><audio></audio></p>');
+    testXss('<p><audio><script><svg></svg></script></audio>', '<p><audio>\n<script></s' + 'cript>\n</audio></p>');
   });
 });
