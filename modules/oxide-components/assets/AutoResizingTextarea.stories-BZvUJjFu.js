7c251382f7bc3c98a38518411ac791adb981c8a5
ephox <is-accounts@tiny.cloud>
2026-03-24 06:54:24 +0000

    Updates

---
+++ modules/oxide-components/assets/AutoResizingTextarea.stories-BZvUJjFu.js
@@ -0,0 +1,20 @@
+import{r as o,j as u}from"./iframe-B83LWLdH.js";import{A as n}from"./AutoResizingTextarea-DaFOAE90.js";import"./preload-helper-PPVm8Dsz.js";import"./Bem-lXDMPlS3.js";import"./Optional-CEyTH43r.js";import"./Strings-Bo1W-g2o.js";import"./Fun--VEwoXIw.js";import"./Obj-d-8KONHS.js";const x={title:"components/AutoResizingTextarea",component:n,parameters:{layout:"centered"},tags:["autodocs","skip-visual-testing"]},a={args:{value:"initial value"},render:e=>{const[r,s]=o.useState(e.value);return u.jsx(n,{...e,value:r,onChange:s})}},t={args:{value:"initial value"},render:e=>{const[r,s]=o.useState(e.value);return u.jsx(n,{value:r,onChange:s,minHeight:{unit:"rows",value:2}})}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: 'initial value'
+  },
+  render: args => {
+    const [value, setValue] = useState(args.value);
+    return <AutoResizingTextarea {...args} value={value} onChange={setValue} />;
+  }
+}`,...a.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: 'initial value'
+  },
+  render: args => {
+    const [value, setValue] = useState(args.value);
+    return <AutoResizingTextarea value={value} onChange={setValue} minHeight={{
+      unit: 'rows',
+      value: 2
+    }} />;
+  }
+}`,...t.parameters?.docs?.source}}};const h=["Example","StartsWith2Rows"];export{a as Example,t as StartsWith2Rows,h as __namedExportsOrder,x as default};
