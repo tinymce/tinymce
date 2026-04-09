871f90ef4b4c3a97eb548ecba618cbdd43023254
Hannes Frimmel Moström <hannes.mostrom@tiny.cloud>
2026-04-09 17:14:52 +0200

    TINY-10283: Fixed undo history issue.

--- modules/tinymce/src/core/main/ts/content/SetContent.ts
+++ modules/tinymce/src/core/main/ts/content/SetContent.ts
@@ -19,5 +19,15 @@ export const setContent = (editor: Editor, content: Content, args: Partial<SetCo
   preProcessSetContent(editor, defaultedArgs).each((updatedArgs) => {
     const result = Rtc.setContent(editor, updatedArgs.content, updatedArgs);
     postProcessSetContent(editor, result.html, updatedArgs);
+    /*
+      Editor must be:
+        * Initialized ( or undo can't manage to get the content, resulting in errors )
+        * Not removed, see above
+        * Have a body, see above.
+      The check for getBody returns undefined in the case the editor is in the invalid state, which is why it is used here.
+    */
+    if (editor.initialized && !editor.removed && editor.getBody()) {
+      editor.undoManager.add();
+    }
   });
 };
