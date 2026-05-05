1d4c86b9c516160ef5382800a4351089da3beb7a
spocke <johan.sorlin@tiny.cloud>
2026-05-05 10:07:07 +0200

    TINY-14357: Fixed data-mce-object injection (#19)

--- modules/tinymce/src/plugins/media/main/ts/core/Sanitize.ts
+++ modules/tinymce/src/plugins/media/main/ts/core/Sanitize.ts
@@ -4,11 +4,11 @@ import AstNode from 'tinymce/core/api/html/Node';
 import * as Options from '../api/Options';
 import { Parser } from './Parser';
 
-const parseAndSanitize = (editor: Editor, context: string, html: string): AstNode => {
+const parseAndSanitize = (editor: Editor, html: string): AstNode => {
   const getEditorOption = editor.options.get;
   const sanitize = getEditorOption('xss_sanitization');
   const validate = Options.shouldFilterHtml(editor);
-  return Parser(editor.schema, { sanitize, validate }).parse(html, { context });
+  return Parser(editor.schema, { sanitize, validate }).parse(html);
 };
 
 export {
