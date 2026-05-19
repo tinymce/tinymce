710d12b7e0d04f9a17a7cb8c6c2aa2f2c8ef580d
MitchCTiny <mitchell.crompton@tiny.cloud>
2026-05-19 12:25:38 +1000

    TINY-14334: Lint fix for 7.9.3 patch release

--- modules/tinymce/src/core/main/ts/dom/DomSerializerFilters.ts
+++ modules/tinymce/src/core/main/ts/dom/DomSerializerFilters.ts
@@ -7,8 +7,6 @@ import * as Zwsp from '../text/Zwsp';
 import { DomSerializerSettings } from './DomSerializerImpl';
 import * as RemoveTrailingBr from './RemoveTrailingBr';
 
-declare const unescape: any;
-
 const register = (htmlParser: DomParser, settings: DomSerializerSettings, dom: DOMUtils): void => {
   // Convert tabindex back to elements when serializing contents
   htmlParser.addAttributeFilter('data-mce-tabindex', (nodes, name) => {
