f0f3bc22bf0803038765a9a663aa3d224d9df37a
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2026-05-22 10:31:30 +0200

    TINY-14255: fix regression (#11119)
    * TINY-14255: fix regression
    
    * TINY-14255: refactor tests to wrok on MacOS too
    
    * TINY-14255: refactor
    
    * TINY-14255: refactor to have a better feedback on the test
    
    * TINY-14255: fix test

--- modules/tinymce/src/core/test/ts/browser/undo/UndoKeyboardShortcutsTest.ts
+++ modules/tinymce/src/core/test/ts/browser/undo/UndoKeyboardShortcutsTest.ts
@@ -79,4 +79,66 @@ describe('browser.tinymce.core.undo.UndoKeyboardShortcutTest', () => {
     TinyAssertions.assertContent(editor, '<p>abc</p>');
     TinyAssertions.assertSelection(editor, [ 0, 0 ], 1, [ 0, 0 ], 3);
   });
+
+  const simulateRealBackspaceViaKeyboardWithCtrl = (editor: Editor) => {
+    const isMac = platform.os.isMacOS();
+    const modKeyCode = isMac ? Keys.meta() : Keys.control();
+    const modifier = isMac ? { meta: true, metaKey: true } : { ctrl: true, ctrlKey: true };
+    TinyContentActions.keydown(editor, Keys.backspace(), modifier);
+    // The browser would now delete "def" itself. Reproduce that DOM mutation:
+    (editor.getBody().firstChild as HTMLElement).textContent = 'abc ';
+    TinySelections.setCursor(editor, [ 0, 0 ], 'abc '.length);
+    TinyContentActions.keyup(editor, Keys.backspace(), modifier);
+    TinyContentActions.keyup(editor, modKeyCode, {});
+  };
+
+  const simulateRealDeleteViaKeyboardWithCtrl = (editor: Editor) => {
+    const isMac = platform.os.isMacOS();
+    const modKeyCode = isMac ? Keys.meta() : Keys.control();
+    const modifier = isMac ? { meta: true, metaKey: true } : { ctrl: true, ctrlKey: true };
+    TinyContentActions.keydown(editor, Keys.delete(), modifier);
+    // The browser would now delete "def" itself. Reproduce that DOM mutation:
+    (editor.getBody().firstChild as HTMLElement).textContent = 'abc ';
+    TinySelections.setCursor(editor, [ 0, 0 ], 'abc '.length);
+    TinyContentActions.keyup(editor, Keys.delete(), modifier);
+    TinyContentActions.keyup(editor, modKeyCode, {});
+  };
+
+  // this is refered to the case in `TINY-8910`
+  it('TINY-14255: undo after ctrl+backspace/delete should put the caret in the correct position', async () => {
+    // backspace
+    const editor = hook.editor();
+    editor.resetContent('<p>abc 001</p>');
+    TinySelections.setCursor(editor, [ 0, 0 ], 'abc 001'.length);
+    simulateRealBackspaceViaKeyboardWithCtrl(editor);
+    TinyAssertions.assertContent(editor, 'abc ', { format: 'text' });
+    editor.execCommand('Undo');
+    TinyAssertions.assertContent(editor, '<p>abc 001</p>');
+    TinyAssertions.assertCursor(editor, [ 0, 0 ], 'abc 001'.length);
+
+    editor.resetContent('<p>abc 002</p>');
+    TinySelections.setCursor(editor, [ 0, 0 ], 'abc 002'.length);
+    simulateRealBackspaceViaKeyboardWithCtrl(editor);
+    TinyAssertions.assertContent(editor, 'abc ', { format: 'text' });
+    undoKeystrokeRealistic(editor);
+    TinyAssertions.assertContent(editor, '<p>abc 002</p>');
+    TinyAssertions.assertCursor(editor, [ 0, 0 ], 'abc 002'.length);
+
+    // delete
+    editor.resetContent('<p>abc 003</p>');
+    TinySelections.setCursor(editor, [ 0, 0 ], 'abc '.length);
+    simulateRealDeleteViaKeyboardWithCtrl(editor);
+    TinyAssertions.assertContent(editor, 'abc ', { format: 'text' });
+    editor.execCommand('Undo');
+    TinyAssertions.assertContent(editor, '<p>abc 003</p>');
+    TinyAssertions.assertCursor(editor, [ 0, 0 ], 'abc '.length);
+
+    editor.resetContent('<p>abc 004</p>');
+    TinySelections.setCursor(editor, [ 0, 0 ], 'abc '.length);
+    simulateRealDeleteViaKeyboardWithCtrl(editor);
+    TinyAssertions.assertContent(editor, 'abc ', { format: 'text' });
+    undoKeystrokeRealistic(editor);
+    TinyAssertions.assertContent(editor, '<p>abc 004</p>');
+    TinyAssertions.assertCursor(editor, [ 0, 0 ], 'abc '.length);
+  });
 });
