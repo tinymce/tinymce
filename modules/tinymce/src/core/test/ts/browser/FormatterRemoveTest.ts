9ddc123f1b471bbde7773e1f30f8c4c6c0108547
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2026-06-08 15:20:24 +0200

    TINY-10283: revert a couple of test to the original version

--- modules/tinymce/src/core/test/ts/browser/FormatterRemoveTest.ts
+++ modules/tinymce/src/core/test/ts/browser/FormatterRemoveTest.ts
@@ -8,7 +8,6 @@ import { ZWSP } from 'tinymce/core/text/Zwsp';
 
 import * as KeyUtils from '../module/test/KeyUtils';
 
-import * as TableTestUtils from './TableTestUtils';
 
 describe('browser.tinymce.core.FormatterRemoveTest', () => {
   const browser = PlatformDetection.detect().browser;
@@ -577,8 +576,10 @@ describe('browser.tinymce.core.FormatterRemoveTest', () => {
 
   it('Remove format on node outside fake table selection', () => {
     const editor = hook.editor();
-    editor.setContent('<p><strong>test</strong></p><table><tbody><tr><td><strong>cell 1</strong></td><td>cell 2</td></tr><tr><td><strong>cell 3</strong></td><td>cell 4</td></tr></tbody></table>');
-    TableTestUtils.selectCells(editor, [ 1 ], [[ 0, 0 ], [ 0, 1 ]]);
+    editor.undoManager.ignore(() =>
+      editor.setContent('<p><strong>test</strong></p><table><tbody><tr><td data-mce-selected="1"><strong>cell 1</strong></td><td>cell 2</td></tr><tr><td data-mce-selected="1"><strong>cell 3</strong></td><td>cell 4</td></tr></tbody></table>')
+    );
+    LegacyUnit.setSelection(editor, 'td', 0, 'td', 0);
     const para = editor.dom.select('p')[0];
     // Remove bold on custom node
     editor.formatter.remove('bold', { }, para);
