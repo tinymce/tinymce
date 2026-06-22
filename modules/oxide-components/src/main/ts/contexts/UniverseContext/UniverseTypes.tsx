277a4f776063b66648e971563166c86edaf77eb0
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2026-06-22 10:33:57 +0200

    TINYMCE-14507: remove duplication

--- modules/oxide-components/src/main/ts/contexts/UniverseContext/UniverseTypes.tsx
+++ modules/oxide-components/src/main/ts/contexts/UniverseContext/UniverseTypes.tsx
@@ -4,11 +4,6 @@ export interface TooltipTriggerRef {
   readonly set: (value: string | null) => void;
   readonly subscribe: (callback: () => void) => () => void;
 }
-export interface TooltipTriggerRef {
-  readonly get: () => string | null;
-  readonly set: (value: string | null) => void;
-  readonly subscribe: (callback: () => void) => () => void;
-}
 
 export interface UniverseResources {
   readonly getIcon: (name: string) => string;
