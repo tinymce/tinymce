a48a266147845736b3081688dc6330d9ce74fc28
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2026-07-01 17:19:16 +0200

    TINYMCE-14527: update size constraints

--- modules/tinymce/src/themes/silver/main/ts/ui/sidebar/SidebarResize.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/sidebar/SidebarResize.ts
@@ -4,10 +4,10 @@ import { numToPx } from '../sizing/Utils';
 
 export const requestedWidthProperty = '--tox-private-requested-sidebar-width';
 
-export const minWidth = 200;
-export const maxWidth = 600;
+export const minWidth = 300;
+export const maxWidth = 800;
 
-export const initialWidth = 300;
+export const initialWidth = 440;
 
 export const applyWidth = (sidebar: SugarElement<HTMLElement>, width: number): void => {
   Css.set(sidebar, requestedWidthProperty, numToPx(width));
