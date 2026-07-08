0420aca9bd828d56ac4d9baacaefb5a7d4e65d0e
lorenzo-pomili <pomili.lorenzo85@gmail.com>
2026-07-08 08:17:26 +0200

    TINYMCE-14490: fix  edge case of `TINY-13886` (#11172)
    * TINYMCE-14490: fix  edge case of `TINY-13886`
    
    * TINYMCE-14490: improve check
    
    * TINYMCE-14490: apply test only for affected browser
    
    * TINYMCE-14490: refactor
    
    * TINYMCE-14490: tests refactor
    
    * TINYMCE-14490: refactor

--- modules/tinymce/src/plugins/accordion/test/ts/browser/QuirksTest.ts
+++ modules/tinymce/src/plugins/accordion/test/ts/browser/QuirksTest.ts
@@ -1,8 +1,9 @@
-import { describe, it } from '@ephox/bedrock-client';
+import { Keys } from '@ephox/agar';
+import { before, context, describe, it } from '@ephox/bedrock-client';
 import { Arr } from '@ephox/katamari';
 import { PlatformDetection } from '@ephox/sand';
 import { PredicateFind, SugarElement, SugarNode } from '@ephox/sugar';
-import { TinyAssertions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
+import { TinyAssertions, TinyContentActions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
 
 import type Editor from 'tinymce/core/api/Editor';
 import Plugin from 'tinymce/plugins/accordion/Plugin';
@@ -34,74 +35,96 @@ describe('browser.tinymce.plugins.accordion.QuirksTest', () => {
     TinyAssertions.assertCursor(editor, [ 1, 0 ], 0);
   });
 
-  const testClickOnRightSideOfLI = async (editor: Editor, { content, path, offset }: { content: string; path: number[]; offset: number }) => {
-    editor.setContent(content);
-
-    const li = editor.dom.select('li')[0];
-    const firstChild = SugarElement.fromDom(li.firstChild as Node);
-    const textNode = SugarNode.isText(firstChild) ? firstChild.dom : PredicateFind.descendant(firstChild, SugarNode.isText).getOrDie().dom;
-    const rng = editor.getDoc().createRange();
-    rng.setStart(textNode, 0);
-    rng.setEnd(textNode, textNode.data.length);
-    const rect = rng.getClientRects()[0];
-
-    if (content.includes('<img')) {
-      const img = editor.dom.select('img')[0];
-      if (img && !img.complete) {
-        await new Promise((resolve) => {
-          img.onload = resolve;
-          img.onerror = resolve;
-        });
+  context('TINY-13886: nested list Chrome selection quirk', () => {
+    before(function () {
+      if (browser.isFirefox() || browser.isSafari()) {
+        this.skip();
       }
-    }
+    });
 
-    const mouseEvent = {
-      target: li as EventTarget,
-      clientX: rect.right + 100,
-      clientY: rect.top + rect.height / 2
-    } as MouseEvent;
-    editor.dispatch('mousedown', mouseEvent);
-    editor.dispatch('click', mouseEvent);
-    editor.dispatch('mouseup', mouseEvent);
+    const testClickOnRightSideOfLI = async (editor: Editor, { content, path, offset }: { content: string; path: number[]; offset: number }) => {
+      const li = editor.dom.select('li')[0];
+      const firstChild = SugarElement.fromDom(li.firstChild as Node);
+      const textNode = SugarNode.isText(firstChild) ? firstChild.dom : PredicateFind.descendant(firstChild, SugarNode.isText).getOrDie().dom;
+      const rng = editor.getDoc().createRange();
+      rng.setStart(textNode, 0);
+      rng.setEnd(textNode, textNode.data.length);
+      const rect = rng.getClientRects()[0];
 
-    TinyAssertions.assertCursor(editor, path, offset);
-  };
+      if (content.includes('<img')) {
+        const img = editor.dom.select('img')[0];
+        if (img && !img.complete) {
+          await new Promise((resolve) => {
+            img.onload = resolve;
+            img.onerror = resolve;
+          });
+        }
+      }
 
-  const cases = [
-    { content: '<ol><li>abc<div>def</div></li></ol>', path: [ 0, 0, 0 ], offset: 3 },
-    { content: '<ol><li>abc\n<div>def</div></li></ol>', path: [ 0, 0, 0 ], offset: 3 },
-    { content: '<ul><li>abc<br><div>def</div></li></ul>', path: [ 0, 0, 0 ], offset: 3 },
-    { content: '<ul><li>abc<div>def</div></li></ul>', path: [ 0, 0, 0 ], offset: 3 },
-    { content: '<ul><li>abc\n<div>def</div></li></ul>', path: [ 0, 0, 0 ], offset: 3 },
-    { content: '<ol><li>abc<ul><li>def</li></ul></li></ol>', path: [ 0, 0, 0 ], offset: 3 },
-    { content: '<ul><li>abc<ul><li>def</li></ul></li></ul>', path: [ 0, 0, 0 ], offset: 3 },
-    { content: '<ol><li><span style="border: 2px solid red;">abc</span><div>def</div></li></ol>', path: [ 0, 0, 0, 0 ], offset: 3 },
-    { content: '<ol><li><span style="border: 2px solid red;">abc</span>\n<div>def</div></li></ol>', path: [ 0, 0, 0, 0 ], offset: 3 },
-    { content: '<ul><li><span style="border: 2px solid red;">abc</span><div>def</div></li></ul>', path: [ 0, 0, 0, 0 ], offset: 3 },
-    { content: '<ol><li><span style="border: 2px solid red;">abc</span><ul><li>def</li></ul></li></ol>', path: [ 0, 0, 0, 0 ], offset: 3 },
-    { content: '<ul><li><span style="border: 2px solid red;">abc</span><ul><li>def</li></ul></li></ul>', path: [ 0, 0, 0, 0 ], offset: 3 },
-    { content: '<ol><li>abc<span style="display: block;">def</span></li></ol>', path: [ 0, 0, 0 ], offset: 3 },
-    { content: '<ul><li>abc<span style="display: block;">def</span></li></ul>', path: [ 0, 0, 0 ], offset: 3 },
-    { content: '<ol><li><span style="border: 2px solid red;">abc</span><span style="display: block;">def</span></li></ol>', path: [ 0, 0, 0, 0 ], offset: 3 },
-    { content: '<ul><li><span style="border: 2px solid red;">abc</span><span style="display: block;">def</span></li></ul>', path: [ 0, 0, 0, 0 ], offset: 3 },
-    { content: '<ol><li><span style="border: 2px solid red;">abc</span>\n<span style="display: block;">def</span></li></ol>', path: [ 0, 0, 1 ], offset: 1 },
-    { content: '<ol><li>a<b>b</b>c<div>def</div></li></ol>', path: [ 0, 0, 2 ], offset: 1 },
-    { content: '<ol><li>&nbsp;<span style="display: block;">def</span></li></ol>', path: [ 0, 0, 0 ], offset: 1 },
-    { content: '<ol><li>&nbsp;\n<span style="display: block;">def</span></li></ol>', path: [ 0, 0, 0 ], offset: 2 },
-    { content: '<ol><li><span style="border: 2px solid red;">&nbsp;</span><span style="display: block;">def</span></li></ol>', path: [ 0, 0, 0, 0 ], offset: 1 },
-    { content: '<ol><li><span style="border: 2px solid red;">&nbsp;</span>\n<span style="display: block;">def</span></li></ol>', path: [ 0, 0, 1 ], offset: 1 },
-    { content: '<ol><li>abc<img src="some-fake-url"><div>def</div></li></ol>', path: [ 0, 0 ], offset: 2 },
-    { content: '<ol><li>abc<img src="some-fake-url">\n<div>def</div></li></ol>', path: [ 0, 0 ], offset: 2 },
-  ];
+      const mouseEvent = {
+        target: li as EventTarget,
+        clientX: rect.right + 100,
+        clientY: rect.top + rect.height / 2
+      } as MouseEvent;
+      editor.dispatch('mousedown', mouseEvent);
+      editor.dispatch('click', mouseEvent);
+      editor.dispatch('mouseup', mouseEvent);
 
-  Arr.each(cases, ({ content, path, offset }, i) => {
-    ((browser.isFirefox() || browser.isSafari()) ?
-      it.skip :
-      it
-    )(`TINY-13886: clicking on the right of the first element (which must be an inline element) of li that also have a block element inside should place the caret at the end of the first element (${i}, ${content})`,
-      async () => {
-        const editor = hook.editor();
-        await testClickOnRightSideOfLI(editor, { content, path, offset });
-      });
+      TinyAssertions.assertCursor(editor, path, offset);
+    };
+
+    const cases = [
+      { content: '<ol><li>abc<div>def</div></li></ol>', path: [ 0, 0, 0 ], offset: 3 },
+      { content: '<ol><li>abc\n<div>def</div></li></ol>', path: [ 0, 0, 0 ], offset: 3 },
+      { content: '<ul><li>abc<br><div>def</div></li></ul>', path: [ 0, 0, 0 ], offset: 3 },
+      { content: '<ul><li>abc<div>def</div></li></ul>', path: [ 0, 0, 0 ], offset: 3 },
+      { content: '<ul><li>abc\n<div>def</div></li></ul>', path: [ 0, 0, 0 ], offset: 3 },
+      { content: '<ol><li>abc<ul><li>def</li></ul></li></ol>', path: [ 0, 0, 0 ], offset: 3 },
+      { content: '<ul><li>abc<ul><li>def</li></ul></li></ul>', path: [ 0, 0, 0 ], offset: 3 },
+      { content: '<ol><li><span style="border: 2px solid red;">abc</span><div>def</div></li></ol>', path: [ 0, 0, 0, 0 ], offset: 3 },
+      { content: '<ol><li><span style="border: 2px solid red;">abc</span>\n<div>def</div></li></ol>', path: [ 0, 0, 0, 0 ], offset: 3 },
+      { content: '<ul><li><span style="border: 2px solid red;">abc</span><div>def</div></li></ul>', path: [ 0, 0, 0, 0 ], offset: 3 },
+      { content: '<ol><li><span style="border: 2px solid red;">abc</span><ul><li>def</li></ul></li></ol>', path: [ 0, 0, 0, 0 ], offset: 3 },
+      { content: '<ul><li><span style="border: 2px solid red;">abc</span><ul><li>def</li></ul></li></ul>', path: [ 0, 0, 0, 0 ], offset: 3 },
+      { content: '<ol><li>abc<span style="display: block;">def</span></li></ol>', path: [ 0, 0, 0 ], offset: 3 },
+      { content: '<ul><li>abc<span style="display: block;">def</span></li></ul>', path: [ 0, 0, 0 ], offset: 3 },
+      { content: '<ol><li><span style="border: 2px solid red;">abc</span><span style="display: block;">def</span></li></ol>', path: [ 0, 0, 0, 0 ], offset: 3 },
+      { content: '<ul><li><span style="border: 2px solid red;">abc</span><span style="display: block;">def</span></li></ul>', path: [ 0, 0, 0, 0 ], offset: 3 },
+      { content: '<ol><li><span style="border: 2px solid red;">abc</span>\n<span style="display: block;">def</span></li></ol>', path: [ 0, 0, 1 ], offset: 1 },
+      { content: '<ol><li>a<b>b</b>c<div>def</div></li></ol>', path: [ 0, 0, 2 ], offset: 1 },
+      { content: '<ol><li>&nbsp;<span style="display: block;">def</span></li></ol>', path: [ 0, 0, 0 ], offset: 1 },
+      { content: '<ol><li>&nbsp;\n<span style="display: block;">def</span></li></ol>', path: [ 0, 0, 0 ], offset: 2 },
+      { content: '<ol><li><span style="border: 2px solid red;">&nbsp;</span><span style="display: block;">def</span></li></ol>', path: [ 0, 0, 0, 0 ], offset: 1 },
+      { content: '<ol><li><span style="border: 2px solid red;">&nbsp;</span>\n<span style="display: block;">def</span></li></ol>', path: [ 0, 0, 1 ], offset: 1 },
+      { content: '<ol><li>abc<img src="some-fake-url"><div>def</div></li></ol>', path: [ 0, 0 ], offset: 2 },
+      { content: '<ol><li>abc<img src="some-fake-url">\n<div>def</div></li></ol>', path: [ 0, 0 ], offset: 2 },
+    ];
+
+    Arr.each(cases, ({ content, path, offset }, i) => {
+      it(`TINY-13886: clicking on the right of the first element (which must be an inline element) of li that also have a block element inside should place the caret at the end of the first element (${i}, ${content})`,
+        async () => {
+          const editor = hook.editor();
+          editor.setContent(content);
+          await testClickOnRightSideOfLI(editor, { content, path, offset });
+        });
+    });
+
+    it(`TINYMCE-14490: inserting a new li pressing enter in an li that also has a block element inside and clicking on the right of the first li the caret should be at the end of the element`, async () => {
+      const editor = hook.editor();
+      const content = [ '<ol>' +
+        '<li>abc' +
+          '<ol>' +
+            '<li>first</li>' +
+            '<li>second</li>' +
+          '</ol>' +
+        '</li>' +
+      '</ol>' ].join('');
+      editor.setContent(content);
+      TinySelections.setCursor(editor, [ 0, 0, 0 ], 3);
+      TinyContentActions.keystroke(editor, Keys.enter());
+      await TinyContentActions.pType(editor, 'def');
+
+      await testClickOnRightSideOfLI(editor, { content, path: [ 0, 0, 0 ], offset: 3 });
+    });
   });
 });
