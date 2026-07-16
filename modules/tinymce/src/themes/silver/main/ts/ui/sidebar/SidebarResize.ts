9043e71439d6b08f3af3f4414398204411ac339f
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2026-07-16 13:12:39 +0200

    TINYMCE-14678: add resizable flag to addSidebar

--- modules/tinymce/src/themes/silver/main/ts/ui/sidebar/SidebarResize.ts
+++ modules/tinymce/src/themes/silver/main/ts/ui/sidebar/SidebarResize.ts
@@ -6,6 +6,8 @@ export const requestedWidthProperty = '--tox-private-requested-sidebar-width';
 export const minEditingAreaWidthProperty = '--tox-private-min-editing-area-width';
 export const minEditingAreaWidth = 280;
 
+export const resizableClass = 'tox-sidebar-wrap--resizable';
+
 export const applyWidth = (sidebar: SugarElement<HTMLElement>, width: number): void => {
   Css.set(sidebar, requestedWidthProperty, numToPx(width));
 };
