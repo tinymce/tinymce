7b2f8de2196aa283bb3970f6ca872a0efd361ece
Andrew Herron <thespyder@programmer.net>
2026-08-21 16:19:06 +1000

    TINYMCE-14775: More tests, less flaky tests, and the headless tests (#1795)
    * TINYMCE-14775: Enable miniature and phoenix tests. Work around a flake where `tinymce` could be undefined if the script downloaded but failed to parse.
    
    * TINYMCE-14775: Fix actual product timing bugs in the link checker
    
    * TINYMCE-14775: mismatched brackets created the context twice
    
    * TINYMCE-14775: Re-enable separate headless testing, using a bedrock flag to work around the failures with modern Chrome
    
    * TINYMCE-14775: Lower headless timeout to 10 minutes, with a retry of 15
    
    * TINYMCE-14775: Remove the string array input from VersionLoader, we decided a long time ago the object input is more stable
    
    * TINYMCE-14775: Catch an error that comes up in flaky tests, providing a more useful error message we can use to track it down
    
    * TINYMCE-14775: Code review fix - Handle cancellation separately from stream failures
    
    (cherry picked from commit da44b76577e5cad311172b233a0f3e7eeba15091)

--- modules/tinymce/src/core/test/ts/browser/html/DomParserTest.ts
+++ modules/tinymce/src/core/test/ts/browser/html/DomParserTest.ts
@@ -1721,30 +1721,30 @@ describe('browser.tinymce.core.html.DomParserTest', () => {
             testConversion('<embed src="about:blank" type="application/pdf" width="100" height="100" style="color: red;">', '<iframe src="about:blank" width="100" height="100"></iframe>'));
         });
       });
-    });
 
-    context('allow_html_in_comments', () => {
-      it('TINY-12220: Should allow html in comment elements', () => {
-        const parser = DomParser({ ...scenario.settings, allow_html_in_comments: true }, schema);
-        const serializer = HtmlSerializer({}, schema);
+      context('allow_html_in_comments', () => {
+        it('TINY-12220: Should allow html in comment elements', () => {
+          const parser = DomParser({ ...scenario.settings, allow_html_in_comments: true }, schema);
+          const serializer = HtmlSerializer({}, schema);
 
-        const initialHtml = '<!-- <b>test</b> -->';
-        const fragment = parser.parse(initialHtml);
-        const serializedHtml = serializer.serialize(fragment);
+          const initialHtml = '<!-- <b>test</b> -->';
+          const fragment = parser.parse(initialHtml);
+          const serializedHtml = serializer.serialize(fragment);
 
-        assert.equal(serializedHtml, initialHtml, 'Should match the initial HTML');
-      });
+          assert.equal(serializedHtml, initialHtml, 'Should match the initial HTML');
+        });
 
-      it('TINY-12220: Should allow html in comment if sanitize is set to false', () => {
-        const parser = DomParser({ ...scenario.settings }, schema);
-        const serializer = HtmlSerializer({}, schema);
+        it('TINY-12220: Should allow html in comment if sanitize is set to false', () => {
+          const parser = DomParser({ ...scenario.settings }, schema);
+          const serializer = HtmlSerializer({}, schema);
 
-        const initialHtml = '<p>foo<!-- <b>bar</b> --></p><!-- <b>baz</b> -->';
-        const fragment = parser.parse(initialHtml);
-        const serializedHtml = serializer.serialize(fragment);
-        const expectedHtml = scenario.isSanitizeEnabled ? '<p>foo</p>' : initialHtml;
+          const initialHtml = '<p>foo<!-- <b>bar</b> --></p><!-- <b>baz</b> -->';
+          const fragment = parser.parse(initialHtml);
+          const serializedHtml = serializer.serialize(fragment);
+          const expectedHtml = scenario.isSanitizeEnabled ? '<p>foo</p>' : initialHtml;
 
-        assert.equal(serializedHtml, expectedHtml, 'Should match the expected HTML');
+          assert.equal(serializedHtml, expectedHtml, 'Should match the expected HTML');
+        });
       });
     });
   });
