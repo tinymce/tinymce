c9a83ec775b718596d3dd95990990a02c3fba5d9
ephox <is-accounts@tiny.cloud>
2026-09-16 05:36:59 +0000

    Updates

--- modules/oxide-components/assets/Universe-C1EEQTFw.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./iframe-BA8tVn17.js";import{a as t}from"./UniverseProvider-ByLtBE8b.js";import{b as s}from"./Optional-CwIPeCD0.js";const a=()=>{const e=r.useContext(t);if(s(e))throw new Error("useUniverse must be used within a UniverseProvider");return e};export{a as u};
