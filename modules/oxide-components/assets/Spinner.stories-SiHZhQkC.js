230c2bcb54c8dc3299610ee7e86373cb2b7005be
ephox <is-accounts@tiny.cloud>
2026-09-16 00:38:32 +0000

    Updates

--- modules/oxide-components/assets/Spinner.stories-SiHZhQkC.js
+++
@@ -1,3 +0,0 @@
-import{S as e}from"./Spinner-mA3vf7E4.js";import"./iframe-BUoh9U7W.js";import"./preload-helper-PPVm8Dsz.js";import"./Bem-Du84Tdvq.js";import"./Optional-CwIPeCD0.js";import"./Strings-DwL7BECk.js";import"./Obj-BEXbhZhc.js";const i={title:"bespoke/tinymceai/Spinner",component:e,parameters:{layout:"centered"},tags:["autodocs"]},r={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...r.parameters?.docs?.source}}};const c=["SimpleSpinner"];export{r as SimpleSpinner,c as __namedExportsOrder,i as default};
